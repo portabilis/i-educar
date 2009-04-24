92a2a1764122385917cd88de21c62348a15bb911
Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
2009-04-24 23:42:14 +0000

    #16 by Davi Silva, Eriksen: Corrigido erro 404 no carregamento de estados de um país via AJAX.

--- ieducar/intranet/public_uf_xml.php
+++ ieducar/intranet/public_uf_xml.php
@@ -1,54 +1,28 @@
 <?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-	*																	     *
-	*	@author Prefeitura Municipal de Itajaí								 *
-	*	@updated 29/03/2007													 *
-	*   Pacote: i-PLB Software Público Livre e Brasileiro					 *
-	*																		 *
-	*	Copyright (C) 2006	PMI - Prefeitura Municipal de Itajaí			 *
-	*						ctima@itajai.sc.gov.br					    	 *
-	*																		 *
-	*	Este  programa  é  software livre, você pode redistribuí-lo e/ou	 *
-	*	modificá-lo sob os termos da Licença Pública Geral GNU, conforme	 *
-	*	publicada pela Free  Software  Foundation,  tanto  a versão 2 da	 *
-	*	Licença   como  (a  seu  critério)  qualquer  versão  mais  nova.	 *
-	*																		 *
-	*	Este programa  é distribuído na expectativa de ser útil, mas SEM	 *
-	*	QUALQUER GARANTIA. Sem mesmo a garantia implícita de COMERCIALI-	 *
-	*	ZAÇÃO  ou  de ADEQUAÇÃO A QUALQUER PROPÓSITO EM PARTICULAR. Con-	 *
-	*	sulte  a  Licença  Pública  Geral  GNU para obter mais detalhes.	 *
-	*																		 *
-	*	Você  deve  ter  recebido uma cópia da Licença Pública Geral GNU	 *
-	*	junto  com  este  programa. Se não, escreva para a Free Software	 *
-	*	Foundation,  Inc.,  59  Temple  Place,  Suite  330,  Boston,  MA	 *
-	*	02111-1307, USA.													 *
-	*																		 *
-	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-	header( 'Content-type: text/xml' );
+/**
+ * Retorna um XML formatado com UF e estado de um determinado país.
+ *
+ * @author  Eriksen Costa <eriksen.paixao_bs@cobra.com.br>
+ * @version SVN: $Id$
+ */
 
-	require_once( "include/clsBanco.inc.php" );
-	require_once( "include/funcoes.inc.php" );
-	echo "<?xml version=\"1.0\" encoding=\"ISO-8859-15\"?>\n<query xmlns=\"sugestoes\">\n";
-	if( is_numeric( $_GET["pais"] ) )
-	{
-		$db = new clsBanco();
-		$db->Consulta( "
-			SELECT
-				sigla_uf
-				, nome
-			FROM
-				public.uf
-			WHERE
-				idpais = {$_GET["pais"]}
-			ORDER BY
-				nome ASC
-			" );
+require_once 'include/pessoa/clsUf.inc.php';
 
-		while ( $db->ProximoRegistro() )
-		{
-			list( $cod, $nome ) = $db->Tupla();
-			echo " <uf sigla_uf=\"{$cod}\">{$nome}</uf>\n";
-		}
-	}
-	echo "</query>";
-?>
+// Id do país na tabela public.pais
+$id = isset($_GET['pais']) ? $_GET['pais'] : NULL;
+
+header('Content-type: text/xml; charset=iso-8859-1');
+print '<?xml version="1.0" encoding="iso-8859-1"?>' . PHP_EOL;
+print '<query>' . PHP_EOL;
+
+if ($id == strval(intval($id))) {
+  $uf = new clsUf();
+  $ufs = $uf->lista(NULL, NULL, $id, NULL, NULL, 'sigla_uf');
+
+  foreach ($ufs as $uf) {
+    print sprintf('  <estado sigla_uf="%s">%s</estado>' . PHP_EOL,
+      $uf['sigla_uf'], $uf['nome']);
+  }
+}
+
+print '</query>';
