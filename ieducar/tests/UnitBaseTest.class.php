9399c27a83c54459d3d8bbc3564fc02b99a7c2f7
Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
2009-05-06 15:06:42 +0000

    by Eriksen: Refactoring de código dos testes:  * Cabeçalhos seguindo os padrões de codificação  * Classes de teste Selenium codificadas em UTF-8  * UnitBasteTest incluindo arquivo da classe clsBanco, para facilitar conexão pelos testes que a extendem

--- ieducar/tests/UnitBaseTest.class.php
+++ ieducar/tests/UnitBaseTest.class.php
@@ -1,5 +1,26 @@
 <?php
 
+/*
+ * i-Educar - Sistema de gestão escolar
+ *
+ * Copyright (C) 2006  Prefeitura Municipal de Itajaí
+ *                     <ctima@itajai.sc.gov.br>
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+ * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+ * qualquer versão posterior.
+ *
+ * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
+ * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+ * do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
+ * com este programa; se não, escreva para a Free Software Foundation, Inc., no
+ * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
 /**
  * UnitBaseTest abstract class.
  *
@@ -8,12 +29,15 @@
  *
  * Abstrai o PHPUnit, diminuindo a dependência de seu uso.
  *
- * @author  Eriksen Costa <eriksen.paixao_bs@cobra.com.br>
- * @since   1.0.1
- * @version SVN: $Id$
+ * @author   Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
+ * @license  http://creativecommons.org/licenses/GPL/2.0/legalcode.pt  CC GNU GPL
+ * @package  Test
+ * @since    Classe disponível desde a versão 1.0.1
+ * @version  $Id$
  */
 
 chdir(realpath(dirname(__FILE__) . '/../') . '/intranet');
 require_once 'PHPUnit/Framework.php';
+require_once 'include/clsBanco.inc.php';
 
 abstract class UnitBaseTest extends PHPUnit_Framework_TestCase {}
