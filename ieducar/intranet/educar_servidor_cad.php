3506baf0e1c26c32413ba672fc8468f742e208ae
Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
2009-05-14 00:50:16 +0000

    #25 by Eriksen: Corrigido bug em select de funções durante cadastro de novo servidor. Correção inclui:
     * Correção e documentação do código javascript do select de funções
     * Teste funcional com Selenium para verificar correção do bug

--- ieducar/intranet/educar_servidor_cad.php
+++ ieducar/intranet/educar_servidor_cad.php
@@ -1,42 +1,47 @@
 <?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-	*																	     *
-	*	@author Prefeitura Municipal de Itajaí								 *
-	*	@updated 29/03/2007													 *
-	*   Pacote: i-PLB Software Público Livre e Brasileiro					 *
-	*																		 *
-	*	Copyright (C) 2006	PMI - Prefeitura Municipal de Itajaí			 *
-	*						ctima@itajai.sc.gov.br					    	 *
-	*																		 *
-	*	Este  programa  é  software livre, você pode redistribuí-lo e/ou	 *
-	*	modificá-lo sob os termos da Licença Pública Geral GNU, conforme	 *
-	*	publicada pela Free  Software  Foundation,  tanto  a versão 2 da	 *
-	*	Licença   como  (a  seu  critério)  qualquer  versão  mais  nova.	 *
-	*																		 *
-	*	Este programa  é distribuído na expectativa de ser útil, mas SEM	 *
-	*	QUALQUER GARANTIA. Sem mesmo a garantia implícita de COMERCIALI-	 *
-	*	ZAÇÃO  ou  de ADEQUAÇÃO A QUALQUER PROPÓSITO EM PARTICULAR. Con-	 *
-	*	sulte  a  Licença  Pública  Geral  GNU para obter mais detalhes.	 *
-	*																		 *
-	*	Você  deve  ter  recebido uma cópia da Licença Pública Geral GNU	 *
-	*	junto  com  este  programa. Se não, escreva para a Free Software	 *
-	*	Foundation,  Inc.,  59  Temple  Place,  Suite  330,  Boston,  MA	 *
-	*	02111-1307, USA.													 *
-	*																		 *
-	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-require_once ("include/clsBase.inc.php");
-require_once ("include/clsCadastro.inc.php");
-require_once ("include/clsBanco.inc.php");
-require_once( "include/pmieducar/geral.inc.php" );
+/*
+ * i-Educar - Sistema de gestão de escolas
+ *
+ * Copyright (c) 2006   Prefeitura Municipal de Itajaí
+ *                                 <ctima@itajai.sc.gov.br>
+ *
+ * Este programa é software livre; você pode redistribuÃí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+ * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+ * qualquer versão posterior.
+ *
+ * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
+ * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+ * do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
+ * com este programa; se não, escreva para a Free Software Foundation, Inc., no
+ * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+ */
 
-class clsIndexBase extends clsBase
-{
-	function Formular()
-	{
-		$this->SetTitulo( "{$this->_instituicao} i-Educar - Servidor" );
-		$this->processoAp = "635";
-	}
+/**
+ * Formulário de cadastro de servidor
+ *
+ * @author   Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
+ * @license  http://creativecommons.org/licenses/GPL/2.0/legalcode.pt  CC GNU GPL
+ * @package  Core
+ * @since    Arquivo disponível desde a versão 1.0.0
+ * @version  $Id$
+ */
+
+require_once 'include/clsBase.inc.php';
+require_once 'include/clsCadastro.inc.php';
+require_once 'include/clsBanco.inc.php';
+require_once 'include/pmieducar/geral.inc.php';
+
+
+class clsIndexBase extends clsBase {
+  public function Formular() {
+    $this->SetTitulo($this->_instituicao . " i-Educar - Servidor");
+    $this->processoAp = "635";
+  }
 }
 
 class indice extends clsCadastro
@@ -214,10 +219,10 @@ class indice extends clsCadastro
 			echo "<!--\nErro\nClasse clsCadastroEscolaridade nao encontrada\n-->";
 			$opcoes = array( "" => "Erro na geracao" );
 		}
-		
+
 		$script = "javascript:showExpansivelIframe(350, 100, 'educar_escolaridade_cad_pop.php');";
 		$script = "<img id='img_deficiencia' style='display: \'\'' src='imagens/banco_imagens/escreve.gif' style='cursor:hand; cursor:pointer;' border='0' onclick=\"{$script}\">";
-		
+
 		$this->campoLista( "ref_idesco", "Escolaridade", $opcoes, $this->ref_idesco,"",false,"",$script,false,false );
 
 		/**
@@ -542,180 +547,184 @@ $pagina->addForm( $miolo );
 // gera o html
 $pagina->MakeAll();
 ?>
-<script>
 
-	function getFuncao(id_campo)
-	{
-		var campoInstituicao = document.getElementById('ref_cod_instituicao').value;
-		var campoFuncao	= document.getElementById(id_campo);
-		campoFuncao.length = 1;
+<script type="text/javascript">
+/**
+ * Carrega as opções de um campo select de funções via Ajax
+ */
+function getFuncao(id_campo) {
+  var campoInstituicao = document.getElementById('ref_cod_instituicao').value;
+  var campoFuncao      = document.getElementById(id_campo);
+  campoFuncao.length = 1;
 
-		if( campoFuncao )
-		{
-			campoFuncao.disabled = true;
-			campoFuncao.options[0].text = 'Carregando funções';
+  if (campoFuncao) {
+    campoFuncao.disabled = true;
+    campoFuncao.options[0].text = 'Carregando funções';
 
-			var xml = new ajax(atualizaLstFuncao,id_campo);
-			xml.envia("educar_funcao_xml.php?ins="+campoInstituicao+"&professor=true");
-		}
-		else
-		{
-			campoFuncao.options[0].text = 'Selecione';
-		}
-	}
+    var xml = new ajax(atualizaLstFuncao,id_campo);
+    xml.envia("educar_funcao_xml.php?ins="+campoInstituicao+"&professor=true");
+  }
+  else {
+    campoFuncao.options[0].text = 'Selecione';
+  }
+}
 
-	function atualizaLstFuncao(xml)
-	{
+/**
+ * Parse de resultado da chamada Ajax de getFuncao(). Adiciona cada item
+ * retornado como option do select
+ */
+function atualizaLstFuncao(xml) {
+  var campoFuncao = document.getElementById(arguments[1]);
 
-		var campoFuncao = document.getElementById(arguments[1]);
+  campoFuncao.length = 1;
+  campoFuncao.options[0].text = 'Selecione uma função';
+  campoFuncao.disabled = false;
 
-		campoFuncao.length = 1;
-		campoFuncao.options[0].text = 'Selecione uma função';
-		campoFuncao.disabled = false;
+  funcaoChange(campoFuncao);
 
-		funcaoChange(campoFuncao);
-
-		var funcoes = xml.getElementsByTagName('funcao');
-		if(funcoes.length)
-		{
-			for( var i = 0; i < funcoes.length; i++ )
-			{
-				campoFuncao.options[campoFuncao.options.length] = new Option( funcoes[i].firstChild.data, funcoes[i].getAttribute('cod_funcao'),false,false);
-			}
-		}
-		else
-		{
-			campoFuncao.options[0].text = 'A instituição não possui nenhuma função';
-		}
+  var funcoes = xml.getElementsByTagName('funcao');
+  if (funcoes.length) {
+    for (var i = 0; i < funcoes.length; i++) {
+      campoFuncao.options[campoFuncao.options.length] =
+        new Option(funcoes[i].firstChild.data, funcoes[i].getAttribute('cod_funcao'), false, false);
+    }
+  }
+  else {
+    campoFuncao.options[0].text = 'A instituição não possui funções de servidores';
+  }
+}
 
 
-	}
+/**
+ * Altera a visibilidade de opções extras
+ *
+ * Quando a função escolhida para o servidor for do tipo professor, torna as
+ * opções de escolha de disciplina e cursos visíveis
+ *
+ * É um toggle on/off
+ */
+function funcaoChange(campo) {
+  var valor = campo.value.split("-");
+  var id = /[0-9]+/.exec(campo.id)[0];
+  var professor = (valor[1] == true);
 
-	function funcaoChange(campo)
-	{
-		var valor = campo.value.split("-");
-		var id = /[0-9]+/.exec(campo.id)[0];
-		var professor = valor[1] == true;
+  var campo_img  = document.getElementById('td_disciplina['+ id +']').lastChild.lastChild;
+  var campo_img2 = document.getElementById('td_curso['+ id +']').lastChild.lastChild;
 
-		var campo_img = document.getElementById('td_disciplina[0]').lastChild.lastChild;
-		var campo_img2 = document.getElementById('td_curso[0]').lastChild.lastChild;
+  // Se for professor
+  if (professor == true) {
+    setVisibility(campo_img,  true);
+    setVisibility(campo_img2, true);
+  }
+  else {
+    setVisibility(campo_img,  false);
+    setVisibility(campo_img2, false);
+  }
+}
 
-		//this.previousSibling.previousSibling.id
 
-		//professor
-		if(professor)
-		{
-			//campo_img = campo_img.lastChild;
+/**
+ * Chama as funções getFuncao e funcaoChange para todas as linhas da tabela
+ * de função de servidor
+ */
+function trocaTodasfuncoes() {
+  for (var ct = 0; ct < tab_add_1.id; ct++) {
+    // Não executa durante onload senão, funções atuais são substituídas
+    if (onloadCallOnce == false) {
+      getFuncao('ref_cod_funcao[' + ct + ']');
+    }
+    funcaoChange(document.getElementById('ref_cod_funcao[' + ct + ']'));
+  }
+}
 
-			//document.getElementById('novo_func_tab['+(/[0-9]+/.exec(campo.id))+']').value = document.getElementById('novo_func_tab['+(/[0-9]+/.exec(campo.id))+']').value == '' ? 's' : document.getElementById('novo_func_tab['+(/[0-9]+/.exec(campo.id))+']').value == 'n' ? 's' : 'n';
 
-			/*while(campo_img.tagName != 'IMG')
-			{
-				campo_img = campo_img.nextSibling;
-			}*/
-			setVisibility(campo_img, true);
-			setVisibility(campo_img2, true);
-		}
-		else
-		{
-			//campo_img = campo_img.childNodes[0];
+/**
+ * Verifica se ref_cod_instituicao existe via DOM e dá um bind no evento
+ * onchange do elemento para executar a função trocaTodasfuncoes()
+ */
+if (document.getElementById('ref_cod_instituicao')) {
+  var ref_cod_instituicao = document.getElementById('ref_cod_instituicao');
 
-			//document.getElementById('novo_func_tab['+(/[0-9]+/.exec(campo.id))+']').value = document.getElementById('novo_func_tab['+(/[0-9]+/.exec(campo.id))+']').value == '' ? 's' : document.getElementById('novo_func_tab['+(/[0-9]+/.exec(campo.id))+']').value == 'n' ? 's' : 'n';
+  // Função anônima para evento onchance do select de instituição
+  ref_cod_instituicao.onchange = function() {
+    trocaTodasfuncoes();
+    var xml = new ajax(function(){});
+    xml.envia("educar_limpa_sessao_curso_disciplina_servidor.php");
+  }
+}
 
-			/*while(campo_img.tagName != 'IMG')
-			{
-				campo_img = campo_img.nextSibling;
-			}*/
 
-			setVisibility(campo_img, false);
-			setVisibility(campo_img2, false);
-			//!getVisibility(campo)
+/**
+ * Chama as funções funcaoChange e getFuncao após a execução da função addRow
+ */
+tab_add_1.afterAddRow = function () {
+  funcaoChange(document.getElementById('ref_cod_funcao['+(tab_add_1.id - 1)+']'));
+  getFuncao('ref_cod_funcao['+(tab_add_1.id-1)+']');
+}
 
-		}
 
-	}
-	function popless()
-	{
-		var campoInstituicao = document.getElementById('ref_cod_instituicao').value;
-		var campoServidor = document.getElementById('cod_servidor').value;
-		pesquisa_valores_popless1('educar_servidor_disciplina_lst.php?ref_cod_servidor='+campoServidor+'&ref_cod_instituicao='+campoInstituicao, '');
-	}
+/**
+ * Variável de estado, deve ser checada por funções que queiram executar ou
+ * não um trecho de código apenas durante o onload
+ */
+var onloadCallOnce = true;
+window.onload = function() {
+  trocaTodasfuncoes();
+  onloadCallOnce = false;
+}
 
-	function popCurso()
-	{
-		var campoInstituicao = document.getElementById('ref_cod_instituicao').value;
-		var campoServidor = document.getElementById('cod_servidor').value;
-		pesquisa_valores_popless('educar_servidor_curso_lst.php?ref_cod_servidor='+campoServidor+'&ref_cod_instituicao='+campoInstituicao, '');
-	}
 
-	function pesquisa_valores_popless1(caminho, campo)
-	{
-		new_id = DOM_divs.length;
-		div = 'div_dinamico_' + new_id;
-		if ( caminho.indexOf( '?' ) == -1 )
-			showExpansivel( 850, 500, '<iframe src="' + caminho + '?campo=' + campo + '&div=' + div + '&popless=1" frameborder="0" height="100%" width="100%" marginheight="0" marginwidth="0" name="temp_win_popless"></iframe>', 'Pesquisa de valores' );
-		else
-			showExpansivel( 850, 500, '<iframe src="' + caminho + '&campo=' + campo + '&div=' + div + '&popless=1" frameborder="0" height="100%" width="100%" marginheight="0" marginwidth="0" name="temp_win_popless"></iframe>', 'Pesquisa de valores' );
-	}
-	
-	tab_add_1.afterAddRow = function () { funcaoChange(document.getElementById('ref_cod_funcao['+(tab_add_1.id-1)+']')); getFuncao('ref_cod_funcao['+(tab_add_1.id-1)+']');}
+function getArrayHora(hora) {
+  var array_h;
+  if (hora) {
+    array_h = hora.split(":");
+  }
+  else {
+    array_h = new Array(0,0);
+  }
 
-	window.onload = function()
-	{
-		trocaTodasfuncoes();
-	}
+  return array_h;
+}
 
-	function trocaTodasfuncoes()
-	{
-		for(var ct=0;ct<tab_add_1.id;ct++)
-		{
-			//funcaoChange(document.getElementById('ref_cod_funcao['+ct+']'));
-			funcaoChange(document.getElementById('ref_cod_funcao['+ct+']'));
-			//getFuncao('ref_cod_funcao['+ct+']');
-		}
-	}
+function acao2() {
+  var total_horas_alocadas = getArrayHora(document.getElementById('total_horas_alocadas').value);
+  var carga_horaria = (document.getElementById('carga_horaria').value).replace(',', '.');
 
-	if ( document.getElementById( 'ref_cod_instituicao' ) ) {
-		var ref_cod_instituicao = document.getElementById( 'ref_cod_instituicao' );
-		ref_cod_instituicao.onchange = function() {
-			trocaTodasfuncoes();
-			var xml = new ajax(function(){});
-			xml.envia("educar_limpa_sessao_curso_disciplina_servidor.php");}
-	}
+  if (parseFloat(total_horas_alocadas) > parseFloat(carga_horaria)) {
+    alert('Atenção, carga horária deve ser maior que horas alocadas!');
 
-	function getArrayHora(hora){
-		var array_h;
-		if(hora)
-			array_h = hora.split(":");
-		else
-			array_h = new Array(0,0);
+    return false;
+  }
+  else {
+    acao();
+  }
+}
 
-		return array_h;
+if (document.getElementById('total_horas_alocadas')) {
+  document.getElementById('total_horas_alocadas').style.textAlign='right';
+}
 
-	}
 
-	function acao2()
-	{
-		var total_horas_alocadas = getArrayHora( document.getElementById( 'total_horas_alocadas' ).value );
+function popless() {
+  var campoInstituicao = document.getElementById('ref_cod_instituicao').value;
+  var campoServidor = document.getElementById('cod_servidor').value;
+  pesquisa_valores_popless1('educar_servidor_disciplina_lst.php?ref_cod_servidor='+campoServidor+'&ref_cod_instituicao='+campoInstituicao, '');
+}
 
-		var carga_horaria = ( document.getElementById( 'carga_horaria' ).value ).replace( ',', '.' );
+function popCurso() {
+  var campoInstituicao = document.getElementById('ref_cod_instituicao').value;
+  var campoServidor = document.getElementById('cod_servidor').value;
+  pesquisa_valores_popless('educar_servidor_curso_lst.php?ref_cod_servidor='+campoServidor+'&ref_cod_instituicao='+campoInstituicao, '');
+}
 
-		//var horas_trabalhadas = Date.UTC( 1970, 01, 01, parseInt( total_horas_alocadas[0], 10 ), parseInt( total_horas_alocadas[1], 10 ), 0 );
-
-		//var total_horas = Date.UTC( 1970, 01, 01, parseInt( carga_horaria, 10 ), ( carga_horaria - parseInt( carga_horaria, 10 ) ) * 60, 0 );
-
-		if( parseFloat( total_horas_alocadas ) > parseFloat( carga_horaria ) )
-		{
-			alert( 'Atenção, carga horária deve ser maior que horas alocadas!' );
-			return false;
-
-		}
-		else
-		{
-			acao();
-		}
-	}
-
-	if ( document.getElementById('total_horas_alocadas') )
-		document.getElementById('total_horas_alocadas').style.textAlign='right';
+function pesquisa_valores_popless1(caminho, campo) {
+  new_id = DOM_divs.length;
+  div = 'div_dinamico_' + new_id;
+  if (caminho.indexOf('?') == -1) {
+    showExpansivel(850, 500, '<iframe src="' + caminho + '?campo=' + campo + '&div=' + div + '&popless=1" frameborder="0" height="100%" width="100%" marginheight="0" marginwidth="0" name="temp_win_popless"></iframe>', 'Pesquisa de valores' );
+  }
+  else {
+    showExpansivel(850, 500, '<iframe src="' + caminho + '&campo=' + campo + '&div=' + div + '&popless=1" frameborder="0" height="100%" width="100%" marginheight="0" marginwidth="0" name="temp_win_popless"></iframe>', 'Pesquisa de valores' );
+  }
+}
 </script>
