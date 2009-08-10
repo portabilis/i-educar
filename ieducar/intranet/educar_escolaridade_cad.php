e01779739e8ce7de0ac78b4f8c87d725b28bb742
Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
2009-08-10 14:16:01 +0000

    Refactoring para coding standards

--- ieducar/intranet/educar_escolaridade_cad.php
+++ ieducar/intranet/educar_escolaridade_cad.php
@@ -1,182 +1,172 @@
 <?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-	*																	     *
-	*	@author Prefeitura Municipal de Itajaí								 *
-	*	@updated 29/03/2007													 *
-	*   Pacote: i-PLB Software Público Livre e Brasileiro					 *
-	*																		 *
-	*	Copyright (C) 2006	PMI - Prefeitura Municipal de Itajaí			 *
-	*						ctima@itajai.sc.gov.br					    	 *
-	*																		 *
-	*	Este  programa  é  software livre, você pode redistribuí-lo e/ou	 *
-	*	modificá-lo sob os termos da Licença Pública Geral GNU, conforme	 *
-	*	publicada pela Free  Software  Foundation,  tanto  a versão 2 da	 *
-	*	Licença   como  (a  seu  critério)  qualquer  versão  mais  nova.	 *
-	*																		 *
-	*	Este programa  é distribuído na expectativa de ser útil, mas SEM	 *
-	*	QUALQUER GARANTIA. Sem mesmo a garantia implícita de COMERCIALI-	 *
-	*	ZAÇÃO  ou  de ADEQUAÇÃO A QUALQUER PROPÓSITO EM PARTICULAR. Con-	 *
-	*	sulte  a  Licença  Pública  Geral  GNU para obter mais detalhes.	 *
-	*																		 *
-	*	Você  deve  ter  recebido uma cópia da Licença Pública Geral GNU	 *
-	*	junto  com  este  programa. Se não, escreva para a Free Software	 *
-	*	Foundation,  Inc.,  59  Temple  Place,  Suite  330,  Boston,  MA	 *
-	*	02111-1307, USA.													 *
-	*																		 *
-	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 /**
- * @author Adriano Erik Weiguert Nagasava
+ * i-Educar - Sistema de gestão escolar
+ *
+ * Copyright (C) 2006  Prefeitura Municipal de Itajaí
+ *                     <ctima@itajai.sc.gov.br>
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+ * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+ * qualquer versão posterior.
+ *
+ * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
+ * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+ * do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
+ * com este programa; se não, escreva para a Free Software Foundation, Inc., no
+ * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * @author      Adriano Erik Weiguert Nagasava <ctima@itajai.sc.gov.br>
+ * @license     http://creativecommons.org/licenses/GPL/2.0/legalcode.pt  CC GNU GPL
+ * @package     Core
+ * @subpackage  Escolaridade
+ * @since       Arquivo disponível desde a versão 1.0.0
+ * @version     $Id$
  */
-require_once ("include/clsBase.inc.php");
-require_once ("include/clsCadastro.inc.php");
-require_once ("include/clsBanco.inc.php");
-require_once( "include/Geral.inc.php" );
-require_once( "include/pmieducar/geral.inc.php" );
+
+require_once 'include/clsBase.inc.php';
+require_once 'include/clsCadastro.inc.php';
+require_once 'include/clsBanco.inc.php';
+require_once 'include/Geral.inc.php';
+require_once 'include/pmieducar/geral.inc.php';
 
 class clsIndexBase extends clsBase
 {
-	function Formular()
-	{
-		$this->SetTitulo( "{$this->_instituicao} i-Educar - Escolaridade" );
-		$this->processoAp = "632";
-	}
+  function Formular()
+  {
+    $this->SetTitulo($this->_instituicao . ' i-Educar - Escolaridade');
+    $this->processoAp = '632';
+  }
 }
 
 class indice extends clsCadastro
 {
-	/**
-	 * Referencia pega da session para o idpes do usuario atual
-	 *
-	 * @var int
-	 */
-	var $pessoa_logada;
+  /**
+   * Referência a usuário da sessão
+   * @var int
+   */
+  var $pessoa_logada = NULL;
 
-	var $idesco;
-	var $descricao;
+  var $idesco;
+  var $descricao;
 
-	function Inicializar()
-	{
-		$retorno = "Novo";
-		@session_start();
-		$this->pessoa_logada = $_SESSION['id_pessoa'];
-		@session_write_close();
+  function Inicializar()
+  {
+    $retorno = 'Novo';
 
-		$this->idesco=$_GET["idesco"];
+    session_start();
+    $this->pessoa_logada = $_SESSION['id_pessoa'];
+    session_write_close();
 
-		$obj_permissoes = new clsPermissoes();
-		$obj_permissoes->permissao_cadastra( 632, $this->pessoa_logada, 3,  "educar_escolaridade_lst.php" );
+    $this->idesco = $_GET['idesco'];
 
-		if( is_numeric( $this->idesco ) )
-		{
+    $obj_permissoes = new clsPermissoes();
+    $obj_permissoes->permissao_cadastra(632, $this->pessoa_logada, 3, 'educar_escolaridade_lst.php');
 
-			$obj = new clsCadastroEscolaridade( $this->idesco );
-			$registro  = $obj->detalhe();
-			if( $registro )
-			{
-				foreach( $registro AS $campo => $val )	// passa todos os valores obtidos no registro para atributos do objeto
-					$this->$campo = $val;
+    if (is_numeric($this->idesco)) {
+      $obj = new clsCadastroEscolaridade($this->idesco);
+      $registro = $obj->detalhe();
 
-				if( $obj_permissoes->permissao_excluir( 632, $this->pessoa_logada, 3 ) )
-				{
-					$this->fexcluir = true;
-				}
+      if ($registro) {
+        // Passa todos os valores obtidos no registro para atributos do objeto
+        foreach($registro as $campo => $val) {
+          $this->$campo = $val;
+        }
 
-				$retorno = "Editar";
-			}
-		}
-		$this->url_cancelar = ($retorno == "Editar") ? "educar_escolaridade_det.php?idesco={$registro["idesco"]}" : "educar_escolaridade_lst.php";
-		$this->nome_url_cancelar = "Cancelar";
-		return $retorno;
-	}
+        if ($obj_permissoes->permissao_excluir(632, $this->pessoa_logada, 3)) {
+          $this->fexcluir = true;
+        }
 
-	function Gerar()
-	{
-		// primary keys
-		$this->campoOculto( "idesco", $this->idesco );
+        $retorno = 'Editar';
+      }
+    }
 
-		// foreign keys
+    $this->url_cancelar = ($retorno == 'Editar') ?
+      'educar_escolaridade_det.php?idesco=' . $registro['idesco'] :
+      'educar_escolaridade_lst.php';
 
-		// text
-		$this->campoTexto( "descricao", "Descri&ccedil;&atilde;o", $this->descricao, 30, 255, true );
+    $this->nome_url_cancelar = 'Cancelar';
 
-		// data
+    return $retorno;
+  }
 
-	}
+  function Gerar()
+  {
+    // Primary keys
+    $this->campoOculto('idesco', $this->idesco);
 
-	function Novo()
-	{
-		@session_start();
-		 $this->pessoa_logada = $_SESSION['id_pessoa'];
-		@session_write_close();
+    // Outros campos
+    $this->campoTexto('descricao', 'Descri&ccedil;&atilde;o', $this->descricao, 30, 255, TRUE);
+  }
 
-		$obj = new clsCadastroEscolaridade( null, $this->descricao );
-		$cadastrou = $obj->cadastra();
-		if( $cadastrou )
-		{
-			$this->mensagem .= "Cadastro efetuado com sucesso.<br>";
-			header( "Location: educar_escolaridade_lst.php" );
-			die();
-			return true;
-		}
+  function Novo()
+  {
+    session_start();
+    $this->pessoa_logada = $_SESSION['id_pessoa'];
+    session_write_close();
 
-		$this->mensagem = "Cadastro n&atilde;o realizado.<br>";
-		echo "<!--\nErro ao cadastrar clsCadastroEscolaridade\nvalores obrigatorios\nis_numeric( $this->idesco ) && is_string( $this->descricao )\n-->";
-		return false;
-	}
+    $obj = new clsCadastroEscolaridade(NULL, $this->descricao);
+    $cadastrou = $obj->cadastra();
 
-	function Editar()
-	{
-		@session_start();
-		 $this->pessoa_logada = $_SESSION['id_pessoa'];
-		@session_write_close();
+    if ($cadastrou) {
+      $this->mensagem .= 'Cadastro efetuado com sucesso.<br>';
+      header('Location: educar_escolaridade_lst.php');
+      die();
+    }
 
+    $this->mensagem = 'Cadastro n&atilde;o realizado.<br>';
+    return FALSE;
+  }
 
+  function Editar()
+  {
+    session_start();
+    $this->pessoa_logada = $_SESSION['id_pessoa'];
+    session_write_close();
 
-		$obj = new clsCadastroEscolaridade($this->idesco, $this->descricao);
-		$editou = $obj->edita();
-		if( $editou )
-		{
-			$this->mensagem .= "Edi&ccedil;&atilde;o efetuada com sucesso.<br>";
-			header( "Location: educar_escolaridade_lst.php" );
-			die();
-			return true;
-		}
+    $obj = new clsCadastroEscolaridade($this->idesco, $this->descricao);
+    $editou = $obj->edita();
+    if ($editou) {
+      $this->mensagem .= "Edi&ccedil;&atilde;o efetuada com sucesso.<br>";
+      header("Location: educar_escolaridade_lst.php");
+      die();
+    }
 
-		$this->mensagem = "Edi&ccedil;&atilde;o n&atilde;o realizada.<br>";
-		echo "<!--\nErro ao editar clsCadastroEscolaridade\nvalores obrigatorios\nif( is_numeric( $this->idesco ) )\n-->";
-		return false;
-	}
+    $this->mensagem = 'Edi&ccedil;&atilde;o n&atilde;o realizada.<br>';
+    return FALSE;
+  }
 
-	function Excluir()
-	{
-		@session_start();
-		 $this->pessoa_logada = $_SESSION['id_pessoa'];
-		@session_write_close();
+  function Excluir()
+  {
+    session_start();
+    $this->pessoa_logada = $_SESSION['id_pessoa'];
+    session_write_close();
 
+    $obj = new clsCadastroEscolaridade($this->idesco, $this->descricao);
+    $excluiu = $obj->excluir();
+    if ($excluiu) {
+      $this->mensagem .= 'Exclus&atilde;o efetuada com sucesso.<br>';
+      header('Location: educar_escolaridade_lst.php');
+      die();
+    }
 
-
-		$obj = new clsCadastroEscolaridade($this->idesco, $this->descricao);
-		$excluiu = $obj->excluir();
-		if( $excluiu )
-		{
-			$this->mensagem .= "Exclus&atilde;o efetuada com sucesso.<br>";
-			header( "Location: educar_escolaridade_lst.php" );
-			die();
-			return true;
-		}
-
-		$this->mensagem = "Exclus&atilde;o n&atilde;o realizada.<br>";
-		echo "<!--\nErro ao excluir clsCadastroEscolaridade\nvalores obrigatorios\nif( is_numeric( $this->idesco ) )\n-->";
-		return false;
-	}
+    $this->mensagem = 'Exclus&atilde;o n&atilde;o realizada.<br>';
+    return FALSE;
+  }
 }
 
-// cria uma extensao da classe base
+// Instancia objeto de página
 $pagina = new clsIndexBase();
-// cria o conteudo
+
+// Instancia objeto de conteúdo
 $miolo = new indice();
-// adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
-// gera o html
+
+// Atribui o conteúdo à página
+$pagina->addForm($miolo);
+
+// Gera o código HTML
 $pagina->MakeAll();
-?>
--- ieducar/intranet/educar_escolaridade_lst.php
+++ ieducar/intranet/educar_escolaridade_lst.php
@@ -1,148 +1,140 @@
 <?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-	*																	     *
-	*	@author Prefeitura Municipal de Itajaí								 *
-	*	@updated 29/03/2007													 *
-	*   Pacote: i-PLB Software Público Livre e Brasileiro					 *
-	*																		 *
-	*	Copyright (C) 2006	PMI - Prefeitura Municipal de Itajaí			 *
-	*						ctima@itajai.sc.gov.br					    	 *
-	*																		 *
-	*	Este  programa  é  software livre, você pode redistribuí-lo e/ou	 *
-	*	modificá-lo sob os termos da Licença Pública Geral GNU, conforme	 *
-	*	publicada pela Free  Software  Foundation,  tanto  a versão 2 da	 *
-	*	Licença   como  (a  seu  critério)  qualquer  versão  mais  nova.	 *
-	*																		 *
-	*	Este programa  é distribuído na expectativa de ser útil, mas SEM	 *
-	*	QUALQUER GARANTIA. Sem mesmo a garantia implícita de COMERCIALI-	 *
-	*	ZAÇÃO  ou  de ADEQUAÇÃO A QUALQUER PROPÓSITO EM PARTICULAR. Con-	 *
-	*	sulte  a  Licença  Pública  Geral  GNU para obter mais detalhes.	 *
-	*																		 *
-	*	Você  deve  ter  recebido uma cópia da Licença Pública Geral GNU	 *
-	*	junto  com  este  programa. Se não, escreva para a Free Software	 *
-	*	Foundation,  Inc.,  59  Temple  Place,  Suite  330,  Boston,  MA	 *
-	*	02111-1307, USA.													 *
-	*																		 *
-	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 /**
- * @author Adriano Erik Weiguert Nagasava
+ * i-Educar - Sistema de gestão escolar
+ *
+ * Copyright (C) 2006  Prefeitura Municipal de Itajaí
+ *                     <ctima@itajai.sc.gov.br>
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+ * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+ * qualquer versão posterior.
+ *
+ * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
+ * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+ * do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
+ * com este programa; se não, escreva para a Free Software Foundation, Inc., no
+ * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * @author      Adriano Erik Weiguert Nagasava <ctima@itajai.sc.gov.br>
+ * @license     http://creativecommons.org/licenses/GPL/2.0/legalcode.pt  CC GNU GPL
+ * @package     Core
+ * @subpackage  Escolaridade
+ * @since       Arquivo disponível desde a versão 1.0.0
+ * @version     $Id$
  */
-require_once ("include/clsBase.inc.php");
-require_once ("include/clsListagem.inc.php");
-require_once ("include/clsBanco.inc.php");
-require_once( "include/Geral.inc.php" );
-require_once( "include/pmieducar/geral.inc.php" );
+
+require_once 'include/clsBase.inc.php';
+require_once 'include/clsListagem.inc.php';
+require_once 'include/clsBanco.inc.php';
+require_once 'include/Geral.inc.php';
+require_once 'include/pmieducar/geral.inc.php';
 
 class clsIndexBase extends clsBase
 {
-	function Formular()
-	{
-		$this->SetTitulo( "{$this->_instituicao} i-Educar - Escolaridade" );
-		$this->processoAp = "632";
-	}
+  function Formular() {
+    $this->SetTitulo($this->_instituicao . ' i-Educar - Escolaridade');
+    $this->processoAp = '632';
+  }
 }
 
 class indice extends clsListagem
 {
-	/**
-	 * Referencia pega da session para o idpes do usuario atual
-	 *
-	 * @var int
-	 */
-	var $pessoa_logada;
+  /**
+   * Referência a usuário da sessão
+   * @var int
+   */
+  var $pessoa_logada = NULL;
 
-	/**
-	 * Titulo no topo da pagina
-	 *
-	 * @var int
-	 */
-	var $titulo;
+  /**
+   * Título no topo da página
+   * @var string
+   */
+  var $titulo = '';
 
-	/**
-	 * Quantidade de registros a ser apresentada em cada pagina
-	 *
-	 * @var int
-	 */
-	var $limite;
+  /**
+   * Limite de registros por página
+   * @var int
+   */
+  var $limite = 0;
 
-	/**
-	 * Inicio dos registros a serem exibidos (limit)
-	 *
-	 * @var int
-	 */
-	var $offset;
+  /**
+   * Início dos registros a serem exibidos (limit)
+   * @var int
+   */
+  var $offset = 0;
 
-	var $idesco;
-	var $descricao;
+  var $idesco;
+  var $descricao;
 
-	function Gerar()
-	{
-		@session_start();
-		$this->pessoa_logada = $_SESSION['id_pessoa'];
-		session_write_close();
+  function Gerar()
+  {
+    session_start();
+    $this->pessoa_logada = $_SESSION['id_pessoa'];
+    session_write_close();
 
-		$this->titulo = "Escolaridade - Listagem";
+    $this->titulo = 'Escolaridade - Listagem';
 
-		foreach( $_GET AS $var => $val ) // passa todos os valores obtidos no GET para atributos do objeto
-			$this->$var = ( $val === "" ) ? null: $val;
+    // Passa todos os valores obtidos no GET para atributos do objeto
+    foreach ($_GET AS $var => $val){
+      $this->$var = ($val === '') ? NULL : $val;
+    }
 
-		$this->addBanner( "imagens/nvp_top_intranet.jpg", "imagens/nvp_vert_intranet.jpg", "Intranet" );
+    $this->addBanner('imagens/nvp_top_intranet.jpg', 'imagens/nvp_vert_intranet.jpg', 'Intranet');
 
-		$this->addCabecalhos( array(
-			"Descri&ccedil;&atilde;o"
-		) );
+    $this->addCabecalhos(array(
+      'Descri&ccedil;&atilde;o'
+    ));
 
-		// Filtros de Foreign Keys
+    // Outros Filtros
+    $this->campoTexto('descricao', 'Descrição', $this->descricao, 30, 255, FALSE);
 
+    // Paginador
+    $this->limite = 20;
+    $this->offset = ($_GET['pagina_' . $this->nome]) ?
+      $_GET['pagina_' . $this->nome] * $this->limite-$this->limite : 0;
 
-		// outros Filtros
-		$this->campoTexto( "descricao", "Descrição", $this->descricao, 30, 255, false );
+    $obj_escolaridade = new clsCadastroEscolaridade();
+    $obj_escolaridade->setOrderby('descricao ASC');
+    $obj_escolaridade->setLimite($this->limite, $this->offset);
+    $lista = $obj_escolaridade->lista(NULL,
+      $this->descricao
+    );
 
+    $total = $obj_escolaridade->_total;
 
-		// Paginador
-		$this->limite = 20;
-		$this->offset = ( $_GET["pagina_{$this->nome}"] ) ? $_GET["pagina_{$this->nome}"]*$this->limite-$this->limite: 0;
+    // Monta a lista
+    if (is_array($lista) && count($lista)) {
+      foreach ($lista as $registro) {
+        $this->addLinhas(array(
+          "<a href=\"educar_escolaridade_det.php?idesco={$registro["idesco"]}\">{$registro["descricao"]}</a>"
+        ));
+      }
+    }
 
-		$obj_escolaridade = new clsCadastroEscolaridade();
-		$obj_escolaridade->setOrderby( "descricao ASC" );
-		$obj_escolaridade->setLimite( $this->limite, $this->offset );
-		$lista = $obj_escolaridade->lista( null,
-			$this->descricao
-		);
+    $this->addPaginador2('educar_escolaridade_lst.php', $total, $_GET, $this->nome, $this->limite);
+    $obj_permissoes = new clsPermissoes();
+    if ($obj_permissoes->permissao_cadastra(632, $this->pessoa_logada, 3)) {
+      $this->acao = 'go("educar_escolaridade_cad.php")';
+      $this->nome_acao = 'Novo';
+    }
 
-		$total = $obj_escolaridade->_total;
-
-		// monta a lista
-		if( is_array( $lista ) && count( $lista ) )
-		{
-			foreach ( $lista AS $registro )
-			{
-				// muda os campos data
-
-				// pega detalhes de foreign_keys
-
-				$this->addLinhas( array(
-					"<a href=\"educar_escolaridade_det.php?idesco={$registro["idesco"]}\">{$registro["descricao"]}</a>"
-				) );
-			}
-		}
-		$this->addPaginador2( "educar_escolaridade_lst.php", $total, $_GET, $this->nome, $this->limite );
-		$obj_permissoes = new clsPermissoes();
-		if( $obj_permissoes->permissao_cadastra( 632, $this->pessoa_logada, 3 ) )
-		{
-			$this->acao = "go(\"educar_escolaridade_cad.php\")";
-			$this->nome_acao = "Novo";
-		}
-
-		$this->largura = "100%";
-	}
+    $this->largura = '100%';
+  }
 }
-// cria uma extensao da classe base
+
+// Instancia objeto de página
 $pagina = new clsIndexBase();
-// cria o conteudo
+
+// Instancia objeto de conteúdo
 $miolo = new indice();
-// adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
-// gera o html
+
+// Atribui o conteúdo à página
+$pagina->addForm($miolo);
+
+// Gera o código HTML
 $pagina->MakeAll();
-?>
