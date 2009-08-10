39189783884ab00b1aa281535ca5e7fbefe6bbf9
Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
2009-08-10 14:16:01 +0000

    Refactoring para coding standards

--- ieducar/intranet/educar_escolaridade_det.php
+++ ieducar/intranet/educar_escolaridade_det.php
@@ -1,101 +1,107 @@
 <?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-	*																	     *
-	*	@author Prefeitura Municipal de Itajaí								 *
-	*	@updated 29/03/2007													 *
-	*   Pacote: i-PLB Software Público Livre e Brasileiro					 *
-	*																		 *
-	*	Copyright (C) 2006	PMI - Prefeitura Municipal de Itajaí			 *
-	*						ctima@itajai.sc.gov.br					    	 *
-	*																		 *
-	*	Este  programa  é  software livre, você pode redistribuí-lo e/ou	 *
-	*	modificá-lo sob os termos da Licença Pública Geral GNU, conforme	 *
-	*	publicada pela Free  Software  Foundation,  tanto  a versão 2 da	 *
-	*	Licença   como  (a  seu  critério)  qualquer  versão  mais  nova.	 *
-	*																		 *
-	*	Este programa  é distribuído na expectativa de ser útil, mas SEM	 *
-	*	QUALQUER GARANTIA. Sem mesmo a garantia implícita de COMERCIALI-	 *
-	*	ZAÇÃO  ou  de ADEQUAÇÃO A QUALQUER PROPÓSITO EM PARTICULAR. Con-	 *
-	*	sulte  a  Licença  Pública  Geral  GNU para obter mais detalhes.	 *
-	*																		 *
-	*	Você  deve  ter  recebido uma cópia da Licença Pública Geral GNU	 *
-	*	junto  com  este  programa. Se não, escreva para a Free Software	 *
-	*	Foundation,  Inc.,  59  Temple  Place,  Suite  330,  Boston,  MA	 *
-	*	02111-1307, USA.													 *
-	*																		 *
-	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 /**
- * @author Adriano Erik Weiguert Nagasava
+ * i-Educar - Sistema de gestão escolar
+ *
+ * Copyright (C) 2006  Prefeitura Municipal de Itajaí
+ *                     <ctima@itajai.sc.gov.br>
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+ * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+ * qualquer versão posterior.
+ *
+ * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
+ * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+ * do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
+ * com este programa; se não, escreva para a Free Software Foundation, Inc., no
+ * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * @author      Adriano Erik Weiguert Nagasava <ctima@itajai.sc.gov.br>
+ * @license     http://creativecommons.org/licenses/GPL/2.0/legalcode.pt  CC GNU GPL
+ * @package     Core
+ * @subpackage  Escolaridade
+ * @since       Arquivo disponível desde a versão 1.0.0
+ * @version     $Id$
  */
-require_once ("include/clsBase.inc.php");
-require_once ("include/clsDetalhe.inc.php");
-require_once ("include/clsBanco.inc.php");
-require_once( "include/Geral.inc.php" );
-require_once( "include/pmieducar/geral.inc.php" );
+
+require_once 'include/clsBase.inc.php';
+require_once 'include/clsDetalhe.inc.php';
+require_once 'include/clsBanco.inc.php';
+require_once 'include/Geral.inc.php';
+require_once 'include/pmieducar/geral.inc.php';
 
 class clsIndexBase extends clsBase
 {
-	function Formular()
-	{
-		$this->SetTitulo( "{$this->_instituicao} i-Educar - Escolaridade" );
-		$this->processoAp = "632";
-	}
+  function Formular()
+  {
+    $this->SetTitulo($this->_instituicao . ' i-Educar - Escolaridade');
+    $this->processoAp = '632';
+  }
 }
 
 class indice extends clsDetalhe
 {
-	/**
-	 * Titulo no topo da pagina
-	 *
-	 * @var int
-	 */
-	var $titulo;
+  /**
+   * Referência a usuário da sessão
+   * @var int
+   */
+  var $pessoa_logada = NULL;
 
-	var $idesco;
-	var $descricao;
+  /**
+   * Título no topo da página
+   * @var string
+   */
+  var $titulo = '';
 
-	function Gerar()
-	{
-		@session_start();
-		$this->pessoa_logada = $_SESSION['id_pessoa'];
-		session_write_close();
+  var $idesco;
+  var $descricao;
 
-		$this->titulo = "Escolaridade - Detalhe";
-		$this->addBanner( "imagens/nvp_top_intranet.jpg", "imagens/nvp_vert_intranet.jpg", "Intranet" );
+  function Gerar()
+  {
+    @session_start();
+    $this->pessoa_logada = $_SESSION['id_pessoa'];
+    session_write_close();
 
-		$this->idesco=$_GET["idesco"];
+    $this->titulo = 'Escolaridade - Detalhe';
+    $this->addBanner('imagens/nvp_top_intranet.jpg', 'imagens/nvp_vert_intranet.jpg', 'Intranet');
 
-		$tmp_obj = new clsCadastroEscolaridade( $this->idesco );
-		$registro = $tmp_obj->detalhe();
+    $this->idesco = $_GET['idesco'];
 
-		if( ! $registro )
-		{
-			header( "location: educar_escolaridade_lst.php" );
-			die();
-		}
+    $tmp_obj = new clsCadastroEscolaridade($this->idesco);
+    $registro = $tmp_obj->detalhe();
 
-		if( $registro["descricao"] )
-		{
-			$this->addDetalhe( array( "Descri&ccedil;&atilde;o", "{$registro["descricao"]}") );
-		}
-		$obj_permissoes = new clsPermissoes();
-		if( $obj_permissoes->permissao_cadastra( 632, $this->pessoa_logada, 3 ) )
-		{
-			$this->url_novo = "educar_escolaridade_cad.php";
-			$this->url_editar = "educar_escolaridade_cad.php?idesco={$registro["idesco"]}";
-		}
+    if (! $registro) {
+      header('Location: educar_escolaridade_lst.php');
+      die();
+    }
 
-		$this->url_cancelar = "educar_escolaridade_lst.php";
-		$this->largura = "100%";
-	}
+    if ($registro['descricao']) {
+      $this->addDetalhe(array('Descri&ccedil;&atilde;o', $registro['descricao']));
+    }
+
+    $obj_permissoes = new clsPermissoes();
+    if ($obj_permissoes->permissao_cadastra(632, $this->pessoa_logada, 3)) {
+      $this->url_novo   = 'educar_escolaridade_cad.php';
+      $this->url_editar = 'educar_escolaridade_cad.php?idesco=' . $registro['idesco'];
+    }
+
+    $this->url_cancelar = 'educar_escolaridade_lst.php';
+    $this->largura      = '100%';
+  }
 }
 
-// cria uma extensao da classe base
+// Instancia objeto de página
 $pagina = new clsIndexBase();
-// cria o conteudo
+
+// Instancia objeto de conteúdo
 $miolo = new indice();
-// adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
-// gera o html
+
+// Atribui o conteúdo à página
+$pagina->addForm($miolo);
+
+// Gera o código HTML
 $pagina->MakeAll();
-?>
