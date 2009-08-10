07cf03879c9552871ccfd46445c33953cf5c9aff
Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
2009-08-10 14:15:55 +0000

    Refactoring para Coding Standards

--- ieducar/intranet/include/pessoa/clsCadastroEscolaridade.inc.php
+++ ieducar/intranet/include/pessoa/clsCadastroEscolaridade.inc.php
@@ -1,382 +1,342 @@
 <?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-*																	     *
-*	@author Prefeitura Municipal de Itajaí								 *
-*	@updated 29/03/2007													 *
-*   Pacote: i-PLB Software Público Livre e Brasileiro					 *
-*																		 *
-*	Copyright (C) 2006	PMI - Prefeitura Municipal de Itajaí			 *
-*						ctima@itajai.sc.gov.br					    	 *
-*																		 *
-*	Este  programa  é  software livre, você pode redistribuí-lo e/ou	 *
-*	modificá-lo sob os termos da Licença Pública Geral GNU, conforme	 *
-*	publicada pela Free  Software  Foundation,  tanto  a versão 2 da	 *
-*	Licença   como  (a  seu  critério)  qualquer  versão  mais  nova.	 *
-*																		 *
-*	Este programa  é distribuído na expectativa de ser útil, mas SEM	 *
-*	QUALQUER GARANTIA. Sem mesmo a garantia implícita de COMERCIALI-	 *
-*	ZAÇÃO  ou  de ADEQUAÇÃO A QUALQUER PROPÓSITO EM PARTICULAR. Con-	 *
-*	sulte  a  Licença  Pública  Geral  GNU para obter mais detalhes.	 *
-*																		 *
-*	Você  deve  ter  recebido uma cópia da Licença Pública Geral GNU	 *
-*	junto  com  este  programa. Se não, escreva para a Free Software	 *
-*	Foundation,  Inc.,  59  Temple  Place,  Suite  330,  Boston,  MA	 *
-*	02111-1307, USA.													 *
-*																		 *
-* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-/**
-* @author Prefeitura Municipal de Itajaí
-*
-* Criado em 09/05/2006
-*/
 
+/**
+ * i-Educar - Sistema de gestão escolar
+ *
+ * Copyright (C) 2006  Prefeitura Municipal de Itajaí
+ *                     <ctima@itajai.sc.gov.br>
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+ * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+ * qualquer versão posterior.
+ *
+ * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
+ * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+ * do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
+ * com este programa; se não, escreva para a Free Software Foundation, Inc., no
+ * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * @author      Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
+ * @license     http://creativecommons.org/licenses/GPL/2.0/legalcode.pt  CC GNU GPL
+ * @package     Core
+ * @subpackage  pessoa
+ * @since       Arquivo disponível desde a versão 1.0.0
+ * @version     $Id$
+ */
+
+/**
+ * clsCadastroEscolaridade class.
+ *
+ * @author      Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
+ * @license     http://creativecommons.org/licenses/GPL/2.0/legalcode.pt  CC GNU GPL
+ * @package     Core
+ * @subpackage  pessoa
+ * @since       Classe disponível desde a versão 1.0.0
+ * @version     $Id$
+ */
 class clsCadastroEscolaridade
 {
-	var $idesco;
-	var $descricao;
+  var $idesco;
+  var $descricao;
 
+  /**
+   * Armazena o total de resultados obtidos na última chamada ao método lista.
+   * @var int
+   */
+  var $_total;
 
-	// propriedades padrao
+  /**
+   * Nome do schema.
+   * @var string
+   */
+  var $_schema;
 
-	/**
-	 * Armazena o total de resultados obtidos na ultima chamada ao metodo lista
-	 *
-	 * @var int
-	 */
-	var $_total;
+  /**
+   * Nome da tabela.
+   * @var string
+   */
+  var $_tabela;
 
-	/**
-	 * Nome do schema
-	 *
-	 * @var string
-	 */
-	var $_schema;
+  /**
+   * Lista separada por virgula, com os campos que devem ser selecionados na próxima chamado ao método lista.
+   * @var string
+   */
+  var $_campos_lista;
 
-	/**
-	 * Nome da tabela
-	 *
-	 * @var string
-	 */
-	var $_tabela;
+  /**
+   * Lista com todos os campos da tabela separados por vírgula, padrão para seleção no método lista.
+   * @var string
+   */
+  var $_todos_campos;
 
-	/**
-	 * Lista separada por virgula, com os campos que devem ser selecionados na proxima chamado ao metodo lista
-	 *
-	 * @var string
-	 */
-	var $_campos_lista;
+  /**
+   * Valor que define a quantidade de registros a ser retornada pelo método lista.
+   * @var int
+   */
+  var $_limite_quantidade;
 
-	/**
-	 * Lista com todos os campos da tabela separados por virgula, padrao para selecao no metodo lista
-	 *
-	 * @var string
-	 */
-	var $_todos_campos;
+  /**
+   * Define o valor de offset no retorno dos registros no método lista.
+   * @var int
+   */
+  var $_limite_offset;
 
-	/**
-	 * Valor que define a quantidade de registros a ser retornada pelo metodo lista
-	 *
-	 * @var int
-	 */
-	var $_limite_quantidade;
+  /**
+   * Define o campo padrão para ser usado como padrão de ordenação no método lista.
+   * @var string
+   */
+  var $_campo_order_by;
 
-	/**
-	 * Define o valor de offset no retorno dos registros no metodo lista
-	 *
-	 * @var int
-	 */
-	var $_limite_offset;
+  /**
+   * Construtor (PHP 5).
+   */
+  function __construct($idesco = NULL, $descricao = NULL)
+  {
+    $db = new clsBanco();
+    $this->_schema = "cadastro.";
+    $this->_tabela = "{$this->_schema}escolaridade";
 
-	/**
-	 * Define o campo padrao para ser usado como padrao de ordenacao no metodo lista
-	 *
-	 * @var string
-	 */
-	var $_campo_order_by;
+    $this->_campos_lista = $this->_todos_campos = "idesco, descricao";
 
+    if (is_numeric($idesco)) {
+      $this->idesco = $idesco;
+    }
+    if (is_string($descricao)) {
+      $this->descricao = $descricao;
+    }
+  }
 
-	/**
-	 * Construtor (PHP 5)
-	 *
-	 * @return object
-	 */
-	function __construct( $idesco = null, $descricao = null )
-	{
-		$db = new clsBanco();
-		$this->_schema = "cadastro.";
-		$this->_tabela = "{$this->_schema}escolaridade";
+  /**
+   * Construtor (PHP 4).
+   */
+  function clsCadastroEscolaridade( $idesco = null, $descricao = null )
+  {
+    $db = new clsBanco();
+    $this->_schema = "cadastro.";
+    $this->_tabela = "{$this->_schema}escolaridade";
 
-		$this->_campos_lista = $this->_todos_campos = "idesco, descricao";
+    $this->_campos_lista = $this->_todos_campos = "idesco, descricao";
 
-		if( is_numeric( $idesco ) )
-		{
-			$this->idesco = $idesco;
-		}
-		if( is_string( $descricao ) )
-		{
-			$this->descricao = $descricao;
-		}
+    if (is_numeric($idesco)) {
+      $this->idesco = $idesco;
+    }
+    if (is_string($descricao)) {
+      $this->descricao = $descricao;
+    }
+  }
 
-	}
-	/**
-	 * Construtor (PHP 4)
-	 *
-	 * @return object
-	 */
-	function clsCadastroEscolaridade( $idesco = null, $descricao = null )
-	{
-		$db = new clsBanco();
-		$this->_schema = "cadastro.";
-		$this->_tabela = "{$this->_schema}escolaridade";
+  /**
+   * Cria um novo registro.
+   * @return bool
+   */
+  function cadastra()
+  {
+    if (is_string($this->descricao))
+    {
+      $db = new clsBanco();
 
-		$this->_campos_lista = $this->_todos_campos = "idesco, descricao";
+      $campos  = '';
+      $valores = '';
+      $gruda   = '';
 
-		if( is_numeric( $idesco ) )
-		{
-			$this->idesco = $idesco;
-		}
-		if( is_string( $descricao ) )
-		{
-			$this->descricao = $descricao;
-		}
+      $this->idesco = $db->CampoUnico('SELECT MAX(idesco) + 1
+                      FROM cadastro.escolaridade');
 
-	}
+      if (is_numeric($this->idesco)) {
+        $campos  .= "{$gruda}idesco";
+        $valores .= "{$gruda}'{$this->idesco}'";
+        $gruda = ", ";
+      }
+      if (is_string($this->descricao)) {
+        $campos  .= "{$gruda}descricao";
+        $valores .= "{$gruda}'{$this->descricao}'";
+        $gruda = ", ";
+      }
 
-	/**
-	 * Cria um novo registro
-	 *
-	 * @return bool
-	 */
-	function cadastra()
-	{
-		if(  is_string( $this->descricao )  )
-		{
-			$db = new clsBanco();
+      $db->Consulta("INSERT INTO {$this->_tabela} ($campos) VALUES($valores)");
+      return $this->idesco;
+    }
 
-			$campos = "";
-			$valores = "";
-			$gruda = "";
+    return FALSE;
+  }
 
-			$this->idesco = $db->CampoUnico( "SELECT MAX( idesco ) + 1
-										  FROM cadastro.escolaridade" );
-			if( is_numeric( $this->idesco ) )
-			{
-				$campos .= "{$gruda}idesco";
-				$valores .= "{$gruda}'{$this->idesco}'";
-				$gruda = ", ";
-			}
-			if( is_string( $this->descricao ) )
-			{
-				$campos .= "{$gruda}descricao";
-				$valores .= "{$gruda}'{$this->descricao}'";
-				$gruda = ", ";
-			}
-			$db->Consulta( "INSERT INTO {$this->_tabela} ( $campos ) VALUES( $valores )" );
-			return $this->idesco;
-		}
-		return false;
-	}
+  /**
+   * Edita os dados de um registro.
+   * @return bool
+   */
+  function edita()
+  {
+    if (is_numeric($this->idesco)) {
+      $db  = new clsBanco();
+      $set = "";
 
-	/**
-	 * Edita os dados de um registro
-	 *
-	 * @return bool
-	 */
-	function edita()
-	{
-		if( is_numeric( $this->idesco ) )
-		{
-			$db = new clsBanco();
-			$set = "";
+      if (is_string($this->descricao)) {
+        $set  .= "{$gruda}descricao = '{$this->descricao}'";
+        $gruda = ", ";
+      }
 
-			if( is_string( $this->descricao ) )
-			{
-				$set .= "{$gruda}descricao = '{$this->descricao}'";
-				$gruda = ", ";
-			}
+      if ($set) {
+        $db->Consulta("UPDATE {$this->_tabela} SET $set WHERE idesco = '{$this->idesco}'");
+        return TRUE;
+      }
+    }
 
+    return FALSE;
+  }
 
-			if( $set )
-			{
-				$db->Consulta( "UPDATE {$this->_tabela} SET $set WHERE idesco = '{$this->idesco}'" );
-				return true;
-			}
-		}
-		return false;
-	}
+  /**
+   * Retorna uma lista de registros filtrados de acordo com os parâmetros.
+   * @return array
+   */
+  function lista($int_idesco = NULL, $str_descricao = NULL) {
+    $sql = "SELECT {$this->_campos_lista} FROM {$this->_tabela}";
+    $filtros = "";
 
-	/**
-	 * Retorna uma lista filtrados de acordo com os parametros
-	 *
-	 * @return array
-	 */
-	function lista( $int_idesco = null, $str_descricao = null )
-	{
-		$sql = "SELECT {$this->_campos_lista} FROM {$this->_tabela}";
-		$filtros = "";
+    $whereAnd = " WHERE ";
 
-		$whereAnd = " WHERE ";
+    if (is_numeric($int_idesco)) {
+      $filtros .= "{$whereAnd} idesco = '{$int_idesco}'";
+      $whereAnd = " AND ";
+    }
+    if (is_string($str_descricao)) {
+      $filtros .= "{$whereAnd} descricao ILIKE '%{$str_descricao}%'";
+      $whereAnd = " AND ";
+    }
 
-		if( is_numeric( $int_idesco ) )
-		{
-			$filtros .= "{$whereAnd} idesco = '{$int_idesco}'";
-			$whereAnd = " AND ";
-		}
-		if( is_string( $str_descricao ) )
-		{
-			$filtros .= "{$whereAnd} descricao ILIKE '%{$str_descricao}%'";
-			$whereAnd = " AND ";
-		}
+    $db = new clsBanco();
+    $countCampos = count(explode(",", $this->_campos_lista));
+    $resultado = array();
 
+    $sql .= $filtros . $this->getOrderby() . $this->getLimite();
 
-		$db = new clsBanco();
-		$countCampos = count( explode( ",", $this->_campos_lista ) );
-		$resultado = array();
+    $this->_total = $db->CampoUnico("SELECT COUNT(0) FROM {$this->_tabela} {$filtros}");
 
-		$sql .= $filtros . $this->getOrderby() . $this->getLimite();
+    $db->Consulta($sql);
 
-		$this->_total = $db->CampoUnico( "SELECT COUNT(0) FROM {$this->_tabela} {$filtros}" );
+    if ($countCampos > 1) {
+      while ($db->ProximoRegistro()) {
+        $tupla = $db->Tupla();
 
-		$db->Consulta( $sql );
+        $tupla["_total"] = $this->_total;
+        $resultado[] = $tupla;
+      }
+    }
+    else {
+      while ($db->ProximoRegistro()) {
+        $tupla = $db->Tupla();
+        $resultado[] = $tupla[$this->_campos_lista];
+      }
+    }
+    if (count($resultado)) {
+      return $resultado;
+    }
 
-		if( $countCampos > 1 )
-		{
-			while ( $db->ProximoRegistro() )
-			{
-				$tupla = $db->Tupla();
+    return FALSE;
+  }
 
-				$tupla["_total"] = $this->_total;
-				$resultado[] = $tupla;
-			}
-		}
-		else
-		{
-			while ( $db->ProximoRegistro() )
-			{
-				$tupla = $db->Tupla();
-				$resultado[] = $tupla[$this->_campos_lista];
-			}
-		}
-		if( count( $resultado ) )
-		{
-			return $resultado;
-		}
-		return false;
-	}
+  /**
+   * Retorna um array com os dados de um registro.
+   * @return array
+   */
+  function detalhe()
+  {
+    if (is_numeric($this->idesco)) {
+      $db = new clsBanco();
+      $db->Consulta("SELECT {$this->_todos_campos} FROM {$this->_tabela} WHERE idesco = '{$this->idesco}'");
+      $db->ProximoRegistro();
 
-	/**
-	 * Retorna um array com os dados de um registro
-	 *
-	 * @return array
-	 */
-	function detalhe()
-	{
-		if( is_numeric( $this->idesco ) )
-		{
-			$db = new clsBanco();
-			$db->Consulta( "SELECT {$this->_todos_campos} FROM {$this->_tabela} WHERE idesco = '{$this->idesco}'" );
-			$db->ProximoRegistro();
-			return $db->Tupla();
-		}
-		return false;
-	}
+      return $db->Tupla();
+    }
 
-	/**
-	 * Exclui um registro
-	 *
-	 * @return bool
-	 */
-	function excluir()
-	{
-		if( is_numeric( $this->idesco ) )
-		{
-			//	delete
-			$db = new clsBanco();
-			$db->Consulta( "DELETE FROM {$this->_tabela} WHERE idesco = '{$this->idesco}'" );
-			return true;
-		}
-		return false;
-	}
+    return FALSE;
+  }
 
-	/**
-	 * Define quais campos da tabela serao selecionados na invocacao do metodo lista
-	 *
-	 * @return null
-	 */
-	function setCamposLista( $str_campos )
-	{
-		$this->_campos_lista = $str_campos;
-	}
+  /**
+   * Exclui um registro.
+   * @return bool
+   */
+  function excluir()
+  {
+    if (is_numeric($this->idesco)) {
+      $db = new clsBanco();
+      $db->Consulta("DELETE FROM {$this->_tabela} WHERE idesco = '{$this->idesco}'");
 
-	/**
-	 * Define que o metodo Lista devera retornoar todos os campos da tabela
-	 *
-	 * @return null
-	 */
-	function resetCamposLista()
-	{
-		$this->_campos_lista = $this->_todos_campos;
-	}
+      return TRUE;
+    }
 
-	/**
-	 * Define limites de retorno para o metodo lista
-	 *
-	 * @return null
-	 */
-	function setLimite( $intLimiteQtd, $intLimiteOffset = null )
-	{
-		$this->_limite_quantidade = $intLimiteQtd;
-		$this->_limite_offset = $intLimiteOffset;
-	}
+    return FALSE;
+  }
 
-	/**
-	 * Retorna a string com o trecho da query resposavel pelo Limite de registros
-	 *
-	 * @return string
-	 */
-	function getLimite()
-	{
-		if( is_numeric( $this->_limite_quantidade ) )
-		{
-			$retorno = " LIMIT {$this->_limite_quantidade}";
-			if( is_numeric( $this->_limite_offset ) )
-			{
-				$retorno .= " OFFSET {$this->_limite_offset} ";
-			}
-			return $retorno;
-		}
-		return "";
-	}
+  /**
+   * Define quais campos da tabela serão selecionados na invocação do método lista.
+   * @return null
+   */
+  function setCamposLista($str_campos) {
+    $this->_campos_lista = $str_campos;
+  }
 
-	/**
-	 * Define campo para ser utilizado como ordenacao no metolo lista
-	 *
-	 * @return null
-	 */
-	function setOrderby( $strNomeCampo )
-	{
-		// limpa a string de possiveis erros (delete, insert, etc)
-		//$strNomeCampo = eregi_replace();
+  /**
+   * Define que o método Lista deverá retornoar todos os campos da tabela.
+   * @return null
+   */
+  function resetCamposLista() {
+    $this->_campos_lista = $this->_todos_campos;
+  }
 
-		if( is_string( $strNomeCampo ) && $strNomeCampo )
-		{
-			$this->_campo_order_by = $strNomeCampo;
-		}
-	}
+  /**
+   * Define limites de retorno para o método lista.
+   * @return null
+   */
+  function setLimite($intLimiteQtd, $intLimiteOffset = NULL)
+  {
+    $this->_limite_quantidade = $intLimiteQtd;
+    $this->_limite_offset = $intLimiteOffset;
+  }
 
-	/**
-	 * Retorna a string com o trecho da query resposavel pela Ordenacao dos registros
-	 *
-	 * @return string
-	 */
-	function getOrderby()
-	{
-		if( is_string( $this->_campo_order_by ) )
-		{
-			return " ORDER BY {$this->_campo_order_by} ";
-		}
-		return "";
-	}
+  /**
+   * Retorna a string com o trecho da query resposável pelo limite de registros.
+   * @return string
+   */
+  function getLimite()
+  {
+    if (is_numeric($this->_limite_quantidade)) {
+      $retorno = " LIMIT {$this->_limite_quantidade}";
 
+      if (is_numeric($this->_limite_offset)) {
+        $retorno .= " OFFSET {$this->_limite_offset} ";
+      }
+
+      return $retorno;
+    }
+
+    return '';
+  }
+
+  /**
+   * Define campo para ser utilizado como ordenação no método lista.
+   * @return null
+   */
+  function setOrderby($strNomeCampo)
+  {
+    if (is_string($strNomeCampo) && $strNomeCampo) {
+      $this->_campo_order_by = $strNomeCampo;
+    }
+  }
+
+  /**
+   * Retorna a string com o trecho da query resposável pela ordenação dos registros.
+   * @return string
+   */
+  function getOrderby()
+  {
+    if (is_string($this->_campo_order_by)) {
+      return " ORDER BY {$this->_campo_order_by} ";
+    }
+
+    return '';
+  }
 }
-?>
