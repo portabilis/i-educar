8a28d98c78141195e657b79b02a3eb06bb72a8fb
Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
2010-05-30 16:33:18 +0000

    Refactoring para coding standards

--- ieducar/intranet/educar_relatorio_diario_classe_temp.php
+++ ieducar/intranet/educar_relatorio_diario_classe_temp.php
@@ -1,346 +1,316 @@
 <?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-	*																	     *
-	*	@author Prefeitura Municipal de Itajaí								 *
-	*	@updated 29/03/2007													 *
-	*   Pacote: i-PLB Software Público Livre e Brasileiro					 *
-	*																		 *
-	*	Copyright (C) 2006	PMI - Prefeitura Municipal de Itajaí			 *
-	*						ctima@itajai.sc.gov.br					    	 *
-	*																		 *
-	*	Este  programa  é  software livre, você pode redistribuí-lo e/ou	 *
-	*	modificá-lo sob os termos da Licença Pública Geral GNU, conforme	 *
-	*	publicada pela Free  Software  Foundation,  tanto  a versão 2 da	 *
-	*	Licença   como  (a  seu  critério)  qualquer  versão  mais  nova.	 *
-	*																		 *
-	*	Este programa  é distribuído na expectativa de ser útil, mas SEM	 *
-	*	QUALQUER GARANTIA. Sem mesmo a garantia implícita de COMERCIALI-	 *
-	*	ZAÇÃO  ou  de ADEQUAÇÃO A QUALQUER PROPÓSITO EM PARTICULAR. Con-	 *
-	*	sulte  a  Licença  Pública  Geral  GNU para obter mais detalhes.	 *
-	*																		 *
-	*	Você  deve  ter  recebido uma cópia da Licença Pública Geral GNU	 *
-	*	junto  com  este  programa. Se não, escreva para a Free Software	 *
-	*	Foundation,  Inc.,  59  Temple  Place,  Suite  330,  Boston,  MA	 *
-	*	02111-1307, USA.													 *
-	*																		 *
-	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-require_once ("include/clsBase.inc.php");
-require_once ("include/clsCadastro.inc.php");
-require_once ("include/clsBanco.inc.php");
-require_once( "include/pmieducar/geral.inc.php" );
-require_once ("include/clsPDF.inc.php");
 
+/**
+ * i-Educar - Sistema de gestão escolar
+ *
+ * Copyright (C) 2006  Prefeitura Municipal de Itajaí
+ *                     <ctima@itajai.sc.gov.br>
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+ * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+ * qualquer versão posterior.
+ *
+ * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
+ * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+ * do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
+ * com este programa; se não, escreva para a Free Software Foundation, Inc., no
+ * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   iEd_Pmieducar
+ * @since     Arquivo disponível desde a versão 1.0.0
+ * @version   $Id$
+ */
+
+require_once 'include/clsBase.inc.php';
+require_once 'include/clsCadastro.inc.php';
+require_once 'include/clsBanco.inc.php';
+require_once 'include/pmieducar/geral.inc.php';
+require_once 'include/clsPDF.inc.php';
+
+/**
+ * clsIndexBase class.
+ *
+ * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   iEd_Pmieducar
+ * @since     Classe disponível desde a versão 1.0.0
+ * @version   @@package_version@@
+ */
 class clsIndexBase extends clsBase
 {
-	function Formular()
-	{
-		$this->SetTitulo( "{$this->_instituicao} i-Educar - Diário de Classe" );
-		$this->processoAp = "927";
-	}
+  function Formular()
+  {
+    $this->SetTitulo($this->_instituicao . ' i-Educar - Diário de Classe');
+    $this->processoAp = 927;
+  }
 }
 
+/**
+ * indice class.
+ *
+ * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   iEd_Pmieducar
+ * @since     Classe disponível desde a versão 1.0.0
+ * @version   @@package_version@@
+ */
 class indice extends clsCadastro
 {
+  var $pessoa_logada;
 
+  var $ref_cod_instituicao;
+  var $ref_cod_escola;
+  var $ref_cod_serie;
+  var $ref_cod_turma;
 
-	/**
-	 * Referencia pega da session para o idpes do usuario atual
-	 *
-	 * @var int
-	 */
-	var $pessoa_logada;
+  var $ano;
+  var $mes;
 
+  var $nm_escola;
+  var $nm_instituicao;
+  var $ref_cod_curso;
+  var $sequencial;
+  var $pdf;
+  var $pagina_atual  = 1;
+  var $total_paginas = 1;
+  var $nm_professor;
+  var $nm_turma;
+  var $nm_serie;
+  var $nm_disciplina;
 
-	var $ref_cod_instituicao;
-	var $ref_cod_escola;
-	var $ref_cod_serie;
-	var $ref_cod_turma;
+  var $page_y = 125;
 
-	var $ano;
-	var $mes;
+  var $get_file;
 
-	var $nm_escola;
-	var $nm_instituicao;
-	var $ref_cod_curso;
-	var $sequencial;
-	var $pdf;
-	var $pagina_atual = 1;
-	var $total_paginas = 1;
-	var $nm_professor;
-	var $nm_turma;
-	var $nm_serie;
-	var $nm_disciplina;
+  var $cursos = array();
 
-	var $page_y = 125;
+  var $get_link;
 
-	var $get_file;
+  var $total;
 
-	var $cursos = array();
+  var $ref_cod_modulo;
 
-	var $get_link;
+  var $meses_do_ano = array(
+    1  => 'JANEIRO',
+    2  => 'FEVEREIRO',
+    3  => 'MARÇO',
+    4  => 'ABRIL',
+    5  => 'MAIO',
+    6  => 'JUNHO',
+    7  => 'JULHO',
+    8  => 'AGOSTO',
+    9  => 'SETEMBRO',
+    10 => 'OUTUBRO',
+    11 => 'NOVEMBRO',
+    12 => 'DEZEMBRO'
+  );
 
-	var $total;
+  function Inicializar()
+  {
+    $retorno = 'Novo';
 
-	//var $array_disciplinas = array();
+    @session_start();
+    $this->pessoa_logada = $_SESSION['id_pessoa'];
+    @session_write_close();
 
-	var $ref_cod_modulo;
+    return $retorno;
+  }
 
-	var $meses_do_ano = array(
-							 "1" => "JANEIRO"
-							,"2" => "FEVEREIRO"
-							,"3" => "MAR&Ccedil;O"
-							,"4" => "ABRIL"
-							,"5" => "MAIO"
-							,"6" => "JUNHO"
-							,"7" => "JULHO"
-							,"8" => "AGOSTO"
-							,"9" => "SETEMBRO"
-							,"10" => "OUTUBRO"
-							,"11" => "NOVEMBRO"
-							,"12" => "DEZEMBRO"
-						);
+  function Gerar()
+  {
+    $obj_permissoes = new clsPermissoes();
+    $nivel_usuario  = $obj_permissoes->nivel_acesso($this->pessoa_logada);
 
+    @session_start();
+    $this->pessoa_logada = $_SESSION['id_pessoa'];
+    @session_write_close();
 
-	function Inicializar()
-	{
-		$retorno = "Novo";
-		@session_start();
-		$this->pessoa_logada = $_SESSION['id_pessoa'];
-		@session_write_close();
+    if ($_POST) {
+      foreach ($_POST as $key => $value) {
+        $this->$key = $value;
+      }
+    }
 
-		//$obj_permissoes = new clsPermissoes();
-		//if($obj_permissoes->nivel_acesso($this->pessoa_logada) > 7)
-			//header("location: index.php");
+    $this->ano = $ano_atual = date('Y');
 
-		return $retorno;
-	}
+    $this->campoNumero('ano', 'Ano', $this->ano, 4, 4, TRUE);
 
-	function Gerar()
-	{
+    $this->campoCheck('em_branco', 'Relatório em branco', '');
+    $this->campoNumero('numero_registros', 'Número de linhas', '', 3, 3);
 
-		$obj_permissoes = new clsPermissoes();
-		$nivel_usuario = $obj_permissoes->nivel_acesso($this->pessoa_logada);
+    $get_escola             = TRUE;
+    $obrigatorio            = TRUE;
+    $exibe_nm_escola        = TRUE;
+    $get_curso              = TRUE;
+    $get_escola_curso_serie = TRUE;
 
-		//if(!$nivel_usuario)
-			//header("location: index.php");
-		@session_start();
-		$this->pessoa_logada = $_SESSION['id_pessoa'];
-		@session_write_close();
+    include 'include/pmieducar/educar_campo_lista.php';
 
-		if($_POST){
-			foreach ($_POST as $key => $value) {
-				$this->$key = $value;
+    $opcoes_turma = array('' => 'Selecione');
 
-			}
-		}
+    if (($this->ref_ref_cod_serie && $this->ref_cod_escola) || $this->ref_cod_curso) {
+      $obj_turma = new clsPmieducarTurma();
+      $obj_turma->setOrderby('nm_turma ASC');
 
+      $lst_turma = $obj_turma->lista(NULL, NULL, NULL, $this->ref_ref_cod_serie,
+        $this->ref_cod_escola, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, $this->ref_cod_curso);
 
+      if (is_array($lst_turma) && count($lst_turma)) {
+        foreach ($lst_turma as $turma) {
+          $opcoes_turma[$turma['cod_turma']] = $turma['nm_turma'];
+        }
+      }
+    }
 
-		$this->ano = $ano_atual = date("Y");
-		//$this->mes = $mes_atual = date("n");
-		/*
-		$lim = 5;
-		for($a = date('Y') ; $a < $ano_atual + $lim ; $a++ )
-				$anos["{$a}"] = "{$a}";
+    $this->campoLista('ref_cod_turma', 'Turma', $opcoes_turma, $this->ref_cod_turma);
 
-		$this->campoLista( "ano", "Ano",$anos, $this->ano,"",false );
-		*/
+    $this->campoLista('ref_cod_modulo', 'Módulo', array('' => 'Selecione'), '');
 
-		$this->campoNumero( "ano", "Ano", $this->ano, 4, 4, true );
+    if ($this->ref_cod_escola) {
+      $this->ref_ref_cod_escola = $this->ref_cod_escola;
+    }
 
-		$this->campoCheck("em_branco","Relatório em branco","");
-		$this->campoNumero("numero_registros","N&uacute;mero de linhas","",3,3);
+    if ($this->get_link) {
+      $this->campoRotulo('rotulo11', '-', sprintf('<a href="%s" target="_blank">Baixar Relatório</a>', $this->get_link));
+    }
 
-	//	$this->campoLista( "mes", "M&ecirc;s",$this->meses_do_ano, $this->mes,"",false );
+    $this->url_cancelar      = 'educar_index.php';
+    $this->nome_url_cancelar = 'Cancelar';
 
-		$get_escola = true;
-		$obrigatorio = true;
-		$exibe_nm_escola = true;
-//		$get_escola_curso = true;
-		$get_curso = true;
-		$get_escola_curso_serie = true;
-		//$get_turma = true;
-		//$curso_padrao_ano_escolar = 1;
-		//$exibe_campo_lista_curso_escola = true;
-		include("include/pmieducar/educar_campo_lista.php");
-
-		/*$db = new clsBanco();
-		$consulta ="SELECT distinct
-					       m.cod_turma
-					       ,m.nm_turma
-					       ,s.cod_serie
-					       ,s.nm_serie
-					       ,m.ref_ref_cod_escola
-					  FROM pmieducar.turma          m
-					       ,pmieducar.serie         s
-					 WHERE m.ref_ref_cod_serie = s.cod_serie";
-
-		$db->Consulta($consulta);
-
-		$script = "<script>turma = new Array();\n";
-		while ($db->ProximoRegistro()) {
-			$tupla = $db->Tupla();
-			$script .= "turma[turma.length] = new Array('{$tupla['cod_turma']}','{$tupla['nm_turma']}','{$tupla['cod_serie']}','{$tupla['nm_serie']}','{$tupla['ref_ref_cod_escola']}');\n";
-		}
-		echo $script .= "</script>";
-*/
-		$opcoes_turma = array('' => 'Selecione');
-		if ( ($this->ref_ref_cod_serie && $this->ref_cod_escola) || $this->ref_cod_curso )
-		{
-			$obj_turma = new clsPmieducarTurma();
-			$obj_turma->setOrderby("nm_turma ASC");
-			$lst_turma = $obj_turma->lista( null, null, null, $this->ref_ref_cod_serie, $this->ref_cod_escola, null, null, null, null, null, null, null, null, null, 1, null, null, null, null, null, null, null, null, null, $this->ref_cod_curso );
-			if ( is_array( $lst_turma ) && count( $lst_turma ) )
-			{
-				foreach ( $lst_turma as $turma )
-				{
-					$opcoes_turma["{$turma['cod_turma']}"] = "{$turma['nm_turma']}";
-				}
-			}
-		}
-		$this->campoLista("ref_cod_turma","Turma",$opcoes_turma,$this->ref_cod_turma);
-
-		$this->campoLista("ref_cod_modulo","M&oacute;dulo",array('' => 'Selecione'),"");
-
-		if($this->ref_cod_escola)
-			$this->ref_ref_cod_escola = $this->ref_cod_escola;
-
-		if($this->get_link)
-			$this->campoRotulo("rotulo11", "-", "<a href='$this->get_link' target='_blank'>Baixar Relatório</a>");
-
-		$this->url_cancelar = "educar_index.php";
-		$this->nome_url_cancelar = "Cancelar";
-
-		$this->acao_enviar = 'acao2()';
-		$this->acao_executa_submit = false;
-	}
-
-
+    $this->acao_enviar         = 'acao2()';
+    $this->acao_executa_submit = FALSE;
+  }
 }
 
-// cria uma extensao da classe base
+// Instancia objeto de página
 $pagina = new clsIndexBase();
-// cria o conteudo
+
+// Instancia objeto de conteúdo
 $miolo = new indice();
-// adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
-// gera o html
+
+// Atribui o conteúdo à  página
+$pagina->addForm($miolo);
+
+// Gera o código HTML
 $pagina->MakeAll();
-
-
 ?>
-<script>
-
+<script type="text/javascript">
 function acao2()
 {
+  if (!acao()) {
+    return false;
+  }
 
-	if(!acao())
-		return false;
+  showExpansivelImprimir(400, 200, '', [], 'Diário de Classe');
 
-	showExpansivelImprimir(400, 200,'',[], "Diário de Classe");
+  document.formcadastro.target = 'miolo_' + (DOM_divs.length - 1);
 
-	document.formcadastro.target = 'miolo_'+(DOM_divs.length-1);
-
-	document.formcadastro.submit();
+  document.formcadastro.submit();
 }
 
 document.formcadastro.action = 'educar_relatorio_diario_classe_temp_proc.php';
 
 function getModulos()
 {
-	var campoEscola = document.getElementById( 'ref_cod_escola' ).value;
-	var campoCurso = document.getElementById( 'ref_cod_curso' ).value;
-	var campoAno = document.getElementById( 'ano' ).value;
-	var campoTurma = document.getElementById( 'ref_cod_turma' ).value;
-	var xml1 = new ajax(getModulos_XML);
-	strURL = "educar_modulo_xml.php?esc="+campoEscola+"&ano="+campoAno+"&curso="+campoCurso+"&turma="+campoTurma;
-	xml1.envia(strURL);
+  var campoEscola = document.getElementById('ref_cod_escola').value;
+  var campoCurso  = document.getElementById('ref_cod_curso').value;
+  var campoAno    = document.getElementById('ano').value;
+  var campoTurma  = document.getElementById('ref_cod_turma').value;
+
+  var xml1 = new ajax(getModulos_XML);
+  strURL   = 'educar_modulo_xml.php?esc=' + campoEscola + '&ano=' + campoAno
+           + '&curso=' + campoCurso + '&turma=' + campoTurma;
+
+  xml1.envia(strURL);
 }
 
 function getModulos_XML(xml)
 {
+  var modulos = xml.getElementsByTagName('ano_letivo_modulo');
 
-	var modulos = xml.getElementsByTagName( "ano_letivo_modulo" );
+  var campoEscola = document.getElementById('ref_cod_escola').value;
+  var campoCurso  = document.getElementById('ref_cod_curso').value;
+  var campoModulo = document.getElementById('ref_cod_modulo');
+  var campoAno    = document.getElementById('ano').value;
 
-	var campoEscola = document.getElementById( 'ref_cod_escola' ).value;
-	var campoCurso= document.getElementById( 'ref_cod_curso' ).value;
-	var campoModulo = document.getElementById( 'ref_cod_modulo' );
-	var campoAno = document.getElementById( 'ano' ).value;
+  campoModulo.length     = 1;
+  campoModulo.options[0] = new Option('Selecione um módulo', '', false, false);
 
-	campoModulo.length = 1;
-	campoModulo.options[0] = new Option( 'Selecione um módulo', '', false, false );
-	for ( var j = 0; j < modulos.length; j++ )
-	{
-		//if ( modulos[j][2] == campoEscola && modulos[j][3] == campoAno)
-		//{
-		campoModulo.options[campoModulo.options.length] = new Option( modulos[j].firstChild.nodeValue, modulos[j].getAttribute('cod_modulo') + "-" +modulos[j].getAttribute('sequencial') , false, false );
-		//}
-	}
-	if ( campoModulo.length == 1 ) {
-		campoModulo.options[0] = new Option( 'O curso não possui nenhum módulo', '', false, false );
-	}
+  for (var j = 0; j < modulos.length; j++) {
+    campoModulo.options[campoModulo.options.length] = new Option(
+      modulos[j].firstChild.nodeValue, modulos[j].getAttribute('cod_modulo') + '-' + modulos[j].getAttribute('sequencial'),
+      false, false
+    );
+  }
 
-
+  if (campoModulo.length == 1) {
+    campoModulo.options[0] = new Option('O curso não possui nenhum módulo', '', false, false);
+  }
 }
-//before_getEscola = function(){getModulos(); };
 
-//addEvent_('load',getModulos);
 document.getElementById('ref_cod_escola').onchange = function()
 {
-	getEscolaCurso();
-	document.getElementById('ref_cod_curso').onchange();
-
+  getEscolaCurso();
+  document.getElementById('ref_cod_curso').onchange();
 }
+
 document.getElementById('ano').onchange = function()
 {
-	getModulos();
-
+  getModulos();
 }
 
 document.getElementById('ref_cod_curso').onchange = function()
 {
-	getEscolaCursoSerie();
-	document.getElementById( 'ref_cod_modulo' ).length = 1;
-	getModulos();
+  getEscolaCursoSerie();
+  document.getElementById('ref_cod_modulo').length = 1;
+  getModulos();
 }
+
 document.getElementById('ref_cod_turma').onchange = function()
 {
-	document.getElementById( 'ref_cod_modulo' ).length = 1;
-	getModulos();
+  document.getElementById('ref_cod_modulo').length = 1;
+  getModulos();
 }
 
 document.getElementById('ref_ref_cod_serie').onchange = function()
 {
-	var campoEscola = document.getElementById( 'ref_cod_escola' ).value;
-	var campoSerie = document.getElementById( 'ref_ref_cod_serie' ).value;
+  var campoEscola = document.getElementById('ref_cod_escola').value;
+  var campoSerie  = document.getElementById('ref_ref_cod_serie').value;
 
-	var xml1 = new ajax(getTurma_XML);
-	strURL = "educar_turma_xml.php?esc="+campoEscola+"&ser="+campoSerie;
-	xml1.envia(strURL);
+  var xml1 = new ajax(getTurma_XML);
+  strURL   = 'educar_turma_xml.php?esc=' + campoEscola + '&ser=' + campoSerie;
+  xml1.envia(strURL);
 }
 
 function getTurma_XML(xml)
 {
+  var campoSerie = document.getElementById('ref_ref_cod_serie').value;
 
+  var campoTurma = document.getElementById('ref_cod_turma');
 
-	var campoSerie = document.getElementById( 'ref_ref_cod_serie' ).value;
+  var turma = xml.getElementsByTagName('turma');
 
-	var campoTurma = document.getElementById( 'ref_cod_turma' );
+  campoTurma.length = 1;
+  campoTurma.options[0] = new Option('Selecione uma Turma', '', false, false);
 
-	var turma = xml.getElementsByTagName( "turma" );
+  for (var j = 0; j < turma.length; j++) {
+    campoTurma.options[campoTurma.options.length] = new Option(
+      turma[j].firstChild.nodeValue, turma[j].getAttribute('cod_turma'), false, false
+    );
+  }
 
-	campoTurma.length = 1;
-	campoTurma.options[0] = new Option( 'Selecione uma Turma', '', false, false );
-	for ( var j = 0; j < turma.length; j++ )
-	{
-
-		campoTurma.options[campoTurma.options.length] = new Option( turma[j].firstChild.nodeValue, turma[j].getAttribute('cod_turma'), false, false );
-
-	}
-	if ( campoTurma.length == 1 && campoSerie != '' ) {
-		campoTurma.options[0] = new Option( 'A série não possui nenhuma turma', '', false, false );
-	}
-
+  if (campoTurma.length == 1 && campoSerie != '') {
+    campoTurma.options[0] = new Option('A série não possui nenhuma turma', '', false, false);
+  }
 }
-
 </script>
