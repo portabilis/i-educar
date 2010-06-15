ea49035faf6249db62fd1383d0824cc7051a93bb
Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
2010-06-15 15:05:20 +0000

    Refactoring para coding standards

--- ieducar/intranet/educar_servidor_formacao_det.php
+++ ieducar/intranet/educar_servidor_formacao_det.php
@@ -1,163 +1,195 @@
 <?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-	*																	     *
-	*	@author Prefeitura Municipal de Itajaí								 *
-	*	@updated 29/03/2007													 *
-	*   Pacote: i-PLB Software Público Livre e Brasileiro					 *
-	*																		 *
-	*	Copyright (C) 2006	PMI - Prefeitura Municipal de Itajaí			 *
-	*						ctima@itajai.sc.gov.br					    	 *
-	*																		 *
-	*	Este  programa  é  software livre, você pode redistribuí-lo e/ou	 *
-	*	modificá-lo sob os termos da Licença Pública Geral GNU, conforme	 *
-	*	publicada pela Free  Software  Foundation,  tanto  a versão 2 da	 *
-	*	Licença   como  (a  seu  critério)  qualquer  versão  mais  nova.	 *
-	*																		 *
-	*	Este programa  é distribuído na expectativa de ser útil, mas SEM	 *
-	*	QUALQUER GARANTIA. Sem mesmo a garantia implícita de COMERCIALI-	 *
-	*	ZAÇÃO  ou  de ADEQUAÇÃO A QUALQUER PROPÓSITO EM PARTICULAR. Con-	 *
-	*	sulte  a  Licença  Pública  Geral  GNU para obter mais detalhes.	 *
-	*																		 *
-	*	Você  deve  ter  recebido uma cópia da Licença Pública Geral GNU	 *
-	*	junto  com  este  programa. Se não, escreva para a Free Software	 *
-	*	Foundation,  Inc.,  59  Temple  Place,  Suite  330,  Boston,  MA	 *
-	*	02111-1307, USA.													 *
-	*																		 *
-	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-/**
- * @author Adriano Erik Weiguert Nagasava
- */
-require_once ("include/clsBase.inc.php");
-require_once ("include/clsDetalhe.inc.php");
-require_once ("include/clsBanco.inc.php");
-require_once( "include/pmieducar/geral.inc.php" );
 
+/**
+ * i-Educar - Sistema de gestão escolar
+ *
+ * Copyright (C) 2006  Prefeitura Municipal de Itajaí
+ *                     <ctima@itajai.sc.gov.br>
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+ * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+ * qualquer versão posterior.
+ *
+ * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
+ * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+ * do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
+ * com este programa; se não, escreva para a Free Software Foundation, Inc., no
+ * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * @author    Adriano Erik Weiguert Nagasava <ctima@itajai.sc.gov.br>
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   iEd_Pmieducar
+ * @since     Arquivo disponível desde a versão 1.0.0
+ * @version   $Id$
+ */
+
+require_once 'include/clsBase.inc.php';
+require_once 'include/clsDetalhe.inc.php';
+require_once 'include/clsBanco.inc.php';
+require_once 'include/pmieducar/geral.inc.php';
+
+/**
+ * clsIndexBase class.
+ *
+ * @author    Adriano Erik Weiguert Nagasava <ctima@itajai.sc.gov.br>
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   iEd_Pmieducar
+ * @since     Classe disponível desde a versão 1.0.0
+ * @version   @@package_version@@
+ */
 class clsIndexBase extends clsBase
 {
-	function Formular()
-	{
-		$this->SetTitulo( "{$this->_instituicao} i-Educar - Servidor Formacao" );
-		$this->processoAp = "635";
-	}
+  function Formular()
+  {
+    $this->SetTitulo($this->_instituicao . ' i-Educar - Servidor Formação');
+    $this->processoAp = 635;
+  }
 }
 
+/**
+ * indice class.
+ *
+ * @author    Adriano Erik Weiguert Nagasava <ctima@itajai.sc.gov.br>
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   iEd_Pmieducar
+ * @since     Classe disponível desde a versão 1.0.0
+ * @version   @@package_version@@
+ */
 class indice extends clsDetalhe
 {
-	/**
-	 * Titulo no topo da pagina
-	 *
-	 * @var int
-	 */
-	var $titulo;
+  var $titulo;
 
-	var $cod_formacao;
-	var $ref_usuario_exc;
-	var $ref_usuario_cad;
-	var $ref_cod_servidor;
-	var $nm_formacao;
-	var $tipo;
-	var $descricao;
-	var $data_cadastro;
-	var $data_exclusao;
-	var $ativo;
+  var $cod_formacao;
+  var $ref_usuario_exc;
+  var $ref_usuario_cad;
+  var $ref_cod_servidor;
+  var $nm_formacao;
+  var $tipo;
+  var $descricao;
+  var $data_cadastro;
+  var $data_exclusao;
+  var $ativo;
 
-	function Gerar()
-	{
-		@session_start();
-		$this->pessoa_logada = $_SESSION['id_pessoa'];
-		session_write_close();
+  function Gerar()
+  {
+    @session_start();
+    $this->pessoa_logada = $_SESSION['id_pessoa'];
+    session_write_close();
 
-		$this->titulo = "Servidor Formacao - Detalhe";
-		$this->addBanner( "imagens/nvp_top_intranet.jpg", "imagens/nvp_vert_intranet.jpg", "Intranet" );
+    $this->titulo = 'Servidor Formacao - Detalhe';
+    $this->addBanner('imagens/nvp_top_intranet.jpg', 'imagens/nvp_vert_intranet.jpg', 'Intranet');
 
-		$this->cod_formacao=$_GET["cod_formacao"];
+    $this->cod_formacao = $_GET['cod_formacao'];
 
-		$tmp_obj = new clsPmieducarServidorFormacao( $this->cod_formacao );
-		$registro = $tmp_obj->detalhe();
+    $tmp_obj = new clsPmieducarServidorFormacao($this->cod_formacao);
+    $registro = $tmp_obj->detalhe();
 
-		if( ! $registro )
-		{
-			header( "location: educar_servidor_formacao_lst.php" );
-			die();
-		}
+    if (! $registro) {
+      header('Location: educar_servidor_formacao_lst.php');
+      die();
+    }
 
-		if( class_exists( "clsPmieducarServidor" ) )
-		{
-			$obj_ref_cod_servidor = new clsPmieducarServidor( $registro["ref_cod_servidor"], null, null, null, null, null, 1, $registro["ref_ref_cod_instituicao"] );
-			$det_ref_cod_servidor = $obj_ref_cod_servidor->detalhe();
-			$registro["ref_cod_servidor"] = $det_ref_cod_servidor["cod_servidor"];
-		}
-		else
-		{
-			$registro["ref_cod_servidor"] = "Erro na geracao";
-			echo "<!--\nErro\nClasse nao existente: clsPmieducarServidor\n-->";
-		}
-		if( $registro["nm_formacao"] )
-		{
-			$this->addDetalhe( array( "Nome Forma&ccedil;&atilde;o", "{$registro["nm_formacao"]}") );
-		}
-		if ( $registro["tipo"] == "C" ) {
-			$obj_curso = new clsPmieducarServidorCurso( null, $this->cod_formacao );
-			$det_curso = $obj_curso->detalhe();
-		}
-		elseif ( $registro["tipo"] == "T" || $registro["tipo"] == "O" ) {
-			$obj_titulo = new clsPmieducarServidorTituloConcurso( null, $this->cod_formacao );
-			$det_titulo = $obj_titulo->detalhe();
-		}
-		if( $registro["tipo"] )
-		{
-			if ( $registro["tipo"] == "C" ) {
-				$registro["tipo"] = "Curso";
-			}
-			elseif ( $registro["tipo"] == "T" ) {
-				$registro["tipo"] = "T&iacute;tulo";
-			}
-			else {
-				$registro["tipo"] = "Concurso";
-			}
-			$this->addDetalhe( array( "Tipo", "{$registro["tipo"]}") );
-		}
-		if( $registro["descricao"] )
-		{
-			$this->addDetalhe( array( "Descric&atilde;o", "{$registro["descricao"]}") );
-		}
-		if ( $det_curso["data_conclusao"] ) {
-			$this->addDetalhe( array( "Data de Conclus&atilde;o", "".dataFromPgToBr( $det_curso["data_conclusao"] )."" ) );
-		}
-		if ( $det_curso["data_registro"] ) {
-			$this->addDetalhe( array( "Data de Registro", "".dataFromPgToBr( $det_curso["data_registro"] )."" ) );
-		}
-		if ( $det_curso["diplomas_registros"] ) {
-			$this->addDetalhe( array( "Diplomas e Registros", "{$det_curso["diplomas_registros"]}" ) );
-		}
-		if ( $det_titulo["data_vigencia_homolog"] && $registro["tipo"] == "T&iacute;tulo") {
-			$this->addDetalhe( array( "Data de Vigência", "".dataFromPgToBr( $det_titulo["data_vigencia_homolog"] )."" ) );
-		}
-		elseif ( $det_titulo["data_vigencia_homolog"] && $registro["tipo"] == "Concurso") {
-			$this->addDetalhe( array( "Data de Homologação", "".dataFromPgToBr( $det_titulo["data_vigencia_homolog"] )."" ) );
-		}
-		if ( $det_titulo["data_publicacao"] ) {
-			$this->addDetalhe( array( "Data de Publicação", "".dataFromPgToBr( $det_titulo["data_publicacao"] )."" ) );
-		}
+    if (class_exists('clsPmieducarServidor')) {
+      $obj_ref_cod_servidor = new clsPmieducarServidor($registro['ref_cod_servidor'],
+        null, null, null, null, null, 1, $registro['ref_ref_cod_instituicao']
+      );
 
-		$obj_permissoes = new clsPermissoes();
-		if( $obj_permissoes->permissao_cadastra( 635, $this->pessoa_logada, 3 ) )
-		{
-			$this->url_novo = "educar_servidor_formacao_cad.php";
-			$this->url_editar = "educar_servidor_formacao_cad.php?cod_formacao={$registro["cod_formacao"]}&ref_cod_instituicao={$registro["ref_ref_cod_instituicao"]}&ref_cod_servidor={$registro["ref_cod_servidor"]}";
-		}
+      $det_ref_cod_servidor = $obj_ref_cod_servidor->detalhe();
+      $registro['ref_cod_servidor'] = $det_ref_cod_servidor['cod_servidor'];
+    }
+    else {
+      $registro['ref_cod_servidor'] = 'Erro na geracao';
+    }
 
-		$this->url_cancelar = "educar_servidor_formacao_lst.php?ref_cod_servidor={$registro["ref_cod_servidor"]}&ref_cod_instituicao={$registro["ref_ref_cod_instituicao"]}";
-		$this->largura = "100%";
-	}
+    if ($registro['nm_formacao']) {
+      $this->addDetalhe(array('Nome Formação', $registro['nm_formacao']));
+    }
+
+    if ($registro['tipo'] == 'C') {
+      $obj_curso = new clsPmieducarServidorCurso( null, $this->cod_formacao );
+      $det_curso = $obj_curso->detalhe();
+    }
+    elseif ($registro['tipo'] == 'T' || $registro['tipo'] == 'O') {
+      $obj_titulo = new clsPmieducarServidorTituloConcurso(NULL, $this->cod_formacao);
+      $det_titulo = $obj_titulo->detalhe();
+    }
+
+    if ($registro['tipo']) {
+      if ($registro['tipo'] == 'C') {
+        $registro['tipo'] = 'Curso';
+      }
+      elseif ($registro['tipo'] == 'T') {
+        $registro['tipo'] = 'T&iacute;tulo';
+      }
+      else {
+        $registro['tipo'] = 'Concurso';
+      }
+
+      $this->addDetalhe(array('Tipo', $registro['tipo']));
+    }
+
+    if ($registro['descricao']) {
+      $this->addDetalhe(array('Descricção', $registro['descricao']));
+    }
+
+    if ($det_curso['data_conclusao']) {
+      $this->addDetalhe(array('Data de Conclusão', dataFromPgToBr($det_curso['data_conclusao'])));
+    }
+
+    if ($det_curso['data_registro']) {
+      $this->addDetalhe(array('Data de Registro', dataFromPgToBr($det_curso['data_registro'])));
+    }
+
+    if ($det_curso['diplomas_registros']) {
+      $this->addDetalhe(array('Diplomas e Registros', $det_curso['diplomas_registros']));
+    }
+
+    if ($det_titulo['data_vigencia_homolog'] && $registro['tipo'] == 'Título') {
+      $this->addDetalhe(array('Data de Vigência', dataFromPgToBr($det_titulo['data_vigencia_homolog'])));
+    }
+    elseif ($det_titulo['data_vigencia_homolog'] && $registro['tipo'] == 'Concurso') {
+      $this->addDetalhe(array('Data de Homologação', dataFromPgToBr($det_titulo['data_vigencia_homolog'])));
+    }
+
+    if ($det_titulo['data_publicacao']) {
+      $this->addDetalhe(array('Data de Publicação', dataFromPgToBr($det_titulo['data_publicacao'])));
+    }
+
+    $obj_permissoes = new clsPermissoes();
+
+    if ($obj_permissoes->permissao_cadastra(635, $this->pessoa_logada, 3)) {
+      $this->url_novo = 'educar_servidor_formacao_cad.php';
+
+      $this->url_editar = sprintf(
+        'educar_servidor_formacao_cad.php?cod_formacao=%d&ref_cod_instituicao=%d&ref_cod_servidor=%d',
+        $registro['cod_formacao'], $registro['ref_ref_cod_instituicao'], $registro['ref_cod_servidor']
+      );
+    }
+
+    $this->url_cancelar = sprintf(
+      'educar_servidor_formacao_lst.php?ref_cod_servidor=%d&ref_cod_instituicao=%d',
+      $registro['ref_cod_servidor'], $registro['ref_ref_cod_instituicao']
+    );
+
+    $this->largura = '100%';
+  }
 }
 
-// cria uma extensao da classe base
+// Instancia objeto de página
 $pagina = new clsIndexBase();
-// cria o conteudo
+
+// Instancia objeto de conteúdo
 $miolo = new indice();
-// adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
-// gera o html
+
+// Atribui o conteúdo à  página
+$pagina->addForm($miolo);
+
+// Gera o código HTML
 $pagina->MakeAll();
-?>
--- ieducar/intranet/educar_servidor_formacao_lst.php
+++ ieducar/intranet/educar_servidor_formacao_lst.php
@@ -1,209 +1,219 @@
 <?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-	*																	     *
-	*	@author Prefeitura Municipal de Itajaí								 *
-	*	@updated 29/03/2007													 *
-	*   Pacote: i-PLB Software Público Livre e Brasileiro					 *
-	*																		 *
-	*	Copyright (C) 2006	PMI - Prefeitura Municipal de Itajaí			 *
-	*						ctima@itajai.sc.gov.br					    	 *
-	*																		 *
-	*	Este  programa  é  software livre, você pode redistribuí-lo e/ou	 *
-	*	modificá-lo sob os termos da Licença Pública Geral GNU, conforme	 *
-	*	publicada pela Free  Software  Foundation,  tanto  a versão 2 da	 *
-	*	Licença   como  (a  seu  critério)  qualquer  versão  mais  nova.	 *
-	*																		 *
-	*	Este programa  é distribuído na expectativa de ser útil, mas SEM	 *
-	*	QUALQUER GARANTIA. Sem mesmo a garantia implícita de COMERCIALI-	 *
-	*	ZAÇÃO  ou  de ADEQUAÇÃO A QUALQUER PROPÓSITO EM PARTICULAR. Con-	 *
-	*	sulte  a  Licença  Pública  Geral  GNU para obter mais detalhes.	 *
-	*																		 *
-	*	Você  deve  ter  recebido uma cópia da Licença Pública Geral GNU	 *
-	*	junto  com  este  programa. Se não, escreva para a Free Software	 *
-	*	Foundation,  Inc.,  59  Temple  Place,  Suite  330,  Boston,  MA	 *
-	*	02111-1307, USA.													 *
-	*																		 *
-	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-/**
- * @author Adriano Erik Weiguert Nagasava
- */
-require_once ("include/clsBase.inc.php");
-require_once ("include/clsListagem.inc.php");
-require_once ("include/clsBanco.inc.php");
-require_once( "include/pmieducar/geral.inc.php" );
 
+/**
+ * i-Educar - Sistema de gestão escolar
+ *
+ * Copyright (C) 2006  Prefeitura Municipal de Itajaí
+ *                     <ctima@itajai.sc.gov.br>
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+ * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+ * qualquer versão posterior.
+ *
+ * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
+ * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+ * do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
+ * com este programa; se não, escreva para a Free Software Foundation, Inc., no
+ * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * @author    Adriano Erik Weiguert Nagasava <ctima@itajai.sc.gov.br>
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   iEd_Pmieducar
+ * @since     Arquivo disponível desde a versão 1.0.0
+ * @version   $Id$
+ */
+
+require_once 'include/clsBase.inc.php';
+require_once 'include/clsListagem.inc.php';
+require_once 'include/clsBanco.inc.php';
+require_once 'include/pmieducar/geral.inc.php';
+
+/**
+ * clsIndexBase class.
+ *
+ * @author    Adriano Erik Weiguert Nagasava <ctima@itajai.sc.gov.br>
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   iEd_Pmieducar
+ * @since     Classe disponível desde a versão 1.0.0
+ * @version   @@package_version@@
+ */
 class clsIndexBase extends clsBase
 {
-	function Formular()
-	{
-		$this->SetTitulo( "{$this->_instituicao} i-Educar - Servidor Formacao" );
-		$this->processoAp = "635";
-	}
+  function Formular()
+  {
+    $this->SetTitulo($this->_instituicao . ' i-Educar - Servidor Formacao' );
+    $this->processoAp = 635;
+  }
 }
 
+/**
+ * indice class.
+ *
+ * @author    Adriano Erik Weiguert Nagasava <ctima@itajai.sc.gov.br>
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   iEd_Pmieducar
+ * @since     Classe disponível desde a versão 1.0.0
+ * @version   @@package_version@@
+ */
 class indice extends clsListagem
 {
-	/**
-	 * Referencia pega da session para o idpes do usuario atual
-	 *
-	 * @var int
-	 */
-	var $pessoa_logada;
+  var $pessoa_logada;
+  var $titulo;
+  var $limite;
+  var $offset;
 
-	/**
-	 * Titulo no topo da pagina
-	 *
-	 * @var int
-	 */
-	var $titulo;
+  var $cod_formacao;
+  var $ref_usuario_exc;
+  var $ref_usuario_cad;
+  var $ref_cod_servidor;
+  var $nm_formacao;
+  var $tipo;
+  var $descricao;
+  var $data_cadastro;
+  var $data_exclusao;
+  var $ativo;
+  var $ref_cod_instituicao;
 
-	/**
-	 * Quantidade de registros a ser apresentada em cada pagina
-	 *
-	 * @var int
-	 */
-	var $limite;
+  function Gerar()
+  {
+    @session_start();
+    $this->pessoa_logada = $_SESSION['id_pessoa'];
+    session_write_close();
 
-	/**
-	 * Inicio dos registros a serem exibidos (limit)
-	 *
-	 * @var int
-	 */
-	var $offset;
+    $this->ref_cod_servidor    = $_GET['ref_cod_servidor'];
+    $this->ref_cod_instituicao = $_GET['ref_cod_instituicao'];
 
-	var $cod_formacao;
-	var $ref_usuario_exc;
-	var $ref_usuario_cad;
-	var $ref_cod_servidor;
-	var $nm_formacao;
-	var $tipo;
-	var $descricao;
-	var $data_cadastro;
-	var $data_exclusao;
-	var $ativo;
-	var $ref_cod_instituicao;
+    $this->titulo = 'Servidor Formacao - Listagem';
 
-	function Gerar()
-	{
-		@session_start();
-		$this->pessoa_logada = $_SESSION['id_pessoa'];
-		session_write_close();
+    // passa todos os valores obtidos no GET para atributos do objeto
+    foreach( $_GET AS $var => $val ) {
+      $this->$var = ($val === '') ? NULL : $val;
+    }
 
-		$this->ref_cod_servidor	   = $_GET["ref_cod_servidor"];
-		$this->ref_cod_instituicao = $_GET["ref_cod_instituicao"];
+    $this->addBanner('imagens/nvp_top_intranet.jpg', 'imagens/nvp_vert_intranet.jpg', 'Intranet');
 
-		$this->titulo = "Servidor Formacao - Listagem";
+    $this->addCabecalhos(array(
+      'Nome Formação',
+      'Tipo'
+    ));
 
-		foreach( $_GET AS $var => $val ) // passa todos os valores obtidos no GET para atributos do objeto
-			$this->$var = ( $val === "" ) ? null: $val;
+    $this->campoOculto('ref_cod_servidor', $this->ref_cod_servidor);
+    $this->campoOculto('ref_cod_instituicao', $this->ref_cod_instituicao);
 
-		$this->addBanner( "imagens/nvp_top_intranet.jpg", "imagens/nvp_vert_intranet.jpg", "Intranet" );
+    // Filtros
+    $this->campoTexto('nm_formacao', 'Nome da Formação', $this->nm_formacao,
+      30, 255, FALSE);
 
-		$this->addCabecalhos( array(
-			"Nome Forma&ccedil;&atilde;o",
-			"Tipo"
-		) );
-		$this->campoOculto( "ref_cod_servidor", $this->ref_cod_servidor );
-		$this->campoOculto( "ref_cod_instituicao", $this->ref_cod_instituicao );
+    $opcoes = array(
+      ''  => 'Selecione',
+      'C' => 'Cursos',
+      'T' => 'Títulos',
+      'O' => 'Concursos'
+    );
 
-		// outros Filtros
-		$this->campoTexto( "nm_formacao", "Nome da Forma&ccedil;&atilde;o", $this->nm_formacao, 30, 255, false );
-		$opcoes = array( "" => "Selecione", "C" => "Cursos", "T" => "Títulos", "O" => "Concursos" );
-		$this->campoLista( "tipo", "Tipo de Formação", $opcoes, $this->tipo );
+    $this->campoLista('tipo', 'Tipo de Formação', $opcoes, $this->tipo);
 
-		// Paginador
-		$this->limite = 20;
-		$this->offset = ( $_GET["pagina_{$this->nome}"] ) ? $_GET["pagina_{$this->nome}"]*$this->limite-$this->limite: 0;
+    // Paginador
+    $this->limite = 20;
+    $this->offset = ($_GET['pagina_' . $this->nome]) ?
+      $_GET['pagina_' . $this->nome] * $this->limite - $this->limite : 0;
 
-		$obj_servidor_formacao = new clsPmieducarServidorFormacao();
-		$obj_servidor_formacao->setOrderby( "nm_formacao ASC" );
-		$obj_servidor_formacao->setLimite( $this->limite, $this->offset );
+    $obj_servidor_formacao = new clsPmieducarServidorFormacao();
+    $obj_servidor_formacao->setOrderby('nm_formacao ASC');
+    $obj_servidor_formacao->setLimite($this->limite, $this->offset);
 
-		if ( !isset( $this->tipo ) ) {
-			$this->tipo = null;
-		}
-		$lista = $obj_servidor_formacao->lista(
-			null,
-			null,
-			null,
-			$this->ref_cod_servidor,
-			$this->nm_formacao,
-			$this->tipo,
-			null,
-			null,
-			null,
-			1
-		);
-		$total = $obj_servidor_formacao->_total;
+    if (! isset($this->tipo)) {
+      $this->tipo = NULL;
+    }
 
-		// monta a lista
-		if( is_array( $lista ) && count( $lista ) )
-		{
-			foreach ( $lista AS $registro )
-			{
-				// pega detalhes de foreign_keys
-				if( class_exists( "clsPmieducarUsuario" ) )
-				{
-					$obj_ref_usuario_exc 		 = new clsPmieducarUsuario( $registro["ref_usuario_exc"] );
-					$det_ref_usuario_exc 		 = $obj_ref_usuario_exc->detalhe();
-					$registro["ref_usuario_exc"] = $det_ref_usuario_exc["data_cadastro"];
-				}
-				else
-				{
-					$registro["ref_usuario_exc"] = "Erro na geracao";
-					echo "<!--\nErro\nClasse nao existente: clsPmieducarUsuario\n-->";
-				}
+    $lista = $obj_servidor_formacao->lista(
+      NULL,
+      NULL,
+      NULL,
+      $this->ref_cod_servidor,
+      $this->nm_formacao,
+      $this->tipo,
+      NULL,
+      NULL,
+      NULL,
+      1
+    );
 
-				if( class_exists( "clsPmieducarServidor" ) )
-				{
-					$obj_ref_cod_servidor = new clsPmieducarServidor( $registro["ref_cod_servidor"] );
-					$det_ref_cod_servidor = $obj_ref_cod_servidor->detalhe();
-					$registro["ref_cod_servidor"] = $det_ref_cod_servidor["cod_servidor"];
-				}
-				else
-				{
-					$registro["ref_cod_servidor"] = "Erro na geracao";
-					echo "<!--\nErro\nClasse nao existente: clsPmieducarServidor\n-->";
-				}
+    $total = $obj_servidor_formacao->_total;
 
-				if ( $registro["tipo"] == "C" ) {
-					$registro["tipo"] = "Curso";
-				}
-				elseif ( $registro["tipo"] == "T" ) {
-					$registro["tipo"] = "T&iacute;tulo";
-				}
-				else {
-					$registro["tipo"] = "Concurso";
-				}
+    // Monta a lista
+    if (is_array($lista) && count($lista)) {
+      foreach ($lista as $registro) {
+        // Pega detalhes de foreign_keys
+        if (class_exists('clsPmieducarUsuario')) {
+          $obj_ref_usuario_exc = new clsPmieducarUsuario($registro['ref_usuario_exc']);
+          $det_ref_usuario_exc = $obj_ref_usuario_exc->detalhe();
 
-				$this->addLinhas( array(
-					"<a href=\"educar_servidor_formacao_det.php?cod_formacao={$registro["cod_formacao"]}\">{$registro["nm_formacao"]}</a>",
-					"<a href=\"educar_servidor_formacao_det.php?cod_formacao={$registro["cod_formacao"]}\">{$registro["tipo"]}</a>"
-				) );
-				$this->tipo = "";
-			}
-		}
-		$this->addPaginador2( "educar_servidor_formacao_lst.php", $total, $_GET, $this->nome, $this->limite );
-		$obj_permissoes = new clsPermissoes();
-		if( $obj_permissoes->permissao_cadastra( 635, $this->pessoa_logada, 3 ) )
-		{
-			$this->array_botao[] 	 = "Novo";
-			$this->array_botao_url[] = "educar_servidor_formacao_cad.php?ref_cod_servidor={$this->ref_cod_servidor}&ref_cod_instituicao={$this->ref_cod_instituicao}";
-			//$this->acao = "go(\"educar_servidor_formacao_cad.php?ref_cod_servidor={$this->ref_cod_servidor}&ref_cod_instituicao={$this->ref_cod_instituicao}\")";
-			//$this->nome_acao = "Novo";
-		}
-		$this->array_botao[] 	 = "Voltar";
-		$this->array_botao_url[] = "educar_servidor_det.php?cod_servidor={$this->ref_cod_servidor}&ref_cod_instituicao={$this->ref_cod_instituicao}";
+          $registro['ref_usuario_exc'] = $det_ref_usuario_exc['data_cadastro'];
+        }
+        else {
+          $registro['ref_usuario_exc'] = 'Erro na geracao';
+        }
 
-		$this->largura = "100%";
-	}
+        if (class_exists('clsPmieducarServidor')) {
+          $obj_ref_cod_servidor = new clsPmieducarServidor($registro['ref_cod_servidor']);
+          $det_ref_cod_servidor = $obj_ref_cod_servidor->detalhe();
+
+          $registro['ref_cod_servidor'] = $det_ref_cod_servidor['cod_servidor'];
+        }
+        else {
+          $registro['ref_cod_servidor'] = 'Erro na geracao';
+        }
+
+        if ($registro['tipo'] == 'C') {
+          $registro['tipo'] = 'Curso';
+        }
+        elseif ($registro['tipo'] == 'T') {
+          $registro['tipo'] = 'Título';
+        }
+        else {
+          $registro['tipo'] = 'Concurso';
+        }
+
+        $this->addLinhas(array(
+          "<a href=\"educar_servidor_formacao_det.php?cod_formacao={$registro["cod_formacao"]}\">{$registro["nm_formacao"]}</a>",
+          "<a href=\"educar_servidor_formacao_det.php?cod_formacao={$registro["cod_formacao"]}\">{$registro["tipo"]}</a>"
+        ));
+
+        $this->tipo = '';
+      }
+    }
+
+    $this->addPaginador2( "educar_servidor_formacao_lst.php", $total, $_GET, $this->nome, $this->limite );
+    $obj_permissoes = new clsPermissoes();
+
+    if ($obj_permissoes->permissao_cadastra(635, $this->pessoa_logada, 3)) {
+      $this->array_botao[]     = "Novo";
+      $this->array_botao_url[] = "educar_servidor_formacao_cad.php?ref_cod_servidor={$this->ref_cod_servidor}&ref_cod_instituicao={$this->ref_cod_instituicao}";
+    }
+
+    $this->array_botao[]     = 'Voltar';
+    $this->array_botao_url[] = "educar_servidor_det.php?cod_servidor={$this->ref_cod_servidor}&ref_cod_instituicao={$this->ref_cod_instituicao}";
+
+    $this->largura = '100%';
+  }
 }
-// cria uma extensao da classe base
+
+// Instancia objeto de página
 $pagina = new clsIndexBase();
-// cria o conteudo
+
+// Instancia objeto de conteúdo
 $miolo = new indice();
-// adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
-// gera o html
+
+// Atribui o conteúdo à  página
+$pagina->addForm($miolo);
+
+// Gera o código HTML
 $pagina->MakeAll();
-?>
