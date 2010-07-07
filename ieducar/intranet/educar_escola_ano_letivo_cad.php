f38d80b6213b168e766dfedea70dc7ee43f8e118
Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
2010-07-07 10:48:58 +0000

    Refactoring para coding standards

--- ieducar/intranet/educar_escola_ano_letivo_cad.php
+++ ieducar/intranet/educar_escola_ano_letivo_cad.php
@@ -1,136 +1,168 @@
 <?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-	*																	     *
-	*	@author Prefeitura Municipal de Itajaí								 *
-	*	@updated 29/03/2007													 *
-	*   Pacote: i-PLB Software Público Livre e Brasileiro					 *
-	*																		 *
-	*	Copyright (C) 2006	PMI - Prefeitura Municipal de Itajaí			 *
-	*						ctima@itajai.sc.gov.br					    	 *
-	*																		 *
-	*	Este  programa  é  software livre, você pode redistribuí-lo e/ou	 *
-	*	modificá-lo sob os termos da Licença Pública Geral GNU, conforme	 *
-	*	publicada pela Free  Software  Foundation,  tanto  a versão 2 da	 *
-	*	Licença   como  (a  seu  critério)  qualquer  versão  mais  nova.	 *
-	*																		 *
-	*	Este programa  é distribuído na expectativa de ser útil, mas SEM	 *
-	*	QUALQUER GARANTIA. Sem mesmo a garantia implícita de COMERCIALI-	 *
-	*	ZAÇÃO  ou  de ADEQUAÇÃO A QUALQUER PROPÓSITO EM PARTICULAR. Con-	 *
-	*	sulte  a  Licença  Pública  Geral  GNU para obter mais detalhes.	 *
-	*																		 *
-	*	Você  deve  ter  recebido uma cópia da Licença Pública Geral GNU	 *
-	*	junto  com  este  programa. Se não, escreva para a Free Software	 *
-	*	Foundation,  Inc.,  59  Temple  Place,  Suite  330,  Boston,  MA	 *
-	*	02111-1307, USA.													 *
-	*																		 *
-	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-require_once ("include/clsBase.inc.php");
-require_once ("include/clsCadastro.inc.php");
-require_once ("include/clsBanco.inc.php");
-require_once( "include/pmieducar/geral.inc.php" );
 
+/**
+ * i-Educar - Sistema de gestão escolar
+ *
+ * Copyright (C) 2006  Prefeitura Municipal de Itajaí
+ *                     <ctima@itajai.sc.gov.br>
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+ * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+ * qualquer versão posterior.
+ *
+ * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
+ * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+ * do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
+ * com este programa; se não, escreva para a Free Software Foundation, Inc., no
+ * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   iEd_Pmieducar
+ * @since     Arquivo disponível desde a versão 1.0.0
+ * @version   $Id$
+ */
+
+require_once 'include/clsBase.inc.php';
+require_once 'include/clsCadastro.inc.php';
+require_once 'include/clsBanco.inc.php';
+require_once 'include/pmieducar/geral.inc.php';
+
+/**
+ * clsIndexBase class.
+ *
+ * @author    Prefeitura Municipal de Itajaí
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   iEd_Pmieducar
+ * @since     Classe disponível desde a versão 1.0.0
+ * @version   @@package_version@@
+ */
 class clsIndexBase extends clsBase
 {
-	function Formular()
-	{
-		$this->SetTitulo( "{$this->_instituicao} i-Educar - Escola Ano Letivo" );
-		$this->processoAp = "561";
-	}
+  function Formular()
+  {
+    $this->SetTitulo($this->_instituicao . ' i-Educar - Escola Ano Letivo');
+    $this->processoAp = 561;
+  }
 }
 
+/**
+ * indice class.
+ *
+ * @author    Prefeitura Municipal de Itajaí
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   iEd_Pmieducar
+ * @since     Classe disponível desde a versão 1.0.0
+ * @version   @@package_version@@
+ */
 class indice extends clsCadastro
 {
-	/**
-	 * Referencia pega da session para o idpes do usuario atual
-	 *
-	 * @var int
-	 */
-	var $pessoa_logada;
+  var $pessoa_logada;
 
-	var $ref_cod_escola;
-	var $ano;
-	var $ref_usuario_cad;
-	var $ref_usuario_exc;
-	var $andamento;
-	var $data_cadastro;
-	var $data_exclusao;
-	var $ativo;
+  var $ref_cod_escola;
+  var $ano;
+  var $ref_usuario_cad;
+  var $ref_usuario_exc;
+  var $andamento;
+  var $data_cadastro;
+  var $data_exclusao;
+  var $ativo;
 
-	function Inicializar()
-	{
-		$retorno = "Novo";
-		@session_start();
-		$this->pessoa_logada = $_SESSION['id_pessoa'];
-		@session_write_close();
+  function Inicializar()
+  {
+    $retorno = 'Novo';
 
-		$this->ano=$_GET["ano"];
-		$this->ref_cod_escola=$_GET["cod_escola"];
+    @session_start();
+    $this->pessoa_logada = $_SESSION['id_pessoa'];
+    @session_write_close();
 
-		$obj_permissoes = new clsPermissoes();
-		$obj_permissoes->permissao_cadastra( 561, $this->pessoa_logada, 7,  "educar_escola_lst.php" );
+    $this->ano            = $_GET['ano'];
+    $this->ref_cod_escola = $_GET['cod_escola'];
 
-		$this->nome_url_sucesso = "Continuar";
-		$this->url_cancelar = "educar_escola_det.php?cod_escola={$this->ref_cod_escola}";
-		$this->nome_url_cancelar = "Cancelar";
-		return $retorno;
-	}
+    $obj_permissoes = new clsPermissoes();
+    $obj_permissoes->permissao_cadastra(561, $this->pessoa_logada, 7,
+      'educar_escola_lst.php');
 
-	function Gerar()
-	{
-		// primary keys
-		$this->campoOculto( "ref_cod_escola", $this->ref_cod_escola );
-		$this->campoOculto( "ano", $this->ano );
+    $this->nome_url_sucesso  = 'Continuar';
+    $this->url_cancelar      = 'educar_escola_det.php?cod_escola=' . $this->ref_cod_escola;
+    $this->nome_url_cancelar = 'Cancelar';
 
-		$obj_anos = new clsPmieducarEscolaAnoLetivo();
-		$lista_ano = $obj_anos->lista($this->ref_cod_escola,null,null,null,2,null,null,null,null,1);
+    return $retorno;
+  }
 
-		$ano_array = array();
+  function Gerar()
+  {
+    // Primary keys
+    $this->campoOculto('ref_cod_escola', $this->ref_cod_escola);
+    $this->campoOculto('ano', $this->ano);
 
-		if($lista_ano)
-		{
-			foreach ($lista_ano as $ano) {
-				$ano_array["{$ano['ano']}"] = $ano['ano'];
-			}
-		}
+    $obj_anos = new clsPmieducarEscolaAnoLetivo();
+    $lista_ano = $obj_anos->lista($this->ref_cod_escola, NULL, NULL, NULL, 2,
+      NULL, NULL, NULL, NULL, 1);
 
+    $ano_array = array();
 
-		$ano_atual = date("Y");
+    if ($lista_ano) {
+      foreach ($lista_ano as $ano) {
+        $ano_array[$ano['ano']] = $ano['ano'];
+      }
+    }
 
-		// foreign keys
-		$opcoes = array( "" => "Selecione" );
-		$lim = 5;
-		for ( $i=0; $i < $lim; $i++ )
-		{
-			$ano = $ano_atual + $i;
-			if(!key_exists($ano,$ano_array))
-				$opcoes["{$ano}"] = "{$ano}";
-			else
-				$lim++;
-		}
-		$this->campoLista( "ano", "Ano", $opcoes, $this->ano );
-	}
+    $ano_atual = date('Y');
 
-	function Novo()
-	{
-		@session_start();
-		 $this->pessoa_logada = $_SESSION['id_pessoa'];
-		@session_write_close();
+    // Foreign keys
+    $opcoes = array('' => 'Selecione');
+    $lim = 5;
 
-		$obj_permissoes = new clsPermissoes();
-		$obj_permissoes->permissao_cadastra( 561, $this->pessoa_logada, 7,  "educar_escola_lst.php" );
+    for ($i = 0; $i < $lim; $i++) {
+      $ano = $ano_atual + $i;
 
-		header( "Location: educar_ano_letivo_modulo_cad.php?ref_cod_escola={$this->ref_cod_escola}&ano={$this->ano}" );
-		die();
-		return true;
-	}
+      if (! key_exists($ano,$ano_array)) {
+        $opcoes[$ano] = $ano;
+      }
+      else {
+        $lim++;
+      }
+    }
+
+    $this->campoLista('ano', 'Ano', $opcoes, $this->ano);
+  }
+
+  function Novo()
+  {
+    @session_start();
+    $this->pessoa_logada = $_SESSION['id_pessoa'];
+    @session_write_close();
+
+    $obj_permissoes = new clsPermissoes();
+    $obj_permissoes->permissao_cadastra(561, $this->pessoa_logada, 7,
+      'educar_escola_lst.php');
+
+    $url = sprintf(
+      'educar_ano_letivo_modulo_cad.php?ref_cod_escola=%s&ano=%s',
+      $this->ref_cod_escola, $this->ano
+    );
+
+    header('Location: ' . $url);
+    die();
+  }
 }
 
-// cria uma extensao da classe base
+// Instancia objeto de página
 $pagina = new clsIndexBase();
-// cria o conteudo
+
+// Instancia objeto de conteúdo
 $miolo = new indice();
-// adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
-// gera o html
+
+// Atribui o conteúdo à  página
+$pagina->addForm($miolo);
+
+// Gera o código HTML
 $pagina->MakeAll();
-?>
