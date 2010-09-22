0f3e73b30952672692fd29012078c5184341fc1f
ricardo <ricardo@7437dc5b-4087-4459-821d-2f2a6da01ffc>
2010-09-22 23:29:42 +0000

    Inserido os arquivos .php para chamada dos relatórios.
    git-svn-id: svn://portabilis.com.br/portabilis/dev@16 7437dc5b-4087-4459-821d-2f2a6da01ffc

---
+++ trunk/intranet/portabilis_ficha_aluno_branco_proc.php
@@ -0,0 +1,148 @@
+<?php
+
+/*
+ * i-Educar - Sistema de gestão escolar
+ *
+ * Copyright (C) 2006  Prefeitura Municipal de Itajaí
+ *                     <ctima@itajai.sc.gov.br>
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+ * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+ * qualquer versão posterior.
+ *
+ * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
+ * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+ * do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
+ * com este programa; se não, escreva para a Free Software Foundation, Inc., no
+ * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+/**
+ * Histórico escolar.
+ *
+ * @author      Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
+ * @license     http://creativecommons.org/licenses/GPL/2.0/legalcode.pt  CC GNU GPL
+ * @package     Core
+ * @subpackage  Aluno
+ * @since       Arquivo disponível desde a versão 1.0.0
+ * @version     $Id: educar_relatorio_historico_escolar_proc.php 58 2009-07-17 18:57:29Z eriksen.paixao_bs@cobra.com.br $
+ */
+require_once 'include/clsBase.inc.php';
+require_once 'include/clsCadastro.inc.php';
+require_once 'include/clsBanco.inc.php';
+require_once 'include/pmieducar/geral.inc.php';
+require_once 'include/clsPDF.inc.php';
+
+require_once 'relatorios/phpjasperxml/class/fpdf/fpdf.php';
+require_once 'relatorios/phpjasperxml/class/PHPJasperXML.inc';
+
+
+class clsIndexBase extends clsBase
+{
+	function Formular()
+	{
+		$this->SetTitulo( "{$this->_instituicao} i-Educar - Ficha de Aluno em Branco" );
+		$this->processoAp = "707";
+		$this->renderMenu = false;
+		$this->renderMenuSuspenso = false;
+	}
+}
+
+class indice extends clsCadastro
+{
+
+
+	/**
+	 * Referencia pega da session para o idpes do usuario atual
+	 *
+	 * @var int
+	 */
+	var $pessoa_logada;
+
+
+	var $ref_cod_instituicao;
+	var $ref_cod_escola;
+	var $ref_cod_aluno;
+
+	var $nm_escola;
+	var $nm_instituicao;
+	var $nm_curso;
+	var $nm_municipio;
+
+	var $pdf;
+
+	var $page_y = 195;
+
+	var $get_link;
+	var $cor_fundo;
+	var $endereco;
+
+	var $meses_do_ano = array(
+							 "1" => "JANEIRO"
+							,"2" => "FEVEREIRO"
+							,"3" => "MARÇO"
+							,"4" => "ABRIL"
+							,"5" => "MAIO"
+							,"6" => "JUNHO"
+							,"7" => "JULHO"
+							,"8" => "AGOSTO"
+							,"9" => "SETEMBRO"
+							,"10" => "OUTUBRO"
+							,"11" => "NOVEMBRO"
+							,"12" => "DEZEMBRO"
+						);
+
+	function renderHTML()
+	{
+	
+	$xml =  simplexml_load_file("relatorios/jasperreports/portabilis_ficha_aluno_branco.jrxml");
+	
+
+/*	print "instituicao: ";
+	print $_POST['ref_cod_instituicao'];
+	print "escola: ";
+	print $_POST['ref_cod_escola'];
+	print "aluno: ";
+	print $_POST['ref_cod_aluno'];
+	print "serie: ";
+	print $_POST['ref_ref_cod_serie'];
+	print "aluno: ";
+	print $_POST['nm_aluno'];
+	print $_POST['ano'];
+	print $_POST['data_validade'];
+	print "passei";
+	
+*/
+
+	$PHPJasperXML = new PHPJasperXML();
+	$PHPJasperXML->debugsql=false;
+	$PHPJasperXML->arrayParameter=array("instituicao"=>$_POST['ref_cod_instituicao'],"escola"=>$_POST['ref_cod_escola']); 
+    $PHPJasperXML->xml_dismantle($xml);
+
+	$PHPJasperXML->transferDBtoArray($server,$user,$pass,$db,$port);
+	$PHPJasperXML->outpage("I");    //page output method I:standard output  D:Download file
+
+		if($_POST){
+			$query = "";
+			foreach ($_POST as $key => $value) {
+				//$query .= $key . '=' . $value . '&';
+				//$this->$key = $value;
+			}
+		}
+	}  
+}
+
+// cria uma extensao da classe base
+$pagina = new clsIndexBase();
+// cria o conteudo
+$miolo = new indice();
+// adiciona o conteudo na clsBase
+$pagina->addForm( $miolo );
+// gera o html
+$pagina->MakeAll();
+
+?>
