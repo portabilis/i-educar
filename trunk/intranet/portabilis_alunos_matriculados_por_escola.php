396291db794d1b88aa6ccbf61891bf5a4a3e6b77
ricardo <ricardo@7437dc5b-4087-4459-821d-2f2a6da01ffc>
2010-10-19 18:10:37 +0000

    - Foi criado um novo relatório chamado de Alunos Matriculados Por Escola, solicitado pela Sec. de Educação de cliente.
    git-svn-id: svn://portabilis.com.br/portabilis/dev@24 7437dc5b-4087-4459-821d-2f2a6da01ffc

---
+++ trunk/intranet/portabilis_alunos_matriculados_por_escola.php
@@ -0,0 +1,153 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	*																	     *
+	*	@author Prefeitura Municipal de Itajaí								 *
+	*	@updated 29/03/2007													 *
+	*   Pacote: i-PLB Software Público Livre e Brasileiro					 *
+	*																		 *
+	*	Copyright (C) 2006	PMI - Prefeitura Municipal de Itajaí			 *
+	*						ctima@itajai.sc.gov.br					    	 *
+	*																		 *
+	*	Este  programa  é  software livre, você pode redistribuí-lo e/ou	 *
+	*	modificá-lo sob os termos da Licença Pública Geral GNU, conforme	 *
+	*	publicada pela Free  Software  Foundation,  tanto  a versão 2 da	 *
+	*	Licença   como  (a  seu  critério)  qualquer  versão  mais  nova.	 *
+	*																		 *
+	*	Este programa  é distribuído na expectativa de ser útil, mas SEM	 *
+	*	QUALQUER GARANTIA. Sem mesmo a garantia implícita de COMERCIALI-	 *
+	*	ZAÇÃO  ou  de ADEQUAÇÃO A QUALQUER PROPÓSITO EM PARTICULAR. Con-	 *
+	*	sulte  a  Licença  Pública  Geral  GNU para obter mais detalhes.	 *
+	*																		 *
+	*	Você  deve  ter  recebido uma cópia da Licença Pública Geral GNU	 *
+	*	junto  com  este  programa. Se não, escreva para a Free Software	 *
+	*	Foundation,  Inc.,  59  Temple  Place,  Suite  330,  Boston,  MA	 *
+	*	02111-1307, USA.													 *
+	*																		 *
+	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+require_once ("include/clsBase.inc.php");
+require_once ("include/clsCadastro.inc.php");
+require_once ("include/clsBanco.inc.php");
+require_once( "include/pmieducar/geral.inc.php" );
+require_once ("include/clsPDF.inc.php");
+
+class clsIndexBase extends clsBase
+{
+	function Formular()
+	{
+		$this->SetTitulo( "{$this->_instituicao} i-Educar - Alunos Matriculados por Escola" );
+		$this->processoAp = "999105";
+	}
+}
+
+class indice extends clsCadastro
+{
+
+
+	/**
+	 * Referencia pega da session para o idpes do usuario atual
+	 *
+	 * @var int
+	 */
+	var $pessoa_logada;
+
+	var $pdf;
+
+
+	var $page_y = 139;
+
+
+
+	function Inicializar()
+	{
+		$retorno = "Novo";
+		@session_start();
+		$this->pessoa_logada = $_SESSION['id_pessoa'];
+		@session_write_close();
+
+		return $retorno;
+	}
+
+	function Gerar()
+	{
+		@session_start();
+		$this->pessoa_logada = $_SESSION['id_pessoa'];
+		@session_write_close();
+
+		if($_POST){
+			foreach ($_POST as $key => $value) {
+				$this->$key = $value;
+
+			}
+		}
+
+		if($this->ref_cod_escola)
+			$this->ref_ref_cod_escola = $this->ref_cod_escola;
+
+		$get_escola = true;
+		//$get_curso = true;
+		//$get_escola_curso_serie = true;
+		//$get_turma = true;
+		$sem_padrao = true;
+		$instituicao_obrigatorio = true;
+		$escola_obrigatorio = false;
+		
+		$this->ano = $ano_atual = date("Y");
+		
+		//campo adicionado para pegar por parametro o Ano Letivo da Escola
+		$this->campoNumero( "ano", "Ano", $this->ano, 4, 4, true);
+
+		include("include/pmieducar/educar_campo_lista.php");
+		
+		$this->url_cancelar = "educar_index.php";
+		$this->nome_url_cancelar = "Cancelar";
+
+		$this->acao_enviar = 'acao2()';
+		$this->acao_executa_submit = false;
+
+	}
+}
+
+// cria uma extensao da classe base
+$pagina = new clsIndexBase();
+// cria o conteudo
+$miolo = new indice();
+// adiciona o conteudo na clsBase
+$pagina->addForm( $miolo );
+// gera o html
+$pagina->MakeAll();
+
+
+?>
+
+
+<script>
+
+function acao2()
+{
+
+	if(!acao())
+		return false;
+
+	document.formcadastro.target = '_blank';
+	document.getElementById( 'btn_enviar' ).disabled =false;
+	document.formcadastro.submit();
+}
+
+// Chamado do arquivo que ira processar o relatorio
+document.formcadastro.action = 'portabilis_alunos_matriculados_por_escola_proc.php';
+
+document.getElementById('ref_cod_escola').onchange = function()
+{
+	getEscolaCurso();
+}
+
+</script>
+
+
+
+
+
+
+
+
