fc056f2a1c8f18457fd57783132cf0cdad0f32ee
Lucas D'Avila <lucassdvl@gmai.com>
2012-01-16 19:20:35 -0200

    Ativado escola_ano_escolar (caso inativo) ao editar ano_escolar_modulo, closes issue #116

--- trunk/intranet/include/pmieducar/clsPmieducarAnoLetivoModulo.inc.php
+++ trunk/intranet/include/pmieducar/clsPmieducarAnoLetivoModulo.inc.php
@@ -241,6 +241,20 @@ class clsPmieducarAnoLetivoModulo
 				$gruda = ", ";
 			}
 
+      // ativa escolaAnoLetivo se estiver desativado
+      // (quando o escolaAnoLetivo é 'excluido' o registro não é removido)
+      $escolaAnoLetivo = new clsPmieducarEscolaAnoLetivo($this->ref_ref_cod_escola, 
+                                                         $this->ref_ano,
+                                                         null,
+                                                         $_SESSION['id_pessoa'],
+                                                         null,
+                                                         null,
+                                                         null,
+                                                         1);
+      $escolaAnoLetivoDetalhe = $escolaAnoLetivo->detalhe();
+
+      if (isset($escolaAnoLetivoDetalhe['ativo']) and $escolaAnoLetivoDetalhe['ativo'] != '1')
+        $escolaAnoLetivo->edita();
 
 			$db->Consulta( "INSERT INTO {$this->_tabela} ( $campos ) VALUES( $valores )" );
 			return true;
