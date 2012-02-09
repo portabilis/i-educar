5c937b81537643e6497c1c56915ece28b856dfab
Lucas D'Avila <lucassdvl@gmail.com>
2012-02-09 15:43:20 -0200

    corrigido relacao de alunos por turma, para mostrar alunos em outros anos que nao estejam em andamento, adicionado obrigatoriedade para campos curso, serie e turma closes #122

--- trunk/intranet/portabilis_relacao_alunos_por_turma.php
+++ trunk/intranet/portabilis_relacao_alunos_por_turma.php
@@ -10,13 +10,13 @@ class PortabilisRelacaoAlunosTurma extends Report
   function setForm()
   {
 
-    $get_escola = true;
-    $get_curso = true;
-    $get_escola_curso_serie = true;
-    $get_turma = true;
+    $get_escola = $escola_obrigatorio = true;
+    $get_curso = $curso_obrigatorio = true;
+    $get_escola_curso_serie = $escola_curso_serie_obrigatorio = true;
+    $get_turma = $turma_obrigatorio = true;
     $instituicao_obrigatorio = true;
     $escola_obrigatorio = true;
-    
+
     $this->ano = $ano_atual = date("Y");
     $this->campoNumero( "ano", "Ano", $this->ano, 4, 4, true);
 
@@ -29,30 +29,30 @@ class PortabilisRelacaoAlunosTurma extends Report
     $opcoes[6] = "Abandono";
     $opcoes[9] = "Exceto Transferidos/Abandono";
     $opcoes[10] = "Todas";
-    
+
     $this->campoLista('situacao', 'Situação', $opcoes, 3, $this->situacao);
-    
+
   }
 
   function onValidationSuccess()
   {
-  
+
     $this->addArg('ano', (int)$_POST['ano']);
-  
-    if (! isset($_POST['ref_cod_curso']))
+
+    if (! isset($_POST['ref_cod_curso']) || trim($_POST['ref_cod_curso']) == '')
       $this->addArg('curso', 0);
     else
       $this->addArg('curso', (int)$_POST['ref_cod_curso']);
-      
-    if (! isset($_POST['ref_ref_cod_serie']))
+
+    if (! isset($_POST['ref_ref_cod_serie']) || trim($_POST['ref_ref_cod_serie']) == '')
       $this->addArg('serie', 0);
     else
       $this->addArg('serie', (int)$_POST['ref_ref_cod_serie']);
-      
-    if (! isset($_POST['ref_cod_turma']))
+
+    if (! isset($_POST['ref_cod_turma']) || trim($_POST['ref_cod_turma']) == '')
       $this->addArg('turma', 0);
     else
-      $this->addArg('turma', (int)$_POST['ref_cod_turma']);   
+      $this->addArg('turma', (int)$_POST['ref_cod_turma']);
 
     $this->addArg('instituicao', (int)$_POST['ref_cod_instituicao']);
     $this->addArg('escola', (int)$_POST['ref_cod_escola']);
@@ -66,6 +66,9 @@ $report = new PortabilisRelacaoAlunosTurma($name = 'Relação de Alunos por Turma'
 $report->addRequiredField('ano');
 $report->addRequiredField('ref_cod_instituicao', 'instituicao');
 $report->addRequiredField('ref_cod_escola', 'escola');
+$report->addRequiredField('ref_cod_curso', 'curso');
+$report->addRequiredField('ref_ref_cod_serie', 'serie');
+$report->addRequiredField('ref_cod_turma', 'turma');
 $report->addRequiredField('situacao', 'situacao');
 
 $report->render();
