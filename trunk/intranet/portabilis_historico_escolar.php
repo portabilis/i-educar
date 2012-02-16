24304704506e8a955a28ef987d3a45090bc3eff5
Lucas D'Avila <lucassdvl@gmail.com>
2012-02-16 17:53:42 -0200

    Corrigido histórico 8 anos para mostrar informacoes do ultimo histórico de cada série, removido campo ano do formulário de impressão do histórico de 8 anos  #127

--- trunk/intranet/portabilis_historico_escolar.php
+++ trunk/intranet/portabilis_historico_escolar.php
@@ -15,46 +15,46 @@ class PortabilisHistoricoEscolarSeries extends Report
     $get_aluno = true;
     $escola_obrigatorio = true;
     $aluno_obrigatorio = true;
-    
-    $this->ano = $ano_atual = date("Y");
-    $this->campoNumero( "ano", "Ano", $this->ano, 4, 4, true);
 
-    include("include/pmieducar/educar_campo_lista.php");  
- 
-    
+    #$this->ano = $ano_atual = date("Y");
+    #$this->campoNumero( "ano", "Ano", $this->ano, 4, 4, true);
+
+    include("include/pmieducar/educar_campo_lista.php");
+
+
   }
 
   function onValidationSuccess()
   {
-    $this->addArg('ano', (int)$_POST['ano']);
+    #$this->addArg('ano', (int)$_POST['ano']);
     $this->addArg('instituicao', (int)$_POST['ref_cod_instituicao']);
     $this->addArg('escola', (int)$_POST['ref_cod_escola']);
     $this->addArg('aluno', (int)$_POST['ref_cod_aluno']);
   }
 
- function validate()
-  {
-
-    $anos_historicos_sem_situacao = $this->db->select("select ano from pmieducar.historico_escolar where ref_cod_aluno = {$_POST['ref_cod_aluno']} and aprovado is null");
-
-    if (count($anos_historicos_sem_situacao))
-    {
-      $msg = 'O(s) histórico(s) do(s) ano(s): ';
-      foreach($anos_historicos_sem_situacao as $ano)
-      {
-        $msg .= "{$ano['ano']}, ";
-      }
-      $msg .= 'está(ão) sem situação definida.';
-      $this->addValidationError($msg);
-    }
-
-  }
-
+# function validate()
+#  {
+#
+#    $anos_historicos_sem_situacao = $this->db->select("select ano from pmieducar.historico_escolar where ref_cod_aluno = {$_POST['ref_cod_aluno']} and aprovado is null");
+#
+#    if (count($anos_historicos_sem_situacao))
+#    {
+#      $msg = 'O(s) histórico(s) do(s) ano(s): ';
+#      foreach($anos_historicos_sem_situacao as $ano)
+#      {
+#        $msg .= "{$ano['ano']}, ";
+#      }
+#      $msg .= 'está(ão) sem situação definida.';
+#      $this->addValidationError($msg);
+#    }
+#
+#  }
+#
 }
 
 $report = new PortabilisHistoricoEscolarSeries($name = 'Histórico Escolar (8 Anos)', $templateName = 'portabilis_historico_escolar');
 
-$report->addRequiredField('ano','ano');
+#$report->addRequiredField('ano','ano');
 $report->addRequiredField('ref_cod_instituicao', 'instituicao');
 $report->addRequiredField('ref_cod_escola', 'escola');
 $report->addRequiredField('ref_cod_aluno', 'aluno');
