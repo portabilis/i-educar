a7be49465f5cd4a163d9046195a4e90ae385f8b2
Lucas D'Avila <lucassdvl@gmail.com>
2012-03-16 15:41:36 -0300

    Adicionado campo pesquisa cliente

--- trunk/modules/Biblioteca/Views/ReservaController.php
+++ trunk/modules/Biblioteca/Views/ReservaController.php
@@ -58,40 +58,21 @@ class ReservaController extends Core_Controller_Page_ListController
 
   protected function setVars()
   {
-
-    #$this->ref_cod_aluno = $_GET['aluno_id'];
+    #$this->ref_cod_cliente = $_GET['cliente_id'];
     #$this->ref_cod_instituicao = $_GET['instituicao_id'];
     #$this->ref_cod_escola = $_GET['escola_id'];
-    #$this->ref_cod_curso = $_GET['curso_id'];
-    #$this->ref_cod_turma = $_GET['turma_id'];
-    #$this->ref_ref_cod_serie = $this->ref_cod_serie = $_GET['serie_id'];
-    #$this->ano = $_GET['ano'];
 
-    #if ($this->ref_cod_aluno)
+    #if ($this->ref_cod_cliente)
     #{
-    #  $nome_aluno_filtro = new clsPmieducarAluno();
-    #  $nome_aluno_filtro = $nome_aluno_filtro->lista($int_cod_aluno = $this->ref_cod_aluno);
-    #  $this->nm_aluno = $nome_aluno_filtro[0]['nome_aluno'];
+    #  $cliente = new clsPmieducarCliente();
+    #  $cliente = $cliente->lista();
+    #  $this->nm_cliente = $cliente[0]['nome_cliente'];
     #}
   }
 
 
   protected function setSelectionFields()
   {
-
-    #variaveis usadas pelo modulo /intranet/include/pmieducar/educar_campo_lista.php
-    #$this->verificar_campos_obrigatorios = true;
-    #$this->add_onchange_events = true;
-
-    #$this->campoNumero( "ano", "Ano", date("Y"), 4, 4, true);
-    #$instituicao_obrigatorio = true;
-    #$get_escola = $escola_obrigatorio = true;
-    #$get_curso = true;
-    #$get_escola_curso_serie = true;
-    #$get_turma = true;
-    #$get_alunos_matriculados = true;
-    #include 'include/pmieducar/educar_campo_lista.php';
-
     $dynamicSelectMenusHelper = new DynamicSelectMenusHelper($this);
 
     $dynamicSelectMenusHelper->instituicao(array('value' => $this->ref_cod_instituicao));
@@ -100,7 +81,8 @@ class ReservaController extends Core_Controller_Page_ListController
 
     # TODO add field código cliente
     # TODO add field senha cliente
-    # TODO add field find cliente
+    $dynamicSelectMenusHelper->bibliotecaPesquisaCliente(array('value' => $this->nm_cliente,
+                                                               'required' => true));
   }
 
 
@@ -119,18 +101,12 @@ class ReservaController extends Core_Controller_Page_ListController
 
       <tr>
         <td><label for=''>LabelName *</label></td>
-        <td colspan='2'><input type='text' id='' name='' class='obrigatorio disable-on-search clear-on-change-curso validates-value-is-numeric'></input></td>
+        <td colspan='2'><input type='text' id='' name='' class='obrigatorio disable-on-search validates-value-is-numeric'></input></td>
       </tr>
 
     </table>";
 
     $this->appendOutput($resourceOptionsTable);
-
-    //$this->appendOutput('<script type="text/javascript" src="scripts/jquery/jquery.js"></script>');
-    //$this->appendOutput('<script type="text/javascript" src="scripts/jquery/jquery.form.js"></script>');
-    //$this->appendOutput('<script type="text/javascript" src="scripts/jquery/jquery-ui.js"></script>');
-    //$this->appendOutput('<link type="text/css" rel="stylesheet" href="/modules/HistoricoEscolar/Static/styles/processamentoController.css"></script>');
-    //$this->appendOutput('<link type="text/css" rel="stylesheet" href="/intranet/styles/jquery-ui/ui-lightness/jquery-ui.css"></script>');
 
     ApplicationHelper::loadJavascript($this, 'scripts/jquery/jquery.form.js');
     ApplicationHelper::loadJavascript($this, '/modules/Portabilis/Assets/Javascripts/FrontendApi.js');
