fdb6daf4bb6d429229a983d334e9a2a5746f6fa3
Lucas D'Avila <lucassdvl@gmail.com>
2012-06-05 10:17:27 -0300

    Alterado para usar novo helper ano, e para escola não ficar obrigatório

--- trunk/intranet/portabilis_relacao_escolas.php
+++ trunk/intranet/portabilis_relacao_escolas.php
@@ -5,37 +5,26 @@
 
 require_once("include/portabilis/report.php");
 
-class PortabilisRelacaoEscolas extends Report
-{
-  function setForm()
-  {
-
-    $get_escola = true;
-    $instituicao_obrigatorio = true;
-    $escola_obrigatorio = false;
-    $this->ano = $ano_atual = date("Y");
-    $this->campoNumero( "ano", "Ano", $this->ano, 4, 4, true);
-
-    include("include/pmieducar/educar_campo_lista.php");
-    //$this->campoLista("ref_cod_escola","Escola",array('' => 'Selecione'),'',"",false,"","",false,false);
+class PortabilisRelacaoEscolas extends Report {
+  function setForm() {
+    $this->addFilterFor(array('ano', 'instituicao'));
+    $this->addFilterFor('escola', array('required' => false));
   }
 
-  function onValidationSuccess()
-  {
+  function onValidationSuccess() {
     if (! isset($_POST['ref_cod_escola']))
       $this->addArg('escola', 0);
     else
       $this->addArg('escola', (int)$_POST['ref_cod_escola']);
-    
+
     $this->addArg('instituicao', (int)$_POST['ref_cod_instituicao']);
   }
-
 }
 
 $report = new PortabilisRelacaoEscolas($name = 'Relação de Escolas', $templateName = 'portabilis_relacao_escolas');
 
 $report->addRequiredField('ano');
 $report->addRequiredField('ref_cod_instituicao', 'instituição');
-  
+
 $report->render();
 ?>
