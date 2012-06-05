aca24c755ad06ff77a003a2266bdb96319e2dd21
Lucas D'Avila <lucassdvl@gmail.com>
2012-06-05 11:19:11 -0300

    Adicionado helpers select menu para Curso, dataInicial e DataFinal

---
+++ trunk/modules/DynamicSelectMenus/Assets/Javascripts/DynamicCursos.js
@@ -0,0 +1,36 @@
+(function($){
+  $(document).ready(function(){
+    var $escolaField = $('#ref_cod_escola');
+    var $cursoField  = $('#ref_cod_curso');
+
+    var handleGetCursos = function(resources) {
+      var selectOptions = xmlResourcesToSelectOptions(resources, 'query', 'cod_curso');
+      updateSelect($cursoField, selectOptions, "Selecione uma curso");
+    }
+
+    var updateCursos = function(){
+      resetSelect($cursoField);
+
+      if ($escolaField.val() && $escolaField.is(':enabled')) {
+        $cursoField.children().first().html('Aguarde carregando...');
+
+        var urlForGetCursos = getResourceUrlBuilder.buildUrl('educar_curso_xml.php', '', {
+                                                       esc : $escolaField.attr('value') });
+
+        var options = {
+          url : urlForGetCursos,
+          dataType : 'xml',
+          success  : handleGetCursos
+        };
+
+        getResources(options, handleGetCursos);
+      }
+
+      $cursoField.change();
+    };
+
+    // bind onchange event
+    $escolaField.change(updateCursos);
+
+  }); // ready
+})(jQuery);
