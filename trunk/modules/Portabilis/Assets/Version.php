e2047da5ad28b9c3fa79d0147667d0bb0d5e73a9
Lucas D'Avila <lucassdvl@gmail.com>
2012-12-06 14:27:33 -0200

    Implementado controle de versão dos assets

---
+++ trunk/modules/Portabilis/Assets/Version.php
@@ -0,0 +1,10 @@
+<?php
+
+/* mantem a versão dos assets (js, css), a qual pode ser alterada para resetar o cache.
+   usado pelo metodos loadJavascript e loadStylesheet do helper Portabilis_View_Helper_Application */
+
+class Portabilis_Assets_Version {
+  const VERSION = "1_0_0";
+}
+
+?>
