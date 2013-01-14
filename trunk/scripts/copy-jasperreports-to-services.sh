c747f6ea5f8a49a27120c52381a9bc1799be3f93
Lucas D'Avila <lucas@lucasdavi.la>
2013-01-14 10:34:55 -0200

    Movido script para ieducar-utils

--- trunk/scripts/copy-jasperreports-to-services.sh
+++
@@ -1,8 +0,0 @@
-cd ~/projects/i*educar/trunk/intranet/relatorios/jasperreports/
-echo "Copiando arquivos jasper para services local"
-
-path="/sites_media_root/services/reports/jasper/"
-sudo mkdir -p /sites_media_root/services/reports/jasper/
-sudo cp *.jasper $path
-
-sudo chmod -R 777 /sites_media_root
