2e33b9d9fbacc1de6f078f5afd934f983e847ded
Lucas D'Avila <lucas@lucasdavi.la>
2013-01-31 15:39:48 -0200

    Refatorado deprecacoes, removido mensagens todo, fixme #137

--- trunk/lib/Portabilis/Message.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'lib/Portabilis/Messenger.php';
-
-# deprecated class
-#TODO trocar todas referencias de Message para Portabilis_Messenger
-class Message extends Portabilis_Messenger {
-}
