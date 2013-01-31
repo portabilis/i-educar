e9e4b6ed3cb763f66eea016e88cbcd97478d6b9c
Lucas D'Avila <lucas@lucasdavi.la>
2013-01-31 20:20:15 -0200

    Adicionado modulo Error (para exibir detalhes de erros ocorridos), adicionado try a /module/index.php para pegar erros e redirecionar para /modules/Error

--- ieducar/module/index.php
+++ ieducar/module/index.php
@@ -39,29 +39,50 @@ require_once 'CoreExt/Controller/Request.php';
 require_once 'CoreExt/Controller/Front.php';
 require_once 'CoreExt/DataMapper.php';
 
-// Objeto de requisição
-$request = new CoreExt_Controller_Request();
+require_once 'modules/Error/Mailers/NotificationMailer.php';
 
-// Helper de URL. Auxilia para criar uma URL no formato http://www.example.org/module
-$url = CoreExt_View_Helper_UrlHelper::getInstance();
-$url = $url->url($request->get('REQUEST_URI'), array('components' => CoreExt_View_Helper_UrlHelper::URL_HOST));
+try
+{
+  // Objeto de requisição
+  $request = new CoreExt_Controller_Request();
 
-// Configura o baseurl da request
-$request->setBaseurl(sprintf('%s/module', $url));
+  // Helper de URL. Auxilia para criar uma URL no formato http://www.example.org/module
+  $url = CoreExt_View_Helper_UrlHelper::getInstance();
+  $url = $url->url($request->get('REQUEST_URI'), array('components' => CoreExt_View_Helper_UrlHelper::URL_HOST));
 
-// Configura o DataMapper para usar uma instância de clsBanco com fetch de resultados
-// usando o tipo FETCH_ASSOC
-CoreExt_DataMapper::setDefaultDbAdapter(new clsBanco(array('fetchMode' => clsBanco::FETCH_ASSOC)));
+  // Configura o baseurl da request
+  $request->setBaseurl(sprintf('%s/module', $url));
 
-// Inicia o Front Controller
-$frontController = CoreExt_Controller_Front::getInstance();
-$frontController->setRequest($request);
+  // Configura o DataMapper para usar uma instância de clsBanco com fetch de resultados
+  // usando o tipo FETCH_ASSOC
+  CoreExt_DataMapper::setDefaultDbAdapter(new clsBanco(array('fetchMode' => clsBanco::FETCH_ASSOC)));
 
-// Configura o caminho aonde os módulos estão instalados
-$frontController->setOptions(
-  array('basepath' => PROJECT_ROOT . DS . 'modules')
-);
-$frontController->dispatch();
+  // Inicia o Front Controller
+  $frontController = CoreExt_Controller_Front::getInstance();
+  $frontController->setRequest($request);
 
-// Resultado
-print $frontController->getViewContents();
+  // Configura o caminho aonde os módulos estão instalados
+  $frontController->setOptions(
+    array('basepath' => PROJECT_ROOT . DS . 'modules')
+  );
+
+  $frontController->dispatch();
+
+  // Resultado
+  print $frontController->getViewContents();
+}
+catch (Exception $e) {
+  $lastError = error_get_last();
+
+  @session_start();
+  $_SESSION['last_error_message']     = $e->getMessage();
+  $_SESSION['last_php_error_message'] = $lastError['message'];
+  $_SESSION['last_php_error_line']    = $lastError['line'];
+  $_SESSION['last_php_error_file']    = $lastError['file'];
+  @session_write_close();
+
+  error_log("Erro inesperado (pego em /module/index.php): " . $e->getMessage());
+  NotificationMailer::unexpectedError($e->getMessage());
+
+  die("<script>document.location.href = '/module/Error/unexpected';</script>");
+}
