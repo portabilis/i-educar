e3697e391c4ac61f3c16ac74c4481e81ab4b90ee
Lucas D'Avila <lucas@lucasdavi.la>
2013-01-31 21:00:37 -0200

    Alterado topo páginas

--- ieducar/intranet/topo.php
+++ ieducar/intranet/topo.php
@@ -1,6 +1,20 @@
+<?php
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/bootstrap.php';
+  $entityName = $GLOBALS['coreExt']['Config']->app->entity->name;
+?>
+
 <html>
-<head></head>
-<body bgcolor="white" background="imagens/fundo.png" marginwidth="0" leftmargin="0" topmargin="0">
-<img src="imagens/pingus.jpg">
-</body>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel=stylesheet type='text/css' href='styles/reset.css' />
+    <link rel=stylesheet type='text/css' href='styles/portabilis.css' />
+  </head>
+
+  <body>
+    <div id="cabecalho" class="texto-normal">
+      <div id="ccorpo">
+        <p><a id="logo" href="/">i-Educar</a> <span id="status"><span id="entidade"><?php echo $entityName; ?></span></span></p>
+      </div>
+    </div>
+  </body>
 </html>
