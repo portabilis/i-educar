8da6756db829ae3ed435df56c5888754d0c55423
Lucas D'Avila <lucas@lucasdavi.la>
2013-02-04 15:04:50 -0200

    Corrigido API matricula, para pesquisar por códigos similares ao informado fixes #2

--- ieducar/modules/Api/Views/MatriculaController.php
+++ ieducar/modules/Api/Views/MatriculaController.php
@@ -62,7 +62,7 @@ class MatriculaController extends ApiCoreController
             pmieducar.aluno, cadastro.pessoa where aluno.cod_aluno = matricula.ref_cod_aluno and
             pessoa.idpes = aluno.ref_idpes and aluno.ativo = matricula.ativo and
             matricula.ativo = 1 and matricula.aprovado in (1, 2, 3, 4, 7, 8, 9) and
-            (matricula.cod_matricula like $1 or matricula.ref_cod_aluno like $1) and
+            (matricula.cod_matricula like $1||'%' or matricula.ref_cod_aluno like $1||'%') and
             (select case when $2 != 0 then matricula.ref_ref_cod_escola = $2 else 1=1 end) and
             (select case when $3 != 0 then matricula.ano = $3 else 1=1 end) limit 15";
   }
