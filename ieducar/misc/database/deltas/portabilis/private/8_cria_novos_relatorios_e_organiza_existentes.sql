36aad3b817b2c72ce802f2133cd1bc6c9158697d
Lucas D'Avila <lucas@lucasdavi.la>
2013-02-22 18:14:02 -0300

    Removido migração que foi adicionada as migrações da comunidade

--- ieducar/misc/database/deltas/portabilis/private/8_cria_novos_relatorios_e_organiza_existentes.sql
+++ ieducar/misc/database/deltas/portabilis/private/8_cria_novos_relatorios_e_organiza_existentes.sql
@@ -13,9 +13,6 @@
 	update pmicontrolesis.menu set ord_menu = 2 where cod_menu = 999450;
 	update pmicontrolesis.menu set ord_menu = 3 where cod_menu = 999460;
 
-	ALTER TABLE pmieducar.historico_escolar ADD COLUMN frequencia double precision;
-	ALTER TABLE pmieducar.historico_escolar ALTER COLUMN frequencia SET STORAGE PLAIN;
-
 	insert into portal.menu_submenu values(999216,55,2,'Atestado de Transferência','portabilis_atestado_transferencia.php',NULL,3);
 	insert into portal.menu_funcionario values(1,0,0,999216);
 	insert into pmicontrolesis.menu values(999216,999216,999400,'Atestado de Transferência',2,'portabilis_atestado_transferencia.php','_self',1,15,NULL);
@@ -30,13 +27,13 @@
 	insert into portal.menu_funcionario values(1,0,0,999217);
 	insert into pmicontrolesis.menu values(999217,999217,999301,'Ocorrências Disciplinares por Aluno',4,'portabilis_alunos_ocorrencias_disciplinares.php','_self',1,15,NULL);
 	insert into pmieducar.menu_tipo_usuario values(1,999217,1,0,1);
- 	
+
 	UPDATE pmicontrolesis.menu set ref_cod_ico = 192 where ref_cod_ico is null;
 	UPDATE pmicontrolesis.menu set ref_cod_ico = 36 where cod_menu = 999500;
+
 	-- //@UNDO
-	
-	ALTER TABLE pmieducar.historico_escolar DROP COLUMN frequencia;
-	
+
+
 	delete from portal.menu_submenu where cod_menu_submenu = 999216;
 	delete from portal.menu_funcionario where ref_cod_menu_submenu = 999216;
 	delete from pmieducar.menu_tipo_usuario where ref_cod_menu_submenu = 999216;
@@ -51,5 +48,5 @@
 	delete from portal.menu_funcionario where ref_cod_menu_submenu = 999217;
 	delete from pmieducar.menu_tipo_usuario where ref_cod_menu_submenu = 999217;
 	delete from pmicontrolesis.menu where cod_menu = 999217;
-	
+
 	-- //
