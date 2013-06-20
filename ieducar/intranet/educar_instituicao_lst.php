f9c22dcf892e30f57280a368b16245db7a5709bf
Rafael Ferreira <raafael.saantos@gmail.com>
2013-06-19 23:39:27 -0300

    Modificacoes nos arquivos de localizacao: erro persiste no CSS (ieducar-escola diferente de ieducar-biblioteca

--- ieducar/intranet/educar_instituicao_lst.php
+++ ieducar/intranet/educar_instituicao_lst.php
@@ -155,7 +155,7 @@ class indice extends clsListagem
                 $localizacao = new LocalizacaoSistema();
                 $localizacao->entradaCaminhos( array(
                     $_SERVER['SERVER_NAME']."/intranet" => "i-Educar",
-                    "educar_index.php"       => "Escola",
+                    "educar_index.php"                  => "Escola",
                     ""                                  => "Cadastro de Instituição"
                 ));
                 $this->enviaLocalizacao($localizacao->montar());
