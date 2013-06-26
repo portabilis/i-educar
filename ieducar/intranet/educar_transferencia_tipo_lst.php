cc6c8ad27278e8c9c363f41160138388f51a8fbd
Vanessa Barbosa <vanessabmdf@gmail.com>
2013-06-26 20:42:59 -0300

    Issue 15 : Algumas páginas com a localização do sistema

--- ieducar/intranet/educar_transferencia_tipo_lst.php
+++ ieducar/intranet/educar_transferencia_tipo_lst.php
@@ -28,6 +28,7 @@ require_once ("include/clsBase.inc.php");
 require_once ("include/clsListagem.inc.php");
 require_once ("include/clsBanco.inc.php");
 require_once( "include/pmieducar/geral.inc.php" );
+require_once ("include/localizacaoSistema.php");
 
 class clsIndexBase extends clsBase
 {
@@ -35,6 +36,7 @@ class clsIndexBase extends clsBase
 	{
 		$this->SetTitulo( "{$this->_instituicao} i-Educar - Motivo Transfer&ecirc;ncia" );
 		$this->processoAp = "575";
+                $this->addEstilo( "localizacaoSistema" );
 	}
 }
 
@@ -194,6 +196,14 @@ class indice extends clsListagem
 			$this->nome_acao = "Novo";
 		}
 		$this->largura = "100%";
+                
+                $localizacao = new LocalizacaoSistema();
+                $localizacao->entradaCaminhos( array(
+                    $_SERVER['SERVER_NAME']."/intranet" => "i-Educar",
+                    "educar_index.php"                  => "Escola",
+                    ""                                  => "Lista de Tipos de Transferência"
+                ));
+                $this->enviaLocalizacao($localizacao->montar());
 	}
 }
 // cria uma extensao da classe base
