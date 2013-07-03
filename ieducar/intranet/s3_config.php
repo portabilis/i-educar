79433c72b43e36cf924d732b69a6f8702413149c
Lucas Schmoeller da Silva <llucas_gs@hotmail.com>
2013-07-03 15:26:17 -0300

    #326 FImplementação da Foto

---
+++ ieducar/intranet/s3_config.php
@@ -0,0 +1,17 @@
+<?php
+// Bucket Name
+require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/bootstrap.php';
+$bucket="apps.ieducar.images.".$GLOBALS['coreExt']['Config']->app->database->dbname;
+if (!class_exists('S3'))
+	require_once 'S3.php';
+/*
+//AWS access info
+if (!defined('awsAccessKey')) define('awsAccessKey', '***REMOVED***');
+if (!defined('awsSecretKey')) define('awsSecretKey', '***REMOVED***');
+*/
+	
+//instantiate the class
+$s3 = new S3('***REMOVED***', '***REMOVED***');
+
+$s3->putBucket($bucket); //, S3::ACL_PUBLIC_READ)
+?>
