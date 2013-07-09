873740882f9e1023d17fad953d8da0be0bb5d710
Lucas Schmoeller da Silva <llucas_gs@hotmail.com>
2013-07-09 09:26:02 -0300

    #431 Diretorios e inis referente a Foto

--- ieducar/intranet/s3_config.php
+++ ieducar/intranet/s3_config.php
@@ -1,17 +1,17 @@
 <?php
 // Bucket Name
 require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/bootstrap.php';
-$bucket="apps-ieducar-images-".$GLOBALS['coreExt']['Config']->app->database->dbname;
+
+$bucket= $GLOBALS['coreExt']['Config']->app->aws->bucketname;
+$directory=$GLOBALS['coreExt']['Config']->app->database->dbname.'/';
+$key = $GLOBALS['coreExt']['Config']->app->aws->awsacesskey;
+$secretKey = $GLOBALS['coreExt']['Config']->app->aws->awssecretkey;
+
 if (!class_exists('S3'))
 	require_once 'S3.php';
-/*
-//AWS access info
-if (!defined('awsAccessKey')) define('awsAccessKey', '***REMOVED***');
-if (!defined('awsSecretKey')) define('awsSecretKey', '***REMOVED***');
-*/
-	
+
 //instantiate the class
-$s3 = new S3('***REMOVED***', '***REMOVED***');
+$s3 = new S3($key, $secretKey);
 
 $s3->putBucket($bucket, S3::ACL_PUBLIC_READ);
 ?>
