855fc32f46cca64dbfe639449a07d06b1991fc96
Lucas Schmoeller da Silva <llucas_gs@hotmail.com>
2013-08-01 12:06:53 -0300

    #287 Corrigindo data salva no banco

--- ieducar/intranet/educar_matricula_ocorrencia_disciplinar_cad.php
+++ ieducar/intranet/educar_matricula_ocorrencia_disciplinar_cad.php
@@ -28,6 +28,7 @@ require_once ("include/clsBase.inc.php");
 require_once ("include/clsCadastro.inc.php");
 require_once ("include/clsBanco.inc.php");
 require_once( "include/pmieducar/geral.inc.php" );
+require_once("Portabilis/Date/Utils.php");
 
 class clsIndexBase extends clsBase
 {
@@ -295,7 +296,7 @@ class indice extends clsCadastro
 		$obj_permissoes->permissao_excluir( 578, $this->pessoa_logada, 7,  "educar_matricula_ocorrencia_disciplinar_lst.php" );
 
 
-		$obj = new clsPmieducarMatriculaOcorrenciaDisciplinar($this->ref_cod_matricula, $this->ref_cod_tipo_ocorrencia_disciplinar, $this->sequencial, $this->pessoa_logada, $this->pessoa_logada, $this->observacao, $this->data_cadastro, $this->data_exclusao, 0);
+		$obj = new clsPmieducarMatriculaOcorrenciaDisciplinar($this->ref_cod_matricula, $this->ref_cod_tipo_ocorrencia_disciplinar, $this->sequencial, $this->pessoa_logada, $this->pessoa_logada, $this->observacao, Portabilis_Date_Utils::brToPgSQL($this->data_cadastro), $this->data_exclusao, 0);
 		$excluiu = $obj->excluir();
 		if( $excluiu )
 		{
