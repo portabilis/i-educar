7afae8e070db7344fd6f477de11ea2cfc0dc5e89
Lucas Schmoeller da Silva <llucas_gs@hotmail.com>
2013-08-12 16:21:51 -0300

    #389 Delta para alteração do nome do menu, que causava bug

---
+++ ieducar/misc/database/deltas/portabilis/58_altera_nome_menu_corrige_bug_pessoa.sql
@@ -0,0 +1,20 @@
+  -- //
+
+  --
+  -- Altera nome do menu 'Pessoa F/J' para corrigir bug
+  --
+  -- @author   Lucas Schmoeller da Silva <lucas@portabilis.com.br>
+  -- @license  @@license@@
+  -- @version  $Id$
+
+	UPDATE portal.menu_menu SET nm_menu 'Pessoa FJ' WHERE cod_menu_menu = 7;
+
+  -- //@UNDO
+
+	UPDATE portal.menu_menu SET nm_menu 'Pessoa F/J' WHERE cod_menu_menu = 7;
+
+
+  -- //
+
+
+
