61c06c01529bf311fab6d1aa4279b777e8aadb61
Lucas Schmoeller da Silva <llucas_gs@hotmail.com>
2013-08-22 15:10:44 -0300

    #448 Adicionando menu para relatório ficha individual

--- ieducar/misc/database/deltas/portabilis/private/50_menu_relatorios_cbal.sql
+++ ieducar/misc/database/deltas/portabilis/private/50_menu_relatorios_cbal.sql
@@ -11,17 +11,20 @@
   insert into portal.menu_submenu values(999801,55,2,'Apresentação histórico','module/Reports/HistoricoInfantil',NULL,3);
   insert into pmicontrolesis.menu values(999801,999801,999800,'Apresentação histórico',1,'module/Reports/HistoricoInfantil','_self',1,15,192);
 
-  insert into portal.menu_submenu values(999802,55,2,'Parecer final','module/Reports/ParecerFinalCbal',NULL,3);
-  insert into pmicontrolesis.menu values(999802,999802,999800,'Parecer final',2,'module/Reports/ParecerFinalCbal','_self',1,15,192);
+  insert into portal.menu_submenu values(999802,55,2,'Ficha individual','module/Reports/FichaIndividualCbal',NULL,3);
+  insert into pmicontrolesis.menu values(999802,999802,999800,'Ficha individual',2,'module/Reports/FichaIndividualCbal','_self',1,15,192);
 
-  insert into portal.menu_submenu values(999803,55,2,'Parecer histórico','module/Reports/HistoricoInfantilParecer',NULL,3);
-  insert into pmicontrolesis.menu values(999803,999803,999800,'Parecer histórico',3,'module/Reports/HistoricoInfantilParecer','_self',1,15,192);
+  insert into portal.menu_submenu values(999803,55,2,'Parecer final','module/Reports/ParecerFinalCbal',NULL,3);
+  insert into pmicontrolesis.menu values(999803,999803,999800,'Parecer final',3,'module/Reports/ParecerFinalCbal','_self',1,15,192);
+
+  insert into portal.menu_submenu values(999804,55,2,'Parecer histórico','module/Reports/HistoricoInfantilParecer',NULL,3);
+  insert into pmicontrolesis.menu values(999804,999804,999800,'Parecer histórico',4,'module/Reports/HistoricoInfantilParecer','_self',1,15,192);
 
 
 	-- //@UNDO
   
-  delete from pmicontrolesis.menu where cod_menu in (999801,999802,999803);
-  delete from portal.menu_submenu where cod_menu_submenu in (999801, 999802, 999803);
+  delete from pmicontrolesis.menu where cod_menu in (999801,999802,999803,999804);
+  delete from portal.menu_submenu where cod_menu_submenu in (999801, 999802, 999803,999804);
   delete from pmicontrolesis.menu where cod_menu = 999800;
   
 	-- //
