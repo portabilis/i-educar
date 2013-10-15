59e84b15f84dc522a2b6a7eeb0f4659c709ce0c1
Lucas Schmoeller da Silva <llucas_gs@hotmail.com>
2013-10-15 16:03:11 -0300

    #541 Alterando Trigger  + Function

--- ieducar/misc/database/deltas/portabilis/67_cria_colunas_data_processos_matricula.sql
+++ ieducar/misc/database/deltas/portabilis/67_cria_colunas_data_processos_matricula.sql
@@ -9,62 +9,33 @@
 
   ALTER TABLE pmieducar.matricula ADD COLUMN data_matricula timestamp without time zone;
   ALTER TABLE pmieducar.matricula ADD COLUMN data_cancel timestamp without time zone;
-
-  CREATE OR REPLACE FUNCTION pmieducar.fcn_aft_update()
+  
+  CREATE OR REPLACE FUNCTION pmieducar.matricula_fcn_after_update()
     RETURNS "trigger" AS
   $BODY$
-  DECLARE
-    nm_tabela   varchar(255);
-    alteracoes    text;
-    data_cadastro   TIMESTAMP;
-    v_insercao    int2;  
-    
-    BEGIN
-      IF (new.aprovado = 3 AND new.data_cancel <> NULL) THEN
-        UPDATE pmieducar.aluno SET data_cancel = NULL WHERE cod_aluno = new.cod_aluno;
-      END IF;
-      v_insercao    := 0;
-      nm_tabela   := TG_RELNAME;
-      alteracoes    := NEW;
-      data_cadastro   := CURRENT_TIMESTAMP;
-      IF TG_OP = 'INSERT' THEN
-        v_insercao := 1;
-      END IF;
-      insert into pmieducar.historico_educar (tabela, alteracao, data, insercao) values (nm_tabela, alteracoes, data_cadastro, v_insercao);
+  BEGIN     
+    IF (new.aprovado = 3 AND new.data_cancel <> NULL) THEN
+      UPDATE pmieducar.aluno SET data_cancel = NULL WHERE cod_aluno = new.cod_aluno;
+    END IF;
     RETURN NEW;
   END; $BODY$
-    LANGUAGE plpgsql VOLATILE;
-  ALTER FUNCTION pmieducar.fcn_aft_update()
-    OWNER TO ieducar;
+  LANGUAGE plpgsql VOLATILE;
 
-  
+  CREATE TRIGGER "pmieducar.matricula_trg_after_update"
+  AFTER UPDATE
+  ON pmieducar.matricula
+  FOR EACH ROW
+  EXECUTE PROCEDURE pmieducar.matricula_fcn_after_update();
+
+ 
   -- //@UNDO
 
   ALTER TABLE pmieducar.matricula DROP COLUMN data_matricula;
   ALTER TABLE pmieducar.matricula DROP COLUMN data_cancel;
 
-  CREATE OR REPLACE FUNCTION pmieducar.fcn_aft_update()
-    RETURNS "trigger" AS
-  $BODY$
-  DECLARE
-    nm_tabela   varchar(255);
-    alteracoes    text;
-    data_cadastro   TIMESTAMP;
-    v_insercao    int2;      
-    BEGIN    
+  
+  DROP TRIGGER "pmieducar.matricula_trg_after_update" ON pmieducar.matricula;
 
-      v_insercao    := 0;
-      nm_tabela   := TG_RELNAME;
-      alteracoes    := NEW;
-      data_cadastro   := CURRENT_TIMESTAMP;
-      IF TG_OP = 'INSERT' THEN
-        v_insercao := 1;
-      END IF;
-      insert into pmieducar.historico_educar (tabela, alteracao, data, insercao) values (nm_tabela, alteracoes, data_cadastro, v_insercao);
-    RETURN NEW;
-  END; $BODY$
-    LANGUAGE plpgsql VOLATILE
-  ALTER FUNCTION pmieducar.fcn_aft_update()
-    OWNER TO ieducar;  
+  DROP FUNCTION Pmieducar.matricula_fcn_after_update();
 
   -- //
