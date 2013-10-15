d8a2b351f067e33a27466a59cf297e4440d9b079
Lucas Schmoeller da Silva <llucas_gs@hotmail.com>
2013-10-15 12:11:02 -0300

    #541 Alterando function de trigger para deletar a data_cancel quando o aluno voltar a estar em andamento

--- ieducar/misc/database/deltas/portabilis/67_cria_colunas_data_processos_matricula.sql
+++ ieducar/misc/database/deltas/portabilis/67_cria_colunas_data_processos_matricula.sql
@@ -9,10 +9,62 @@
 
   ALTER TABLE pmieducar.matricula ADD COLUMN data_matricula timestamp without time zone;
   ALTER TABLE pmieducar.matricula ADD COLUMN data_cancel timestamp without time zone;
+
+  CREATE OR REPLACE FUNCTION pmieducar.fcn_aft_update()
+    RETURNS "trigger" AS
+  $BODY$
+  DECLARE
+    nm_tabela   varchar(255);
+    alteracoes    text;
+    data_cadastro   TIMESTAMP;
+    v_insercao    int2;  
+    
+    BEGIN
+      IF (new.aprovado = 3 AND new.data_cancel <> NULL) THEN
+        UPDATE pmieducar.aluno SET data_cancel = NULL WHERE cod_aluno = new.cod_aluno;
+      END IF;
+      v_insercao    := 0;
+      nm_tabela   := TG_RELNAME;
+      alteracoes    := NEW;
+      data_cadastro   := CURRENT_TIMESTAMP;
+      IF TG_OP = 'INSERT' THEN
+        v_insercao := 1;
+      END IF;
+      insert into pmieducar.historico_educar (tabela, alteracao, data, insercao) values (nm_tabela, alteracoes, data_cadastro, v_insercao);
+    RETURN NEW;
+  END; $BODY$
+    LANGUAGE plpgsql VOLATILE;
+  ALTER FUNCTION pmieducar.fcn_aft_update()
+    OWNER TO ieducar;
+
   
   -- //@UNDO
 
   ALTER TABLE pmieducar.matricula DROP COLUMN data_matricula;
   ALTER TABLE pmieducar.matricula DROP COLUMN data_cancel;
+
+  CREATE OR REPLACE FUNCTION pmieducar.fcn_aft_update()
+    RETURNS "trigger" AS
+  $BODY$
+  DECLARE
+    nm_tabela   varchar(255);
+    alteracoes    text;
+    data_cadastro   TIMESTAMP;
+    v_insercao    int2;      
+    BEGIN    
+
+      v_insercao    := 0;
+      nm_tabela   := TG_RELNAME;
+      alteracoes    := NEW;
+      data_cadastro   := CURRENT_TIMESTAMP;
+      IF TG_OP = 'INSERT' THEN
+        v_insercao := 1;
+      END IF;
+      insert into pmieducar.historico_educar (tabela, alteracao, data, insercao) values (nm_tabela, alteracoes, data_cadastro, v_insercao);
+    RETURN NEW;
+  END; $BODY$
+    LANGUAGE plpgsql VOLATILE
+  ALTER FUNCTION pmieducar.fcn_aft_update()
+    OWNER TO ieducar;  
 
   -- //
