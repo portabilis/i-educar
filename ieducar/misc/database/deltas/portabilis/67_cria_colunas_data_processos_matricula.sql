0689ce035fcec30f98afd1aebee9316156aecf0f
Lucas Schmoeller da Silva <llucas_gs@hotmail.com>
2013-10-16 11:38:06 -0300

    #541 Corrigindo trigger

--- ieducar/misc/database/deltas/portabilis/67_cria_colunas_data_processos_matricula.sql
+++ ieducar/misc/database/deltas/portabilis/67_cria_colunas_data_processos_matricula.sql
@@ -9,33 +9,34 @@
 
   ALTER TABLE pmieducar.matricula ADD COLUMN data_matricula timestamp without time zone;
   ALTER TABLE pmieducar.matricula ADD COLUMN data_cancel timestamp without time zone;
-  
-  CREATE OR REPLACE FUNCTION pmieducar.matricula_fcn_after_update()
-    RETURNS "trigger" AS
-  $BODY$
-  BEGIN     
-    IF (new.aprovado = 3 AND new.data_cancel <> NULL) THEN
-      UPDATE pmieducar.aluno SET data_cancel = NULL WHERE cod_aluno = new.cod_aluno;
-    END IF;
-    RETURN NEW;
-  END; $BODY$
-  LANGUAGE plpgsql VOLATILE;
-
-  CREATE TRIGGER "pmieducar.matricula_trg_after_update"
-  AFTER UPDATE
-  ON pmieducar.matricula
-  FOR EACH ROW
-  EXECUTE PROCEDURE pmieducar.matricula_fcn_after_update();
-
  
+  CREATE OR REPLACE FUNCTION retira_data_cancel_matricula_fun()
+  RETURNS trigger AS
+  $func$
+  BEGIN
+
+  UPDATE pmieducar.matricula
+  SET    data_cancel = NULL
+  WHERE  cod_matricula = new.cod_matricula
+  AND    aprovado = 3
+  AND    data_cancel IS DISTINCT FROM NULL;
+
+  RETURN NULL;
+  END
+  $func$  LANGUAGE plpgsql;
+
+  CREATE TRIGGER retira_data_cancel_matricula_trg
+  AFTER UPDATE ON pmieducar.matricula
+  FOR EACH ROW
+  EXECUTE PROCEDURE retira_data_cancel_matricula_fun();
+
   -- //@UNDO
 
   ALTER TABLE pmieducar.matricula DROP COLUMN data_matricula;
   ALTER TABLE pmieducar.matricula DROP COLUMN data_cancel;
 
-  
-  DROP TRIGGER "pmieducar.matricula_trg_after_update" ON pmieducar.matricula;
+  DROP TRIGGER retira_data_cancel_matricula_trg ON pmieducar.matricula;
 
-  DROP FUNCTION Pmieducar.matricula_fcn_after_update();
+  DROP FUNCTION public.retira_data_cancel_matricula_fun();
 
   -- //
