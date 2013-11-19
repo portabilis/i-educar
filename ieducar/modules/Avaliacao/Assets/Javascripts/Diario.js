4452a9140c8621763e04f3617a77abee27856246
Lucas Schmoeller da Silva <llucas_gs@hotmail.com>
2013-11-19 14:35:32 -0200

    #591 Criando parâmetro no ini para alterar nomenclatura do campo 'Exame Final' no Diário e no Boletim Bimestral

--- ieducar/modules/Avaliacao/Assets/Javascripts/Diario.js
+++ ieducar/modules/Avaliacao/Assets/Javascripts/Diario.js
@@ -12,6 +12,8 @@ var DELETE_LABEL = '';
 
 var SEARCH_ORIENTATION = '';
 
+var nomenclatura_exame = '';
+
 // funcoes usados pelo modulo Frontend/Process.js
 var onClickSelectAllEvent = false;
 var onClickActionEvent    = false;
@@ -439,7 +441,6 @@ function deleteParecer($parecerFieldElement) {
 function handleChange(dataResponse) {
   var targetId = dataResponse.resource + '-matricula-' + dataResponse.matricula_id +
                  '-cc-' + dataResponse.componente_curricular_id;
-
   handleMessages(dataResponse.msgs, targetId);
   updateResourceRow(dataResponse);
 }
@@ -496,6 +497,8 @@ function setTableSearchDetails($tableSearchDetails, dataDetails) {
 
   $linha.appendTo($tableSearchDetails);
   $tableSearchDetails.show();
+
+  nomenclatura_exame = dataDetails.nomenclatura_exame;
 
   //dataDetails.opcoes_notas = safeSortArray(dataDetails.opcoes_notas);
   $tableSearchDetails.data('details', dataDetails);
@@ -715,8 +718,8 @@ function updateComponenteCurricularHeaders($targetElement, $tagElement) {
     $tagElement.clone().addClass('center').html('Nota').appendTo($targetElement);
 
     if ($tableSearchDetails.data('details').quantidade_etapas == $j('#etapa').val()){
-      $tagElement.clone().addClass('center').html('Nota exame').appendTo($targetElement);
-      $tagElement.clone().addClass('center').html(safeUtf8Decode('Nota necessária no exame')).appendTo($targetElement);
+      $tagElement.clone().addClass('center').html('Nota '+nomenclatura_exame).appendTo($targetElement);
+      $tagElement.clone().addClass('center').html(safeUtf8Decode('Nota necessária no '+nomenclatura_exame)).appendTo($targetElement);
     }
   }
 
