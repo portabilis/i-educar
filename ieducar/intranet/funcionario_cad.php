18d797f168eee65a2e026a66b03e65881a364ed8
Lucas Schmoeller da Silva <llucas_gs@hotmail.com>
2013-12-09 17:24:22 -0200

    #241 Corrigindo bug que alterava o menu sempre para menu Alterado ao editar funcionário

--- ieducar/intranet/funcionario_cad.php
+++ ieducar/intranet/funcionario_cad.php
@@ -504,7 +504,7 @@ class indice extends clsCadastro
 			$this->_senha = md5($this->_senha);
 		}
 
-		$obj_funcionario = new clsPortalFuncionario($this->ref_pessoa, $this->matricula, $this->_senha, $this->ativo, null, $this->ramal, null, null, null, null, null, null, null, null, $this->ref_cod_funcionario_vinculo, $this->tempo_expira_senha, $this->tempo_expira_conta, "NOW()", "NOW()", $this->pessoa_logada, empty($this->proibido) ? 0 : 1, $this->ref_cod_setor_new, null, empty($this->matricula_permanente) ? 0 : 1, 1, $this->email);
+		$obj_funcionario = new clsPortalFuncionario($this->ref_pessoa, $this->matricula, $this->_senha, $this->ativo, null, $this->ramal, null, null, null, null, null, null, null, null, $this->ref_cod_funcionario_vinculo, $this->tempo_expira_senha, $this->tempo_expira_conta, "NOW()", "NOW()", $this->pessoa_logada, empty($this->proibido) ? 0 : 1, $this->ref_cod_setor_new, null, empty($this->matricula_permanente) ? 0 : 1, null, $this->email);
 		if( $obj_funcionario->edita() )
 		{
 			$obj_menu_funcionario = new clsPortalMenuFuncionario($this->ref_pessoa);
