3f5a175a8599c48a535d1ecca8ab60790556c656
Lucas Schmoeller da Silva <llucas_gs@hotmail.com>
2013-12-27 16:32:17 -0200

    #36 Alterando função de conversão de data no cadastro de falta/atraso para servidores

--- ieducar/intranet/educar_falta_atraso_cad.php
+++ ieducar/intranet/educar_falta_atraso_cad.php
@@ -32,6 +32,7 @@ require_once 'include/clsBase.inc.php';
 require_once 'include/clsCadastro.inc.php';
 require_once 'include/clsBanco.inc.php';
 require_once 'include/pmieducar/geral.inc.php';
+require_once 'Portabilis/Date/Utils.php';
 
 /**
  * clsIndexBase class.
@@ -102,7 +103,7 @@ class indice extends clsCadastro
       $registro  = $obj->detalhe();
 
       if ($registro) {
-        // passa todos os valores obtidos no registro para atributos do objeto
+        // passa to$this->data_falta_atraso = Portabilis_Date_Utils::brToPgSQL($this->data_falta_atraso);dos os valores obtidos no registro para atributos do objeto
         foreach ($registro as $campo => $val) {
           $this->$campo = $val;
         }
@@ -170,6 +171,8 @@ class indice extends clsCadastro
     $this->pessoa_logada = $_SESSION['id_pessoa'];
     @session_write_close();
 
+    $this->data_falta_atraso = Portabilis_Date_Utils::brToPgSQL($this->data_falta_atraso);
+
     $obj_permissoes = new clsPermissoes();
     $obj_permissoes->permissao_cadastra(635, $this->pessoa_logada, 7,
       sprintf('educar_falta_atraso_lst.php?ref_cod_servidor=%d&ref_cod_instituicao=%d',
@@ -183,7 +186,7 @@ class indice extends clsCadastro
     }
     elseif ($this->tipo == 2) {
       $db = new clsBanco();
-      $dia_semana = $db->CampoUnico(sprintf('(SELECT EXTRACT (DOW FROM date \'%s\') + 1 )', dataToBanco($this->data_falta_atraso)));
+      $dia_semana = $db->CampoUnico(sprintf('(SELECT EXTRACT (DOW FROM date \'%s\') + 1 )', $this->data_falta_atraso));
 
       $obj_ser = new clsPmieducarServidor();
       $horas   = $obj_ser->qtdhoras( $this->ref_cod_servidor, $this->ref_cod_escola, $this->ref_cod_instituicao, $dia_semana );
