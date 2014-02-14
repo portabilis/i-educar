1c7ffc4ffd29f0d5329a47819eabcd70b69a1ae7
Lucas Schmoeller da Silva <llucas_gs@hotmail.com>
2014-02-14 17:56:09 -0200

    Corrigindo delta menu alunos por bairro

--- ieducar/misc/database/deltas/portabilis/private/67_cria_menu_relatório_alunos_por_bairro_log.sql
+++ ieducar/misc/database/deltas/portabilis/private/67_cria_menu_relatório_alunos_por_bairro_log.sql
@@ -7,12 +7,12 @@
   -- @version  $Id$  
   
   
-  insert into portal.menu_submenu values(999229,55,2,'Alunos por Bairro/Logradouro','module/Reports/AlunoPorBairro',NULL,3);
-  insert into pmicontrolesis.menu values(999229,999229,999300,'Alunos por Bairro/Logradouro',2,'module/Reports/AlunoPorBairro','_self',1,15,192);
+  insert into portal.menu_submenu values(999230,55,2,'Alunos por Bairro','module/Reports/AlunoPorBairro',NULL,3);
+  insert into pmicontrolesis.menu values(999230,999230,999300,'Alunos por Bairro',2,'module/Reports/AlunoPorBairro','_self',1,15,192);
 
   -- //@UNDO
   
-  delete from pmicontrolesis.menu where cod_menu = 999229;
-  delete from portal.menu_submenu where cod_menu_submenu = 999229;
+  delete from pmicontrolesis.menu where cod_menu = 999230;
+  delete from portal.menu_submenu where cod_menu_submenu = 999230;
   
   -- //
