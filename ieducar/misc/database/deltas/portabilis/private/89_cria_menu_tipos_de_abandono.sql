fdc3b74034e10dfc91a055dd12955a4ab458b84f
gabrielms <gabrielmatosdesouza@gmail.com>
2014-03-28 11:23:57 -0300

    *Corrigido delta da criação de menus; portabilis/i-educar#681

--- ieducar/misc/database/deltas/portabilis/private/89_cria_menu_tipos_de_abandono.sql
+++ ieducar/misc/database/deltas/portabilis/private/89_cria_menu_tipos_de_abandono.sql
@@ -7,19 +7,20 @@
   -- @license  @@license@@
   -- @version  $Id$
 
-  INSERT INTO pmicontrolesis.menu VALUES(21245,
-                                         575, 
-                                         21122, 
-                                         'Tipo de Abandono', 
-                                         15,
-                                         'educar_abandono_tipo_lst.php',
-                                         '_self',
-                                         1,
-                                         15,
-                                         1)
+INSERT INTO portal.menu_submenu VALUES(950, 55, 2, 'Abandono Tipo', 'educar_abandono_tipo_lst.php', null, 3);
 
+INSERT INTO pmicontrolesis.menu VALUES(21245,
+                                       950, 
+                                       21122, 
+                                       'Tipo de Abandono',
+                                       15,
+                                       'educar_abandono_tipo_lst.php',
+                                       '_self',
+                                       1,
+                                       15,
+                                       1);
   -- //@UNDO
-
-  DELETE FROM pmicontrolesis.menu WHERE cod_menu = 21245
+  DELETE FROM portal.menu_submenu WHERE cod_menu = 950;
+  DELETE FROM pmicontrolesis.menu WHERE cod_menu = 21245;
 
   -- //
