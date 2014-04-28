1122fe5a53e952cdc7384a9e831d1a27c0f739e9
gabrielms <gabrielmatosdesouza@gmail.com>
2014-04-28 11:16:46 -0300

    Retornado à forma de busca de Áreas de conhecimento anterior devido à esta nova ter ficado muito lenta; portabilis/i-educar#731

--- ieducar/modules/DynamicInput/Views/AreaConhecimentoController.php
+++ ieducar/modules/DynamicInput/Views/AreaConhecimentoController.php
@@ -22,26 +22,15 @@ class AreaConhecimentoController extends ApiCoreController{
   protected function getAreasConhecimento() {
 
      $instituicaoId = $this->getRequest()->instituicao_id;
-     $turmaId       = $this->getRequest()->turma_id;
-     $sql    = 'SELECT DISTINCT
-                       ac.id as id,
-                       to_ascii(lower(ac.nome)) as nome
-                  FROM pmieducar.turma AS t, 
-                       pmieducar.escola_serie_disciplina AS esd, 
-                       modules.componente_curricular AS cc, 
-                       modules.area_conhecimento AS ac
-                 WHERE t.cod_turma = $2
-                   AND esd.ref_ref_cod_escola = t.ref_ref_cod_escola 
-                   AND esd.ref_ref_cod_serie = t.ref_ref_cod_serie 
-                   AND esd.ref_cod_disciplina = cc.id
-                   AND t.ativo = 1 
-                   AND esd.ativo = 1 
-                   AND cc.area_conhecimento_id = ac.id
-                   AND ac.instituicao_id = $1
-                 ORDER BY to_ascii(lower(ac.nome)), ac.id';
+     //$turmaId       = $this->getRequest()->turma_id;
+     $sql    = 'SELECT ac.id AS id,
+                       ac.nome AS nome 
+                  FROM modules.area_conhecimento ac
+                 WHERE instituicao_id = $1
+              ORDER BY to_ascii(lower(nome)) ASC';
 
     $areasConhecimento = array();
-    $paramsSql = array($instituicaoId,$turmaId);
+    $paramsSql = array('params' => $instituicaoId);
     $areasConhecimento = $this->fetchPreparedQuery($sql, $paramsSql);
     $options = array();
 
