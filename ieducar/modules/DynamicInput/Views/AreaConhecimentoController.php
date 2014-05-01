b620f8fd7718f73ec5cb5e54559be90ede3d4557
gabrielms <gabrielmatosdesouza@gmail.com>
2014-05-01 16:21:27 -0300

    Adicionado possibilidade de seleção de mais de uma área de conhecimento; Organizado disposição dos campos na impressão do boletim; portabilis/i-educar#731

--- ieducar/modules/DynamicInput/Views/AreaConhecimentoController.php
+++ ieducar/modules/DynamicInput/Views/AreaConhecimentoController.php
@@ -20,16 +20,15 @@ require_once 'App/Model/IedFinder.php';
 class AreaConhecimentoController extends ApiCoreController{
 
   protected function getAreasConhecimento() {
-
+     
      $instituicaoId = $this->getRequest()->instituicao_id;
-     //$turmaId       = $this->getRequest()->turma_id;
      $sql    = 'SELECT ac.id AS id,
                        ac.nome AS nome 
                   FROM modules.area_conhecimento ac
                  WHERE instituicao_id = $1
               ORDER BY to_ascii(lower(nome)) ASC';
 
-    $areasConhecimento = array();
+
     $paramsSql = array('params' => $instituicaoId);
     $areasConhecimento = $this->fetchPreparedQuery($sql, $paramsSql);
     $options = array();
@@ -37,13 +36,24 @@ class AreaConhecimentoController extends ApiCoreController{
     foreach ($areasConhecimento as $areaConhecimento){
       $options['__' . $areaConhecimento['id']] = $this->toUtf8($areaConhecimento['nome']);
     }
+
     return array('options' => $options);
 
   }
 
+  protected function searchOptions() {
+     return array('namespace' => 'modules', 'table' => 'area_conhecimento', 'labelAttr' => 'nome', 'idAttr' => 'id');
+   }
+
+  // protected function search(){
+  //   return $this->getAreasConhecimento();
+  // }
+
   public function Gerar() {
     if ($this->isRequestFor('get', 'area_conhecimento'))
       $this->appendResponse($this->getAreasConhecimento());
+    elseif($this->isRequestFor('get','area_conhecimento-search')) 
+      $this->appendResponse($this->search());
     else
       $this->notImplementedOperationError();
   }
