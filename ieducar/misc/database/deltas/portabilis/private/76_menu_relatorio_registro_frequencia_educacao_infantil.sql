3e5c7d06e39950843a0ad1297591cb1bfedff896
gabrielms <gabrielmatosdesouza@gmail.com>
2014-07-22 15:39:10 -0300

    Alterado arquivo não mandado por engano; portabilis/i-educar#832

--- ieducar/misc/database/deltas/portabilis/private/76_menu_relatorio_registro_frequencia_educacao_infantil.sql
+++ ieducar/misc/database/deltas/portabilis/private/76_menu_relatorio_registro_frequencia_educacao_infantil.sql
@@ -6,9 +6,11 @@
   -- @license  @@license@@
   -- @version  $Id$
 
+    ALTER TABLE portal.menu_submenu ALTER COLUMN nm_submenu type character varying(60);
+  ALTER TABLE pmicontrolesis.menu ALTER COLUMN tt_menu type character varying(60);
 
   insert into portal.menu_submenu values(999236, 55, 2, 'Registro de Frequência - Anos Iniciais Educ Infantil', 'module/Reports/RegistroFrequenciaAnosIniciaisEducInfantil', NULL, 3);
-  insert into pmicontrolesis.menu values(999236, 999236, 999500, 'Registro de Frequência - Anos Iniciais Educ. Infantil', 5, 'module/Reports/RegistroFrequenciaAnosIniciaisEducInfantil', '_self', 1, 15, 192)
+  insert into pmicontrolesis.menu values(999236, 999236, 999500, 'Registro de Frequência - Anos Iniciais Educ. Infantil', 5, 'module/Reports/RegistroFrequenciaAnosIniciaisEducInfantil', '_self', 1, 15, 192);
 
 
   -- //@UNDO
