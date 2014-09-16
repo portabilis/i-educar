2cf4e8817cda0d10a3fbc2ec57239c6e03e05c93
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2014-09-16 17:55:50 -0300

    WIP Aprimorar gestão de usuários e permissões de acesso
    Mesclado cadastro de funcionário com cadastro de usuário;
    Removido módulo DRH;
    Removido campos de permissões do cadastro de funcionário/usuário;
    "Criado" módulo administrativo na lateral esquerda que contém agora os cadastros de usuários que ficavam dentro do módulo i-Educar - Escola;
    
    portabilis/i-educar#351

--- ieducar/misc/database/deltas/portabilis/126_remove_drh_funcionarios.sql
+++ ieducar/misc/database/deltas/portabilis/126_remove_drh_funcionarios.sql
@@ -1,5 +1,5 @@
   -- 
-  -- Remove menu DRH funcionários (unificado com usuários)
+  -- Remove menu DRH funcionários (unificado com usuários) e move usuários para menu lateral
   -- @author   Lucas Schmoeller da Silva <lucas@portabilis.com.br>
   -- @license  @@license@@
   -- @version  $Id$
@@ -7,6 +7,8 @@
 	DELETE FROM portal.menu_funcionario WHERE ref_cod_menu_submenu IN (190, 295, 297, 293, 36);
 	DELETE FROM pmieducar.menu_tipo_usuario WHERe ref_cod_menu_submenu IN (190, 295, 297, 293, 36);
 	DELETE FROM portal.menu_submenu WHERe cod_menu_submenu IN (190, 295, 297, 293, 36);
-	DELETE FROM portal.menu_menu WHERE cod_menu_menu = 25;
+	UPDATE portal.menu_menu SET nm_menu = 'Administrativo' WHERE cod_menu_menu = 25;
+  DELETE FROM pmicontrolesis.menu WHERE ref_cod_menu_submenu IN (554, 555)
+  UPDATE portal.menu_submenu SET ref_cod_menu_menu = 25 WHERE cod_menu_submenu IN (554,555);
 
   -- //
