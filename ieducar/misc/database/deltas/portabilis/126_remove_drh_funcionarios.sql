9cd5c80f901a79401589e1cbc004d136e04ca5c5
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2014-09-16 18:14:36 -0300

    Aprimorar gestão de usuários e permissões de acesso
    Corrigido delta
    
    portabilis/i-educar#351

--- ieducar/misc/database/deltas/portabilis/126_remove_drh_funcionarios.sql
+++ ieducar/misc/database/deltas/portabilis/126_remove_drh_funcionarios.sql
@@ -8,7 +8,7 @@
 	DELETE FROM pmieducar.menu_tipo_usuario WHERe ref_cod_menu_submenu IN (190, 295, 297, 293, 36);
 	DELETE FROM portal.menu_submenu WHERe cod_menu_submenu IN (190, 295, 297, 293, 36);
 	UPDATE portal.menu_menu SET nm_menu = 'Administrativo' WHERE cod_menu_menu = 25;
-  DELETE FROM pmicontrolesis.menu WHERE ref_cod_menu_submenu IN (554, 555)
+  DELETE FROM pmicontrolesis.menu WHERE ref_cod_menu_submenu IN (554, 555);
   UPDATE portal.menu_submenu SET ref_cod_menu_menu = 25 WHERE cod_menu_submenu IN (554,555);
 
   -- //
