117253cac31155f7df79a0cb515ffafce0a79906
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2014-09-22 17:25:34 -0300

    Adicionado permissões de transporte ao admin
    portabilis/i-educar#1046

---
+++ ieducar/misc/database/deltas/portabilis/127_insere_permissoes_modulo_transporte.sql
@@ -0,0 +1,14 @@
+  -- 
+  -- Inserir permissões para
+  -- @author   Lucas Schmoeller da Silva <lucas@portabilis.com.br>
+  -- @license  @@license@@
+  -- @version  $Id$
+
+  INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) VALUES (1, 1, 1, 21235);
+  INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) VALUES (1, 1, 1, 21236);
+  INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) VALUES (1, 1, 1, 21237);
+  INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) VALUES (1, 1, 1, 21238);
+	INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) VALUES (1, 1, 1, 21239);
+  INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) VALUES (1, 1, 1, 21240);
+
+  -- //
