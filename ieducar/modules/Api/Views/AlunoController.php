123aa4854d87296182e6587f8233e09c1816361f
Lucas Schmoeller da Silva <lucas@portabilis.com.br>
2014-10-13 12:04:26 -0300

    Corrigido retorno método matriculas na Api do Aluno
    portabilis/i-educar#1156

--- ieducar/modules/Api/Views/AlunoController.php
+++ ieducar/modules/Api/Views/AlunoController.php
@@ -1067,7 +1067,7 @@ protected function createOrUpdateUniforme($id) {
         $matriculas[$index]['transferencia_em_aberto'] = $this->possuiTransferenciaEmAberto($matricula['id']);
       }
       
-      $attrs = array('id', 'instituicao_id', 'escola_id', 'curso_id', 'serie_id', 'aluno_id', 'aluno_nome', 'situacao', 'ano', 'nome_aluno', 'turma_id', 
+      $attrs = array('id', 'instituicao_id', 'escola_id', 'curso_id', 'serie_id', 'aluno_id', 'aluno_nome', 'situacao', 'ano', 'turma_id', 
           'turma_nome', 'escola_nome', 'escola_nome', 'curso_nome', 'serie_nome', 'ultima_enturmacao', 'data_entrada', 'data_entrada', 'data_saida', 'user_can_access', 'transferencia_em_aberto');
 
       $matriculas = Portabilis_Array_Utils::filterSet($matriculas, $attrs);      
