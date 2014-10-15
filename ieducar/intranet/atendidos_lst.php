bb5a1b951d5d9a308678703ec446eb977c3eebb9
Lucas Schmoeller da Silva <lucas@portabilis.com.br>
2014-10-15 10:31:39 -0300

    Corrigido permissões de visualização
    Corrigido lst e det pessoa fj
    
    portabilis/i-educar#952

--- ieducar/intranet/atendidos_lst.php
+++ ieducar/intranet/atendidos_lst.php
@@ -19,6 +19,10 @@ class indice extends clsListagem
 {
 	function Gerar()
 	{
+		@session_start();
+		$this->pessoa_logada = $_SESSION['id_pessoa'];
+		session_write_close();
+				
 		$this->titulo = "Pessoas Físicas";
 		
 
