3e7b08dda61cfa8f6a4bdfa604a325dad42e3fdc
Lucas Schmoeller da Silva <lucas@portabilis.com.br>
2014-10-16 10:43:43 -0300

    Permissões de transporte escolar
    Foi adicionado um delta com consultas que inserem todas permissões necessárias para acesso do módulo de transporte escolar;
    
    portabilis/ieducar#66

---
+++ ieducar/misc/database/deltas/portabilis/58_insere_permissoes_admin_modulo_transporte.sql
@@ -0,0 +1,14 @@
+  -- 
+  -- Insere permissões para o usuário 1 no módulo de transporte escolar
+  -- @author   Lucas Schmoeller da Silva <lucas@portabilis.com.br>
+  -- @license  @@license@@
+  -- @version  $Id$
+
+  INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) VALUES (1, 1, 1, 21235);
+  INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) VALUES (1, 1, 1, 21236);
+  INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) VALUES (1, 1, 1, 21237);
+  INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) VALUES (1, 1, 1, 21238);
+	INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) VALUES (1, 1, 1, 21239);
+  INSERT INTO portal.menu_funcionario (ref_ref_cod_pessoa_fj, cadastra, exclui, ref_cod_menu_submenu) VALUES (1, 1, 1, 21240);
+
+  -- //
