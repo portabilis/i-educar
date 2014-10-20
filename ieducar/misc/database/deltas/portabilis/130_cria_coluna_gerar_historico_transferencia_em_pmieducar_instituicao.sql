c5f41d3f50e4c653c84b08e5c0bfd3c3776bec40
Lucas Schmoeller da Silva <lucas@portabilis.com.br>
2014-10-20 14:57:42 -0200

    Atualizado autoria de delta

--- ieducar/misc/database/deltas/portabilis/130_cria_coluna_gerar_historico_transferencia_em_pmieducar_instituicao.sql
+++ ieducar/misc/database/deltas/portabilis/130_cria_coluna_gerar_historico_transferencia_em_pmieducar_instituicao.sql
@@ -1,7 +1,6 @@
   -- 
   -- Cria coluna gerar_historico_transferencia na tabela pmieducar.instituicao
   -- @author   Lucas Schmoeller da Silva <lucas@portabilis.com.br>
-  -- @author   Samuel Brognoli <Samuel@portabilis.com.br>
   -- @license  @@license@@
   -- @version  $Id$
 
