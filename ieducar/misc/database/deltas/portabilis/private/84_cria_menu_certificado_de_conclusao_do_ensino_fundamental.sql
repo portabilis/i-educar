2b2ff0cb7b8158ba85d26340dac9cf0907a90d82
gabrielms <gabrielmatosdesouza@gmail.com>
2014-11-03 10:32:50 -0200

    Corrigido nome do menu do certificado;

--- ieducar/misc/database/deltas/portabilis/private/84_cria_menu_certificado_de_conclusao_do_ensino_fundamental.sql
+++ ieducar/misc/database/deltas/portabilis/private/84_cria_menu_certificado_de_conclusao_do_ensino_fundamental.sql
@@ -5,9 +5,9 @@
 -- @version  $Id$
 
 
-insert into portal.menu_submenu values( 999808, 55, 2, 'Certificado de Conclusão do Ensino Médio', 'module/Reports/CertificadoEnsinoMedio', null, 3);
+insert into portal.menu_submenu values( 999808, 55, 2, 'Certificado de Conclusão do Ensino Fundamental', 'module/Reports/CertificadoEnsinoMedio', null, 3);
 insert into pmicontrolesis.menu values( 999807, null, 21127, 'Certificados', 3, null, '_self', 1, 15, 25);
-insert into pmicontrolesis.menu values( 999808, 999808, 999807, 'Certificado de Conclusão do Ensino Médio', 1, 'module/Reports/CertificadoEnsinoMedio', '_self', 1, 15, 122);
+insert into pmicontrolesis.menu values( 999808, 999808, 999807, 'Certificado de Conclusão do Ensino Fundamental', 1, 'module/Reports/CertificadoEnsinoMedio', '_self', 1, 15, 122);
 
 -- //@UNDO
 
