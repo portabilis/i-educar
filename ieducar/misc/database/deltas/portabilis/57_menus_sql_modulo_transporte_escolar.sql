c40bce6a52aa49109fcfee1d69d2cd747b87ff88
Lucas D'Avila <lucas@lucasdavi.la>
2014-11-12 11:00:03 -0200

    Corrigido identação para usar espaços.

--- ieducar/misc/database/deltas/portabilis/57_menus_sql_modulo_transporte_escolar.sql
+++ ieducar/misc/database/deltas/portabilis/57_menus_sql_modulo_transporte_escolar.sql
@@ -8,69 +8,69 @@
   -- @license  @@license@@
   -- @version  $Id$
 
-	INSERT INTO portal.menu_menu (cod_menu_menu, nm_menu) 
-	VALUES (69,'Transporte Escolar');
+  INSERT INTO portal.menu_menu (cod_menu_menu, nm_menu)
+  VALUES (69,'Transporte Escolar');
 
-	INSERT INTO portal.menu_submenu (cod_menu_submenu, ref_cod_menu_menu, cod_sistema, nm_submenu, arquivo, nivel) 
-	VALUES (21234, 69, 2, 'Apresentação', 'transporte_index.php', '2');
-	
-	INSERT INTO pmicontrolesis.tutormenu (cod_tutormenu,nm_tutormenu) 
-	VALUES (17,'Transporte Escolar');
-	
-	INSERT INTO pmicontrolesis.menu (cod_menu,tt_menu,ord_menu,alvo,suprime_menu,ref_cod_tutormenu,ref_cod_ico)
-	VALUES(20710,'Cadastros',1,'_self',1,17,1);
+  INSERT INTO portal.menu_submenu (cod_menu_submenu, ref_cod_menu_menu, cod_sistema, nm_submenu, arquivo, nivel)
+  VALUES (21234, 69, 2, 'Apresentação', 'transporte_index.php', '2');
 
-	INSERT INTO pmicontrolesis.menu (cod_menu,tt_menu,ord_menu,alvo,suprime_menu,ref_cod_tutormenu,ref_cod_ico) 
-	VALUES(20711,'Movimentação',2,'_self',1,17,1);
+  INSERT INTO pmicontrolesis.tutormenu (cod_tutormenu,nm_tutormenu)
+  VALUES (17,'Transporte Escolar');
 
-	INSERT INTO pmicontrolesis.menu (cod_menu,tt_menu,ord_menu,alvo,suprime_menu,ref_cod_tutormenu,ref_cod_ico) 
-	VALUES(20712,'Relatórios',3,'_self',1,17,1);
+  INSERT INTO pmicontrolesis.menu (cod_menu,tt_menu,ord_menu,alvo,suprime_menu,ref_cod_tutormenu,ref_cod_ico)
+  VALUES(20710,'Cadastros',1,'_self',1,17,1);
 
-	INSERT INTO portal.menu_submenu 
-	VALUES (21235, 69, 2, 'Empresas', 'transporte_empresa_lst.php',null,3); 
+  INSERT INTO pmicontrolesis.menu (cod_menu,tt_menu,ord_menu,alvo,suprime_menu,ref_cod_tutormenu,ref_cod_ico)
+  VALUES(20711,'Movimentação',2,'_self',1,17,1);
 
-	INSERT INTO portal.menu_submenu 
-	VALUES (21236, 69, 2, 'Motoristas', 'transporte_motorista_lst.php',null,3); 
+  INSERT INTO pmicontrolesis.menu (cod_menu,tt_menu,ord_menu,alvo,suprime_menu,ref_cod_tutormenu,ref_cod_ico)
+  VALUES(20712,'Relatórios',3,'_self',1,17,1);
 
-	INSERT INTO portal.menu_submenu 
-	VALUES (21237, 69, 2, 'Veículos', 'transporte_veiculo_lst.php',null,3); 
+  INSERT INTO portal.menu_submenu
+  VALUES (21235, 69, 2, 'Empresas', 'transporte_empresa_lst.php',null,3);
 
-	INSERT INTO portal.menu_submenu 
-	VALUES (21238, 69, 2, 'Rotas', 'transporte_rota_lst.php',null,3); 
+  INSERT INTO portal.menu_submenu
+  VALUES (21236, 69, 2, 'Motoristas', 'transporte_motorista_lst.php',null,3);
 
-	INSERT INTO portal.menu_submenu 
-	VALUES (21239, 69, 2, 'Pontos', 'transporte_ponto_lst.php',null,3); 
+  INSERT INTO portal.menu_submenu
+  VALUES (21237, 69, 2, 'Veículos', 'transporte_veiculo_lst.php',null,3);
 
-	INSERT INTO portal.menu_submenu 
-	VALUES (21240, 69, 2, 'Usuários de Transporte', 'transporte_pessoa_lst.php',null,3);
+  INSERT INTO portal.menu_submenu
+  VALUES (21238, 69, 2, 'Rotas', 'transporte_rota_lst.php',null,3);
 
-	INSERT INTO pmicontrolesis.menu 
-	VALUES(21235,21235,20710,'Empresas',1,'transporte_empresa_lst.php','_self',1,17,192);
+  INSERT INTO portal.menu_submenu
+  VALUES (21239, 69, 2, 'Pontos', 'transporte_ponto_lst.php',null,3);
 
-	INSERT INTO pmicontrolesis.menu 
-	VALUES(21236,21236,20710,'Motoristas',2,'transporte_motorista_lst.php','_self',1,17,192);
+  INSERT INTO portal.menu_submenu
+  VALUES (21240, 69, 2, 'Usuários de Transporte', 'transporte_pessoa_lst.php',null,3);
 
-	INSERT INTO pmicontrolesis.menu 
-	VALUES(21237,21237,20710,'Veículos',3,'transporte_veiculo_lst.php','_self',1,17,192);
+  INSERT INTO pmicontrolesis.menu
+  VALUES(21235,21235,20710,'Empresas',1,'transporte_empresa_lst.php','_self',1,17,192);
 
-	INSERT INTO pmicontrolesis.menu 
-	VALUES(21238,21238,20710,'Pontos',4,'transporte_ponto_lst.php','_self',1,17,192);
+  INSERT INTO pmicontrolesis.menu
+  VALUES(21236,21236,20710,'Motoristas',2,'transporte_motorista_lst.php','_self',1,17,192);
 
-	INSERT INTO pmicontrolesis.menu 
-	VALUES(21239,21239,20710,'Rotas',5,'transporte_rota_lst.php','_self',1,17,192);
+  INSERT INTO pmicontrolesis.menu
+  VALUES(21237,21237,20710,'Veículos',3,'transporte_veiculo_lst.php','_self',1,17,192);
 
-	INSERT INTO pmicontrolesis.menu 
-	VALUES(21240,21240,20711,'Usuários de Transporte',5,'transporte_pessoa_lst.php','_self',1,17,192);
+  INSERT INTO pmicontrolesis.menu
+  VALUES(21238,21238,20710,'Pontos',4,'transporte_ponto_lst.php','_self',1,17,192);
+
+  INSERT INTO pmicontrolesis.menu
+  VALUES(21239,21239,20710,'Rotas',5,'transporte_rota_lst.php','_self',1,17,192);
+
+  INSERT INTO pmicontrolesis.menu
+  VALUES(21240,21240,20711,'Usuários de Transporte',5,'transporte_pessoa_lst.php','_self',1,17,192);
 
 
   -- //@UNDO
 
-	DELETE FROM pmicontrolesis.menu where cod_menu in (21235,21236,21237,21238,21239);
-	DELETE FROM portal.menu_submenu where cod_menu_submenu in (21234,21235,21236,21237,21238,21239);
-	DELETE FROM pmicontrolesis.menu where cod_menu in (20710,20711,20712);
-	DELETE FROM pmicontrolesis.tutormenu where cod_tutormenu in (17);
-	DELETE FROM menu_submenu where ref_cod_menu_menu = 69;
-	DELETE FROM menu_menu where cod_menu_menu = 69;
+  DELETE FROM pmicontrolesis.menu where cod_menu in (21235,21236,21237,21238,21239);
+  DELETE FROM portal.menu_submenu where cod_menu_submenu in (21234,21235,21236,21237,21238,21239);
+  DELETE FROM pmicontrolesis.menu where cod_menu in (20710,20711,20712);
+  DELETE FROM pmicontrolesis.tutormenu where cod_tutormenu in (17);
+  DELETE FROM menu_submenu where ref_cod_menu_menu = 69;
+  DELETE FROM menu_menu where cod_menu_menu = 69;
 
 
   -- //
