7a2fb6cd7ab3445a79e82df653a340584727d131
Lucas D'Avila <lucas@lucasdavi.la>
2014-11-12 11:06:19 -0200

    Corrigido acentuação dos menus, usando caracteres especiais do html.
    * Em alguns casos a acentuação estava ficando errada no navegador.

--- ieducar/misc/database/deltas/portabilis/57_menus_sql_modulo_transporte_escolar.sql
+++ ieducar/misc/database/deltas/portabilis/57_menus_sql_modulo_transporte_escolar.sql
@@ -12,7 +12,7 @@
   VALUES (69,'Transporte Escolar');
 
   INSERT INTO portal.menu_submenu (cod_menu_submenu, ref_cod_menu_menu, cod_sistema, nm_submenu, arquivo, nivel)
-  VALUES (21234, 69, 2, 'Apresentação', 'transporte_index.php', '2');
+  VALUES (21234, 69, 2, 'Apresenta&ccedil;&atilde;o', 'transporte_index.php', '2');
 
   INSERT INTO pmicontrolesis.tutormenu (cod_tutormenu,nm_tutormenu)
   VALUES (17,'Transporte Escolar');
@@ -21,10 +21,10 @@
   VALUES(20710,'Cadastros',1,'_self',1,17,1);
 
   INSERT INTO pmicontrolesis.menu (cod_menu,tt_menu,ord_menu,alvo,suprime_menu,ref_cod_tutormenu,ref_cod_ico)
-  VALUES(20711,'Movimentação',2,'_self',1,17,1);
+  VALUES(20711,'Movimenta&ccedil;&atilde;o',2,'_self',1,17,1);
 
   INSERT INTO pmicontrolesis.menu (cod_menu,tt_menu,ord_menu,alvo,suprime_menu,ref_cod_tutormenu,ref_cod_ico)
-  VALUES(20712,'Relatórios',3,'_self',1,17,1);
+  VALUES(20712,'Relat&oacute;rios',3,'_self',1,17,1);
 
   INSERT INTO portal.menu_submenu
   VALUES (21235, 69, 2, 'Empresas', 'transporte_empresa_lst.php',null,3);
@@ -33,7 +33,7 @@
   VALUES (21236, 69, 2, 'Motoristas', 'transporte_motorista_lst.php',null,3);
 
   INSERT INTO portal.menu_submenu
-  VALUES (21237, 69, 2, 'Veículos', 'transporte_veiculo_lst.php',null,3);
+  VALUES (21237, 69, 2, 'Ve&iacute;culos', 'transporte_veiculo_lst.php',null,3);
 
   INSERT INTO portal.menu_submenu
   VALUES (21238, 69, 2, 'Rotas', 'transporte_rota_lst.php',null,3);
@@ -42,7 +42,7 @@
   VALUES (21239, 69, 2, 'Pontos', 'transporte_ponto_lst.php',null,3);
 
   INSERT INTO portal.menu_submenu
-  VALUES (21240, 69, 2, 'Usuários de Transporte', 'transporte_pessoa_lst.php',null,3);
+  VALUES (21240, 69, 2, 'Usu&aacute;rios de Transporte', 'transporte_pessoa_lst.php',null,3);
 
   INSERT INTO pmicontrolesis.menu
   VALUES(21235,21235,20710,'Empresas',1,'transporte_empresa_lst.php','_self',1,17,192);
@@ -51,7 +51,7 @@
   VALUES(21236,21236,20710,'Motoristas',2,'transporte_motorista_lst.php','_self',1,17,192);
 
   INSERT INTO pmicontrolesis.menu
-  VALUES(21237,21237,20710,'Veículos',3,'transporte_veiculo_lst.php','_self',1,17,192);
+  VALUES(21237,21237,20710,'Ve&iacute;culos',3,'transporte_veiculo_lst.php','_self',1,17,192);
 
   INSERT INTO pmicontrolesis.menu
   VALUES(21238,21238,20710,'Pontos',4,'transporte_ponto_lst.php','_self',1,17,192);
@@ -60,7 +60,7 @@
   VALUES(21239,21239,20710,'Rotas',5,'transporte_rota_lst.php','_self',1,17,192);
 
   INSERT INTO pmicontrolesis.menu
-  VALUES(21240,21240,20711,'Usuários de Transporte',5,'transporte_pessoa_lst.php','_self',1,17,192);
+  VALUES(21240,21240,20711,'Usu&aacute;rios de Transporte',5,'transporte_pessoa_lst.php','_self',1,17,192);
 
 
   -- //@UNDO
@@ -74,6 +74,3 @@
 
 
   -- //
-
-
-
