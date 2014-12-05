8a638e6e84633aa93c31081004b0c4b08333588d
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2014-12-05 16:37:05 -0200

    Criado relatório para emissão do protocolo da reserva de vaga
    portabilis/i-educar#1234

---
+++ ieducar/misc/database/deltas/portabilis/private/91_cria_menu_emissa_protocolo_reserva_vaga.sql
@@ -0,0 +1,9 @@
+--
+-- 
+-- @author   Lucas Schmoeller da Silva <lucas@portabilis.com.br>
+-- @license  @@license@@
+-- @version  $Id$
+
+INSERT INTO portal.menu_submenu values(999814, 55, 2, 'Emissão do protocolo do candidato à reserva de vaga', 'module/Reports/ProtocoloCandidatoReserva', null, 3);
+INSERT INTO pmicontrolesis.menu values(999814,999814, 999300, 'Emissão do protocolo do candidato à reserva de vaga', 0, 'module/Reports/ProtocoloCandidatoReserva', '_self', 1, 15, 155, 2);
+INSERT INTO pmieducar.menu_tipo_usuario VALUES (1, 999814, 1, 1, 1);
