8ea663b8d0f58f9a41ec3bd9adb6157f66c42c6e
isacborgert <isac@portabilis.com.br>
2014-12-29 16:02:51 -0200

    Ajuste no titulos de menus em relatórios
    Ajustado letras para maiúsculo conforme solicitado.
    
    portabilis/i-educar#1274

---
+++ ieducar/misc/database/deltas/portabilis/private/93_ajuste_nome_de_submenus.sql
@@ -0,0 +1,8 @@
+--
+-- 
+-- @author   Isac Borgert <isac@portabilis.com.br>
+-- @license  @@license@@
+-- @version  $Id$
+update pmicontrolesis.menu set tt_menu = 'Gr&aacute;fico - Alunos que utilizam transporte' where cod_menu = 999611;
+
+update portal.menu_submenu set nm_submenu = 'Gr&aacute;fico - Alunos que utilizam transporte' where cod_menu_submenu = 999611;
