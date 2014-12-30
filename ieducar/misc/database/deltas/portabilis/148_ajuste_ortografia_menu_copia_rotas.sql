cd7a2f17d644d07ebdf4845ffb06330d24e009de
isacborgert <isac@portabilis.com.br>
2014-12-30 11:30:17 -0200

    Movento arquivos .sql para diretório correto.
    Arquivos estavam na pasta "private", onde, segudno o Lucas, deve estar apenas os relacionados a relatório.
    portabilis/i-educar#695

---
+++ ieducar/misc/database/deltas/portabilis/148_ajuste_ortografia_menu_copia_rotas.sql
@@ -0,0 +1,8 @@
+-- 
+-- @author   Isac Borgert <isac@portabilis.com.br>
+-- @license  @@license@@
+-- @version  $Id$
+update portal.menu_submenu set nm_submenu = 'C&oacute;pia de rotas' where cod_menu_submenu = 21246;
+
+update pmicontrolesis.menu set tt_menu = 'C&oacute;pia de rotas' where cod_menu = 21246;
+
