f895d34b08ea5f5e5287da3813d213c9741e4685
isacborgert <isac@portabilis.com.br>
2014-12-30 14:10:15 -0200

    Removendo sql do diretório errado.
    portabilis/i-educar#695

--- ieducar/misc/database/deltas/portabilis/private/95_ajuste_ortografia_menu_copia_rotas.sql
+++
@@ -1,8 +0,0 @@
--- 
--- @author   Isac Borgert <isac@portabilis.com.br>
--- @license  @@license@@
--- @version  $Id$
-update portal.menu_submenu set nm_submenu = 'C&oacute;pia de rotas' where cod_menu_submenu = 21246;
-
-update pmicontrolesis.menu set tt_menu = 'C&oacute;pia de rotas' where cod_menu = 21246;
-
