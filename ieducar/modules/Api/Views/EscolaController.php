94d0c2353c9a2cf03247a23123838fe19bf44cd3
gabrielms <gabrielmatosdesouza@gmail.com>
2015-01-10 00:11:00 -0200

    Ajustada API de busca de escolas: * Ajustado para buscar somente escolas que possuam vagas de acordo com formula pre definida; * Ajustado para responder uma mensagem pre definida quando não possuir nenhuma escola com vagas em aberto; portabilis/i-educar#1342

--- ieducar/modules/Api/Views/EscolaController.php
+++ ieducar/modules/Api/Views/EscolaController.php
@@ -1,6 +1,7 @@
 <?php
 
-
+error_reporting(E_ERROR);
+ini_set("display_errors", 1);
 /**
  * i-Educar - Sistema de gestão escolar
  *
@@ -32,6 +33,13 @@
 
 require_once 'lib/Portabilis/Controller/ApiCoreController.php';
 require_once 'Portabilis/Array/Utils.php';
+require_once 'include/clsBase.inc.php';
+require_once 'include/clsCadastro.inc.php';
+require_once 'include/clsBanco.inc.php';
+require_once 'include/pmieducar/geral.inc.php';
+require_once 'lib/Portabilis/Date/Utils.php';
+require_once 'lib/Portabilis/String/Utils.php';
+require_once 'lib/Portabilis/Utils/Database.php';
 
 class EscolaController extends ApiCoreController
 {
@@ -143,10 +151,85 @@ class EscolaController extends ApiCoreController
 				AND eal.ano = $1";
       $escolaIds = $this->fetchPreparedQuery($sql, array($ano, $turmaTurnoId, $cursoId, $instituicaoId, $serieId));
 
+      foreach($escolaIds as $escolaId){
+      	if(!$this->existeVagasDisponiveis($escolaId[0])){
+      		if (($key = array_search($escolaId, $escolaIds)) !== false) {
+    			unset($escolaIds[$key]);
+			}
+      	}
+      }
+      if(empty($escolaIds)){
+      	$this->messenger->append("Desculpe, mas aparentemente não existem mais vagas disponíveis para a seleção informada. Altere a seleção e tente novamente.");
+      	return array( 'escolas' => 0);
+      }
+      else{
       $attrs = array('cod_escola');
       return array( 'escolas' => Portabilis_Array_Utils::filterSet($escolaIds, $attrs));
+  	  }
     }
   }
+
+    function existeVagasDisponiveis($escolaId){
+
+    // Caso a capacidade de alunos naquele turno seja menor ou igual ao ao número de alunos matrículados + alunos na reserva de vaga externa deve bloquear
+    if ($this->_getMaxAlunoTurno($escolaId) <= ($this->_getQtdAlunosFila($escolaId) + $this->_getQtdMatriculaTurno($escolaId) )){
+      // $this->mensagem .= Portabilis_String_Utils::toLatin1("Não existem vagas disponíveis para essa série/turno!") . '<br/>';
+      return false;
+    }
+
+    return true;
+  }
+
+  function _getQtdAlunosFila($escolaId){
+
+    $sql = 'SELECT qtd_alunos FROM pmieducar.quantidade_reserva_externa WHERE ref_cod_instituicao = $1 AND ref_cod_escola = $2 AND ref_cod_curso = $3 AND ref_cod_serie = $4 AND ref_turma_turno_id = $5 ';
+
+    return (int) Portabilis_Utils_Database::selectField($sql, array($this->getRequest()->instituicao_id, $escolaId,
+    									                            $this->getRequest()->curso_id, $this->getRequest()->serie_id,
+    									                            $this->getRequest()->turma_turno_id));
+  }
+
+  function _getQtdMatriculaTurno($escolaId){
+    $obj_mt = new clsPmieducarMatriculaTurma();
+
+    return count(array_filter(($obj_mt->lista($int_ref_cod_matricula = NULL, $int_ref_cod_turma = NULL,
+              $int_ref_usuario_exc = NULL, $int_ref_usuario_cad = NULL,
+              $date_data_cadastro_ini = NULL, $date_data_cadastro_fim = NULL,
+              $date_data_exclusao_ini = NULL, $date_data_exclusao_fim = NULL, $int_ativo = 1,
+              $int_ref_cod_serie = $this->ref_cod_serie, $int_ref_cod_curso = $this->getRequest()->curso_id,
+              $int_ref_cod_escola = $escolaId,
+              $int_ref_cod_instituicao = $this->getRequest()->instituicao_id, $int_ref_cod_aluno = NULL, $mes = NULL,
+              $aprovado = NULL, $mes_menor_que = NULL, $int_sequencial = NULL,
+              $int_ano_matricula = NULL, $tem_avaliacao = NULL, $bool_get_nome_aluno = FALSE,
+              $bool_aprovados_reprovados = NULL, $int_ultima_matricula = NULL,
+              $bool_matricula_ativo = NULL, $bool_escola_andamento = FALSE,
+              $mes_matricula_inicial = FALSE, $get_serie_mult = FALSE,
+              $int_ref_cod_serie_mult = NULL, $int_semestre = NULL,
+              $pegar_ano_em_andamento = FALSE, $parar=NULL, $diario = FALSE, 
+              $int_turma_turno_id = $this->getRequest()->turma_turno_id, $int_ano_turma = $this->getRequest()->ano))));
+  }
+  function _getMaxAlunoTurno($escolaId){
+    $obj_t = new clsPmieducarTurma();
+    $det_t = $obj_t->detalhe();
+
+    $lista_t = $obj_t->lista($int_cod_turma = null, $int_ref_usuario_exc = null, $int_ref_usuario_cad = null, 
+    $int_ref_ref_cod_serie = $this->getRequest()->serie_id, $int_ref_ref_cod_escola = $escolaId, $int_ref_cod_infra_predio_comodo = null, 
+    $str_nm_turma = null, $str_sgl_turma = null, $int_max_aluno = null, $int_multiseriada = null, $date_data_cadastro_ini = null, 
+    $date_data_cadastro_fim = null, $date_data_exclusao_ini = null, $date_data_exclusao_fim = null, $int_ativo = 1, $int_ref_cod_turma_tipo = null, 
+    $time_hora_inicial_ini = null, $time_hora_inicial_fim = null, $time_hora_final_ini = null, $time_hora_final_fim = null, $time_hora_inicio_intervalo_ini = null, 
+    $time_hora_inicio_intervalo_fim = null, $time_hora_fim_intervalo_ini = null, $time_hora_fim_intervalo_fim = null, $int_ref_cod_curso = $this->getRequest()->curso_id, $int_ref_cod_instituicao = null, 
+    $int_ref_cod_regente = null, $int_ref_cod_instituicao_regente = null, $int_ref_ref_cod_escola_mult = null, $int_ref_ref_cod_serie_mult = null, $int_qtd_min_alunos_matriculados = null, 
+    $bool_verifica_serie_multiseriada = false, $bool_tem_alunos_aguardando_nota = null, $visivel = null, $turma_turno_id = $this->getRequest()->turma_turno_id, $tipo_boletim = null, $ano = $this->getRequest()->ano, $somenteAnoLetivoEmAndamento = FALSE);
+
+    $max_aluno_turmas = 0;
+
+    foreach ($lista_t as $reg) {
+      $max_aluno_turmas += $reg['max_aluno'];
+    }
+
+    return $max_aluno_turmas;
+  }
+
 
   public function Gerar() {
     if ($this->isRequestFor('get', 'escola'))
