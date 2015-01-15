64a6021c02933166298832c59a2fbdfdc45be2ce
Lucas D'Avila <lucas@lucasdavi.la>
2015-01-15 16:01:54 -0200

    Corrigido configuração AWS S3 usada para upload de fotos.
    * Corrigido e otimizado uso da variavel global de configuração.
    * Validado se configuração foi definida.

--- ieducar/intranet/s3_config.php
+++ ieducar/intranet/s3_config.php
@@ -1,17 +1,21 @@
 <?php
-// Bucket Name
-require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/bootstrap.php';
 
-$bucket= $GLOBALS['coreExt']['Config']->app->aws->bucketname;
-$directory=$GLOBALS['coreExt']['Config']->app->database->dbname.'/';
-$key = $GLOBALS['coreExt']['Config']->app->aws->awsacesskey;
-$secretKey = $GLOBALS['coreExt']['Config']->app->aws->awssecretkey;
+$config = $GLOBALS['coreExt']['Config']->app;
 
-if (!class_exists('S3'))
-        require_once 'S3.php';
+$bucket = $config->aws->bucketname;
+$directory = $config->database->dbname.'/';
+$key = $config->aws->awsacesskey;
+$secretKey = $config->aws->awssecretkey;
 
-//instantiate the class
+if (empty($bucket) || empty($key) || empty($secretKey)) {
+  throw new Exception("Defina as configs AWS.");
+}
+
+if (!class_exists('S3')) {
+  require_once 'S3.php';
+}
+
 $s3 = new S3($key, $secretKey);
-
 $s3->putBucket($bucket, S3::ACL_PUBLIC_READ);
+
 ?>
