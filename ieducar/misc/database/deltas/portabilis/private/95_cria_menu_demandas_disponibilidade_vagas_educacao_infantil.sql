5c9acc4d23b72f30c8f7926aeba5755d42ffa33b
gabrielms <gabrielmatosdesouza@gmail.com>
2015-01-26 15:55:28 -0200

    WIP Relatório gerencial de demandas x disponibilidade de vagas; portabilis/i-educar#1248

---
+++ ieducar/misc/database/deltas/portabilis/private/95_cria_menu_demandas_disponibilidade_vagas_educacao_infantil.sql
@@ -0,0 +1,23 @@
+-- @author   Gabriel Matos de Souza <gabriel@portabilis.com.br>
+-- @license  @@license@@
+-- @version  $Id$
+
+insert into portal.menu_submenu values (21248, 55, 2, 'Relação de alunos e localidade', 'module/Reports/AlunosLocalidade', null, 3);
+
+insert into pmicontrolesis.menu values (21248, 21248, 999300, 'Relação de alunos e localidade', 0, 'module/Reports/AlunosLocalidade', '_self', 1, 15, 192, 2);
+
+insert into portal.menu_funcionario values(1,0,0,21247);
+
+insert into pmieducar.menu_tipo_usuario values(1,21247,1,0,1);
+
+
+---Reverse
+--delete from pmieducar.menu_tipo_usuario where ref_cod_menu_submenu = 21247;
+
+--delete from portal.menu_funcionario where ref_cod_menu_submenu = 21247;
+
+--delete from pmicontrolesis.menu where cod_menu = 21247;
+
+--delete from portal.menu_submenu where cod_menu_submenu = 21247;
+
+
