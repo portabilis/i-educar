9084cee51b1bb29b31ec77d6fa33fe3557298ec0
gabrielms <gabrielmatosdesouza@gmail.com>
2015-01-28 11:31:26 -0200

    Adicionado relatório de demandas x disp. vagas; portabilis/i-educar#1248

--- ieducar/misc/database/deltas/portabilis/private/95_cria_menu_demandas_disponibilidade_vagas_educacao_infantil.sql
+++ ieducar/misc/database/deltas/portabilis/private/95_cria_menu_demandas_disponibilidade_vagas_educacao_infantil.sql
@@ -2,22 +2,22 @@
 -- @license  @@license@@
 -- @version  $Id$
 
-insert into portal.menu_submenu values (21248, 55, 2, 'Relação de alunos e localidade', 'module/Reports/AlunosLocalidade', null, 3);
+insert into portal.menu_submenu values (21248, 55, 2, 'Demandas x disponibilidade de vagas na ed. infantil', 'module/Reports/DemandasDisponibilidadeVagasInfantil', null, 3);
 
-insert into pmicontrolesis.menu values (21248, 21248, 999300, 'Relação de alunos e localidade', 0, 'module/Reports/AlunosLocalidade', '_self', 1, 15, 192, 2);
+insert into pmicontrolesis.menu values (21248, 21248, 999300, 'Demandas x disponibilidade de vagas na ed. infantil', 0, 'module/Reports/DemandasDisponibilidadeVagasInfantil', '_self', 1, 15, 192, 1);
 
-insert into portal.menu_funcionario values(1,0,0,21247);
+insert into portal.menu_funcionario values(1,1,1,21248);
 
-insert into pmieducar.menu_tipo_usuario values(1,21247,1,0,1);
+insert into pmieducar.menu_tipo_usuario values(1,21248,1,1,1);
 
 
 ---Reverse
---delete from pmieducar.menu_tipo_usuario where ref_cod_menu_submenu = 21247;
+--delete from pmieducar.menu_tipo_usuario where ref_cod_menu_submenu = 21248;
 
---delete from portal.menu_funcionario where ref_cod_menu_submenu = 21247;
+--delete from portal.menu_funcionario where ref_cod_menu_submenu = 21248;
 
---delete from pmicontrolesis.menu where cod_menu = 21247;
+--delete from pmicontrolesis.menu where cod_menu = 21248;
 
---delete from portal.menu_submenu where cod_menu_submenu = 21247;
+--delete from portal.menu_submenu where cod_menu_submenu = 21248;
 
 
