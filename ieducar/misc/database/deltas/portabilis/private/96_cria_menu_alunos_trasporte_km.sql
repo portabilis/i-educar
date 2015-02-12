4a4d5964fad2bb52cb466e27076fb4d99536450c
isacborgert <isac@portabilis.com.br>
2015-02-12 13:48:54 +0000

    Alterado chave primária pois a antiga já estava em uso.
    portabilis/i-educar#1023

--- ieducar/misc/database/deltas/portabilis/private/96_cria_menu_alunos_trasporte_km.sql
+++ ieducar/misc/database/deltas/portabilis/private/96_cria_menu_alunos_trasporte_km.sql
@@ -4,10 +4,10 @@
 -- @version  $Id$
 
 
-insert into portal.menu_submenu values (21248, 69, 2 ,'Alunos do transporte escolar', 'module/Reports/AlunosTransporteKm', null, 3);
+insert into portal.menu_submenu values (21249, 69, 2 ,'Alunos do transporte escolar', 'module/Reports/AlunosTransporteKm', null, 3);
 
-insert into pmicontrolesis.menu values (21248, 21248, 20712, 'Alunos do transporte escolar', 0, 'module/Reports/AlunosTransporteKm', '_self', 1, 17, 192);
+insert into pmicontrolesis.menu values (21249, 21249, 20712, 'Alunos do transporte escolar', 0, 'module/Reports/AlunosTransporteKm', '_self', 1, 17, 192);
 
-insert into portal.menu_funcionario values(1,0,0,21248);
+insert into portal.menu_funcionario values(1,0,0,21249);
 
-insert into pmieducar.menu_tipo_usuario values(1,21248,1,0,1);
+insert into pmieducar.menu_tipo_usuario values(1,21249,1,0,1);
