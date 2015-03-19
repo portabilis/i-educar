d137eda3f7f8adacbeaa39ad8ee09e84645ff0a6
isacborgert <isac@portabilis.com.br>
2015-03-19 11:09:33 -0300

    Ajustes na tela do relatório de motoristas.
    portabilis/i-educar#1198

--- ieducar/misc/database/deltas/portabilis/private/101_cria_menu_motoristas_transporte_escolar.sql
+++ ieducar/misc/database/deltas/portabilis/private/101_cria_menu_motoristas_transporte_escolar.sql
@@ -3,11 +3,11 @@
 -- @version  $Id$
 
 
-insert into portal.menu_submenu values (999817, 69, 2 ,'Motoristas de transporte escolar', 'module/Reports/RelatorioMotorista', null, 3);
-insert into pmicontrolesis.menu values (999817, 999817, 20712, 'Motoristas de transporte escolar', 0, 'module/Reports/RelatorioMotorista', '_self', 1, 17, 192);
-insert into pmieducar.menu_tipo_usuario values(1,999817,1,1,1);
+insert into portal.menu_submenu values (21252, 69, 2 ,'Motoristas de transporte escolar', 'module/Reports/RelatorioMotorista', null, 3);
+insert into pmicontrolesis.menu values (21252, 21252, 20712, 'Motoristas de transporte escolar', 0, 'module/Reports/RelatorioMotorista', '_self', 1, 17, 192);
+insert into pmieducar.menu_tipo_usuario values(1,21252,1,1,1);
 
 --undo
 
-delete from portal.menu_submenu where cod_menu_submenu = 999817;
-delete from pmicontrolesis.menu where cod_menu = 999817;
+delete from portal.menu_submenu where cod_menu_submenu = 21252;
+delete from pmicontrolesis.menu where cod_menu = 21252;
