383419250d036e7d9118bacb66f94a9ae8b6f4bc
Iago Effting <iago.effting@gmail.com>
2015-04-23 10:29:36 -0300

    Modificar relacionamento de servidores * Alterado relacionamento do servidor para pessoa portabilis/i-educar#1519

---
+++ ieducar/misc/database/deltas/34_atualiza_foreign_key_de_pmieducar_servidor_referenciar_pessoa.sql
@@ -0,0 +1,22 @@
+
+-- //
+
+--
+-- Atualiza a foreign key constraint de pmieducar.servidor
+-- para referenciar cadastro.pessoa.
+--
+-- Essa atualização é devido a issue #1519 referente a não necessidade do 
+-- cadastro de um novo usuário ao criar um novo servidor.
+--
+-- @author   Iago Effting <iago@portabilis.com.br>
+-- @license  @@license@@
+-- @version  $Id$
+--
+ALTER TABLE pmieducar.servidor DROP CONSTRAINT servidor_cod_servidor_fkey;
+ALTER TABLE pmieducar.servidor ADD CONSTRAINT fk_servidor_pessoa FOREIGN KEY (cod_servidor) REFERENCES pessoa (idpes);
+
+-- //@UNDO
+ALTER TABLE pmieducar.servidor DROP CONSTRAINT fk_servidor_pessoa;
+ALTER TABLE pmieducar.servidor ADD CONSTRAINT servidor_cod_servidor_fkey FOREIGN KEY (cod_servidor) REFERENCES funcionario (ref_cod_pessoa_fj) ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+-- //
