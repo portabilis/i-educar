238e324f818ea34ba83afad5d3aafe7c84e0c38a
Iago Effting <iago.effting@gmail.com>
2015-04-23 15:04:03 -0300

    Revert "Modificar relacionamento de servidores"
    This reverts commit 282fc6b84d44d1672d7df9d1186c11dfcf9a8403.

--- ieducar/misc/database/deltas/34_atualiza_foreign_key_de_pmieducar_servidor_referenciar_pessoa.sql
+++ ieducar/misc/database/deltas/34_atualiza_foreign_key_de_pmieducar_servidor_referenciar_pessoa.sql
@@ -13,10 +13,10 @@
 -- @version  $Id$
 --
 ALTER TABLE pmieducar.servidor DROP CONSTRAINT servidor_cod_servidor_fkey;
-ALTER TABLE pmieducar.servidor ADD CONSTRAINT fk_servidor_pessoa FOREIGN KEY (cod_servidor) REFERENCES cadastro.pessoa (idpes);
+ALTER TABLE pmieducar.servidor ADD CONSTRAINT fk_servidor_pessoa FOREIGN KEY (cod_servidor) REFERENCES pessoa (idpes);
 
 -- //@UNDO
 ALTER TABLE pmieducar.servidor DROP CONSTRAINT fk_servidor_pessoa;
-ALTER TABLE pmieducar.servidor ADD CONSTRAINT servidor_cod_servidor_fkey FOREIGN KEY (cod_servidor) REFERENCES portal.funcionario (ref_cod_pessoa_fj) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE pmieducar.servidor ADD CONSTRAINT servidor_cod_servidor_fkey FOREIGN KEY (cod_servidor) REFERENCES funcionario (ref_cod_pessoa_fj) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 -- //
