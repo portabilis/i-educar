a04fca7a508003bbb83b7e26ec12cbcf1bfdc3b6
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2015-06-11 16:28:34 -0300

    Criado relatório gráfico de usuários e acessos
    portabilis/i-educar#1373

--- ieducar/misc/database/deltas/portabilis/private/104_menu_usuarios_acessos_resumido.sql
+++ ieducar/misc/database/deltas/portabilis/private/104_menu_usuarios_acessos_resumido.sql
@@ -7,14 +7,14 @@
  	-- @version  $Id$
 
 
-  insert into portal.menu_submenu values(999244,55,2,'Usuários e acessos resumido','module/Reports/UsuarioAcessoResumido',NULL,3);
-  insert into pmicontrolesis.menu values(999244,999244,999300,'Usuários e acessos resumido',11,'module/Reports/UsuarioAcessoResumido','_self',1,15,192, 1);
+  insert into portal.menu_submenu values(999244,55,2,'Gráfico de usuários e acessos','module/Reports/UsuarioAcessoResumido',NULL,3);
+  insert into pmicontrolesis.menu values(999244,999244,999300,'Gráfico de usuários e acessos',11,'module/Reports/UsuarioAcessoResumido','_self',1,15,192, 1);
 
 	-- //@UNDO
 
   DELETE FROM menu_tipo_usuario WHERE ref_cod_menu_submenu = 999244
-  DELETE FROM pmicontrolesis.menu WHERE cod_menu = 999244;
   DELETE FROM portal.menu_submenu WHERE cod_menu_submenu = 999244;
+  DELETE FROM pmicontrolesis.menu WHERE cod_menu = 999244;
 
 
 	-- //
