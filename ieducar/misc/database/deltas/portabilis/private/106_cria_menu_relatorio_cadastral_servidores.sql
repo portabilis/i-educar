3d704bfb7e34b7a08cd2e4fb6e6c58c0ea93be6e
Caroline Salib Canto <caroline@portabilis.com.br>
2015-06-17 17:40:44 -0300

    Menu de chamada do relatório cadastral de servidores; Portabilis/i-educar#1527

---
+++ ieducar/misc/database/deltas/portabilis/private/106_cria_menu_relatorio_cadastral_servidores.sql
@@ -0,0 +1,17 @@
+
+-- @author   Caroline Salib Canto <caroline@portabilis.com.br>
+-- @license  @@license@@
+-- @version  $Id$
+
+
+insert into portal.menu_submenu values (999820, 55, 2,'Relatório Cadastral de Servidores', 'module/Reports/RelacaoServidores', null, 3);
+insert into portal.menu_funcionario values(1,0,0,999820);
+insert into pmicontrolesis.menu values (999820, 999820, 999302, 'Relatório Cadastral de Servidores', 0, 'module/Reports/RelacaoServidores', '_self', 1, 15, 192);
+insert into pmieducar.menu_tipo_usuario values(1,999820,1,0,1);
+
+--undo
+
+delete from pmieducar.menu_tipo_usuario where ref_cod_menu_submenu = 999820;
+delete from pmicontrolesis.menu where cod_menu = 999820;
+delete from portal.menu_funcionario where ref_cod_menu_submenu = 999820;
+delete from portal.menu_submenu where cod_menu_submenu = 999820;
