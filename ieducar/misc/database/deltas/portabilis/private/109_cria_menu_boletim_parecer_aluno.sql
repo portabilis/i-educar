e29d38b8d59ea6002564326aef50de099c3d4087
Alan Felipe Farias <alan_felipe_farias@hotmail.com>
2015-07-03 11:25:46 -0300

    foram criados submenus e formulario para a emissão de relatorios portabilis/i-educar#1646

---
+++ ieducar/misc/database/deltas/portabilis/private/109_cria_menu_boletim_parecer_aluno.sql
@@ -0,0 +1,16 @@
+
+-- @author   Alan Felipe Farias <alan@portabilis.com.br>
+-- @license  @@license@@
+-- @version  $Id$
+
+
+insert into portal.menu_submenu values (999823, 55, 2,'Boletim Parecer Aluno', 'module/Reports/BoletimParecerAluno', null, 3);
+insert into pmicontrolesis.menu values (999823, 999823, 999450, 'Boletim Parecer Aluno', 0, 'module/Reports/BoletimParecerAluno', '_self', 1, 15, 192, null);
+insert into pmieducar.menu_tipo_usuario values(1,999823,1,1,1);
+
+--undo
+
+delete from pmieducar.menu_tipo_usuario where ref_cod_menu_submenu = 999823;
+delete from pmicontrolesis.menu where cod_menu = 999823;
+delete from portal.menu_submenu where cod_menu_submenu = 999823;
+
