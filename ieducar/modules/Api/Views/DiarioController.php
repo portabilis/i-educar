e54fe6102e098ce2dd041f29f29d1557d75d9bb8
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2015-08-04 15:48:24 -0300

    Dev. API para receber postagem de notas do Educação
    portabilis/i-educar#1822

---
+++ ieducar/modules/Api/Views/DiarioController.php
@@ -0,0 +1,142 @@
+<?php
+//error_reporting(E_ERROR);
+//ini_set("display_errors", 1);
+/**
+ * i-Educar - Sistema de gestão escolar
+ *
+ * Copyright (C) 2006  Prefeitura Municipal de Itajaí
+ *     <ctima@itajai.sc.gov.br>
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+ * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+ * qualquer versão posterior.
+ *
+ * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
+ * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+ * do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
+ * com este programa; se não, escreva para a Free Software Foundation, Inc., no
+ * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * @author    Lucas Schmoeller da Silva <lucas@portabilis.com.br>
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   Api
+ * @subpackage  Modules
+ * @since   Arquivo disponível desde a versão ?
+ * @version   $Id$
+ */
+
+require_once 'Portabilis/Controller/ApiCoreController.php';
+require_once 'Avaliacao/Service/Boletim.php';
+require_once 'Avaliacao/Model/NotaComponenteDataMapper.php';
+require_once 'Portabilis/String/Utils.php';
+
+class DiarioController extends ApiCoreController
+{
+  protected $_processoAp        = 642;
+
+  protected function trySaveServiceBoletim($turmaId, $alunoId) {
+    try {
+      $this->serviceBoletim($turmaId, $alunoId)->save();
+    }
+    catch (CoreExt_Service_Exception $e) {
+      // excecoes ignoradas :( pois servico lanca excecoes de alertas, que não são exatamente erros.
+      // error_log('CoreExt_Service_Exception ignorada: ' . $e->getMessage());
+    }
+  }
+
+  protected function findMatriculaByTurmaAndAluno($turmaId, $alunoId){
+    $resultado = array();
+
+    $sql = 'SELECT m.cod_matricula AS id
+              FROM pmieducar.matricula m
+              INNER JOIN pmieducar.matricula_turma mt ON m.cod_matricula = mt.ref_cod_matricula
+              WHERE m.ativo = 1
+              AND mt.ativo = 1
+              AND mt.ref_cod_turma = $1
+              AND m.ref_cod_aluno = $2
+              LIMIT 1';
+
+    $matriculaId = $this->fetchPreparedQuery($sql, array($turmaId, $alunoId), true, 'first-field');
+
+    if(empty($matriculaId))
+      throw new CoreExt_Exception(Portabilis_String_Utils::toLatin1("Não foi possível encontrar uma matrícula para o aluno {$alunoId}."));
+
+    return $matriculaId;
+  }
+
+  protected function serviceBoletim($turmaId, $alunoId) {
+    $matriculaId = $this->findMatriculaByTurmaAndAluno($turmaId, $alunoId);
+
+    if (! isset($this->_boletimServiceInstances))
+      $this->_boletimServiceInstances = array();
+
+    // set service
+    if (! isset($this->_boletimServiceInstances[$matriculaId])) {
+      try {
+        $params = array('matricula' => $matriculaId);
+        $this->_boletimServiceInstances[$matriculaId] = new Avaliacao_Service_Boletim($params);
+      }
+      catch (Exception $e){
+        $this->messenger->append(Portabilis_String_Utils::toLatin1("Erro ao instanciar serviço boletim para matricula {$matriculaId}: ") . $e->getMessage(), 'error', true);
+      }
+    }
+
+    // validates service
+    if (is_null($this->_boletimServiceInstances[$matriculaId]))
+      throw new CoreExt_Exception(Portabilis_String_Utils::toLatin1("Não foi possivel instanciar o serviço boletim para a matrícula $matriculaId."));
+
+    return $this->_boletimServiceInstances[$matriculaId];
+  }
+
+  protected function canPostNotas(){
+    return $this->validatesPresenceOf('turmas') && $this->validatesPresenceOf('etapa');
+  }
+
+  protected function postNotas(){
+    if($this->canPostNotas()){
+      $turmas = $this->getRequest()->turmas;
+      $etapa = $this->getRequest()->etapa;
+
+      foreach ($turmas as $turma) {
+        $turmaId = $turma['turma_id'];
+        $alunos = $turma['alunos'];
+
+        foreach ($alunos as $aluno) {
+          $alunoId = $aluno['aluno_id'];
+
+          $componentesCurriculares = $aluno['componentes_curriculares'];
+          foreach ($componentesCurriculares as $componenteCurricular) {
+            $componenteCurricularId = $componenteCurricular['componente_curricular_id'];
+            $valor = $componenteCurricular['valor'];
+
+
+            $array_nota = array(
+                  'componenteCurricular' => $componenteCurricularId,
+                  'nota'                 => $valor,
+                  'etapa'                => $etapa,
+                  'notaOriginal'         => $valor);
+
+            $nota = new Avaliacao_Model_NotaComponente($array_nota);
+
+            $this->serviceBoletim($turmaId, $alunoId)->addNota($nota);
+            $this->trySaveServiceBoletim($turmaId, $alunoId);
+          }
+        }
+      }
+
+      $this->messenger->append('Notas postadas com sucesso!', 'success');
+    }
+  }
+
+  public function Gerar() {
+    if ($this->isRequestFor('post', 'notas'))
+      $this->appendResponse($this->postNotas());
+    else
+      $this->notImplementedOperationError();
+  }
+}
