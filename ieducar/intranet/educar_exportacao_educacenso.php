b0ef8bfe8a88127d5de10dbafc7b13b878aa14d2
Gabriel Souza <gabriel@portabilis.com.br>
2015-08-12 10:00:51 -0300

    Ajustado registro 80 para não buscar enturmações excluidas; portabilis/i-educar#1863

--- ieducar/intranet/educar_exportacao_educacenso.php
+++ ieducar/intranet/educar_exportacao_educacenso.php
@@ -1627,6 +1627,7 @@ protected function exportaDadosRegistro70($escolaId, $ano, $data_ini, $data_fim,
         AND m.ano = $2
         AND a.cod_aluno = $5
         AND m.ativo = 1
+        AND mt.ativo = 1
     ';
 
     // Transforma todos resultados em variáveis
