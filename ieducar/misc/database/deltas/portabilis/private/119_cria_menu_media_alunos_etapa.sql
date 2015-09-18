de5e633d7b2f57c5455a7aadfc004d722e974289
Alan Farias <alan@portabilis.com.br>
2015-09-18 17:13:25 -0300

    Alterado nome e local do menu de alunos com melhor desempenho; * Trocado a string Limite de alunos para Limite de posições; portabilis/i-educar#1771;

--- ieducar/misc/database/deltas/portabilis/private/119_cria_menu_media_alunos_etapa.sql
+++ ieducar/misc/database/deltas/portabilis/private/119_cria_menu_media_alunos_etapa.sql
@@ -4,8 +4,8 @@
 -- @version  $Id$
 
 
-insert into portal.menu_submenu values (999834, 55, 2,'Comparativo de médias por etapa', 'module/Reports/MediaAlunos', null, 3);
-insert into pmicontrolesis.menu values (999834, 999834, 999300, 'Comparativo de médias por etapa', 0, 'module/Reports/MediaAlunos', '_self', 1, 15, 192);
+insert into portal.menu_submenu values (999834, 55, 2,'Relação de alunos com o melhor desempenho', 'module/Reports/MediaAlunos', null, 3);
+insert into pmicontrolesis.menu values (999834, 999834, 999301, 'Relação de alunos com o melhor desempenho', 0, 'module/Reports/MediaAlunos', '_self', 1, 15, 127);
 insert into pmieducar.menu_tipo_usuario values(1,999834,1,0,1);
 
 --undo
@@ -13,4 +13,3 @@ insert into pmieducar.menu_tipo_usuario values(1,999834,1,0,1);
 delete from pmieducar.menu_tipo_usuario where ref_cod_menu_submenu = 999834;
 delete from pmicontrolesis.menu where cod_menu = 999834;
 delete from portal.menu_submenu where cod_menu_submenu = 999834;
-
