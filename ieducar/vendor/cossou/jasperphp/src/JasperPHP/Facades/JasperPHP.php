1bdb02d286a609afd9b762289e5735dddca2d21e
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2015-09-25 14:12:29 -0300

    Implementada solução alternativa para emissão de relatórios

---
+++ ieducar/vendor/cossou/jasperphp/src/JasperPHP/Facades/JasperPHP.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace JasperPHP\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class JasperPHP extends Facade {
+
+    protected static function getFacadeAccessor() { return 'jasperphp'; }
+}
