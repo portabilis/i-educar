0f479e6298ac90c70110afd2e76a27d1d0fd5a1f
Alan Farias <alan@portabilis.com.br>
2015-10-06 15:28:54 -0300

    Atualizado tipo de autor em obras com autores já cadastrados; portabilis/i-educar#1955;

--- ieducar/misc/database/deltas/portabilis/226_adiciona_campos_biblioteca_geral.sql
+++ ieducar/misc/database/deltas/portabilis/226_adiciona_campos_biblioteca_geral.sql
@@ -20,7 +20,9 @@ INSERT INTO pmieducar.tipo_autor VALUES (4, 'Anônimo');
  ALTER TABLE pmieducar.acervo ALTER COLUMN num_edicao drop not null;
  ALTER TABLE pmieducar.acervo ALTER COLUMN ano drop not null;
  ALTER TABLE pmieducar.acervo ALTER COLUMN num_paginas drop not null;
-      UPDATE pmieducar.acervo SET tipo_autor = 1 WHERE cod_acervo < 500;
+      update pmieducar.acervo set ref_cod_tipo_autor = 1 
+       where cod_acervo in(select ref_cod_acervo 
+        from pmieducar.acervo_acervo_autor);
  --undo
 
  ALTER TABLE pmieducar.acervo DROP COLUMN dimencao;
@@ -34,4 +36,6 @@ INSERT INTO pmieducar.tipo_autor VALUES (4, 'Anônimo');
  ALTER TABLE pmieducar.acervo ALTER COLUMN num_edicao type int set not null;
  ALTER TABLE pmieducar.acervo ALTER COLUMN ano type int set not null;
  ALTER TABLE pmieducar.acervo ALTER COLUMN num_paginas type int set not null;
-      UPDATE pmieducar.acervo SET tipo_autor = null WHERE cod_acervo < 500;
+      update pmieducar.acervo set ref_cod_tipo_autor = null
+       where cod_acervo in(select ref_cod_acervo 
+        from pmieducar.acervo_acervo_autor);
