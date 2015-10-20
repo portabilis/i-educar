983cd27ba2f2e9867307ae99e838f3b4d6dc9250
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2015-10-20 11:26:01 -0200

    Permitida multiplas candidaturas reserva de vaga
    portabilis/i-educar#2067

---
+++ ieducar/misc/database/deltas/portabilis/230_cria_parametro_reserva_vaga_instituicao.sql
@@ -0,0 +1,6 @@
+--
+-- @author   Lucas Schmoeller da Silva <lucas@portabilis.com.br>
+-- @license  @@license@@
+-- @version  $Id$
+
+ALTER TABLE pmieducar.instituicao ADD COLUMN multiplas_reserva_vaga BOOLEAN NOT NULL DEFAULT FALSE;
