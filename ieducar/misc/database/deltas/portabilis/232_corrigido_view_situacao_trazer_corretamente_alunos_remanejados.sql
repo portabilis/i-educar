6e3073b5f8f690d361318a65afa12472c9fb9b67
Caroline Salib <caroline@portabilis.com.br>
2015-10-22 10:29:00 -0200

    Ajustado view de situação para contornar bug de alunos remanejados; * Corrigido para que não traga alunos remanejados se o status de remanejado estiver na ultima enturmação, pois esso é incorreto; Portabilis/i-educar#2063

--- ieducar/misc/database/deltas/portabilis/232_corrigido_view_situacao_trazer_corretamente_alunos_remanejados.sql
+++ ieducar/misc/database/deltas/portabilis/232_corrigido_view_situacao_trazer_corretamente_alunos_remanejados.sql
@@ -17,12 +17,6 @@ SELECT matricula.cod_matricula,
               WHEN matricula.aprovado = 1 THEN 'Aprovado'::character varying
               WHEN matricula.aprovado = 12 THEN 'Ap. Depen.'::character varying
               WHEN matricula.aprovado = 2 THEN 'Reprovado'::character varying
-              WHEN matricula.aprovado = 3
-                   AND COALESCE(
-                                  (SELECT mt.remanejado
-                                   FROM pmieducar.matricula_turma mt
-                                   WHERE mt.ref_cod_matricula = matricula_turma.ref_cod_matricula
-                                     AND mt.remanejado = TRUE LIMIT 1), FALSE) THEN 'Remanejado'::character varying
               WHEN matricula.aprovado = 3 THEN 'Andamento'::character varying
               WHEN matricula.aprovado = 4 THEN 'Transferido'::character varying
               WHEN matricula.aprovado = 5 THEN 'Reclassificado'::character varying
@@ -38,12 +32,6 @@ SELECT matricula.cod_matricula,
               WHEN matricula.aprovado = 1 THEN 'Apr'::character varying
               WHEN matricula.aprovado = 12 THEN 'ApDp'::character varying
               WHEN matricula.aprovado = 2 THEN 'Rep'::character varying
-              WHEN matricula.aprovado = 3
-                   AND COALESCE(
-                                  (SELECT mt.remanejado
-                                   FROM pmieducar.matricula_turma mt
-                                   WHERE mt.ref_cod_matricula = matricula_turma.ref_cod_matricula
-                                     AND mt.remanejado = TRUE LIMIT 1), FALSE) THEN 'Rem'::character varying
               WHEN matricula.aprovado = 3 THEN 'And'::character varying
               WHEN matricula.aprovado = 4 THEN 'Trs'::character varying
               WHEN matricula.aprovado = 5 THEN 'Recl'::character varying
@@ -53,26 +41,29 @@ SELECT matricula.cod_matricula,
 FROM relatorio.situacao_matricula,
      pmieducar.matricula
 LEFT JOIN pmieducar.matricula_turma ON matricula_turma.ref_cod_matricula = matricula.cod_matricula
-WHERE CASE
-          WHEN matricula.aprovado = 4 THEN (matricula_turma.ativo = 1
-                                            OR (EXISTS
-                                                  (SELECT 1
-                                                   FROM pmieducar.transferencia_solicitacao
-                                                   WHERE transferencia_solicitacao.ref_cod_matricula_saida = matricula.cod_matricula
-                                                     AND transferencia_solicitacao.ativo = 1 LIMIT 1))
-                                            OR matricula_turma.transferido
-                                            OR matricula_turma.reclassificado
-                                            OR matricula_turma.remanejado)
-               AND matricula_turma.sequencial =
-                 (SELECT MAX(sequencial)
-                  FROM pmieducar.matricula_turma mt
-                  WHERE mt.ref_cod_matricula = matricula.cod_matricula)
-          WHEN matricula.aprovado = 6 THEN matricula_turma.ativo = 1
-               OR matricula_turma.abandono
-          WHEN matricula.aprovado = 5 THEN matricula_turma.ativo = 1
-               OR matricula_turma.reclassificado
-          ELSE matricula_turma.ativo = 1
-      END
+WHERE (CASE WHEN matricula.aprovado = 4 THEN (matricula_turma.ativo = 1
+                                              OR (EXISTS
+                                                    (SELECT 1
+                                                     FROM pmieducar.transferencia_solicitacao
+                                                     WHERE transferencia_solicitacao.ref_cod_matricula_saida = matricula.cod_matricula
+                                                       AND transferencia_solicitacao.ativo = 1 LIMIT 1))
+                                              OR matricula_turma.transferido
+                                              OR matricula_turma.reclassificado
+                                              OR matricula_turma.remanejado)
+       AND matricula_turma.sequencial =
+         (SELECT MAX(sequencial)
+          FROM pmieducar.matricula_turma mt
+          WHERE mt.ref_cod_matricula = matricula.cod_matricula) WHEN matricula.aprovado = 6 THEN matricula_turma.ativo = 1
+       OR matricula_turma.abandono WHEN matricula.aprovado = 5 THEN matricula_turma.ativo = 1
+       OR matricula_turma.reclassificado ELSE (matricula_turma.ativo = 1
+                                               OR matricula_turma.transferido
+                                               OR matricula_turma.reclassificado
+                                               OR matricula_turma.abandono
+                                               OR (matricula_turma.remanejado -- Se for remanejado não pode ser a ultima enturmação
+                                                   AND matricula_turma.sequencial <
+                                                     (SELECT MAX(sequencial)
+                                                      FROM pmieducar.matricula_turma mt
+                                                      WHERE mt.ref_cod_matricula = matricula.cod_matricula))) END)
   AND CASE
           WHEN situacao_matricula.cod_situacao = 10 THEN matricula.aprovado = ANY (ARRAY[1::smallint, 2::smallint, 3::smallint, 4::smallint, 5::smallint, 6::smallint, 12::smallint])
           WHEN situacao_matricula.cod_situacao = 9 THEN (matricula.aprovado = ANY (ARRAY[1::smallint, 2::smallint, 3::smallint, 5::smallint, 12::smallint]))
@@ -96,4 +87,6 @@ WHERE CASE
           ELSE matricula.aprovado = situacao_matricula.cod_situacao
       END;
 
+
 ALTER TABLE relatorio.view_situacao OWNER TO ieducar;
+
