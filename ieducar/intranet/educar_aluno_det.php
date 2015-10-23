9840cc3deb75ae8e5411d60de3a3709b5e19f319
Caroline Salib <caroline@portabilis.com.br>
2015-10-23 09:10:17 -0200

    Retirado debug nos detalhes do aluno; Portabilis/i-educar#1615

--- ieducar/intranet/educar_aluno_det.php
+++ ieducar/intranet/educar_aluno_det.php
@@ -494,8 +494,6 @@ class indice extends clsDetalhe
 
     $responsavel = $tmp_obj->getResponsavelAluno();
 
-    echo print_r($responsavel);
-
     if ($responsavel && is_null($registro['ref_idpes_responsavel'])) {
       $this->addDetalhe(array('Nome do Responsável', $responsavel['nome_responsavel']));
     }
