c57b7d5a741e5674bcc21bba6a7aabad49b34187
Caroline Salib <carolinesalibc@gmail.com>
2015-12-18 15:33:28 -0200

    Adicionado permissão para lançamento de notas fora do período em tipo de usuário; portabilis/i-educar#2005

---
+++ ieducar/misc/database/deltas/portabilis/256_adiciona_permissao_lancamento_notas_fora_periodo.sql
@@ -0,0 +1,8 @@
+
+-- @author   Caroline Salib Canto <caroline@portabilis.com.br>
+-- @license  @@license@@
+-- @version  $Id$
+
+insert into pmicontrolesis.menu values(999849,950,null,'Permitir lançamento de notas/faltas fora do período de bloqueio por etapa',0,'','_self',1,7,1,null);
+insert into portal.menu_submenu values(999849,55,0,'Permitir lançamento de notas/faltas fora do período de bloqueio por etapa', '', null, 3);
+insert into pmieducar.menu_tipo_usuario values(1, 999849, 1, 1, 1);
