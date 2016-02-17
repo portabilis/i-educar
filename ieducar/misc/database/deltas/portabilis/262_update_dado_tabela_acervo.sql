6505e59432b8ca2ff1a9148f9a0f7acd73c1c2b7
Bonot <paula_bonot@hotmail.com>
2016-02-17 15:00:31 -0200

    Revert "Altera campos volume e num_edicao para NULL, para reproduzir erro de campo vazio no relatório"
    This reverts commit 7aff8bd26145447e63e9d1660e20837cliente784e5ed.

--- ieducar/misc/database/deltas/portabilis/262_update_dado_tabela_acervo.sql
+++
@@ -1,6 +0,0 @@
--- @author   Paula Bonot <bonot@portabilis.com.br>
--- @license  @@license@@
--- @version  $Id$
-
--- Altera campos volume e num_edicao para NULL, para reproduzir erro de campo vazio no relatório
-UPDATE acervo set volume = NULL,num_edicao = NULL WHERE titulo = 'BRANCA DE NEVE'
