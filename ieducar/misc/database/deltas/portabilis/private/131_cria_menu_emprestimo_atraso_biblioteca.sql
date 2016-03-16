d28cd28b7fbd2af09a257a29fdb9b8e08148d657
Bonot <paula_bonot@hotmail.com>
2016-03-16 17:31:03 -0300

    Cria relatório de emprestimos em atrasos e corrige erros nos arquivos Refs #424

--- ieducar/misc/database/deltas/portabilis/private/131_cria_menu_emprestimo_atraso_biblioteca.sql
+++ ieducar/misc/database/deltas/portabilis/private/131_cria_menu_emprestimo_atraso_biblioteca.sql
@@ -3,7 +3,7 @@
 -- @license  @@license@@
 -- @version  $Id$
 
-insert into portal.menu_submenu values (999856, 57, 2,'Empréstimos em atraso', 'module/Reports/EmprestimosAtrasoBiblioteca', null, 3);
+insert into portal.menu_submenu values (999856, 57, 2,'Empréstimos em atraso', 'module/Reports/EmprestimoAtrasoBiblioteca', null, 3);
 insert into pmicontrolesis.menu values (999856, 999856, 999614, 'Empréstimos em atraso', 0, 'module/Reports/EmprestimoAtrasoBiblioteca', '_self', 1, 16, 192);
 insert into pmieducar.menu_tipo_usuario values(1,999856,1,1,1);
 
