50e2c12732ff79a09de5111083ca45ff73cf5d62
Caroline Salib <carolinesalibc@gmail.com>
2016-03-18 14:23:09 -0300

    Corrigido problema de css nas mensagens de login; Refs #541

--- ieducar/intranet/scripts/mensagens.js
+++ ieducar/intranet/scripts/mensagens.js
@@ -1,6 +1,6 @@
 jQuery.get("http://portabilis-adm.herokuapp.com/api/v1/messages?product=2", function(response) {
 	for (var i = response.length - 1; i >= 0; i--) {
-	  	var cabecalho = '<div style="min-height: 0px; padding: 5px 10px; background-color:'+response[i].color+';>';
+	  	var cabecalho = '<div class="teste '+i+'" style="min-height: 0px; padding: 5px 10px; background-color:'+response[i].color+';">';
 
 	  	$j(".mensagens").append(cabecalho+response[i].message+'</div>'+'<br>');
 	}
