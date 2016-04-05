a3a8412723734cf71fc8d4e64a754284d359d991
Caroline Salib Canto <carolinesalibc@gmail.com>
2016-04-05 11:49:27 -0300

    Alterado para verificar se utiliza regra diferenciada na escola; Refs #667

--- ieducar/intranet/educar_disciplina_dependencia_cad.php
+++ ieducar/intranet/educar_disciplina_dependencia_cad.php
@@ -226,11 +226,16 @@ class indice extends clsCadastro
 
   function validaQuantidadeDisciplinasDependencia(){
     $db = new clsBanco();
-    $db->consulta("SELECT COALESCE(regra_avaliacao_diferenciada.qtd_disciplinas_dependencia, regra_avaliacao.qtd_disciplinas_dependencia) AS qtd_disciplinas_dependencia
-                     FROM pmieducar.serie
-                     LEFT JOIN modules.regra_avaliacao ON (serie.regra_avaliacao_id = regra_avaliacao.id)
-                     LEFT JOIN modules.regra_avaliacao AS regra_avaliacao_diferenciada ON (serie.regra_avaliacao_diferenciada_id = regra_avaliacao_diferenciada.id)
-                    WHERE serie.cod_serie = {$this->ref_cod_serie} ");
+    $db->consulta("SELECT (CASE
+                              WHEN escola.utiliza_regra_diferenciada THEN regra_avaliacao_diferenciada.qtd_disciplinas_dependencia
+                              ELSE regra_avaliacao.qtd_disciplinas_dependencia
+                          END) AS qtd_disciplinas_dependencia
+                  FROM pmieducar.escola,
+                       pmieducar.serie
+                  LEFT JOIN modules.regra_avaliacao ON (serie.regra_avaliacao_id = regra_avaliacao.id)
+                  LEFT JOIN modules.regra_avaliacao AS regra_avaliacao_diferenciada ON (serie.regra_avaliacao_diferenciada_id = regra_avaliacao_diferenciada.id)
+                  WHERE serie.cod_serie = {$this->ref_cod_serie}
+                    AND escola.cod_escola = {$this->ref_cod_escola}");
 
     $db->ProximoRegistro();
     $m = $db->Tupla();
