3d9e14d87b99f847f43a971236b78b0c9d457d65
Bonot <paula_bonot@hotmail.com>
2016-04-07 14:37:58 -0300

    Altera posição do menu referente ao relatorio de docentes e disciplinas por turma Refs #570

--- ieducar/misc/database/deltas/portabilis/private/135_cria_menu_relatorio_disciplinas_por_docentes_por_turma.sql
+++ ieducar/misc/database/deltas/portabilis/private/135_cria_menu_relatorio_disciplinas_por_docentes_por_turma.sql
@@ -5,7 +5,7 @@
 
 insert into portal.menu_submenu values (999860, 55, 2,'Relação de docentes e disciplinas lecionadas por turma', 'module/Reports/DocentesDisciplinasPorTurma', null, 3);
 insert into portal.menu_funcionario values(1,0,0,999860);
-insert into pmicontrolesis.menu values (999860, 999860, 999300, 'Relação de docentes e disciplinas lecionadas por turma', 0, 'module/Reports/DocentesDisciplinasPorTurma', '_self', 1, 15, 192, 1);
+insert into pmicontrolesis.menu values (999860, 999860, 999302, 'Relação de docentes e disciplinas lecionadas por turma', 0, 'module/Reports/DocentesDisciplinasPorTurma', '_self', 1, 15, 192, 1);
 insert into pmieducar.menu_tipo_usuario values(1,999860,1,0,1);
 
 -- undo
