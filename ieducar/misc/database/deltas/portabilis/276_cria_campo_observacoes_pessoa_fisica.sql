0f3139bc3f407d2217703b5f60dc156f02280a63
Bonot <paula_bonot@hotmail.com>
2016-04-13 14:58:28 -0300

    Cria sql de adição do campo observações no cadastro de pessoa fisica Refs #423

---
+++ ieducar/misc/database/deltas/portabilis/276_cria_campo_observacoes_pessoa_fisica.sql
@@ -0,0 +1,5 @@
+--Cria campo para armazenar observações sobre pessoa fisica no cadastro de clientes no modulo biblioteca.
+
+--@author   Paula Bonot <bonot@portabilis.com.br>
+
+ALTER TABLE cadastro.fisica ADD COLUMN observacoes text;
