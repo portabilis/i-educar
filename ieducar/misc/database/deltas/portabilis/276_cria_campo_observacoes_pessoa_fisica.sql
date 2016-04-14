9cf7d41c7f5a49ab6b2460aa55292c4eac6f8f1e
Bonot <paula_bonot@hotmail.com>
2016-04-14 10:57:29 -0300

    Altera sql para que observações estejam ligadas diretamente ao cliente Refs #423

--- ieducar/misc/database/deltas/portabilis/276_cria_campo_observacoes_pessoa_fisica.sql
+++ ieducar/misc/database/deltas/portabilis/276_cria_campo_observacoes_pessoa_fisica.sql
@@ -1,5 +1,5 @@
---Cria campo para armazenar observações sobre pessoa fisica no cadastro de clientes no modulo biblioteca.
+--Cria campo para armazenar observações no cadastro de clientes no modulo biblioteca.
 
 --@author   Paula Bonot <bonot@portabilis.com.br>
 
-ALTER TABLE cadastro.fisica ADD COLUMN observacoes text;
+ALTER TABLE pmieducar.cliente ADD COLUMN observacoes text;
