8c76a49bc42b30016986c2277594c478d737d970
Caroline Salib Canto <carolinesalibc@gmail.com>
2016-04-25 09:55:38 -0300

    Ajustado para abrir página em PDF mostrando resultado da análise; Refs #723

--- ieducar/modules/Educacenso/Assets/Javascripts/Educacenso.js
+++ ieducar/modules/Educacenso/Assets/Javascripts/Educacenso.js
@@ -1,13 +1,29 @@
 $j(document).ready(function(){
 
-	var modalLoad = '<div id="ex5" class="modal">' +
+	var modalLoad = '<div id="modal_load" class="modal">' +
 					'<div style="float:left;width:100px;">' +
   					'	<img src="http://www.tourismthailand.org/images/loading/load.gif" width="100px" height="100px" alt="">' +
   					'</div>'+
   					'<div style="float:right;width:300px;">'+
-  					'	<p style="margin-left: 20px; margin-top: 40px;font-family: verdana, arial; font-size: 18px;">Analisando as informa&ccedil;&otilde;es</p>' +
+  					'	<p style="margin-left: 20px; margin-top: 30px;font-family: verdana, arial; font-size: 18px;">Analisando as informa&ccedil;&otilde;es</p>' +
+  					'	<p id="registro_load" style="margin-left: 20px; margin-top: 10px;font-family: verdana, arial; font-size: 10px;">Registro 00 teste</p>' +
   					'</div>'+
   					'</div>';
+
+    var paginaResposta = '<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Analize exportação</title></head><body>'+
+						 '<div id="content">'+
+						 '  <h3>Analise de exporta&ccedil;&atilde;o</h3>'+
+						 '  <p>Este e um exemplo de problema relatado pela analise do i-Educar.</p>'+
+						 '</div>'+
+						 '<div id="editor"></div>'+
+						 '</body></html>';
+
+    var doc = new jsPDF();
+    var specialElementHandlers = {
+        '#editor': function (element, renderer) {
+            return true;
+        }
+    };
 
     $j("body").append(modalLoad);
     $j("#btn_enviar").click(function(){
@@ -21,16 +37,17 @@ $j(document).ready(function(){
     		return;
     	}
 
-    	$j("#ex5").modal({
-		  escapeClose: false,
-		  clickClose: false,
-		  showClose: false
-		});
+      doc.fromHTML(paginaResposta, 15, 15, {
+          'width': 170,
+              'elementHandlers': specialElementHandlers
+      });
+      doc.output('dataurlnewwindow');
+
+    	$j("#modal_load").modal({
+  		  escapeClose: false,
+  		  clickClose: false,
+  		  showClose: false
+  		});
     });
-
-    function analizarExportar(){
-
-    }
-
 
 });
