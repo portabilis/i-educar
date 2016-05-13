689220b51484467b47123bcc9fad3689d23fb5ce
Bonot <paula_bonot@hotmail.com>
2016-05-13 17:26:40 -0300

    Corrige problema de duplicidade na chave primaria do menu Refs #871

--- ieducar/misc/database/deltas/portabilis/private/140_cria_menu_documentacao_padrao.sql
+++ ieducar/misc/database/deltas/portabilis/private/140_cria_menu_documentacao_padrao.sql
@@ -4,7 +4,7 @@
 -- @version  $Id$
 
 INSERT INTO portal.menu_submenu VALUES (999861, 57, 2,'Documentação Padrão', 'DocumentacaoPadrao.php', null, 3);
-INSERT INTO pmicontrolesis.menu VALUES (999861, 999861, 21127, 'Documentação Padrão', 0, 'DocumentacaoPadrao.php', '_self', 1, 15, 21);
+INSERT INTO pmicontrolesis.menu VALUES (999865, 999861, 21127, 'Documentação Padrão', 0, 'DocumentacaoPadrao.php', '_self', 1, 15, 21);
 INSERT INTO pmieducar.menu_tipo_usuario VALUES(1,999861,1,1,1);
 
 -- undo
