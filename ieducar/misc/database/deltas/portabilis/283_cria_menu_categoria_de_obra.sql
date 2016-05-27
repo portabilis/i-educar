1b94ca56cba42b579ef635ae057941e4317be732
Mauricio Citadini Bilessimo <pariulon@hotmail.com>
2016-05-27 16:18:43 -0300

    Corrigido inserção de menu para categoria de obras; Refs #437

--- ieducar/misc/database/deltas/portabilis/283_cria_menu_categoria_de_obra.sql
+++ ieducar/misc/database/deltas/portabilis/283_cria_menu_categoria_de_obra.sql
@@ -1,7 +1,7 @@
 -- Cria menu categoria de obras
 -- @author Maurício Citadini Biléssimo <mauricio@portabilis.com.br>
 
-insert into pmicontrolesis.menu values(999865, 598, 15858, 'Categoria', 0, 'educar_categoria_lst.php', '_self', 1, 16, 1, null);
-insert into portal.menu_submenu values(999866,57,2,'Categoria de obras','educar_categoria_lst.php',NULL,3);
-insert into pmicontrolesis.menu values(999866,999866,999865,'Categoria de obras',0,'educar_categoria_lst.php','_self',1,16,1);
-insert into pmieducar.menu_tipo_usuario values(1,999866,1,1,1);
+insert into pmicontrolesis.menu values(999866, 598, 15858, 'Categoria', 0, 'educar_categoria_lst.php', '_self', 1, 16, 1, null);
+insert into portal.menu_submenu values(999867,57,2,'Categoria de obras','educar_categoria_lst.php',NULL,3);
+insert into pmicontrolesis.menu values(999867,999867,999866,'Categoria de obras',0,'educar_categoria_lst.php','_self',1,16,1);
+insert into pmieducar.menu_tipo_usuario values(1,999867,1,1,1);
