565f1f20dd08fbbf127df8acda89be452332c560
Bonot <paula_bonot@hotmail.com>
2016-06-22 16:33:44 -0300

    Corrige primarias do menu de exportação de usuarios Refs #1014

--- ieducar/misc/database/deltas/portabilis/private/143_cria_menu_exportar_usuarios.sql
+++ ieducar/misc/database/deltas/portabilis/private/143_cria_menu_exportar_usuarios.sql
@@ -3,10 +3,10 @@
 -- @license  @@license@@
 -- @version  $Id$
 
-INSERT INTO portal.menu_submenu VALUES (999862, 25, 2,'Exporta&ccedil;&atilde;o de Usu&aacute;rios', 'educar_exportacao_usuarios.php', NULL, 3);
-INSERT INTO pmieducar.menu_tipo_usuario VALUES(1,999862,1,1,1);
+INSERT INTO portal.menu_submenu VALUES (999869, 25, 2,'Exporta&ccedil;&atilde;o de Usu&aacute;rios', 'educar_exportacao_usuarios.php', NULL, 3);
+INSERT INTO pmieducar.menu_tipo_usuario VALUES(1,999869,1,1,1);
 
 --Undo
 
-DELETE FROM pmieducar.menu_tipo_usuario WHERE ref_cod_menu_submenu = 999862;
-DELETE FROM portal.menu_submenu WHERE cod_menu_submenu = 999862;
+DELETE FROM pmieducar.menu_tipo_usuario WHERE ref_cod_menu_submenu = 999869;
+DELETE FROM portal.menu_submenu WHERE cod_menu_submenu = 999869;
