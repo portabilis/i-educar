1adf816a92702db6a373f0bd01ac287dd63b0f7f
Bonot <paula_bonot@hotmail.com>
2016-06-29 09:03:14 -0300

    Altera menu de exportação de usuarios Refs #1014

---
+++ ieducar/misc/database/deltas/portabilis/private/144_altera_menu_exportar_usuarios.sql
@@ -0,0 +1,6 @@
+-- Altera menu de exportação de usuários
+-- @author   Paula Bonot <bonot@portabilis.com.br>
+-- @license  @@license@@
+-- @version  $Id$
+
+UPDATE portal.menu_submenu SET nm_submenu = 'Exporta&ccedil;&atilde;o de usu&aacute;rios' WHERE cod_menu_submenu = 999869
