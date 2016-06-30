17dad550fb888fffec6a6b19ea54aaa4ea77f5c8
Caroline <carolinesalibc@gmail.com>
2016-06-30 10:31:58 -0300

    Corrigido função que remove acentos no arquivo de exportação; Refs #1041

--- ieducar/modules/Api/Views/EducacensoExportController.php
+++ ieducar/modules/Api/Views/EducacensoExportController.php
@@ -1672,9 +1672,22 @@ protected function exportaDadosRegistro70($escolaId, $ano, $data_ini, $data_fim,
   }
 
   protected function upperAndUnaccent($string){
-    $string = strtr($string,'àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ','aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
+    $string = Portabilis_String_Utils::toUtf8($string);
+    $string = preg_replace(array("/(á|à|ã|â|ä)/",
+                                 "/(Á|À|Ã|Â|Ä)/",
+                                 "/(é|è|ê|ë)/",
+                                 "/(É|È|Ê|Ë)/",
+                                 "/(í|ì|î|ï)/",
+                                 "/(Í|Ì|Î|Ï)/",
+                                 "/(ó|ò|õ|ô|ö)/",
+                                 "/(Ó|Ò|Õ|Ô|Ö)/",
+                                 "/(ú|ù|û|ü)/",
+                                 "/(Ú|Ù|Û|Ü)/",
+                                 "/(ñ)/","/(Ñ)/",
+                                 "/(ç)/","/(Ç)/"),
+                            explode(" ","a A e E i I o O u U n N c C"), $string);
 
-    return strtoupper(str_replace('?', '', $string));
+    return strtoupper($string);
   }
 
   public function Gerar() {
