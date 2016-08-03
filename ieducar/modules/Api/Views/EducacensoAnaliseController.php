ca3be50b786ec751d11028ec0677ab0877e30cd8
Caroline Salib <carolinesalibc@gmail.com>
2016-08-03 16:29:28 -0300

    Alterado validação para considerar só enturmações ativas; Refs #1233

--- ieducar/modules/Api/Views/EducacensoAnaliseController.php
+++ ieducar/modules/Api/Views/EducacensoAnaliseController.php
@@ -970,6 +970,7 @@ class EducacensoAnaliseController extends ApiCoreController
                AND turma.visivel = TRUE
                AND COALESCE(turma.nao_informar_educacenso, 0) = 0
                AND matricula.ativo = 1
+               AND matricula_turma.ativo = 1
                AND matricula.ano = $1
                AND escola.cod_escola = $2
                AND COALESCE(matricula.data_matricula,matricula.data_cadastro) BETWEEN DATE($3) AND DATE($4)
@@ -1064,6 +1065,7 @@ class EducacensoAnaliseController extends ApiCoreController
                AND turma.visivel = TRUE
                AND COALESCE(turma.nao_informar_educacenso, 0) = 0
                AND matricula.ativo = 1
+               AND matricula_turma.ativo = 1
                AND matricula.ano = $1
                AND escola.cod_escola = $2
                AND COALESCE(matricula.data_matricula,matricula.data_cadastro) BETWEEN DATE($3) AND DATE($4)
@@ -1177,6 +1179,7 @@ class EducacensoAnaliseController extends ApiCoreController
                AND turma.visivel = TRUE
                AND COALESCE(turma.nao_informar_educacenso, 0) = 0
                AND matricula.ativo = 1
+               AND matricula_turma.ativo = 1
                AND matricula.ano = $1
                AND escola.cod_escola = $2
                AND COALESCE(matricula.data_matricula,matricula.data_cadastro) BETWEEN DATE($3) AND DATE($4)
