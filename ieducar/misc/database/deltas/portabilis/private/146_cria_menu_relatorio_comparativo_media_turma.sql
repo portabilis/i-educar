7055aa8d73801cf360686d2600009aa621f42bf6
Mauricio Citadini Bilessimo <pariulon@hotmail.com>
2016-08-12 15:28:16 -0300

    Desenvolvido relatório comparativo de média por turma; Refs #934

---
+++ ieducar/misc/database/deltas/portabilis/private/146_cria_menu_relatorio_comparativo_media_turma.sql
@@ -0,0 +1,7 @@
+-- @author   Maurício Citadini Biléssimo <mauricio@portabilis.com.br>
+-- @license  @@license@@
+-- @version  $Id$
+
+insert into portal.menu_submenu values(999872,55,2,'Relatório comparativo de média da turma','module/Reports/RelatorioComparativoMediaTurma',NULL,3);
+insert into pmicontrolesis.menu values(999872,999872,999300,'Relatório comparativo de média da turma',0,'module/Reports/RelatorioComparativoMediaTurma','_self',1,15,192);
+insert into pmieducar.menu_tipo_usuario values(1,999872,1,1,1);
