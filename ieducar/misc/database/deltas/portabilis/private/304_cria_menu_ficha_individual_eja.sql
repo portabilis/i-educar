6b5e42d54f2be7c6f08751da6f8d54c4802da9a0
Paula Bonot <paula_bonot@hotmail.com>
2016-09-20 13:43:02 -0300

    Corrije sql de inserção do menu de ficha individual EJA Refs #1241

--- ieducar/misc/database/deltas/portabilis/private/304_cria_menu_ficha_individual_eja.sql
+++ ieducar/misc/database/deltas/portabilis/private/304_cria_menu_ficha_individual_eja.sql
@@ -1,8 +1,8 @@
 -- Cria menu Ficha individual - EJA
 -- @author Paula Bonot <bono@portabilis.com.br>
 
-INSERT INTO pmicontrolesis.menu VALUES(999876,999876,999450,'Ficha individual - EJA',0,'module/Reports/FichaIndividualEJA','_self',1,15,192);
 INSERT INTO portal.menu_submenu VALUES(999876,55,2,'Ficha individual - EJA','module/Reports/FichaIndividualEJA',NULL,3);
+INSERT INTO pmicontrolesis.menu VALUES(999876,999876,999450,'Ficha individual - EJA',0,'module/Reports/FichaIndividualEJA','_self',1,15,192);
 INSERT INTO pmieducar.menu_tipo_usuario VALUES(1,999876,1,1,1);
 
 --UNDO
