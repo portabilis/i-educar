c7c36e2003690773b7d4d59991e543ce7dc896ab
Caroline Salib <carolinesalibc@gmail.com>
2016-09-20 14:55:59 -0300

    Adicionado campo updated_at na tabela de matricula; Refs #1428

--- ieducar/misc/database/deltas/portabilis/305_cria_campo_updated_at_em_matricula_turma.sql
+++ ieducar/misc/database/deltas/portabilis/305_cria_campo_updated_at_em_matricula_turma.sql
@@ -12,3 +12,16 @@ CREATE TRIGGER trigger_updated_at_matricula_turma
 BEFORE
 UPDATE ON pmieducar.matricula_turma
 FOR EACH ROW EXECUTE PROCEDURE pmieducar.updated_at_matricula_turma();
+
+
+-- adiciona coluna na tabela matricula
+ALTER TABLE pmieducar.matricula ADD COLUMN updated_at TIMESTAMP;
+
+-- adiciona trigger para atualizar a coluna updated_at
+CREATE OR REPLACE FUNCTION pmieducar.updated_at_matricula() RETURNS TRIGGER AS $$ BEGIN NEW.updated_at = now(); RETURN NEW; END; $$ LANGUAGE 'plpgsql';
+
+
+CREATE TRIGGER trigger_updated_at_matricula
+BEFORE
+UPDATE ON pmieducar.matricula
+FOR EACH ROW EXECUTE PROCEDURE pmieducar.updated_at_matricula();
