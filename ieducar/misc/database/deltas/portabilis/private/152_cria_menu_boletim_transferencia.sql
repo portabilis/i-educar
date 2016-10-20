5300ca7aaed7058f89b1901d12b7c76002c4c54f
Mauricio Citadini Bilessimo <pariulon@hotmail.com>
2016-10-20 09:24:45 -0200

    Criado boletim de alunos transferidos; Refs #718

---
+++ ieducar/misc/database/deltas/portabilis/private/152_cria_menu_boletim_transferencia.sql
@@ -0,0 +1,9 @@
+-- Cria menu para relatório de boletim de transferência
+--
+-- @author   Maurício Citadini Biléssimo <mauricio@portabilis.com.br>
+-- @license  @@license@@
+-- @version  $Id$
+
+insert into portal.menu_submenu values (999880, 55, 2,'Boletim de transferência', 'module/Reports/BoletimTransferencia', null, 3);
+insert into pmicontrolesis.menu values (999880, 999880, 999450, 'Boletim de transferência', 0, 'module/Reports/BoletimTransferencia', '_self', 1, 15, 192, null);
+insert into pmieducar.menu_tipo_usuario values(1,999880,1,1,1);
---
+++ ieducar/modules/DynamicInput/Views/TransferidoController.php
@@ -0,0 +1,111 @@
+<?php
+
+#error_reporting(E_ALL);
+#ini_set("display_errors", 1);
+
+/**
+ * i-Educar - Sistema de gestão escolar
+ *
+ * Copyright (C) 2006  Prefeitura Municipal de Itajaí
+ *     <ctima@itajai.sc.gov.br>
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+ * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+ * qualquer versão posterior.
+ *
+ * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
+ * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+ * do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
+ * com este programa; se não, escreva para a Free Software Foundation, Inc., no
+ * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * @author    Lucas D'Avila <lucasdavila@portabilis.com.br>
+ * @category  i-Educar
+ * @license   @@license@@
+ * @package   Avaliacao
+ * @subpackage  Modules
+ * @since   Arquivo disponível desde a versão ?
+ * @version   $Id$
+ */
+
+require_once 'lib/Portabilis/Controller/ApiCoreController.php';
+require_once 'intranet/include/pmieducar/clsPmieducarMatricula.inc.php';
+
+/**
+ * MatriculaTransferidoController class.
+ *
+ * @author     Maurício Citadini Biléssimo <mauricio@portabilis.com.br>
+ * @category    i-Educar
+ * @license     @@license@@
+ * @package     Avaliacao
+ * @subpackage  Modules
+ * @since       Classe disponível desde a versão 1.1.0
+ * @version     @@package_version@@
+ */
+class TransferidoController extends ApiCoreController
+{
+
+  protected function canGetTransferido() {
+    return $this->validatesId('turma') &&
+           $this->validatesPresenceOf('ano');
+  }
+
+  protected function getTransferido() {
+    if ($this->canGetTransferido()) {
+      $matriculas = new clsPmieducarMatricula();
+      $matriculas->setOrderby("sequencial_fechamento , translate(nome,'".Portabilis_String_Utils::toLatin1(åáàãâäéèêëíìîïóòõôöúùüûçÿýñÅÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÔÖÚÙÛÜÇÝÑ)."', '".Portabilis_String_Utils::toLatin1(aaaaaaeeeeiiiiooooouuuucyynAAAAAAEEEEIIIIOOOOOUUUUCYN)."') ");
+      $matriculas = $matriculas->lista_transferidos(NULL,
+                                       NULL,
+                                       $this->getRequest()->escola_id,
+                                       $this->getRequest()->serie_id,
+                                       NULL,
+                                       NULL,
+                                       $this->getRequest()->aluno_id,
+                                       '4',
+                                       NULL,
+                                       NULL,
+                                       NULL,
+                                       NULL,
+                                       $ativo = 1,
+                                       $this->getRequest()->ano,
+                                       NULL,
+                                       $this->getRequest()->instituicao_id,
+                                       NULL,
+                                       NULL,
+                                       NULL,
+                                       NULL,
+                                       NULL,
+                                       NULL,
+                                       NULL,
+                                       NULL,
+                                       $this->getRequest()->curso_id,
+                                       NULL,
+                                       $this->getRequest()->matricula_id,
+                                       NULL,
+                                       NULL,
+                                       NULL,
+                                       NULL,
+                                       $this->getRequest()->turma_id,
+                                       NULL,
+                                       false); // Mostra alunos em abandono/transferidos se não existir nenhuma matricula_turma ativa pra outra turma
+
+      $options = array();
+
+      foreach ($matriculas as $matricula)
+        $options['__' . $matricula['cod_matricula']] = $this->toUtf8($matricula['nome']);
+
+      return array('options' => $options);
+    }
+  }
+
+  public function Gerar() {
+    if ($this->isRequestFor('get', 'transferidos'))
+      $this->appendResponse($this->getTransferido());
+    else
+      $this->notImplementedOperationError();
+  }
+}
