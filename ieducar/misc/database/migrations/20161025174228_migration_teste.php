530779ac16f261848cbd2146ebb8a61b8402f1a0
Caroline Salib <carolinesalibc@gmail.com>
2016-10-25 15:49:24 -0200

    Ajustado migração de teste; Refs #1683

--- ieducar/misc/database/migrations/20161025174228_migration_teste.php
+++ ieducar/misc/database/migrations/20161025174228_migration_teste.php
@@ -6,7 +6,7 @@ class MigrationTeste extends AbstractMigration
 {
     public function up()
     {
-        $count = $this->execute('update portal.funcionario set email = 'teste@phinx' where ref_cod_pessoa_fj = 1');
+        $count = $this->execute("update portal.funcionario set email = 'teste@phinx' where ref_cod_pessoa_fj = 1");
     }
 
     public function down()
