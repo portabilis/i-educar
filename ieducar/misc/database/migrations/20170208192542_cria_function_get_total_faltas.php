889900d557f711a2518d56bfcb22308bdb1378af
Caroline Salib <carolinesalibc@gmail.com>
2017-02-09 09:46:10 -0200

    Refatorado boletim portabilis default; Refs #2212

---
+++ ieducar/misc/database/migrations/20170208192542_cria_function_get_total_faltas.php
@@ -0,0 +1,20 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CriaFunctionGetTotalFaltas extends AbstractMigration
+{
+    public function up()
+    {
+        $this->execute("CREATE OR REPLACE FUNCTION relatorio.get_total_faltas(matricula_i integer) RETURNS NUMERIC AS $$
+                        BEGIN
+                        RETURN (SELECT sum(falta_geral.quantidade)
+                                FROM modules.falta_geral,
+                                        modules.falta_aluno
+                                WHERE falta_geral.falta_aluno_id = falta_aluno.id AND
+                                        falta_aluno.matricula_id = matricula_i AND
+                                        falta_aluno.tipo_falta = 1 AND
+                                        falta_geral.etapa in ('1','2','3','4'));
+                        END; $$ LANGUAGE plpgsql;");
+    }
+}
