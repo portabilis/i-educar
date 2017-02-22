7f67222c92c99a6e8eefa695f38a5e236d0427c6
Caroline Salib <carolinesalibc@gmail.com>
2017-02-22 09:36:53 -0300

    Movido js para arquivo separado; Refs #2314

--- ieducar/intranet/educar_usuario_cad.php
+++ ieducar/intranet/educar_usuario_cad.php
@@ -391,6 +391,10 @@ class indice extends clsCadastro
 		$this->inputsHelper()->multipleSearchEscola(null, array('label' => 'Escola(s)',
 																'required' => false));
 
+	    $scripts = array('/modules/Cadastro/Assets/Javascripts/Usuario.js');
+
+	    Portabilis_View_Helper_Application::loadJavascript($this, $scripts);
+
 		$this->acao_enviar = "valida()";
 
 	}
@@ -629,60 +633,3 @@ $pagina->addForm( $miolo );
 // gera o html
 $pagina->MakeAll();
 ?>
-<script>
-
-var campo_tipo_usuario = $j("#ref_cod_tipo_usuario");
-var campo_instituicao = $j("#ref_cod_instituicao");
-var campo_escola = $j("#escola");
-
-function habilitaCampos()
-{
-	var nivel = cod_tipo_usuario[campo_tipo_usuario.val()];
-
-	if( nivel == 1 )
-	{
-		campo_instituicao.prop( "disabled", false );
-		campo_escola.prop( "disabled", false );
-	}
-	else if( nivel == 2 )
-	{
-		campo_instituicao.prop( "disabled", false );
-		campo_escola.prop( "disabled", true );
-	}
-	else if( nivel == 4 )
-	{
-		campo_instituicao.prop( "disabled", false );
-		campo_escola.prop( "disabled", false );
-	}
-	else if( nivel == 8 )
-	{
-		campo_instituicao.prop( "disabled", true );
-		campo_escola.prop( "disabled", true );
-	}
-  campo_escola.trigger("chosen:updated");
-}
-
-function valida()
-{
-	if( campo_instituicao.is(':enabled') && campo_instituicao.val() == "" )
-	{
-		alert("É obrigatório a escolha de uma Instituição!");
-		return false;
-	}
-	else if( campo_escola.is(':enabled') && campo_escola.val() == null )
-	{
-		alert("É obrigatório a escolha de uma Escola! ");
-		return false;
-	}
-	if(!acao())
-		return;
-	document.forms[0].submit();
-}
-
-campo_tipo_usuario.change(function(){
-	habilitaCampos();
-});
-
-habilitaCampos();
-
-</script>
