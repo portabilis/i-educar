c06e37003dff41c3b7637f39682221079e233acd
MatheusNicoski <matheusnicoski@gmail.com>
2017-03-23 09:42:10 -0300

    Ajusta texto de busca; Refs #1333

--- ieducar/misc/database/migrations/20170314172558_novo_menu_aluno_por_escola_setor.php
+++ ieducar/misc/database/migrations/20170314172558_novo_menu_aluno_por_escola_setor.php
@@ -6,8 +6,8 @@ class NovoMenuAlunoPorEscolaSetor extends AbstractMigration
 {
    public function up()
     {
-        $this->execute("INSERT INTO portal.menu_submenu VALUES (999890, 55, 2,'Relat&oacute;rio de alunos por escola e setor', 'module/Reports/AlunoSetor', null, 3);");
-        $this->execute("INSERT INTO pmicontrolesis.menu VALUES (999890, 999890, 999300, 'Relat&oacute;rio de alunos por escola e setor', 0, 'module/Reports/AlunoSetor', '_self', 1, 15, 192, 1);");
+        $this->execute("INSERT INTO portal.menu_submenu VALUES (999890, 55, 2,'Relatório de alunos por escola e setor', 'module/Reports/AlunoSetor', null, 3);");
+        $this->execute("INSERT INTO pmicontrolesis.menu VALUES (999890, 999890, 999300, 'Relatório de alunos por escola e setor', 0, 'module/Reports/AlunoSetor', '_self', 1, 15, 192, 1);");
         $this->execute("INSERT INTO pmieducar.menu_tipo_usuario VALUES(1,999890,1,1,1);");
     }
 
