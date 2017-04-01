699cb76d102e1739ea6bd7394b1bc681f891ffa6
Caroline Salib <carolinesalibc@gmail.com>
2017-04-01 02:59:35 -0300

    Ajustado erro de sintaxe em migração; Refs #2583

--- ieducar/misc/database/migrations/20170401043517_ajusta_ordem_menu_lateral.php
+++ ieducar/misc/database/migrations/20170401043517_ajusta_ordem_menu_lateral.php
@@ -6,7 +6,7 @@ class AjustaOrdemMenuLateral extends AbstractMigration
 {
     public function up()
     {
-        $this->execute->("UPDATE portal.menu_menu SET ord_menu = 9 WHERE cod_menu_menu = 25;
+        $this->execute("UPDATE portal.menu_menu SET ord_menu = 9 WHERE cod_menu_menu = 25;
                           UPDATE portal.menu_menu SET ord_menu = 6 WHERE cod_menu_menu = 70;
                           UPDATE portal.menu_menu SET ord_menu = 5 WHERE cod_menu_menu = 71;");
     }
