8268c0c48354c9b7851363b4290f1e67912c49e5
Caroline Salib <carolinesalibc@gmail.com>
2017-04-01 00:37:45 -0300

    Ajustado migrations para deploy; Refs #2583

--- ieducar/misc/database/migrations/20170331190101_ajusta_menus_modulo_transporte.php
+++ ieducar/misc/database/migrations/20170331190101_ajusta_menus_modulo_transporte.php
@@ -10,15 +10,15 @@ class AjustaMenusModuloTransporte extends AbstractMigration
                       INSERT INTO pmicontrolesis.menu (cod_menu, tt_menu, ord_menu, alvo, suprime_menu, ref_cod_tutormenu, ref_cod_ico)
                       VALUES (9998846, 'Documentos', 4, '_self', 1, 17, 1);
                       UPDATE pmicontrolesis.menu SET ref_cod_menu_pai = 9998846 WHERE cod_menu = 21241;
-                      UPDATE menu_submenu SET nm_submenu = 'Relatório de motoristas do transporte' WHERE cod_menu_submenu = 21252;
+                      UPDATE portal.menu_submenu SET nm_submenu = 'Relatório de motoristas do transporte' WHERE cod_menu_submenu = 21252;
                       UPDATE pmicontrolesis.menu SET tt_menu = 'Relatório de motoristas do transporte' WHERE cod_menu = 21252;
-                      UPDATE menu_submenu SET nm_submenu = 'Relatório de usuários do transporte' WHERE cod_menu_submenu = 999825;
+                      UPDATE portal.menu_submenu SET nm_submenu = 'Relatório de usuários do transporte' WHERE cod_menu_submenu = 999825;
                       UPDATE pmicontrolesis.menu SET tt_menu = 'Relatório de usuários do transporte' WHERE cod_menu = 999825;
-                      UPDATE menu_submenu SET nm_submenu = 'Relatório de rotas do transporte' WHERE cod_menu_submenu = 21242;
+                      UPDATE portal.menu_submenu SET nm_submenu = 'Relatório de rotas do transporte' WHERE cod_menu_submenu = 21242;
                       UPDATE pmicontrolesis.menu SET tt_menu = 'Relatório de rotas do transporte' WHERE cod_menu = 21242;
-                      UPDATE menu_submenu SET nm_submenu = 'Relatório de alunos do transporte' WHERE cod_menu_submenu = 21249;
+                      UPDATE portal.menu_submenu SET nm_submenu = 'Relatório de alunos do transporte' WHERE cod_menu_submenu = 21249;
                       UPDATE pmicontrolesis.menu SET tt_menu = 'Relatório de alunos do transporte' WHERE cod_menu = 21249;
-                      UPDATE menu_submenu SET nm_submenu = 'Relatório de usuários do transporte por empresa' WHERE cod_menu_submenu = 21243;
+                      UPDATE portal.menu_submenu SET nm_submenu = 'Relatório de usuários do transporte por empresa' WHERE cod_menu_submenu = 21243;
                       UPDATE pmicontrolesis.menu SET tt_menu = 'Relatório de usuários do transporte por empresa' WHERE cod_menu = 21243;
                       INSERT INTO pmicontrolesis.menu VALUES (9998847, NULL, 20712, 'Cadastrais', 1, null, '_self', 1, 17);
                       UPDATE pmicontrolesis.menu SET ref_cod_menu_pai = 9998847 WHERE cod_menu IN (21252, 999825, 21242, 21249, 21243);");
