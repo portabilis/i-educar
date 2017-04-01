934d27afa3b1f0d457fb088e96b5a35a4924563f
bonot <paula_bonot@hotmail.com>
2017-04-01 01:39:53 -0300

    Ajusta ordem do menu lateral; Refs #2578

---
+++ ieducar/misc/database/migrations/20170401043517_ajusta_ordem_menu_lateral.php
@@ -0,0 +1,13 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AjustaOrdemMenuLateral extends AbstractMigration
+{
+    public function up()
+    {
+        $this->execute->("UPDATE portal.menu_menu SET ord_menu = 9 WHERE cod_menu_menu = 25;
+                          UPDATE portal.menu_menu SET ord_menu = 6 WHERE cod_menu_menu = 70;
+                          UPDATE portal.menu_menu SET ord_menu = 5 WHERE cod_menu_menu = 71;");
+    }
+}
