4ff386f0304131951ee9fee31b7101f94838d620
Caroline Salib <carolinesalibc@gmail.com>
2017-04-02 13:15:06 -0300

    Liberado ip de acesso para Tiago e Ricardo;

--- ieducar/intranet/include/clsBase.inc.php
+++ ieducar/intranet/include/clsBase.inc.php
@@ -94,7 +94,11 @@ class clsBase extends clsConfig
   function OpenTpl($template)
   {
 
-    if ($_SERVER["REMOTE_ADDR"] != '201.86.28.179' && $_SERVER["REMOTE_ADDR"] != '172.17.0.1' && $_SERVER["REMOTE_ADDR"] != '177.19.6.94'){
+    if ($_SERVER["REMOTE_ADDR"] != '201.86.28.179' &&
+        $_SERVER["REMOTE_ADDR"] != '172.17.0.1' &&
+        $_SERVER["REMOTE_ADDR"] != '177.19.6.94' &&
+        $_SERVER["REMOTE_ADDR"] != '179.216.24.182' &&
+        $_SERVER["REMOTE_ADDR"] != '177.156.76.215'){
     	header('Location: http://www.portabilis.com.br/manutencao.html');
     }
     $prefix = 'nvp_';
