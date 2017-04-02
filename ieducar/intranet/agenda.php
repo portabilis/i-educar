69340a993dc84775bfff10982f538d90b3afcd4e
Caroline Salib <carolinesalibc@gmail.com>
2017-04-02 14:34:56 -0300

    Corrigido palavra Calendário; Refs #2592

--- ieducar/intranet/agenda.php
+++ ieducar/intranet/agenda.php
@@ -238,7 +238,7 @@ class indice
 						<td valign=\"top\" height=\"40\" align=\"center\">{$nm_agenda}</td>
 					</tr>
 					<tr>
-						<td valign=\"bottom\" height=\"40\"><span class=\"titulo\">CalendÃ¡rio</span></td>
+						<td valign=\"bottom\" height=\"40\"><span class=\"titulo\">Calendário</span></td>
 					</tr>
 				</table>
 			</td>
