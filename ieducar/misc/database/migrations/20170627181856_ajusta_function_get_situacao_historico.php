40dd8edc2a9eb9c6e8099a896c83ff6385ed9b08
Caroline Salib <carolinesalibc@gmail.com>
2017-06-27 15:36:38 -0300

    Ajustado migração da situação de histórico para Ap. Depen.; Refs #3160

--- ieducar/misc/database/migrations/20170627181856_ajusta_function_get_situacao_historico.php
+++ ieducar/misc/database/migrations/20170627181856_ajusta_function_get_situacao_historico.php
@@ -14,7 +14,7 @@ class AjustaFunctionGetSituacaoHistorico extends AbstractMigration
                                 WHEN situacao = 4 THEN 'Transferido'::character varying
                                 WHEN situacao = 5 THEN 'Reclassificado'::character varying
                                 WHEN situacao = 6 THEN 'Abandono'::character varying
-                                WHEN situacao = 12 THEN 'Aprovado dependência'::character varying
+                                WHEN situacao = 12 THEN 'Ap. Depen.'::character varying
                                 WHEN situacao = 13 THEN 'Aprovado conselho'::character varying
                                 WHEN situacao = 14 THEN 'Reprovado por faltas'::character varying
                                 ELSE ''::character varying
