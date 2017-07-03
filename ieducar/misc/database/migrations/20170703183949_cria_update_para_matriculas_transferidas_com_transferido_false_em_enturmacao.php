410f3434651812b245ec2712fa48b075a6b7e4f2
Matheus Nicoski Damasio <matheus@portabilis.com.br>
2017-07-03 15:56:52 -0300

    Cria update para matriculas Transferidas com transferencia False; Refs #2754

---
+++ ieducar/misc/database/migrations/20170703183949_cria_update_para_matriculas_transferidas_com_transferido_false_em_enturmacao.php
@@ -0,0 +1,18 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CriaUpdateParaMatriculasTransferidasComTransferidoFalseEmEnturmacao extends AbstractMigration
+{
+    public function up()
+    {
+        $this->execute("UPDATE pmieducar.matricula_turma 
+                           SET transferido = TRUE
+                          FROM pmieducar.matricula
+                         WHERE matricula.cod_matricula = matricula_turma.ref_cod_matricula
+                           AND matricula.aprovado = 4
+                           AND matricula_turma.sequencial = relatorio.get_max_sequencial_matricula(matricula_turma.ref_cod_matricula)
+                           AND (matricula_turma.transferido = FALSE OR matricula_turma.transferido IS NULL)
+                           AND matricula.ano = 2017");
+    }
+}
