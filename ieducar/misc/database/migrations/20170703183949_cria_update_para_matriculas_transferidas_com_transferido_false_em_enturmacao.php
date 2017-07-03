2b61d7c9da1a9e7d612919d8aabbb78746a1b951
Matheus Nicoski Damasio <matheus@portabilis.com.br>
2017-07-03 16:26:35 -0300

    Ajusta script para corrigir em todos os anos; Refs #2754

--- ieducar/misc/database/migrations/20170703183949_cria_update_para_matriculas_transferidas_com_transferido_false_em_enturmacao.php
+++ ieducar/misc/database/migrations/20170703183949_cria_update_para_matriculas_transferidas_com_transferido_false_em_enturmacao.php
@@ -12,7 +12,6 @@ class CriaUpdateParaMatriculasTransferidasComTransferidoFalseEmEnturmacao extend
                          WHERE matricula.cod_matricula = matricula_turma.ref_cod_matricula
                            AND matricula.aprovado = 4
                            AND matricula_turma.sequencial = relatorio.get_max_sequencial_matricula(matricula_turma.ref_cod_matricula)
-                           AND (matricula_turma.transferido = FALSE OR matricula_turma.transferido IS NULL)
-                           AND matricula.ano = 2017");
+                           AND (matricula_turma.transferido = FALSE OR matricula_turma.transferido IS NULL)");
     }
 }
