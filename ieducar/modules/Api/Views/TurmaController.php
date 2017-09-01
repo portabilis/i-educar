72e8864fe82acb964f58a577ed4b66f5e40f46dd
Caroline Salib <carolinesalibc@gmail.com>
2017-08-31 22:15:42 -0300

    Ajustado APi para solicitar id da turma;

--- ieducar/modules/Api/Views/TurmaController.php
+++ ieducar/modules/Api/Views/TurmaController.php
@@ -83,35 +83,43 @@ class TurmaController extends ApiCoreController
     return array('turmas' => $turmas);
   }
 
-  protected function getTurmasDisciplinas() {
-    $sql = "(SELECT escola_serie_disciplina.ref_cod_disciplina AS id,
-              turma.cod_turma,
-              componente_curricular.nome,
-              componente_curricular.abreviatura,
-              componente_curricular.area_conhecimento_id,
-              escola_serie_disciplina.carga_horaria
-            FROM turma
-              JOIN escola_serie_disciplina ON escola_serie_disciplina.ref_ref_cod_serie = turma.ref_ref_cod_serie AND escola_serie_disciplina.ref_ref_cod_escola = turma.ref_ref_cod_escola AND escola_serie_disciplina.ativo = 1
-              JOIN componente_curricular ON componente_curricular.id = escola_serie_disciplina.ref_cod_disciplina AND (( SELECT count(cct.componente_curricular_id) AS count
-                    FROM componente_curricular_turma cct
-                    WHERE cct.turma_id = turma.cod_turma)) = 0
-              JOIN area_conhecimento ON area_conhecimento.id = componente_curricular.area_conhecimento_id
-            WHERE turma.ativo = 1
-            ORDER BY area_conhecimento.nome, componente_curricular.nome)
-          UNION ALL
-          ( SELECT componente_curricular_turma.componente_curricular_id AS id,
-              componente_curricular_turma.turma_id AS cod_turma,
-              componente_curricular.nome,
-              componente_curricular.abreviatura,
-              componente_curricular.area_conhecimento_id,
-              componente_curricular_turma.carga_horaria
-            FROM componente_curricular_turma
-              JOIN componente_curricular ON componente_curricular.id = componente_curricular_turma.componente_curricular_id
-              JOIN area_conhecimento ON area_conhecimento.id = componente_curricular.area_conhecimento_id
-            ORDER BY area_conhecimento.nome, componente_curricular.nome);";
+  protected function canGetTurmasDisciplinas() {
+    return $this->validatesId('turma');
+  }
 
-    $turmasDisciplinas = $this->fetchPreparedQuery($sql);
-    return array('turmas-disciplinas' => $turmasDisciplinas);
+  protected function getTurmasDisciplinas() {
+    if ($this->canGetTurmasDisciplinas()) {
+      $sql = "SELECT *
+              FROM ((SELECT escola_serie_disciplina.ref_cod_disciplina AS id,
+                            turma.cod_turma,
+                            componente_curricular.nome,
+                            componente_curricular.abreviatura,
+                            componente_curricular.area_conhecimento_id,
+                            escola_serie_disciplina.carga_horaria
+                        FROM turma
+                          JOIN escola_serie_disciplina ON escola_serie_disciplina.ref_ref_cod_serie = turma.ref_ref_cod_serie AND escola_serie_disciplina.ref_ref_cod_escola = turma.ref_ref_cod_escola AND escola_serie_disciplina.ativo = 1
+                          JOIN componente_curricular ON componente_curricular.id = escola_serie_disciplina.ref_cod_disciplina AND (( SELECT count(cct.componente_curricular_id) AS count
+                                FROM componente_curricular_turma cct
+                                WHERE cct.turma_id = turma.cod_turma)) = 0
+                          JOIN area_conhecimento ON area_conhecimento.id = componente_curricular.area_conhecimento_id
+                        WHERE turma.ativo = 1
+                        ORDER BY area_conhecimento.nome, componente_curricular.nome)
+                      UNION ALL
+                      ( SELECT componente_curricular_turma.componente_curricular_id AS id,
+                          componente_curricular_turma.turma_id AS cod_turma,
+                          componente_curricular.nome,
+                          componente_curricular.abreviatura,
+                          componente_curricular.area_conhecimento_id,
+                          componente_curricular_turma.carga_horaria
+                        FROM componente_curricular_turma
+                          JOIN componente_curricular ON componente_curricular.id = componente_curricular_turma.componente_curricular_id
+                          JOIN area_conhecimento ON area_conhecimento.id = componente_curricular.area_conhecimento_id
+                        ORDER BY area_conhecimento.nome, componente_curricular.nome)) as foo
+            WHERE cod_turma = $1;";
+
+      $turmasDisciplinas = $this->fetchPreparedQuery($sql, $this->getRequest()->turma_id);
+      return array('turmas-disciplinas' => $turmasDisciplinas);
+    }
   }
 
   public function Gerar() {
