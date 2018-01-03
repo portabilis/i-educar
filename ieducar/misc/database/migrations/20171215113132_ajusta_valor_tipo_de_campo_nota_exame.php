f4a490720bfa29088efce86e45ac29d235cebd5e
bonot <paula_bonot@hotmail.com>
2018-01-03 09:32:11 -0200

    Altera migração para usar padrão up/down; Refs #4223

--- ieducar/misc/database/migrations/20171215113132_ajusta_valor_tipo_de_campo_nota_exame.php
+++ ieducar/misc/database/migrations/20171215113132_ajusta_valor_tipo_de_campo_nota_exame.php
@@ -4,8 +4,13 @@ use Phinx\Migration\AbstractMigration;
 
 class AjustaValorTipoDeCampoNotaExame extends AbstractMigration
 {
-    public function change()
+    public function up()
     {
         $this->execute("ALTER TABLE modules.nota_exame ALTER COLUMN nota_exame TYPE NUMERIC(6,3);");
+    }
+
+    public function down()
+    {
+        $this->execute("ALTER TABLE modules.nota_exame ALTER COLUMN nota_exame TYPE NUMERIC(5,3);");
     }
 }
