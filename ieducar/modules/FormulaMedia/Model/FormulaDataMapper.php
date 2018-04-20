06408131b39281e5c388fa2b136dcd27f3f3d4d6
Rodrigo Rodrigues <rodrigogbgod@gmail.com>
2018-04-20 15:49:34 -0300

    Padronização declaração primary key e atributos da tabela; Refs #3266

--- ieducar/modules/FormulaMedia/Model/FormulaDataMapper.php
+++ ieducar/modules/FormulaMedia/Model/FormulaDataMapper.php
@@ -1,33 +1,5 @@
 <?php
 
-/**
- * i-Educar - Sistema de gestão escolar
- *
- * Copyright (C) 2006  Prefeitura Municipal de Itajaí
- *                     <ctima@itajai.sc.gov.br>
- *
- * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
- * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
- * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
- * qualquer versão posterior.
- *
- * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
- * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
- * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
- * do GNU para mais detalhes.
- *
- * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
- * com este programa; se não, escreva para a Free Software Foundation, Inc., no
- * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
- *
- * @author      Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
- * @category    i-Educar
- * @license     @@license@@
- * @package     FormulaMedia
- * @subpackage  Modules
- * @since       Arquivo disponível desde a versão 1.1.0
- * @version     $Id$
- */
 
 require_once 'CoreExt/DataMapper.php';
 require_once 'FormulaMedia/Model/Formula.php';
@@ -45,16 +17,21 @@ require_once 'FormulaMedia/Model/Formula.php';
  */
 class FormulaMedia_Model_FormulaDataMapper extends CoreExt_DataMapper
 {
-  protected $_entityClass = 'FormulaMedia_Model_Formula';
-  protected $_tableName   = 'formula_media';
-  protected $_tableSchema = 'modules';
+    protected $_entityClass = 'FormulaMedia_Model_Formula';
+    protected $_tableName   = 'formula_media';
+    protected $_tableSchema = 'modules';
 
-  protected $_attributeMap = array(
-    'instituicao'  => 'instituicao_id',
-    'formulaMedia' => 'formula_media',
-    'tipoFormula'  => 'tipo_formula',
-    'substituiMenorNotaRc'  => 'substitui_menor_nota_rc',
-  );
+    protected $_attributeMap = array(
+        'id'                    => 'id',
+        'instituicao'           => 'instituicao_id',
+        'nome'                  => 'nome',
+        'formulaMedia'          => 'formula_media',
+        'tipoFormula'           => 'tipo_formula',
+        'substituiMenorNotaRc'  => 'substitui_menor_nota_rc'
+    );
 
-  protected $_primaryKey = array('id', 'instituicao');
+    protected $_primaryKey = array(
+        'id'            => 'id',
+        'instituicao'   => 'instituicao_id'
+    );
 }
