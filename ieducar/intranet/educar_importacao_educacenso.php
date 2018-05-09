0b0e5d219edbd6d32bffa61a7ac60fc00bacd11a
bonot <paula_bonot@hotmail.com>
2018-05-09 09:32:37 -0300

    Ajusta novo campo de recurso para prova INEP nos arquivos do Educacenso;

--- ieducar/intranet/educar_importacao_educacenso.php
+++ ieducar/intranet/educar_importacao_educacenso.php
@@ -1126,17 +1126,13 @@ class indice extends clsCadastro
       }
     }
 
-    $recursosProva = array(
-      'recurso_prova_inep_aux_ledor' => $dadosRegistro[30-1],
-      'recurso_prova_inep_aux_transcricao' => $dadosRegistro[31-1],
-      'recurso_prova_inep_guia_interprete' => $dadosRegistro[32-1],
-      'recurso_prova_inep_interprete_libras' => $dadosRegistro[33-1],
-      'recurso_prova_inep_leitura_labial' => $dadosRegistro[34-1],
-      'recurso_prova_inep_prova_ampliada_16' => $dadosRegistro[35-1],
-      'recurso_prova_inep_prova_ampliada_20' => $dadosRegistro[36-1],
-      'recurso_prova_inep_prova_ampliada_24' => $dadosRegistro[37-1],
-      'recurso_prova_inep_prova_braille' => $dadosRegistro[38-1],
-    );
+    $recursosProva = array();
+    for ($i=1; $i <= 9; $i++) { 
+      if($dadosRegistro[29+$i-1]){
+        $recursosProva[] = $i;
+      }
+    }
+    $recursosProva = '{'.implode(',', $recursosProva).'}';
 
     $codAluno = $this->existeAluno($inepAluno);
 
@@ -1149,9 +1145,7 @@ class indice extends clsCadastro
         $this->createOrUpdateRaca($idpesAluno, $corRacaEducacenso);
       }
       $aluno = new clsPmieducarAluno(null, null, null, null, $this->pessoa_logada, $idpesAluno, null, null, 1);
-      foreach ($recursosProva as $key => $value) {
-        $aluno->{$key} = $value;
-      }
+      $aluno->recursos_prova_inep = $recursosProva;
       $codAluno = $aluno->cadastra();
       $this->createAlunoEducacenso($codAluno, $inepAluno);
       foreach ($deficiencias as $key => $deficienciaEducacenso) {
