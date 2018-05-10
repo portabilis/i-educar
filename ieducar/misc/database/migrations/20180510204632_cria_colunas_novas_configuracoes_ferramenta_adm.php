dae9b4fb6b769d68fc3c61b0d194ef98f0550a11
Everton Muniz <munizeverton@gmail.com>
2018-05-10 18:29:59 -0300

    Cria novos campos de configuração

--- ieducar/misc/database/migrations/20180510204632_cria_colunas_novas_configuracoes_ferramenta_adm.php
+++ ieducar/misc/database/migrations/20180510204632_cria_colunas_novas_configuracoes_ferramenta_adm.php
@@ -4,29 +4,16 @@ use Phinx\Migration\AbstractMigration;
 
 class CriaColunasNovasConfiguracoesFerramentaAdm extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    public function change()
+    public function up()
     {
-
+        $this->execute('ALTER TABLE pmieducar.configuracoes_gerais ADD active_on_ieducar SMALLINT DEFAULT 1 NULL;');
+        $this->execute('ALTER TABLE pmieducar.configuracoes_gerais ADD ieducar_image VARCHAR(255) DEFAULT NULL NULL;');
+        $this->execute('ALTER TABLE pmieducar.configuracoes_gerais ADD ieducar_entity_name VARCHAR(255) DEFAULT NULL NULL;');
+        $this->execute('ALTER TABLE pmieducar.configuracoes_gerais ADD ieducar_login_footer VARCHAR(255) DEFAULT \'<p>Portabilis Tecnologia - suporte@portabilis.com.br - <a class="   light" href="http://suporte.portabilis.com.br" target="_blank"> Obter Suporte </a></p> \' NULL;');
+        $this->execute('ALTER TABLE pmieducar.configuracoes_gerais ADD ieducar_external_footer VARCHAR(255) DEFAULT \'<p>Conhe&ccedil;a mais sobre o i-Educar e a Portabilis, acesse nosso <a href="   http://blog.portabilis.com.br">blog</a></p> \' NULL;');
+        $this->execute('ALTER TABLE pmieducar.configuracoes_gerais ADD ieducar_internal_footer VARCHAR(255) DEFAULT \'<p>Conhe&ccedil;a mais sobre o i-Educar e a Portabilis, <a href="   http://blog.portabilis.com.br" target="_blank">acesse nosso blog</a> &nbsp;&nbsp;&nbsp; &copy; Portabilis - Todos os direitos reservados</p>\' NULL;');
+        $this->execute('ALTER TABLE pmieducar.configuracoes_gerais ADD facebook_url VARCHAR(255) DEFAULT \'https://www.facebook.com/portabilis\' NULL;');
+        $this->execute('ALTER TABLE pmieducar.configuracoes_gerais ADD twitter_url VARCHAR(255) DEFAULT \'https://twitter.com/portabilis\' NULL;');
+        $this->execute('ALTER TABLE pmieducar.configuracoes_gerais ADD linkedin_url VARCHAR(255) DEFAULT \'https://www.linkedin.com/company/portabilis-tecnologia\' NULL;');
     }
 }
