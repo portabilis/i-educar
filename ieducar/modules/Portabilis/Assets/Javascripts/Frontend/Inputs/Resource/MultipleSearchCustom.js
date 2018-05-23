c3b9ba26649e718a28d68f5d1ca40ddb148039d6
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2018-05-23 15:26:48 -0300

    Ajustes no MultipleSearchCustom
    Quando não existir valores válidos não deve enviar uma string em branco

--- ieducar/modules/Portabilis/Assets/Javascripts/Frontend/Inputs/Resource/MultipleSearchCustom.js
+++ ieducar/modules/Portabilis/Assets/Javascripts/Frontend/Inputs/Resource/MultipleSearchCustom.js
@@ -1,9 +1,23 @@
 (function($){
+  let validValue = (value) => {
+    return value !== undefined && value !== null && value !== "";
+  };
+
   $(document).ready(function(){
     $j.each(arrayOptions, function(id, e) {
       var element = e.element;
       var values = e.values;
+
+      if (values && values.constructor === Array ) {
+        values = values.filter((x) => validValue(x));
+      } else if(!validValue(values) ) {
+        values = [];
+      }
+
       setTimeout(function() {
+        console.log(element);
+        console.log(values);
+
         element.val(values);
         element.trigger('chosen:updated');
       }, 500);
