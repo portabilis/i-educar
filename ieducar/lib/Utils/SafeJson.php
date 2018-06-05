d0deee7d55f5bf9e3a987d20fc46b4bd550709e2
Éber Freitas Dias <eber.freitas@gmail.com>
2018-06-05 11:24:37 -0300

    Corrigindo classe para seguir melhor os padrões;
    Refs #encoding-fix

--- ieducar/lib/Utils/SafeJson.php
+++ ieducar/lib/Utils/SafeJson.php
@@ -2,17 +2,17 @@
 
 class SafeJson {
 
-    static function encode($value, $options = 0, $depth = 512)
+    public static function encode($value, $options = 0, $depth = 512)
     {
         return self::handle(json_encode($value, $options, $depth));
     }
 
-    static function decode($json, $assoc = false)
+    public static function decode($json, $assoc = false)
     {
         return self::handle(json_decode($json, $assoc));
     }
 
-    static private function handle($value)
+    private static function handle($value)
     {
         if ($value === false) {
             throw new Exception(json_last_error_msg());
