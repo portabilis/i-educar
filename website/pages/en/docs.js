502eb0e41e864b672f3abd66ac99559515e3eaa1
Rodrigo Devigo <rodrigo.devigo@ifms.edu.br>
2018-07-06 18:45:34 -0400

    Documentção Finalizada

--- website/pages/en/docs.js
+++ website/pages/en/docs.js
@@ -11,6 +11,7 @@ const CompLibrary = require('../../core/CompLibrary.js');
 const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
+const DocsBlock = require(process.cwd()+ '/core/DocsBlock.js');
 
 const siteConfig = require(process.cwd() + '/siteConfig.js');
 
@@ -59,11 +60,11 @@ const Block = props => (
     id={props.id}
     background={props.background}
     className={props.className}>
-    <GridBlock className={props.classNameGrid} align="left" contents={props.children} layout={props.layout} />
-    <div className='docsText'>
-      <p>Veja algo que precisa ser corrigido? Propor uma mudança na <em>fonte</em></p>
-      <p>Precisa de uma versão diferente dos documentos? Veja as versões disponíveis.</p>
-    </div>
+    <DocsBlock className={props.classNameGrid} align="left" contents={props.children} layout={props.layout} />
+    <span className='docsText'>
+      <p>Veja algo que precisa ser corrigido? Propor uma mudança na <a href="https://github.com/portabilis/i-educar/issues" >fonte</a></p>
+      <p>Precisa de uma versão diferente dos documentos?<a> Veja as versões disponíveis.</a></p>
+    </span>
   </Container>
 );
 
@@ -96,18 +97,21 @@ class Docs extends React.Component {
             image: imgUrl('funcionalidades/book.svg'),
             imageAlign: 'top',
             title: 'Professores',
-          },
-          {
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt sed lectus non auctor. Duis eu vulputate neque, eu eleifend justo. Curabitur eleifend nisi eu porta laoreet.',
-            image: imgUrl('funcionalidades/book.svg'),
-            imageAlign: 'top',
-            title: 'Secretários da Escola',
+            link: docUrl("user-intro"),
           },
           {
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt sed lectus non auctor. Duis eu vulputate neque, eu eleifend justo. Curabitur eleifend nisi eu porta laoreet.',
             image: imgUrl('funcionalidades/book.svg'),
             imageAlign: 'top',
             title: 'Administradores',
+            link: docUrl("admin-intro"),
+          },
+          {
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt sed lectus non auctor. Duis eu vulputate neque, eu eleifend justo. Curabitur eleifend nisi eu porta laoreet.',
+            image: imgUrl('funcionalidades/book.svg'),
+            imageAlign: 'top',
+            title: 'Desenvolvedores',
+            link: docUrl("dev-intro"),
           }
         ]}
         </Block>
