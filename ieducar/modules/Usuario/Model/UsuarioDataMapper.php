07493dc0e37af8fb766e2c2885d9e9ac41a8c460
Eder Soares <edersoares@me.com>
2018-07-12 10:56:50 -0300

    Ajustes solicitados no review

--- ieducar/modules/Usuario/Model/UsuarioDataMapper.php
+++ ieducar/modules/Usuario/Model/UsuarioDataMapper.php
@@ -12,15 +12,15 @@ class Usuario_Model_UsuarioDataMapper extends CoreExt_DataMapper
     protected $_tableSchema = 'pmieducar';
 
     protected $_attributeMap = [
-        'id'               => 'cod_usuario',
-        'escolaId'         => 'ref_cod_escola',
-        'instituicaoId'    => 'ref_cod_instituicao',
+        'id' => 'cod_usuario',
+        'escolaId' => 'ref_cod_escola',
+        'instituicaoId' => 'ref_cod_instituicao',
         'funcionarioCadId' => 'ref_funcionario_cad',
         'funcionarioExcId' => 'ref_funcionario_exc',
-        'tipoUsuarioId'    => 'ref_cod_tipo_usuario',
-        'dataCadastro'     => 'data_cadastro',
-        'dataExclusao'     => 'data_exclusao',
-        'ativo'            => 'ativo'
+        'tipoUsuarioId' => 'ref_cod_tipo_usuario',
+        'dataCadastro' => 'data_cadastro',
+        'dataExclusao' => 'data_exclusao',
+        'ativo' => 'ativo'
     ];
 
     protected $_primaryKey = [
