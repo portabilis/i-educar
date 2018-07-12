9dbd6dd8258aea4b32cf7c76872b6ff5cb020c47
Eder Soares <edersoares@me.com>
2018-07-12 14:15:14 -0300

    Aplica PSR-2 em modules/AreaConhecimento

--- ieducar/modules/AreaConhecimento/Model/AreaDataMapper.php
+++ ieducar/modules/AreaConhecimento/Model/AreaDataMapper.php
@@ -3,33 +3,24 @@
 require_once 'CoreExt/DataMapper.php';
 require_once 'AreaConhecimento/Model/Area.php';
 
-/**
- * AreaConhecimento_Model_AreaDataMapper class.
- *
- * @author      Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
- * @category    i-Educar
- * @license     @@license@@
- * @package     AreaConhecimento
- * @subpackage  Modules
- * @since       Classe disponível desde a versão 1.1.0
- * @version     @@package_version@@
- */
 class AreaConhecimento_Model_AreaDataMapper extends CoreExt_DataMapper
 {
-  protected $_entityClass = 'AreaConhecimento_Model_Area';
-  protected $_tableName   = 'area_conhecimento';
-  protected $_tableSchema = 'modules';
+    protected $_entityClass = 'AreaConhecimento_Model_Area';
 
-  protected $_attributeMap = array(
-    'id'            => 'id',
-    'instituicao'   => 'instituicao_id',
-    'nome'          => 'nome',
-    'secao'         => 'secao',
-    'ordenamento_ac'=> 'ordenamento_ac',
-  );
+    protected $_tableName   = 'area_conhecimento';
 
-  protected $_primaryKey = array(
-    'id'          => 'id',
-    'instituicao' => 'instituicao_id'
-  );
+    protected $_tableSchema = 'modules';
+
+    protected $_attributeMap = [
+        'id' => 'id',
+        'instituicao' => 'instituicao_id',
+        'nome' => 'nome',
+        'secao' => 'secao',
+        'ordenamento_ac' => 'ordenamento_ac',
+    ];
+
+    protected $_primaryKey = [
+        'id' => 'id',
+        'instituicao' => 'instituicao_id'
+    ];
 }
