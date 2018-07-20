5705c7c074bb32650aea411339a1834349c9466b
Everton Muniz <munizeverton@gmail.com>
2018-07-20 17:34:18 -0300

    Adiciona constantes faltando na classe DependenciaAdministrativaEscola

--- src/Modules/Educacenso/Model/DependenciaAdministrativaEscola.php
+++ src/Modules/Educacenso/Model/DependenciaAdministrativaEscola.php
@@ -4,6 +4,8 @@ namespace iEducar\Modules\Educacenso\Model;
 
 class DependenciaAdministrativaEscola
 {
+    const FEREDAL = 1;
     const ESTADUAL = 2;
     const MUNICIPAL = 3;
+    const PRIVADA = 4;
 }
