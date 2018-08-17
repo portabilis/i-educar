67b2851c92316ae89abc23905737d7b8de091333
Éber Freitas Dias <eber.freitas@gmail.com>
2018-08-17 16:45:43 -0300

    Altera classes de inputs;
    Refs portabilis/i-educar-portabilis#4082
    
    - Adequa para PSR-2;
    - Melhora fluxo de alguns métodos;
    - Correção de bugs.

--- ieducar/lib/Portabilis/View/Helper/Input/Resource/MultipleSearchCurso.php
+++ ieducar/lib/Portabilis/View/Helper/Input/Resource/MultipleSearchCurso.php
@@ -1,83 +1,50 @@
 <?php
-#error_reporting(E_ALL);
-#ini_set("display_errors", 1);
-/**
- * i-Educar - Sistema de gestão escolar
- *
- * Copyright (C) 2006  Prefeitura Municipal de Itajaí
- *                     <ctima@itajai.sc.gov.br>
- *
- * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
- * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
- * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
- * qualquer versão posterior.
- *
- * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
- * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
- * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
- * do GNU para mais detalhes.
- *
- * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
- * com este programa; se não, escreva para a Free Software Foundation, Inc., no
- * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
- *
- * @author    Lucas Schmoeller da Silva <lucas@portabilis.com.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   Portabilis
- * @since     ?
- * @version   $Id$
- */
 
 require_once 'lib/Portabilis/View/Helper/Input/MultipleSearch.php';
 require_once 'lib/Portabilis/Utils/Database.php';
 require_once 'lib/Portabilis/String/Utils.php';
 
-/**
- * Portabilis_View_Helper_Input_MultipleSearchCurso class.
- *
- * @author    Paula Bonot <bonot@portabilis.com.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   Portabilis
- * @since     ?
- * @version   @@package_version@@
- */
-class Portabilis_View_Helper_Input_Resource_MultipleSearchCurso extends Portabilis_View_Helper_Input_MultipleSearch {
+class Portabilis_View_Helper_Input_Resource_MultipleSearchCurso extends Portabilis_View_Helper_Input_MultipleSearch
+{
+    protected function getOptions($resources)
+    {
+        return $this->insertOption(null, '', $resources);
+    }
 
-  protected function getOptions($resources) {
+    public function multipleSearchCurso($attrName, $options = [])
+    {
+        $defaultOptions = [
+            'objectName' => 'curso',
+            'max_selected_options' => 3
+        ];
 
-    // if (empty($resources)) {
-    //   $resources = Portabilis_Utils_Database::fetchPreparedQuery('SELECT cod_escola, nome FROM pmieducar.escola INNER JOIN cadastro.pessoa ON (ref_idpes = idpes) WHERE escola.ativo = 1');
-    //   $resources = Portabilis_Array_Utils::setAsIdValue($resources, 'cod_escola', 'nome');
-    // }
+        $options = $this->mergeOptions($options, $defaultOptions);
+        $options['options']['resources'] = $options['options']['resources'] ?? [];
+        $options['options']['resources'] = $this->getOptions($options['options']['resources']);
 
-    return $this->insertOption(null, '', $resources);
-  }
+        $this->placeholderJs($options);
 
-  public function multipleSearchCurso($attrName, $options = array()) {
-    $defaultOptions = array('objectName'    => 'curso', 'max_selected_options' => 3);
+        parent::multipleSearch($options['objectName'], $attrName, $options);
+    }
 
-    $options                         = $this->mergeOptions($options, $defaultOptions);
-    $options['options']['resources'] = $this->getOptions($options['options']['resources']);
+    protected function placeholderJs($options)
+    {
+        $optionsVarName = 'multipleSearch' . Portabilis_String_Utils::camelize($options['objectName']) . 'Options';
 
-    $this->placeholderJs($options);
+        $js = "
+            if (typeof $optionsVarName == 'undefined') { $optionsVarName = {} };
+            $optionsVarName.placeholder = safeUtf8Decode('Selecione os cursos');
+        ";
 
-    parent::multipleSearch($options['objectName'], $attrName, $options);
-  }
+        Portabilis_View_Helper_Application::embedJavascript($this->viewInstance, $js, $afterReady = true);
+    }
 
-  protected function placeholderJs($options) {
-    $optionsVarName = "multipleSearch" . Portabilis_String_Utils::camelize($options['objectName']) . "Options";
-    $js             = "if (typeof $optionsVarName == 'undefined') { $optionsVarName = {} };
-                       $optionsVarName.placeholder = safeUtf8Decode('Selecione os cursos');";
-
-    Portabilis_View_Helper_Application::embedJavascript($this->viewInstance, $js, $afterReady = true);
-  }
-  protected function loadAssets() {
-    Portabilis_View_Helper_Application::loadChosenLib($this->viewInstance);
-    $jsFile = '/modules/Portabilis/Assets/Javascripts/Frontend/Inputs/MultipleSearch.js';
-    Portabilis_View_Helper_Application::loadJavascript($this->viewInstance, $jsFile);
-    $jsFile = '/modules/Portabilis/Assets/Javascripts/Frontend/Inputs/Resource/MultipleSearchCurso.js';
-    Portabilis_View_Helper_Application::loadJavascript($this->viewInstance, $jsFile);
-  }
+    protected function loadAssets()
+    {
+        Portabilis_View_Helper_Application::loadChosenLib($this->viewInstance);
+        $jsFile = '/modules/Portabilis/Assets/Javascripts/Frontend/Inputs/MultipleSearch.js';
+        Portabilis_View_Helper_Application::loadJavascript($this->viewInstance, $jsFile);
+        $jsFile = '/modules/Portabilis/Assets/Javascripts/Frontend/Inputs/Resource/MultipleSearchCurso.js';
+        Portabilis_View_Helper_Application::loadJavascript($this->viewInstance, $jsFile);
+    }
 }
