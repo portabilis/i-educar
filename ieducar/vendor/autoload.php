8f882da71e8beb809ba7ae83fc85e2eec0e91838
Eder Soares <edersoares@me.com>
2018-08-16 21:10:28 -0300

    Criar autoload fake para retro compatibilidade

---
+++ ieducar/vendor/autoload.php
@@ -0,0 +1,6 @@
+<?php
+
+// Este arquivo serve apenas como forma de retrocompatibilidade com a antiga
+// estrutura do i-Educar e será removido no futuro.
+
+require_once __DIR__ . '/../vendor/autoload.php';
