fe9c03b6860d7a63d62b794422f49403bbd5ff9e
Everton Muniz <munizeverton@gmail.com>
2018-08-17 16:32:47 -0300

    Cria triggers para impedir duplicações nas tabelas falta_aluno, nota_aluno e parecer_aluno

---
+++ ieducar/misc/database/migrations/20180817192158_cria_trigger_para_impedir_duplicacao_de_faltas.php
@@ -0,0 +1,28 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CriaTriggerParaImpedirDuplicacaoDeFaltas extends AbstractMigration
+{
+    public function up()
+    {
+        $sql = <<<'SQL'
+                    CREATE OR REPLACE FUNCTION modules.impede_duplicacao_falta_aluno()
+                        RETURNS TRIGGER AS $$
+                    BEGIN
+                        PERFORM * FROM modules.falta_aluno
+                        WHERE falta_aluno.matricula_id = NEW.matricula_id
+                          AND falta_aluno.tipo_falta = NEW.tipo_falta;
+                        IF FOUND THEN
+                            RAISE EXCEPTION 'A matrícula % já existe na tabela falta_aluno', NEW.matricula_id;
+                        END IF;
+                    
+                        RETURN NEW;
+                    END;
+                    $$ language 'plpgsql';
+                    
+                    CREATE TRIGGER impede_duplicacao_falta_aluno BEFORE INSERT OR UPDATE ON modules.falta_aluno FOR EACH ROW EXECUTE                            PROCEDURE modules.impede_duplicacao_falta_aluno();
+SQL;
+        $this->execute($sql);
+    }
+}
