d507f6502c49d2a712524225c304549b79bfa213
Eder Soares <edersoares@me.com>
2018-08-24 17:33:30 -0300

    Registra controller e comandos úteis para testes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,46 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\Browser;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register routes for fake auth using Laravel Dusk.
+     *
+     * @return void
+     */
+    private function registerRoutesForFakeAuth()
+    {
+        Route::get('/_dusk/legacy/login', [
+            'middleware' => 'web',
+            'uses' => 'App\Http\Controllers\LegacyFakeAuthController@doFakeLogin',
+        ]);
+
+        Route::get('/_dusk/legacy/logout', [
+            'middleware' => 'web',
+            'uses' => 'App\Http\Controllers\LegacyFakeAuthController@doFakeLogout',
+        ]);
+    }
+
+    /**
+     * Add custom methods in Browser class used by Laravel Dusk.
+     *
+     * @return void
+     */
+    private function customBrowserForFakeAuth()
+    {
+        Browser::macro('loginLegacy', function () {
+            return $this->visit('/_dusk/legacy/login');
+        });
+
+        Browser::macro('logoutLegacy', function () {
+            return $this->visit('/_dusk/legacy/logout');
+        });
+    }
+
     /**
      * Bootstrap any application services.
      *
@@ -13,7 +49,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->environment('development', 'dusk', 'local', 'testing')) {
+            $this->registerRoutesForFakeAuth();
+            $this->customBrowserForFakeAuth();
+        }
     }
 
     /**
