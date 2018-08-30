58d9e2fd4d253c57a7704bf84516cfa434c503c2
Éber Freitas Dias <eber.freitas@gmail.com>
2018-08-30 15:43:02 -0300

    Ajustes de PSR-2;
    Refs portabilis/i-educar-portabilis#4128

--- ieducar/modules/Avaliacao/Model/NotaAlunoDataMapper.php
+++ ieducar/modules/Avaliacao/Model/NotaAlunoDataMapper.php
@@ -3,25 +3,14 @@
 require_once 'CoreExt/DataMapper.php';
 require_once 'Avaliacao/Model/NotaAluno.php';
 
-/**
- * Avaliacao_Model_NotaAluno class.
- *
- * @author      Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
- * @category    i-Educar
- * @license     @@license@@
- * @package     Avaliacao
- * @subpackage  Modules
- * @since       Classe disponível desde a versão 1.1.0
- * @version     @@package_version@@
- */
 class Avaliacao_Model_NotaAlunoDataMapper extends CoreExt_DataMapper
 {
     protected $_entityClass = 'Avaliacao_Model_NotaAluno';
-    protected $_tableName   = 'nota_aluno';
+    protected $_tableName = 'nota_aluno';
     protected $_tableSchema = 'modules';
 
-    protected $_attributeMap = array(
-        'id'        => 'id',
+    protected $_attributeMap = [
+        'id' => 'id',
         'matricula' => 'matricula_id'
-    );
+    ];
 }
