8650bccd7f71bbae28a8b8610ab2888e1c238d5d
Everton Muniz <munizeverton@gmail.com>
2018-08-31 09:21:49 -0300

    Testa as rotas do menu

--- tests/Browser/IntranetTest.php
+++ tests/Browser/IntranetTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser\Intranet;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
@@ -13,9 +14,12 @@ class IntranetTest extends DuskTestCase
      *
      * @return array
      */
-    private function getIntranetFiles()
+    private function getIntranetRoutes()
     {
-        return file(__DIR__ . '/legacy/intranet.txt', FILE_IGNORE_NEW_LINES);
+        $files = $this->getRoutesFromFile();
+        $routes = $this->getRoutesFromDatabase();
+
+        return array_unique(array_merge($routes, $files));
     }
 
     /**
@@ -30,17 +34,40 @@ class IntranetTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->loginLegacy();
 
-            foreach ($this->getIntranetFiles() as $file) {
-                $browser->visit('/intranet/' . $file);
-
+            foreach ($this->getIntranetRoutes() as $route) {
+                $route = $this->parseRoute($route);
+                $browser->visit($route);
                 $title = $browser->driver->getTitle();
 
                 $assert = Str::contains($title, 'i-Educar');
 
                 $this->assertTrue(
-                    $assert, "The file [/intranet/{$file}] returned [{$title}] instead [i-Educar] in the title."
+                    $assert, "The file [{$route}] returned [{$title}] instead [i-Educar] in the title."
                 );
             }
         });
+    }
+
+    private function parseRoute($route)
+    {
+        if (Str::contains($route, 'module'))  {
+            return $route;
+        }
+
+        return '/intranet/' . $route;
+    }
+
+    private function getRoutesFromFile()
+    {
+        return file(__DIR__ . '/legacy/intranet.txt', FILE_IGNORE_NEW_LINES);
+    }
+
+    private function getRoutesFromDatabase()
+    {
+        $result = DB::select("SELECT arquivo FROM portal.menu_submenu WHERE arquivo <> ''");
+
+        return array_map(function ($value) {
+            return $value->arquivo;
+        }, $result);
     }
 }
