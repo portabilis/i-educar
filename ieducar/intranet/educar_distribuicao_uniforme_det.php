2ce2d8459fed5d8da1caeeda092087ee4f803d35
Eder Soares <edersoares@me.com>
2018-09-05 16:39:54 -0300

    Alguns ajustes e melhorias

--- ieducar/intranet/educar_distribuicao_uniforme_det.php
+++ ieducar/intranet/educar_distribuicao_uniforme_det.php
@@ -11,7 +11,7 @@ class clsIndexBase extends clsBase
 {
     public function Formular()
     {
-        $this->SetTitulo("{$this->_instituicao} i-Educar - Hist&oacute;rico Escolar");
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Distribuições de uniforme escolar");
         $this->processoAp = '578';
         $this->addEstilo('localizacaoSistema');
     }
@@ -32,19 +32,14 @@ class indice extends clsDetalhe
 
     public function Gerar()
     {
-        @session_start();
-        $this->pessoa_logada = $_SESSION['id_pessoa'];
-        session_write_close();
-
-        $this->titulo = 'Hist&oacute;rico Escolar - Detalhe';
-
-        $this->cod_distribuicao_uniforme=$_GET['cod_distribuicao_uniforme'];
-        $this->ref_cod_aluno=$_GET['ref_cod_aluno'];
+        $this->titulo = 'Distribuições de uniforme escolar - Detalhe';
+        $this->cod_distribuicao_uniforme = $_GET['cod_distribuicao_uniforme'];
+        $this->ref_cod_aluno = $_GET['ref_cod_aluno'];
 
         $tmp_obj = new clsPmieducarDistribuicaoUniforme($this->cod_distribuicao_uniforme);
         $registro = $tmp_obj->detalhe();
 
-        if (! $registro) {
+        if (!$registro) {
             header("location: educar_distribuicao_uniforme_lst.php?ref_cod_aluno={$this->ref_cod_aluno}");
             die();
         }
@@ -58,15 +53,15 @@ class indice extends clsDetalhe
         }
 
         if ($nm_aluno) {
-            $this->addDetalhe([ 'Aluno', "{$nm_aluno}"]);
+            $this->addDetalhe(['Aluno', "{$nm_aluno}"]);
         }
 
         if ($registro['ano']) {
-            $this->addDetalhe([ 'Ano', "{$registro['ano']}"]);
+            $this->addDetalhe(['Ano', "{$registro['ano']}"]);
         }
 
         if ($registro['data']) {
-            $this->addDetalhe([ Portabilis_String_Utils::toLatin1('Data da distribuiÃ§Ã£o'), Portabilis_Date_Utils::pgSQLToBr($registro['data']) ]);
+            $this->addDetalhe(['Data da distribuição', Portabilis_Date_Utils::pgSQLToBr($registro['data'])]);
         }
 
         if ($registro['ref_cod_escola']) {
@@ -81,16 +76,16 @@ class indice extends clsDetalhe
         }
 
         if (dbBool($registro['kit_completo'])) {
-            $this->addDetalhe([ 'Recebeu kit completo', 'Sim']);
+            $this->addDetalhe(['Recebeu kit completo', 'Sim']);
         } else {
-            $this->addDetalhe(['Recebeu kit completo', Portabilis_String_Utils::toLatin1('NÃ£o')]);
-            $this->addDetalhe([Portabilis_String_Utils::toLatin1('Quantidade de agasalhos (jaqueta e calÃ§a)'), $registro['agasalho_qtd'] ?: '0' ]);
-            $this->addDetalhe(['Quantidade de camisetas (manga curta)', $registro['camiseta_curta_qtd'] ?: '0' ]);
-            $this->addDetalhe(['Quantidade de camisetas (manga longa)', $registro['camiseta_longa_qtd'] ?: '0' ]);
-            $this->addDetalhe(['Quantidade de meias', $registro['meias_qtd'] ?: '0' ]);
-            $this->addDetalhe(['Bermudas tectels (masculino)', $registro['bermudas_tectels_qtd'] ?: '0' ]);
-            $this->addDetalhe(['Bermudas coton (feminino)', $registro['bermudas_coton_qtd'] ?: '0' ]);
-            $this->addDetalhe([Portabilis_String_Utils::toLatin1('Quantidade de tÃªnis'), $registro['tenis_qtd'] ?: '0' ]);
+            $this->addDetalhe(['Recebeu kit completo', Portabilis_String_Utils::toLatin1('Não')]);
+            $this->addDetalhe([Portabilis_String_Utils::toLatin1('Quantidade de agasalhos (jaqueta e calça)'), $registro['agasalho_qtd'] ?: '0']);
+            $this->addDetalhe(['Quantidade de camisetas (manga curta)', $registro['camiseta_curta_qtd'] ?: '0']);
+            $this->addDetalhe(['Quantidade de camisetas (manga longa)', $registro['camiseta_longa_qtd'] ?: '0']);
+            $this->addDetalhe(['Quantidade de meias', $registro['meias_qtd'] ?: '0']);
+            $this->addDetalhe(['Bermudas tectels (masculino)', $registro['bermudas_tectels_qtd'] ?: '0']);
+            $this->addDetalhe(['Bermudas coton (feminino)', $registro['bermudas_coton_qtd'] ?: '0']);
+            $this->addDetalhe([Portabilis_String_Utils::toLatin1('Quantidade de tênis'), $registro['tenis_qtd'] ?: '0']);
         }
 
         $obj_permissoes = new clsPermissoes();
