f8959739daa21f3d256ad6398e63737e6e0a151f
Eder Soares <edersoares@me.com>
2018-10-02 15:39:03 -0300

    Não encripta os cookies

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            \App\Http\Middleware\EncryptCookies::class,
+            // \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             // \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
