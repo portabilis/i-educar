df6b97484916682f69a6b9145c5987921b093567
diegoocezar <39813815+diegoocezar@users.noreply.github.com>
2018-10-09 20:52:33 -0300

    Update ListController.php

--- ieducar/lib/Core/Controller/Page/ListController.php
+++ ieducar/lib/Core/Controller/Page/ListController.php
@@ -21,10 +21,15 @@
  * endereחo 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
  *
  * @author    Eriksen Costa Paixדo <eriksen.paixao_bs@cobra.com.br>
+ *
  * @category  i-Educar
+ *
  * @license   @@license@@
+ *
  * @package   Core_Controller
+ *
  * @since     Arquivo disponםvel desde a versדo 1.1.0
+ *
  * @version   $Id$
  */
 
@@ -38,129 +43,138 @@ require_once 'CoreExt/View/Helper/UrlHelper.php';
  * Provך um controller padrדo para listagem de registros.
  *
  * @author    Eriksen Costa Paixדo <eriksen.paixao_bs@cobra.com.br>
+ *
  * @category  i-Educar
+ *
  * @license   @@license@@
+ *
  * @package   Core_Controller
+ *
  * @since     Classe disponםvel desde a versדo 1.1.0
+ *
  * @version   @@package_version@@
  */
 class Core_Controller_Page_ListController extends clsListagem implements Core_View_Tabulable
 {
-  /**
-   * Mapeia um nome descritivo a um atributo de CoreExt_Entity retornado pela
-   * instגncia CoreExt_DataMapper retornada por getDataMapper().
-   *
-   * Para uma instגncia de CoreExt_Entity que tenha os seguintes atributos:
-   * <code>
-   * <?php
-   * $_data = array(
-   *   'nome' => NULL
-   *   'idade' => NULL,
-   *   'data_validacao' => NULL
-   * );
-   * </code>
-   *
-   * O mapeamento poderia ser feito da seguinte forma:
-   * <code>
-   * <?php
-   * $_tableMap = array(
-   *   'Nome' => 'nome',
-   *   'Idade (anos)' => 'idade'
-   * );
-   * </code>
-   *
-   * Se um atributo nדo for mapeado, ele nדo serב exibido por padrדo durante
-   * a geraחדo de HTML na execuחדo do mיtodo Gerar().
-   *
-   * @var array
-   */
-  protected $_tableMap = array();
+    /**
+     * Mapeia um nome descritivo a um atributo de CoreExt_Entity retornado pela
+     * instגncia CoreExt_DataMapper retornada por getDataMapper().
+     *
+     * Para uma instגncia de CoreExt_Entity que tenha os seguintes atributos:
+     * <code>
+     * <?php
+     * $_data = array(
+     *   'nome' => NULL
+     *   'idade' => NULL,
+     *   'data_validacao' => NULL
+     * );
+     * </code>
+     *
+     * O mapeamento poderia ser feito da seguinte forma:
+     * <code>
+     * <?php
+     * $_tableMap = array(
+     *   'Nome' => 'nome',
+     *   'Idade (anos)' => 'idade'
+     * );
+     * </code>
+     *
+     * Se um atributo nדo for mapeado, ele nדo serב exibido por padrדo durante
+     * a geraחדo de HTML na execuחדo do mיtodo Gerar().
+     *
+     * @var array
+     */
+    protected $_tableMap = [];
 
-  /**
-   * Getter.
-   * @see Core_View_Tabulable#getTableMap()
-   */
-  public function getTableMap()
-  {
-    return $this->_tableMap;
-  }
-
-  /**
-   * Retorna os registros a serem exibidos na listagem.
-   *
-   * Subclasses devem sobrescrever este mיtodo quando os parגmetros para
-   * CoreExt_DataMapper::findAll forem mais especםficos.
-   *
-   * @return array (int => CoreExt_Entity)
-   */
-  public function getEntries()
-  {
-    $mapper = $this->getDataMapper();
-    return $mapper->findAll();
-  }
-
-  /**
-   * Configura o botדo de aחדo padrדo para a criaחדo de novo registro.
-   */
-  public function setAcao()
-  {
-    $obj_permissao = new clsPermissoes();
-
-    if($obj_permissao->permissao_cadastra($this->_processoAp, $this->getPessoaLogada(),7,null,true))
+    /**
+     * Getter.
+     *
+     * @see Core_View_Tabulable#getTableMap()
+     */
+    public function getTableMap()
     {
-      $this->acao = 'go("edit")';
-      $this->nome_acao = 'Novo';
+        return $this->_tableMap;
     }
-  }
 
-  protected function getPessoaLogada(){
-    return $_SESSION['id_pessoa'];
-  }
+    /**
+     * Retorna os registros a serem exibidos na listagem.
+     *
+     * Subclasses devem sobrescrever este mיtodo quando os parגmetros para
+     * CoreExt_DataMapper::findAll forem mais especםficos.
+     *
+     * @return array (int => CoreExt_Entity)
+     */
+    public function getEntries()
+    {
+        $mapper = $this->getDataMapper();
 
-  /**
-   * Implementaחדo padrדo para as subclasses que estenderem essa classe. Cria
-   * uma lista de apresentaחדo de dados simples utilizando o mapeamento de
-   * $_tableMap.
-   *
-   * @see Core_Controller_Page_ListController#$_tableMap
-   * @see clsDetalhe#Gerar()
-   */
-  public function Gerar()
-  {
-    $headers = $this->getTableMap();
+        return $mapper->findAll();
+    }
 
-    // Configura o cabeחalho da listagem.
-    $this->addCabecalhos(array_keys($headers));
+    /**
+     * Configura o botדo de aחדo padrדo para a criaחדo de novo registro.
+     */
+    public function setAcao()
+    {
+        $obj_permissao = new clsPermissoes();
 
-    // Recupera os registros para a listagem.
-    $entries = $this->getEntries();
+        if ($obj_permissao->permissao_cadastra($this->_processoAp, $this->getPessoaLogada(), 7, null, true)) {
+            $this->acao = 'go("edit")';
+            $this->nome_acao = 'Novo';
+        }
+    }
 
-    // Paginador
-    $this->limite = 20;
-    $this->offset = ($_GET['pagina_' . $this->nome]) ?
+    protected function getPessoaLogada()
+    {
+        return $_SESSION['id_pessoa'];
+    }
+
+    /**
+     * Implementaחדo padrדo para as subclasses que estenderem essa classe. Cria
+     * uma lista de apresentaחדo de dados simples utilizando o mapeamento de
+     * $_tableMap.
+     *
+     * @see Core_Controller_Page_ListController#$_tableMap
+     * @see clsDetalhe#Gerar()
+     */
+    public function Gerar()
+    {
+        $headers = $this->getTableMap();
+
+        // Configura o cabeחalho da listagem.
+        $this->addCabecalhos(array_keys($headers));
+
+        // Recupera os registros para a listagem.
+        $entries = $this->getEntries();
+
+        // Paginador
+        $this->limite = 20;
+        $this->offset = ($_GET['pagina_' . $this->nome]) ?
       $_GET['pagina_' . $this->nome] * $this->limite - $this->limite
       : 0;
 
-    foreach ($entries as $entry) {
-      $item = array();
-      $data = $entry->toArray();
-      $options = array('query' => array('id' => $entry->id));
+        foreach ($entries as $entry) {
+            $item = [];
+            $data = $entry->toArray();
+            $options = ['query' => ['id' => $entry->id]];
 
-      foreach ($headers as $label => $attr) {
-        $item[] = CoreExt_View_Helper_UrlHelper::l(
-          $entry->$attr, 'view', $options
+            foreach ($headers as $label => $attr) {
+                $item[] = CoreExt_View_Helper_UrlHelper::l(
+          $entry->$attr,
+            'view',
+            $options
         );
-      }
+            }
 
-      $this->addLinhas($item);
+            $this->addLinhas($item);
+        }
+
+        $this->addPaginador2('', count($entries), $_GET, $this->nome, $this->limite);
+
+        // Configura o botדo padrדo de aחדo para a criaחדo de novo registro.
+        $this->setAcao();
+
+        // Largura da tabela HTML onde se encontra a listagem.
+        $this->largura = '100%';
     }
-
-    $this->addPaginador2('', count($entries), $_GET, $this->nome, $this->limite);
-
-    // Configura o botדo padrדo de aחדo para a criaחדo de novo registro.
-    $this->setAcao();
-
-    // Largura da tabela HTML onde se encontra a listagem.
-    $this->largura = '100%';
-  }
 }
