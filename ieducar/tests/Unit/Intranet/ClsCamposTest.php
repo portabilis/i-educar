b444c4ce452cad2f4eca605a33c57d2ba58557e5
Vitor Mattos <vitor.mattos@phprio.org>
2018-10-08 21:00:07 -0300

    Removido skip Corrigido código para passar o teste

--- ieducar/tests/Unit/Intranet/ClsCamposTest.php
+++ ieducar/tests/Unit/Intranet/ClsCamposTest.php
@@ -12,13 +12,13 @@ class ClsCamposTest extends TestCase
 {
     public function testShowFileField()
     {
-        $this->markTestSkipped('must be revisited.');
         $clsCampos = new clsCampos();
 
         $clsCampos->campoArquivo('file', 'Foto', '/caminho/foto', 40, '<b>Foto do aluno</b>');
 
         $this->assertEquals(
             $this->getHtmlCodeFromFile('form_scripts.html') . $this->getHtmlCodeFromFile('file_field.html'),
-            $clsCampos->makeCampos());
+            $clsCampos->makeCampos()
+        );
     }
 }
