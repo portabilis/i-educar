5f8ea3cbd369ea46668815240aaaf1675f77417a
Vitor Mattos <vitor.mattos@phprio.org>
2018-10-09 14:48:07 -0300

    fix #419

--- ieducar/tests/Unit/Api/RegraTest.php
+++ ieducar/tests/Unit/Api/RegraTest.php
@@ -5,6 +5,8 @@ namespace Tests\unit\Api;
 use PHPUnit\DbUnit\DataSet\DefaultDataSet;
 use Tests\SuiteTestCase\ApiTestCase;
 use Tests\SuiteTestCase\TestCase;
+use PHPUnit\Framework\Constraint\JsonMatches;
+use PHPUnit\Util\Json;
 
 require_once __DIR__ . '/../../../modules/Api/Views/RegraController.php';
 
@@ -18,8 +20,11 @@ class RegraTest extends ApiTestCase
 
     public function testRegression()
     {
-        $this->markTestSkipped('must be revisited.');
         $responseBody = $this->doAuthenticatedRequest('regras', ['instituicao_id' => 1, 'ano' => '2018']);
+        file_put_contents('AAA', Json::canonicalize($responseBody));
+        
+        file_put_contents('BBB', Json::canonicalize(file_get_contents($this->getJsonFile('regra_json_valid.json'))));
+        
         $this->assertJsonStringEqualsJsonFile($this->getJsonFile('regra_json_valid.json'), $responseBody);
     }
 }
