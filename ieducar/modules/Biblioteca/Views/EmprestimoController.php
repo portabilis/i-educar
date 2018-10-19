f5cfaaa74244ad767d6969069157bcf92ac481c6
Leonam Pereira Dias <leonam.pd@gmail.com>
2018-10-18 22:06:12 -0300

    adjust files on Biblioteca dir accordingly code standard

--- ieducar/modules/Biblioteca/Views/EmprestimoController.php
+++ ieducar/modules/Biblioteca/Views/EmprestimoController.php
@@ -1,34 +1,17 @@
 <?php
-
-#error_reporting(E_ALL);
-#ini_set("display_errors", 1);
-
 /**
- * i-Educar - Sistema de gestão escolar
- *
- * Copyright (C) 2006  Prefeitura Municipal de Itajaí
- *           <ctima@itajai.sc.gov.br>
- *
- * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
- * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
- * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
- * qualquer versão posterior.
- *
- * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
- * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
- * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
- * do GNU para mais detalhes.
- *
- * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
- * com este programa; se não, escreva para a Free Software Foundation, Inc., no
- * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
  *
  * @author    Lucas D'Avila <lucasdavila@portabilis.com.br>
+ *
  * @category  i-Educar
+ *
  * @license   @@license@@
+ *
  * @package   Biblioteca
  * @subpackage  Modules
+ *
  * @since     Arquivo disponível desde a versão ?
+ *
  * @version   $Id$
  */
 
@@ -36,45 +19,61 @@ require_once 'Portabilis/Controller/Page/ListController.php';
 
 class EmprestimoController extends Portabilis_Controller_Page_ListController
 {
-  protected $_dataMapper = '';
-  protected $_titulo     = 'Emprestimo';
-  protected $_formMap    = array();
-  protected $_processoAp = 610;
+    /**
+     * @var string
+     */
+    protected $_dataMapper = '';
 
-  protected function _preRender(){
-    @session_start();
-    $pessoa_logada = $_SESSION['id_pessoa'];
-    @session_write_close();
+    /**
+     * @var string
+     */
+    protected $_titulo = 'Emprestimo';
 
-    $obj_permissao = new clsPermissoes();
-    $obj_permissao->permissao_cadastra(610, $pessoa_logada, 7, '/intranet/educar_biblioteca_index.php');
+    /**
+     * @var array
+     */
+    protected $_formMap = [];
 
-    parent::_preRender();
+    /**
+     * @var int
+     */
+    protected $_processoAp = 610;
 
-    Portabilis_View_Helper_Application::loadStylesheet($this, 'intranet/styles/localizacaoSistema.css');
+    protected function _preRender()
+    {
+        @session_start();
+        $pessoa_logada = $_SESSION['id_pessoa'];
+        @session_write_close();
 
-    $localizacao = new LocalizacaoSistema();
+        $obj_permissao = new clsPermissoes();
+        $obj_permissao->permissao_cadastra(610, $pessoa_logada, 7, '/intranet/educar_biblioteca_index.php');
 
-    $localizacao->entradaCaminhos( array(
-         $_SERVER['SERVER_NAME']."/intranet" => "In&iacute;cio",
-         "educar_biblioteca_index.php"                  => "Biblioteca",
-         ""                                  => "Empr&eacute;stimo de exemplares"             
-    ));
-    $this->enviaLocalizacao($localizacao->montar(), true);     
-  }  
+        parent::_preRender();
 
-  public function Gerar() {
-    // inputs
-    $this->inputsHelper()->dynamic('instituicao', array('id' => 'instituicao_id'));
-    $this->inputsHelper()->dynamic('escola',      array('id' => 'escola_id'));
-    $this->inputsHelper()->dynamic('biblioteca',  array('id' => 'biblioteca_id'));
-    $this->campoNumero('tombo_exemplar', 'Tombo exemplar', '', 13, 13, true);
+        Portabilis_View_Helper_Application::loadStylesheet($this, 'intranet/styles/localizacaoSistema.css');
 
-    $helperOptions = array('hiddenInputOptions' => array('id' => 'cliente_id'));
-    $this->inputsHelper()->dynamic('bibliotecaPesquisaCliente', array(), $helperOptions);
+        $localizacao = new LocalizacaoSistema();
 
-    // assets
-    $this->loadResourceAssets($this->getDispatcher());
-  }
+        $localizacao->entradaCaminhos([
+            $_SERVER['SERVER_NAME'].'/intranet' => 'In&iacute;cio',
+            'educar_biblioteca_index.php' => 'Biblioteca',
+            '' => 'Empr&eacute;stimo de exemplares'
+        ]);
+        $this->enviaLocalizacao($localizacao->montar(), true);
+    }
+
+    public function Gerar()
+    {
+        // inputs
+        $this->inputsHelper()->dynamic('instituicao', ['id' => 'instituicao_id']);
+        $this->inputsHelper()->dynamic('escola', ['id' => 'escola_id']);
+        $this->inputsHelper()->dynamic('biblioteca', ['id' => 'biblioteca_id']);
+        $this->campoNumero('tombo_exemplar', 'Tombo exemplar', '', 13, 13, true);
+
+        $helperOptions = ['hiddenInputOptions' => ['id' => 'cliente_id']];
+        $this->inputsHelper()->dynamic('bibliotecaPesquisaCliente', [], $helperOptions);
+
+        // assets
+        $this->loadResourceAssets($this->getDispatcher());
+    }
 }
-?>
