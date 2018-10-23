569da9c7994a95de82877d478a40d1272daebbc6
Everton Muniz <munizeverton@gmail.com>
2018-10-23 17:23:52 -0300

    Não exibe sexo da aluno se o campo estiver vazio

--- ieducar/intranet/educar_aluno_det.php
+++ ieducar/intranet/educar_aluno_det.php
@@ -124,7 +124,16 @@ class indice extends clsDetalhe
             $registro['nome_aluno'] = strtoupper($det_pessoa_fj['nome']);
             $registro['cpf'] = int2IdFederal($det_fisica['cpf']);
             $registro['data_nasc'] = Portabilis_Date_Utils::pgSQLToBr($det_fisica['data_nasc']);
-            $registro['sexo'] = $det_fisica['sexo'] == 'F' ? 'Feminino' : 'Masculino';
+
+            $registro['sexo'] = '';
+
+            if ($det_fisica['sexo'] == 'F') {
+                $registro['sexo'] = 'Feminino';
+            }
+
+            if ($det_fisica['sexo'] == 'M') {
+                $registro['sexo'] = 'Masculino';
+            }
 
             $obj_estado_civil = new clsEstadoCivil();
             $obj_estado_civil_lista = $obj_estado_civil->lista();
