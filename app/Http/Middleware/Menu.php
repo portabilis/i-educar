81defe286c2d79ec52c4afa6c170ddadf628fb59
Everton Muniz <munizeverton@gmail.com>
2018-11-06 17:29:46 -0200

    Adiciona cache para o menu lateral

--- app/Http/Middleware/Menu.php
+++ app/Http/Middleware/Menu.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Entities\User;
 use App\Services\MenuService;
 use Closure;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\View;
 
 class Menu
@@ -18,12 +19,25 @@ class Menu
      */
     public function handle($request, Closure $next)
     {
+        View::share('menu', $this->getCachedMenu());
+
+        return $next($request);
+    }
+
+    private function getCachedMenu()
+    {
+        // TODO: Criar classe de cache manager baseada na PSR-6
+        $cacheKey =  md5(request()->getHttpHost() . session('id_pessoa'));
+        if (Cache::has($cacheKey)) {
+            return Cache::get($cacheKey);
+        }
+
         /** @var MenuService $menuService */
         $menuService = app(MenuService::class);
         $menuArray = $menuService->getByUser(User::find(session('id_pessoa')));
 
-        View::share('menu', $menuArray);
+        Cache::add($cacheKey, $menuArray, 1);
 
-        return $next($request);
+        return $menuArray;
     }
 }
