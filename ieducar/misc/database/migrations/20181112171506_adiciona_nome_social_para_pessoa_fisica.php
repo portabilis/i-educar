9f82e6e37b2357026b82b025dbcb16ff08916006
bonot <paula_bonot@hotmail.com>
2018-11-12 15:41:16 -0200

    Cria migrações referentes ao nome social da pessoa física

---
+++ ieducar/misc/database/migrations/20181112171506_adiciona_nome_social_para_pessoa_fisica.php
@@ -0,0 +1,14 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AdicionaNomeSocialParaPessoaFisica extends AbstractMigration
+{
+    public function change()
+    {
+        $this->getAdapter()->setOptions(array_replace($this->getAdapter()->getOptions(), ['schema' => 'cadastro']));
+        $table = $this->table('fisica');
+        $table->addColumn('nome_social', 'string', ['limit' => 30, 'null' => true])
+            ->update();
+    }
+}
