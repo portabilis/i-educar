2bb94263723ef87de16f1f0ed29dbd0cdb8a74df
Vitor Mattos <vitor.mattos@phprio.org>
2018-11-19 10:22:51 -0200

    Remoção de blocos de comentários deprecated

--- ieducar/tests/Unit/CoreExt/LocaleTest.php
+++ ieducar/tests/Unit/CoreExt/LocaleTest.php
@@ -1,47 +1,7 @@
 <?php
 
-/**
- * i-Educar - Sistema de gestão escolar
- *
- * Copyright (C) 2006  Prefeitura Municipal de Itajaí
- *                     <ctima@itajai.sc.gov.br>
- *
- * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
- * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
- * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
- * qualquer versão posterior.
- *
- * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
- * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
- * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
- * do GNU para mais detalhes.
- *
- * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
- * com este programa; se não, escreva para a Free Software Foundation, Inc., no
- * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
- *
- * @author      Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
- * @category    i-Educar
- * @license     @@license@@
- * @package     CoreExt_Locale
- * @subpackage  UnitTests
- * @since       Arquivo disponível desde a versão 1.1.0
- * @version     $Id: /ieducar/branches/1.1.0-avaliacao/ieducar/tests/unit/CoreExt/EnumTest.php 770 2009-11-24T18:31:56.633773Z eriksen  $
- */
-
 require_once 'CoreExt/Locale.php';
 
-/**
- * CoreExt_LocaleTest class.
- *
- * @author      Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
- * @category    i-Educar
- * @license     @@license@@
- * @package     CoreExt_Locale
- * @subpackage  UnitTests
- * @since       Classe disponível desde a versão 1.1.0
- * @version     @@package_version@@
- */
 class CoreExt_LocaleTest extends UnitBaseTest
 {
   protected $_instance = NULL;
