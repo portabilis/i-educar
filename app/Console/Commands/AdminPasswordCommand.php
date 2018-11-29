86fa0c4e888eca6b16d71a0aa6c84f866b6dd5e2
Éber Freitas Dias <eber.freitas@gmail.com>
2018-11-29 06:14:40 -0200

    Corrigindo nome da classe.

--- app/Console/Commands/AdminPasswordCommand.php
+++ app/Console/Commands/AdminPasswordCommand.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
-class LegacyLinkCommand extends Command
+class AdminPasswordCommand extends Command
 {
     /**
      * The name and signature of the console command.
