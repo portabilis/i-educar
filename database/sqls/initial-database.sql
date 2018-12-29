6e1b414eb7a755889dbe75c179d0e85da244c07a
Eder Soares <edersoares@me.com>
2018-12-29 21:21:41 -0200

    Seed: portal.menu_menu

--- database/sqls/initial-database.sql
+++ database/sqls/initial-database.sql
@@ -25119,27 +25119,6 @@ SELECT pg_catalog.setval('portal.mailling_historico_cod_mailling_historico_seq',
 
 
 --
--- TOC entry 8074 (class 0 OID 9445510)
--- Dependencies: 786
--- Data for Name: menu_menu; Type: TABLE DATA; Schema: portal; Owner: -
---
-
-INSERT INTO portal.menu_menu VALUES (1, 'Todos', '', NULL, '#', 9999, false, NULL);
-INSERT INTO portal.menu_menu VALUES (23, 'Principal', '', NULL, '#', 9999, false, NULL);
-INSERT INTO portal.menu_menu VALUES (38, 'i-Pauta', '', NULL, '#', 9999, false, NULL);
-INSERT INTO portal.menu_menu VALUES (56, 'i-Educar - Controle', NULL, NULL, '#', 9999, false, NULL);
-INSERT INTO portal.menu_menu VALUES (5, 'Agenda', 'Agendas', NULL, '#', 9999, false, NULL);
-INSERT INTO portal.menu_menu VALUES (68, 'Endereçamento', '', NULL, '/intranet/educar_enderecamento_index.php', 2, true, 'fa-map');
-INSERT INTO portal.menu_menu VALUES (7, 'Pessoas', 'Pessoas Físicas e Jurídicas', NULL, '/intranet/educar_pessoas_index.php', 3, true, 'fa-user');
-INSERT INTO portal.menu_menu VALUES (55, 'Escola', NULL, NULL, '/intranet/educar_index.php', 4, true, 'fa-leanpub');
-INSERT INTO portal.menu_menu VALUES (69, 'Transporte escolar', NULL, NULL, '/intranet/educar_transporte_escolar_index.php', 7, true, 'fa-bus');
-INSERT INTO portal.menu_menu VALUES (57, 'Biblioteca', NULL, NULL, '/intranet/educar_biblioteca_index.php', 8, true, 'fa-book');
-INSERT INTO portal.menu_menu VALUES (25, 'Configurações', 'Sistemas do RH', NULL, '/intranet/educar_configuracoes_index.php', 9, true, 'fa-gear');
-INSERT INTO portal.menu_menu VALUES (70, 'Educacenso', NULL, NULL, '/intranet/educar_educacenso_index.php', 6, true, 'fa-bar-chart');
-INSERT INTO portal.menu_menu VALUES (71, 'Servidores', '', NULL, '/intranet/educar_servidores_index.php', 5, true, 'fa-users');
-
-
---
 -- TOC entry 8394 (class 0 OID 0)
 -- Dependencies: 785
 -- Name: menu_menu_cod_menu_menu_seq; Type: SEQUENCE SET; Schema: portal; Owner: -
