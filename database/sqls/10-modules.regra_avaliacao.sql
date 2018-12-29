c6c565e411c9575267e415d0877079a68714b053
Eder Soares <edersoares@me.com>
2018-12-29 21:10:58 -0200

    Seed: modules.regra_avaliacao

---
+++ database/sqls/10-modules.regra_avaliacao.sql
@@ -0,0 +1,2 @@
+INSERT INTO modules.regra_avaliacao VALUES (1, 1, 1, 2, 1, 'Regra de avaliação quantitativa', 1, 2, 6.000, 75.000, 0, 2, 6.000, 0, NULL, 10, 10, 2, 0, 0, 0, 0, 0, 0, 0, NULL, NULL);
+INSERT INTO modules.regra_avaliacao VALUES (2, 1, 1, NULL, 2, 'Regra de avaliação qualitativa', 2, 1, 6.000, 80.000, 0, 2, 6.000, 0, NULL, 10, 10, 5, 0, 0, 0, 0, 0, 0, 0, NULL, NULL);
