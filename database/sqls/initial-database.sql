c37b36deeb6dc1285652965926afbb29c45630a7
Eder Soares <edersoares@me.com>
2018-12-29 21:09:23 -0200

    Seed: modules.etapas_educacenso

--- database/sqls/initial-database.sql
+++ database/sqls/initial-database.sql
@@ -21812,26 +21812,6 @@ SELECT pg_catalog.setval('modules.empresa_transporte_escolar_seq', 1, false);
 --
 
 
-
---
--- TOC entry 7684 (class 0 OID 9443799)
--- Dependencies: 395
--- Data for Name: etapas_educacenso; Type: TABLE DATA; Schema: modules; Owner: -
---
-
-INSERT INTO modules.etapas_educacenso VALUES (1, 'Educação Infantil - Creche (0 a 3 anos)');
-INSERT INTO modules.etapas_educacenso VALUES (2, 'Educação Infantil - Pré-escola (4 a 5 anos)');
-INSERT INTO modules.etapas_educacenso VALUES (3, 'Ensino Fundamental 8 anos');
-INSERT INTO modules.etapas_educacenso VALUES (4, 'Ensino Fundamental 9 anos');
-INSERT INTO modules.etapas_educacenso VALUES (5, 'Ensino médio - Médio');
-INSERT INTO modules.etapas_educacenso VALUES (6, 'Ensino médio - Integrado');
-INSERT INTO modules.etapas_educacenso VALUES (7, 'Ensino médio - Normal/Magistério');
-INSERT INTO modules.etapas_educacenso VALUES (8, 'Ensino médio - Educação profissional');
-INSERT INTO modules.etapas_educacenso VALUES (9, 'EJA - Ensino fundamental');
-INSERT INTO modules.etapas_educacenso VALUES (10, 'EJA - Ensino médio');
-INSERT INTO modules.etapas_educacenso VALUES (11, 'EJA - Ensino fundamental - Projovem (urbano)');
-
-
 --
 -- TOC entry 7685 (class 0 OID 9443802)
 -- Dependencies: 396
