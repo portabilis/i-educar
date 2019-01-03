62052e30b66374416f4b3d3704bc7e46f616ec2f
Vitor Mattos <vitor.mattos@phprio.org>
2019-01-03 11:12:03 -0200

    Remoção de require desnecessário Renomeando classe para compatibilidade com nome da classe testada

--- tests/Unit/Modules/FaltaAlunoTest.php
+++ tests/Unit/Modules/FaltaAlunoTest.php
@@ -29,8 +29,6 @@
  * @version     $Id$
  */
 
-require_once 'Avaliacao/Model/FaltaAluno.php';
-
 /**
  * FaltaAlunoTest class.
  *
@@ -42,7 +40,7 @@ require_once 'Avaliacao/Model/FaltaAluno.php';
  * @since       Classe disponível desde a versão 1.1.0
  * @version     @@package_version@@
  */
-class FaltaAlunoTest extends UnitBaseTest
+class Avaliacao_Model_FaltaAlunoTest extends UnitBaseTest
 {
   protected $_entity = NULL;
 
