38e549865855b660ff325c1c7ff2b2bf88ca80de
Rodrigo Rodrigues <rodrigogbgod@gmail.com>
2019-01-04 15:30:48 -0200

    Remove texto desnecessário;

--- ieducar/intranet/educar_matricula_turma_cad.php
+++ ieducar/intranet/educar_matricula_turma_cad.php
@@ -1,33 +1,5 @@
 <?php
 
-/**
- * i-Educar - Sistema de gestão escolar
- *
- * Copyright (C) 2006  Prefeitura Municipal de Itajaí
- *                     <ctima@itajai.sc.gov.br>
- *
- * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
- * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
- * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
- * qualquer versão posterior.
- *
- * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
- * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
- * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
- * do GNU para mais detalhes.
- *
- * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
- * com este programa; se não, escreva para a Free Software Foundation, Inc., no
- * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Arquivo disponível desde a versão 1.0.0
- * @version   $Id$
- */
-
 require_once 'include/clsBase.inc.php';
 require_once 'include/clsCadastro.inc.php';
 require_once 'include/clsBanco.inc.php';
@@ -35,16 +7,6 @@ require_once 'include/pmieducar/geral.inc.php';
 require_once 'include/pmieducar/clsPmieducarMatricula.inc.php';
 require_once 'lib/Portabilis/Date/Utils.php';
 
-/**
- * clsIndexBase class.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Classe disponível desde a versão 1.0.0
- * @version   @@package_version@@
- */
 class clsIndexBase extends clsBase
 {
   function Formular()
@@ -55,16 +17,6 @@ class clsIndexBase extends clsBase
   }
 }
 
-/**
- * indice class.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Classe disponível desde a versão 1.0.0
- * @version   @@package_version@@
- */
 class indice extends clsCadastro
 {
   var $pessoa_logada;
@@ -132,7 +84,7 @@ class indice extends clsCadastro
                 <script type="text/javascript">
                     window.alert("%s");
                     window.location.href= "./educar_matricula_det.php?cod_matricula=%u";
-                </script>',$this->mensagem,$this->ref_cod_matricula);
+                </script>', $this->mensagem, $this->ref_cod_matricula);
           echo $alert;
       } else {
         header('Location: educar_matricula_det.php?cod_matricula=' . $this->ref_cod_matricula);
