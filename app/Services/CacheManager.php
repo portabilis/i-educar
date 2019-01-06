2e9937eec102f97c5ca2c0a518ab8c1377a15b7e
Everton Muniz <munizeverton@gmail.com>
2019-01-06 21:35:51 -0200

    Trata drivers de cache que não suportam tags

--- app/Services/CacheManager.php
+++ app/Services/CacheManager.php
@@ -14,20 +14,44 @@ class CacheManager extends LaravelCacheManager
      */
     public static function invalidateByTags($tags)
     {
+        if (!self::supportsTags()) {
+            return Cache::store();
+        }
+
         Cache::tags($tags)->flush();
+    }
+
+    private static function supportsTags()
+    {
+        $doNotSupportTags = [
+            'file', 'database'
+        ];
+
+        if (in_array(config('cache.default'), $doNotSupportTags)) {
+            return false;
+        }
+
+        return true;
     }
 
     /**
      * Sobreescreve o método que chama todas as ações de cache para setar o prefixo, que será definido de
      * acordo com o tenant
      *
-     * @param  string  $method
-     * @param  array   $parameters
+     * @param  string $method
+     * @param  array $parameters
      * @return mixed
      */
     public function __call($method, $parameters)
     {
-        $this->store()->setPrefix(config('app.name'));
+        if ($method == 'tags' && !self::supportsTags()) {
+            return $this->store();
+        }
+
+        if (self::supportsTags()) {
+            $this->store()->setPrefix(config('app.name'));
+        }
+
         return $this->store()->$method(...$parameters);
     }
 }
