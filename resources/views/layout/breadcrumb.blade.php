698413fb4f2c842b8c1711c0f793a3a226412fad
Everton Muniz <munizeverton@gmail.com>
2019-01-08 16:37:02 -0200

    Trata breadcrumbs dos arquivos legados

--- resources/views/layout/breadcrumb.blade.php
+++ resources/views/layout/breadcrumb.blade.php
@@ -1,17 +1,23 @@
-<div id="localizacao">
-    <a href="{{ route('home') }}" title="Ir para o Início">
-        <i class="fa fa-home" aria-hidden="true"></i><span>Início</span>
-    </a>
+@if ($breadcrumb->pages())
+    <div id="localizacao">
+        <a href="{{ route('home') }}" title="Ir para o Início">
+            <i class="fa fa-home" aria-hidden="true"></i><span>Início</span>
+        </a>
 
-    @if($breadcrumb->hasPages())
-    <a class="flechinha"> / </a>
-    @endif
+        @if($breadcrumb->hasPages())
+            <a class="flechinha"> / </a>
+        @endif
 
-    @foreach ($breadcrumb->pages() as $page)
-        <a href="{{ $page->link }}" title="{{ $page->label }}">{{ $page->label }}</a>
-        <a class="flechinha"> / </a>
-    @endforeach
+        @foreach ($breadcrumb->pages() as $page)
+            <a href="{{ $page->link }}" title="{{ $page->label }}">{{ $page->label }}</a>
+            <a class="flechinha"> / </a>
+        @endforeach
 
-    <span class="pagina_atual">{{ $breadcrumb->currentPage() }}</span>
+        <span class="pagina_atual">{{ $breadcrumb->currentPage() }}</span>
 
-</div>
+    </div>
+@endif
+
+@if ($breadcrumb->getLegacy())
+    {!! $breadcrumb->getLegacy() !!}
+@endif
