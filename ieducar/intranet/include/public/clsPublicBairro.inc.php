3e35ce3873cba48fef2e4b08faaa938998a7ba1f
Everton Muniz <munizeverton@gmail.com>
2019-01-17 17:06:13 -0200

    Escapa string dos campos de nome nos cadastros de endereço

--- ieducar/intranet/include/public/clsPublicBairro.inc.php
+++ ieducar/intranet/include/public/clsPublicBairro.inc.php
@@ -325,7 +325,7 @@ class clsPublicBairro
 
       if (is_string($this->nome)) {
         $campos  .= "{$gruda}nome";
-        $valores .= "{$gruda}'" . addslashes($this->nome) . "'";
+        $valores .= "{$gruda}'" . pg_escape_string($this->nome) . "'";
         $gruda    = ', ';
       }
 
