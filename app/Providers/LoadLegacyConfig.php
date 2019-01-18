ebb104c9d99651f9fd31b20a839585b732691844
Everton Muniz <munizeverton@gmail.com>
2019-01-18 11:12:47 -0200

    Corrige bug ao se connectar ao banco do tenant

--- app/Providers/LoadLegacyConfig.php
+++ app/Providers/LoadLegacyConfig.php
@@ -15,7 +15,11 @@ class LoadLegacyConfig extends ServiceProvider
     public function register()
     {
         $legacyConfigPath = base_path() . '/' . config('legacy.path') . '/configuration/';
-        $configObject = new LegacyConfig($legacyConfigPath, config('app.env'), request()->getHttpHost());
-        \Config::set($configObject->getArrayConfig());
+
+        $configObject = new LegacyConfig($legacyConfigPath, config('legacy.env'), request()->getHttpHost());
+
+        $laravelLegacyConfig = config()->get('legacy');
+
+        config()->set(['legacy' => array_merge($laravelLegacyConfig, $configObject->getArrayConfig())]);
     }
 }
