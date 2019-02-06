3cea10da8ff4bf40b051b27e5a1428853b2188e1
Eder Soares <edersoares@me.com>
2019-02-06 14:34:47 -0200

    Cria model para turma

---
+++ app/Models/SchoolClass.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SchoolClass extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'pmieducar.turma';
+}
