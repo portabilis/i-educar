4caed12f945b7b1e73419614b2e8e50b65faba09
Eder Soares <edersoares@me.com>
2019-02-07 21:15:22 -0200

    Course: Curso

---
+++ tests/Unit/Eloquent/CourseTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit\Eloquent;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CourseTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
