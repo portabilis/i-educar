e550e5a38afbed2258def287635ffa8d86e92c66
Everton Muniz <munizeverton@gmail.com>
2019-02-09 16:26:34 -0200

    Remove view não usada

--- resources/views/inputs/turno.blade.php
+++
@@ -1,6 +0,0 @@
-<select name="turno_id" id="turno_id" class="geral">
-    <option value="">Selecione</option>
-    @foreach($turnos as $id => $value)
-        <option value="{{$id}}">{{$value}}</option>
-    @endforeach
-</select>
