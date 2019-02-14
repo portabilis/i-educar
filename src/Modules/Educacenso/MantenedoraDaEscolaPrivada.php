6743ef6579cec14487b0fd6f235133a5ed95c7c1
bonot <paula_bonot@hotmail.com>
2019-02-14 15:39:52 -0200

    Ajusta nomenclatura das opções

--- src/Modules/Educacenso/MantenedoraDaEscolaPrivada.php
+++ src/Modules/Educacenso/MantenedoraDaEscolaPrivada.php
@@ -21,7 +21,7 @@ class MantenedoraDaEscolaPrivada
             self::SINDICATOS_TRABALHISTAS => 'Sindicatos de trabalhadores ou patronais, associações ou cooperativas',
             self::ORGANIZACOES_NAO_GOVERNAMENTAIS => 'Organização não governamental - ONG internacional ou nacional',
             self::INSTITUICOES_SIM_FINS_LUCRATIVOS => 'Instituições sem fins lucrativos',
-            self::SISTEMA_S => 'Sistema S (Sesi, Senai, Sesc, outros',
+            self::SISTEMA_S => 'Sistema S (Sesi, Senai, Sesc, outros)',
             self::OSCIP => 'Organização da Sociedade Civil de Interesse Público (Oscip)',
         ];
     }
