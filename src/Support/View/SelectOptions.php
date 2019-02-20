4e2a7ee2ab0783b273b9810e8e4e504452aeb564
Eder Soares <edersoares@me.com>
2019-02-20 18:03:30 -0300

    Default option

--- src/Support/View/SelectOptions.php
+++ src/Support/View/SelectOptions.php
@@ -7,12 +7,22 @@ use iEducar\Modules\Transport\Period;
 class SelectOptions
 {
     /**
+     * Retorna a opção default para os selects.
+     *
+     * @return array
+     */
+    public static function getDefaultOption()
+    {
+        return [0 => 'Selecione'];
+    }
+
+    /**
      * Retorna as opções disponíveis de turno para o módulo de transporte.
      *
      * @return array
      */
     public static function transportPeriods()
     {
-        return [0 => 'Selecione'] + Period::getDescriptiveValues();
+        return self::getDefaultOption() + Period::getDescriptiveValues();
     }
 }
