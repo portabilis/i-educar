1040ad3495d537b45b8b24d071b552baae786866
Everton Muniz <munizeverton@gmail.com>
2019-02-27 08:57:06 -0300

    Desabilita temporariamente a exportação do censo

--- ieducar/modules/Educacenso/Assets/Javascripts/Educacenso.js
+++ ieducar/modules/Educacenso/Assets/Javascripts/Educacenso.js
@@ -20,8 +20,7 @@ $j(document).ready(function(){
             ' <p style="margin-left: 20px; margin-top: 30px;font-family: verdana, arial; font-size: 18px;">Aguarde, os dados est&atilde;o sendo exportados</p>' +
             '</div>'+
             '<div id="modal_mensagem_sucesso" style="width:400px;display:none;">'+
-            ' <p style=" margin-top: 20px;font-family: verdana, arial; font-size: 18px;">Exporta&ccedil;&atilde;o realizada com sucesso.</p>' +
-            ' <a id="download_file" href="#" style="margin-top: 10px;font-family: verdana, arial;font-size: 14px;">Clique aqui para realizar o download</a>' +
+            ' <p style=" margin-top: 20px;font-family: verdana, arial; font-size: 18px;">A exportação está desabilitada temporariamente</p>' +
             '</div>'+
             '</div>';
 
