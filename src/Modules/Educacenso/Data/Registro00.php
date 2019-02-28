0407a90d96ef4c687f38c7fd02ba4b1863a23f89
Everton Muniz <munizeverton@gmail.com>
2019-02-28 10:14:47 -0300

    Formata campo email da escola

--- src/Modules/Educacenso/Data/Registro00.php
+++ src/Modules/Educacenso/Data/Registro00.php
@@ -41,6 +41,7 @@ class Registro00 extends AbstractRegistro
         $this->model->numero = $this->convertStringToCenso($this->model->numero);
         $this->model->complemento = $this->convertStringToCenso($this->model->complemento);
         $this->model->bairro = $this->convertStringToCenso($this->model->bairro);
+        $this->model->email = strtoupper($this->model->email);
         $this->model->orgaoRegional = ($this->model->orgaoRegional ? str_pad($this->model->orgaoRegional, 5, "0", STR_PAD_LEFT) : null);
         $this->model->cnpjEscolaPrivada = $this->cnpjToCenso($this->model->cnpjEscolaPrivada);
         $this->model->cnpjMantenedoraPrincipal = $this->cnpjToCenso($this->model->cnpjMantenedoraPrincipal);
