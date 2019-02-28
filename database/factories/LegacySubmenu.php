3b85d23a2c58e24d57af79d00e790d89988161b0
Éber Freitas Dias <eber.freitas@gmail.com>
2019-02-28 13:01:09 -0300

    Corrige relação de model legado em factory

--- database/factories/LegacySubmenu.php
+++ database/factories/LegacySubmenu.php
@@ -6,7 +6,7 @@ $factory->define(\App\Models\LegacySubmenu::class, function (Faker $faker) {
     return [
         'nm_submenu' => $faker->words(2, true),
         'ref_cod_menu_menu' => function() {
-            return factory(\App\Models\Menu::class)->create()->cod_menu_menu;
+            return factory(\App\Models\LegacyMenu::class)->create()->cod_menu_menu;
         },
         'cod_sistema' => 2,
         'nivel' => $faker->randomElement([2,3]),
