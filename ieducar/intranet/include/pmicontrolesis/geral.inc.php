2ef833e77d5af389a3df5ddc610d45a347a805ed
Eder Soares <edersoares@me.com>
2019-03-02 01:13:44 -0300

    Remove tabela topo_portal

--- ieducar/intranet/include/pmicontrolesis/geral.inc.php
+++ ieducar/intranet/include/pmicontrolesis/geral.inc.php
@@ -5,5 +5,4 @@ require_once 'include/clsBanco.inc.php';
 require_once 'include/Geral.inc.php';
 require_once 'include/pmicontrolesis/clsMenuSuspenso.inc.php';
 require_once 'include/pmicontrolesis/clsTutormenu.inc.php';
-require_once 'include/pmicontrolesis/clsPmicontrolesisTopoPortal.inc.php';
 require_once 'include/pmicontrolesis/clsPmicontrolesisMenuPortal.inc.php';
