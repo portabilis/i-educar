99e71993df01462e9d419d1861110a7b9c7f0bd8
Eder Soares <edersoares@me.com>
2019-03-03 18:48:50 -0300

    Remove tabelas de licitações

--- ieducar/intranet/include/portal/geral.inc.php
+++ ieducar/intranet/include/portal/geral.inc.php
@@ -9,7 +9,6 @@ require_once 'include/portal/clsPortalMenuMenu.inc.php';
 require_once 'include/portal/clsPortalMenuSubmenu.inc.php';
 require_once 'include/portal/clsPortalNotificacao.inc.php';
 require_once 'include/portal/clsPortalArtigoOpiniao.inc.php';
-require_once 'include/portal/clsPortalComprasModalidade.inc.php';
 require_once 'include/portal/clsPortalFiaAgenda.inc.php';
 require_once 'include/portal/clsPortalFiaAplicacao.inc.php';
 require_once 'include/portal/clsPortalFiaContas.inc.php';
