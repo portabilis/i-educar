f4ab2fe8b483309a307bdbb6cff36491966b3f20
Eder Soares <edersoares@me.com>
2019-03-03 19:10:57 -0300

    Remove classe clsPortalFiaFaleConosco

--- ieducar/intranet/include/portal/geral.inc.php
+++ ieducar/intranet/include/portal/geral.inc.php
@@ -6,5 +6,4 @@ require_once 'include/portal/clsPortalFuncionario.inc.php';
 require_once 'include/portal/clsPortalMenuFuncionario.inc.php';
 require_once 'include/portal/clsPortalMenuMenu.inc.php';
 require_once 'include/portal/clsPortalMenuSubmenu.inc.php';
-require_once 'include/portal/clsPortalFiaFaleConosco.inc.php';
 require_once 'clsPortalSmsProntuario.inc.php';
