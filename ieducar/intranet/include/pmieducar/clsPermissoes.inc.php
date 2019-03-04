f87f335e6246700f6fa97d30b905e0b1c72f3884
Eder Soares <edersoares@me.com>
2019-03-04 11:44:58 -0300

    Remove comentários de clsPermissoes

--- ieducar/intranet/include/pmieducar/clsPermissoes.inc.php
+++ ieducar/intranet/include/pmieducar/clsPermissoes.inc.php
@@ -1,56 +1,12 @@
 <?php
 
-/**
- * i-Educar - Sistema de gestão escolar
- *
- * Copyright (C) 2006  Prefeitura Municipal de Itajaí
- *                     <ctima@itajai.sc.gov.br>
- *
- * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
- * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
- * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
- * qualquer versão posterior.
- *
- * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
- * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
- * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
- * do GNU para mais detalhes.
- *
- * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
- * com este programa; se não, escreva para a Free Software Foundation, Inc., no
- * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Arquivo disponível desde a versão 1.0.0
- * @version   $Id$
- */
-
 require_once 'include/pmieducar/geral.inc.php';
 require_once 'include/clsMenuFuncionario.inc.php';
 require_once 'include/pmieducar/clsPmieducarEscolaUsuario.inc.php';
 require_once 'lib/Portabilis/Array/Utils.php';
 
-
-/**
- * clsPermissoes class.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Classe disponível desde a versão 1.0.0
- * @todo      Eliminar a lógica duplicada dos métodos permissao_*
- * @version   @@package_version@@
- */
 class clsPermissoes
 {
-  function __construct()
-  {
-  }
-
   /**
    * Verifica se um usuário tem permissão para cadastrar baseado em um
    * identificador de processo.
