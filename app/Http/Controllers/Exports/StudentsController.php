b8377afdb894fd5e6cce1a45d117b9044fb02c5a
Éber Freitas Dias <eber.freitas@gmail.com>
2019-03-07 17:30:15 -0300

    Move a resp. de traduzir chaves do request para o request em si

--- app/Http/Controllers/Exports/StudentsController.php
+++ app/Http/Controllers/Exports/StudentsController.php
@@ -11,39 +11,9 @@ class StudentsController extends Controller
 {
     public function export(Request $request)
     {
-        $params = $this->translateParams($request->all());
+        $params = $request->allWithTranslatedKeys();
         $export = new StudentsExport($params);
 
         return Excel::download($export, 'alunos.xlsx');
-    }
-
-    protected function translateParams($params)
-    {
-        $paramMap = [
-            'cod_aluno' => 'id',
-            'cod_inep' => 'inep_code',
-            'aluno_estado_id' => 'registry_code',
-            'nome_aluno' => 'student_name',
-            'data_nascimento' => 'birthdate',
-            'nome_pai' => 'father_name',
-            'nome_mae' => 'mother_name',
-            'nome_responsavel' => 'guardian_name',
-            'ano' => 'year',
-            'ref_cod_escola' => 'school_id',
-            'ref_cod_curso' => 'course_id',
-            'ref_cod_serie' => 'level_id',
-        ];
-
-        $newParams = [];
-
-        foreach ($params as $k => $v) {
-            if (!in_array($k, array_keys($paramMap))) {
-                continue;
-            }
-
-            $newParams[$paramMap[$k]] = $v;
-        }
-
-        return $newParams;
     }
 }
--- app/Http/Requests/StudentsExport.php
+++ app/Http/Requests/StudentsExport.php
@@ -13,7 +13,7 @@ class StudentsExport extends FormRequest
      */
     public function authorize()
     {
-        return (int) session('id_pessoa') !== 0;
+        return (bool) session('id_pessoa');
     }
 
     /**
@@ -41,5 +41,36 @@ class StudentsExport extends FormRequest
             'ref_cod_curso' => 'nullable|numeric',
             'ref_cod_serie' => 'nullable|numeric',
         ];
+    }
+
+    public function allWithTranslatedKeys()
+    {
+        $paramMap = [
+            'cod_aluno' => 'id',
+            'cod_inep' => 'inep_code',
+            'aluno_estado_id' => 'registry_code',
+            'nome_aluno' => 'student_name',
+            'data_nascimento' => 'birthdate',
+            'nome_pai' => 'father_name',
+            'nome_mae' => 'mother_name',
+            'nome_responsavel' => 'guardian_name',
+            'ano' => 'year',
+            'ref_cod_escola' => 'school_id',
+            'ref_cod_curso' => 'course_id',
+            'ref_cod_serie' => 'level_id',
+        ];
+
+        $params = $this->all();
+        $newParams = [];
+
+        foreach ($params as $k => $v) {
+            if (!in_array($k, array_keys($paramMap))) {
+                continue;
+            }
+
+            $newParams[$paramMap[$k]] = $v;
+        }
+
+        return $newParams;
     }
 }
