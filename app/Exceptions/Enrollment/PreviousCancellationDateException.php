a2634b25aa3faaa38f92312182da5c77db12704c
Eder Soares <edersoares@me.com>
2019-03-15 18:09:42 -0300

    Melhor mensagem de erro

--- app/Exceptions/Enrollment/PreviousCancellationDateException.php
+++ app/Exceptions/Enrollment/PreviousCancellationDateException.php
@@ -17,7 +17,7 @@ class PreviousCancellationDateException extends RuntimeException
      */
     public function __construct(LegacyEnrollment $enrollment, DateTime $cancellationDate)
     {
-        $message = 'A data de cancelamento %s deve ser maior que a data de enturmação %s.';
+        $message = 'A data de saída %s deve ser maior que a data de enturmação %s.';
 
         $message = sprintf(
             $message, $cancellationDate->format('d/m/Y'), $enrollment->date->format('d/m/Y')
