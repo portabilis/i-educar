4189e6a3cb3da1597541f4a699809866948556ee
Eder Soares <edersoares@me.com>
2019-03-17 14:47:11 -0300

    PSR-2

--- tests/Unit/Core/ClsPmieducarQuadroHorarioHorariosTest.php
+++ tests/Unit/Core/ClsPmieducarQuadroHorarioHorariosTest.php
@@ -6,17 +6,17 @@ require_once 'include/pmieducar/clsPmieducarQuadroHorarioHorarios.inc.php';
 
 class ClsPmieducarQuadroHorarioHorariosTest extends TestCase
 {
-  /**
-   * Testa o método substituir_servidor()
-   */
-  public function testSubstituirServidor()
-  {
-    $stub = $this->getMockBuilder('clsPmieducarQuadroHorarioHorarios')->getMock();
+    /**
+     * Testa o método substituir_servidor()
+     */
+    public function testSubstituirServidor()
+    {
+        $stub = $this->getMockBuilder('clsPmieducarQuadroHorarioHorarios')->getMock();
 
-    $stub->expects($this->any())
-         ->method('substituir_servidor')
-         ->will($this->returnValue(TRUE));
+        $stub->expects($this->any())
+            ->method('substituir_servidor')
+            ->will($this->returnValue(true));
 
-    $this->assertTrue($stub->substituir_servidor(1));
-  }
+        $this->assertTrue($stub->substituir_servidor(1));
+    }
 }
