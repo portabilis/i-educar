abf0d6726ef2b278227ace7915ddaf9daa6e4d90
Eder Soares <edersoares@me.com>
2019-03-17 15:02:05 -0300

    Move migration que cria a tabela pmieducar.matricula_turma_excluidos

--- database/migrations/legacy/2019_01_01_100000_create_pmieducar_matricula_turma_excluidos_table.php
+++ database/migrations/legacy/2019_01_01_100000_create_pmieducar_matricula_turma_excluidos_table.php
@@ -1,11 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CriaTabelaMatriculaTurmaExcluidos extends Migration
+class CreatePmieducarMatriculaTurmaExcluidosTable extends Migration
 {
     /**
      * Run the migrations.
