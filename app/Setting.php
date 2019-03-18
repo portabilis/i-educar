bfe2b6a9102c4ba117bed259bb25d900e6fa9325
Eder Soares <edersoares@me.com>
2019-03-18 00:55:50 -0300

    Setting: Configuração

---
+++ app/Setting.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    const TYPE_STRING = 'string';
+    const TYPE_FLOAT = 'float';
+    const TYPE_INTEGER = 'integer';
+    const TYPE_BOOLEAN = 'boolean';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'key', 'value', 'type', 'description',
+    ];
+
+    /**
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    public function getValueAttribute($value)
+    {
+        switch ($this->type) {
+            case self::TYPE_STRING:
+                return (string) $value;
+
+            case self::TYPE_INTEGER:
+                return (int) $value;
+
+            case self::TYPE_FLOAT:
+                return (float) $value;
+
+            case self::TYPE_BOOLEAN:
+                return (boolean) $value;
+        }
+
+        return $value;
+    }
+}
