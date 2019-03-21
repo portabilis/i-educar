0305e58c720fd02fc1054482fcad350571e71581
Eder Soares <edersoares@me.com>
2019-03-21 16:49:52 -0300

    Revert "Logoff do Laravel User"
    This reverts commit 866b84826ec0093f5ee1868038ef915f82ad587e.

--- ieducar/intranet/logof.php
+++ ieducar/intranet/logof.php
@@ -2,14 +2,12 @@
 
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
 @session_start();
 $_SESSION = array();
 @session_destroy();
 
-Auth::logout();
 Session::flush();
 
 throw new HttpResponseException(
