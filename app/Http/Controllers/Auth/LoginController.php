7b581a8856a318d7b4dfc6ef3021da7603940bd2
Eder Soares <edersoares@me.com>
2019-03-29 10:48:39 -0300

    Redireciona controllers de autenticação para a raiz

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
