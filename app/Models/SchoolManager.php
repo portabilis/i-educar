9231b3ce756370f47ed86d83c2fc8055b76f3f7f
Everton Muniz <munizeverton@gmail.com>
2019-04-03 15:29:55 -0300

    Adiciona relacionamentos no model SchoolManager

--- app/Models/SchoolManager.php
+++ app/Models/SchoolManager.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class SchoolManager extends Model
 {
+    public function individual()
+    {
+        return $this->belongsTo(Individual::class, 'individual_id', 'id');
+    }
 
+    public function school()
+    {
+        return $this->belongsTo(School::class, 'school_id', 'cod_escola');
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(ManagerRole::class, 'role_id', 'id');
+    }
+
+    public function accessCriteria()
+    {
+        return $this->belongsTo(ManagerAccessCriteria::class, 'access_criteria_id', 'id');
+    }
+
+    public function linkType()
+    {
+        return $this->belongsTo(ManagerLinkType::class, 'link_type_id', 'id');
+    }
 }
