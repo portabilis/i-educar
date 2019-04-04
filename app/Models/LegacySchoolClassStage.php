8e5e98b57941bb6ff9bc31bbb6c76eacc32ec239
Eder Soares <edersoares@me.com>
2019-04-04 10:59:02 -0300

    LegacySchoolClassStage: Turma etapa

---
+++ app/Models/LegacySchoolClassStage.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LegacySchoolClassStage extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'pmieducar.turma_modulo';
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'ref_cod_turma';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'ref_cod_turma',
+        'ref_cod_modulo',
+        'sequencial',
+        'data_inicio',
+        'data_fim',
+        'dias_letivos',
+    ];
+
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+}
