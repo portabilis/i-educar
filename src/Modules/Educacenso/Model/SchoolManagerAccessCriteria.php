c0e46ed1523d82a118a947e47dded73cc9702736
Everton Muniz <munizeverton@gmail.com>
2019-04-04 12:05:17 -0300

    Cria classe para validar as regras do censo nos gestores da escola

---
+++ src/Modules/Educacenso/Model/SchoolManagerAccessCriteria.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace iEducar\Modules\Educacenso\Model;
+
+class SchoolManagerAccessCriteria
+{
+    const OUTRO = 7;
+}
---
+++ src/Modules/Educacenso/Validator/Managers.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace iEducar\Modules\Educacenso\Validator;
+
+use iEducar\Modules\Educacenso\ExportRule\DependenciaAdministrativa;
+use iEducar\Modules\Educacenso\Model\DependenciaAdministrativaEscola;
+use iEducar\Modules\Educacenso\Model\SchoolManagerAccessCriteria;
+use iEducar\Modules\Educacenso\Model\SchoolManagerRole;
+
+class Managers implements EducacensoValidator
+{
+    private $message;
+    private $individualArray;
+    private $roleArray;
+    private $accessCriteriaArray;
+    private $accessCriteriaDescriptionArray;
+    private $linkTypeArray;
+    private $isChiefArray;
+    private $administrativeDependency;
+    private $valid = true;
+
+    /**
+     * @param integer[] $individualArray
+     * @param integer[] $roleArray
+     * @param integer[] $accessCriteriaArray
+     * @param string[] $accessCriteriaDescriptionArray
+     * @param integer[] $linkTypeArray
+     * @param boolean[] $isChiefArray
+     */
+    public function __construct($individualArray, $roleArray, $accessCriteriaArray, $accessCriteriaDescriptionArray, $linkTypeArray, $isChiefArray, $administrativeDependency)
+    {
+        $this->individualArray = $individualArray;
+        $this->roleArray = $roleArray;
+        $this->accessCriteriaArray = $accessCriteriaArray;
+        $this->accessCriteriaDescriptionArray = $accessCriteriaDescriptionArray;
+        $this->linkTypeArray = $linkTypeArray;
+        $this->isChiefArray = $isChiefArray;
+        $this->administrativeDependency = $administrativeDependency;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isValid(): bool
+    {
+        if ($this->containsEmptyOrIsNull($this->individualArray)) {
+            $this->message[] = 'Você precisa cadastrar pelo menos um gestor';
+            $this->valid = false;
+        }
+
+        if ($this->containsEmptyOrIsNull($this->roleArray)) {
+            $this->message[] = 'Você precisa informar o cargo dos um gestores';
+            $this->valid = false;
+        }
+
+        foreach ($this->individualArray as $key => $value) {
+            $this->validateAccessCriteria($key);
+            $this->validateAccessCriteriaDescription($key);
+            $this->validateAccessLinkType($key);
+        }
+
+        return $this->valid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param $array
+     * @return bool
+     */
+    private function containsEmptyOrIsNull($array)
+    {
+        return empty($array) || in_array(null, $array);
+    }
+
+    /**
+     * @param $key
+     */
+    private function validateAccessCriteria($key)
+    {
+        if ($this->roleArray[$key] == SchoolManagerRole::DIRETOR && empty($this->accessCriteriaArray[$key])) {
+            $this->valid = false;
+            $this->message[] = 'Se o cargo do gestor for <b>Diretor</b>, você precisa informar o critério de acesso ao cargo';
+        }
+    }
+
+    private function validateAccessCriteriaDescription($key)
+    {
+        if ($this->accessCriteriaArray[$key] == SchoolManagerAccessCriteria::OUTRO && empty($this->accessCriteriaArray[$key])) {
+            $this->valid = false;
+            $this->message[] = 'Se o citério de acesso ao cargo for <b>Outros</b>, você precisa informar uma especificação';
+        }
+    }
+
+    private function validateAccessLinkType(int $key)
+    {
+        if ($this->administrativeDependency == DependenciaAdministrativaEscola::PRIVADA) {
+            return;
+        }
+
+        if ($this->roleArray[$key] == SchoolManagerRole::DIRETOR && empty($this->linkTypeArray[$key])) {
+            $this->valid = false;
+            $this->message[] = 'O campo <b>Tipo de vínculo</b> precisa ser preenchido';
+        }
+    }
+}
