e1c8afb2188bb895247dc4051157141c5e6a6dab
Éber Freitas Dias <eber.freitas@gmail.com>
2019-04-05 16:14:44 -0300

    Ajustes visuais e de conteúdo

--- resources/views/partials/flash-message.blade.php
+++ resources/views/partials/flash-message.blade.php
@@ -1,5 +1,21 @@
 <div class="flashMessages">
-    <div class="flashMessages__container"></div>
+    <div class="flashMessages__container">
+        @if ($message = Session::get('success'))
+            <div title="Clique para fechar" class="flashMessages__message -success"><time>{{ date('H:i:s', time()) }}</time>{{ $message }}</div>
+        @endif
+
+        @if ($message = Session::get('error'))
+            <div title="Clique para fechar" class="flashMessages__message -error"><time>{{ date('H:i:s', time()) }}</time>{{ $message }}</div>
+        @endif
+
+        @if ($message = Session::get('notice'))
+            <div title="Clique para fechar" class="flashMessages__message -notice"><time>{{ date('H:i:s', time()) }}</time>{{ $message }}</div>
+        @endif
+
+        @if ($message = Session::get('info'))
+            <div title="Clique para fechar" class="flashMessages__message -info"><time>{{ date('H:i:s', time()) }}</time>{{ $message }}</div>
+        @endif
+    </div>
     <ul class="flashMessages__controls">
         <li><a href="#" data-action="showAll">mostrar todos (+<span>0</span>)</a></li><li><a href="#" data-action="closeAll">fechar todos</a></li>
     </ul>
