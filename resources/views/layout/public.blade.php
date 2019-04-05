4a154e36ee80c145e79b9c1e05614197ec3911bd
Eder Soares <edersoares@me.com>
2019-04-05 15:00:35 -0300

    Customiza e-mails de redefinição de senha

--- resources/views/layout/public.blade.php
+++ resources/views/layout/public.blade.php
@@ -41,7 +41,7 @@
         <img alt="Logo" width="150px" height="150px" src="{{ config('legacy.config.ieducar_image') ?? url('intranet/imagens/brasao-republica.png') }}"/>
     </div>
 
-    <h1>{{ config('legacy.config.ieducar_entity_name')}}</h1>
+    <h1>{{ config('legacy.config.ieducar_entity_name') }}</h1>
 
     @if (session('status'))
         <p class="success">{{ session('status') }}</p>
