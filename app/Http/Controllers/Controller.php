d5757a7d0bc825d38f79c94fbb47571d47058258
Eder Soares <edersoares@me.com>
2019-04-17 13:56:57 -0300

    Exibe menu em páginas que utilizam rotas próprias

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Menu;
 use iEducar\Support\Navigation\Breadcrumb;
 use iEducar\Support\Navigation\TopMenu;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 
 class Controller extends BaseController
 {
@@ -37,5 +40,29 @@ class Controller extends BaseController
         }
 
         app(TopMenu::class)->current($currentSubmenuId, $currentUri);
+    }
+
+    /**
+     * Share with view, title, mainmenu and menu links.
+     * 
+     * @param int $process
+     *
+     * @return void
+     */
+    public function menu($process)
+    {
+        $user = Auth::user();
+        $menu = Menu::user($user);
+
+        $topmenu = Menu::query()
+            ->where('process', $process)
+            ->first();
+
+        if ($topmenu) {
+            View::share('mainmenu', $topmenu->root()->getKey());
+        }
+
+        View::share('menu', $menu);
+        View::share('title', '');
     }
 }
