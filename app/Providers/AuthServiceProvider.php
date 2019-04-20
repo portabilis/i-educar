a42ff0b195ea2ac9eb6cfac8dab41601ba0c8f45
Eder Soares <edersoares@me.com>
2019-04-20 19:58:14 -0300

    Implementa os gates de segurança

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Extensions\LegacyUserProvider;
+use App\Policies\ProcessPolicy;
 use App\User;
 use Illuminate\Contracts\Hashing\Hasher;
 use Illuminate\Support\Facades\Auth;
@@ -32,6 +33,16 @@ class AuthServiceProvider extends ServiceProvider
         Auth::provider('legacy', function ($app) {
             return new LegacyUserProvider($app->make(Hasher::class));
         });
+
+        Gate::before(function (User $user) {
+            if ($user->isAdmin()) {
+                return true;
+            }
+        });
+
+        Gate::define('view', ProcessPolicy::class . '@view');
+        Gate::define('modify', ProcessPolicy::class . '@modify');
+        Gate::define('remove', ProcessPolicy::class . '@remove');
     }
 
     /**
