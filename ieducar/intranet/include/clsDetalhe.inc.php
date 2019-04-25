e82d4a22b21987dcf4acf8f0ddd792017bb17ce2
Eder Soares <edersoares@me.com>
2019-04-25 12:31:58 -0300

    Remove docs de clsDetalhe

--- ieducar/intranet/include/clsDetalhe.inc.php
+++ ieducar/intranet/include/clsDetalhe.inc.php
@@ -1,64 +1,21 @@
 <?php
 
-/**
- * i-Educar - Sistema de gestão escolar
- *
- * Copyright (C) 2006  Prefeitura Municipal de Itajaí
- *                     <ctima@itajai.sc.gov.br>
- *
- * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
- * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
- * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
- * qualquer versão posterior.
- *
- * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
- * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
- * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
- * do GNU para mais detalhes.
- *
- * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
- * com este programa; se não, escreva para a Free Software Foundation, Inc., no
- * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Include
- * @since     Arquivo disponível desde a versão 1.0.0
- * @version   $Id$
- */
-
 use iEducar\Support\Navigation\Breadcrumb;
 use Illuminate\Support\Facades\View;
 
 require_once 'Core/Controller/Page/Abstract.php';
 require_once 'include/localizacaoSistema.php';
 
-/**
- * clsDetalhe class.
- *
- * Cria um template para a visualização de um registro de alguma tabela do banco
- * de dados.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Include
- * @since     Classe disponível desde a versão 1.0.0
- * @version   @@package_version@@
- */
 class clsDetalhe extends Core_Controller_Page_Abstract
 {
   var $titulo;
   var $largura;
   var $detalhe = array();
-
   var $url_novo;
   var $caption_novo = "Novo";
   var $url_editar;
   var $url_cancelar;
   var $nome_url_cancelar = "Voltar";
-
   var $array_botao;
   var $array_botao_url;
   var $array_botao_url_script;
@@ -74,7 +31,6 @@ class clsDetalhe extends Core_Controller_Page_Abstract
   ) {
       // Método permanece somente para fins de compatibilidade e evitar erros.
   }
-
 
   function addDetalhe($detalhe) {
     $this->detalhe[] = $detalhe;
