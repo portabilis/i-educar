94d2195514598776b706dd476a72910f701bedd4
Éber Freitas Dias <eber.freitas@gmail.com>
2019-04-26 02:49:16 -0300

    Documentação basica do BaseModifier

--- src/Reports/BaseModifier.php
+++ src/Reports/BaseModifier.php
@@ -4,15 +4,32 @@ namespace iEducar\Reports;
 
 abstract class BaseModifier
 {
+    /**
+     * @var string
+     */
     protected $templateName;
 
+    /**
+     * @var array
+     */
     protected $args;
 
+    /**
+     * @param string $templateName
+     * @param array $args
+     *
+     * @return void
+     */
     public function __construct($templateName, $args)
     {
         $this->templateName = $templateName;
         $this->args = $args;
     }
 
+    /**
+     * @param array $data
+     *
+     * @return array
+     */
     abstract public function modify($data);
 }
