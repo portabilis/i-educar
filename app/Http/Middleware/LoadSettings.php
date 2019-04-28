ee261fbaa2ca321819c9ec3d27ad97de4f97859a
Eder Soares <edersoares@me.com>
2019-04-28 20:25:04 -0300

    Ajuste ao carregar configurações

--- app/Http/Middleware/LoadSettings.php
+++ app/Http/Middleware/LoadSettings.php
@@ -6,9 +6,26 @@ use Closure;
 use App\Setting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 
 class LoadSettings
 {
+    /**
+     * Return configurations for institution.
+     *
+     * @return array
+     */
+    private function getConfig()
+    {
+        $config = (array) DB::table('pmieducar.configuracoes_gerais as cg')
+            ->select('cg.*')
+            ->join('pmieducar.instituicao as i', 'cod_instituicao', '=', 'ref_cod_instituicao')
+            ->where('i.ativo', 1)
+            ->first();
+
+        return ['legacy.config' => $config];
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -22,6 +39,7 @@ class LoadSettings
         $settings = Setting::all()->pluck('value', 'key')->toArray();
 
         Config::set($settings);
+        Config::set($this->getConfig());
 
         return $next($request);
     }
