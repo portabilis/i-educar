14372f1b75a2b874e41a1dcbdcf82a91df0612c8
Everton Muniz <munizeverton@gmail.com>
2019-05-08 15:00:11 -0300

    Ajusta regras do campo Pos graduação

--- ieducar/modules/Cadastro/Assets/Javascripts/Servidor.js
+++ ieducar/modules/Cadastro/Assets/Javascripts/Servidor.js
@@ -98,7 +98,6 @@ function validaCursoFormacaoContinuada() {
 }
 
 verificaCamposObrigatorio();
-habilitaCampoPosGraduacao();
 
 let habilitaTipoEnsinoMedio = () => {
   if (obrigarCamposCenso) {
@@ -151,6 +150,7 @@ function verificaCamposObrigatorio() {
       ),
       success : function(dataResponse) {
         escolaridadeSuperior = dataResponse.escolaridade.escolaridade == '6'
+        habilitaCampoPosGraduacao();
       }
     }
     getResource(options);
@@ -159,16 +159,11 @@ function verificaCamposObrigatorio() {
 
 
 function habilitaCampoPosGraduacao() {
-  var possuiSuperiorConcuido = true;
-
-  $j('#tr_pos_graduacao').hide();
-  $j('#pos_graduacao').makeUnrequired();
-  if (possuiSuperiorConcuido) {
-    $j('#tr_pos_graduacao').show();
-    if (obrigarCamposCenso) {
-      $j('#pos_graduacao').makeRequired();
-    }
+  $j('#pos_graduacao').removeAttr('disabled');
+  if (!escolaridadeSuperior) {
+    $j('#pos_graduacao').attr('disabled', 'disabled').makeUnrequired().val('');
   }
+  $j("#pos_graduacao").trigger("chosen:updated");
 }
 
 //abas
