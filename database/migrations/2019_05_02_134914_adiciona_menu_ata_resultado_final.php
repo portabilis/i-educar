cb06aaa301ff16cfd8d5f9ac344ffa19d832e499
Matheus Nicoski <matheusnicoski@gmail.com>
2019-05-16 14:12:43 -0300

    Adiciona novo menu na tabela de menus

--- database/migrations/2019_05_02_134914_adiciona_menu_ata_resultado_final.php
+++ database/migrations/2019_05_02_134914_adiciona_menu_ata_resultado_final.php
@@ -17,5 +17,7 @@ class AdicionaMenuAtaResultadoFinal extends Migration
         DB::statement("INSERT INTO portal.menu_submenu values (9998911, 55, 2, 'Ata Resultado final', 'module/Reports/AtaResultadoFinal',null,3);");
         DB::statement("INSERT INTO pmicontrolesis.menu VALUES(9998911,9998911,999925,'Ata Resultado final',0,'module/Reports/AtaResultadoFinal','_self',1,15,192);");
         DB::statement("INSERT INTO pmieducar.menu_tipo_usuario VALUES(1,9998911,1,1,1);");
+
+        DB::statement("INSERT INTO menus (parent_id, title, description, link, icon, \"order\", type, process, old, parent_old, active) VALUES (77,'Ata de resultado final', 'Ata de resultado final', '/module/Reports/AtaResultadoFinal','',0,4,9998911,9998911,999925,true);");
     }
 }
