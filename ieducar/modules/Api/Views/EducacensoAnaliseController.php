3136a7ae5b1d6bb246414c1474c8747a6445ee71
Eder Soares <edersoares@me.com>
2019-05-20 12:04:37 -0300

    Limita a consulta dos horários conflitantes na análise
    Limita na análise da exportação do Educacenso a data a qual serão
    consideradas as enturmações que não podem possuir conflito de horários.

--- ieducar/modules/Api/Views/EducacensoAnaliseController.php
+++ ieducar/modules/Api/Views/EducacensoAnaliseController.php
@@ -9,6 +9,7 @@ use App\Models\Educacenso\Registro50;
 use App\Models\Individual;
 use App\Models\Educacenso\Registro60;
 use App\Models\LegacyInstitution;
+use App\Models\LegacySchool;
 use App\Models\School;
 use App\Repositories\EducacensoRepository;
 use App\Services\SchoolClass\AvailableTimeService;
@@ -1292,6 +1293,14 @@ class EducacensoAnaliseController extends ApiCoreController
         $countAtividadesComplementar = [];
         $notAvaliableTime = [];
 
+        /** @var LegacySchool $school */
+        $school = LegacySchool::query()->findOrFail($escola);
+        $educacensoDate = new DateTime($school->institution->data_educacenso);
+
+        $avaliableTimeService = new AvailableTimeService();
+
+        $avaliableTimeService->onlyUntilEnrollmentDate($educacensoDate);
+
         foreach ($alunos as $aluno) {
             $nomeEscola = strtoupper($aluno->nomeEscola);
             $nomeAluno = strtoupper($aluno->nomeAluno);
@@ -1299,8 +1308,6 @@ class EducacensoAnaliseController extends ApiCoreController
             $codigoAluno = $aluno->codigoAluno;
             $codigoTurma = $aluno->codigoTurma;
             $codigoMatricula = $aluno->codigoMatricula;
-
-            $avaliableTimeService = new AvailableTimeService();
 
             if (!$avaliableTimeService->isAvailable($codigoAluno, $codigoTurma)) {
                 $notAvaliableTime[$codigoAluno] = [
