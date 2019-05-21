ad493c5e4ecb60407c9531f4738143496a060f75
Eder Soares <edersoares@me.com>
2019-05-21 17:43:49 -0300

    Remove inserts do schema acesso

--- database/sqls/inserts/acesso.sistema.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO acesso.sistema VALUES (1, 'i-Educar', 'Sistema básico', '', 'A');
-INSERT INTO acesso.sistema VALUES (9, 'Código legado', 'Sistema básico', '', 'A');
-INSERT INTO acesso.sistema VALUES (17, 'Código legado', 'Sistema básico', '', 'A');
