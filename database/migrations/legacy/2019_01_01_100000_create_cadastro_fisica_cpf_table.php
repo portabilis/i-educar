fbbbd53b3d345123b78b9c8c111e3134d880b3ca
Eder Soares <edersoares@me.com>
2019-05-21 17:05:37 -0300

    Remove referências da tabela cadastro.fisica_cpf

--- database/migrations/legacy/2019_01_01_100000_create_cadastro_fisica_cpf_table.php
+++ database/migrations/legacy/2019_01_01_100000_create_cadastro_fisica_cpf_table.php
@@ -26,8 +26,6 @@ class CreateCadastroFisicaCpfTable extends Migration
                     idpes_cad numeric,
                     data_cad timestamp without time zone NOT NULL,
                     operacao character(1) NOT NULL,
-                    idsis_rev integer,
-                    idsis_cad integer NOT NULL,
                     CONSTRAINT ck_fisica_cpf_operacao CHECK (((operacao = \'I\'::bpchar) OR (operacao = \'A\'::bpchar) OR (operacao = \'E\'::bpchar))),
                     CONSTRAINT ck_fisica_cpf_origem_gravacao CHECK (((origem_gravacao = \'M\'::bpchar) OR (origem_gravacao = \'U\'::bpchar) OR (origem_gravacao = \'C\'::bpchar) OR (origem_gravacao = \'O\'::bpchar)))
                 );
