0d285520e42e253cbd4059cf6cc822994c98fa87
Everton Muniz <munizeverton@gmail.com>
2019-05-23 09:23:36 -0300

    Remove imports não usados

--- app/Http/Controllers/StudentLogUnificationController.php
+++ app/Http/Controllers/StudentLogUnificationController.php
@@ -2,19 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Exceptions\Enrollment\ExistsActiveEnrollmentException;
-use App\Http\Requests\BatchEnrollmentRequest;
-use App\Http\Requests\CancelBatchEnrollmentRequest;
-use App\Models\LegacySchoolClass;
 use App\Models\LogUnification;
-use App\Services\EnrollmentService;
-use App\Services\RegistrationService;
-use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
-use Illuminate\Support\MessageBag;
-use Illuminate\View\View;
-use Throwable;
 
 class StudentLogUnificationController extends Controller
 {
