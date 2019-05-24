9eff9c70861ad05d6f39e125de3f3ab3b835ed37
Everton Muniz <munizeverton@gmail.com>
2019-05-24 09:58:57 -0300

    Ajustes do CR

--- resources/views/form/select-institution.blade.php
+++ resources/views/form/select-institution.blade.php
@@ -2,19 +2,9 @@
     <select class="geral" name="ref_cod_instituicao" id="ref_cod_instituicao" style="width: 308px;">
         <option value="">Selecione uma instituição</option>
         @foreach(\App\Models\LegacyInstitution::active()->get() as $institution)
-            <option value="{{$institution->cod_instituicao}}">{{$institution->name}}</option>
+            <option value="{{$institution->cod_instituicao}}" @if(Request::get('ref_cod_instituicao') == $institution->cod_instituicao) selected @endif>
+                {{$institution->name}}
+            </option>
         @endforeach
     </select>
 </span>
-
-@if(Request::get('ref_cod_instituicao'))
-    @push('scripts')
-        <script>
-            (function($){
-                $(document).ready(function() {
-                    $j('#ref_cod_instituicao').val({{Request::get('ref_cod_instituicao')}});
-                });
-            })(jQuery);
-        </script>
-    @endpush
-@endif
