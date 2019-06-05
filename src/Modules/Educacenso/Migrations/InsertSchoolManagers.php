af9c304c8b00d7b21e0d3e083c54c7f006269952
Everton Muniz <munizeverton@gmail.com>
2019-06-05 19:00:08 -0300

    Corrige campo alterado na migration que cria os gestores escolares

--- src/Modules/Educacenso/Migrations/InsertSchoolManagers.php
+++ src/Modules/Educacenso/Migrations/InsertSchoolManagers.php
@@ -9,7 +9,7 @@ class InsertSchoolManagers implements EducacensoMigrationInterface
     public static function execute()
     {
         $sql = <<<'SQL'
-                INSERT INTO school_managers (individual_id, school_id, role_id, chief)
+                INSERT INTO school_managers (employee_id, school_id, role_id, chief)
                 (
                     SELECT ref_idpes_gestor,
                            cod_escola,
