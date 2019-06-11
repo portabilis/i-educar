26767cf95b6fe326c42183412d889723819243d0
Eder Soares <edersoares@me.com>
2019-06-11 15:49:14 -0300

    Implementação dos filtros da tela de consulta do Educacenso

--- app/Http/Controllers/EducacensoController.php
+++ app/Http/Controllers/EducacensoController.php
@@ -2,18 +2,90 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LegacyInstitution;
+use App\Repositories\EducacensoRepository;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class EducacensoController extends Controller
 {
-    public function consult()
+    /**
+     * @param LegacyInstitution $institution
+     * @param array             $records
+     *
+     * @return View
+     */
+    private function view(LegacyInstitution $institution, $records = [])
     {
         $this->breadcrumb('Consulta', [
-            '' => 'Educacenso',
+            url('intranet/educar_educacenso_index.php') => 'Educacenso',
         ]);
 
         $this->menu(70);
 
-        return view('educacenso.consult');
+        $schools = $institution->schools()->with('person')->get()->sortBy(function ($school) {
+            return $school->name;
+        });
+
+        return view('educacenso.consult', [
+            'schools' => $schools,
+            'record20' => $records['record20'] ?? null,
+            'record40' => $records['record40'] ?? null,
+            'record50' => $records['record50'] ?? null,
+            'record60' => $records['record60'] ?? null,
+        ]);
+    }
+
+    /**
+     * @param LegacyInstitution $institution
+     *
+     * @return View
+     */
+    public function show(LegacyInstitution $institution)
+    {
+        return $this->view($institution);
+    }
+
+    /**
+     * @param Request              $request
+     * @param EducacensoRepository $repository
+     * @param LegacyInstitution    $institution
+     *
+     * @return View
+     */
+    public function consult(
+        Request $request,
+        EducacensoRepository $repository,
+        LegacyInstitution $institution
+    ) {
+        $record = $request->input('record');
+        $school = $request->input('school');
+        $year = $request->input('year');
+
+        $records = [];
+
+        if ($record == '20') {
+            $records['record20'] = $repository->getDataForRecord20($school, $year);
+        }
+
+        if ($record == '40') {
+            $records['record40'] = $repository->getDataForRecord40($school);
+        }
+
+        if ($record == '50') {
+            $records['record50'] = $repository->getDataForRecord50($year, $school);
+
+            $records['record50'] = collect($records['record50'])->map(function ($item) {
+                $item->componentes = array_unique(explode(',', substr($item->componentes, 1, -1)));
+
+                return $item;
+            })->values();
+        }
+
+        if ($record == '60') {
+            $records['record60'] = $repository->getDataForRecord60($school, $year);
+        }
+
+        return $this->view($institution, $records);
     }
 }
--- resources/views/educacenso/consult.blade.php
+++ resources/views/educacenso/consult.blade.php
@@ -5,7 +5,7 @@
 @endpush
 
 @section('content')
-    <form id="formcadastro" method="post">
+    <form id="formcadastro" method="post" action="{{ route('educacenso.consult') }}">
         <table class="table-default">
             <thead>
                 <tr>
@@ -20,36 +20,29 @@
                         <sub style="vertical-align:top;" class="text-muted">somente números</sub>
                     </td>
                     <td>
-                        <input class="obrigatorio" type="text" name="ano" id="ano" value="2019" size="4" maxlength="4">
-                    </td>
-                </tr>
-                <tr>
-                    <td>Instituição<span class="campo_obrigatorio">*</span></td>
-                    <td>
-                        <select onchange="" class="obrigatorio" name="ref_cod_instituicao" id="ref_cod_instituicao" style="width: 314px;">
-                            <option value="">Selecione uma instituição</option>
-                            <option value="1">PREFEITURA MUNICIPAL DE AVELINO LOPES</option>
-                        </select>
+                        <input name="year" class="obrigatorio" type="text" value="2019" size="4" maxlength="4">
                     </td>
                 </tr>
                 <tr>
                     <td>Escola<span class="campo_obrigatorio">*</span></td>
                     <td>
-                        <select onchange="" class="obrigatorio" style="width: 314px;">
+                        <select name="school" class="obrigatorio" style="width: 314px;">
                             <option value="">Selecione uma escola</option>
-                            <option value="1">PREFEITURA MUNICIPAL DE AVELINO LOPES</option>
+                            @foreach($schools as $school)
+                                <option value="{{ $school->getKey() }}">{{ $school->name }}</option>
+                            @endforeach
                         </select>
                     </td>
                 </tr>
                 <tr>
                     <td>Registro<span class="campo_obrigatorio">*</span></td>
                     <td>
-                        <select onchange="" class="obrigatorio" style="width: 314px;">
+                        <select name="record" class="obrigatorio" style="width: 314px;">
                             <option value="">Selecione um registro</option>
-                            <option value="1">Registro 20</option>
-                            <option value="1">Registro 40</option>
-                            <option value="1">Registro 50</option>
-                            <option value="1">Registro 60</option>
+                            <option value="20">Registro 20</option>
+                            <option value="40">Registro 40</option>
+                            <option value="50">Registro 50</option>
+                            <option value="60">Registro 60</option>
                         </select>
                     </td>
                 </tr>
@@ -61,10 +54,14 @@
                 </tr>
             </tbody>
         </table>
+        <div class="text-center">
+            <button class="btn-green">Consultar</button>
+        </div>
     </form>
 
     <div style="height: 30px;"></div>
 
+    @isset($record20)
     <h2>Registro 20</h2>
 
     <table class="table-default">
@@ -78,42 +75,25 @@
             </tr>
         </thead>
         <tbody>
-            @if(false)
+            @foreach($record20 as $item)
                 <tr>
-                    <td colspan="5">Nenhum registro encontrado.</td>
+                    <td>{{ $item->codigoEscolaInep }}</td>
+                    <td>{{ $item->nomeEscola }}</td>
+                    <td>{{ $item->nomeTurma }}</td>
+                    <td>{{ \iEducar\Modules\Educacenso\Model\TipoAtendimentoTurma::getDescription($item->tipoAtendimento) }}</td>
+                    <td>{{ $item->horaInicial }} - {{ $item->horaFinal }}</td>
                 </tr>
-            @endif
+            @endforeach
+            @empty($record20)
             <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>Turminha</td>
-                <td>Regular</td>
-                <td>07:30 às 12:00</td>
+                <td colspan="5">Nenhum registro encontrado.</td>
             </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>Turminha</td>
-                <td>Regular</td>
-                <td>07:30 às 12:00</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>Turminha</td>
-                <td>Regular</td>
-                <td>07:30 às 12:00</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>Turminha</td>
-                <td>Regular</td>
-                <td>07:30 às 12:00</td>
-            </tr>
+            @endempty
         </tbody>
     </table>
+    @endisset
 
+    @isset($record40)
     <h2>Registro 40</h2>
 
     <table class="table-default">
@@ -129,50 +109,27 @@
             </tr>
         </thead>
         <tbody>
-            @if(false)
+            @foreach($record40 as $item)
+                <tr>
+                    <td>{{ $item->inepEscola }}</td>
+                    <td>{{ $item->nomeEscola }}</td>
+                    <td>{{ $item->codigoPessoa }}</td>
+                    <td>{{ $item->nomePessoa }}</td>
+                    <td>{{ $item->inepGestor }}</td>
+                    <td>{{ \iEducar\Modules\Educacenso\Model\SchoolManagerRole::getDescription($item->cargo) }}</td>
+                    <td>{{ \iEducar\Modules\Servidores\Model\TipoVinculo::getDescription($item->tipoVinculo) }}</td>
+                </tr>
+            @endforeach
+            @empty($record40)
                 <tr>
                     <td colspan="7">Nenhum registro encontrado.</td>
                 </tr>
-            @endif
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-                <td>Efetivo</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-                <td>Efetivo</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-                <td>Efetivo</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-                <td>Efetivo</td>
-            </tr>
+            @endempty
         </tbody>
     </table>
+    @endisset
 
+    @isset($record50)
     <h2>Registro 50</h2>
 
     <table class="table-default">
@@ -188,50 +145,22 @@
             </tr>
         </thead>
         <tbody>
-            @if(false)
+            @foreach($record50 as $item)
                 <tr>
-                    <td colspan="7">Nenhum registro encontrado.</td>
+                    <td>{{ $item->inepEscola }}</td>
+                    <td>{{ $item->nomeEscola }}</td>
+                    <td>{{ $item->codigoPessoa }}</td>
+                    <td>{{ $item->inepDocente }}</td>
+                    <td>{{ $item->nomeDocente }}</td>
+                    <td>{{ \iEducar\Modules\Servidores\Model\FuncaoExercida::getDescription($item->funcaoDocente) }}</td>
+                    <td>{{ implode(',', $item->componentes) }}</td>
                 </tr>
-            @endif
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-                <td>Efetivo</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-                <td>Efetivo</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-                <td>Efetivo</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-                <td>Efetivo</td>
-            </tr>
+            @endforeach
         </tbody>
     </table>
+    @endisset
 
+    @isset($record60)
     <h2>Registro 60</h2>
 
     <table class="table-default">
@@ -246,76 +175,23 @@
             </tr>
         </thead>
         <tbody>
-            @if(false)
+            @foreach($record60 as $item)
+                <tr>
+                    <td>{{ $item->inepEscola }}</td>
+                    <td>{{ $item->nomeEscola }}</td>
+                    <td>{{ $item->codigoPessoa }}</td>
+                    <td>{{ $item->inepAluno }}</td>
+                    <td>{{ $item->nomeAluno }}</td>
+                    <td>{{ $item->nomeTurma }}</td>
+                </tr>
+            @endforeach
+            @empty($record60)
                 <tr>
                     <td colspan="7">Nenhum registro encontrado.</td>
                 </tr>
-            @endif
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Escolinha</td>
-                <td>123</td>
-                <td>543</td>
-                <td>Eder</td>
-                <td>Diretor</td>
-            </tr>
+            @endempty
         </tbody>
     </table>
+    @endif
 
 @endsection
