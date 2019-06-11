e235afc96c32af41c9b19d9fd2f6e4eed992b78e
Eder Soares <edersoares@me.com>
2019-06-11 15:15:35 -0300

    Modifica queries do repositório para trazer mais dados

--- app/Repositories/EducacensoRepository.php
+++ app/Repositories/EducacensoRepository.php
@@ -260,7 +260,9 @@ SQL;
             SELECT
                40 AS registro,
                educacenso_cod_escola.cod_escola_inep AS "inepEscola",
+               juridica.fantasia AS "nomeEscola",
                school_managers.employee_id AS "codigoPessoa",
+               pessoa.nome AS "nomePessoa",
                educacenso_cod_docente.cod_docente_inep AS "inepGestor",
                school_managers.role_id AS cargo,
                school_managers.access_criteria_id AS "criterioAcesso",
@@ -269,8 +271,10 @@ SQL;
                escola.situacao_funcionamento AS "situacaoFuncionamento"
           FROM school_managers
           JOIN pmieducar.escola ON escola.cod_escola = school_managers.school_id
+          JOIN cadastro.juridica ON juridica.idpes = escola.ref_idpes 
      LEFT JOIN modules.educacenso_cod_escola ON educacenso_cod_escola.cod_escola = escola.cod_escola
      LEFT JOIN pmieducar.servidor ON servidor.cod_servidor = school_managers.employee_id
+     LEFT JOIN cadastro.pessoa ON pessoa.idpes = school_managers.employee_id
      LEFT JOIN modules.educacenso_cod_docente ON educacenso_cod_docente.cod_servidor = servidor.cod_servidor
         WHERE school_managers.school_id = :school
 SQL;
