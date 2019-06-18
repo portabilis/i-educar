4e4960d6a3fad656953d91b6c0e14d82a07df21f
Eder Soares <edersoares@me.com>
2019-06-18 18:42:44 -0300

    Remove seeder da tabela historico_educar

--- database/migrations/2019_05_08_000000_default_data.php
+++ database/migrations/2019_05_08_000000_default_data.php
@@ -60,7 +60,6 @@ class DefaultData extends Migration
         Artisan::call('db:seed', ['--class' => DefaultPmieducarAbandonoTipoTableSeeder::class]);
         Artisan::call('db:seed', ['--class' => DefaultPmieducarConfiguracoesGeraisTableSeeder::class]);
         Artisan::call('db:seed', ['--class' => DefaultPmieducarEscolaLocalizacaoTableSeeder::class]);
-        Artisan::call('db:seed', ['--class' => DefaultPmieducarHistoricoEducarTableSeeder::class]);
         Artisan::call('db:seed', ['--class' => DefaultPmieducarHistoricoGradeCursoTableSeeder::class]);
         Artisan::call('db:seed', ['--class' => DefaultPmieducarMenuTipoUsuarioTableSeeder::class]);
         Artisan::call('db:seed', ['--class' => DefaultPmieducarTipoAutorTableSeeder::class]);
@@ -93,7 +92,6 @@ class DefaultData extends Migration
         DB::raw('TRUNCATE pmieducar.tipo_autor CASCADE;');
         DB::raw('TRUNCATE pmieducar.menu_tipo_usuario CASCADE;');
         DB::raw('TRUNCATE pmieducar.historico_grade_curso CASCADE;');
-        DB::raw('TRUNCATE pmieducar.historico_educar CASCADE;');
         DB::raw('TRUNCATE pmieducar.escola_localizacao CASCADE;');
         DB::raw('TRUNCATE pmieducar.configuracoes_gerais CASCADE;');
         DB::raw('TRUNCATE pmieducar.abandono_tipo CASCADE;');
