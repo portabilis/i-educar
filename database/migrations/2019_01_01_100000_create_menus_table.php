ad476bfac93060f0e1d4f425ff5895772a726477
Eder Soares <edersoares@me.com>
2019-06-21 18:07:06 -0300

    Corrige nome da migration

--- database/migrations/2019_01_01_100000_create_menus_table.php
+++ database/migrations/2019_01_01_100000_create_menus_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMenusTableTable extends Migration
+class CreateMenusTable extends Migration
 {
     /**
      * Run the migrations.
