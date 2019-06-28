f9dcc0d5f512b413381688dc3cd8b9c0ae02e2c3
Eder Soares <edersoares@me.com>
2019-06-28 19:20:20 -0300

    Corrige view_dados_escola

--- database/sqls/views/relatorio.view_dados_escola.sql
+++ database/sqls/views/relatorio.view_dados_escola.sql
@@ -11,8 +11,8 @@ SELECT escola.cod_escola,
     COALESCE(municipio.nome, endereco_externo.cidade) AS municipio,
     COALESCE(municipio.sigla_uf, endereco_externo.sigla_uf::character varying) AS uf_municipio,
     educacenso_cod_escola.cod_escola_inep AS inep,
-    null AS telefone_ddd,
-    null AS telefone,
+    fone_pessoa.ddd AS telefone_ddd,
+    to_char(fone_pessoa.fone, '99999-9999'::text) AS telefone,
     fone_pessoa.ddd AS celular_ddd,
     to_char(fone_pessoa.fone, '99999-9999'::text) AS celular
 FROM pmieducar.escola
