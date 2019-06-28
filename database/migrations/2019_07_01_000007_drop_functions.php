bac6dbf672182217399a472d12cab988b545fe59
Eder Soares <edersoares@me.com>
2019-06-28 14:59:26 -0300

    Mantém funções do schema relatorio

--- database/migrations/2019_07_01_000007_drop_functions.php
+++ database/migrations/2019_07_01_000007_drop_functions.php
@@ -30,26 +30,6 @@ class DropFunctions extends Migration
                 
                 DROP FUNCTION fcn_aft_update();
                 
-                SET search_path = relatorio, pg_catalog;
-                
-                DROP FUNCTION get_ddd_escola(integer);
-                
-                DROP FUNCTION get_dias_letivos_da_turma_por_etapa(v_turma integer, v_etapa integer);
-                
-                DROP FUNCTION get_mae_aluno(integer);
-                
-                DROP FUNCTION get_media_geral_turma(turma_i integer, componente_i integer);
-                
-                DROP FUNCTION get_media_recuperacao_semestral(matricula integer, componente integer);
-                
-                DROP FUNCTION get_media_turma(turma_i integer, componente_i integer, etapa_i integer);
-                
-                DROP FUNCTION get_nacionalidade(nacionalidade_id numeric);
-                
-                DROP FUNCTION get_qtde_alunos_situacao(ano integer, instituicao integer, escola integer, curso integer, serie integer, turma integer, situacao integer, bairro integer, sexo character, idadeini integer, idadefim integer);
-                
-                DROP FUNCTION retorna_situacao_matricula_componente(cod_situacao_matricula numeric, cod_situacao_componente numeric);
-                
                 SET search_path = public, pg_catalog;
             '
         );
