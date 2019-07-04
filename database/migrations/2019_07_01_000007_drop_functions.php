c3d448b22329f3468742eddf22d73e66f8c633f5
Eder Soares <edersoares@me.com>
2019-07-04 15:28:41 -0300

    Drop função se existir

--- database/migrations/2019_07_01_000007_drop_functions.php
+++ database/migrations/2019_07_01_000007_drop_functions.php
@@ -16,19 +16,19 @@ class DropFunctions extends Migration
             '
                 SET search_path = cadastro, pg_catalog;
 
-                DROP FUNCTION fcn_aft_fisica();
+                DROP FUNCTION IF EXISTS fcn_aft_fisica();
                 
-                DROP FUNCTION fcn_aft_fisica_cpf_provisorio();
+                DROP FUNCTION IF EXISTS fcn_aft_fisica_cpf_provisorio();
                 
-                DROP FUNCTION fcn_aft_fisica_provisorio();
+                DROP FUNCTION IF EXISTS fcn_aft_fisica_provisorio();
                 
                 SET search_path = modules, pg_catalog;
                 
-                DROP FUNCTION corrige_sequencial_historico();
+                DROP FUNCTION IF EXISTS corrige_sequencial_historico();
                 
                 SET search_path = pmieducar, pg_catalog;
                 
-                DROP FUNCTION fcn_aft_update();
+                DROP FUNCTION IF EXISTS fcn_aft_update();
                 
                 SET search_path = public, pg_catalog;
             '
