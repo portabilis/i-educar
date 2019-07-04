f3dfc3b1897e5afad04ca400736ee0cbec48477b
Everton Muniz <munizeverton@gmail.com>
2019-07-04 10:15:49 -0300

    Cria model para pessoa jurídica

---
+++ app/Models/LegacyCompany.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class Pessoa.
+ *
+ * @package namespace App\Entities;
+ */
+class LegacyCompany extends EloquentBaseModel
+{
+    /**
+     * @var string
+     */
+    protected $table = 'cadastro.juridica';
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'idpes';
+
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+
+}
