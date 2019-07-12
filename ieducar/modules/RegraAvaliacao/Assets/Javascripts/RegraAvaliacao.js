9b002202d2f8dcef1472a2c27bd2e2454afc7799
Eder Soares <edersoares@me.com>
2019-07-12 19:47:09 -0300

    Remove fixup de checkbox

--- ieducar/modules/RegraAvaliacao/Assets/Javascripts/RegraAvaliacao.js
+++ ieducar/modules/RegraAvaliacao/Assets/Javascripts/RegraAvaliacao.js
@@ -64,7 +64,7 @@ $j('[name^="recuperacao[excluir]"]').on('change', function(){
     $j(this).val('');
 });
 
-$j('#notaGeralPorEtapa, #aprovaMediaDisciplina, #reprovacaoAutomatica, #definirComponentePorEtapa, #tipoCalculoRecuperacaoParalela').on('change', function(){
+$j('#notaGeralPorEtapa, #aprovaMediaDisciplina, #reprovacaoAutomatica, #definirComponentePorEtapa').on('change', function(){
   if($j(this).is(':checked'))
     $j(this).val('1');
   else
