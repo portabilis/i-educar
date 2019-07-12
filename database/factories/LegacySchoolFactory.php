dd10d28d538448447aefaa5b177fb106e4d0f0db
Eder Soares <edersoares@me.com>
2019-07-12 16:33:49 -0300

    Cria algumas models e factories para facilitar trabalho

--- database/factories/LegacySchoolFactory.php
+++ database/factories/LegacySchoolFactory.php
@@ -2,6 +2,7 @@
 
 use App\Models\LegacyEducationNetwork;
 use App\Models\LegacyInstitution;
+use App\Models\LegacyOrganization;
 use App\Models\LegacySchool;
 use App\Models\LegacyUser;
 use Faker\Generator as Faker;
@@ -13,5 +14,6 @@ $factory->define(LegacySchool::class, function (Faker $faker) {
         'ref_cod_escola_rede_ensino' => factory(LegacyEducationNetwork::class)->create(),
         'sigla' => $faker->asciify(),
         'data_cadastro' => now(),
+        'ref_idpes' => factory(LegacyOrganization::class)->create(),
     ];
 });
