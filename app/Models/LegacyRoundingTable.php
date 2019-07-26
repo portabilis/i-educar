d880484c98e9631571009d5539d2c24fc7b58d43
bonot <paula_bonot@hotmail.com>
2019-07-26 14:37:50 -0300

    Adiciona models e factories para novo cenário

---
+++ app/Models/LegacyRoundingTable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LegacyRoundingTable extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'modules.tabela_arredondamento';
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'instituicao_id', 'nome', 'tipo_nota'
+    ];
+
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+}
---
+++ database/factories/LegacyRoundingTableFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\LegacyRoundingTable;
+use Faker\Generator as Faker;
+use App\Models\LegacyInstitution;
+
+$factory->define(LegacyRoundingTable::class, function (Faker $faker) {
+    
+    return [
+        'instituicao_id' => factory(LegacyInstitution::class)->state('unique')->make(),
+        'nome' => $faker->words(3, true),
+        'tipo_nota' => $faker->randomElement([1, 2]),
+    ];
+});
+
+$factory->defineAs(LegacyRoundingTable::class, 'numeric', function (Faker $faker) use ($factory) {
+    $roundingTable = $factory->raw(LegacyRoundingTable::class);
+
+    return array_merge($roundingTable, [
+        'tipo_nota' => RegraAvaliacao_Model_Nota_TipoValor::NUMERICA,
+    ]);
+});
