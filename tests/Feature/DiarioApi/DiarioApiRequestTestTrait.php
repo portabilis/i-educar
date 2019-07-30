dc9ee20c9e9b905e32d8ef4480774e9710006f6e
bonot <paula_bonot@hotmail.com>
2019-07-30 11:49:48 -0300

    Adiciona testes ao cenário principal
    - Reprovação por média;
    - Reprovação por falta;
    - Tornar cursando ao excluir última nota;
    - Bloqueio de exclusão de notas que não são de etapa concluinte;
    - Tornar cursando ao excluir última falta;
    - Bloqueio de exclusão de faltas que não são da etapa concluinte;

--- tests/Feature/DiarioApi/DiarioApiRequestTestTrait.php
+++ tests/Feature/DiarioApi/DiarioApiRequestTestTrait.php
@@ -82,6 +82,70 @@ trait DiarioApiRequestTestTrait
         return json_decode($response->content());
     }
 
+    public function deleteScore($enrollment, $disciplineId, $stage)
+    {
+        $this->cleanGlobals();
+
+        $schoolClass = $enrollment->schoolClass;
+
+        $data = [
+            'resource' => 'nota',
+            'oper' => 'delete',
+            'instituicao_id' => $schoolClass->school->institution->id,
+            'escola_id' => $schoolClass->school_id,
+            'curso_id' => $schoolClass->course_id,
+            'serie_id' => $schoolClass->grade_id,
+            'turma_id' => $schoolClass->id,
+            'ano_escolar' => $schoolClass->year,
+            'componente_curricular_id' => $disciplineId,
+            'etapa' => $stage,
+            'matricula_id' => $enrollment->registration->id,
+            'access_key' => env('API_ACCESS_KEY'),
+            'secret_key' => env('API_SECRET_KEY'),
+        ];
+
+        $_GET = $data;
+
+        $user = factory(User::class, 'admin')->make();
+
+        /** @var TestResponse $response */
+        $response = $this->actingAs($user)->get('/module/Avaliacao/diarioApi?' . http_build_query($data));
+
+        return json_decode($response->content());
+    }
+
+    public function deleteAbsence($enrollment, $disciplineId, $stage)
+    {
+        $this->cleanGlobals();
+
+        $schoolClass = $enrollment->schoolClass;
+
+        $data = [
+            'resource' => 'falta',
+            'oper' => 'delete',
+            'instituicao_id' => $schoolClass->school->institution->id,
+            'escola_id' => $schoolClass->school_id,
+            'curso_id' => $schoolClass->course_id,
+            'serie_id' => $schoolClass->grade_id,
+            'turma_id' => $schoolClass->id,
+            'ano_escolar' => $schoolClass->year,
+            'componente_curricular_id' => $disciplineId,
+            'etapa' => $stage,
+            'matricula_id' => $enrollment->registration->id,
+            'access_key' => env('API_ACCESS_KEY'),
+            'secret_key' => env('API_SECRET_KEY'),
+        ];
+
+        $_GET = $data;
+
+        $user = factory(User::class, 'admin')->make();
+
+        /** @var TestResponse $response */
+        $response = $this->actingAs($user)->get('/module/Avaliacao/diarioApi?' . http_build_query($data));
+
+        return json_decode($response->content());
+    }
+
     /**
      * Limpa variáveis globais que podem interferir testes no código legado
      */
--- tests/Feature/DiarioApi/NaoContinuadaMediaPresencaSemRecuperacaoTest.php
+++ tests/Feature/DiarioApi/NaoContinuadaMediaPresencaSemRecuperacaoTest.php
@@ -63,4 +63,210 @@ class NaoContinuadaMediaPresencaSemRecuperacao extends TestCase
         $registration = $this->enrollment->registration;
         $this->assertEquals(1, $registration->refresh()->aprovado);
     }
+
+    public function testReprovacaoPorMediaAposTodasFaltasENotasLancadas()
+    {
+        $schoolClass = $this->enrollment->schoolClass;
+        $school = $schoolClass->school;
+
+        $this->addAcademicYearStage($schoolClass, 2);
+        $this->addAcademicYearStage($schoolClass, 3);
+        $this->addAcademicYearStage($schoolClass, 4);
+
+        $discipline = factory(LegacyDiscipline::class)->create();
+        $schoolClass->disciplines()->attach($discipline->id, [
+            'ano_escolar_id' => $schoolClass->grade_id,
+            'escola_id' => $school->id
+        ]);
+
+        factory(LegacyDisciplineAcademicYear::class)->create([
+            'componente_curricular_id' => $discipline->id,
+            'ano_escolar_id' => $schoolClass->grade_id,
+        ]);
+
+        $disciplines = $schoolClass->disciplines;
+
+        foreach ($disciplines as $discipline) {
+            for ($stage = 1; $stage <= 4; $stage++) {
+                $this->postAbsence($this->enrollment, $discipline->id, $stage, '3');
+                $response = $this->postScore($this->enrollment, $discipline->id, $stage, '5');
+            }
+            $this->assertEquals('Retido', $response->situacao);
+        }
+        $registration = $this->enrollment->registration;
+        $this->assertEquals(2, $registration->refresh()->aprovado);
+    }
+
+    public function testReprovacaoPorFaltaAposTodasFaltasENotasLancadas()
+    {
+        $schoolClass = $this->enrollment->schoolClass;
+        $school = $schoolClass->school;
+
+        $this->addAcademicYearStage($schoolClass, 2);
+        $this->addAcademicYearStage($schoolClass, 3);
+        $this->addAcademicYearStage($schoolClass, 4);
+
+        $discipline = factory(LegacyDiscipline::class)->create();
+        $schoolClass->disciplines()->attach($discipline->id, [
+            'ano_escolar_id' => $schoolClass->grade_id,
+            'escola_id' => $school->id
+        ]);
+
+        factory(LegacyDisciplineAcademicYear::class)->create([
+            'componente_curricular_id' => $discipline->id,
+            'ano_escolar_id' => $schoolClass->grade_id,
+        ]);
+
+        $disciplines = $schoolClass->disciplines;
+
+        foreach ($disciplines as $discipline) {
+            for ($stage = 1; $stage <= 4; $stage++) {
+                $this->postAbsence($this->enrollment, $discipline->id, $stage, '50');
+                $response = $this->postScore($this->enrollment, $discipline->id, $stage, '9');
+            }
+            $this->assertEquals('Retido', $response->situacao);
+        }
+        $registration = $this->enrollment->registration;
+        $this->assertEquals(14, $registration->refresh()->aprovado);
+    }
+
+    public function testCursandoAposRemocaoDeNotaEmEtapaConcluinte()
+    {
+        $schoolClass = $this->enrollment->schoolClass;
+        $school = $schoolClass->school;
+
+        $this->addAcademicYearStage($schoolClass, 2);
+        $this->addAcademicYearStage($schoolClass, 3);
+        $this->addAcademicYearStage($schoolClass, 4);
+
+        $discipline = factory(LegacyDiscipline::class)->create();
+        $schoolClass->disciplines()->attach($discipline->id, [
+            'ano_escolar_id' => $schoolClass->grade_id,
+            'escola_id' => $school->id
+        ]);
+
+        factory(LegacyDisciplineAcademicYear::class)->create([
+            'componente_curricular_id' => $discipline->id,
+            'ano_escolar_id' => $schoolClass->grade_id,
+        ]);
+
+        $disciplines = $schoolClass->disciplines;
+
+        foreach ($disciplines as $discipline) {
+            for ($stage = 1; $stage <= 4; $stage++) {
+                $this->postAbsence($this->enrollment, $discipline->id, $stage, '3');
+                $response = $this->postScore($this->enrollment, $discipline->id, $stage, '7');
+            }
+            $this->assertEquals('Aprovado', $response->situacao);
+            $response = $this->deleteScore($this->enrollment, $discipline->id, 4);
+            $this->assertEquals('Cursando', $response->situacao);
+        }
+        $registration = $this->enrollment->registration;
+        $this->assertEquals(3, $registration->refresh()->aprovado);
+    }
+
+    public function testRemocaoDeNotaEmEtapaNaoConcluinte()
+    {
+        $schoolClass = $this->enrollment->schoolClass;
+        $school = $schoolClass->school;
+
+        $this->addAcademicYearStage($schoolClass, 2);
+        $this->addAcademicYearStage($schoolClass, 3);
+        $this->addAcademicYearStage($schoolClass, 4);
+
+        $discipline = factory(LegacyDiscipline::class)->create();
+        $schoolClass->disciplines()->attach($discipline->id, [
+            'ano_escolar_id' => $schoolClass->grade_id,
+            'escola_id' => $school->id
+        ]);
+
+        factory(LegacyDisciplineAcademicYear::class)->create([
+            'componente_curricular_id' => $discipline->id,
+            'ano_escolar_id' => $schoolClass->grade_id,
+        ]);
+
+        $disciplines = $schoolClass->disciplines;
+
+        foreach ($disciplines as $discipline) {
+            for ($stage = 1; $stage <= 4; $stage++) {
+                $this->postAbsence($this->enrollment, $discipline->id, $stage, '3');
+                $response = $this->postScore($this->enrollment, $discipline->id, $stage, '7');
+            }
+            $this->assertEquals('Aprovado', $response->situacao);
+            $response = $this->deleteScore($this->enrollment, $discipline->id, 2);
+            $this->assertTrue($response->any_error_msg);
+        }
+        $registration = $this->enrollment->registration;
+        $this->assertEquals(1, $registration->refresh()->aprovado);
+    }
+
+    public function testCursandoAposRemocaoDeFaltaEmEtapaConcluinte()
+    {
+        $schoolClass = $this->enrollment->schoolClass;
+        $school = $schoolClass->school;
+
+        $this->addAcademicYearStage($schoolClass, 2);
+        $this->addAcademicYearStage($schoolClass, 3);
+        $this->addAcademicYearStage($schoolClass, 4);
+
+        $discipline = factory(LegacyDiscipline::class)->create();
+        $schoolClass->disciplines()->attach($discipline->id, [
+            'ano_escolar_id' => $schoolClass->grade_id,
+            'escola_id' => $school->id
+        ]);
+
+        factory(LegacyDisciplineAcademicYear::class)->create([
+            'componente_curricular_id' => $discipline->id,
+            'ano_escolar_id' => $schoolClass->grade_id,
+        ]);
+
+        $disciplines = $schoolClass->disciplines;
+
+        foreach ($disciplines as $discipline) {
+            for ($stage = 1; $stage <= 4; $stage++) {
+                $this->postAbsence($this->enrollment, $discipline->id, $stage, '3');
+                $response = $this->postScore($this->enrollment, $discipline->id, $stage, '7');
+            }
+            $this->assertEquals('Aprovado', $response->situacao);
+            $response = $this->deleteAbsence($this->enrollment, $discipline->id, 4);
+            $this->assertEquals('Cursando', $response->situacao);
+        }
+        $registration = $this->enrollment->registration;
+        $this->assertEquals(3, $registration->refresh()->aprovado);
+    }
+
+    public function testRemocaoDeFaltaEmEtapaNaoConcluinte()
+    {
+        $schoolClass = $this->enrollment->schoolClass;
+        $school = $schoolClass->school;
+
+        $this->addAcademicYearStage($schoolClass, 2);
+        $this->addAcademicYearStage($schoolClass, 3);
+        $this->addAcademicYearStage($schoolClass, 4);
+
+        $discipline = factory(LegacyDiscipline::class)->create();
+        $schoolClass->disciplines()->attach($discipline->id, [
+            'ano_escolar_id' => $schoolClass->grade_id,
+            'escola_id' => $school->id
+        ]);
+
+        factory(LegacyDisciplineAcademicYear::class)->create([
+            'componente_curricular_id' => $discipline->id,
+            'ano_escolar_id' => $schoolClass->grade_id,
+        ]);
+
+        $disciplines = $schoolClass->disciplines;
+
+        foreach ($disciplines as $discipline) {
+            for ($stage = 1; $stage <= 4; $stage++) {
+                $this->postAbsence($this->enrollment, $discipline->id, $stage, '3');
+                $response = $this->postScore($this->enrollment, $discipline->id, $stage, '8');
+            }
+            $this->assertEquals('Aprovado', $response->situacao);
+            $response = $this->deleteAbsence($this->enrollment, $discipline->id, 2);
+            $this->assertTrue($response->any_error_msg);
+        }
+        $registration = $this->enrollment->registration;
+        $this->assertEquals(1, $registration->refresh()->aprovado);
+    }
 }
