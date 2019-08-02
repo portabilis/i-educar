22f5e926ea89b7a50ec487cc2dcc8a370bb675da
Eder Soares <edersoares@me.com>
2019-08-02 16:09:22 -0300

    Migrations para adicionar a coluna updated_at

---
+++ database/migrations/2019_08_02_100000_create_modules_regra_avaliacao_serie_ano_excluidos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModulesRegraAvaliacaoSerieAnoExcluidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modules.regra_avaliacao_serie_ano_excluidos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('serie_id');
+            $table->integer('regra_avaliacao_id');
+            $table->integer('regra_avaliacao_diferenciada_id')->nullable();
+            $table->integer('ano_letivo');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modules.regra_avaliacao_serie_ano_excluidos');
+    }
+}
