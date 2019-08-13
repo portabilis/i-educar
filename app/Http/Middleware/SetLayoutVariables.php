64444434867d65d0a3d0abb20fc1c7e0315801a9
Eder Soares <edersoares@me.com>
2019-08-13 10:37:54 -0300

    Altera middleware que seta variáveis para view

--- app/Http/Middleware/SetLayoutVariables.php
+++ app/Http/Middleware/SetLayoutVariables.php
@@ -3,7 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\View;
 
 class SetLayoutVariables
@@ -11,42 +12,16 @@ class SetLayoutVariables
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
+     * @param Request $request
+     * @param Closure $next
+     *
+     * @return Response
      */
     public function handle($request, Closure $next)
     {
-        $this->setLoggedUser();
-        $this->setLegacyConfig();
+        View::share('loggedUser', session('logged_user'));
+        View::share('config', config('legacy'));
 
         return $next($request);
-    }
-
-    /**
-     * Set view ariable with logged user date
-     */
-    private function setLoggedUser()
-    {
-        $personId = session('id_pessoa');
-
-        if ($personId) {
-            $person = DB::selectOne('SELECT nome, email FROM cadastro.pessoa WHERE idpes = :personId', ['personId' => $personId]);
-        }
-
-        $loggedUser = new \stdClass();
-        $loggedUser->personId = $personId;
-        $loggedUser->name = $person->nome ?? null;
-        $loggedUser->email = $person->email ?? null;
-
-        View::share('loggedUser', $loggedUser);
-    }
-
-    /**
-     * Set view variable with legacy configs
-     */
-    private function setLegacyConfig()
-    {
-        View::share('config', config('legacy'));
     }
 }
