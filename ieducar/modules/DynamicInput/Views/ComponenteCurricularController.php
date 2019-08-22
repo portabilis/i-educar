64052ae36db9651e6eeef1f5cab0d0d0ca22cb20
bonot <paula_bonot@hotmail.com>
2019-08-22 15:13:54 -0300

    Aplica PSR no controller do componente de seleção de componentes

--- ieducar/modules/DynamicInput/Views/ComponenteCurricularController.php
+++ ieducar/modules/DynamicInput/Views/ComponenteCurricularController.php
@@ -1,93 +1,53 @@
 <?php
 
-#error_reporting(E_ALL);
-#ini_set("display_errors", 1);
-
-/**
- * i-Educar - Sistema de gestão escolar
- *
- * Copyright (C) 2006  Prefeitura Municipal de Itajaí
- *     <ctima@itajai.sc.gov.br>
- *
- * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
- * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
- * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
- * qualquer versão posterior.
- *
- * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
- * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
- * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
- * do GNU para mais detalhes.
- *
- * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
- * com este programa; se não, escreva para a Free Software Foundation, Inc., no
- * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
- *
- * @author    Lucas D'Avila <lucasdavila@portabilis.com.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   Avaliacao
- * @subpackage  Modules
- * @since   Arquivo disponível desde a versão ?
- * @version   $Id$
- */
-
 require_once 'lib/Portabilis/Controller/ApiCoreController.php';
 require_once 'Portabilis/Business/Professor.php';
 
-/**
- * ComponenteCurricularController class.
- *
- * @author      Lucas D'Avila <lucasdavila@portabilis.com.br>
- * @category    i-Educar
- * @license     @@license@@
- * @package     Avaliacao
- * @subpackage  Modules
- * @since       Classe disponível desde a versão 1.1.0
- * @version     @@package_version@@
- */
 class ComponenteCurricularController extends ApiCoreController
 {
-
-  protected function canGetComponentesCurriculares() {
-    return $this->validatesId('turma') &&
-           $this->validatesPresenceOf('ano');
-  }
-
-  private function agrupaComponentesCurriculares($componentesCurriculares){
-    $options = array();
-
-    foreach ($componentesCurriculares as $componenteCurricular) {
-        $areaConhecimento = (($componenteCurricular['secao_area_conhecimento'] != '') ? $componenteCurricular['secao_area_conhecimento'] . ' - ' : '') . $componenteCurricular['area_conhecimento'];
-        $options['__' . $componenteCurricular['id']] = array(
-                 'value' => mb_strtoupper($componenteCurricular['nome'], 'UTF-8'),
-                 'group' => mb_strtoupper($areaConhecimento, 'UTF-8')
-        );
+    protected function canGetComponentesCurriculares()
+    {
+        return
+            $this->validatesId('turma') &&
+            $this->validatesPresenceOf('ano');
     }
 
-    return $options;
+    private function agrupaComponentesCurriculares($componentesCurriculares)
+    {
+        $options = [];
 
-  }
+        foreach ($componentesCurriculares as $componenteCurricular) {
+            $areaConhecimento = (($componenteCurricular['secao_area_conhecimento'] != '') ? $componenteCurricular['secao_area_conhecimento'] . ' - ' : '') . $componenteCurricular['area_conhecimento'];
+            $options[
+                '__' . $componenteCurricular['id']
+            ] = [
+                'value' => mb_strtoupper($componenteCurricular['nome'], 'UTF-8'),
+                'group' => mb_strtoupper($areaConhecimento, 'UTF-8')
+            ];
+        }
 
-  protected function getComponentesCurricularesForDiario(){
-    if ($this->canGetComponentesCurriculares()) {
-        $userId        = $this->getSession()->id_pessoa;
-        $instituicaoId = $this->getRequest()->instituicao_id;
-        $turmaId       = $this->getRequest()->turma_id;
-        $ano           = $this->getRequest()->ano;
-    $etapa         = $this->getRequest()->etapa;
-
-    if($etapa == ""){
-      $etapa = "0";
+        return $options;
     }
 
-      $isProfessor   = Portabilis_Business_Professor::isProfessor($instituicaoId, $userId);
+    protected function getComponentesCurricularesForDiario()
+    {
+        if ($this->canGetComponentesCurriculares()) {
+            $userId = $this->getSession()->id_pessoa;
+            $instituicaoId = $this->getRequest()->instituicao_id;
+            $turmaId = $this->getRequest()->turma_id;
+            $ano = $this->getRequest()->ano;
+            $etapa = $this->getRequest()->etapa;
 
-      if ($isProfessor) {
-        $componentesCurriculares = Portabilis_Business_Professor::componentesCurricularesAlocado($instituicaoId, $turmaId, $ano, $userId);
-      }
-      else{
-        $sql = "select cc.id,
+            if ($etapa == '') {
+                $etapa = '0';
+            }
+
+            $isProfessor = Portabilis_Business_Professor::isProfessor($instituicaoId, $userId);
+
+            if ($isProfessor) {
+                $componentesCurriculares = Portabilis_Business_Professor::componentesCurricularesAlocado($instituicaoId, $turmaId, $ano, $userId);
+            } else {
+                $sql = 'select cc.id,
                        cc.nome,
                        ac.nome as area_conhecimento,
                        ac.secao as area_conhecimento_secao,
@@ -103,13 +63,13 @@ class ComponenteCurricularController extends ApiCoreController
                        turma.ref_ref_cod_escola = al.ref_cod_escola and
                        cc.area_conhecimento_id = ac.id and
                        (turma.ref_cod_disciplina_dispensada <> cc.id OR turma.ref_cod_disciplina_dispensada is null) and
-                     (case when cct.etapas_especificas = 1 then $3 = ANY (string_to_array(cct.etapas_utilizadas,',')::int[]) else true end)
-                 order by ac.secao, ac.nome, cc.ordenamento, cc.nome";
+                     (case when cct.etapas_especificas = 1 then $3 = ANY (string_to_array(cct.etapas_utilizadas,\',\')::int[]) else true end)
+                 order by ac.secao, ac.nome, cc.ordenamento, cc.nome';
 
-        $componentesCurriculares = $this->fetchPreparedQuery($sql, array($turmaId, $ano, $etapa));
+                $componentesCurriculares = $this->fetchPreparedQuery($sql, [$turmaId, $ano, $etapa]);
 
-        if (count($componentesCurriculares) < 1) {
-          $sql = "select cc.id,
+                if (count($componentesCurriculares) < 1) {
+                    $sql = 'select cc.id,
                          cc.nome,
                          ac.nome as area_conhecimento,
                          ac.secao as secao_area_conhecimento,
@@ -127,35 +87,34 @@ class ComponenteCurricularController extends ApiCoreController
                          al.ativo = 1 and
                          $2 = ANY(esd.anos_letivos) and
                          (t.ref_cod_disciplina_dispensada <> cc.id OR t.ref_cod_disciplina_dispensada is null) and
-                         (case when esd.etapas_especificas = 1 then $3 = ANY (string_to_array(esd.etapas_utilizadas,',')::int[]) else true end)
-                   order by ac.secao, ac.nome, cc.ordenamento, cc.nome";
+                         (case when esd.etapas_especificas = 1 then $3 = ANY (string_to_array(esd.etapas_utilizadas,\',\')::int[]) else true end)
+                   order by ac.secao, ac.nome, cc.ordenamento, cc.nome';
 
-          $componentesCurriculares = $this->fetchPreparedQuery($sql, array($turmaId, $ano, $etapa));
+                    $componentesCurriculares = $this->fetchPreparedQuery($sql, [$turmaId, $ano, $etapa]);
+                }
+            }
+
+            $options = [];
+            $options = $this->agrupaComponentesCurriculares($componentesCurriculares);
+
+            return ['options' => $options];
         }
-      }
-
-      $options = array();
-      $options = $this->agrupaComponentesCurriculares($componentesCurriculares);
-      return array('options' => $options);
     }
-  }
 
+    protected function getComponentesCurriculares()
+    {
+        if ($this->canGetComponentesCurriculares()) {
+            $userId = $this->getSession()->id_pessoa;
+            $instituicaoId = $this->getRequest()->instituicao_id;
+            $turmaId = $this->getRequest()->turma_id;
+            $ano = $this->getRequest()->ano;
 
-  protected function getComponentesCurriculares() {
-    if ($this->canGetComponentesCurriculares()) {
+            $isProfessor = Portabilis_Business_Professor::isProfessor($instituicaoId, $userId);
 
-      $userId        = $this->getSession()->id_pessoa;
-      $instituicaoId = $this->getRequest()->instituicao_id;
-      $turmaId       = $this->getRequest()->turma_id;
-      $ano           = $this->getRequest()->ano;
-
-      $isProfessor   = Portabilis_Business_Professor::isProfessor($instituicaoId, $userId);
-
-      if ($isProfessor) {
-        $componentesCurriculares = Portabilis_Business_Professor::componentesCurricularesAlocado($instituicaoId, $turmaId, $ano, $userId);
-      }
-      else{
-        $sql = "select cc.id,
+            if ($isProfessor) {
+                $componentesCurriculares = Portabilis_Business_Professor::componentesCurricularesAlocado($instituicaoId, $turmaId, $ano, $userId);
+            } else {
+                $sql = 'select cc.id,
                        cc.nome,
                        ac.nome as area_conhecimento,
                        ac.secao as area_conhecimento_secao,
@@ -171,13 +130,12 @@ class ComponenteCurricularController extends ApiCoreController
                        cct.componente_curricular_id = cc.id and al.ano = $2 and
                        cct.escola_id = al.ref_cod_escola and
                        cc.area_conhecimento_id = ac.id
-                 order by ac.secao, ac.nome, cc.ordenamento, cc.nome";
+                 order by ac.secao, ac.nome, cc.ordenamento, cc.nome';
 
+                $componentesCurriculares = $this->fetchPreparedQuery($sql, [$turmaId, $ano]);
 
-        $componentesCurriculares = $this->fetchPreparedQuery($sql, array($turmaId, $ano));
-
-        if (count($ComponentesCurriculares) < 1) {
-          $sql = "select cc.id,
+                if (count($ComponentesCurriculares) < 1) {
+                    $sql = 'select cc.id,
                          cc.nome,
                          ac.nome as area_conhecimento,
                          ac.secao as secao_area_conhecimento,
@@ -196,24 +154,27 @@ class ComponenteCurricularController extends ApiCoreController
                          al.ativo = 1 and
                          $2 = ANY(esd.anos_letivos) AND
                          cc.area_conhecimento_id = ac.id
-                  order by ac.secao, ac.nome, cc.ordenamento, cc.nome";
+                  order by ac.secao, ac.nome, cc.ordenamento, cc.nome';
 
-          $componentesCurriculares = $this->fetchPreparedQuery($sql, array($turmaId, $ano));
+                    $componentesCurriculares = $this->fetchPreparedQuery($sql, [$turmaId, $ano]);
+                }
+            }
+
+            $options = [];
+            $options = $this->agrupaComponentesCurriculares($componentesCurriculares);
+
+            return ['options' => $options];
         }
-      }
-
-      $options = array();
-      $options = $this->agrupaComponentesCurriculares($componentesCurriculares);
-      return array('options' => $options);
     }
-  }
 
-  public function Gerar() {
-    if ($this->isRequestFor('get', 'componentesCurriculares'))
-      $this->appendResponse($this->getComponentesCurriculares());
-    elseif($this->isRequestFor('get', 'componentesCurricularesForDiario'))
-       $this->appendResponse($this->getComponentesCurricularesForDiario());
-    else
-      $this->notImplementedOperationError();
-  }
+    public function Gerar()
+    {
+        if ($this->isRequestFor('get', 'componentesCurriculares')) {
+            $this->appendResponse($this->getComponentesCurriculares());
+        } elseif ($this->isRequestFor('get', 'componentesCurricularesForDiario')) {
+            $this->appendResponse($this->getComponentesCurricularesForDiario());
+        } else {
+            $this->notImplementedOperationError();
+        }
+    }
 }
