119eff54b6862d5f9db507040a4f45d91a230ee4
bonot <paula_bonot@hotmail.com>
2019-08-22 14:51:34 -0300

    Melhora identação das querys

--- ieducar/modules/DynamicInput/Views/TurmaController.php
+++ ieducar/modules/DynamicInput/Views/TurmaController.php
@@ -39,15 +39,37 @@ class TurmaController extends ApiCoreController
                 $turmas = Portabilis_Business_Professor::turmasAlocado($instituicaoId, $escolaId, $serieId, $userId);
             } else {
                 if (is_numeric($ano)) {
-                    $sql = 'select cod_turma as id, nm_turma || \' - \' || COALESCE(ano::varchar,\'SEM ANO\') as nome from pmieducar.turma where ref_ref_cod_escola = $1
-                   and (ref_ref_cod_serie = $2 or ref_ref_cod_serie_mult = $2) and ativo = 1 and
-                   visivel != \'f\' and turma.ano = $3 order by nm_turma asc';
+                    $sql = "
+                        SELECT
+                            cod_turma AS id,
+                            nm_turma || ' - ' || COALESCE(ano::varchar,'SEM ANO') AS nome
+                        FROM pmieducar.turma
+                        WHERE ref_ref_cod_escola = $1
+                        AND (
+                            ref_ref_cod_serie = $2
+                            OR ref_ref_cod_serie_mult = $2
+                        )
+                        AND ativo = 1
+                        AND visivel != 'f'
+                        AND turma.ano = $3
+                        ORDER BY nm_turma asc
+                    ";
 
                     $turmas = $this->fetchPreparedQuery($sql, [$escolaId, $serieId, $ano]);
                 } else {
-                    $sql = 'select cod_turma as id, nm_turma || \' - \' || COALESCE(ano::varchar,\'SEM ANO\') as nome from pmieducar.turma where ref_ref_cod_escola = $1
-                   and (ref_ref_cod_serie = $2 or ref_ref_cod_serie_mult = $2) and ativo = 1 and
-                   visivel != \'f\' order by nm_turma asc';
+                    $sql = "
+                        SELECT
+                            cod_turma AS id,
+                            nm_turma || ' - ' || COALESCE(ano::varchar,'SEM ANO') AS nome
+                        FROM pmieducar.turma
+                        WHERE ref_ref_cod_escola = $1
+                        AND (
+                            ref_ref_cod_serie = $2
+                            OR ref_ref_cod_serie_mult = $2
+                        ) and ativo = 1
+                        AND visivel != 'f'
+                        ORDER BY nm_turma asc
+                    ";
 
                     $turmas = $this->fetchPreparedQuery($sql, [$escolaId, $serieId]);
                 }
