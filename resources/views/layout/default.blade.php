638a48f6aab23b62e6f54cb89e5e207dc2d923bb
Eder Soares <edersoares@me.com>
2019-08-27 17:39:39 -0300

    Envia o timestamp como inteiro
    - Utilizado parseInt para não gerar erro para IDE

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -13,7 +13,7 @@
             'user_id': '{{$loggedUser->personId}}',
             'user_name': '{{$loggedUser->name}}',
             'user_email': '{{$loggedUser->email}}',
-            'user_created_at': '{{$loggedUser->created_at}}',
+            'user_created_at': parseInt('{{$loggedUser->created_at}}'),
             'institution': '{{ $loggedUser->institution }}',
             'city': '{{ $loggedUser->city }}',
             'state': '{{ $loggedUser->state }}',
