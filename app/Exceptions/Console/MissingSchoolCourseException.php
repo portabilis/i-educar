a528340a149454729496f783edb79d5d591885be
Everton Muniz <munizeverton@gmail.com>
2019-08-30 17:25:17 -0300

    Cria comando para alterar a série de uma turma

---
+++ app/Exceptions/Console/MissingSchoolCourseException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Exceptions\Console;
+
+use App\Models\LegacyCourse;
+use App\Models\LegacySchool;
+use DomainException;
+
+class MissingSchoolCourseException extends DomainException
+{
+    public function __construct(LegacySchool $school, LegacyCourse $course)
+    {
+        $message = 'Não existe registro em escola curso para escola %s e curso %s';
+
+        $message = sprintf(
+            $message, $school->getKey(), $course->getKey()
+        );
+
+        parent::__construct($message);
+    }
+}
