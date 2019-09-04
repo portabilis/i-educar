14a7b9141f8956463d3cd781b5c3b991c9ca241c
Eder Soares <edersoares@me.com>
2019-09-04 16:37:31 -0300

    Ajusta factory de usuário

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,14 +1,12 @@
 <?php
 
 use App\User;
-use Illuminate\Database\Eloquent\Factory;
-use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Support\Str;
 
 /** @var Factory $factory */
 
-/** @var Factory $factory */
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
