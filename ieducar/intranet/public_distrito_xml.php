f0f497a7aced8e8c0f7a45b11d03e38e16a731a1
Eder Soares <edersoares@me.com>
2019-09-07 15:42:15 -0300

    Melhoria de código em distritos

--- ieducar/intranet/public_distrito_xml.php
+++ ieducar/intranet/public_distrito_xml.php
@@ -1,34 +1,28 @@
 <?php
 
-    header( 'Content-type: text/xml' );
+header('Content-type: text/xml');
 
-    require_once( "include/clsBanco.inc.php" );
-    require_once( "include/funcoes.inc.php" );
+require_once('include/clsBanco.inc.php');
+require_once('include/funcoes.inc.php');
+require_once 'Portabilis/Utils/DeprecatedXmlApi.php';
 
-  require_once 'Portabilis/Utils/DeprecatedXmlApi.php';
-  Portabilis_Utils_DeprecatedXmlApi::returnEmptyQueryUnlessUserIsLoggedIn();
+Portabilis_Utils_DeprecatedXmlApi::returnEmptyQueryUnlessUserIsLoggedIn();
 
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<query xmlns=\"sugestoes\">\n";
-    if( is_numeric( $_GET["idmun"] ) )
-    {
-        $db = new clsBanco();
-        $db->Consulta( "
-            SELECT
-                iddis
-                , nome
-            FROM
-                public.distrito
-            WHERE
-                idmun = '{$_GET["idmun"]}'
-            ORDER BY
-                nome ASC
-            " );
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<query xmlns=\"sugestoes\">\n";
+if (is_numeric($_GET['idmun'])) {
+    $db = new clsBanco();
+    $db->Consulta(
+        "
+            SELECT iddis, nome
+            FROM public.distrito
+            WHERE idmun = '{$_GET['idmun']}'
+            ORDER BY nome ASC
+        "
+    );
 
-        while ( $db->ProximoRegistro() )
-        {
-            list( $cod, $nome ) = $db->Tupla();
-            echo " <distrito iddis=\"{$cod}\">{$nome}</distrito>\n";
-        }
+    while ($db->ProximoRegistro()) {
+        list($cod, $nome) = $db->Tupla();
+        echo " <distrito iddis=\"{$cod}\">{$nome}</distrito>\n";
     }
-    echo "</query>";
-?>
+}
+echo '</query>';
