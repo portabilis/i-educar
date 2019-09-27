40193191baee05aaa2332655832b959ae1b5b577
Matheus Nicoski <matheusnicoski@gmail.com>
2019-09-27 11:01:08 -0300

    Altera a view de situação

--- database/migrations/2019_09_26_142534_altera_view_situacao.php
+++ database/migrations/2019_09_26_142534_altera_view_situacao.php
@@ -12,7 +12,7 @@ class AlteraViewSituacao extends Migration
      */
     public function up()
     {
-        $sql = file_get_contents(database_path('sqls/views/relatorio.view_situacao-2019-09-26.sql'));
+        $sql = file_get_contents(database_path('sqls/views/relatorio.view_situacao.sql'));
 
         DB::unprepared($sql);
     }
