587d39f5c21c287715654e6beed7ab65c219cc6f
Matheus Nicoski <matheusnicoski@gmail.com>
2019-10-04 11:39:52 -0300

    Altera nome do arquivo e class da migration

--- database/migrations/2019_09_26_142534_remove_condicao_view_situacao.php
+++ database/migrations/2019_09_26_142534_remove_condicao_view_situacao.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
 
-class AlteraViewSituacao extends Migration
+class RemoveCondicaoViewSituacao extends Migration
 {
     /**
      * Run the migrations.
