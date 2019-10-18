49304508646f2be3063d6b16ff9b52e955634f84
bonot <paula_bonot@hotmail.com>
2019-10-18 14:49:49 -0300

    Relaciona model de pessoa com deficiências

--- app/Models/LegacyPerson.php
+++ app/Models/LegacyPerson.php
@@ -41,4 +41,27 @@ class LegacyPerson extends EloquentBaseModel implements Transformable
     {
         return $this->nome;
     }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function deficiencies()
+    {
+        return $this->belongsToMany(
+            LegacyDeficiency::class,
+            'cadastro.fisica_deficiencia',
+            'ref_idpes',
+            'ref_cod_deficiencia',
+            'idpes',
+            'cod_deficiencia'
+        );
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function considerableDeficiencies()
+    {
+        return $this->deficiencies()->where('desconsidera_regra_diferenciada', false);
+    }
 }
