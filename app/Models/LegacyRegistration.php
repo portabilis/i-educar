ebc01e79b6da05c161499346ca6a08cece2404bb
bonot <paula_bonot@hotmail.com>
2019-10-18 14:50:47 -0300

    Implementa relacionamentos da matrícula e cria escopo para ativas

--- app/Models/LegacyRegistration.php
+++ app/Models/LegacyRegistration.php
@@ -85,6 +85,27 @@ class LegacyRegistration extends Model
     }
 
     /**
+     * Relação com a escola.
+     *
+     * @return BelongsTo
+     */
+    public function school()
+    {
+        return $this->belongsTo(LegacySchool::class, 'ref_ref_cod_escola');
+    }
+
+    /**
+     * Relação com a série.
+     *
+     * @return BelongsTo
+     */
+    public function level()
+    {
+        return $this->belongsTo(LegacyLevel::class, 'ref_ref_cod_serie');
+    }
+
+
+    /**
      * @return HasMany
      */
     public function enrollments()
@@ -118,5 +139,15 @@ class LegacyRegistration extends Model
     public function exemptions()
     {
         return $this->hasMany(LegacyDisciplineExemption::class, 'ref_cod_matricula', 'cod_matricula');
+    }
+
+    /**
+     * @param Builder $query
+     *
+     * @return Builder
+     */
+    public function scopeActive($query)
+    {
+        return $query->where('ativo', 1);
     }
 }
