f7d4af8324be39ef68df32731d4edc99c1dbcd40
Everton Muniz <munizeverton@gmail.com>
2019-10-24 17:33:13 -0300

    Força conexão para leitura casa a query seja um select

--- app/Providers/Postgres/PostgresConnection.php
+++ app/Providers/Postgres/PostgresConnection.php
@@ -3,14 +3,19 @@
 namespace App\Providers\Postgres;
 
 use Illuminate\Database\PostgresConnection as ParentPostgresConnection;
+use Illuminate\Support\Str;
 
 class PostgresConnection extends ParentPostgresConnection
 {
-    public function publicRun($query, $bindings = [], $useReadPdo = true)
+    public function publicRun($query, $bindings = [], $useReadPdo = false)
     {
         return parent::run($query, $bindings, function ($query, $bindings) use ($useReadPdo) {
             if ($this->pretending()) {
                 return [];
+            }
+
+            if (Str::startsWith(trim($query), 'SELECT')) {
+                $useReadPdo = true;
             }
 
             $statement = $this->prepared($this->getPdoForSelect($useReadPdo)
