05aa30b912e54b8e7f45d55b2db67a61dfad2b7c
Everton Muniz <munizeverton@gmail.com>
2019-10-25 09:36:38 -0300

    Cria parâmetro para forçar o uso da conexão de escrita

--- app/Providers/Postgres/PostgresConnection.php
+++ app/Providers/Postgres/PostgresConnection.php
@@ -7,15 +7,20 @@ use Illuminate\Support\Str;
 
 class PostgresConnection extends ParentPostgresConnection
 {
-    public function publicRun($query, $bindings = [], $useReadPdo = false)
+    public function publicRun($query, $bindings = [], $forceUseWritePdo = false)
     {
-        return parent::run($query, $bindings, function ($query, $bindings) use ($useReadPdo) {
+        return parent::run($query, $bindings, function ($query, $bindings) use ($forceUseWritePdo) {
             if ($this->pretending()) {
                 return [];
             }
 
+            $useReadPdo = false;
             if (Str::startsWith(trim($query), 'SELECT')) {
                 $useReadPdo = true;
+            }
+
+            if ($forceUseWritePdo) {
+                $useReadPdo = false;
             }
 
             $statement = $this->prepared($this->getPdoForSelect($useReadPdo)
