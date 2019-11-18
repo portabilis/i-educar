3b83c7537728e23b6dd43703a3a30ba4a0570682
Eder Soares <edersoares@me.com>
2019-11-18 14:44:43 -0300

    Style code

--- ieducar/intranet/educar_escola_serie_lst.php
+++ ieducar/intranet/educar_escola_serie_lst.php
@@ -1,207 +1,153 @@
 <?php
 
-/**
- * i-Educar - Sistema de gestão escolar
- *
- * Copyright (C) 2006  Prefeitura Municipal de Itajaí
- *                     <ctima@itajai.sc.gov.br>
- *
- * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
- * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
- * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
- * qualquer versão posterior.
- *
- * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
- * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
- * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
- * do GNU para mais detalhes.
- *
- * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
- * com este programa; se não, escreva para a Free Software Foundation, Inc., no
- * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Arquivo disponível desde a versão 1.0.0
- * @version   $Id$
- */
-
 require_once 'include/clsBase.inc.php';
 require_once 'include/clsListagem.inc.php';
 require_once 'include/clsBanco.inc.php';
 require_once 'include/pmieducar/geral.inc.php';
 
-/**
- * clsIndexBase class.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Classe disponível desde a versão 1.0.0
- * @version   @@package_version@@
- */
 class clsIndexBase extends clsBase
 {
-  function Formular()
-  {
-    $this->SetTitulo($this->_instituicao . ' i-Educar - Séries da escola');
-    $this->processoAp = '585';
-  }
+    public function Formular()
+    {
+        $this->SetTitulo($this->_instituicao . ' i-Educar - Séries da escola');
+
+        $this->processoAp = 585;
+    }
 }
 
-/**
- * indice class.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Classe disponível desde a versão 1.0.0
- * @version   @@package_version@@
- */
 class indice extends clsListagem
 {
-  var $pessoa_logada;
-  var $titulo;
-  var $limite;
-  var $offset;
+    public $limite;
+    public $offset;
+    public $ref_cod_serie;
+    public $ref_usuario_exc;
+    public $ref_usuario_cad;
+    public $hora_inicial;
+    public $hora_final;
+    public $data_cadastro;
+    public $data_exclusao;
+    public $ativo;
+    public $hora_inicio_intervalo;
+    public $hora_fim_intervalo;
+    public $ref_cod_curso;
+    public $ref_ref_cod_serie;
 
-  var $ref_cod_escola;
-  var $ref_cod_serie;
-  var $ref_usuario_exc;
-  var $ref_usuario_cad;
-  var $hora_inicial;
-  var $hora_final;
-  var $data_cadastro;
-  var $data_exclusao;
-  var $ativo;
-  var $hora_inicio_intervalo;
-  var $hora_fim_intervalo;
+    public function Gerar()
+    {
+        $this->titulo = 'Escola Série - Listagem';
 
-  var $ref_cod_curso;
-  var $ref_cod_instituicao;
-  var $ref_ref_cod_serie;
+        foreach ($_GET as $var => $val) { // passa todos os valores obtidos no GET para atributos do objeto
+            $this->$var = ($val === '') ? null : $val;
+        }
 
-  function Gerar()
-  {
-    $this->titulo = 'Escola Série - Listagem';
+        $lista_busca = [
+            'Série',
+            'Curso'
+        ];
 
-    foreach( $_GET AS $var => $val ) // passa todos os valores obtidos no GET para atributos do objeto
-      $this->$var = ( $val === '' ) ? null: $val;
+        $obj_permissao = new clsPermissoes();
+        $nivel_usuario = $obj_permissao->nivel_acesso($this->pessoa_logada);
 
-    $this->addBanner( 'imagens/nvp_top_intranet.jpg', 'imagens/nvp_vert_intranet.jpg', 'Intranet' );
+        $lista_busca[] = 'Escola';
+        $lista_busca[] = 'Instituição';
+        $lista_busca[] = 'Escola';
+        $this->addCabecalhos($lista_busca);
 
-    $lista_busca = array(
-      'S&eacute;rie',
-      'Curso'
-    );
+        $this->inputsHelper()->dynamic(['instituicao', 'escola', 'curso', 'serie']);
 
-    $obj_permissao = new clsPermissoes();
-    $nivel_usuario = $obj_permissao->nivel_acesso($this->pessoa_logada);
-    $lista_busca[] = 'Escola';
-    $lista_busca[] = 'Instituição';
-    $lista_busca[] = 'Escola';
-    $this->addCabecalhos($lista_busca);
+        // Paginador
+        $this->limite = 20;
+        $this->offset = $_GET["pagina_{$this->nome}"]
+            ? $_GET["pagina_{$this->nome}"] * $this->limite - $this->limite
+            : 0;
 
-    $this->inputsHelper()->dynamic(array('instituicao', 'escola', 'curso', 'serie'));
-
-    // Paginador
-    $this->limite = 20;
-    $this->offset = ($_GET["pagina_{$this->nome}"] ) ?
-      $_GET["pagina_{$this->nome}"] * $this->limite - $this->limite :
-      0;
-
-    $obj_escola_serie = new clsPmieducarEscolaSerie();
-    $obj_escola_serie->setOrderby('nm_serie ASC');
-    $obj_escola_serie->setLimite($this->limite, $this->offset);
+        $obj_escola_serie = new clsPmieducarEscolaSerie();
+        $obj_escola_serie->setOrderby('nm_serie ASC');
+        $obj_escola_serie->setLimite($this->limite, $this->offset);
 
         if (App_Model_IedFinder::usuarioNivelBibliotecaEscolar($this->pessoa_logada)) {
             $obj_escola_serie->codUsuario = $this->pessoa_logada;
         }
 
-    $lista = $obj_escola_serie->lista(
-      $this->ref_cod_escola,
-      $this->ref_cod_serie,
-      NULL,
-      NULL,
-      NULL,
-      NULL,
-      NULL,
-      NULL,
-      NULL,
-      NULL,
-      NULL,
-      NULL,
-      1,
-      NULL,
-      NULL,
-      NULL,
-      NULL,
-      $this->ref_cod_instituicao,
-      $this->ref_cod_curso
-    );
-
-    $total = $obj_escola_serie->_total;
-
-    // monta a lista
-    if (is_array($lista) && count($lista)) {
-      foreach ($lista as $registro) {
-        $obj_ref_cod_serie = new clsPmieducarSerie($registro['ref_cod_serie']);
-        $det_ref_cod_serie = $obj_ref_cod_serie->detalhe();
-        $nm_serie = $det_ref_cod_serie['nm_serie'];
-
-        $obj_curso = new clsPmieducarCurso($registro['ref_cod_curso']);
-        $det_curso = $obj_curso->detalhe();
-        $registro['ref_cod_curso'] = $det_curso['nm_curso'];
-
-        $obj_ref_cod_escola = new clsPmieducarEscola($registro['ref_cod_escola']);
-        $det_ref_cod_escola = $obj_ref_cod_escola->detalhe();
-        $nm_escola = $det_ref_cod_escola['nome'];
-
-        $obj_ref_cod_instituicao = new clsPmieducarInstituicao($registro['ref_cod_instituicao']);
-        $det_ref_cod_instituicao = $obj_ref_cod_instituicao->detalhe();
-        $registro['ref_cod_instituicao'] = $det_ref_cod_instituicao['nm_instituicao'];
-
-        $lista_busca = array(
-          "<a href=\"educar_escola_serie_det.php?ref_cod_escola={$registro["ref_cod_escola"]}&ref_cod_serie={$registro["ref_cod_serie"]}\">{$nm_serie}</a>",
-          "<a href=\"educar_escola_serie_det.php?ref_cod_escola={$registro["ref_cod_escola"]}&ref_cod_serie={$registro["ref_cod_serie"]}\">{$registro["ref_cod_curso"]}</a>"
+        $lista = $obj_escola_serie->lista(
+            $this->ref_cod_escola,
+            $this->ref_cod_serie,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            1,
+            null,
+            null,
+            null,
+            null,
+            $this->ref_cod_instituicao,
+            $this->ref_cod_curso
         );
 
-        $lista_busca[] = "<a href=\"educar_escola_serie_det.php?ref_cod_escola={$registro["ref_cod_escola"]}&ref_cod_serie={$registro["ref_cod_serie"]}\">{$nm_escola}</a>";
-        $lista_busca[] = "<a href=\"educar_escola_serie_det.php?ref_cod_escola={$registro["ref_cod_escola"]}&ref_cod_serie={$registro["ref_cod_serie"]}\">{$registro["ref_cod_instituicao"]}</a>";
-        $lista_busca[] = "<a href=\"educar_escola_serie_det.php?ref_cod_escola={$registro["ref_cod_escola"]}&ref_cod_serie={$registro["ref_cod_serie"]}\">{$nm_escola}</a>";
+        $total = $obj_escola_serie->_total;
 
-        $this->addLinhas($lista_busca);
-      }
+        // monta a lista
+        if (is_array($lista) && count($lista)) {
+            foreach ($lista as $registro) {
+                $obj_ref_cod_serie = new clsPmieducarSerie($registro['ref_cod_serie']);
+                $det_ref_cod_serie = $obj_ref_cod_serie->detalhe();
+                $nm_serie = $det_ref_cod_serie['nm_serie'];
+
+                $obj_curso = new clsPmieducarCurso($registro['ref_cod_curso']);
+                $det_curso = $obj_curso->detalhe();
+                $registro['ref_cod_curso'] = $det_curso['nm_curso'];
+
+                $obj_ref_cod_escola = new clsPmieducarEscola($registro['ref_cod_escola']);
+                $det_ref_cod_escola = $obj_ref_cod_escola->detalhe();
+                $nm_escola = $det_ref_cod_escola['nome'];
+
+                $obj_ref_cod_instituicao = new clsPmieducarInstituicao($registro['ref_cod_instituicao']);
+                $det_ref_cod_instituicao = $obj_ref_cod_instituicao->detalhe();
+                $registro['ref_cod_instituicao'] = $det_ref_cod_instituicao['nm_instituicao'];
+
+                $lista_busca = [
+                    "<a href=\"educar_escola_serie_det.php?ref_cod_escola={$registro['ref_cod_escola']}&ref_cod_serie={$registro['ref_cod_serie']}\">{$nm_serie}</a>",
+                    "<a href=\"educar_escola_serie_det.php?ref_cod_escola={$registro['ref_cod_escola']}&ref_cod_serie={$registro['ref_cod_serie']}\">{$registro['ref_cod_curso']}</a>"
+                ];
+
+                $lista_busca[] = "<a href=\"educar_escola_serie_det.php?ref_cod_escola={$registro['ref_cod_escola']}&ref_cod_serie={$registro['ref_cod_serie']}\">{$nm_escola}</a>";
+                $lista_busca[] = "<a href=\"educar_escola_serie_det.php?ref_cod_escola={$registro['ref_cod_escola']}&ref_cod_serie={$registro['ref_cod_serie']}\">{$registro['ref_cod_instituicao']}</a>";
+                $lista_busca[] = "<a href=\"educar_escola_serie_det.php?ref_cod_escola={$registro['ref_cod_escola']}&ref_cod_serie={$registro['ref_cod_serie']}\">{$nm_escola}</a>";
+
+                $this->addLinhas($lista_busca);
+            }
+        }
+
+        $this->addPaginador2(
+            'educar_escola_serie_lst.php',
+            $total,
+            $_GET,
+            $this->nome,
+            $this->limite
+        );
+
+        if ($obj_permissao->permissao_cadastra(585, $this->pessoa_logada, 7)) {
+            $this->acao = 'go("educar_escola_serie_cad.php")';
+            $this->nome_acao = 'Novo';
+        }
+
+        $this->largura = '100%';
+
+        $this->breadcrumb('Séries da escola', [
+            url('intranet/educar_index.php') => 'Escola',
+        ]);
     }
-    $this->addPaginador2("educar_escola_serie_lst.php", $total, $_GET,
-      $this->nome, $this->limite);
-
-    if ($obj_permissao->permissao_cadastra(585, $this->pessoa_logada, 7)) {
-      $this->acao = "go(\"educar_escola_serie_cad.php\")";
-      $this->nome_acao = "Novo";
-    }
-
-    $this->largura = "100%";
-
-    $this->breadcrumb('Séries da escola', [
-        url('intranet/educar_index.php') => 'Escola',
-    ]);
-  }
 }
 
-// Instancia objeto de página
 $pagina = new clsIndexBase();
-
-// Instancia objeto de conteúdo
 $miolo = new indice();
 
-// Atribui o conteúdo à  página
 $pagina->addForm($miolo);
-
-// Gera o código HTML
 $pagina->MakeAll();
-?>
