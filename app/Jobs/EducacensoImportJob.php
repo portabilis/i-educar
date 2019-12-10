75a959b0e4b0a1b82ed2318e791549640ad3f358
Everton Muniz <munizeverton@gmail.com>
2019-12-10 14:01:20 -0300

    Adiciona bloca try catch para a execução da importação

--- app/Jobs/EducacensoImportJob.php
+++ app/Jobs/EducacensoImportJob.php
@@ -10,6 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class EducacensoImportJob implements ShouldQueue
 {
@@ -29,7 +30,7 @@ class EducacensoImportJob implements ShouldQueue
      */
     private $databaseConnection;
 
-    public $timeout = 300;
+    public $timeout = 600;
 
     /**
      * Create a new job instance.
@@ -49,14 +50,20 @@ class EducacensoImportJob implements ShouldQueue
      * Execute the job.
      *
      * @return void
+     * @throws Throwable
      */
     public function handle()
     {
         DB::setDefaultConnection($this->databaseConnection);
         DB::beginTransaction();
 
-        $importService = ImportServiceFactory::createImportService($this->educacensoImport->year);
-        $importService->import($this->importArray, $this->educacensoImport->user);
+        try {
+            $importService = ImportServiceFactory::createImportService($this->educacensoImport->year);
+            $importService->import($this->importArray, $this->educacensoImport->user);
+        } catch (Throwable $e) {
+            DB::rollBack();
+            throw $e;
+        }
 
         $educacensoImport = $this->educacensoImport;
         $educacensoImport->finished = true;
