e1ba417562b21538e0c72fa5512d0b1ac4b8a5d0
Eder Soares <edersoares@me.com>
2019-12-26 15:36:17 -0300

    Corrige componente_curricular_ano_escolar_excluidos

--- database/migrations/2019_01_01_400000_add_trigger_deleted_at_in_modules_componente_curricular_ano_escolar_excluidos_table.php
+++ database/migrations/2019_01_01_400000_add_trigger_deleted_at_in_modules_componente_curricular_ano_escolar_excluidos_table.php
@@ -15,15 +15,11 @@ class AddTriggerDeletedAtInModulesComponenteCurricularAnoEscolarExcluidosTable e
     public function up()
     {
         $this->whenDeletedMoveTo('modules.componente_curricular_ano_escolar', 'modules.componente_curricular_ano_escolar_excluidos', [
-            'id',
-            'ano',
-            'instituicao_id',
-            'turma_id',
-            'servidor_id',
-            'funcao_exercida',
-            'tipo_vinculo',
-            'permite_lancar_faltas_componente',
-            'turno_id',
+            'componente_curricular_id',
+            'ano_escolar_id',
+            'carga_horaria',
+            'tipo_nota',
+            'anos_letivos',
         ]);
     }
 
