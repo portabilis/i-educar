62d31d99397cfda83a2c2b5954f71bbb764e6e30
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2020-02-20 18:11:49 -0300

    Exportação para o Seb
    Refs #6985

---
+++ app/Http/Controllers/SebExportController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SebExportRequest;
+use App\Services\SebExport\ExportService;
+use App\Process;
+use Illuminate\View\View;
+use Illuminate\Http\Response as ResponseReturn;
+use Response;
+
+class SebExportController extends Controller
+{
+    public function index() : View
+    {
+        $this->breadcrumb('Exportação para o SEB', [
+            url('intranet/educar_index.php') => 'Escola',
+        ]);
+        $this->menu(Process::SEB_EXPORT);
+
+        return view('seb-export.index');
+    }
+
+    public function export(SebExportRequest $request, ExportService $exportService) : ResponseReturn
+    {
+        $exportContent = $exportService->export($request->all());
+
+        $headers = [
+          'Content-type' => 'text/plain',
+          'Content-Disposition' => 'attachment; filename="exportacao_seb.txt"',
+          'Content-Length' => strlen($exportContent),
+        ];
+
+        return Response::make($exportContent, 200, $headers);
+    }
+}
