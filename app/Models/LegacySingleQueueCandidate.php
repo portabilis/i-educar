76380fc5478c471afe5e816c46684e6ae3dda0c7
Everton Muniz <munizeverton@gmail.com>
2020-03-20 10:25:30 -0300

    Reverte alteração

--- app/Models/LegacySingleQueueCandidate.php
+++ app/Models/LegacySingleQueueCandidate.php
@@ -107,14 +107,7 @@ class LegacySingleQueueCandidate extends Model
      */
     public function schools()
     {
-        return $this->belongsToMany(
-            LegacySchool::class,
-            'pmieducar.escola_candidato_fila_unica',
-            'ref_cod_candidato_fila_unica',
-            'ref_cod_escola',
-            'cod_candidato_fila_unica',
-            'cod_escola',
-        );
+        return $this->hasMany(LegacySchoolSingleQueueCandidate::class, 'ref_cod_candidato_fila_unica');
     }
 
     /**
