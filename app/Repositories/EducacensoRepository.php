5b7a65e651cfb3907a0635896ee044544721a01a
Everton Muniz <munizeverton@gmail.com>
2020-03-25 11:34:55 -0300

    Ajusta forma de pegar telefones da escola

--- app/Repositories/EducacensoRepository.php
+++ app/Repositories/EducacensoRepository.php
@@ -52,7 +52,7 @@ class EducacensoRepository
             (SELECT COALESCE(
               (SELECT min(fone_pessoa.fone)
                     FROM cadastro.fone_pessoa
-                    WHERE j.idpes = fone_pessoa.idpes),
+                    WHERE j.idpes = fone_pessoa.idpes AND fone_pessoa.tipo = 1),
               (SELECT min(telefone)
                 FROM pmieducar.escola_complemento
                 WHERE escola_complemento.ref_cod_escola = e.cod_escola))) AS telefone,
