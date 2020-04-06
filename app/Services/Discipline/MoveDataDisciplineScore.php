52b85982d3311e02cbf664fcb624607244021c79
Everton Muniz <munizeverton@gmail.com>
2020-04-06 07:01:19 -0300

    Ajusta update em nota_componente_curricular

--- app/Services/Discipline/MoveDataDisciplineScore.php
+++ app/Services/Discipline/MoveDataDisciplineScore.php
@@ -10,7 +10,7 @@ class MoveDataDisciplineScore implements MoveDisciplineDataInterface
     public function moveData($disciplineFrom, $disciplineTo, $year, $gradeId)
     {
         LegacyDisciplineScore::query()
-            ->where('componente_curricular_id', $disciplineTo)
+            ->where('componente_curricular_id', $disciplineFrom)
             ->whereHas('registrationScore', function (Builder $registrationScoreQuery) use ($gradeId, $year) {
                 $registrationScoreQuery->whereHas('registration', function (Builder $registrationQuery) use ($gradeId, $year) {
                     $registrationQuery->where('ano', $year);
