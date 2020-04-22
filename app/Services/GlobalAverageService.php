68d03008a588e8ca58da11c28277c8cee441198e
Everton Muniz <munizeverton@gmail.com>
2020-04-22 10:25:16 -0300

    Ajusta cálculo de acordo com a regra de avaliação

--- app/Services/GlobalAverageService.php
+++ app/Services/GlobalAverageService.php
@@ -3,10 +3,34 @@
 namespace App\Services;
 
 use App\Models\LegacyRegistration;
+use App_Model_IedFinder;
 
 class GlobalAverageService
 {
     public function getGlobalAverage(LegacyRegistration $registration)
+    {
+        $evaluationRule = App_Model_IedFinder::getRegraAvaliacaoPorMatricula($registration->getKey());
+
+        if ($this->isGlobalScore($evaluationRule)) {
+            return  $this->getAverageGlobalScore($registration);
+        }
+
+        return $this->getAverage($registration);
+    }
+
+    private function isGlobalScore(\RegraAvaliacao_Model_Regra $evaluationRule)
+    {
+        return $evaluationRule->notaGeralPorEtapa == 1;
+    }
+
+    private function getAverageGlobalScore(LegacyRegistration $registration)
+    {
+        $scores = $registration->studentScore->averageGeneral->pluck('media');
+
+        return $scores->avg();
+    }
+
+    private function getAverage(LegacyRegistration $registration)
     {
         $scores = $registration->studentScore->averageByDiscipline->pluck('media');
 
