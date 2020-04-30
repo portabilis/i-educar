6b77dc9ef22172e74a5c8e30d070795e34cabb9c
bonot <paula_bonot@hotmail.com>
2020-04-30 10:50:03 -0300

    Implementa check de notas e pareceres

--- app/Services/CheckPostedDataService.php
+++ app/Services/CheckPostedDataService.php
@@ -2,15 +2,18 @@
 
 namespace App\Services;
 
-use App\Models\LegacyRegistration;
 use App\Models\LegacyStudentAbsence;
+use App\Models\LegacyStudentDescriptiveOpinion;
+use App\Models\LegacyStudentScore;
 
 class CheckPostedDataService
 {
     public function hasDataPosted($discipline, $level, $year)
     {
         return
-            $this->hasAbsencePosted($discipline, $level, $year);
+            $this->hasAbsencePosted($discipline, $level, $year) ||
+            $this->hasScorePosted($discipline, $level, $year) ||
+            $this->hasDescritiveOpinion($discipline, $level, $year);
     }
 
     public function hasAbsencePosted($discipline, $level, $year)
@@ -19,18 +22,53 @@ class CheckPostedDataService
             ->whereHas('registration', function ($queryRegistration) use ($level, $year) {
                 $queryRegistration
                     ->where('ref_ref_cod_serie', $level)
-                    ->where('ativo', 1)
-                    ->where('ano', $year);
-            })->exists();
+                    ->where('ativo', 1);
+                    if ($year) {
+                        $queryRegistration->where('ano', $year);
+                    }
+            })
+            ->whereHas('absencesByDiscipline', function($queryAbsencesByDiscipline) use ($discipline) {
+                $queryAbsencesByDiscipline
+                    ->where('componente_curricular_id', $discipline);
+            })
+            ->exists();
     }
 
     public function hasScorePosted($discipline, $level, $year)
     {
-
+        return LegacyStudentScore::query()
+            ->whereHas('registration', function ($queryRegistration) use ($level, $year) {
+                $queryRegistration
+                    ->where('ref_ref_cod_serie', $level)
+                    ->where('ativo', 1);
+                    if ($year) {
+                        $queryRegistration
+                            ->where('ano', $year);
+                    }
+            })
+            ->whereHas('scoreByDiscipline', function ($queryScoreByDiscipline) use ($discipline){
+                $queryScoreByDiscipline
+                    ->where('componente_curricular_id', $discipline);
+            })
+            ->exists();
     }
 
     public function hasDescritiveOpinion($discipline, $level, $year)
     {
-
+        return LegacyStudentDescriptiveOpinion::query()
+        ->whereHas('registration', function ($queryRegistration) use ($level, $year) {
+            $queryRegistration
+                ->where('ref_ref_cod_serie', $level)
+                ->where('ativo', 1);
+                if ($year) {
+                    $queryRegistration
+                        ->where('ano', $year);
+                }
+        })
+        ->whereHas('descriptiveOpinionByDiscipline', function ($queryDescriptiveOpinionByDiscipline) use ($discipline){
+            $queryDescriptiveOpinionByDiscipline
+                ->where('componente_curricular_id', $discipline);
+        })
+        ->exists();
     }
 }
