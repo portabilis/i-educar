a747328715b9bd0350dcf3e9dfb2936c93d0c686
Everton Muniz <munizeverton@gmail.com>
2020-05-06 11:11:37 -0300

    Ajusta nome da migration

--- database/migrations/2020_05_06_104131_change_person_unification_menu.php
+++ database/migrations/2020_05_06_104131_change_person_unification_menu.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangePersonUnifcationMenu extends Migration
+class ChangePersonUnificationMenu extends Migration
 {
     /**
      * Run the migrations.
