9e315afb1d1be1a18ad1119487d600d2dc36e303
Everton Muniz <munizeverton@gmail.com>
2020-05-25 13:57:22 -0300

    Altera a data de referência do Educacenso para 11/03/2020

---
+++ database/migrations/2020_05_25_135118_altera_data_educacenso2020_v2.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Migrations\Migration;
+
+class AlteraDataEducacenso2020V2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('pmieducar.instituicao')
+            ->where('ativo', 1)
+            ->update(['data_educacenso' => '2020-03-11']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('pmieducar.instituicao')
+            ->where('ativo', 1)
+            ->update(['data_educacenso' => '2020-05-27']);
+    }
+}
