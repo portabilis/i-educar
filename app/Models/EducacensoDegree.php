529805f3dd839074165be9fea894dabeafbf5371
Everton Muniz <munizeverton@gmail.com>
2020-05-27 11:21:07 -0300

    Adiciona novo grau acadêmico

--- app/Models/EducacensoDegree.php
+++ app/Models/EducacensoDegree.php
@@ -17,6 +17,11 @@ class EducacensoDegree extends Model
      */
     protected $primaryKey = 'id';
 
+    const GRAU_TECNOLOGICO = 1;
+    const GRAU_LICENCIATURA = 2;
+    const GRAU_BACHARELADO = 3;
+    const GRAU_SEQUENCIAL = 4;
+
     protected $fillable = [
         'curso_id',
         'nome',
