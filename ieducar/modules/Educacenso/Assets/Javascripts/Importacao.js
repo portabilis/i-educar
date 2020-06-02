4f0b47fcd10cbf80b81c4fffdd7072c4a52157a7
Everton Muniz <munizeverton@gmail.com>
2020-06-02 11:15:23 -0300

    Ajusta action do form

--- ieducar/modules/Educacenso/Assets/Javascripts/Importacao.js
+++ ieducar/modules/Educacenso/Assets/Javascripts/Importacao.js
@@ -3,6 +3,8 @@ $j(document).ready(function(){
 });
 
 function acao() {
+  $j('#formcadastro').attr('action', '/educacenso/importacao');
+
   $j('#formcadastro').removeAttr('onsubmit');
   $j('#formcadastro').submit();
 }
