03250f0c46ffccb23ce85af52da29b901c4c5f7e
Everton Muniz <munizeverton@gmail.com>
2020-06-25 16:30:04 -0300

    Ajusta migration

--- database/migrations/2020_06_24_170500_create_release_period_dates_table.php
+++ database/migrations/2020_06_24_170500_create_release_period_dates_table.php
@@ -15,8 +15,8 @@ class CreateReleasePeriodDatesTable extends Migration
     {
         Schema::create('release_period_dates', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->date('start_data');
-            $table->date('end_data');
+            $table->date('start_date');
+            $table->date('end_date');
             $table->integer('release_period_id');
 
             $table->foreign('release_period_id')->references('id')->on('release_period');
