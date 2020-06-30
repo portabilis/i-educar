700265b809a9b3c79b96b39950d6c70ab5ad2d5e
bonot <paula_bonot@hotmail.com>
2020-06-30 09:20:44 -0300

    Cria models para upload de arquivos

---
+++ app/Models/File.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    protected $fillable = [
+        'url',
+        'type',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function relations()
+    {
+        return $this->hasMany(FilesRelations::class);
+    }
+}
---
+++ resources/views/uploads/upload.blade.php
@@ -0,0 +1,13 @@
+<tr id="tr_documento">
+    <td class="formmdtd" valign="top"><span class="form">Arquivos</span></td>
+    <td class="formmdtd" valign="top">
+        <span class="form">
+            <input class="inputfile inputfile-buttom" name="file" id="file" type="file" size="40" value="">
+            <label id="file" for="file"><span></span> <strong>Escolha um arquivo</strong></label>&nbsp;<br>
+            <img src="imagens/indicator.gif" style="margin-top: 3px; display: none;">
+            <span id="span-documento" style="font-style: italic; font-size: 10px;">
+                São aceitos arquivos nos formatos jpg, png, pdf e gif. Tamanho máximo: 250KB
+            </span>
+        </span>
+    </td>
+</tr>
