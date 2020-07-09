daed222cfb085c3d637e3694b6af721b3af7fa9f
Everton Muniz <munizeverton@gmail.com>
2020-07-09 14:25:12 -0300

    Ajusta nome da view

--- database/sqls/views/public.exporter_stages-2020-07-09.sql
+++ database/sqls/views/public.exporter_stages-2020-07-09.sql
@@ -1,4 +1,4 @@
-create or replace view public.exportes_stages as
+create or replace view public.exporter_stages as
 select * from public.exporter_school_stages
 union
 select * from public.exporter_school_class_stages
