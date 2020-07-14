4947627461a892dc0a252481aea123a79d52eb66
Everton Muniz <munizeverton@gmail.com>
2020-07-14 17:13:23 -0300

    Adiciona LIMIT para bases com menus duplicados

--- database/migrations/2020_07_14_093931_grant_permission_educacenso_consult.php
+++ database/migrations/2020_07_14_093931_grant_permission_educacenso_consult.php
@@ -18,13 +18,13 @@ class GrantPermissionEducacensoConsult extends Migration
                         1,
                         1,
                         1,
-                        (SELECT id FROM public.menus WHERE process = 847)
+                        (SELECT id FROM public.menus WHERE process = 847 LIMIT 1)
                    FROM pmieducar.menu_tipo_usuario
-                   WHERE menu_id = (SELECT id FROM public.menus WHERE process = 846)
+                   WHERE menu_id = (SELECT id FROM public.menus WHERE process = 846 LIMIT 1)
                    AND NOT EXISTS(
                      SELECT 1 FROM pmieducar.menu_tipo_usuario mtu
                      WHERE mtu.ref_cod_tipo_usuario = menu_tipo_usuario.ref_cod_tipo_usuario
-                       AND mtu.menu_id = (SELECT id FROM public.menus WHERE process = 847))');
+                       AND mtu.menu_id = (SELECT id FROM public.menus WHERE process = 847 LIMIT 1))');
     }
 
     /**
