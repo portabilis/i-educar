431f62285d616fd57d84be7a46c55e4b1be882b2
Everton Muniz <munizeverton@gmail.com>
2020-09-03 11:31:04 -0300

    Pega os calendários ao mudar campos

--- resources/views/form/calendar.blade.php
+++ resources/views/form/calendar.blade.php
@@ -2,8 +2,8 @@
     <td class="formmdtd" valign="top"><span class="form">Calendários letivos</span></td>
     <td class="formmdtd" valign="top">
         <span class="form">
-            <select class="geral" name="stage_type" id="stage_type" style="width: 308px;">
-                <option value="">Selecione um tipo</option>
+            <input type="hidden" name="user" id="user" value="{{$user}}">
+            <select class="geral" name="calendars" id="calendars" style="width: 308px;" data-placeholder="Selecione os calendários" multiple>
                 @foreach($calendars as $calendar)
                     <option value="{{$calendar->start_date}}-{{$calendar->end_date}}">
                         {{(new DateTime($calendar->start_date))->format('d/m/Y')}} - {{(new DateTime($calendar->end_date))->format('d/m/Y')}}
