3e0e757334d83ff21b690333ba95963b18b2e092
Everton Muniz <munizeverton@gmail.com>
2020-09-22 15:23:19 -0300

    Controller da tela de dispensa em lote

---
+++ app/Http/Controllers/BatchExemptionController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UpdateSchoolClassReportCardRequest;
+use App\Models\LegacySchoolClass;
+use App\Process;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+use Portabilis_Model_Report_TipoBoletim;
+
+class BatchExemptionController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return View
+     */
+    public function index(Request $request)
+    {
+        $this->breadcrumb('Dispensa em lote', [
+            url('intranet/educar_configuracoes_index.php') => 'Configurações',
+        ]);
+
+        $this->menu(Process::BATCH_EXEMPTION);
+
+        return view('exemption.batch');
+    }
+
+    public function exempt(Request $request)
+    {
+
+    }
+}
