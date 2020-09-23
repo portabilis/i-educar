ef47f813e450cd4b7f84c2cec335b0c542db2303
Everton Muniz <munizeverton@gmail.com>
2020-09-23 16:45:28 -0300

    Cria job para dispensa em lote

---
+++ app/Jobs/BatchExemptionJob.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Concerns\ShouldNotificate;
+use App\Services\Exemption\BatchExemptionService;
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\DB;
+use Throwable;
+
+class BatchExemptionJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, ShouldNotificate;
+
+    /**
+     * @var array
+     */
+    private $importArray;
+
+    /**
+     * @var string
+     */
+    private $databaseConnection;
+
+    /**
+     * @var BatchExemptionService
+     */
+    private $batchExemptionService;
+
+    /**
+     * @var User
+     */
+    private $user;
+
+
+    public function __construct(BatchExemptionService $batchExemptionService, $databaseConnection, User $user)
+    {
+        $this->batchExemptionService = $batchExemptionService;
+        $this->databaseConnection = $databaseConnection;
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     * @throws Throwable
+     */
+    public function handle()
+    {
+        //DB::setDefaultConnection($this->databaseConnection);
+        DB::beginTransaction();
+
+        try {
+            $this->batchExemptionService->handle();
+        } catch (Throwable $exception) {
+            $this->notificateError();
+
+            DB::rollBack();
+            return;
+        }
+
+        $this->notificateSuccess();
+        DB::commit();
+    }
+
+    public function tags()
+    {
+        return [
+            $this->databaseConnection,
+            'batch-exemption'
+        ];
+    }
+
+    public function getSuccessMessage()
+    {
+        return 'O processo de Dispensa em lote foi finalizado.';
+    }
+
+    public function getNotificationUrl()
+    {
+        return url()->route('exemption-list.index');
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
