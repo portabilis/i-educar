1c025d4624749040260c244d8d28833386e15568
gustavomendess <gustavocod13@outlook.com>
2020-10-02 18:11:57 -0300

    primeiro commit

---
+++ resources/views/server-role/server-role.blade.php
@@ -0,0 +1,66 @@
+@inject('service', 'App\Services\UrlPresigner')
+<tr id="tr_funcao_Servidor">
+    <td class="formmdtd" valign="top"><span class="form">Função:</span></td>
+    <td class="formmdtd" valign="top">
+        <span class="form">
+            <table class="table-detail">
+                <tr>
+                    <th>Função</th>
+                    <th>Matrícula</th>
+                    <th>Cursos ministrados</th>
+                    <th>Componentes curriculares</th>
+                </tr>
+                @if(isset($disciplines))
+                    @foreach($disciplines as $discipline)
+                        <tr>
+                            <td> {{$discipline->name}} </td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    @endforeach
+                @endif
+            </table>
+        </span>
+    </td>
+</tr>
+
+<style>
+
+.table-detail {
+  border-collapse: collapse;
+  font-size: 14px;
+}
+
+.table-detail tr th {
+  font-weight: bold;
+  font-size: 14px;
+  text-align: left;
+  line-height: normal;
+  padding: 3px 8px;
+  background: #ccdce6 !important;
+}
+
+.table-detail tr td {
+  padding: 3px 8px;
+}
+
+.table-detail tr td ul{
+  padding: 0;
+  margin: 0;
+}
+
+.table-detail tr td ul li{
+  padding: 0;
+  margin: 0;
+}
+
+.table-detail tbody tr:nth-child(odd) {
+  background-color: #f5f9fd;
+}
+
+.table-detail tbody tr:nth-child(even) {
+  background-color: #ffffff;
+}
+
+</style>
