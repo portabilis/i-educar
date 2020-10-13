d92cf5f5ac3edea50919822e100bc85b82dec9d9
Eder Soares <edersoares@me.com>
2020-10-13 15:32:40 -0300

    Middleware necessário para autenticação

--- config/lighthouse.php
+++ config/lighthouse.php
@@ -28,6 +28,8 @@ return [
          * make sure to return spec-compliant responses in case an error is thrown.
          */
         'middleware' => [
+            'web',
+
             \Nuwave\Lighthouse\Support\Http\Middleware\AcceptJson::class,
 
             // Logs in a user if they are authenticated. In contrast to Laravel's 'auth'
