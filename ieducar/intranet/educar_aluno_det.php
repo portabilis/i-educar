98891be7bbb7d5ba14fa1be5468be770e5ca4091
gustavomendess <gustavocod13@outlook.com>
2020-11-10 11:28:01 -0300

    Corrige tela de detalhes e remove chamadas das colunas que foram removidas

--- ieducar/intranet/educar_aluno_det.php
+++ ieducar/intranet/educar_aluno_det.php
@@ -747,10 +747,8 @@ class indice extends clsDetalhe
                 $this->addDetalhe(['Qual', $reg['desc_plano_saude']]);
             }
 
-            $this->addDetalhe(['<span id="tr_tit_dados_hospital">Em caso de emergência, levar para hospital ou clínica</span>']);
-            $this->addDetalhe(['Nome', $reg['hospital_clinica']]);
-            $this->addDetalhe(['Endereço', $reg['hospital_clinica_endereco']]);
-            $this->addDetalhe(['Telefone', $reg['hospital_clinica_telefone']]);
+            $this->addDetalhe(['<span id="tr_tit_dados_hospital">Em caso de emergência, autorizo levar meu(minha) filho(a) para o Hospital ou Clínica mais próximos:</span>']);
+            $this->addDetalhe(['Responsável', $reg['desc_aceita_hospital_proximo']]);
             $this->addDetalhe(['<span id="tr_tit_dados_hospital">Em caso de emergência, se não for possível contatar os responsáveis, comunicar</span>']);
             $this->addDetalhe(['Nome', $reg['responsavel_nome']]);
             $this->addDetalhe(['Parentesco', $reg['responsavel_parentesco']]);
