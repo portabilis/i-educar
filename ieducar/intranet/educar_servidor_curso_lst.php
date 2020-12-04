bc3dd5d84d62308ca5151ba12d5e564d58c9e9b3
gustavomendess <gustavocod13@outlook.com>
2020-12-04 09:39:58 -0300

    Code style

--- ieducar/intranet/educar_servidor_curso_lst.php
+++ ieducar/intranet/educar_servidor_curso_lst.php
@@ -21,10 +21,15 @@
  * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
  *
  * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
+ *
  * @category  i-Educar
+ *
  * @license   @@license@@
+ *
  * @package   iEd_Pmieducar
+ *
  * @since     Arquivo disponível desde a versão 1.0.0
+ *
  * @version   $Id$
  */
 
@@ -39,156 +44,212 @@ require_once 'include/pmieducar/geral.inc.php';
  * clsIndexBase class.
  *
  * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
+ *
  * @category  i-Educar
+ *
  * @license   @@license@@
+ *
  * @package   iEd_Pmieducar
+ *
  * @since     Classe disponível desde a versão 1.0.0
+ *
  * @version   @@package_version@@
  */
 class clsIndexBase extends clsBase
 {
-  function Formular()
-  {
-    $this->SetTitulo($this->_instituicao . ' i-Educar - Servidor Curso');
-    $this->processoAp         = 0;
-    $this->renderBanner       = FALSE;
-    $this->renderMenu         = FALSE;
-    $this->renderMenuSuspenso = FALSE;
-  }
+    public function Formular()
+    {
+        $this->SetTitulo($this->_instituicao . ' i-Educar - Servidor Curso');
+        $this->processoAp         = 0;
+        $this->renderBanner       = false;
+        $this->renderMenu         = false;
+        $this->renderMenuSuspenso = false;
+    }
 }
 
 /**
  * indice class.
  *
  * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
+ *
  * @category  i-Educar
+ *
  * @license   @@license@@
+ *
  * @package   iEd_Pmieducar
+ *
  * @since     Classe disponível desde a versão 1.0.0
+ *
  * @version   @@package_version@@
  */
 class indice extends clsCadastro
 {
-  var $pessoa_logada;
+    public $pessoa_logada;
 
-  var $cod_servidor;
-  var $ref_cod_instituicao;
-  var $ref_idesco;
-  var $ref_cod_funcao;
-  var $carga_horaria;
-  var $data_cadastro;
-  var $data_exclusao;
-  var $ativo;
-  var $ref_cod_curso;
-  var $ref_cod_disciplina;
-  var $cursos_servidor;
+    public $cod_servidor;
+    public $ref_cod_instituicao;
+    public $ref_idesco;
+    public $ref_cod_funcao;
+    public $carga_horaria;
+    public $data_cadastro;
+    public $data_exclusao;
+    public $ativo;
+    public $ref_cod_curso;
+    public $ref_cod_disciplina;
+    public $cursos_servidor;
 
+    public function Inicializar()
+    {
+        $retorno = 'Novo';
 
-  function Inicializar()
-  {
-    $retorno = 'Novo';
-    
+        $this->cod_servidor        = $_GET['ref_cod_servidor'];
+        $this->ref_cod_instituicao = $_GET['ref_cod_instituicao'];
 
-    $this->cod_servidor        = $_GET['ref_cod_servidor'];
-    $this->ref_cod_instituicao = $_GET['ref_cod_instituicao'];
+        $obj_permissoes = new clsPermissoes();
 
-    $obj_permissoes = new clsPermissoes();
+        $obj_permissoes->permissao_cadastra(635, $this->pessoa_logada, 7, 'educar_servidor_lst.php');
 
-    $obj_permissoes->permissao_cadastra(635, $this->pessoa_logada, 7, 'educar_servidor_lst.php');
+        if (is_numeric($this->cod_servidor) && is_numeric($this->ref_cod_instituicao)) {
+            $obj = new clsPmieducarServidor(
+                $this->cod_servidor,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                $this->ref_cod_instituicao
+            );
 
-    if (is_numeric($this->cod_servidor) && is_numeric($this->ref_cod_instituicao)) {
-      $obj = new clsPmieducarServidor($this->cod_servidor, NULL, NULL, NULL, NULL,
-        NULL, NULL, $this->ref_cod_instituicao);
+            $registro  = $obj->detalhe();
 
-      $registro  = $obj->detalhe();
-
-      if ($registro) {
-        $retorno = 'Editar';
-      }
-    }
-
-    $this->cursos_servidor = Session::get('cursos_servidor');
-
-    if (!$this->cursos_servidor) {
-      $obj_servidor_curso = new clsPmieducarServidorCursoMinistra();
-
-      $lst_servidor_curso = $obj_servidor_curso->lista(NULL,
-        $this->ref_cod_instituicao, $this->cod_servidor);
-
-      if ($lst_servidor_curso) {
-        foreach ($lst_servidor_curso as $curso) {
-          $this->cursos_servidor[$curso['ref_cod_curso']] = $curso['ref_cod_curso'];
+            if ($registro) {
+                $retorno = 'Editar';
+            }
         }
-      }
+
+        $this->cursos_servidor = Session::get('cursos_servidor');
+
+        if (!$this->cursos_servidor) {
+            $obj_servidor_curso = new clsPmieducarServidorCursoMinistra();
+
+            $lst_servidor_curso = $obj_servidor_curso->lista(
+                null,
+                $this->ref_cod_instituicao,
+                $this->cod_servidor
+            );
+
+            if ($lst_servidor_curso) {
+                foreach ($lst_servidor_curso as $curso) {
+                    $this->cursos_servidor[$curso['ref_cod_curso']] = $curso['ref_cod_curso'];
+                }
+            }
+        }
+
+        return $retorno;
     }
 
-    return $retorno;
-  }
+    public function Gerar()
+    {
+        $this->campoOculto('ref_cod_instituicao', $this->ref_cod_instituicao);
+        $opcoes = $opcoes_curso = ['' => 'Selecione'];
 
-  function Gerar()
-  {
-    $this->campoOculto('ref_cod_instituicao', $this->ref_cod_instituicao);
-    $opcoes = $opcoes_curso = array('' => 'Selecione');
+        $obj_cursos = new clsPmieducarCurso();
+        $obj_cursos->setOrderby('nm_curso');
 
-    $obj_cursos = new clsPmieducarCurso();
-    $obj_cursos->setOrderby('nm_curso');
+        $lst_cursos = $obj_cursos->lista(
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            1,
+            null,
+            $this->ref_cod_instituicao
+        );
 
-    $lst_cursos = $obj_cursos->lista( NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-      NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-      NULL, NULL, NULL, 1, NULL, $this->ref_cod_instituicao);
+        if ($lst_cursos) {
+            foreach ($lst_cursos as $curso) {
+                $opcoes_curso[$curso['cod_curso']] = $curso['nm_curso'];
+            }
+        }
 
-    if ($lst_cursos) {
-      foreach ($lst_cursos as $curso) {
-        $opcoes_curso[$curso['cod_curso']] = $curso['nm_curso'];
-      }
+        $arr_valores = [];
+
+        if ($this->cursos_servidor) {
+            foreach ($this->cursos_servidor as $curso) {
+                $arr_valores[] = [$curso];
+            }
+        }
+
+        $this->campoTabelaInicio(
+            'cursos_ministra',
+            'Cursos Ministrados',
+            ['Curso'],
+            $arr_valores,
+            ''
+        );
+
+        $this->campoLista(
+            'ref_cod_curso',
+            'Curso',
+            $opcoes_curso,
+            $this->ref_cod_curso,
+            '',
+            '',
+            '',
+            ''
+        );
+
+        $this->campoTabelaFim();
     }
 
-    $arr_valores = array();
+    public function Novo()
+    {
+        $curso_servidor = [];
+        if ($this->ref_cod_curso) {
+            foreach ($this->ref_cod_curso as $curso) {
+                $curso_servidor[$curso] = $curso;
+            }
+        }
 
-    if ($this->cursos_servidor) {
-      foreach ($this->cursos_servidor as $curso) {
-        $arr_valores[] = array($curso);
-      }
-    }
-
-    $this->campoTabelaInicio('cursos_ministra', 'Cursos Ministrados',
-      array('Curso'), $arr_valores, '');
-
-    $this->campoLista('ref_cod_curso', 'Curso', $opcoes_curso, $this->ref_cod_curso,
-      '', '', '', '');
-
-    $this->campoTabelaFim();
-  }
-
-  function Novo()
-  {
-    $curso_servidor = array();
-    if ($this->ref_cod_curso) {
-      foreach ($this->ref_cod_curso as $curso) {
-        $curso_servidor[$curso] = $curso;
-      }
-    }
-
-    Session::put([
+        Session::put([
         'cursos_servidor' => $curso_servidor,
         'cod_servidor' => $this->cod_servidor,
     ]);
-    Session::save();
-    Session::start();
+        Session::save();
+        Session::start();
 
-    echo "<script>parent.fechaExpansivel( '{$_GET['div']}');</script>";
-    die();
-  }
+        echo "<script>parent.fechaExpansivel( '{$_GET['div']}');</script>";
+        die();
+    }
 
-  public function Editar()
-  {
-    return $this->Novo();
-  }
+    public function Editar()
+    {
+        return $this->Novo();
+    }
 
-  function Excluir()
-  {
-    return FALSE;
-  }
+    public function Excluir()
+    {
+        return false;
+    }
 }
 
 // Instancia objeto de página
