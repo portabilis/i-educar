b249bb336fd2ff87eb0587fe3b6d75d9e1ea08ae
jrbarros <joalissonricardo@gmail.com>
2021-01-21 09:42:14 -0300

    Aplica PRS

--- tests/Unit/Modules/Service/ParecerDescritivoAlunoTest.php
+++ tests/Unit/Modules/Service/ParecerDescritivoAlunoTest.php
@@ -21,11 +21,16 @@
  * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
  *
  * @author      Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
+ *
  * @category    i-Educar
+ *
  * @license     @@license@@
+ *
  * @package     Avaliacao
  * @subpackage  UnitTests
+ *
  * @since       Arquivo disponível desde a versão 1.1.0
+ *
  * @version     $Id$
  */
 
@@ -35,47 +40,52 @@ require_once __DIR__.'/TestCommon.php';
  * Avaliacao_Service_ParecerDescritivoAlunoTest class.
  *
  * @author      Eriksen Costa Paixão <eriksen.paixao_bs@cobra.com.br>
+ *
  * @category    i-Educar
+ *
  * @license     @@license@@
+ *
  * @package     Avaliacao
  * @subpackage  UnitTests
+ *
  * @since       Classe disponível desde a versão 1.1.0
+ *
  * @version     @@package_version@@
  */
 class Avaliacao_Service_ParecerDescritivoAlunoTest extends Avaliacao_Service_TestCommon
 {
-  protected function setUp(): void
-  {
-    $this->_setRegraOption('parecerDescritivo', RegraAvaliacao_Model_TipoParecerDescritivo::ANUAL_COMPONENTE);
-    parent::setUp();
-  }
+    protected function setUp(): void
+    {
+        $this->_setRegraOption('parecerDescritivo', RegraAvaliacao_Model_TipoParecerDescritivo::ANUAL_COMPONENTE);
+        parent::setUp();
+    }
 
-  public function testCriaNovaInstanciaDeParecerDescritivoAluno()
-  {
-    $parecerAluno = $this->_getConfigOption('parecerDescritivoAluno', 'instance');
+    public function testCriaNovaInstanciaDeParecerDescritivoAluno()
+    {
+        $parecerAluno = $this->_getConfigOption('parecerDescritivoAluno', 'instance');
 
-    $parecerSave  = clone $parecerAluno;
-    $parecerSave->id = NULL;
+        $parecerSave  = clone $parecerAluno;
+        $parecerSave->id = null;
 
-    // Configura mock para Avaliacao_Model_ParecerDescritivoAlunoDataMapper
-    $mock = $this->getCleanMock('Avaliacao_Model_ParecerDescritivoAlunoDataMapper');
-    $mock->expects($this->at(0))
+        // Configura mock para Avaliacao_Model_ParecerDescritivoAlunoDataMapper
+        $mock = $this->getCleanMock('Avaliacao_Model_ParecerDescritivoAlunoDataMapper');
+        $mock->expects($this->at(0))
          ->method('findAll')
-         ->with(array(), array('matricula' => $this->_getConfigOption('matricula', 'cod_matricula')))
-         ->will($this->returnValue(array()));
+         ->with([], ['matricula' => $this->_getConfigOption('matricula', 'cod_matricula')])
+         ->will($this->returnValue([]));
 
-    $mock->expects($this->at(1))
+        $mock->expects($this->at(1))
          ->method('save')
          ->with($parecerSave)
-         ->will($this->returnValue(TRUE));
+         ->will($this->returnValue(true));
 
-    $mock->expects($this->at(2))
+        $mock->expects($this->at(2))
          ->method('findAll')
-         ->with(array(), array('matricula' => $this->_getConfigOption('matricula', 'cod_matricula')))
-         ->will($this->returnValue(array($parecerAluno)));
+         ->with([], ['matricula' => $this->_getConfigOption('matricula', 'cod_matricula')])
+         ->will($this->returnValue([$parecerAluno]));
 
-    $this->_setParecerDescritivoAlunoDataMapperMock($mock);
+        $this->_setParecerDescritivoAlunoDataMapperMock($mock);
 
-    $service = $this->_getServiceInstance();
-  }
+        $service = $this->_getServiceInstance();
+    }
 }
