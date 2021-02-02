ddcf5b1642c793ea11d911baaf68f255f8eba2d0
Eder Soares <edersoares@me.com>
2021-02-02 11:54:37 -0300

    Mantém nome padrão

--- app/Console/Commands/LegacyCreateTestsCommand.php
+++ app/Console/Commands/LegacyCreateTestsCommand.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class CreateViewTest extends Command
+class LegacyCreateTestsCommand extends Command
 {
     /**
      * The name and signature of the console command.
