55682a27d92ec5785d766b6ebac1cea8a094e021
bonot <paula_bonot@hotmail.com>
2021-02-08 15:18:34 -0300

    Atualiza series quando altera o curso

--- ieducar/intranet/scripts/tabelaSerieMult.js
+++ ieducar/intranet/scripts/tabelaSerieMult.js
@@ -17,3 +17,37 @@ function configuraCamposExibidos() {
         $j('#tr_turma_serie').hide();
     }
 }
+
+function atualizaOpcoesDeSerie(input) {
+    let instituicaoId = $j('#ref_cod_instituicao').val();
+    let escolaId = $j('#ref_cod_escola').val();
+    let cursoId = input.value;
+    let linha = input.id.replace(/\D/g, '');
+
+    var url = getResourceUrlBuilder.buildUrl(
+        '/module/Api/Serie',
+        'series',
+        {
+            instituicao_id : instituicaoId,
+            escola_id : escolaId,
+            curso_id : cursoId,
+            ativo : 1
+        }
+    );
+
+    var options = {
+        url      : url,
+        dataType : 'json',
+        success  : function(response) {
+            let comboSerie = $j('select[id="mult_serie_id['+linha+']"]');
+            comboSerie.empty();
+            comboSerie.append('<option value="">Selecione uma série</option>');
+
+            $j.each(response.series, function(key, serie) {
+                comboSerie.append('<option value="' + serie.id + '">' + serie.nome + '</option>');
+            });
+        }
+    };
+
+    getResources(options);
+}
