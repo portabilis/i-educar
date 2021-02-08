dff9779ee342fc1bad1fc63cdf48bde3f6b0cbde
bonot <paula_bonot@hotmail.com>
2021-02-08 16:21:34 -0300

    Atualiza opçoes de curso conforme escola selecionada

--- ieducar/intranet/scripts/tabelaSerieMult.js
+++ ieducar/intranet/scripts/tabelaSerieMult.js
@@ -1,7 +1,20 @@
+let opcoesCurso = [];
 configuraCamposExibidos();
 
 $j('#multiseriada').change(function(){
     configuraCamposExibidos();
+});
+
+$j('#ref_cod_escola').change(function(){
+    atualizaOpcoesDeCurso();
+});
+
+$j('#btn_add_tab_add_1').click(function(){
+    let lastComboId = $j('select[name^="mult_curso_id"]').length - 1;
+    let lastCombo = $j('select[name="mult_curso_id['+lastComboId+']"]');
+    $j.each(opcoesCurso, function(key, curso) {
+        lastCombo.append('<option value="' + key + '">' + curso + '</option>');
+    });
 });
 
 function configuraCamposExibidos() {
@@ -16,6 +29,38 @@ function configuraCamposExibidos() {
         $j('#tr_ref_cod_serie').show();
         $j('#tr_turma_serie').hide();
     }
+}
+
+function atualizaOpcoesDeCurso() {
+    let escolaId = $j('#ref_cod_escola').val();
+
+    var url = getResourceUrlBuilder.buildUrl(
+        '/module/Api/Curso',
+        'cursos-da-escola',
+        {
+            escola_id : escolaId
+        }
+    );
+
+    var options = {
+        url      : url,
+        dataType : 'json',
+        success  : function(response) {
+            opcoesCurso = response.cursos;
+            let combosCurso = $j('select[name^="mult_curso_id"]');
+            let combosSerie = $j('select[name^="mult_serie_id"]');
+            combosCurso.empty();
+            combosSerie.empty();
+            combosCurso.append('<option value="">Selecione um curso</option>');
+            combosSerie.append('<option value="">Selecione uma série</option>');
+
+            $j.each(response.cursos, function(key, curso) {
+                combosCurso.append('<option value="' + key + '">' + curso + '</option>');
+            });
+        }
+    };
+
+    getResources(options);
 }
 
 function atualizaOpcoesDeSerie(input) {
