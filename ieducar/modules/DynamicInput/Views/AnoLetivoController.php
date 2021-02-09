86cd5b3d665c538c26a4817918b61701b87d7ce0
bonot <paula_bonot@hotmail.com>
2021-02-09 15:48:36 -0300

    Cria metodo para buscar anos letivos em comum entre series

--- ieducar/modules/DynamicInput/Views/AnoLetivoController.php
+++ ieducar/modules/DynamicInput/Views/AnoLetivoController.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\LegacySchoolGrade;
+
 require_once 'lib/Portabilis/Controller/ApiCoreController.php';
 require_once 'lib/App/Model/IedFinder.php';
 
@@ -61,12 +63,45 @@ class AnoLetivoController extends ApiCoreController
         }
     }
 
+    protected function getAnosLetivosEmComumSeriesDaEscola()
+    {
+        $escolaId = $this->getRequest()->escola_id;
+        $series = explode(',', $this->getRequest()->series);
+
+        $anosLetivosPorSerie = LegacySchoolGrade::query()
+            ->where('ref_cod_escola', $escolaId)
+            ->whereIn('ref_cod_serie', $series)
+            ->get()
+            ->map(function($schoolGrade) {
+                $anosLetivos = str_replace(['{', '}'], '', $schoolGrade->anos_letivos);
+                $anosLetivos = explode(',', $anosLetivos);
+                return [
+                    'ref_cod_serie' => $schoolGrade->ref_cod_serie,
+                    'anos_letivos' => $anosLetivos,
+                ];
+            })
+            ->pluck('anos_letivos', 'ref_cod_serie')
+            ->toArray();
+
+        if (count($anosLetivosPorSerie) <= 1) {
+            return [
+                'anos_letivos' => array_values($anosLetivosPorSerie)[0],
+            ];
+        }
+
+        return [
+            'anos_letivos' => array_intersect(...$anosLetivosPorSerie),
+        ];
+    }
+
     public function Gerar()
     {
         if ($this->isRequestFor('get', 'anos_letivos')) {
             $this->appendResponse($this->getAnosLetivos());
         } elseif ($this->isRequestFor('get', 'anos_letivos_escola_serie')) {
             $this->appendResponse($this->getAnosLetivosPorEscolaSerie());
+        } elseif ($this->isRequestFor('get', 'anos_letivos_em_comum_series_da_escola')) {
+            $this->appendResponse($this->getAnosLetivosEmComumSeriesDaEscola());
         } else {
             $this->notImplementedOperationError();
         }
