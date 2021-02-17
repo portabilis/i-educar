2b582b347ebd6240367d7b80750e9db9efbe669a
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-02-17 08:55:44 -0300

    Ajusta turmas e matriculas com código do curso diferente da serie

---
+++ database/migrations/2021_02_17_082735_ajusta_turmas_e_matriculas_cod_curso_diferente_serie.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class AjustaTurmasEMatriculasCodCursoDiferenteSerie extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+        UPDATE pmieducar.turma t
+        SET ref_cod_curso = s.ref_cod_curso
+        FROM pmieducar.serie s
+        WHERE t.ref_ref_cod_serie = s.cod_serie
+        AND t.ref_cod_curso != s.ref_cod_curso;
+
+        UPDATE pmieducar.matricula m
+        SET ref_cod_curso = s.ref_cod_curso
+        FROM pmieducar.serie s
+        WHERE m.ref_ref_cod_serie = s.cod_serie
+        AND m.ref_cod_curso != s.ref_cod_curso;
+        ');
+    }
+}
