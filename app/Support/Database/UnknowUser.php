8155c296694bc777478738716c59d8eeeecba749
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-02-18 14:42:13 -0300

    Trait com metodos para lidar com o usuario 'Desconhecido'

---
+++ app/Support/Database/UnknowUser.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Support\Database;
+
+use Illuminate\Support\Facades\DB;
+
+trait UnknowUser
+{
+
+    public function getUnknowUserId(): int
+    {
+        $result = DB::selectOne("
+             SELECT idpes
+             FROM cadastro.pessoa
+             WHERE nome = 'Desconhecido'
+        ");
+
+        return $result->idpes;
+    }
+}
