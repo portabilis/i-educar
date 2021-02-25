9f568e4be2ebf929dc90bfbc273c944c0d056811
jrbarros <joalissonricardo@gmail.com>
2021-02-24 22:30:46 -0300

    Fix seção define status

--- ieducar/intranet/educar_definir_cliente_tipo_det.php
+++ ieducar/intranet/educar_definir_cliente_tipo_det.php
@@ -1,15 +1,11 @@
 <?php
 
-/**
- * @author Adriano Erik Weiguert Nagasava
- */
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} i-Educar - Cliente" );
-        $this->processoAp = "623";
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Cliente");
+        $this->processoAp = '623';
     }
 }
 
@@ -20,90 +16,87 @@ class indice extends clsDetalhe
      *
      * @var int
      */
-    var $titulo;
+    public $titulo;
 
-    var $cod_cliente;
-    var $ref_cod_cliente_tipo;
-    var $ref_usuario_exc;
-    var $ref_usuario_cad;
-    var $ref_idpes;
-    var $login;
-    var $senha;
-    var $data_cadastro;
-    var $data_exclusao;
-    var $ativo;
+    public $cod_cliente;
+    public $ref_cod_cliente_tipo;
+    public $ref_usuario_exc;
+    public $ref_usuario_cad;
+    public $ref_idpes;
+    public $login;
+    public $senha;
+    public $data_cadastro;
+    public $data_exclusao;
+    public $ativo;
 
-    function Gerar()
+    public function Gerar()
     {
-        $this->titulo = "Cliente - Detalhe";
+        $this->titulo = 'Cliente - Detalhe';
 
+        $this->cod_cliente          = $_GET['cod_cliente'];
+        $this->ref_cod_cliente_tipo = $_GET['cod_cliente_tipo'];
 
-        $this->cod_cliente          = $_GET["cod_cliente"];
-        $this->ref_cod_cliente_tipo = $_GET["cod_cliente_tipo"];
-
-        if ( !( isset( $this->cod_cliente ) && isset( $this->ref_cod_cliente_tipo ) ) ) {
-            $this->simpleRedirect("educar_definir_cliente_tipo_lst.php");
+        if (!(isset($this->cod_cliente) && isset($this->ref_cod_cliente_tipo))) {
+            $this->simpleRedirect('educar_definir_cliente_tipo_lst.php');
         }
 
         $tmp_obj = new clsPmieducarCliente();
-        $registro = $tmp_obj->listaCompleta( $this->cod_cliente,
-                                             null,
-                                             null,
-                                             null,
-                                             null,
-                                             null,
-                                             null,
-                                             null,
-                                             null,
-                                             null,
-                                             1,
-                                             null,
-                                             null,
-                                             $this->ref_cod_cliente_tipo );
+        $registro = $tmp_obj->listaCompleta(
+            $this->cod_cliente,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            1,
+            null,
+            null,
+            $this->ref_cod_cliente_tipo
+        );
 
-        if( ! $registro )
-        {
-            $this->simpleRedirect("educar_definir_cliente_tipo_lst.php");
-        }
-        else {
-            foreach ( $registro as $cliente )
-            {
-                if ( $cliente["nome"] ) {
-                    $this->addDetalhe( array( "Cliente", "{$cliente["nome"]}") );
+        if (! $registro) {
+            $this->simpleRedirect('educar_definir_cliente_tipo_lst.php');
+        } else {
+            foreach ($registro as $cliente) {
+                if ($cliente['nome']) {
+                    $this->addDetalhe([ 'Cliente', "{$cliente['nome']}"]);
                 }
-                if ( $cliente["nm_biblioteca"] ) {
-                    $this->addDetalhe( array( "Biblioteca", "{$cliente["nm_biblioteca"]}") );
+                if ($cliente['nm_biblioteca']) {
+                    $this->addDetalhe([ 'Biblioteca', "{$cliente['nm_biblioteca']}"]);
                 }
-                if ( $cliente["nm_tipo"] ) {
-                    $this->addDetalhe( array( "Tipo do Cliente", "{$cliente["nm_tipo"]}") );
+                if ($cliente['nm_tipo']) {
+                    $this->addDetalhe([ 'Tipo do Cliente', "{$cliente['nm_tipo']}"]);
                 }
                 $obj_banco = new clsBanco();
                 $sql_unico = "SELECT ref_cod_motivo_suspensao
                                 FROM pmieducar.cliente_suspensao
-                               WHERE ref_cod_cliente = {$cliente["cod_cliente"]}
+                               WHERE ref_cod_cliente = {$cliente['cod_cliente']}
                                  AND data_liberacao IS NULL
                                  AND EXTRACT ( DAY FROM ( NOW() - data_suspensao ) ) < dias";
-                $motivo    = $obj_banco->CampoUnico( $sql_unico );
-                if ( is_numeric( $motivo ) ) {
-                    $this->addDetalhe( array( "Status", "Suspenso" ) );
-                        $obj_motivo_suspensao = new clsPmieducarMotivoSuspensao( $motivo );
-                        $det_motivo_suspensao = $obj_motivo_suspensao->detalhe();
-                        $this->addDetalhe( array( "Motivo da Suspensão", "{$det_motivo_suspensao["nm_motivo"]}" ) );
-                        $this->addDetalhe( array( "Descrição", "{$det_motivo_suspensao["descricao"]}" ) );
+                $motivo    = $obj_banco->CampoUnico($sql_unico);
+                if (is_numeric($motivo)) {
+                    $this->addDetalhe([ 'Status', 'Suspenso' ]);
+                    $obj_motivo_suspensao = new clsPmieducarMotivoSuspensao($motivo);
+                    $det_motivo_suspensao = $obj_motivo_suspensao->detalhe();
+                    $this->addDetalhe([ 'Motivo da Suspensão', "{$det_motivo_suspensao['nm_motivo']}" ]);
+                    $this->addDetalhe([ 'Descrição', "{$det_motivo_suspensao['descricao']}" ]);
+                } else {
+                    $this->addDetalhe([ 'Status', 'Regular' ]);
                 }
-                else
-                    $this->addDetalhe( array( "Status", "Regular" ) );
             }
         }
         $obj_permissoes = new clsPermissoes();
-        if( $obj_permissoes->permissao_cadastra( 623, $this->pessoa_logada, 11 ) )
-        {
-        $this->url_novo = "educar_definir_cliente_tipo_cad.php";
-        $this->url_editar = "educar_definir_cliente_tipo_cad.php?cod_cliente={$cliente["cod_cliente"]}&cod_cliente_tipo={$cliente["cod_cliente_tipo"]}";
+        if ($obj_permissoes->permissao_cadastra(623, $this->pessoa_logada, 11)) {
+            $this->url_novo = 'educar_definir_cliente_tipo_cad.php';
+            $this->url_editar = "educar_definir_cliente_tipo_cad.php?cod_cliente={$cliente['cod_cliente']}&cod_cliente_tipo={$cliente['cod_cliente_tipo']}";
         }
 
-        $this->url_cancelar = "educar_definir_cliente_tipo_lst.php";
-        $this->largura = "100%";
+        $this->url_cancelar = 'educar_definir_cliente_tipo_lst.php';
+        $this->largura = '100%';
     }
 }
 
@@ -112,7 +105,6 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
-?>
--- ieducar/intranet/educar_definir_cliente_tipo_lst.php
+++ ieducar/intranet/educar_definir_cliente_tipo_lst.php
@@ -1,15 +1,11 @@
 <?php
 
-/**
- * @author Adriano Erik Weiguert Nagasava
- */
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} i-Educar - Cliente" );
-        $this->processoAp = "623";
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Cliente");
+        $this->processoAp = '623';
     }
 }
 
@@ -20,158 +16,156 @@ class indice extends clsListagem
      *
      * @var int
      */
-    var $pessoa_logada;
+    public $pessoa_logada;
 
     /**
      * Titulo no topo da pagina
      *
      * @var int
      */
-    var $titulo;
+    public $titulo;
 
     /**
      * Quantidade de registros a ser apresentada em cada pagina
      *
      * @var int
      */
-    var $limite;
+    public $limite;
 
     /**
      * Inicio dos registros a serem exibidos (limit)
      *
      * @var int
      */
-    var $offset;
+    public $offset;
 
-    var $cod_cliente;
-    var $ref_cod_cliente_tipo;
-    var $ref_cod_biblioteca;
-    var $ref_cod_escola;
-    var $ref_cod_instituicao;
-    var $ref_usuario_exc;
-    var $ref_usuario_cad;
-    var $ref_idpes;
-    var $login;
-    var $senha;
-    var $data_cadastro;
-    var $data_exclusao;
-    var $ativo;
-    var $status;
+    public $cod_cliente;
+    public $ref_cod_cliente_tipo;
+    public $ref_cod_biblioteca;
+    public $ref_cod_escola;
+    public $ref_cod_instituicao;
+    public $ref_usuario_exc;
+    public $ref_usuario_cad;
+    public $ref_idpes;
+    public $login;
+    public $senha;
+    public $data_cadastro;
+    public $data_exclusao;
+    public $ativo;
+    public $status;
 
-    function Gerar()
+    public function Gerar()
     {
-        $this->titulo = "Cliente - Listagem";
+        $this->titulo = 'Cliente - Listagem';
 
-        foreach( $_GET AS $var => $val ) // passa todos os valores obtidos no GET para atributos do objeto
-            $this->$var = ( $val === "" ) ? null: $val;
+        foreach ($_GET as $var => $val) { // passa todos os valores obtidos no GET para atributos do objeto
+            $this->$var = ($val === '') ? null: $val;
+        }
 
-
-
-        $lista_busca = array(
-            "Cliente",
-            "Tipo do Cliente"
-        );
+        $lista_busca = [
+            'Cliente',
+            'Tipo do Cliente'
+        ];
 
         // Filtros de Foreign Keys
         $get_instituicao          = true;
         $get_escola               = true;
         $get_biblioteca           = true;
         $get_cliente_tipo         = true;
-        $get_cabecalho = "lista_busca";
-        include("include/pmieducar/educar_campo_lista.php");
+        $get_cabecalho = 'lista_busca';
+        include('include/pmieducar/educar_campo_lista.php');
 
         $this->addCabecalhos($lista_busca);
 
-        $opcoes = array( "" => "Pesquise a pessoa clicando na lupa ao lado" );
+        $opcoes = [ '' => 'Pesquise a pessoa clicando na lupa ao lado' ];
 
         $parametros = new clsParametrosPesquisas();
-        $parametros->setSubmit( 0 );
-        $parametros->adicionaCampoSelect( "ref_idpes", "idpes", "nome" );
-        $parametros->setCodSistema( 1 );
-        $parametros->setPessoa( 'F' );
-        $parametros->setPessoaEditar( 'N' );
-        $parametros->setPessoaNovo( 'N' );
-        $this->campoListaPesq( "ref_idpes", "Cliente", $opcoes, $this->ref_idpes, "pesquisa_pessoa_lst.php", "", false, "", "", null, null, "", false, $parametros->serializaCampos() );
+        $parametros->setSubmit(0);
+        $parametros->adicionaCampoSelect('ref_idpes', 'idpes', 'nome');
+        $parametros->setCodSistema(1);
+        $parametros->setPessoa('F');
+        $parametros->setPessoaEditar('N');
+        $parametros->setPessoaNovo('N');
+        $this->campoListaPesq('ref_idpes', 'Cliente', $opcoes, $this->ref_idpes, 'pesquisa_pessoa_lst.php', '', false, '', '', null, null, '', false, $parametros->serializaCampos());
 
         // Paginador
         $this->limite = 20;
-        $this->offset = ( $_GET["pagina_{$this->nome}"] ) ? $_GET["pagina_{$this->nome}"]*$this->limite-$this->limite: 0;
+        $this->offset = ($_GET["pagina_{$this->nome}"]) ? $_GET["pagina_{$this->nome}"]*$this->limite-$this->limite: 0;
 
         $obj_cliente = new clsPmieducarCliente();
-        $obj_cliente->setOrderby( "nome ASC" );
-        $obj_cliente->setLimite( $this->limite, $this->offset );
+        $obj_cliente->setOrderby('nome ASC');
+        $obj_cliente->setLimite($this->limite, $this->offset);
 
-        if ( $this->status != 'S' )
+        if ($this->status != 'S') {
             $this->status = null;
-            $lista = $obj_cliente->listaCompleta( null,
-                                                  null,
-                                                  null,
-                                                  $this->ref_idpes,
-                                                  null,
-                                                  null,
-                                                  null,
-                                                  null,
-                                                  null,
-                                                  null,
-                                                  1,
-                                                  null,
-                                                  null,
-                                                  $this->ref_cod_cliente_tipo,
-                                                  $this->ref_cod_escola,
-                                                  $this->ref_cod_biblioteca,
-                                                  $this->ref_cod_instituicao );
+        }
+        $lista = $obj_cliente->listaCompleta(
+            null,
+            null,
+            null,
+            $this->ref_idpes,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            1,
+            null,
+            null,
+            $this->ref_cod_cliente_tipo,
+            $this->ref_cod_escola,
+            $this->ref_cod_biblioteca,
+            $this->ref_cod_instituicao
+        );
 
         $total = $obj_cliente->_total;
         $obj_banco = new clsBanco();
         // monta a lista
-        if( is_array( $lista ) && count( $lista ) )
-        {
-            foreach ( $lista AS $registro )
-            {
-                    $obj_ref_cod_biblioteca = new clsPmieducarBiblioteca( $registro["cod_biblioteca"] );
-                    $det_ref_cod_biblioteca = $obj_ref_cod_biblioteca->detalhe();
-                    $registro["cod_biblioteca"] = $det_ref_cod_biblioteca["nm_biblioteca"];
-                if( $registro["cod_instituicao"] )
-                {
-                    $obj_ref_cod_instituicao = new clsPmieducarInstituicao( $registro["cod_instituicao"] );
+        if (is_array($lista) && count($lista)) {
+            foreach ($lista as $registro) {
+                $obj_ref_cod_biblioteca = new clsPmieducarBiblioteca($registro['cod_biblioteca']);
+                $det_ref_cod_biblioteca = $obj_ref_cod_biblioteca->detalhe();
+                $registro['cod_biblioteca'] = $det_ref_cod_biblioteca['nm_biblioteca'];
+                if ($registro['cod_instituicao']) {
+                    $obj_ref_cod_instituicao = new clsPmieducarInstituicao($registro['cod_instituicao']);
                     $det_ref_cod_instituicao = $obj_ref_cod_instituicao->detalhe();
-                    $registro["cod_instituicao"] = $det_ref_cod_instituicao["nm_instituicao"];
+                    $registro['cod_instituicao'] = $det_ref_cod_instituicao['nm_instituicao'];
                 }
-                if( $registro["cod_escola"] )
-                {
+                if ($registro['cod_escola']) {
                     $obj_ref_cod_escola = new clsPmieducarEscola();
-                    $det_ref_cod_escola = array_shift($obj_ref_cod_escola->lista($registro["cod_escola"]));
-                    $registro["cod_escola"] = $det_ref_cod_escola["nome"];
+                    $det_ref_cod_escola = array_shift($obj_ref_cod_escola->lista($registro['cod_escola']));
+                    $registro['cod_escola'] = $det_ref_cod_escola['nome'];
                 }
 
+                $lista_busca = [
+                    "<a href=\"educar_definir_cliente_tipo_det.php?cod_cliente={$registro['cod_cliente']}&cod_cliente_tipo={$registro['cod_cliente_tipo']}\">{$registro['nome']}</a>",
+                    "<a href=\"educar_definir_cliente_tipo_det.php?cod_cliente={$registro['cod_cliente']}&cod_cliente_tipo={$registro['cod_cliente_tipo']}\">{$registro['nm_tipo']}</a>"
+                ];
 
-                $lista_busca = array(
-                    "<a href=\"educar_definir_cliente_tipo_det.php?cod_cliente={$registro["cod_cliente"]}&cod_cliente_tipo={$registro["cod_cliente_tipo"]}\">{$registro["nome"]}</a>",
-                    "<a href=\"educar_definir_cliente_tipo_det.php?cod_cliente={$registro["cod_cliente"]}&cod_cliente_tipo={$registro["cod_cliente_tipo"]}\">{$registro["nm_tipo"]}</a>"
-                );
-
-                if ($qtd_bibliotecas > 1 && ($nivel_usuario == 4 || $nivel_usuario == 8))
-                    $lista_busca[] = "<a href=\"educar_definir_cliente_tipo_det.php?cod_cliente={$registro["cod_cliente"]}&cod_cliente_tipo={$registro["cod_cliente_tipo"]}\">{$registro["cod_biblioteca"]}</a>";
-                else if ($nivel_usuario == 1 || $nivel_usuario == 2 || $nivel_usuario == 4)
-                    $lista_busca[] = "<a href=\"educar_definir_cliente_tipo_det.php?cod_cliente={$registro["cod_cliente"]}&cod_cliente_tipo={$registro["cod_cliente_tipo"]}\">{$registro["cod_biblioteca"]}</a>";
-                if ($nivel_usuario == 1 || $nivel_usuario == 2)
-                    $lista_busca[] = "<a href=\"educar_definir_cliente_tipo_det.php?cod_cliente={$registro["cod_cliente"]}&cod_cliente_tipo={$registro["cod_cliente_tipo"]}\">{$registro["cod_escola"]}</a>";
-                if ($nivel_usuario == 1)
-                    $lista_busca[] = "<a href=\"educar_definir_cliente_tipo_det.php?cod_cliente={$registro["cod_cliente"]}&cod_cliente_tipo={$registro["cod_cliente_tipo"]}\">{$registro["cod_instituicao"]}</a>";
+                if ($qtd_bibliotecas > 1 && ($nivel_usuario == 4 || $nivel_usuario == 8)) {
+                    $lista_busca[] = "<a href=\"educar_definir_cliente_tipo_det.php?cod_cliente={$registro['cod_cliente']}&cod_cliente_tipo={$registro['cod_cliente_tipo']}\">{$registro['cod_biblioteca']}</a>";
+                } elseif ($nivel_usuario == 1 || $nivel_usuario == 2 || $nivel_usuario == 4) {
+                    $lista_busca[] = "<a href=\"educar_definir_cliente_tipo_det.php?cod_cliente={$registro['cod_cliente']}&cod_cliente_tipo={$registro['cod_cliente_tipo']}\">{$registro['cod_biblioteca']}</a>";
+                }
+                if ($nivel_usuario == 1 || $nivel_usuario == 2) {
+                    $lista_busca[] = "<a href=\"educar_definir_cliente_tipo_det.php?cod_cliente={$registro['cod_cliente']}&cod_cliente_tipo={$registro['cod_cliente_tipo']}\">{$registro['cod_escola']}</a>";
+                }
+                if ($nivel_usuario == 1) {
+                    $lista_busca[] = "<a href=\"educar_definir_cliente_tipo_det.php?cod_cliente={$registro['cod_cliente']}&cod_cliente_tipo={$registro['cod_cliente_tipo']}\">{$registro['cod_instituicao']}</a>";
+                }
 
                 $this->addLinhas($lista_busca);
-
             }
         }
-        $this->addPaginador2( "educar_definir_cliente_tipo_lst.php", $total, $_GET, $this->nome, $this->limite );
+        $this->addPaginador2('educar_definir_cliente_tipo_lst.php', $total, $_GET, $this->nome, $this->limite);
         $obj_permissoes = new clsPermissoes();
-        if( $obj_permissoes->permissao_cadastra( 623, $this->pessoa_logada, 11 ) )
-        {
-            $this->acao = "go(\"educar_definir_cliente_tipo_cad.php\")";
-            $this->nome_acao = "Novo";
+        if ($obj_permissoes->permissao_cadastra(623, $this->pessoa_logada, 11)) {
+            $this->acao = 'go("educar_definir_cliente_tipo_cad.php")';
+            $this->nome_acao = 'Novo';
         }
 
-        $this->largura = "100%";
+        $this->largura = '100%';
     }
 }
 // cria uma extensao da classe base
@@ -179,7 +173,7 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
 ?>
