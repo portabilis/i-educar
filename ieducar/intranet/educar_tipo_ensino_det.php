533f6a19000ba891ea8adc096676048376beee43
jrbarros <joalissonricardo@gmail.com>
2021-02-24 22:39:24 -0300

    Fix seção tipo ensino

--- ieducar/intranet/educar_tipo_ensino_det.php
+++ ieducar/intranet/educar_tipo_ensino_det.php
@@ -1,12 +1,11 @@
 <?php
 
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} i-Educar - Tipo Ensino" );
-        $this->processoAp = "558";
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Tipo Ensino");
+        $this->processoAp = '558';
     }
 }
 
@@ -17,63 +16,59 @@ class indice extends clsDetalhe
      *
      * @var int
      */
-    var $titulo;
+    public $titulo;
 
-    var $cod_tipo_ensino;
-    var $ref_usuario_exc;
-    var $ref_usuario_cad;
-    var $nm_tipo;
-    var $data_cadastro;
-    var $data_exclusao;
-    var $ativo;
+    public $cod_tipo_ensino;
+    public $ref_usuario_exc;
+    public $ref_usuario_cad;
+    public $nm_tipo;
+    public $data_cadastro;
+    public $data_exclusao;
+    public $ativo;
 
-    var $ref_cod_instituicao;
+    public $ref_cod_instituicao;
 
-    function Gerar()
+    public function Gerar()
     {
-        $this->titulo = "Tipo Ensino - Detalhe";
+        $this->titulo = 'Tipo Ensino - Detalhe';
 
+        $this->cod_tipo_ensino=$_GET['cod_tipo_ensino'];
 
-        $this->cod_tipo_ensino=$_GET["cod_tipo_ensino"];
-
-        $tmp_obj = new clsPmieducarTipoEnsino( $this->cod_tipo_ensino,null,null,null,null,null,1);
-        if( !$registro = $tmp_obj->detalhe())
+        $tmp_obj = new clsPmieducarTipoEnsino($this->cod_tipo_ensino, null, null, null, null, null, 1);
+        if (!$registro = $tmp_obj->detalhe()) {
             $this->simpleRedirect('educar_tipo_ensino_lst.php');
-
-        if(!$registro["ativo"] )
-            $this->simpleRedirect('educar_tipo_ensino_lst.php');
-
-        if( $registro["cod_tipo_ensino"] )
-        {
-            $this->addDetalhe( array( "Tipo Ensino", "{$registro["cod_tipo_ensino"]}") );
         }
 
-        if( $registro["ref_cod_instituicao"] )
-        {
-                $obj_cod_instituicao = new clsPmieducarInstituicao( $registro["ref_cod_instituicao"] );
-                $obj_cod_instituicao_det = $obj_cod_instituicao->detalhe();
-                $registro["ref_cod_instituicao"] = $obj_cod_instituicao_det["nm_instituicao"];
-
-            $this->addDetalhe( array( "Institui&ccedil;&atilde;o", "{$registro["ref_cod_instituicao"]}") );
+        if (!$registro['ativo']) {
+            $this->simpleRedirect('educar_tipo_ensino_lst.php');
         }
-        if( $registro["nm_tipo"] )
-        {
-            $this->addDetalhe( array( "Nome Tipo", "{$registro["nm_tipo"]}") );
+
+        if ($registro['cod_tipo_ensino']) {
+            $this->addDetalhe([ 'Tipo Ensino', "{$registro['cod_tipo_ensino']}"]);
+        }
+
+        if ($registro['ref_cod_instituicao']) {
+            $obj_cod_instituicao = new clsPmieducarInstituicao($registro['ref_cod_instituicao']);
+            $obj_cod_instituicao_det = $obj_cod_instituicao->detalhe();
+            $registro['ref_cod_instituicao'] = $obj_cod_instituicao_det['nm_instituicao'];
+
+            $this->addDetalhe([ 'Institui&ccedil;&atilde;o', "{$registro['ref_cod_instituicao']}"]);
+        }
+        if ($registro['nm_tipo']) {
+            $this->addDetalhe([ 'Nome Tipo', "{$registro['nm_tipo']}"]);
         }
 
         //** Verificacao de permissao para cadastro ou edicao
         $obj_permissao = new clsPermissoes();
 
-        if($obj_permissao->permissao_cadastra(558, $this->pessoa_logada,7))
-        {
-            $this->url_novo = "educar_tipo_ensino_cad.php";
-            $this->url_editar = "educar_tipo_ensino_cad.php?cod_tipo_ensino={$registro["cod_tipo_ensino"]}";
+        if ($obj_permissao->permissao_cadastra(558, $this->pessoa_logada, 7)) {
+            $this->url_novo = 'educar_tipo_ensino_cad.php';
+            $this->url_editar = "educar_tipo_ensino_cad.php?cod_tipo_ensino={$registro['cod_tipo_ensino']}";
         }
         //**
 
-
-        $this->url_cancelar = "educar_tipo_ensino_lst.php";
-        $this->largura = "100%";
+        $this->url_cancelar = 'educar_tipo_ensino_lst.php';
+        $this->largura = '100%';
 
         $this->breadcrumb('Detalhe do tipo de ensino', [
             url('intranet/educar_index.php') => 'Escola',
@@ -86,7 +81,6 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
-?>
