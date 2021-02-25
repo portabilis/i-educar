d4dc986269cd20e0f4c0279380e44da074862149
jrbarros <joalissonricardo@gmail.com>
2021-02-24 22:45:02 -0300

    Fix seção unificação

--- ieducar/intranet/educar_unifica_aluno.php
+++ ieducar/intranet/educar_unifica_aluno.php
@@ -1,147 +1,123 @@
 <?php
 
-/**
- * i-Educar - Sistema de gestão escolar
- *
- * Copyright (C) 2006  Prefeitura Municipal de Itajaí
- *                     <ctima@itajai.sc.gov.br>
- *
- * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
- * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
- * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
- * qualquer versão posterior.
- *
- * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
- * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
- * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
- * do GNU para mais detalhes.
- *
- * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
- * com este programa; se não, escreva para a Free Software Foundation, Inc., no
- * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
- *
- * @author    Alan Felipe Farias <alan@portabilis.com.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Arquivo disponível desde a versão 1.0.0
- * @version   $Id$
- */
-
-
 use App\Models\LogUnification;
 use iEducar\Modules\Unification\StudentLogUnification;
 use Illuminate\Support\Facades\DB;
 
-
-
-
-
 class clsIndexBase extends clsBase
 {
-  function Formular()
-  {
-    $this->SetTitulo($this->_instituicao . ' i-Educar - Unifica&ccedil;&atilde;o de alunos');
-    $this->processoAp = "999847";
-  }
+    public function Formular()
+    {
+        $this->SetTitulo($this->_instituicao . ' i-Educar - Unifica&ccedil;&atilde;o de alunos');
+        $this->processoAp = '999847';
+    }
 }
-
 
 class indice extends clsCadastro
 {
-  var $pessoa_logada;
+    public $pessoa_logada;
+    public $tabela_alunos = [];
+    public $aluno_duplicado;
 
-  var $tabela_alunos = array();
-  var $aluno_duplicado;
+    public function Inicializar()
+    {
+        $retorno = 'Novo';
 
-  function Inicializar()
-  {
-    $retorno = 'Novo';
+        $obj_permissoes = new clsPermissoes();
+        $obj_permissoes->permissao_cadastra(
+            999847,
+            $this->pessoa_logada,
+            7,
+            'index.php'
+        );
 
-    $obj_permissoes = new clsPermissoes();
-    $obj_permissoes->permissao_cadastra(999847, $this->pessoa_logada, 7,
-      'index.php');
+        $this->breadcrumb('Cadastrar unificação', [
+            url('intranet/educar_index.php') => 'Escola',
+        ]);
 
-    $this->breadcrumb('Cadastrar unificação', [
-        url('intranet/educar_index.php') => 'Escola',
-    ]);
+        $this->url_cancelar = route('student-log-unification.index');
+        $this->nome_url_cancelar = 'Cancelar';
 
-    $this->url_cancelar = route('student-log-unification.index');
-    $this->nome_url_cancelar = "Cancelar";
-
-    return $retorno;
-  }
-
-  function Gerar()
-  {
-
-      $this->inputsHelper()->dynamic('ano', array('required' => false, 'max_length' => 4));
-      $this->inputsHelper()->dynamic('instituicao',  array('required' =>  false, 'show-select' => true));
-      $this->inputsHelper()->dynamic('escola',  array('required' =>  false, 'show-select' => true, 'value' => 0));
-      $this->inputsHelper()->simpleSearchAluno(null,array('label' => 'Aluno principal' ));
-      $this->campoTabelaInicio("tabela_alunos","",array("Aluno duplicado"),$this->tabela_alunos);
-      $this->campoTexto( "aluno_duplicado", "Aluno duplicado", $this->aluno_duplicado, 50, 255, false, true, false, '', '', '', 'onfocus' );
-      $this->campoTabelaFim();
-
-  }
-
-  function Novo()
-  {
-    $obj_permissoes = new clsPermissoes();
-    $obj_permissoes->permissao_cadastra(999847, $this->pessoa_logada, 7,
-      'index.php');
-
-    $cod_aluno_principal = (int) $this->aluno_id;
-
-    if (!$cod_aluno_principal) return;
-
-
-    $cod_alunos = array();
-
-    //Monta um array com o código dos alunos selecionados na tabela
-    foreach ($this->aluno_duplicado as $key => $value) {
-      $explode = explode(" ", $value);
-
-      if($explode[0] == $cod_aluno_principal){
-        $this->mensagem = 'Impossivel de unificar alunos iguais.<br />';
-        return false;
-      }
-
-      $cod_alunos[] = (int) $explode[0];
+        return $retorno;
     }
 
-    if (!count($cod_alunos)) {
-       $this->mensagem = 'Informe no mínimo um aluno para unificação.<br />';
-        return false;
+    public function Gerar()
+    {
+        $this->inputsHelper()->dynamic('ano', ['required' => false, 'max_length' => 4]);
+        $this->inputsHelper()->dynamic('instituicao', ['required' =>  false, 'show-select' => true]);
+        $this->inputsHelper()->dynamic('escola', ['required' =>  false, 'show-select' => true, 'value' => 0]);
+        $this->inputsHelper()->simpleSearchAluno(null, ['label' => 'Aluno principal' ]);
+        $this->campoTabelaInicio('tabela_alunos', '', ['Aluno duplicado'], $this->tabela_alunos);
+        $this->campoTexto('aluno_duplicado', 'Aluno duplicado', $this->aluno_duplicado, 50, 255, false, true, false, '', '', '', 'onfocus');
+        $this->campoTabelaFim();
     }
 
-    DB::beginTransaction();
-    $unificationId = $this->createLog($cod_aluno_principal, $cod_alunos, $this->pessoa_logada);
-    App_Unificacao_Aluno::unifica($cod_aluno_principal, $cod_alunos, $this->pessoa_logada, new clsBanco(), $unificationId);
+    public function Novo()
+    {
+        $obj_permissoes = new clsPermissoes();
+        $obj_permissoes->permissao_cadastra(
+            999847,
+            $this->pessoa_logada,
+            7,
+            'index.php'
+        );
 
-    try {
-        DB::commit();
-    } catch (Throwable $throable) {
-        DB::rollBack();
-        $this->mensagem = 'Não foi possível realizar a unificação';
-        return false;
+        $cod_aluno_principal = (int) $this->aluno_id;
+
+        if (!$cod_aluno_principal) {
+            return;
+        }
+
+        $cod_alunos = [];
+
+        //Monta um array com o código dos alunos selecionados na tabela
+        foreach ($this->aluno_duplicado as $key => $value) {
+            $explode = explode(' ', $value);
+
+            if ($explode[0] == $cod_aluno_principal) {
+                $this->mensagem = 'Impossivel de unificar alunos iguais.<br />';
+
+                return false;
+            }
+
+            $cod_alunos[] = (int) $explode[0];
+        }
+
+        if (!count($cod_alunos)) {
+            $this->mensagem = 'Informe no mínimo um aluno para unificação.<br />';
+
+            return false;
+        }
+
+        DB::beginTransaction();
+        $unificationId = $this->createLog($cod_aluno_principal, $cod_alunos, $this->pessoa_logada);
+        App_Unificacao_Aluno::unifica($cod_aluno_principal, $cod_alunos, $this->pessoa_logada, new clsBanco(), $unificationId);
+
+        try {
+            DB::commit();
+        } catch (Throwable $throable) {
+            DB::rollBack();
+            $this->mensagem = 'Não foi possível realizar a unificação';
+
+            return false;
+        }
+
+        $this->mensagem = '<span>Alunos unificados com sucesso.</span>';
+        $this->simpleRedirect(route('student-log-unification.index'));
     }
 
-    $this->mensagem = "<span>Alunos unificados com sucesso.</span>";
-    $this->simpleRedirect(route('student-log-unification.index'));
-  }
+    private function createLog($mainId, $duplicatesId, $createdBy)
+    {
+        $log = new LogUnification();
+        $log->type = StudentLogUnification::getType();
+        $log->main_id = $mainId;
+        $log->duplicates_id = json_encode($duplicatesId);
+        $log->created_by = $createdBy;
+        $log->updated_by = $createdBy;
+        $log->save();
 
-  private function createLog($mainId, $duplicatesId, $createdBy)
-  {
-    $log = new LogUnification();
-    $log->type = StudentLogUnification::getType();
-    $log->main_id = $mainId;
-    $log->duplicates_id = json_encode($duplicatesId);
-    $log->created_by = $createdBy;
-    $log->updated_by = $createdBy;
-    $log->save();
-    return $log->id;
-  }
+        return $log->id;
+    }
 }
 
 // Instancia objeto de página
@@ -158,44 +134,44 @@ $pagina->MakeAll();
 ?>
 <script type="text/javascript">
 
-  var handleSelect = function(event, ui){
-    $j(event.target).val(ui.item.label);
-    return false;
-  };
+    var handleSelect = function(event, ui){
+        $j(event.target).val(ui.item.label);
+        return false;
+    };
 
-  var search = function(request, response) {
-    var searchPath = '/module/Api/Aluno?oper=get&resource=aluno-search';
-    var params     = { query : request.term };
+    var search = function(request, response) {
+        var searchPath = '/module/Api/Aluno?oper=get&resource=aluno-search';
+        var params     = { query : request.term };
 
-    $j.get(searchPath, params, function(dataResponse) {
-      simpleSearch.handleSearch(dataResponse, response);
-    });
-  };
+        $j.get(searchPath, params, function(dataResponse) {
+            simpleSearch.handleSearch(dataResponse, response);
+        });
+    };
 
-  function setAutoComplete() {
-    $j.each($j('input[id^="aluno_duplicado"]'), function(index, field) {
+    function setAutoComplete() {
+        $j.each($j('input[id^="aluno_duplicado"]'), function(index, field) {
 
-      $j(field).autocomplete({
-        source    : search,
-        select    : handleSelect,
-        minLength : 1,
-        autoFocus : true
-      });
+            $j(field).autocomplete({
+                source    : search,
+                select    : handleSelect,
+                minLength : 1,
+                autoFocus : true
+            });
 
-    });
-  }
+        });
+    }
 
-  setAutoComplete();
-
-  // bind events
-
-  var $addPontosButton = $j('#btn_add_tab_add_1');
-
-  $addPontosButton.click(function(){
     setAutoComplete();
-  });
 
-$j('#btn_enviar').val('Unificar');
+    // bind events
+
+    var $addPontosButton = $j('#btn_add_tab_add_1');
+
+    $addPontosButton.click(function(){
+        setAutoComplete();
+    });
+
+    $j('#btn_enviar').val('Unificar');
 
 
 </script>
--- ieducar/intranet/educar_unifica_pessoa.php
+++ ieducar/intranet/educar_unifica_pessoa.php
@@ -3,127 +3,139 @@
 use App\Models\Individual;
 use App\Models\LogUnification;
 use iEducar\Modules\Unification\PersonLogUnification;
-use Illuminate\Support\Facades\DB;
-
-
 
 class clsIndexBase extends clsBase
 {
-  function Formular()
-  {
-    $this->SetTitulo($this->_instituicao . ' i-Educar - Unificação de pessoas');
-    $this->processoAp = "9998878";
-  }
+    public function Formular()
+    {
+        $this->SetTitulo($this->_instituicao . ' i-Educar - Unificação de pessoas');
+        $this->processoAp = '9998878';
+    }
 }
 
 class indice extends clsCadastro
 {
-  var $pessoa_logada;
+    public $pessoa_logada;
 
-  var $tabela_pessoas = array();
-  var $pessoa_duplicada;
+    public $tabela_pessoas = [];
+    public $pessoa_duplicada;
 
-  function Formular()
-  {
-    $this->breadcrumb('Unificação de pessoas', [
-        url('intranet/educar_index.php') => 'Escola',
-    ]);
-  }
-
-  function Inicializar()
-  {
-    $retorno = 'Novo';
-
-    $obj_permissoes = new clsPermissoes();
-    $obj_permissoes->permissao_cadastra(9998878, $this->pessoa_logada, 7,
-      'index.php');
-
-    return $retorno;
-  }
-
-  function Gerar()
-  {
-      $this->acao_enviar = 'showConfirmationMessage()';
-      $this->inputsHelper()->dynamic('ano', array('required' => false, 'max_length' => 4));
-      $this->inputsHelper()->simpleSearchPessoa(null,array('label' => 'Pessoa principal' ));
-      $this->campoTabelaInicio("tabela_pessoas","",array("Pessoa duplicada"),$this->tabela_pessoas);
-      $this->campoTexto( "pessoa_duplicada", "Pessoa duplicada", $this->pessoa_duplicada, 50, 255, false, true, false, '', '', '', 'onfocus' );
-      $this->campoTabelaFim();
-  }
-
-  function Novo()
-  {
-    $obj_permissoes = new clsPermissoes();
-    $obj_permissoes->permissao_cadastra(9998878, $this->pessoa_logada, 7,
-      'index.php');
-
-    $codPessoaPrincipal = (int) $this->pessoa_id;
-
-    if (!$codPessoaPrincipal) return;
-
-
-    $codPessoas = array();
-
-    //Monta um array com o código dos pessoas selecionados na tabela
-    foreach ($this->pessoa_duplicada as $key => $value) {
-      $explode = explode(" ", $value);
-
-      if($explode[0] == $codPessoaPrincipal){
-        $this->mensagem = 'Impossivel de unificar pessoas iguais.<br />';
-        return false;
-      }
-
-      $codPessoas[] = (int) $explode[0];
+    public function Formular()
+    {
+        $this->breadcrumb('Unificação de pessoas', [
+            url('intranet/educar_index.php') => 'Escola',
+        ]);
     }
 
-    if (!count($codPessoas)) {
-       $this->mensagem = 'Informe no mínimo um pessoa para unificação.<br />';
-        return false;
+    public function Inicializar()
+    {
+        $retorno = 'Novo';
+
+        $obj_permissoes = new clsPermissoes();
+        $obj_permissoes->permissao_cadastra(
+            9998878,
+            $this->pessoa_logada,
+            7,
+            'index.php'
+        );
+
+        return $retorno;
     }
 
-    $unificationId = $this->createLog($codPessoaPrincipal, $codPessoas, $this->pessoa_logada);
-    $unificador = new App_Unificacao_Pessoa($codPessoaPrincipal, $codPessoas, $this->pessoa_logada, new clsBanco(), $unificationId);
-
-    try {
-        $unificador->unifica();
-    } catch (CoreExt_Exception $exception) {
-        $this->mensagem = $exception->getMessage();
-        return FALSE;
+    public function Gerar()
+    {
+        $this->acao_enviar = 'showConfirmationMessage()';
+        $this->inputsHelper()->dynamic('ano', ['required' => false, 'max_length' => 4]);
+        $this->inputsHelper()->simpleSearchPessoa(null, ['label' => 'Pessoa principal' ]);
+        $this->campoTabelaInicio('tabela_pessoas', '', ['Pessoa duplicada'], $this->tabela_pessoas);
+        $this->campoTexto('pessoa_duplicada', 'Pessoa duplicada', $this->pessoa_duplicada, 50, 255, false, true, false, '', '', '', 'onfocus');
+        $this->campoTabelaFim();
     }
 
-    $this->mensagem = "<span>Pessoas unificadas com sucesso.</span>";
-    return true;
-  }
+    public function Novo()
+    {
+        $obj_permissoes = new clsPermissoes();
+        $obj_permissoes->permissao_cadastra(
+            9998878,
+            $this->pessoa_logada,
+            7,
+            'index.php'
+        );
 
-  private function createLog($mainId, $duplicatesId, $createdBy)
-  {
-    $log = new LogUnification();
-    $log->type = PersonLogUnification::getType();
-    $log->main_id = $mainId;
-    $log->duplicates_id = json_encode($duplicatesId);
-    $log->created_by = $createdBy;
-    $log->updated_by = $createdBy;
-    $log->duplicates_name = json_encode($this->getNamesOfUnifiedPeople($duplicatesId));
-    $log->save();
-    return $log->id;
-  }
+        $codPessoaPrincipal = (int) $this->pessoa_id;
 
-  /**
-   * Retorna os nomes das pessoas unificadas
-   *
-   * @param integer[] $duplicatesId
-   * @return string[]
-  */
-  private function getNamesOfUnifiedPeople($duplicatesId)
-  {
-      $names = [];
+        if (!$codPessoaPrincipal) {
+            return;
+        }
 
-      foreach ($duplicatesId as $personId){
-          $names[] = Individual::findOrFail($personId)->real_name;
-      }
+        $codPessoas = [];
 
-      return $names;
-  }
+        //Monta um array com o código dos pessoas selecionados na tabela
+        foreach ($this->pessoa_duplicada as $key => $value) {
+            $explode = explode(' ', $value);
+
+            if ($explode[0] == $codPessoaPrincipal) {
+                $this->mensagem = 'Impossivel de unificar pessoas iguais.<br />';
+
+                return false;
+            }
+
+            $codPessoas[] = (int) $explode[0];
+        }
+
+        if (!count($codPessoas)) {
+            $this->mensagem = 'Informe no mínimo um pessoa para unificação.<br />';
+
+            return false;
+        }
+
+        $unificationId = $this->createLog($codPessoaPrincipal, $codPessoas, $this->pessoa_logada);
+        $unificador = new App_Unificacao_Pessoa($codPessoaPrincipal, $codPessoas, $this->pessoa_logada, new clsBanco(), $unificationId);
+
+        try {
+            $unificador->unifica();
+        } catch (CoreExt_Exception $exception) {
+            $this->mensagem = $exception->getMessage();
+
+            return false;
+        }
+
+        $this->mensagem = '<span>Pessoas unificadas com sucesso.</span>';
+
+        return true;
+    }
+
+    private function createLog($mainId, $duplicatesId, $createdBy)
+    {
+        $log = new LogUnification();
+        $log->type = PersonLogUnification::getType();
+        $log->main_id = $mainId;
+        $log->duplicates_id = json_encode($duplicatesId);
+        $log->created_by = $createdBy;
+        $log->updated_by = $createdBy;
+        $log->duplicates_name = json_encode($this->getNamesOfUnifiedPeople($duplicatesId));
+        $log->save();
+
+        return $log->id;
+    }
+
+    /**
+     * Retorna os nomes das pessoas unificadas
+     *
+     * @param integer[] $duplicatesId
+     *
+     * @return string[]
+     */
+    private function getNamesOfUnifiedPeople($duplicatesId)
+    {
+        $names = [];
+
+        foreach ($duplicatesId as $personId) {
+            $names[] = Individual::findOrFail($personId)->real_name;
+        }
+
+        return $names;
+    }
 }
 
 // Instancia objeto de página
@@ -140,86 +152,86 @@ $pagina->MakeAll();
 ?>
 <script type="text/javascript">
 
-  var handleSelect = function(event, ui){
-    $j(event.target).val(ui.item.label);
-    return false;
-  };
+    var handleSelect = function(event, ui){
+        $j(event.target).val(ui.item.label);
+        return false;
+    };
 
-  var search = function(request, response) {
-    var searchPath = '/module/Api/Pessoa?oper=get&resource=pessoa-search';
-    var params     = { query : request.term };
+    var search = function(request, response) {
+        var searchPath = '/module/Api/Pessoa?oper=get&resource=pessoa-search';
+        var params     = { query : request.term };
 
-    $j.get(searchPath, params, function(dataResponse) {
-      simpleSearch.handleSearch(dataResponse, response);
-    });
-  };
+        $j.get(searchPath, params, function(dataResponse) {
+            simpleSearch.handleSearch(dataResponse, response);
+        });
+    };
 
-  function setAutoComplete() {
-    $j.each($j('input[id^="pessoa_duplicada"]'), function(index, field) {
+    function setAutoComplete() {
+        $j.each($j('input[id^="pessoa_duplicada"]'), function(index, field) {
 
-      $j(field).autocomplete({
-        source    : search,
-        select    : handleSelect,
-        minLength : 1,
-        autoFocus : true
-      });
+            $j(field).autocomplete({
+                source    : search,
+                select    : handleSelect,
+                minLength : 1,
+                autoFocus : true
+            });
 
-    });
-  }
+        });
+    }
 
-  setAutoComplete();
-
-  // bind events
-
-  var $addPontosButton = $j('#btn_add_tab_add_1');
-
-  $addPontosButton.click(function(){
     setAutoComplete();
-  });
 
-$j('#btn_enviar').val('Unificar');
+    // bind events
 
-  function showConfirmationMessage() {
-      makeDialog({
-          content: 'O processo de unificação de pessoas não poderá ser desfeito. Deseja continuar?',
-          title: 'Atenção!',
-          maxWidth: 860,
-          width: 860,
-          close: function () {
-              $j('#dialog-container').dialog('destroy');
-          },
-          buttons: [{
-              text: 'Confirmar',
-              click: function () {
-                  acao();
-                  $j('#dialog-container').dialog('destroy');
-              }
-          }, {
-              text: 'Cancelar',
-              click: function () {
-                  $j('#dialog-container').dialog('destroy');
-              }
-          }]
-      });
-  }
+    var $addPontosButton = $j('#btn_add_tab_add_1');
 
-  function makeDialog(params) {
-      var container = $j('#dialog-container');
+    $addPontosButton.click(function(){
+        setAutoComplete();
+    });
 
-      if (container.length < 1) {
-          $j('body').append('<div id="dialog-container" style="width: 500px;"></div>');
-          container = $j('#dialog-container');
-      }
+    $j('#btn_enviar').val('Unificar');
 
-      if (container.hasClass('ui-dialog-content')) {
-          container.dialog('destroy');
-      }
+    function showConfirmationMessage() {
+        makeDialog({
+            content: 'O processo de unificação de pessoas não poderá ser desfeito. Deseja continuar?',
+            title: 'Atenção!',
+            maxWidth: 860,
+            width: 860,
+            close: function () {
+                $j('#dialog-container').dialog('destroy');
+            },
+            buttons: [{
+                text: 'Confirmar',
+                click: function () {
+                    acao();
+                    $j('#dialog-container').dialog('destroy');
+                }
+            }, {
+                text: 'Cancelar',
+                click: function () {
+                    $j('#dialog-container').dialog('destroy');
+                }
+            }]
+        });
+    }
 
-      container.empty();
-      container.html(params.content);
+    function makeDialog(params) {
+        var container = $j('#dialog-container');
 
-      delete params['content'];
+        if (container.length < 1) {
+            $j('body').append('<div id="dialog-container" style="width: 500px;"></div>');
+            container = $j('#dialog-container');
+        }
 
-      container.dialog(params);
-  }
+        if (container.hasClass('ui-dialog-content')) {
+            container.dialog('destroy');
+        }
+
+        container.empty();
+        container.html(params.content);
+
+        delete params['content'];
+
+        container.dialog(params);
+    }
 </script>
