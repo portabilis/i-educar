e70d96324cf7560afbdbad73e4c8d7a9ee1e56c5
jrbarros <joalissonricardo@gmail.com>
2021-02-24 22:45:57 -0300

    Fix seção portal acesso

--- ieducar/intranet/portal_acesso_cad.php
+++ ieducar/intranet/portal_acesso_cad.php
@@ -1,12 +1,11 @@
 <?php
 
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} Acesso" );
-        $this->processoAp = "666";
+        $this->SetTitulo("{$this->_instituicao} Acesso");
+        $this->processoAp = '666';
     }
 }
 
@@ -17,123 +16,103 @@ class indice extends clsCadastro
      *
      * @var int
      */
-    var $pessoa_logada;
+    public $pessoa_logada;
 
-    var $cod_acesso;
-    var $data_hora;
-    var $ip_externo;
-    var $ip_interno;
-    var $cod_pessoa;
-    var $obs;
-    var $sucesso;
+    public $cod_acesso;
+    public $data_hora;
+    public $ip_externo;
+    public $ip_interno;
+    public $cod_pessoa;
+    public $obs;
+    public $sucesso;
 
-    function Inicializar()
+    public function Inicializar()
     {
-        $retorno = "Novo";
+        $retorno = 'Novo';
 
+        $this->cod_acesso=$_GET['cod_acesso'];
 
-        $this->cod_acesso=$_GET["cod_acesso"];
-
-
-        if( is_numeric( $this->cod_acesso ) )
-        {
-
-            $obj = new clsPortalAcesso( $this->cod_acesso );
+        if (is_numeric($this->cod_acesso)) {
+            $obj = new clsPortalAcesso($this->cod_acesso);
             $registro  = $obj->detalhe();
-            if( $registro )
-            {
-                foreach( $registro AS $campo => $val )  // passa todos os valores obtidos no registro para atributos do objeto
+            if ($registro) {
+                foreach ($registro as $campo => $val) {  // passa todos os valores obtidos no registro para atributos do objeto
                     $this->$campo = $val;
-                $this->data_hora = dataFromPgToBr( $this->data_hora );
-
+                }
+                $this->data_hora = dataFromPgToBr($this->data_hora);
 
                 $this->fexcluir = true;
 
-                $retorno = "Editar";
+                $retorno = 'Editar';
             }
         }
-        $this->url_cancelar = ($retorno == "Editar") ? "portal_acesso_det.php?cod_acesso={$registro["cod_acesso"]}" : "portal_acesso_lst.php";
-        $this->nome_url_cancelar = "Cancelar";
+        $this->url_cancelar = ($retorno == 'Editar') ? "portal_acesso_det.php?cod_acesso={$registro['cod_acesso']}" : 'portal_acesso_lst.php';
+        $this->nome_url_cancelar = 'Cancelar';
+
         return $retorno;
     }
 
-    function Gerar()
+    public function Gerar()
     {
         // primary keys
-        $this->campoOculto( "cod_acesso", $this->cod_acesso );
+        $this->campoOculto('cod_acesso', $this->cod_acesso);
 
         // foreign keys
 
         // text
-        $this->campoTexto( "ip_externo", "Ip Externo", $this->ip_externo, 30, 255, true );
-        $this->campoTexto( "ip_interno", "Ip Interno", $this->ip_interno, 30, 255, true );
-        $this->campoNumero( "cod_pessoa", "Pessoa", $this->cod_pessoa, 15, 255, true );
-        $this->campoMemo( "obs", "Obs", $this->obs, 60, 10, false );
+        $this->campoTexto('ip_externo', 'Ip Externo', $this->ip_externo, 30, 255, true);
+        $this->campoTexto('ip_interno', 'Ip Interno', $this->ip_interno, 30, 255, true);
+        $this->campoNumero('cod_pessoa', 'Pessoa', $this->cod_pessoa, 15, 255, true);
+        $this->campoMemo('obs', 'Obs', $this->obs, 60, 10, false);
 
         // data
-        $this->campoData( "data_hora", "Data Hora", $this->data_hora, true );
+        $this->campoData('data_hora', 'Data Hora', $this->data_hora, true);
 
         // time
 
         // bool
-        $this->campoBoolLista( "sucesso", "Sucesso", $this->sucesso );
+        $this->campoBoolLista('sucesso', 'Sucesso', $this->sucesso);
         //$this->campoCheck( "sucesso", "Sucesso", ( $this->sucesso == 't' ) );
-
     }
 
-    function Novo()
+    public function Novo()
     {
-
-
-
-
-        $obj = new clsPortalAcesso( $this->cod_acesso, $this->data_hora, $this->ip_externo, $this->ip_interno, $this->cod_pessoa, $this->obs, $this->sucesso );
+        $obj = new clsPortalAcesso($this->cod_acesso, $this->data_hora, $this->ip_externo, $this->ip_interno, $this->cod_pessoa, $this->obs, $this->sucesso);
         $cadastrou = $obj->cadastra();
-        if( $cadastrou )
-        {
-            $this->mensagem .= "Cadastro efetuado com sucesso.<br>";
+        if ($cadastrou) {
+            $this->mensagem .= 'Cadastro efetuado com sucesso.<br>';
             $this->simpleRedirect('portal_acesso_lst.php');
         }
 
-        $this->mensagem = "Cadastro n&atilde;o realizado.<br>";
+        $this->mensagem = 'Cadastro n&atilde;o realizado.<br>';
 
         return false;
     }
 
-    function Editar()
+    public function Editar()
     {
-
-
-
-
         $obj = new clsPortalAcesso($this->cod_acesso, $this->data_hora, $this->ip_externo, $this->ip_interno, $this->cod_pessoa, $this->obs, $this->sucesso);
         $editou = $obj->edita();
-        if( $editou )
-        {
-            $this->mensagem .= "Edi&ccedil;&atilde;o efetuada com sucesso.<br>";
+        if ($editou) {
+            $this->mensagem .= 'Edi&ccedil;&atilde;o efetuada com sucesso.<br>';
             $this->simpleRedirect('portal_acesso_lst.php');
         }
 
-        $this->mensagem = "Edi&ccedil;&atilde;o n&atilde;o realizada.<br>";
+        $this->mensagem = 'Edi&ccedil;&atilde;o n&atilde;o realizada.<br>';
 
         return false;
     }
 
-    function Excluir()
+    public function Excluir()
     {
-
-
-
-
         $obj = new clsPortalAcesso($this->cod_acesso, $this->data_hora, $this->ip_externo, $this->ip_interno, $this->cod_pessoa, $this->obs, $this->sucesso);
         $excluiu = $obj->excluir();
-        if( $excluiu )
-        {
-            $this->mensagem .= "Exclus&atilde;o efetuada com sucesso.<br>";
+        if ($excluiu) {
+            $this->mensagem .= 'Exclus&atilde;o efetuada com sucesso.<br>';
             $this->simpleRedirect('portal_acesso_lst.php');
         }
 
-        $this->mensagem = "Exclus&atilde;o n&atilde;o realizada.<br>";
+        $this->mensagem = 'Exclus&atilde;o n&atilde;o realizada.<br>';
 
         return false;
     }
@@ -144,7 +123,6 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
-?>
