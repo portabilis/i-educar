e7b33498ec54d612cc0ac41abc62a6d3b4e11573
jrbarros <joalissonricardo@gmail.com>
2021-02-24 22:42:43 -0300

    Fix seção tipo dispensa

--- ieducar/intranet/educar_tipo_dispensa_cad.php
+++ ieducar/intranet/educar_tipo_dispensa_cad.php
@@ -1,12 +1,11 @@
 <?php
 
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} i-Educar - Tipo Dispensa" );
-        $this->processoAp = "577";
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Tipo Dispensa");
+        $this->processoAp = '577';
     }
 }
 
@@ -17,112 +16,106 @@ class indice extends clsCadastro
      *
      * @var int
      */
-    var $pessoa_logada;
+    public $pessoa_logada;
 
-    var $cod_tipo_dispensa;
-    var $ref_usuario_exc;
-    var $ref_usuario_cad;
-    var $nm_tipo;
-    var $descricao;
-    var $data_cadastro;
-    var $data_exclusao;
-    var $ativo;
-    var $ref_cod_instituicao;
+    public $cod_tipo_dispensa;
+    public $ref_usuario_exc;
+    public $ref_usuario_cad;
+    public $nm_tipo;
+    public $descricao;
+    public $data_cadastro;
+    public $data_exclusao;
+    public $ativo;
+    public $ref_cod_instituicao;
 
-    function Inicializar()
+    public function Inicializar()
     {
-        $retorno = "Novo";
+        $retorno = 'Novo';
 
-
-        $this->cod_tipo_dispensa=$_GET["cod_tipo_dispensa"];
+        $this->cod_tipo_dispensa=$_GET['cod_tipo_dispensa'];
 
         $obj_permissoes = new clsPermissoes();
-        $obj_permissoes->permissao_cadastra( 577, $this->pessoa_logada, 7, "educar_tipo_dispensa_lst.php" );
+        $obj_permissoes->permissao_cadastra(577, $this->pessoa_logada, 7, 'educar_tipo_dispensa_lst.php');
 
-        if( is_numeric( $this->cod_tipo_dispensa ) )
-        {
-
-            $obj = new clsPmieducarTipoDispensa( $this->cod_tipo_dispensa );
+        if (is_numeric($this->cod_tipo_dispensa)) {
+            $obj = new clsPmieducarTipoDispensa($this->cod_tipo_dispensa);
             $registro  = $obj->detalhe();
-            if( $registro )
-            {
-                foreach( $registro AS $campo => $val )  // passa todos os valores obtidos no registro para atributos do objeto
+            if ($registro) {
+                foreach ($registro as $campo => $val) {  // passa todos os valores obtidos no registro para atributos do objeto
                     $this->$campo = $val;
+                }
 
-                $this->ref_cod_instituicao = $det_ref_cod_escola["ref_cod_instituicao"];
+                $this->ref_cod_instituicao = $det_ref_cod_escola['ref_cod_instituicao'];
 
-                $this->fexcluir = $obj_permissoes->permissao_excluir( 577, $this->pessoa_logada,7 );
-                $retorno = "Editar";
+                $this->fexcluir = $obj_permissoes->permissao_excluir(577, $this->pessoa_logada, 7);
+                $retorno = 'Editar';
             }
         }
-        $this->url_cancelar = ($retorno == "Editar") ? "educar_tipo_dispensa_det.php?cod_tipo_dispensa={$registro["cod_tipo_dispensa"]}" : "educar_tipo_dispensa_lst.php";
+        $this->url_cancelar = ($retorno == 'Editar') ? "educar_tipo_dispensa_det.php?cod_tipo_dispensa={$registro['cod_tipo_dispensa']}" : 'educar_tipo_dispensa_lst.php';
 
-        $nomeMenu = $retorno == "Editar" ? $retorno : "Cadastrar";
+        $nomeMenu = $retorno == 'Editar' ? $retorno : 'Cadastrar';
 
         $this->breadcrumb($nomeMenu . ' tipo de dispensa', [
             url('intranet/educar_index.php') => 'Escola',
         ]);
 
-        $this->nome_url_cancelar = "Cancelar";
+        $this->nome_url_cancelar = 'Cancelar';
 
         return $retorno;
     }
 
-    function Gerar()
+    public function Gerar()
     {
         // primary keys
-        $this->campoOculto( "cod_tipo_dispensa", $this->cod_tipo_dispensa );
+        $this->campoOculto('cod_tipo_dispensa', $this->cod_tipo_dispensa);
 
         // foreign keys
         $obrigatorio = true;
-        include("include/pmieducar/educar_campo_lista.php");
+        include('include/pmieducar/educar_campo_lista.php');
 
         // text
-        $this->campoTexto( "nm_tipo", "Tipo Dispensa", $this->nm_tipo, 30, 255, true );
-        $this->campoMemo( "descricao", "Descri&ccedil;&atilde;o", $this->descricao, 60, 5, false );
+        $this->campoTexto('nm_tipo', 'Tipo Dispensa', $this->nm_tipo, 30, 255, true);
+        $this->campoMemo('descricao', 'Descri&ccedil;&atilde;o', $this->descricao, 60, 5, false);
     }
 
-    function Novo()
+    public function Novo()
     {
-        $obj = new clsPmieducarTipoDispensa( null, null, $this->pessoa_logada, $this->nm_tipo, $this->descricao, null, null, 1, $this->ref_cod_instituicao );
+        $obj = new clsPmieducarTipoDispensa(null, null, $this->pessoa_logada, $this->nm_tipo, $this->descricao, null, null, 1, $this->ref_cod_instituicao);
         $cadastrou = $obj->cadastra();
-        if( $cadastrou )
-        {
-            $this->mensagem .= "Cadastro efetuado com sucesso.<br>";
+        if ($cadastrou) {
+            $this->mensagem .= 'Cadastro efetuado com sucesso.<br>';
             $this->simpleRedirect('educar_tipo_dispensa_lst.php');
         }
 
-        $this->mensagem = "Cadastro n&atilde;o realizado.<br>";
+        $this->mensagem = 'Cadastro n&atilde;o realizado.<br>';
 
         return false;
     }
 
-    function Editar()
+    public function Editar()
     {
-        $obj = new clsPmieducarTipoDispensa( $this->cod_tipo_dispensa, $this->pessoa_logada, null, $this->nm_tipo, $this->descricao, null, null, 1, $this->ref_cod_instituicao );
+        $obj = new clsPmieducarTipoDispensa($this->cod_tipo_dispensa, $this->pessoa_logada, null, $this->nm_tipo, $this->descricao, null, null, 1, $this->ref_cod_instituicao);
         $editou = $obj->edita();
-        if( $editou )
-        {
-            $this->mensagem .= "Edi&ccedil;&atilde;o efetuada com sucesso.<br>";
+        if ($editou) {
+            $this->mensagem .= 'Edi&ccedil;&atilde;o efetuada com sucesso.<br>';
             $this->simpleRedirect('educar_tipo_dispensa_lst.php');
         }
 
-        $this->mensagem = "Edi&ccedil;&atilde;o n&atilde;o realizada.<br>";
+        $this->mensagem = 'Edi&ccedil;&atilde;o n&atilde;o realizada.<br>';
 
         return false;
     }
 
-    function Excluir()
+    public function Excluir()
     {
-        $obj = new clsPmieducarTipoDispensa( $this->cod_tipo_dispensa, $this->pessoa_logada, null, null, null, null, null, 0 );
+        $obj = new clsPmieducarTipoDispensa($this->cod_tipo_dispensa, $this->pessoa_logada, null, null, null, null, null, 0);
         $excluiu = $obj->excluir();
-        if( $excluiu )
-        {
-            $this->mensagem .= "Exclus&atilde;o efetuada com sucesso.<br>";
+        if ($excluiu) {
+            $this->mensagem .= 'Exclus&atilde;o efetuada com sucesso.<br>';
             $this->simpleRedirect('educar_tipo_dispensa_lst.php');
         }
 
-        $this->mensagem = "Exclus&atilde;o n&atilde;o realizada.<br>";
+        $this->mensagem = 'Exclus&atilde;o n&atilde;o realizada.<br>';
 
         return false;
     }
@@ -133,7 +126,6 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
-?>
