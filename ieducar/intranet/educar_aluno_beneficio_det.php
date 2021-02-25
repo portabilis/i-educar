1b937d43a4cc359977958693e5fa6292d44d5222
jrbarros <joalissonricardo@gmail.com>
2021-02-24 22:24:18 -0300

    Fix seção de aluno benefício

--- ieducar/intranet/educar_aluno_beneficio_det.php
+++ ieducar/intranet/educar_aluno_beneficio_det.php
@@ -1,13 +1,11 @@
 <?php
 
-
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} i-Educar - Benef&iacute;cio Aluno" );
-        $this->processoAp = "581";
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Benef&iacute;cio Aluno");
+        $this->processoAp = '581';
     }
 }
 
@@ -18,56 +16,50 @@ class indice extends clsDetalhe
      *
      * @var int
      */
-    var $titulo;
+    public $titulo;
 
-    var $cod_aluno_beneficio;
-    var $ref_usuario_exc;
-    var $ref_usuario_cad;
-    var $nm_beneficio;
-    var $desc_beneficio;
-    var $data_cadastro;
-    var $data_exclusao;
-    var $ativo;
+    public $cod_aluno_beneficio;
+    public $ref_usuario_exc;
+    public $ref_usuario_cad;
+    public $nm_beneficio;
+    public $desc_beneficio;
+    public $data_cadastro;
+    public $data_exclusao;
+    public $ativo;
 
-    function Gerar()
+    public function Gerar()
     {
-        $this->titulo = "Aluno Beneficio - Detalhe";
+        $this->titulo = 'Aluno Beneficio - Detalhe';
 
+        $this->cod_aluno_beneficio=$_GET['cod_aluno_beneficio'];
 
-        $this->cod_aluno_beneficio=$_GET["cod_aluno_beneficio"];
-
-        $tmp_obj = new clsPmieducarAlunoBeneficio( $this->cod_aluno_beneficio );
+        $tmp_obj = new clsPmieducarAlunoBeneficio($this->cod_aluno_beneficio);
         $registro = $tmp_obj->detalhe();
 
-        if( ! $registro )
-        {
+        if (! $registro) {
             $this->simpleRedirect('educar_aluno_beneficio_lst.php');
         }
 
-        if( $registro["cod_aluno_beneficio"] )
-        {
-            $this->addDetalhe( array( "C&oacute;digo Benef&iacute;cio", "{$registro["cod_aluno_beneficio"]}") );
+        if ($registro['cod_aluno_beneficio']) {
+            $this->addDetalhe([ 'C&oacute;digo Benef&iacute;cio', "{$registro['cod_aluno_beneficio']}"]);
         }
-        if( $registro["nm_beneficio"] )
-        {
-            $this->addDetalhe( array( "Benef&iacute;cio", "{$registro["nm_beneficio"]}") );
+        if ($registro['nm_beneficio']) {
+            $this->addDetalhe([ 'Benef&iacute;cio', "{$registro['nm_beneficio']}"]);
         }
-        if( $registro["desc_beneficio"] )
-        {
-            $this->addDetalhe( array( "Descri&ccedil;&atilde;o", nl2br("{$registro["desc_beneficio"]}")) );
+        if ($registro['desc_beneficio']) {
+            $this->addDetalhe([ 'Descri&ccedil;&atilde;o', nl2br("{$registro['desc_beneficio']}")]);
         }
 
         //** Verificacao de permissao para cadastro
         $obj_permissao = new clsPermissoes();
 
-        if($obj_permissao->permissao_cadastra(581, $this->pessoa_logada,3))
-        {
-            $this->url_novo = "educar_aluno_beneficio_cad.php";
-            $this->url_editar = "educar_aluno_beneficio_cad.php?cod_aluno_beneficio={$registro["cod_aluno_beneficio"]}";
+        if ($obj_permissao->permissao_cadastra(581, $this->pessoa_logada, 3)) {
+            $this->url_novo = 'educar_aluno_beneficio_cad.php';
+            $this->url_editar = "educar_aluno_beneficio_cad.php?cod_aluno_beneficio={$registro['cod_aluno_beneficio']}";
         }
         //**
-        $this->url_cancelar = "educar_aluno_beneficio_lst.php";
-        $this->largura = "100%";
+        $this->url_cancelar = 'educar_aluno_beneficio_lst.php';
+        $this->largura = '100%';
 
         $this->breadcrumb('Detalhe do benefício de alunos', [
             url('intranet/educar_index.php') => 'Escola',
@@ -80,7 +72,6 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
-?>
--- ieducar/intranet/educar_aluno_beneficio_lst.php
+++ ieducar/intranet/educar_aluno_beneficio_lst.php
@@ -1,12 +1,11 @@
 <?php
 
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} i-Educar - Benefício do aluno");
-        $this->processoAp = "581";
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Benefício do aluno");
+        $this->processoAp = '581';
     }
 }
 
@@ -17,67 +16,65 @@ class indice extends clsListagem
      *
      * @var int
      */
-    var $pessoa_logada;
+    public $pessoa_logada;
 
     /**
      * Titulo no topo da pagina
      *
      * @var int
      */
-    var $titulo;
+    public $titulo;
 
     /**
      * Quantidade de registros a ser apresentada em cada pagina
      *
      * @var int
      */
-    var $limite;
+    public $limite;
 
     /**
      * Inicio dos registros a serem exibidos (limit)
      *
      * @var int
      */
-    var $offset;
+    public $offset;
 
-    var $cod_aluno_beneficio;
-    var $ref_usuario_exc;
-    var $ref_usuario_cad;
-    var $nm_beneficio;
-    var $desc_beneficio;
-    var $data_cadastro;
-    var $data_exclusao;
-    var $ativo;
+    public $cod_aluno_beneficio;
+    public $ref_usuario_exc;
+    public $ref_usuario_cad;
+    public $nm_beneficio;
+    public $desc_beneficio;
+    public $data_cadastro;
+    public $data_exclusao;
+    public $ativo;
 
-    function Gerar()
+    public function Gerar()
     {
-        $this->titulo = "Benef&iacute;cio Aluno - Listagem";
+        $this->titulo = 'Benef&iacute;cio Aluno - Listagem';
 
-        foreach( $_GET AS $var => $val ) // passa todos os valores obtidos no GET para atributos do objeto
-            $this->$var = ( $val === "" ) ? null: $val;
+        foreach ($_GET as $var => $val) { // passa todos os valores obtidos no GET para atributos do objeto
+            $this->$var = ($val === '') ? null: $val;
+        }
 
-
-
-        $this->addCabecalhos( array(
-            "Beneficio"
-        ) );
+        $this->addCabecalhos([
+            'Beneficio'
+        ]);
 
         // Filtros de Foreign Keys
 
-            //$obrigatorio = true;
-            //include("include/pmieducar/educar_pesquisa_instituicao_escola.php");
+        //$obrigatorio = true;
+        //include("include/pmieducar/educar_pesquisa_instituicao_escola.php");
 
         // outros Filtros
-        $this->campoTexto( "nm_beneficio", "Benef&iacute;cio", $this->nm_beneficio, 30, 255, false );
-
+        $this->campoTexto('nm_beneficio', 'Benef&iacute;cio', $this->nm_beneficio, 30, 255, false);
 
         // Paginador
         $this->limite = 20;
-        $this->offset = ( $_GET["pagina_{$this->nome}"] ) ? $_GET["pagina_{$this->nome}"]*$this->limite-$this->limite: 0;
+        $this->offset = ($_GET["pagina_{$this->nome}"]) ? $_GET["pagina_{$this->nome}"]*$this->limite-$this->limite: 0;
 
         $obj_aluno_beneficio = new clsPmieducarAlunoBeneficio();
-        $obj_aluno_beneficio->setOrderby( "nm_beneficio ASC" );
-        $obj_aluno_beneficio->setLimite( $this->limite, $this->offset );
+        $obj_aluno_beneficio->setOrderby('nm_beneficio ASC');
+        $obj_aluno_beneficio->setLimite($this->limite, $this->offset);
 
         $lista = $obj_aluno_beneficio->lista(
             null,
@@ -93,30 +90,25 @@ class indice extends clsListagem
         $total = $obj_aluno_beneficio->_total;
 
         // monta a lista
-        if( is_array( $lista ) && count( $lista ) )
-        {
-            foreach ( $lista AS $registro )
-            {
-
-                $this->addLinhas( array(
-                    "<a href=\"educar_aluno_beneficio_det.php?cod_aluno_beneficio={$registro["cod_aluno_beneficio"]}\">{$registro["nm_beneficio"]}</a>"
-                ) );
+        if (is_array($lista) && count($lista)) {
+            foreach ($lista as $registro) {
+                $this->addLinhas([
+                    "<a href=\"educar_aluno_beneficio_det.php?cod_aluno_beneficio={$registro['cod_aluno_beneficio']}\">{$registro['nm_beneficio']}</a>"
+                ]);
             }
         }
-        $this->addPaginador2( "educar_aluno_beneficio_lst.php", $total, $_GET, $this->nome, $this->limite );
-
+        $this->addPaginador2('educar_aluno_beneficio_lst.php', $total, $_GET, $this->nome, $this->limite);
 
         //** Verificacao de permissao para cadastro
         $obj_permissao = new clsPermissoes();
 
-        if($obj_permissao->permissao_cadastra(581, $this->pessoa_logada,3))
-        {
-            $this->acao = "go(\"educar_aluno_beneficio_cad.php\")";
-            $this->nome_acao = "Novo";
+        if ($obj_permissao->permissao_cadastra(581, $this->pessoa_logada, 3)) {
+            $this->acao = 'go("educar_aluno_beneficio_cad.php")';
+            $this->nome_acao = 'Novo';
         }
         //**
 
-        $this->largura = "100%";
+        $this->largura = '100%';
 
         $this->breadcrumb('Tipos de benefício do aluno', [
             url('intranet/educar_index.php') => 'Escola',
@@ -128,7 +120,6 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
-?>
