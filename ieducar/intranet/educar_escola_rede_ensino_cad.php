a296a0897004af7c7e19efbc7b510dbaa8301263
jrbarros <joalissonricardo@gmail.com>
2021-02-24 22:32:47 -0300

    Fix seção escola

--- ieducar/intranet/educar_escola_rede_ensino_cad.php
+++ ieducar/intranet/educar_escola_rede_ensino_cad.php
@@ -1,12 +1,11 @@
 <?php
 
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} i-Educar - Escola Rede Ensino" );
-        $this->processoAp = "647";
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Escola Rede Ensino");
+        $this->processoAp = '647';
     }
 }
 
@@ -17,120 +16,113 @@ class indice extends clsCadastro
      *
      * @var int
      */
-    var $pessoa_logada;
+    public $pessoa_logada;
 
-    var $cod_escola_rede_ensino;
-    var $ref_usuario_exc;
-    var $ref_usuario_cad;
-    var $nm_rede;
-    var $data_cadastro;
-    var $data_exclusao;
-    var $ativo;
-    var $ref_cod_instituicao;
+    public $cod_escola_rede_ensino;
+    public $ref_usuario_exc;
+    public $ref_usuario_cad;
+    public $nm_rede;
+    public $data_cadastro;
+    public $data_exclusao;
+    public $ativo;
+    public $ref_cod_instituicao;
 
-    function Inicializar()
+    public function Inicializar()
     {
-        $retorno = "Novo";
+        $retorno = 'Novo';
 
-
-        $this->cod_escola_rede_ensino=$_GET["cod_escola_rede_ensino"];
+        $this->cod_escola_rede_ensino=$_GET['cod_escola_rede_ensino'];
 
         $obj_permissoes = new clsPermissoes();
-        $obj_permissoes->permissao_cadastra( 647, $this->pessoa_logada, 3,  "educar_escola_rede_ensino_lst.php" );
+        $obj_permissoes->permissao_cadastra(647, $this->pessoa_logada, 3, 'educar_escola_rede_ensino_lst.php');
 
-        if( is_numeric( $this->cod_escola_rede_ensino ) )
-        {
-
-            $obj = new clsPmieducarEscolaRedeEnsino( $this->cod_escola_rede_ensino );
+        if (is_numeric($this->cod_escola_rede_ensino)) {
+            $obj = new clsPmieducarEscolaRedeEnsino($this->cod_escola_rede_ensino);
             $registro  = $obj->detalhe();
-            if( $registro )
-            {
-                foreach( $registro AS $campo => $val )  // passa todos os valores obtidos no registro para atributos do objeto
+            if ($registro) {
+                foreach ($registro as $campo => $val) {  // passa todos os valores obtidos no registro para atributos do objeto
                     $this->$campo = $val;
+                }
 
-                if( $obj_permissoes->permissao_excluir( 647, $this->pessoa_logada, 3 ) )
-                {
+                if ($obj_permissoes->permissao_excluir(647, $this->pessoa_logada, 3)) {
                     $this->fexcluir = true;
                 }
-                $retorno = "Editar";
+                $retorno = 'Editar';
             }
         }
-        $this->url_cancelar = ($retorno == "Editar") ? "educar_escola_rede_ensino_det.php?cod_escola_rede_ensino={$registro["cod_escola_rede_ensino"]}" : "educar_escola_rede_ensino_lst.php";
+        $this->url_cancelar = ($retorno == 'Editar') ? "educar_escola_rede_ensino_det.php?cod_escola_rede_ensino={$registro['cod_escola_rede_ensino']}" : 'educar_escola_rede_ensino_lst.php';
 
-        $nomeMenu = $retorno == "Editar" ? $retorno : "Cadastrar";
+        $nomeMenu = $retorno == 'Editar' ? $retorno : 'Cadastrar';
 
         $this->breadcrumb($nomeMenu . ' rede de ensino', [
             url('intranet/educar_index.php') => 'Escola',
         ]);
 
-        $this->nome_url_cancelar = "Cancelar";
+        $this->nome_url_cancelar = 'Cancelar';
 
         return $retorno;
     }
 
-    function Gerar()
+    public function Gerar()
     {
         // primary keys
-        $this->campoOculto( "cod_escola_rede_ensino", $this->cod_escola_rede_ensino );
+        $this->campoOculto('cod_escola_rede_ensino', $this->cod_escola_rede_ensino);
 
         // Filtros de Foreign Keys
         $obrigatorio = true;
-        include("include/pmieducar/educar_campo_lista.php");
+        include('include/pmieducar/educar_campo_lista.php');
 
         // text
-        $this->campoTexto( "nm_rede", "Rede Ensino", $this->nm_rede, 30, 255, true );
+        $this->campoTexto('nm_rede', 'Rede Ensino', $this->nm_rede, 30, 255, true);
     }
 
-    function Novo()
+    public function Novo()
     {
         $obj_permissoes = new clsPermissoes();
-        $obj_permissoes->permissao_cadastra( 647, $this->pessoa_logada, 3,  "educar_escola_rede_ensino_lst.php" );
+        $obj_permissoes->permissao_cadastra(647, $this->pessoa_logada, 3, 'educar_escola_rede_ensino_lst.php');
 
-        $obj = new clsPmieducarEscolaRedeEnsino( null,null,$this->pessoa_logada,$this->nm_rede,null,null,1,$this->ref_cod_instituicao );
+        $obj = new clsPmieducarEscolaRedeEnsino(null, null, $this->pessoa_logada, $this->nm_rede, null, null, 1, $this->ref_cod_instituicao);
         $cadastrou = $obj->cadastra();
-        if( $cadastrou )
-        {
-            $this->mensagem .= "Cadastro efetuado com sucesso.<br>";
+        if ($cadastrou) {
+            $this->mensagem .= 'Cadastro efetuado com sucesso.<br>';
             $this->simpleRedirect('educar_escola_rede_ensino_lst.php');
         }
 
-        $this->mensagem = "Cadastro n&atilde;o realizado.<br>";
+        $this->mensagem = 'Cadastro n&atilde;o realizado.<br>';
 
         return false;
     }
 
-    function Editar()
+    public function Editar()
     {
         $obj_permissoes = new clsPermissoes();
-        $obj_permissoes->permissao_cadastra( 647, $this->pessoa_logada, 3,  "educar_escola_rede_ensino_lst.php" );
+        $obj_permissoes->permissao_cadastra(647, $this->pessoa_logada, 3, 'educar_escola_rede_ensino_lst.php');
 
-        $obj = new clsPmieducarEscolaRedeEnsino( $this->cod_escola_rede_ensino,$this->pessoa_logada,null,$this->nm_rede,null,null,1,$this->ref_cod_instituicao );
+        $obj = new clsPmieducarEscolaRedeEnsino($this->cod_escola_rede_ensino, $this->pessoa_logada, null, $this->nm_rede, null, null, 1, $this->ref_cod_instituicao);
         $editou = $obj->edita();
-        if( $editou )
-        {
-            $this->mensagem .= "Edi&ccedil;&atilde;o efetuada com sucesso.<br>";
+        if ($editou) {
+            $this->mensagem .= 'Edi&ccedil;&atilde;o efetuada com sucesso.<br>';
             $this->simpleRedirect('educar_escola_localizacao_lst.php');
         }
 
-        $this->mensagem = "Edi&ccedil;&atilde;o n&atilde;o realizada.<br>";
+        $this->mensagem = 'Edi&ccedil;&atilde;o n&atilde;o realizada.<br>';
 
         return false;
     }
 
-    function Excluir()
+    public function Excluir()
     {
         $obj_permissoes = new clsPermissoes();
-        $obj_permissoes->permissao_excluir( 647, $this->pessoa_logada, 3,  "educar_escola_rede_ensino_lst.php" );
+        $obj_permissoes->permissao_excluir(647, $this->pessoa_logada, 3, 'educar_escola_rede_ensino_lst.php');
 
-        $obj = new clsPmieducarEscolaRedeEnsino( $this->cod_escola_rede_ensino,$this->pessoa_logada,null,null,null,null,0 );
+        $obj = new clsPmieducarEscolaRedeEnsino($this->cod_escola_rede_ensino, $this->pessoa_logada, null, null, null, null, 0);
         $excluiu = $obj->excluir();
-        if( $excluiu )
-        {
-            $this->mensagem .= "Exclus&atilde;o efetuada com sucesso.<br>";
+        if ($excluiu) {
+            $this->mensagem .= 'Exclus&atilde;o efetuada com sucesso.<br>';
             $this->simpleRedirect('educar_escola_localizacao_lst.php');
         }
 
-        $this->mensagem = "Exclus&atilde;o n&atilde;o realizada.<br>";
+        $this->mensagem = 'Exclus&atilde;o n&atilde;o realizada.<br>';
 
         return false;
     }
@@ -141,7 +133,6 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
-?>
--- ieducar/intranet/educar_escola_rede_ensino_lst.php
+++ ieducar/intranet/educar_escola_rede_ensino_lst.php
@@ -1,12 +1,11 @@
 <?php
 
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} i-Educar - Escola Rede Ensino" );
-        $this->processoAp = "647";
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Escola Rede Ensino");
+        $this->processoAp = '647';
     }
 }
 
@@ -17,70 +16,70 @@ class indice extends clsListagem
      *
      * @var int
      */
-    var $pessoa_logada;
+    public $pessoa_logada;
 
     /**
      * Titulo no topo da pagina
      *
      * @var int
      */
-    var $titulo;
+    public $titulo;
 
     /**
      * Quantidade de registros a ser apresentada em cada pagina
      *
      * @var int
      */
-    var $limite;
+    public $limite;
 
     /**
      * Inicio dos registros a serem exibidos (limit)
      *
      * @var int
      */
-    var $offset;
+    public $offset;
 
-    var $cod_escola_rede_ensino;
-    var $ref_usuario_exc;
-    var $ref_usuario_cad;
-    var $nm_rede;
-    var $data_cadastro;
-    var $data_exclusao;
-    var $ativo;
-    var $ref_cod_instituicao;
+    public $cod_escola_rede_ensino;
+    public $ref_usuario_exc;
+    public $ref_usuario_cad;
+    public $nm_rede;
+    public $data_cadastro;
+    public $data_exclusao;
+    public $ativo;
+    public $ref_cod_instituicao;
 
-    function Gerar()
+    public function Gerar()
     {
-        $this->titulo = "Escola Rede Ensino - Listagem";
+        $this->titulo = 'Escola Rede Ensino - Listagem';
 
-        foreach( $_GET AS $var => $val ) // passa todos os valores obtidos no GET para atributos do objeto
-            $this->$var = ( $val === "" ) ? null: $val;
+        foreach ($_GET as $var => $val) { // passa todos os valores obtidos no GET para atributos do objeto
+            $this->$var = ($val === '') ? null: $val;
+        }
 
-
-
-        $lista_busca = array(
-            "Rede Ensino"
-        );
+        $lista_busca = [
+            'Rede Ensino'
+        ];
 
         $obj_permissoes = new clsPermissoes();
         $nivel_usuario = $obj_permissoes->nivel_acesso($this->pessoa_logada);
-        if ($nivel_usuario == 1)
-            $lista_busca[] = "Institui&ccedil;&atilde;o";
+        if ($nivel_usuario == 1) {
+            $lista_busca[] = 'Institui&ccedil;&atilde;o';
+        }
 
         $this->addCabecalhos($lista_busca);
 
         // Filtros de Foreign Keys
-        include("include/pmieducar/educar_campo_lista.php");
+        include('include/pmieducar/educar_campo_lista.php');
 
-        $this->campoTexto( "nm_rede", "Rede Ensino", $this->nm_rede, 30, 255, false );
+        $this->campoTexto('nm_rede', 'Rede Ensino', $this->nm_rede, 30, 255, false);
 
         // Paginador
         $this->limite = 20;
-        $this->offset = ( $_GET["pagina_{$this->nome}"] ) ? $_GET["pagina_{$this->nome}"]*$this->limite-$this->limite: 0;
+        $this->offset = ($_GET["pagina_{$this->nome}"]) ? $_GET["pagina_{$this->nome}"]*$this->limite-$this->limite: 0;
 
         $obj_escola_rede_ensino = new clsPmieducarEscolaRedeEnsino();
-        $obj_escola_rede_ensino->setOrderby( "nm_rede ASC" );
-        $obj_escola_rede_ensino->setLimite( $this->limite, $this->offset );
+        $obj_escola_rede_ensino->setOrderby('nm_rede ASC');
+        $obj_escola_rede_ensino->setLimite($this->limite, $this->offset);
 
         $lista = $obj_escola_rede_ensino->lista(
             null,
@@ -98,33 +97,31 @@ class indice extends clsListagem
         $total = $obj_escola_rede_ensino->_total;
 
         // monta a lista
-        if( is_array( $lista ) && count( $lista ) )
-        {
-            foreach ( $lista AS $registro )
-            {
-                $obj_ref_cod_instituicao = new clsPmieducarInstituicao( $registro["ref_cod_instituicao"] );
+        if (is_array($lista) && count($lista)) {
+            foreach ($lista as $registro) {
+                $obj_ref_cod_instituicao = new clsPmieducarInstituicao($registro['ref_cod_instituicao']);
                 $det_ref_cod_instituicao = $obj_ref_cod_instituicao->detalhe();
-                $registro["ref_cod_instituicao"] = $det_ref_cod_instituicao["nm_instituicao"];
+                $registro['ref_cod_instituicao'] = $det_ref_cod_instituicao['nm_instituicao'];
 
-                $lista_busca = array(
-                    "<a href=\"educar_escola_rede_ensino_det.php?cod_escola_rede_ensino={$registro["cod_escola_rede_ensino"]}\">{$registro["nm_rede"]}</a>"
-                );
+                $lista_busca = [
+                    "<a href=\"educar_escola_rede_ensino_det.php?cod_escola_rede_ensino={$registro['cod_escola_rede_ensino']}\">{$registro['nm_rede']}</a>"
+                ];
 
-                if ($nivel_usuario == 1)
-                    $lista_busca[] = "<a href=\"educar_escola_rede_ensino_det.php?cod_escola_rede_ensino={$registro["cod_escola_rede_ensino"]}\">{$registro["ref_cod_instituicao"]}</a>";
+                if ($nivel_usuario == 1) {
+                    $lista_busca[] = "<a href=\"educar_escola_rede_ensino_det.php?cod_escola_rede_ensino={$registro['cod_escola_rede_ensino']}\">{$registro['ref_cod_instituicao']}</a>";
+                }
                 $this->addLinhas($lista_busca);
             }
         }
-        $this->addPaginador2( "educar_escola_rede_ensino_lst.php", $total, $_GET, $this->nome, $this->limite );
+        $this->addPaginador2('educar_escola_rede_ensino_lst.php', $total, $_GET, $this->nome, $this->limite);
 
         $obj_permissoes = new clsPermissoes();
-        if( $obj_permissoes->permissao_cadastra( 647, $this->pessoa_logada, 3 ) )
-        {
-            $this->acao = "go(\"educar_escola_rede_ensino_cad.php\")";
-            $this->nome_acao = "Novo";
+        if ($obj_permissoes->permissao_cadastra(647, $this->pessoa_logada, 3)) {
+            $this->acao = 'go("educar_escola_rede_ensino_cad.php")';
+            $this->nome_acao = 'Novo';
         }
 
-        $this->largura = "100%";
+        $this->largura = '100%';
 
         $this->breadcrumb('Listagem de redes de ensino', [
             url('intranet/educar_index.php') => 'Escola',
@@ -136,7 +133,6 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
-?>
