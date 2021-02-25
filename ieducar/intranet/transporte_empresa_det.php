d0c1937dcd2450691e599b5b09e9b866ef6fdb13
jrbarros <joalissonricardo@gmail.com>
2021-02-24 22:39:56 -0300

    Fix seção transporte escolar

--- ieducar/intranet/transporte_empresa_det.php
+++ ieducar/intranet/transporte_empresa_det.php
@@ -1,129 +1,82 @@
 <?php
 
-/**
- * i-Educar - Sistema de gestão escolar
- *
- * Copyright (C) 2006  Prefeitura Municipal de Itajaí
- *                     <ctima@itajai.sc.gov.br>
- *
- * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
- * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
- * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
- * qualquer versão posterior.
- *
- * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
- * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
- * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
- * do GNU para mais detalhes.
- *
- * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
- * com este programa; se não, escreva para a Free Software Foundation, Inc., no
- * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Arquivo disponível desde a versão 1.0.0
- * @version   $Id$
- */
-
-
-
-
-/**
- * clsIndexBase class.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Classe disponível desde a versão 1.0.0
- * @version   @@package_version@@
- */
 class clsIndexBase extends clsBase
 {
-  function Formular()
-  {
-    $this->SetTitulo($this->_instituicao . ' i-Educar - Empresas');
-    $this->processoAp = 21235;
-  }
+    public function Formular()
+    {
+        $this->SetTitulo($this->_instituicao . ' i-Educar - Empresas');
+        $this->processoAp = 21235;
+    }
 }
 
-/**
- * indice class.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Classe disponível desde a versão 1.0.0
- * @version   @@package_version@@
- */
 class indice extends clsDetalhe
 {
-  var $titulo;
+    public $titulo;
 
-  function Gerar()
-  {
-    // Verificação de permissão para cadastro.
-    $this->obj_permissao = new clsPermissoes();
-
-    $this->nivel_usuario = $this->obj_permissao->nivel_acesso($this->pessoa_logada);
-
-    $this->titulo = 'Empresa transporte escolar - Detalhe';
-
-
-    $cod_empresa_transporte_escolar = $_GET['cod_empresa'];
-
-    $tmp_obj = new clsModulesEmpresaTransporteEscolar($cod_empresa_transporte_escolar);
-    $registro = $tmp_obj->detalhe();
-
-    if (! $registro) {
-        $this->simpleRedirect('transporte_empresa_lst.php');
-    }
-
-    $objPessoaJuridica = new clsPessoaJuridica();
-    list ($id_federal, $endereco, $cep, $nm_bairro, $cidade, $ddd_telefone_1, $telefone_1, $ddd_telefone_2, $telefone_2, $ddd_telefone_mov, $telefone_mov, $ddd_telefone_fax, $telefone_fax, $email, $ins_est) = $objPessoaJuridica->queryRapida($registro['ref_idpes'], "cnpj","logradouro","cep","bairro","cidade", "ddd_1","fone_1","ddd_2","fone_2","ddd_mov","fone_mov","ddd_fax","fone_fax", "email","insc_estadual");
-
-    $this->addDetalhe( array("Código da empresa", $cod_empresa_transporte_escolar));
-    $this->addDetalhe( array("Nome fantasia", $registro['nome_empresa']) );
-    $this->addDetalhe( array("Nome do responsável", $registro['nome_responsavel']) );
-    $this->addDetalhe( array("CNPJ", int2CNPJ($id_federal)) );
-    $this->addDetalhe( array("Endere&ccedil;o", $endereco) );
-    $this->addDetalhe( array("CEP", $cep) );
-    $this->addDetalhe( array("Bairro", $nm_bairro) );
-    $this->addDetalhe( array("Cidade", $cidade) );
-    if (trim($telefone_1)!='')
-      $this->addDetalhe( array("Telefone 1", "({$ddd_telefone_1}) {$telefone_1}") );
-    if (trim($telefone_2)!='')
-      $this->addDetalhe( array("Telefone 2", "({$ddd_telefone_2}) {$telefone_2}") );
-    if (trim($telefone_mov)!='')
-      $this->addDetalhe( array("Celular", "({$ddd_telefone_mov}) {$telefone_mov}") );
-    if (trim($telefone_fax)!='')
-      $this->addDetalhe( array("Fax", "({$ddd_telefone_fax}) {$telefone_fax}") );
-
-    $this->addDetalhe( array("E-mail", $email) );
-
-    if( ! $ins_est ) $ins_est = "isento";
-      $this->addDetalhe( array("Inscri&ccedil;&atilde;o estadual", $ins_est) );
-    $this->addDetalhe( array("Observa&ccedil;&atilde;o", $registro['observacao']));
-    $this->url_cancelar = "transporte_empresa_lst.php";
-
-    $obj_permissao = new clsPermissoes();
-
-    if($obj_permissao->permissao_cadastra(21235, $this->pessoa_logada,7,null,true))
+    public function Gerar()
     {
-      $this->url_novo = "../module/TransporteEscolar/Empresa";
-      $this->url_editar = "../module/TransporteEscolar/Empresa?id={$cod_empresa_transporte_escolar}";
-    }
+        // Verificação de permissão para cadastro.
+        $this->obj_permissao = new clsPermissoes();
 
-    $this->largura = "100%";
+        $this->nivel_usuario = $this->obj_permissao->nivel_acesso($this->pessoa_logada);
 
-    $this->breadcrumb('Detalhe da empresa de transporte', [
+        $this->titulo = 'Empresa transporte escolar - Detalhe';
+
+        $cod_empresa_transporte_escolar = $_GET['cod_empresa'];
+
+        $tmp_obj = new clsModulesEmpresaTransporteEscolar($cod_empresa_transporte_escolar);
+        $registro = $tmp_obj->detalhe();
+
+        if (! $registro) {
+            $this->simpleRedirect('transporte_empresa_lst.php');
+        }
+
+        $objPessoaJuridica = new clsPessoaJuridica();
+        list($id_federal, $endereco, $cep, $nm_bairro, $cidade, $ddd_telefone_1, $telefone_1, $ddd_telefone_2, $telefone_2, $ddd_telefone_mov, $telefone_mov, $ddd_telefone_fax, $telefone_fax, $email, $ins_est) = $objPessoaJuridica->queryRapida($registro['ref_idpes'], 'cnpj', 'logradouro', 'cep', 'bairro', 'cidade', 'ddd_1', 'fone_1', 'ddd_2', 'fone_2', 'ddd_mov', 'fone_mov', 'ddd_fax', 'fone_fax', 'email', 'insc_estadual');
+
+        $this->addDetalhe(['Código da empresa', $cod_empresa_transporte_escolar]);
+        $this->addDetalhe(['Nome fantasia', $registro['nome_empresa']]);
+        $this->addDetalhe(['Nome do responsável', $registro['nome_responsavel']]);
+        $this->addDetalhe(['CNPJ', int2CNPJ($id_federal)]);
+        $this->addDetalhe(['Endere&ccedil;o', $endereco]);
+        $this->addDetalhe(['CEP', $cep]);
+        $this->addDetalhe(['Bairro', $nm_bairro]);
+        $this->addDetalhe(['Cidade', $cidade]);
+        if (trim($telefone_1)!='') {
+            $this->addDetalhe(['Telefone 1', "({$ddd_telefone_1}) {$telefone_1}"]);
+        }
+        if (trim($telefone_2)!='') {
+            $this->addDetalhe(['Telefone 2', "({$ddd_telefone_2}) {$telefone_2}"]);
+        }
+        if (trim($telefone_mov)!='') {
+            $this->addDetalhe(['Celular', "({$ddd_telefone_mov}) {$telefone_mov}"]);
+        }
+        if (trim($telefone_fax)!='') {
+            $this->addDetalhe(['Fax', "({$ddd_telefone_fax}) {$telefone_fax}"]);
+        }
+
+        $this->addDetalhe(['E-mail', $email]);
+
+        if (! $ins_est) {
+            $ins_est = 'isento';
+        }
+        $this->addDetalhe(['Inscri&ccedil;&atilde;o estadual', $ins_est]);
+        $this->addDetalhe(['Observa&ccedil;&atilde;o', $registro['observacao']]);
+        $this->url_cancelar = 'transporte_empresa_lst.php';
+
+        $obj_permissao = new clsPermissoes();
+
+        if ($obj_permissao->permissao_cadastra(21235, $this->pessoa_logada, 7, null, true)) {
+            $this->url_novo = '../module/TransporteEscolar/Empresa';
+            $this->url_editar = "../module/TransporteEscolar/Empresa?id={$cod_empresa_transporte_escolar}";
+        }
+
+        $this->largura = '100%';
+
+        $this->breadcrumb('Detalhe da empresa de transporte', [
         url('intranet/educar_transporte_escolar_index.php') => 'Transporte escolar',
     ]);
-  }
+    }
 }
 
 // Instancia o objeto da página
--- ieducar/intranet/transporte_ponto_det.php
+++ ieducar/intranet/transporte_ponto_det.php
@@ -1,115 +1,63 @@
 <?php
 
-/**
- * i-Educar - Sistema de gestão escolar
- *
- * Copyright (C) 2006  Prefeitura Municipal de Itajaí
- *                     <ctima@itajai.sc.gov.br>
- *
- * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
- * sob os termos da Licença Pública Geral GNU conforme publicada pela Free
- * Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
- * qualquer versão posterior.
- *
- * Este programa é distribuí­do na expectativa de que seja útil, porém, SEM
- * NENHUMA GARANTIA; nem mesmo a garantia implí­cita de COMERCIABILIDADE OU
- * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
- * do GNU para mais detalhes.
- *
- * Você deve ter recebido uma cópia da Licença Pública Geral do GNU junto
- * com este programa; se não, escreva para a Free Software Foundation, Inc., no
- * endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Arquivo disponível desde a versão 1.0.0
- * @version   $Id$
- */
-
-
-
-
-/**
- * clsIndexBase class.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.sc.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Classe disponível desde a versão 1.0.0
- * @version   @@package_version@@
- */
 class clsIndexBase extends clsBase
 {
-  function Formular()
-  {
-    $this->SetTitulo($this->_instituicao . ' i-Educar - Pontos');
-    $this->processoAp = 21239;
-  }
+    public function Formular()
+    {
+        $this->SetTitulo($this->_instituicao . ' i-Educar - Pontos');
+        $this->processoAp = 21239;
+    }
 }
 
-/**
- * indice class.
- *
- * @author    Prefeitura Municipal de Itajaí <ctima@itajai.scclsModulesPontoTransporteEscolar.gov.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Classe disponível desde a versão 1.0.0
- * @version   @@package_version@@
- */
 class indice extends clsDetalhe
 {
-  var $titulo;
+    public $titulo;
 
-  function Gerar()
-  {
-    // Verificação de permissão para cadastro.
-    $this->obj_permissao = new clsPermissoes();
-
-    $this->nivel_usuario = $this->obj_permissao->nivel_acesso($this->pessoa_logada);
-
-    $this->titulo = 'Ponto - Detalhe';
-
-    $cod_ponto_transporte_escolar = $_GET['cod_ponto'];
-    $tmp_obj = new clsModulesPontoTransporteEscolar($cod_ponto_transporte_escolar);
-    $registro = $tmp_obj->detalhe();
-
-    if (! $registro) {
-        $this->simpleRedirect('transporte_ponto_lst.php');
-    }
-
-    $this->addDetalhe( array("Código do ponto", $cod_ponto_transporte_escolar));
-    $this->addDetalhe( array("Descrição", $registro['descricao']) );
-
-    if (is_numeric($registro['cep']) && is_numeric($registro['idlog']) && is_numeric($registro['idbai'])){
-      $this->addDetalhe( array("CEP", int2CEP($registro['cep'])) );
-      $this->addDetalhe( array("Município - UF", $registro['municipio'] . ' - '. $registro['sigla_uf']) );
-      $this->addDetalhe( array("Bairro", $registro['bairro']) );
-      $this->addDetalhe( array("Zona de localização", $registro['zona_localizacao'] == 1 ? 'Urbana' : 'Rural' ) );
-      $this->addDetalhe( array("Endereço", $registro['logradouro']) );
-      $this->addDetalhe( array("Número", $registro['numero']) );
-      $this->addDetalhe( array("Complemento", $registro['complemento']) );
-    }
-
-    $obj_permissao = new clsPermissoes();
-
-    if($obj_permissao->permissao_cadastra(21239, $this->pessoa_logada,7,null,true))
+    public function Gerar()
     {
-      $this->url_novo = "../module/TransporteEscolar/Ponto";
-      $this->url_editar = "../module/TransporteEscolar/Ponto?id={$cod_ponto_transporte_escolar}";
-    }
+        // Verificação de permissão para cadastro.
+        $this->obj_permissao = new clsPermissoes();
 
-    $this->url_cancelar = "transporte_ponto_lst.php";
+        $this->nivel_usuario = $this->obj_permissao->nivel_acesso($this->pessoa_logada);
 
-    $this->largura = "100%";
+        $this->titulo = 'Ponto - Detalhe';
 
-    $this->breadcrumb('Detalhe do ponto', [
+        $cod_ponto_transporte_escolar = $_GET['cod_ponto'];
+        $tmp_obj = new clsModulesPontoTransporteEscolar($cod_ponto_transporte_escolar);
+        $registro = $tmp_obj->detalhe();
+
+        if (! $registro) {
+            $this->simpleRedirect('transporte_ponto_lst.php');
+        }
+
+        $this->addDetalhe(['Código do ponto', $cod_ponto_transporte_escolar]);
+        $this->addDetalhe(['Descrição', $registro['descricao']]);
+
+        if (is_numeric($registro['cep']) && is_numeric($registro['idlog']) && is_numeric($registro['idbai'])) {
+            $this->addDetalhe(['CEP', int2CEP($registro['cep'])]);
+            $this->addDetalhe(['Município - UF', $registro['municipio'] . ' - '. $registro['sigla_uf']]);
+            $this->addDetalhe(['Bairro', $registro['bairro']]);
+            $this->addDetalhe(['Zona de localização', $registro['zona_localizacao'] == 1 ? 'Urbana' : 'Rural' ]);
+            $this->addDetalhe(['Endereço', $registro['logradouro']]);
+            $this->addDetalhe(['Número', $registro['numero']]);
+            $this->addDetalhe(['Complemento', $registro['complemento']]);
+        }
+
+        $obj_permissao = new clsPermissoes();
+
+        if ($obj_permissao->permissao_cadastra(21239, $this->pessoa_logada, 7, null, true)) {
+            $this->url_novo = '../module/TransporteEscolar/Ponto';
+            $this->url_editar = "../module/TransporteEscolar/Ponto?id={$cod_ponto_transporte_escolar}";
+        }
+
+        $this->url_cancelar = 'transporte_ponto_lst.php';
+
+        $this->largura = '100%';
+
+        $this->breadcrumb('Detalhe do ponto', [
         url('intranet/educar_transporte_escolar_index.php') => 'Transporte escolar',
     ]);
-  }
+    }
 }
 
 // Instancia o objeto da página
