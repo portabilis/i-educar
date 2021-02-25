736f01fc4a5c457885021b09f0f9b1ae535266bb
jrbarros <joalissonricardo@gmail.com>
2021-02-24 22:41:17 -0300

    Fix seção professor

--- ieducar/intranet/educar_rquadro_professores.php
+++ ieducar/intranet/educar_rquadro_professores.php
@@ -1,72 +1,59 @@
 <?php
 
-
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} i-Educar - Quadro Curricular" );
-        $this->processoAp = "696";
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Quadro Curricular");
+        $this->processoAp = '696';
     }
 }
 
 class indice extends clsCadastro
 {
 
-
     /**
      * Referencia pega da session para o idpes do usuario atual
      *
      * @var int
      */
-    var $pessoa_logada;
+    public $pessoa_logada;
 
+    public $ref_cod_instituicao;
+    public $ref_cod_escola;
 
-    var $ref_cod_instituicao;
-    var $ref_cod_escola;
+    public $ano;
 
+    public $nm_escola;
+    public $nm_instituicao;
 
-    var $ano;
+    public $pdf;
 
-    var $nm_escola;
-    var $nm_instituicao;
+    public $page_y = 139;
 
-    var $pdf;
-
-
-    var $page_y = 139;
-
-
-
-    function Inicializar()
+    public function Inicializar()
     {
-        $retorno = "Novo";
-
+        $retorno = 'Novo';
 
         return $retorno;
     }
 
-    function Gerar()
+    public function Gerar()
     {
-
-
-        if($_POST){
+        if ($_POST) {
             foreach ($_POST as $key => $value) {
                 $this->$key = $value;
-
             }
         }
 
-        $this->ano = $ano_atual = date("Y");
+        $this->ano = $ano_atual = date('Y');
 
         $lim = 5;
-        for($a = date('Y') ; $a < $ano_atual + $lim ; $a++ )
-                $anos["{$a}"] = "{$a}";
+        for ($a = date('Y') ; $a < $ano_atual + $lim ; $a++) {
+            $anos["{$a}"] = "{$a}";
+        }
 
-
-        $this->campoLista( "ano", "Ano",$anos, $this->ano,"",false );
-
+        $this->campoLista('ano', 'Ano', $anos, $this->ano, '', false);
 
         $get_escola = true;
         $get_curso = true;
@@ -74,22 +61,18 @@ class indice extends clsCadastro
         $obrigatorio = false;
         $instituicao_obrigatorio = true;
 
+        include('include/pmieducar/educar_campo_lista.php');
 
-        include("include/pmieducar/educar_campo_lista.php");
-
-        if($this->ref_cod_escola)
+        if ($this->ref_cod_escola) {
             $this->ref_ref_cod_escola = $this->ref_cod_escola;
+        }
 
-        $this->url_cancelar = "educar_index.php";
-        $this->nome_url_cancelar = "Cancelar";
+        $this->url_cancelar = 'educar_index.php';
+        $this->nome_url_cancelar = 'Cancelar';
 
         $this->acao_enviar = 'acao2()';
         $this->acao_executa_submit = false;
-
     }
-
-
-
 }
 
 // cria uma extensao da classe base
@@ -97,10 +80,9 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
-
 
 ?>
 <script>
