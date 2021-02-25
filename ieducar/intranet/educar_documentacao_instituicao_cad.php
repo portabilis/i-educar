501f6c470a4c05bacea7d5fdf46b3f8dea758515
jrbarros <joalissonricardo@gmail.com>
2021-02-24 22:32:19 -0300

    Fix seção documento instituição

--- ieducar/intranet/educar_documentacao_instituicao_cad.php
+++ ieducar/intranet/educar_documentacao_instituicao_cad.php
@@ -1,12 +1,11 @@
 <?php
 
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} i-Educar - Documentação padrão" );
-        $this->processoAp = "578";
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Documentação padrão");
+        $this->processoAp = '578';
     }
 }
 
@@ -17,47 +16,45 @@ class indice extends clsCadastro
      *
      * @var int
      */
-    var $pessoa_logada;
+    public $pessoa_logada;
 
-    var $cod_instituicao;
+    public $cod_instituicao;
 
-    function Inicializar()
+    public function Inicializar()
     {
-        $retorno = "Documentação padrão";
+        $retorno = 'Documentação padrão';
 
         $this->breadcrumb('Documentação padrão', [
             url('intranet/educar_index.php') => 'Escola',
         ]);
 
-        $this->cod_instituicao=$_GET["cod_instituicao"];
+        $this->cod_instituicao=$_GET['cod_instituicao'];
 
         return $retorno;
     }
 
-    function Gerar()
+    public function Gerar()
     {
-        Portabilis_View_Helper_Application::loadJavascript($this, array('/modules/Cadastro/Assets/Javascripts/DocumentacaoPadrao.js'));
+        Portabilis_View_Helper_Application::loadJavascript($this, ['/modules/Cadastro/Assets/Javascripts/DocumentacaoPadrao.js']);
 
         $obj_usuario = new clsPmieducarUsuario($this->pessoa_logada);
         $obj_usuario_det = $obj_usuario->detalhe();
-        $this->ref_cod_escola = $obj_usuario_det["ref_cod_escola"];
+        $this->ref_cod_escola = $obj_usuario_det['ref_cod_escola'];
 
-        $this->campoOculto( "cod_instituicao", $this->cod_instituicao );
-        $this->campoOculto( "pessoa_logada", $this->pessoa_logada );
-        $this->campoOculto( "ref_cod_escola", $this->ref_cod_escola );
+        $this->campoOculto('cod_instituicao', $this->cod_instituicao);
+        $this->campoOculto('pessoa_logada', $this->pessoa_logada);
+        $this->campoOculto('ref_cod_escola', $this->ref_cod_escola);
 
+        $this->campoTexto('titulo_documento', 'Título', $this->titulo_documento, 30, 50, false);
 
-        $this->campoTexto( "titulo_documento", "Título", $this->titulo_documento, 30, 50, false );
-
-        $this->campoArquivo('documento','Documentação padrão',$this->documento,40,Portabilis_String_Utils::toLatin1("<span id='aviso_formato'>São aceitos apenas arquivos no formato PDF com até 2MB.</span>", array('escape' => false)));
+        $this->campoArquivo('documento', 'Documentação padrão', $this->documento, 40, Portabilis_String_Utils::toLatin1('<span id=\'aviso_formato\'>São aceitos apenas arquivos no formato PDF com até 2MB.</span>', ['escape' => false]));
 
         $this->array_botao[] = 'Salvar';
-        $this->array_botao_url_script[] = "go('educar_instituicao_lst.php')";
+        $this->array_botao_url_script[] = 'go(\'educar_instituicao_lst.php\')';
 
         $this->array_botao[] = 'Voltar';
-        $this->array_botao_url_script[] = "go('educar_instituicao_lst.php')";
+        $this->array_botao_url_script[] = 'go(\'educar_instituicao_lst.php\')';
     }
-
 }
 
 // cria uma extensao da classe base
@@ -65,7 +62,6 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
-?>
