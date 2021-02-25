01c5eba37a6d34e11d2272d2cf29b9990af20363
jrbarros <joalissonricardo@gmail.com>
2021-02-24 22:31:17 -0300

    Fix seção disciplina

--- ieducar/intranet/educar_disciplina_dependencia_cad.php
+++ ieducar/intranet/educar_disciplina_dependencia_cad.php
@@ -1,110 +1,79 @@
 <?php
 
-/**
- * i-Educar - Sistema de gestÃ£o escolar
- *
- * Copyright (C) 2006  Prefeitura Municipal de ItajaÃ­
- *                     <ctima@itajai.sc.gov.br>
- *
- * Este programa Ã© software livre; vocÃª pode redistribuÃ­-lo e/ou modificÃ¡-lo
- * sob os termos da LicenÃ§a PÃºblica Geral GNU conforme publicada pela Free
- * Software Foundation; tanto a versÃ£o 2 da LicenÃ§a, como (a seu critÃ©rio)
- * qualquer versÃ£o posterior.
- *
- * Este programa Ã© distribuÃ­Â­do na expectativa de que seja Ãºtil, porÃ©m, SEM
- * NENHUMA GARANTIA; nem mesmo a garantia implÃ­Â­cita de COMERCIABILIDADE OU
- * ADEQUAÃÃO A UMA FINALIDADE ESPECÃFICA. Consulte a LicenÃ§a PÃºblica Geral
- * do GNU para mais detalhes.
- *
- * VocÃª deve ter recebido uma cÃ³pia da LicenÃ§a PÃºblica Geral do GNU junto
- * com este programa; se nÃ£o, escreva para a Free Software Foundation, Inc., no
- * endereÃ§o 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
- *
- * @author    Lucas Schmoeller da Silva <lucas@portabilis.com.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Arquivo disponÃ­vel desde a versÃ£o 1.0.0
- * @version   $Id$
- */
-
-
-/**
- * clsIndexBase class.
- *
- * @author    Lucas Schmoeller da Silva <lucas@portabilis.com.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Classe disponÃ­vel desde a versÃ£o 1.0.0
- * @version   @@package_version@@
- */
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
         $this->SetTitulo($this->_instituicao . ' i-Educar - Dispensa Componente Curricular');
         $this->processoAp = 578;
     }
 }
 
-/**
- * indice class.
- *
- * @author    Lucas Schmoeller da Silva <lucas@portabilis.com.br>
- * @category  i-Educar
- * @license   @@license@@
- * @package   iEd_Pmieducar
- * @since     Classe disponÃ­vel desde a versÃ£o 1.0.0
- * @version   @@package_version@@
- */
 class indice extends clsCadastro
 {
-    var $pessoa_logada;
+    public $pessoa_logada;
 
-    var $observacao;
+    public $observacao;
 
-    var $ref_cod_matricula;
-    var $ref_cod_turma;
-    var $ref_cod_serie;
-    var $ref_cod_disciplina;
-    var $ref_sequencial;
-    var $ref_cod_instituicao;
-    var $ref_cod_escola;
+    public $ref_cod_matricula;
+    public $ref_cod_turma;
+    public $ref_cod_serie;
+    public $ref_cod_disciplina;
+    public $ref_sequencial;
+    public $ref_cod_instituicao;
+    public $ref_cod_escola;
 
-    function Inicializar()
+    public function Inicializar()
     {
         $retorno = 'Novo';
-
 
         $this->ref_cod_disciplina = $_GET['ref_cod_disciplina'];
         $this->ref_cod_matricula = $_GET['ref_cod_matricula'];
 
         $obj_permissoes = new clsPermissoes();
-        $obj_permissoes->permissao_cadastra(578, $this->pessoa_logada, 7,
-            'educar_disciplina_dependencia_lst.php?ref_ref_cod_matricula=' . $this->ref_cod_matricula);
+        $obj_permissoes->permissao_cadastra(
+            578,
+            $this->pessoa_logada,
+            7,
+            'educar_disciplina_dependencia_lst.php?ref_ref_cod_matricula=' . $this->ref_cod_matricula
+        );
 
         if (is_numeric($this->ref_cod_matricula)) {
-            $obj_matricula = new clsPmieducarMatricula($this->ref_cod_matricula, NULL,
-                NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
+            $obj_matricula = new clsPmieducarMatricula(
+                $this->ref_cod_matricula,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                1
+            );
 
             $det_matricula = $obj_matricula->detalhe();
 
             if (!$det_matricula) {
-                $this->simpleRedirect("educar_matricula_lst.php");
+                $this->simpleRedirect('educar_matricula_lst.php');
             }
 
             $this->ref_cod_escola = $det_matricula['ref_ref_cod_escola'];
             $this->ref_cod_serie = $det_matricula['ref_ref_cod_serie'];
         } else {
-            $this->simpleRedirect("educar_matricula_lst.php");
+            $this->simpleRedirect('educar_matricula_lst.php');
         }
 
         if (is_numeric($this->ref_cod_matricula) && is_numeric($this->ref_cod_serie) &&
             is_numeric($this->ref_cod_escola) && is_numeric($this->ref_cod_disciplina)
         ) {
-            $obj = new clsPmieducarDisciplinaDependencia($this->ref_cod_matricula,
-                $this->ref_cod_serie, $this->ref_cod_escola, $this->ref_cod_disciplina);
+            $obj = new clsPmieducarDisciplinaDependencia(
+                $this->ref_cod_matricula,
+                $this->ref_cod_serie,
+                $this->ref_cod_escola,
+                $this->ref_cod_disciplina
+            );
 
             $registro = $obj->detalhe();
 
@@ -117,7 +86,7 @@ class indice extends clsCadastro
                 $obj_permissoes = new clsPermissoes();
 
                 if ($obj_permissoes->permissao_excluir(578, $this->pessoa_logada, 7)) {
-                    $this->fexcluir = TRUE;
+                    $this->fexcluir = true;
                 }
 
                 $retorno = 'Editar';
@@ -125,9 +94,13 @@ class indice extends clsCadastro
         }
 
         $this->url_cancelar = $retorno == 'Editar' ?
-            sprintf('educar_disciplina_dependencia_det.php?ref_cod_matricula=%d&ref_cod_serie=%d&ref_cod_escola=%d&ref_cod_disciplina=%d',
-                $registro['ref_cod_matricula'], $registro['ref_cod_serie'],
-                $registro['ref_cod_escola'], $registro['ref_cod_disciplina']) :
+            sprintf(
+                'educar_disciplina_dependencia_det.php?ref_cod_matricula=%d&ref_cod_serie=%d&ref_cod_escola=%d&ref_cod_disciplina=%d',
+                $registro['ref_cod_matricula'],
+                $registro['ref_cod_serie'],
+                $registro['ref_cod_escola'],
+                $registro['ref_cod_disciplina']
+            ) :
             'educar_disciplina_dependencia_lst.php?ref_cod_matricula=' . $this->ref_cod_matricula;
 
         $this->nome_url_cancelar = 'Cancelar';
@@ -139,7 +112,7 @@ class indice extends clsCadastro
         return $retorno;
     }
 
-    function Gerar()
+    public function Gerar()
     {
         /**
          * Busca dados da matricula
@@ -148,19 +121,52 @@ class indice extends clsCadastro
         $detalhe_aluno = array_shift($obj_ref_cod_matricula->lista($this->ref_cod_matricula));
 
         $obj_aluno = new clsPmieducarAluno();
-        $det_aluno = array_shift($det_aluno = $obj_aluno->lista($detalhe_aluno['ref_cod_aluno'],
-            NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1));
+        $det_aluno = array_shift($det_aluno = $obj_aluno->lista(
+            $detalhe_aluno['ref_cod_aluno'],
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            1
+        ));
 
-        $obj_escola = new clsPmieducarEscola($this->ref_cod_escola, NULL, NULL, NULL,
-            NULL, NULL, NULL, NULL, NULL, NULL, 1);
+        $obj_escola = new clsPmieducarEscola(
+            $this->ref_cod_escola,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            1
+        );
 
         $det_escola = $obj_escola->detalhe();
         $this->ref_cod_instituicao = $det_escola['ref_cod_instituicao'];
 
         $obj_matricula_turma = new clsPmieducarMatriculaTurma();
-        $lst_matricula_turma = $obj_matricula_turma->lista($this->ref_cod_matricula,
-            NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, $this->ref_cod_serie, NULL,
-            $this->ref_cod_escola);
+        $lst_matricula_turma = $obj_matricula_turma->lista(
+            $this->ref_cod_matricula,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            1,
+            $this->ref_cod_serie,
+            null,
+            $this->ref_cod_escola
+        );
 
         if (is_array($lst_matricula_turma)) {
             $det = array_shift($lst_matricula_turma);
@@ -172,6 +178,7 @@ class indice extends clsCadastro
 
         if (!isset($this->ref_cod_turma)) {
             $this->mensagem = 'Para cadastrar uma disciplina de depend&ecirc;ncia de um aluno, &eacute; necess&aacute;rio que este esteja enturmado.';
+
             return;
         }
 
@@ -180,15 +187,19 @@ class indice extends clsCadastro
         $this->campoOculto('ref_cod_serie', $this->ref_cod_serie);
         $this->campoOculto('ref_cod_escola', $this->ref_cod_escola);
 
-        $opcoes = array('' => 'Selecione');
+        $opcoes = ['' => 'Selecione'];
 
         // Seleciona os componentes curriculares da turma
 
         try {
-            $componentes = App_Model_IedFinder::getComponentesTurma($this->ref_cod_serie,
-                $this->ref_cod_escola, $this->ref_cod_turma);
+            $componentes = App_Model_IedFinder::getComponentesTurma(
+                $this->ref_cod_serie,
+                $this->ref_cod_escola,
+                $this->ref_cod_turma
+            );
         } catch (App_Model_Exception $e) {
             $this->mensagem = $e->getMessage();
+
             return;
         }
 
@@ -200,14 +211,18 @@ class indice extends clsCadastro
             $this->campoRotulo('nm_disciplina', 'Disciplina', $opcoes[$this->ref_cod_disciplina]);
             $this->campoOculto('ref_cod_disciplina', $this->ref_cod_disciplina);
         } else {
-            $this->campoLista('ref_cod_disciplina', 'Disciplina', $opcoes,
-                $this->ref_cod_disciplina);
+            $this->campoLista(
+                'ref_cod_disciplina',
+                'Disciplina',
+                $opcoes,
+                $this->ref_cod_disciplina
+            );
         }
 
-        $this->campoMemo('observacao', 'Observa&ccedil;&atilde;o', $this->observacao, 60, 10, FALSE);
+        $this->campoMemo('observacao', 'Observa&ccedil;&atilde;o', $this->observacao, 60, 10, false);
     }
 
-    function existeComponenteSerie()
+    public function existeComponenteSerie()
     {
         $db = new clsBanco();
         $sql = "SELECT  EXISTS (SELECT 1
@@ -216,10 +231,11 @@ class indice extends clsCadastro
                                  AND ref_ref_cod_escola = {$this->ref_cod_escola}
                                  AND ref_cod_disciplina = {$this->ref_cod_disciplina}
                                  AND escola_serie_disciplina.ativo = 1)";
+
         return dbBool($db->campoUnico($sql));
     }
 
-    function validaQuantidadeDisciplinasDependencia()
+    public function validaQuantidadeDisciplinasDependencia()
     {
         $query = <<<'SQL'
             SELECT t.ano
@@ -264,13 +280,15 @@ SQL;
         return $valid;
     }
 
-    function Novo()
+    public function Novo()
     {
-
-
         $obj_permissoes = new clsPermissoes();
-        $obj_permissoes->permissao_cadastra(578, $this->pessoa_logada, 7,
-            'educar_disciplina_dependencia_lst.php?ref_cod_matricula=' . $this->ref_cod_matricula);
+        $obj_permissoes->permissao_cadastra(
+            578,
+            $this->pessoa_logada,
+            7,
+            'educar_disciplina_dependencia_lst.php?ref_cod_matricula=' . $this->ref_cod_matricula
+        );
 
         if (!$this->validaQuantidadeDisciplinasDependencia()) {
             return false;
@@ -280,9 +298,9 @@ SQL;
             $this->mensagem = 'O componente não está habilitado na série da escola.';
             $this->url_cancelar = 'educar_disciplina_dependencia_lst.php?ref_cod_matricula=' . $this->ref_cod_matricula;
             $this->nome_url_cancelar = 'Cancelar';
+
             return false;
         }
-
 
         $sql = 'SELECT MAX(cod_disciplina_dependencia) + 1 FROM pmieducar.disciplina_dependencia';
         $db = new clsBanco();
@@ -291,14 +309,23 @@ SQL;
         // Caso nÃ£o exista nenhuma dispensa, atribui o cÃ³digo 1, tabela nÃ£o utiliza sequences
         $max_cod_disciplina_dependencia = $max_cod_disciplina_dependencia > 0 ? $max_cod_disciplina_dependencia : 1;
 
-        $obj = new clsPmieducarDisciplinaDependencia($this->ref_cod_matricula,
-            $this->ref_cod_serie, $this->ref_cod_escola, $this->ref_cod_disciplina,
-            $this->observacao, $max_cod_disciplina_dependencia);
+        $obj = new clsPmieducarDisciplinaDependencia(
+            $this->ref_cod_matricula,
+            $this->ref_cod_serie,
+            $this->ref_cod_escola,
+            $this->ref_cod_disciplina,
+            $this->observacao,
+            $max_cod_disciplina_dependencia
+        );
 
         if ($obj->existe()) {
-            $obj = new clsPmieducarDisciplinaDependencia($this->ref_cod_matricula,
-                $this->ref_cod_serie, $this->ref_cod_escola, $this->ref_cod_disciplina,
-                $this->observacao);
+            $obj = new clsPmieducarDisciplinaDependencia(
+                $this->ref_cod_matricula,
+                $this->ref_cod_serie,
+                $this->ref_cod_escola,
+                $this->ref_cod_disciplina,
+                $this->observacao
+            );
 
             $obj->edita();
             $this->simpleRedirect('educar_disciplina_dependencia_lst.php?ref_cod_matricula=' . $this->ref_cod_matricula);
@@ -312,20 +339,26 @@ SQL;
 
         $this->mensagem = 'Cadastro n&atilde;o realizado.<br />';
 
-        return FALSE;
+        return false;
     }
 
-    function Editar()
+    public function Editar()
     {
-
-
         $obj_permissoes = new clsPermissoes();
-        $obj_permissoes->permissao_cadastra(578, $this->pessoa_logada, 7,
-            'educar_disciplina_dependencia_lst.php?ref_cod_matricula=' . $this->ref_cod_matricula);
+        $obj_permissoes->permissao_cadastra(
+            578,
+            $this->pessoa_logada,
+            7,
+            'educar_disciplina_dependencia_lst.php?ref_cod_matricula=' . $this->ref_cod_matricula
+        );
 
-        $obj = new clsPmieducarDisciplinaDependencia($this->ref_cod_matricula,
-            $this->ref_cod_serie, $this->ref_cod_escola, $this->ref_cod_disciplina,
-            $this->observacao);
+        $obj = new clsPmieducarDisciplinaDependencia(
+            $this->ref_cod_matricula,
+            $this->ref_cod_serie,
+            $this->ref_cod_escola,
+            $this->ref_cod_disciplina,
+            $this->observacao
+        );
 
         $editou = $obj->edita();
         if ($editou) {
@@ -335,20 +368,26 @@ SQL;
 
         $this->mensagem = 'Edi&ccedil;&atilde;o nÃ£o realizada.<br />';
 
-        return FALSE;
+        return false;
     }
 
-    function Excluir()
+    public function Excluir()
     {
-
-
         $obj_permissoes = new clsPermissoes();
-        $obj_permissoes->permissao_excluir(578, $this->pessoa_logada, 7,
-            'educar_disciplina_dependencia_lst.php?ref_cod_matricula=' . $this->ref_cod_matricula);
+        $obj_permissoes->permissao_excluir(
+            578,
+            $this->pessoa_logada,
+            7,
+            'educar_disciplina_dependencia_lst.php?ref_cod_matricula=' . $this->ref_cod_matricula
+        );
 
-        $obj = new clsPmieducarDisciplinaDependencia($this->ref_cod_matricula,
-            $this->ref_cod_serie, $this->ref_cod_escola, $this->ref_cod_disciplina,
-            $this->observacao);
+        $obj = new clsPmieducarDisciplinaDependencia(
+            $this->ref_cod_matricula,
+            $this->ref_cod_serie,
+            $this->ref_cod_escola,
+            $this->ref_cod_disciplina,
+            $this->observacao
+        );
 
         $excluiu = $obj->excluir();
 
@@ -359,7 +398,7 @@ SQL;
 
         $this->mensagem = 'Exclus&atilde;o nÃ£o realizada.<br />';
 
-        return FALSE;
+        return false;
     }
 }
 
--- ieducar/intranet/educar_disciplina_topico_cad.php
+++ ieducar/intranet/educar_disciplina_topico_cad.php
@@ -1,12 +1,11 @@
 <?php
 
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} i-Educar - Disciplina T&oacute;pico" );
-        $this->processoAp = "565";
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Disciplina T&oacute;pico");
+        $this->processoAp = '565';
     }
 }
 
@@ -17,107 +16,95 @@ class indice extends clsCadastro
      *
      * @var int
      */
-    var $pessoa_logada;
+    public $pessoa_logada;
 
-    var $cod_disciplina_topico;
-    var $ref_usuario_exc;
-    var $ref_usuario_cad;
-    var $nm_topico;
-    var $desc_topico;
-    var $data_cadastro;
-    var $data_exclusao;
-    var $ativo;
+    public $cod_disciplina_topico;
+    public $ref_usuario_exc;
+    public $ref_usuario_cad;
+    public $nm_topico;
+    public $desc_topico;
+    public $data_cadastro;
+    public $data_exclusao;
+    public $ativo;
 
-    function Inicializar()
+    public function Inicializar()
     {
-        $retorno = "Novo";
+        $retorno = 'Novo';
 
+        $this->cod_disciplina_topico=$_GET['cod_disciplina_topico'];
 
-        $this->cod_disciplina_topico=$_GET["cod_disciplina_topico"];
-
-        if( is_numeric( $this->cod_disciplina_topico ) )
-        {
-
-            $obj = new clsPmieducarDisciplinaTopico( $this->cod_disciplina_topico );
+        if (is_numeric($this->cod_disciplina_topico)) {
+            $obj = new clsPmieducarDisciplinaTopico($this->cod_disciplina_topico);
             $registro  = $obj->detalhe();
-            if( $registro )
-            {
-                foreach( $registro AS $campo => $val )  // passa todos os valores obtidos no registro para atributos do objeto
+            if ($registro) {
+                foreach ($registro as $campo => $val) {  // passa todos os valores obtidos no registro para atributos do objeto
                     $this->$campo = $val;
+                }
                 $obj_permissao = new clsPermissoes();
-                $this->fexcluir = $obj_permissao->permissao_excluir( 565, $this->pessoa_logada,7 );
-                $retorno = "Editar";
+                $this->fexcluir = $obj_permissao->permissao_excluir(565, $this->pessoa_logada, 7);
+                $retorno = 'Editar';
             }
         }
-        $this->url_cancelar = ($retorno == "Editar") ? "educar_disciplina_topico_det.php?cod_disciplina_topico={$registro["cod_disciplina_topico"]}" : "educar_disciplina_topico_lst.php";
-        $this->nome_url_cancelar = "Cancelar";
+        $this->url_cancelar = ($retorno == 'Editar') ? "educar_disciplina_topico_det.php?cod_disciplina_topico={$registro['cod_disciplina_topico']}" : 'educar_disciplina_topico_lst.php';
+        $this->nome_url_cancelar = 'Cancelar';
+
         return $retorno;
     }
 
-    function Gerar()
+    public function Gerar()
     {
         $obj_permissao = new clsPermissoes();
-        $obj_permissao->permissao_cadastra(565,$this->pessoa_logada,7,"educar_disciplina_topico_lst.php");
+        $obj_permissao->permissao_cadastra(565, $this->pessoa_logada, 7, 'educar_disciplina_topico_lst.php');
         // primary keys
-        $this->campoOculto( "cod_disciplina_topico", $this->cod_disciplina_topico );
+        $this->campoOculto('cod_disciplina_topico', $this->cod_disciplina_topico);
 
         // foreign keys
 
         // text
-        $this->campoTexto( "nm_topico", "Nome T&oacute;pico", $this->nm_topico, 30, 255, true );
-        $this->campoMemo( "desc_topico", "Descri&ccedil;&atilde;o T&oacute;pico", $this->desc_topico, 30, 5, false );
+        $this->campoTexto('nm_topico', 'Nome T&oacute;pico', $this->nm_topico, 30, 255, true);
+        $this->campoMemo('desc_topico', 'Descri&ccedil;&atilde;o T&oacute;pico', $this->desc_topico, 30, 5, false);
 
         // data
-
     }
 
-    function Novo()
+    public function Novo()
     {
-
-
-        $obj = new clsPmieducarDisciplinaTopico( null, null, $this->pessoa_logada, $this->nm_topico, $this->desc_topico);
+        $obj = new clsPmieducarDisciplinaTopico(null, null, $this->pessoa_logada, $this->nm_topico, $this->desc_topico);
         $cadastrou = $obj->cadastra();
-        if( $cadastrou )
-        {
-            $this->mensagem .= "Cadastro efetuado com sucesso.<br>";
+        if ($cadastrou) {
+            $this->mensagem .= 'Cadastro efetuado com sucesso.<br>';
             $this->simpleRedirect('educar_disciplina_topico_lst.php');
         }
 
-        $this->mensagem = "Cadastro n&atilde;o realizado.<br>";
+        $this->mensagem = 'Cadastro n&atilde;o realizado.<br>';
 
         return false;
     }
 
-    function Editar()
+    public function Editar()
     {
-
-
-        $obj = new clsPmieducarDisciplinaTopico($this->cod_disciplina_topico, $this->pessoa_logada, null, $this->nm_topico, $this->desc_topico, null,null,1);
+        $obj = new clsPmieducarDisciplinaTopico($this->cod_disciplina_topico, $this->pessoa_logada, null, $this->nm_topico, $this->desc_topico, null, null, 1);
         $editou = $obj->edita();
-        if( $editou )
-        {
-            $this->mensagem .= "Edi&ccedil;&atilde;o efetuada com sucesso.<br>";
+        if ($editou) {
+            $this->mensagem .= 'Edi&ccedil;&atilde;o efetuada com sucesso.<br>';
             $this->simpleRedirect('educar_disciplina_topico_lst.php');
         }
 
-        $this->mensagem = "Edi&ccedil;&atilde;o n&atilde;o realizada.<br>";
+        $this->mensagem = 'Edi&ccedil;&atilde;o n&atilde;o realizada.<br>';
 
         return false;
     }
 
-    function Excluir()
+    public function Excluir()
     {
-
-
         $obj = new clsPmieducarDisciplinaTopico($this->cod_disciplina_topico, $this->pessoa_logada, null, null, null, null, null, 0);
         $excluiu = $obj->excluir();
-        if( $excluiu )
-        {
-            $this->mensagem .= "Exclus&atilde;o efetuada com sucesso.<br>";
+        if ($excluiu) {
+            $this->mensagem .= 'Exclus&atilde;o efetuada com sucesso.<br>';
             $this->simpleRedirect('educar_disciplina_topico_lst.php');
         }
 
-        $this->mensagem = "Exclus&atilde;o n&atilde;o realizada.<br>";
+        $this->mensagem = 'Exclus&atilde;o n&atilde;o realizada.<br>';
 
         return false;
     }
@@ -128,7 +115,6 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
-?>
