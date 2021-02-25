935538e017169f44ba6fe4dfba862c4ce44e88aa
jrbarros <joalissonricardo@gmail.com>
2021-02-24 22:24:37 -0300

    Fix seção de aluno geral

--- ieducar/intranet/educar_alunos_defasados_nominal.php
+++ ieducar/intranet/educar_alunos_defasados_nominal.php
@@ -1,12 +1,11 @@
 <?php
 
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} i-Educar - Movimentação Mensal de Alunos" );
-        $this->processoAp = "944";
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Movimentação Mensal de Alunos");
+        $this->processoAp = '944';
     }
 }
 
@@ -17,87 +16,79 @@ class indice extends clsCadastro
      *
      * @var int
      */
-    var $pessoa_logada;
+    public $pessoa_logada;
 
+    public $ref_cod_instituicao;
+    public $ref_cod_escola;
+    public $ano;
+    public $mes;
 
-    var $ref_cod_instituicao;
-    var $ref_cod_escola;
-    var $ano;
-    var $mes;
+    public $nm_escola;
+    public $nm_instituicao;
+    public $ref_cod_curso;
+    public $sequencial;
 
-    var $nm_escola;
-    var $nm_instituicao;
-    var $ref_cod_curso;
-    var $sequencial;
+    public $pdf;
+    public $pagina_atual = 1;
+    public $total_paginas = 1;
 
-    var $pdf;
-    var $pagina_atual = 1;
-    var $total_paginas = 1;
+    public $page_y = 125;
 
-    var $page_y = 125;
+    public $cursos = [];
 
-    var $cursos = array();
+    public $array_disciplinas = [];
 
-    var $array_disciplinas = array();
+    public $get_link;
 
-    var $get_link;
+    public $ref_cod_modulo;
 
-    var $ref_cod_modulo;
+    public $meses_do_ano = [
+                             '1' => 'JANEIRO'
+                            ,'2' => 'FEVEREIRO'
+                            ,'3' => 'MAR&Ccedil;O'
+                            ,'4' => 'ABRIL'
+                            ,'5' => 'MAIO'
+                            ,'6' => 'JUNHO'
+                            ,'7' => 'JULHO'
+                            ,'8' => 'AGOSTO'
+                            ,'9' => 'SETEMBRO'
+                            ,'10' => 'OUTUBRO'
+                            ,'11' => 'NOVEMBRO'
+                            ,'12' => 'DEZEMBRO'
+                        ];
 
-    var $meses_do_ano = array(
-                             "1" => "JANEIRO"
-                            ,"2" => "FEVEREIRO"
-                            ,"3" => "MAR&Ccedil;O"
-                            ,"4" => "ABRIL"
-                            ,"5" => "MAIO"
-                            ,"6" => "JUNHO"
-                            ,"7" => "JULHO"
-                            ,"8" => "AGOSTO"
-                            ,"9" => "SETEMBRO"
-                            ,"10" => "OUTUBRO"
-                            ,"11" => "NOVEMBRO"
-                            ,"12" => "DEZEMBRO"
-                        );
+    public $total_dias_uteis;
 
-    var $total_dias_uteis;
-
-
-    function Inicializar()
+    public function Inicializar()
     {
-
-        $retorno = "Novo";
-
+        $retorno = 'Novo';
 
         $obj_permissoes = new clsPermissoes();
-        if($obj_permissoes->nivel_acesso($this->pessoa_logada) > 7){
+        if ($obj_permissoes->nivel_acesso($this->pessoa_logada) > 7) {
             $this->simpleRedirect('index.php');
         }
 
         return $retorno;
     }
 
-    function Gerar()
+    public function Gerar()
     {
-
         $obj_permissoes = new clsPermissoes();
         $nivel_usuario = $obj_permissoes->nivel_acesso($this->pessoa_logada);
 
-        if($_POST){
+        if ($_POST) {
             foreach ($_POST as $key => $value) {
                 $this->$key = $value;
-
             }
         }
 
+        $this->ano = $ano_atual = date('Y');
+        $this->mes = $mes_atual = date('n');
 
-
-        $this->ano = $ano_atual = date("Y");
-        $this->mes = $mes_atual = date("n");
-
-        $this->campoLista( "mes", "M&ecirc;s",$this->meses_do_ano, $this->mes,"",false );
+        $this->campoLista('mes', 'M&ecirc;s', $this->meses_do_ano, $this->mes, '', false);
 //      $this->campoLista( "ano", "Ano",$anos, $this->ano,"",false );
 
-        $this->campoNumero( "ano", "Ano", $this->ano, 4, 4, true );
+        $this->campoNumero('ano', 'Ano', $this->ano, 4, 4, true);
 
         $get_escola = true;
         $obrigatorio = true;
@@ -105,38 +96,28 @@ class indice extends clsCadastro
 
         $this->ref_cod_escola = $obj_permissoes->getEscola($this->pessoa_logada);
         $this->ref_cod_instituicao = $obj_permissoes->getInstituicao($this->pessoa_logada);
-        include("include/pmieducar/educar_campo_lista.php");
-        $this->campoRotulo("cursos_","Cursos","<div id='cursos'>Selecione uma escola</div>");
+        include('include/pmieducar/educar_campo_lista.php');
+        $this->campoRotulo('cursos_', 'Cursos', '<div id=\'cursos\'>Selecione uma escola</div>');
 
-        if($nivel_usuario <=3)
-        {
-
-            echo "<script>
-                    window.onload = function(){document.getElementById('ref_cod_escola').onchange = changeCurso};
-                  </script>";
-
-        }
-        else
-        {
-
-            echo "<script>
+        if ($nivel_usuario <=3) {
+            echo '<script>
+                    window.onload = function(){document.getElementById(\'ref_cod_escola\').onchange = changeCurso};
+                  </script>';
+        } else {
+            echo '<script>
                     window.onload = function(){ changeCurso() };
-                  </script>";
+                  </script>';
         }
 
-    //  if($this->get_link)
+        //  if($this->get_link)
         //  $this->campoRotulo("rotulo11", "-", "<a href='$this->get_link' target='_blank'>Baixar Relatório</a>");
 
-        $this->url_cancelar = "educar_index.php";
-        $this->nome_url_cancelar = "Cancelar";
+        $this->url_cancelar = 'educar_index.php';
+        $this->nome_url_cancelar = 'Cancelar';
 
         $this->acao_enviar = 'acao2()';
         $this->acao_executa_submit = false;
-
-
-
     }
-
 }
 
 // cria uma extensao da classe base
@@ -144,10 +125,9 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
-
 
 ?>
 <script>
