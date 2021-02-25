02090399d209cf7d6d6aa2ca8c615e67bfbb6485
jrbarros <joalissonricardo@gmail.com>
2021-02-24 22:41:05 -0300

    Fix seção religião

--- ieducar/intranet/educar_religiao_lst.php
+++ ieducar/intranet/educar_religiao_lst.php
@@ -1,12 +1,11 @@
 <?php
 
-
 class clsIndexBase extends clsBase
 {
-    function Formular()
+    public function Formular()
     {
-        $this->SetTitulo( "{$this->_instituicao} i-Educar - Religiao" );
-        $this->processoAp = "579";
+        $this->SetTitulo("{$this->_instituicao} i-Educar - Religiao");
+        $this->processoAp = '579';
     }
 }
 
@@ -17,64 +16,61 @@ class indice extends clsListagem
      *
      * @var int
      */
-    var $pessoa_logada;
+    public $pessoa_logada;
 
     /**
      * Titulo no topo da pagina
      *
      * @var int
      */
-    var $titulo;
+    public $titulo;
 
     /**
      * Quantidade de registros a ser apresentada em cada pagina
      *
      * @var int
      */
-    var $limite;
+    public $limite;
 
     /**
      * Inicio dos registros a serem exibidos (limit)
      *
      * @var int
      */
-    var $offset;
+    public $offset;
 
-    var $cod_religiao;
-    var $ref_usuario_exc;
-    var $ref_usuario_cad;
-    var $nm_religiao;
-    var $data_cadastro;
-    var $data_exclusao;
-    var $ativo;
+    public $cod_religiao;
+    public $ref_usuario_exc;
+    public $ref_usuario_cad;
+    public $nm_religiao;
+    public $data_cadastro;
+    public $data_exclusao;
+    public $ativo;
 
-    function Gerar()
+    public function Gerar()
     {
-        $this->titulo = "Religiao - Listagem";
+        $this->titulo = 'Religiao - Listagem';
 
-        foreach( $_GET AS $var => $val ) // passa todos os valores obtidos no GET para atributos do objeto
-            $this->$var = ( $val === "" ) ? null: $val;
+        foreach ($_GET as $var => $val) { // passa todos os valores obtidos no GET para atributos do objeto
+            $this->$var = ($val === '') ? null: $val;
+        }
 
-
-
-        $this->addCabecalhos( array(
-            "Nome Religi&atilde;o"
-        ) );
+        $this->addCabecalhos([
+            'Nome Religi&atilde;o'
+        ]);
 
         // Filtros de Foreign Keys
 
-
         // outros Filtros
-        $this->campoTexto( "nm_religiao", "Nome Religi&atilde;o", $this->nm_religiao, 30, 255, false );
-
+        $this->campoTexto('nm_religiao', 'Nome Religi&atilde;o', $this->nm_religiao, 30, 255, false);
 
         // Paginador
         $this->limite = 20;
-        $this->offset = ( $_GET["pagina_{$this->nome}"] ) ? $_GET["pagina_{$this->nome}"]*$this->limite-$this->limite: 0;
+        $this->offset = ($_GET["pagina_{$this->nome}"]) ? $_GET["pagina_{$this->nome}"]*$this->limite-$this->limite: 0;
 
         $obj_religiao = new clsPmieducarReligiao();
-        $obj_religiao->setOrderby( "nm_religiao ASC" );
-        $obj_religiao->setLimite( $this->limite, $this->offset );
+        $obj_religiao->setOrderby('nm_religiao ASC');
+        $obj_religiao->setLimite($this->limite, $this->offset);
 
         $lista = $obj_religiao->lista(
             null,
@@ -89,29 +85,24 @@ class indice extends clsListagem
         $total = $obj_religiao->_total;
 
         // monta a lista
-        if( is_array( $lista ) && count( $lista ) )
-        {
-            foreach ( $lista AS $registro )
-            {
-
-                $this->addLinhas( array(
-                    "<a href=\"educar_religiao_det.php?cod_religiao={$registro["cod_religiao"]}\">{$registro["nm_religiao"]}</a>"
-                ) );
+        if (is_array($lista) && count($lista)) {
+            foreach ($lista as $registro) {
+                $this->addLinhas([
+                    "<a href=\"educar_religiao_det.php?cod_religiao={$registro['cod_religiao']}\">{$registro['nm_religiao']}</a>"
+                ]);
             }
         }
-        $this->addPaginador2( "educar_religiao_lst.php", $total, $_GET, $this->nome, $this->limite );
-
+        $this->addPaginador2('educar_religiao_lst.php', $total, $_GET, $this->nome, $this->limite);
 
         //** Verificacao de permissao para cadastro
         $obj_permissao = new clsPermissoes();
 
-        if($obj_permissao->permissao_cadastra(579, $this->pessoa_logada,3))
-        {
-            $this->acao = "go(\"educar_religiao_cad.php\")";
-            $this->nome_acao = "Novo";
+        if ($obj_permissao->permissao_cadastra(579, $this->pessoa_logada, 3)) {
+            $this->acao = 'go("educar_religiao_cad.php")';
+            $this->nome_acao = 'Novo';
         }
         //**
-        $this->largura = "100%";
+        $this->largura = '100%';
 
         $this->breadcrumb('Listagem de religiões', [
             url('intranet/educar_pessoas_index.php') => 'Pessoas',
@@ -123,7 +114,6 @@ $pagina = new clsIndexBase();
 // cria o conteudo
 $miolo = new indice();
 // adiciona o conteudo na clsBase
-$pagina->addForm( $miolo );
+$pagina->addForm($miolo);
 // gera o html
 $pagina->MakeAll();
-?>
