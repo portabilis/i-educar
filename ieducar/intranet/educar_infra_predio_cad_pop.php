5ac6daaea743f2c135710bc275c8960e1181daf6
jrbarros <joalissonricardo@gmail.com>
2021-02-26 18:43:22 -0300

    Usa arquivo externo infra predio cad

--- ieducar/intranet/educar_infra_predio_cad_pop.php
+++ ieducar/intranet/educar_infra_predio_cad_pop.php
@@ -1,8 +1,6 @@
 <?php
 
-
-return new class extends clsCadastro
-{
+return new class extends clsCadastro {
     /**
      * Referencia pega da session para o idpes do usuario atual
      *
@@ -108,35 +106,22 @@ return new class extends clsCadastro
 
     public function Excluir()
     {
+    }
 
+    public function makeExtra()
+    {
+        if (! $_GET['precisa_lista']) {
+            return file_get_contents(__DIR__ . '/scripts/extra/educar-infra-predio-cad-pop.js');
+        }
+
+        return '';
     }
 
     public function Formular()
     {
-        $this->titulo = "i-Educar - Infra Predio";
+        $this->titulo = 'i-Educar - Infra Predio';
         $this->processoAp = '567';
         $this->renderMenu = false;
         $this->renderMenuSuspenso = false;
     }
 };
-
-
-?>
-<script>
-
-<?php
-
-if (!$_GET['precisa_lista']) {
-    ?>
-    Event.observe(window, 'load', Init, false);
-
-    function Init()
-    {
-        $('ref_cod_escola').value = parent.document.getElementById('ref_cod_escola').value;
-//      alert($F('ref_cod_escola'));
-    }
-
-<?php
-} ?>
-
-</script>
---
+++ ieducar/intranet/scripts/extra/educar-infra-predio-cad-pop.js
@@ -0,0 +1,8 @@
+<script>
+  Event.observe(window, 'load', Init, false);
+
+  function Init()
+  {
+    $('ref_cod_escola').value = parent.document.getElementById('ref_cod_escola').value;
+  }
+</script>
