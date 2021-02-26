035faf5418d8b7b2e5b112bf37be877657645f34
jrbarros <joalissonricardo@gmail.com>
2021-02-26 16:34:37 -0300

    Usa arquivo externo cliente cad

--- ieducar/intranet/educar_cliente_cad.php
+++ ieducar/intranet/educar_cliente_cad.php
@@ -1,9 +1,6 @@
 <?php
 
-
-
-return new class extends clsCadastro
-{
+return new class extends clsCadastro {
     /**
      * Referencia pega da session para o idpes do usuario atual
      *
@@ -356,69 +353,15 @@ return new class extends clsCadastro
 
         return false;
     }
-}
 
-
-?>
-<script>
-document.getElementById('ref_cod_biblioteca').onchange = function()
-{
-    ajaxBiblioteca();
-};
-
-if(document.getElementById('ref_cod_biblioteca').value != '')
-{
-    ajaxBiblioteca();
-}
-
-function ajaxBiblioteca()
-{
-    var campoBiblioteca = document.getElementById('ref_cod_biblioteca').value;
-    var xml_biblioteca = new ajax( requisitaSenha );
-    xml_biblioteca.envia( "educar_biblioteca_xml.php?bib="+campoBiblioteca );
-}
-
-setVisibility('tr_login_', false);
-setVisibility('tr_senha_', false);
-
-function requisitaSenha(xml)
-{
-    var DOM_array = xml.getElementsByTagName( "biblioteca" );
-    var campoBiblioteca = document.getElementById('ref_cod_biblioteca').value;
-
-    if (campoBiblioteca == '')
+    public function makeExtra()
     {
-        setVisibility('tr_login_', false);
-        setVisibility('tr_senha_', false);
-    }
-    else
-    {
-        for( var i = 0; i < DOM_array.length; i++ )
-        {
-            if (DOM_array[i].getAttribute("requisita_senha") == 0)
-            {
-                setVisibility('tr_login_', false);
-                setVisibility('tr_senha_', false);
-                document.getElementById('login_').setAttribute('class', 'geral');
-                document.getElementById('senha_').setAttribute('class', 'geral');
-                document.getElementById('requisita_senha').value = '0';
-            }
-            else if (DOM_array[i].getAttribute("requisita_senha") == 1)
-            {
-                setVisibility('tr_login_', true);
-                setVisibility('tr_senha_', true);
-                document.getElementById('login_').setAttribute('class', 'obrigatorio');
-                document.getElementById('senha_').setAttribute('class', 'obrigatorio');
-                document.getElementById('requisita_senha').value = '1';
-            }
-        }
-
+        return file_get_contents(__DIR__ . '/scripts/extra/educar-cliente-cad.js');
     }
 
     public function Formular()
     {
-        $this->titulo = "i-Educar - Cliente";
+        $this->titulo = 'i-Educar - Cliente';
         $this->processoAp = '603';
     }
 };
-</script>
