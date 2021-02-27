8d9222a8ad264594e3d6c30f1e5dde53730d11b7
jrbarros <joalissonricardo@gmail.com>
2021-02-26 21:38:10 -0300

    Usa arquivo externo pesquisa cliente

--- ieducar/intranet/educar_pesquisa_cliente_lst.php
+++ ieducar/intranet/educar_pesquisa_cliente_lst.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Session;
 
-
-return new class extends clsListagem
-{
+return new class extends clsListagem {
     /**
      * Referencia pega da session para o idpes do usuario atual
      *
@@ -128,41 +126,17 @@ return new class extends clsListagem
         $this->addPaginador2('educar_pesquisa_cliente_lst.php', $total, $_GET, $this->nome, $this->limite);
         $this->largura = '100%';
     }
-}
 
-?>
-<script>
-
-function addVal1( campo, valor, opcao )
-{
-    if ( window.parent.document.getElementById( campo ).type == "select-one" )
+    public function makeExtra()
     {
-        obj                     = window.parent.document.getElementById( campo );
-        novoIndice              = obj.options.length;
-        obj.options[novoIndice] = new Option( opcao );
-        opcao                   = obj.options[novoIndice];
-        opcao.value             = valor;
-        opcao.selected          = true;
-        obj.onchange();
-    }
-    else if ( window.parent.document.getElementById( campo ) )
-    {
-        obj       =  window.parent.document.getElementById( campo );
-        obj.value = valor;
-
+        return file_get_contents(__DIR__ . '/scripts/extra/educar-pesquisa-cliente-lst.js');
     }
 
     public function Formular()
     {
-        $this->titulo = "i-Educar - Cliente";
+        $this->titulo = 'i-Educar - Cliente';
         $this->processoAp = '0';
         $this->renderMenu = false;
         $this->renderMenuSuspenso = false;
     }
 };
-
-function fecha()
-{
-    window.parent.fechaExpansivel('div_dinamico_'+(parent.DOM_divs.length*1-1));
-}
-</script>
