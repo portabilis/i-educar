9aef692132493c4217bd7df51e7bc67fd274ea21
jrbarros <joalissonricardo@gmail.com>
2021-02-26 21:54:15 -0300

    Usa arquivo externo reserva login cadastro

--- ieducar/intranet/educar_reservas_login_cad.php
+++ ieducar/intranet/educar_reservas_login_cad.php
@@ -5,9 +5,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\URL;
 
-
-return new class extends clsCadastro
-{
+return new class extends clsCadastro {
     /**
      * Referencia pega da session para o idpes do usuario atual
      *
@@ -122,89 +120,15 @@ return new class extends clsCadastro
             return false;
         }
     }
-}
 
-
-?>
-<script>
-
-document.getElementById('login_').setAttribute('autocomplete','off');
-document.getElementById('login_').onkeypress = function(e)
-{
-    //IE bug
-    if(!e)
-        e = window.event;
-    if(e.keyCode == 13)
-        document.getElementById('senha_').focus();
-}
-
-document.getElementById('senha_').onkeypress = function(e)
-{
-    //IE bug
-    if(!e)
-        e = window.event;
-    if(e.keyCode == 13)
-        document.getElementById('btn_enviar').focus();
-    else if(e.keyCode == 8)
-        document.getElementById('senha_').value = null;
-}
-
-function pesquisa_cliente()
-{
-    var campoBiblioteca = document.getElementById('ref_cod_biblioteca').value;
-    pesquisa_valores_popless('educar_pesquisa_cliente_lst.php?campo1=ref_cod_cliente&campo2=nm_cliente1&ref_cod_biblioteca='+campoBiblioteca)
-}
-
-setVisibility('tr_login_', false);
-setVisibility('tr_senha_', false);
-setVisibility('tr_nm_cliente', false);
-
-function requisitaSenha(xml_tipo_regime)
-{
-    var campoBiblioteca = document.getElementById('ref_cod_biblioteca').value;
-
-    var DOM_array = xml_tipo_regime.getElementsByTagName( "biblioteca" );
-
-    if (campoBiblioteca == '')
+    public function makeExtra()
     {
-        setVisibility('tr_login_', false);
-        setVisibility('tr_senha_', false);
-        setVisibility('tr_nm_cliente', false);
-    }
-    else
-    {
-        for( var i = 0; i < DOM_array.length; i++ )
-        {
-            if (DOM_array[i].getAttribute("requisita_senha") == 0)
-            {
-                setVisibility('tr_login_', false);
-                setVisibility('tr_senha_', false);
-                setVisibility('tr_nm_cliente', true);
-            }
-            else if (DOM_array[i].getAttribute("requisita_senha") == 1)
-            {
-                setVisibility('tr_nm_cliente', false);
-                setVisibility('tr_login_', true);
-                setVisibility('tr_senha_', true);
-            }
-        }
-
+        return file_get_contents(__DIR__ . '/scripts/extra/educar-reserva-login-cad.js');
     }
 
     public function Formular()
     {
-        $this->titulo = "i-Educar - Reservas";
+        $this->titulo = 'i-Educar - Reservas';
         $this->processoAp = '609';
     }
 };
-
-document.getElementById('ref_cod_biblioteca').onchange = function()
-{
-//  requisitaSenha();
-    var campoBiblioteca = document.getElementById('ref_cod_biblioteca').value;
-
-    var xml_biblioteca = new ajax( requisitaSenha );
-    xml_biblioteca.envia( "educar_biblioteca_xml.php?bib="+campoBiblioteca );
-}
-
-</script>
