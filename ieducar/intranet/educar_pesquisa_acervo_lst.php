017be9379d4731e6df1ea618417b7a2d5bdbc917
jrbarros <joalissonricardo@gmail.com>
2021-02-26 21:33:14 -0300

    Usa arquivo externo pesquisa acervo lista

--- ieducar/intranet/educar_pesquisa_acervo_lst.php
+++ ieducar/intranet/educar_pesquisa_acervo_lst.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Session;
 
-
-return new class extends clsListagem
-{
+return new class extends clsListagem {
     /**
      * Referencia pega da session para o idpes do usuario atual
      *
@@ -141,41 +139,18 @@ return new class extends clsListagem
         $obj_permissoes = new clsPermissoes();
 
         $this->largura = '100%';
+    }
 
+    public function makeExtra()
+    {
+        return file_get_contents(__DIR__ . '/scripts/extra/educar-pesquisa-acervo-lst.js');
     }
 
     public function Formular()
     {
-        $this->titulo = "i-Educar - Obras";
+        $this->titulo = 'i-Educar - Obras';
         $this->processoAp = '598';
         $this->renderMenu = false;
         $this->renderMenuSuspenso = false;
     }
 };
-
-?>
-<script>
-function addSel1( campo, valor, texto )
-{
-    obj = window.parent.document.getElementById( campo );
-    novoIndice = obj.options.length;
-    obj.options[novoIndice] = new Option( texto );
-    opcao = obj.options[novoIndice];
-    opcao.value = valor;
-    opcao.selected = true;
-    setTimeout( "obj.onchange", 100 );
-}
-
-function addVal1( campo,valor )
-{
-
-    obj =  window.parent.document.getElementById( campo );
-    obj.value = valor;
-}
-
-function fecha()
-{
-    window.parent.fechaExpansivel('div_dinamico_'+(parent.DOM_divs.length*1-1));
-    //window.parent.document.forms[0].submit();
-}
-</script>
