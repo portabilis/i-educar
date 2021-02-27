9d0296c5433a2dc38580bc68bd9c8d8cf6f84537
jrbarros <joalissonricardo@gmail.com>
2021-02-26 21:44:43 -0300

    Usa arquivo externo pesquisa obra lista

--- ieducar/intranet/scripts/extra/educar-pesquisa-aluno.js
+++ ieducar/intranet/scripts/extra/educar-pesquisa-aluno.js
@@ -11,10 +11,9 @@
   function fecha()
   {
     window.parent.fechaExpansivel('div_dinamico_'+(parent.DOM_divs.length*1-1));
-    if( window.parent.document.getElementById('passo') )
-  {
-    window.parent.document.getElementById('passo').value = 2;
-    window.parent.document.forms[0].submit();
-
+    if( window.parent.document.getElementById('passo') ) {
+        window.parent.document.getElementById('passo').value = 2;
+        window.parent.document.forms[0].submit();
+     }
   }
 </script>
