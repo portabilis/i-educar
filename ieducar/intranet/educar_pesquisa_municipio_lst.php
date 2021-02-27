c21170c1fed766b8820c50858f7d4344a9594a35
jrbarros <joalissonricardo@gmail.com>
2021-02-26 21:40:21 -0300

    Usa arquivo externo pesquisa município

--- ieducar/intranet/educar_pesquisa_municipio_lst.php
+++ ieducar/intranet/educar_pesquisa_municipio_lst.php
@@ -4,9 +4,7 @@ use App\Models\City;
 use App\Models\State;
 use Illuminate\Support\Facades\Session;
 
-
-return new class extends clsListagem
-{
+return new class extends clsListagem {
     /**
      * Referencia pega da session para o idpes do usuario atual
      *
@@ -99,41 +97,18 @@ return new class extends clsListagem
         $this->addPaginador2('educar_pesquisa_municipio_lst.php', $total, $_GET, $this->nome, $this->limite);
 
         $this->largura = '100%';
+    }
 
+    public function makeExtra()
+    {
+        return file_get_contents(__DIR__ . '/scripts/extra/educar-pesquisa-municipio-lst.js');
     }
 
     public function Formular()
     {
-        $this->titulo = "i-Educar - Municipio";
+        $this->titulo = 'i-Educar - Municipio';
         $this->processoAp = '0';
         $this->renderMenu = false;
         $this->renderMenuSuspenso = false;
     }
 };
-
-?>
-<script>
-function addSel1( campo, valor, texto )
-{
-    obj = window.parent.document.getElementById( campo );
-    novoIndice = obj.options.length;
-    obj.options[novoIndice] = new Option( texto );
-    opcao = obj.options[novoIndice];
-    opcao.value = valor;
-    opcao.selected = true;
-    setTimeout( "obj.onchange", 100 );
-}
-
-function addVal1( campo,valor )
-{
-
-    obj =  window.parent.document.getElementById( campo );
-    obj.value = valor;
-}
-
-function fecha()
-{
-    window.parent.fechaExpansivel('div_dinamico_'+(parent.DOM_divs.length*1-1));
-    //window.parent.document.forms[0].submit();
-}
-</script>
