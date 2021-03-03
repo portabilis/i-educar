c08336acb7ce39cbcff83b0ff658f0ac4b45a339
jrbarros <joalissonricardo@gmail.com>
2021-03-02 21:33:29 -0300

    Move JS para arquivo externo

--- ieducar/intranet/educar_instituicao_cad.php
+++ ieducar/intranet/educar_instituicao_cad.php
@@ -445,47 +445,14 @@ return new class extends clsCadastro {
         $this->simpleRedirect('educar_instituicao_lst.php');
     }
 
+    public function makeExtra()
+    {
+        return file_get_contents(__DIR__ . '/scripts/extra/educar-instituicao-cad.js');
+    }
+
     public function Formular()
     {
         $this->titulo = 'i-Educar - Instituição';
         $this->processoAp = '559';
     }
 };
-
-?>
-<script type="text/javascript">
-
-    let populaOrgaoRegional = data => {
-        $j('#orgao_regional').append(
-            $j('<option/>').text('Selecione').val('')
-        );
-        if (data.orgaos) {
-            $j.each(data.orgaos, function(){
-                $j('#orgao_regional').append(
-                    $j('<option/>').text(this.codigo).val(this.codigo)
-                );
-            });
-        }
-    }
-
-    $j('#ref_sigla_uf').on('change', function(){
-        let sigla_uf = this.value;
-        $j('#orgao_regional').html('');
-        if (sigla_uf) {
-            let parametros = {
-                oper: 'get',
-                resource: 'orgaos_regionais',
-                sigla_uf: sigla_uf
-            };
-            let link = '../module/Api/EducacensoOrgaoRegional';
-            $j.getJSON(link, parametros)
-            .done(populaOrgaoRegional);
-        } else {
-            $j('#orgao_regional').html('<option value="" selected>Selecione uma UF</option>');
-        }
-    });
-
-    $j('#data_base').mask("99/99");
-    $j('#data_fechamento').mask("99/99");
-
-</script>
