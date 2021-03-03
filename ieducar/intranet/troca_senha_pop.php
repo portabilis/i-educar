16fb0a2cf3a35b790afa61409655a4a95afe3ee0
jrbarros <joalissonricardo@gmail.com>
2021-03-03 00:29:58 -0300

    Move JS para arquivo externo.

--- ieducar/intranet/troca_senha_pop.php
+++ ieducar/intranet/troca_senha_pop.php
@@ -87,22 +87,3 @@ return new class extends clsCadastro
         $this->renderMenuSuspenso = false;
     }
 };
-
-?>
-
-<script type="text/javascript">
-function acao2()
-{
-  if ($F('f_senha').length > 7) {
-    if ($F('f_senha') == $F('f_senha2')) {
-      acao();
-    }
-    else {
-      alert('As senhas devem ser iguais');
-    }
-  }
-  else {
-    alert('A sua nova senha deverá conter pelo menos oito caracteres');
-  }
-}
-</script>
