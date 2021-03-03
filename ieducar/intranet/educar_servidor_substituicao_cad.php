bbc779a91ac369d142ca8068668ed0af9d1042bd
jrbarros <joalissonricardo@gmail.com>
2021-03-02 23:41:59 -0300

    Move JS para arquivo externo

--- ieducar/intranet/educar_servidor_substituicao_cad.php
+++ ieducar/intranet/educar_servidor_substituicao_cad.php
@@ -280,17 +280,3 @@ return new class extends clsCadastro
         $this->processoAp = 635;
     }
 };
-
-
-?>
-<script type="text/javascript">
-function acao2()
-{
-  if (document.getElementById('ref_cod_servidor_todos').value == ''){
-    alert("Selecione um servidor substituto!");
-    return false;
-  }
-
-  acao();
-}
-</script>
