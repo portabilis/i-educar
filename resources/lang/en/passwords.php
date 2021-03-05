cca3a2528257d1d205f7122083edd2005cf5c603
Eder Soares <edersoares@me.com>
2021-03-05 16:02:20 -0300

    Aplica style code nas pastas public e resources

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -18,6 +18,6 @@ return [
     'sent' => 'We have emailed your password reset link!',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'user' => 'We can\'t find a user with that email address.',
 
 ];
