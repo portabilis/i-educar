2743959aa9f0fd84d50de18b836cc5b647880ecb
jrbarros <joalissonricardo@gmail.com>
2021-03-17 11:47:43 -0300

    Cria logica para criar turma_serie se for turma multisseriada

--- ieducar/intranet/educar_ano_letivo_modulo_cad.php
+++ ieducar/intranet/educar_ano_letivo_modulo_cad.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\LegacySchoolClass;
+use App\Models\LegacySchoolClassGrade;
 use App\Services\iDiarioService;
+use App\Services\SchoolClass\MultiGradesService;
 
 return new class extends clsCadastro {
     public $pessoa_logada;
@@ -535,6 +538,36 @@ return new class extends clsCadastro {
 
             $this->copiarComponenteCurricularTurma($turmaOrigem['cod_turma'], $turmaDestinoId);
             $this->copiarModulosTurma($turmaOrigem['cod_turma'], $turmaDestinoId, $anoOrigem, $anoDestino);
+
+            if ((int) $turmaOrigem['multiseriada'] === 1) {
+                $this->criarTurmaMultisseriada($turmaOrigem, $turmaDestinoId);
+            }
+        }
+    }
+
+    private function criarTurmaMultisseriada($turmaOrigem, $turmaDestinoId)
+    {
+        /** @var LegacySchoolClassGrade[] $turmasSeries */
+        $turmasSeries = LegacySchoolClassGrade::query()
+            ->where('escola_id', $turmaOrigem['ref_ref_cod_escola'])
+            ->where('turma)id', $turmaOrigem['cod_turma'])
+        ;
+
+        foreach ($turmasSeries as $turmaSerie) {
+            $schoolClassGrade[] = [
+                'escola_id' => $turmaOrigem['ref_ref_cod_escola'],
+                'serie_id' => $turmaSerie->serie_id,
+                'turma_id' => $turmaDestinoId,
+                'boletim_id' => $turmaSerie->boletim_id,
+                'boletim_diferenciado_id' => $turmaSerie->boletim_diferenciado_id,
+            ];
+
+            /** @var LegacySchoolClass $schoolClass */
+            $schoolClass = LegacySchoolClass::query()->find($turmaDestinoId);
+
+            $service = new MultiGradesService();
+
+            $service->storeSchoolClassGrade($schoolClass, $schoolClassGrade);
         }
     }
 
