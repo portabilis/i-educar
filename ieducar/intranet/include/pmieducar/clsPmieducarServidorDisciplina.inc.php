9ef0475061e748fc7d16201625ac0779264dfdcc
Eder Soares <edersoares@me.com>
2021-03-26 11:40:48 -0300

    Permite remover todas as funções

--- ieducar/intranet/include/pmieducar/clsPmieducarServidorDisciplina.inc.php
+++ ieducar/intranet/include/pmieducar/clsPmieducarServidorDisciplina.inc.php
@@ -294,7 +294,7 @@ class clsPmieducarServidorDisciplina extends Model
             is_numeric($this->ref_cod_servidor)) {
             $where = '';
 
-            if (is_array($funcao)) {
+            if (is_array($funcao) && count($funcao)) {
                 $funcao = implode(',', $funcao);
                 $where = "AND ref_cod_funcao in ({$funcao})";
             }
