eb52160ffa0b22834e44bb0039df4378edc53a78
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-03-31 10:00:27 -0300

    Ajusta nome da variavel e usa datas dinâmicas como date().

--- tests/Feature/SequencialEnturmacaoTest.php
+++ tests/Feature/SequencialEnturmacaoTest.php
@@ -13,47 +13,47 @@ class SequencialEnturmacaoTest extends TestCase
 {
     use DatabaseTransactions;
 
-    public function testSequencialEqualsOne()
+    public function testSequencialEqualsOneWhenRelocationDateIsNull()
     {
-        $shollClass = factory(LegacySchoolClass::class)->create();
+        $schoolClass = factory(LegacySchoolClass::class)->create();
         $registration = factory(LegacyRegistration::class)->create([
-            'ref_ref_cod_serie' => $shollClass->ref_ref_cod_serie,
-            'ref_ref_cod_escola' => $shollClass->ref_ref_cod_escola,
-            'ref_cod_curso' => $shollClass->ref_cod_curso,
+            'ref_ref_cod_serie' => $schoolClass->ref_ref_cod_serie,
+            'ref_ref_cod_escola' => $schoolClass->ref_ref_cod_escola,
+            'ref_cod_curso' => $schoolClass->ref_cod_curso,
         ]);
 
-        $sequencialEnturmacao = new SequencialEnturmacao($registration->cod_matricula, $shollClass->cod_turma, '2021-02-10');
+        $sequencialEnturmacao = new SequencialEnturmacao($registration->cod_matricula, $schoolClass->cod_turma, date('Y-m-d'));
         $sequencial = $sequencialEnturmacao->ordenaSequencialNovaMatricula();
         $this->assertEquals(1, $sequencial);
     }
 
-    public function testSequencialEqualsThree()
+    public function testSequencialEqualsThreeWhenRelocationDateIsNull()
     {
-        $shollClass = factory(LegacySchoolClass::class)->create();
+        $schoolClass = factory(LegacySchoolClass::class)->create();
         $registration1 = factory(LegacyRegistration::class)->create([
-            'ref_ref_cod_serie' => $shollClass->ref_ref_cod_serie,
-            'ref_ref_cod_escola' => $shollClass->ref_ref_cod_escola,
-            'ref_cod_curso' => $shollClass->ref_cod_curso,
+            'ref_ref_cod_serie' => $schoolClass->ref_ref_cod_serie,
+            'ref_ref_cod_escola' => $schoolClass->ref_ref_cod_escola,
+            'ref_cod_curso' => $schoolClass->ref_cod_curso,
         ]);
         $registration2 = factory(LegacyRegistration::class)->create([
-            'ref_ref_cod_serie' => $shollClass->ref_ref_cod_serie,
-            'ref_ref_cod_escola' => $shollClass->ref_ref_cod_escola,
-            'ref_cod_curso' => $shollClass->ref_cod_curso,
+            'ref_ref_cod_serie' => $schoolClass->ref_ref_cod_serie,
+            'ref_ref_cod_escola' => $schoolClass->ref_ref_cod_escola,
+            'ref_cod_curso' => $schoolClass->ref_cod_curso,
         ]);
         $registration3 = factory(LegacyRegistration::class)->create([
-            'ref_ref_cod_serie' => $shollClass->ref_ref_cod_serie,
-            'ref_ref_cod_escola' => $shollClass->ref_ref_cod_escola,
-            'ref_cod_curso' => $shollClass->ref_cod_curso,
+            'ref_ref_cod_serie' => $schoolClass->ref_ref_cod_serie,
+            'ref_ref_cod_escola' => $schoolClass->ref_ref_cod_escola,
+            'ref_cod_curso' => $schoolClass->ref_cod_curso,
         ]);
 
         factory(LegacyEnrollment::class)->create([
-            'ref_cod_turma' => $shollClass->cod_turma,
+            'ref_cod_turma' => $schoolClass->cod_turma,
             'ref_cod_matricula' => $registration1->cod_matricula,
             'sequencial_fechamento' => 1
         ]);
 
         factory(LegacyEnrollment::class)->create([
-            'ref_cod_turma' => $shollClass->cod_turma,
+            'ref_cod_turma' => $schoolClass->cod_turma,
             'ref_cod_matricula' => $registration2->cod_matricula,
             'sequencial_fechamento' => 2
         ]);
@@ -61,7 +61,7 @@ class SequencialEnturmacaoTest extends TestCase
         //Garante que o aluno será o último
         $registration3->student->individual->person->nome = 'zzzz';
         $registration3->student->individual->person->save();
-        $sequencialEnturmacao = new SequencialEnturmacao($registration3->cod_matricula, $shollClass->cod_turma, '2021-02-10');
+        $sequencialEnturmacao = new SequencialEnturmacao($registration3->cod_matricula, $schoolClass->cod_turma, date('Y-m-d'));
         $sequencial = $sequencialEnturmacao->ordenaSequencialNovaMatricula();
         $this->assertEquals(3, $sequencial);
     }
