d96b50d5ce3b2ffc30595e8dccadf552124c51aa
jrbarros <joalissonricardo@gmail.com>
2021-04-08 18:51:18 -0300

    Aplica padrão no botão novo em  Registro de alocações do servidor

--- ieducar/intranet/educar_servidor_alocacao_lst.php
+++ ieducar/intranet/educar_servidor_alocacao_lst.php
@@ -150,7 +150,10 @@ return new class extends clsListagem {
         $this->array_botao_url = [];
         if ($obj_permissoes->permissao_cadastra(635, $this->pessoa_logada, 7)) {
             $this->array_botao_url[]= "educar_servidor_alocacao_cad.php?ref_cod_servidor={$this->ref_cod_servidor}&ref_cod_instituicao={$this->ref_cod_instituicao}";
-            $this->array_botao[]= 'Novo';
+            $this->array_botao[] = [
+                'name' => 'Novo',
+                'css-extra' => 'btn-green'
+            ];
         }
 
         $this->array_botao[] = 'Voltar';
