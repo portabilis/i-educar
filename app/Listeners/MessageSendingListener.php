84dc267fec6eb9867d5342c89ac8e886898e5b2c
jrbarros <joalissonricardo@gmail.com>
2021-04-14 15:52:10 -0300

    Cria Listener para colocar o header de transactional

---
+++ app/Listeners/MessageSendingListener.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Mail\Events\MessageSending;
+
+class MessageSendingListener
+{
+
+    /**
+     * @param MessageSending $event
+     */
+    public function handle(MessageSending $event)
+    {
+        $event->message
+            ->getHeaders()
+            ->addTextHeader('IsTransactional', 'True');
+    }
+}
