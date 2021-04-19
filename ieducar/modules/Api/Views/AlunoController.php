66f22e88ff710df343f30a57e219da6249304f54
jrbarros <joalissonricardo@gmail.com>
2021-04-19 15:34:37 -0300

    Adiciona um filtro de ativo na busca do histórico escolar.

--- ieducar/modules/Api/Views/AlunoController.php
+++ ieducar/modules/Api/Views/AlunoController.php
@@ -844,7 +844,7 @@ class AlunoController extends ApiCoreController
         $sql = '
             SELECT historico_grade_curso_id as id
             FROM pmieducar.historico_escolar
-            WHERE ref_cod_aluno = $1
+            WHERE ref_cod_aluno = $1 AND ativo = 1
             ORDER BY sequencial DESC LIMIT 1
         ';
 
