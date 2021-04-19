34e2d3cdf51da9cf449d1614c187f51c4771ad4d
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-04-19 17:12:13 -0300

    Ajusta migration que inclui na view de estudantes o campo da modalidade de ensino

--- database/migrations/2021_04_19_164925_add_education_modality_view_export_student.php
+++ database/migrations/2021_04_19_164925_add_education_modality_view_export_student.php
@@ -21,16 +21,19 @@ class AddEducationModalityViewExportStudent extends Migration
 
     private function dropViews()
     {
+        $this->dropView('public.exporter_social_assistance');
         $this->dropView('public.exporter_student');
     }
 
     private function upViews()
     {
         $this->createView('public.exporter_student', '2021-04-19');
+        $this->createView('public.exporter_social_assistance', '2020-05-07');
     }
 
     private function downViews()
     {
         $this->createView('public.exporter_student', '2021-03-17');
+        $this->createView('public.exporter_social_assistance', '2020-05-07');
     }
 }
