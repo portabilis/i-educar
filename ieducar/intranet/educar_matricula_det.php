d46d585c898ba170940f423e46cbecb37e544c16
jrbarros <joalissonricardo@gmail.com>
2021-04-19 11:41:04 -0300

    Remove possibilidade de customizar botão de modalidade de ensino

--- ieducar/intranet/educar_matricula_det.php
+++ ieducar/intranet/educar_matricula_det.php
@@ -306,7 +306,7 @@ return new class extends clsDetalhe {
                 $this->array_botao[] = _cl('matricula.detalhe.enturmar');
                 $this->array_botao_url_script[] = "go(\"educar_matricula_turma_lst.php?ref_cod_matricula={$registro['cod_matricula']}&ano_letivo={$registro['ano']}\")";
 
-                $this->array_botao[] = _cl('Modalidade de ensino');
+                $this->array_botao[] = 'Modalidade de ensino';
                 $this->array_botao_url_script[] = "go(\"educar_matricula_modalidade_ensino.php?ref_cod_matricula={$registro['cod_matricula']}&ref_cod_aluno={$registro['ref_cod_aluno']}\")";
 
                 $this->array_botao[] = 'Abandono';
