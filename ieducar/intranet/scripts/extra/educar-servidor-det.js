787e98ccbd4aff65e48d041b29b0bceea635c41a
jrbarros <joalissonricardo@gmail.com>
2021-04-26 17:47:23 -0300

    Adiciona função de modal e ajax para a api

--- ieducar/intranet/scripts/extra/educar-servidor-det.js
+++ ieducar/intranet/scripts/extra/educar-servidor-det.js
@@ -1,12 +1,75 @@
-
-  function trocaDisplay(id) {
-  var element = document.getElementById(id);
+function trocaDisplay(id) {
+  let element = document.getElementById(id);
   element.style.display = (element.style.display == 'none') ? 'inline' : 'none';
 }
 
-  function popless() {
-  var campoServidor = #cod_servidor;
-    var campoInstituicao = #ref_cod_instituicao;
-    pesquisa_valores_popless('educar_servidor_nivel_cad.php?ref_cod_servidor=' + campoServidor + '&ref_cod_instituicao=' + campoInstituicao, '');
+function popless() {
+  let campoServidor = #cod_servidor;
+  let campoInstituicao = #ref_cod_instituicao;
+  pesquisa_valores_popless('educar_servidor_nivel_cad.php?ref_cod_servidor=' + campoServidor + '&ref_cod_instituicao=' + campoInstituicao, '');
 }
 
+
+function makeDialog (params) {
+  let container = $j('#dialog-container');
+
+  if (container.length < 1) {
+    $j('body').append('<div id="dialog-container" style="width: 400px;"></div>');
+    container = $j('#dialog-container');
+  }
+
+  if (container.hasClass('ui-dialog-content')) {
+    container.dialog('destroy');
+  }
+
+  container.empty();
+  container.html(params.content);
+
+  delete params['content'];
+
+  container.dialog(params);
+}
+
+function modalExcluir(id) {
+
+  let content = 'O histórico de afastamento selecionando será removido. Deseja prosseguir?';
+
+  makeDialog({
+      content: content,
+      title: 'Atenção!',
+      maxWidth: 860,
+      width: 860,
+      close: function () {
+        $j(this).dialog('destroy');
+      },
+      buttons: [{
+        text: 'Sim',
+        click: function () {
+          $j.ajax({
+            url: '/api/employee-withdrawal/' + id,
+            type: 'delete',
+            async: false,
+            processData: false,
+            contentType: false,
+            success: function (dataResponse) {
+              if (dataResponse.error) {
+                messageUtils.error(dataResponse.message);
+              } else {
+                messageUtils.success('Afastamentos do servidor removido com sucesso');
+              }
+            },
+            error: function () {
+              messageUtils.error('Não foi possível remover o afastamento do servidor');
+            }
+          })
+
+          $j(this).dialog('destroy');
+        }
+      }, {
+        text: 'Não',
+        click: function () {
+          $j(this).dialog('destroy');
+        }
+      }]
+    });
+}
