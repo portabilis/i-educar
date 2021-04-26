69dd3242a788a734345be0546733eadae9e4f908
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-04-26 14:58:50 -0300

    Implementa validação ao alterar a data de saida de uma matricula

--- app/Services/RegistrationService.php
+++ app/Services/RegistrationService.php
@@ -272,4 +272,21 @@ class RegistrationService
             $enrollment->save();
         }
     }
+
+    /**
+     * Atualiza a data de saida de uma matrícula
+     *
+     * @param LegacyRegistration $registration
+     * @param DateTime $date
+     * @return LegacyRegistration
+     */
+    public function updateCancelDate(LegacyRegistration $registration, DateTime $date)
+    {
+        $date = $date->format('Y-m-d');
+
+        $registration->data_cancel = $date;
+        $registration->save();
+
+        return $registration;
+    }
 }
