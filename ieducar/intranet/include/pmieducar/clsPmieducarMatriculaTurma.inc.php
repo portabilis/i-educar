873b9bc7e00c2ff6846d2056aad20a3f0e283ab9
jrbarros <joalissonricardo@gmail.com>
2021-04-29 18:14:45 -0300

    Adiciona view_situacao_relatorios e expões campo para o front

--- ieducar/intranet/include/pmieducar/clsPmieducarMatriculaTurma.inc.php
+++ ieducar/intranet/include/pmieducar/clsPmieducarMatriculaTurma.inc.php
@@ -1073,13 +1073,17 @@ class clsPmieducarMatriculaTurma extends Model
         $db = new clsBanco();
         $sql = "SELECT nome,
                     sequencial_fechamento,
-                    ref_cod_matricula
+                    ref_cod_matricula,
+                    relatorio.view_situacao_relatorios.texto_situacao situacao
                 FROM cadastro.pessoa
               INNER JOIN pmieducar.aluno ON (aluno.ref_idpes = pessoa.idpes)
               INNER JOIN pmieducar.matricula ON (matricula.ref_cod_aluno = aluno.cod_aluno)
               INNER JOIN pmieducar.matricula_turma ON (matricula_turma.ref_cod_matricula = matricula.cod_matricula)
-              JOIN pmieducar.escola ON escola.cod_escola = matricula.ref_ref_cod_escola
-              JOIN pmieducar.instituicao ON escola.ref_cod_instituicao = instituicao.cod_instituicao
+              INNER JOIN pmieducar.escola ON escola.cod_escola = matricula.ref_ref_cod_escola
+              INNER JOIN pmieducar.instituicao ON escola.ref_cod_instituicao = instituicao.cod_instituicao
+              INNER JOIN relatorio.view_situacao_relatorios ON (view_situacao_relatorios.cod_matricula = matricula.cod_matricula
+                AND view_situacao_relatorios.cod_turma = matricula_turma.ref_cod_turma
+                AND matricula_turma.sequencial = view_situacao_relatorios.sequencial)
               WHERE matricula.ativo = 1
                 AND matricula_turma.ref_cod_turma = {$codTurma}
                 AND CASE WHEN matricula_turma.ativo = 1 THEN
