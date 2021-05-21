da130b6308d82c19a99b0c59d5c62f6b0eeeecd6
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-05-21 09:06:07 -0300

    Adiciona os valores descritivos para serem usados nos inputs selects

---
+++ src/Modules/School/Model/ActiveSearch.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace iEducar\Modules\School\Model;
+
+use iEducar\Support\DescriptionValue;
+
+class ActiveSearch
+{
+    use DescriptionValue;
+
+    public const ACTIVE_SEARCH_ABANDONMENT_RESULT = 1;
+    public const ACTIVE_SEARCH_IN_PROGRESS_RESULT = 2;
+    public const ACTIVE_SEARCH_WITH_ABSENCE_RESULT = 3;
+    public const ACTIVE_SEARCH_WITHOUT_ABSENCE_RESULT = 4;
+
+    public static function getDescriptiveValues(): array
+    {
+        return [
+            self::ACTIVE_SEARCH_ABANDONMENT_RESULT => 'Abandono',
+            self::ACTIVE_SEARCH_WITH_ABSENCE_RESULT => 'Retorno com ausência justificada',
+            self::ACTIVE_SEARCH_WITHOUT_ABSENCE_RESULT => 'Retorno sem ausência justificada',
+        ];
+    }
+}
