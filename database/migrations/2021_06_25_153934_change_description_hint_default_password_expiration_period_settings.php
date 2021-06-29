14bfdd2ab41b5be7f1aad17df0bb0c5d22b11b50
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-06-29 17:30:08 -0300

    Melhora description e hint do parâmetro

---
+++ database/migrations/2021_06_25_153934_change_description_hint_default_password_expiration_period_settings.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Setting;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeDescriptionHintDefaultPasswordExpirationPeriodSettings extends Migration
+{
+    public function up()
+    {
+        Setting::query()->updateOrInsert([
+            'key' => 'legacy.app.user_accounts.default_password_expiration_period',
+        ], [
+            'description' => 'Quantidade de dias para expiração automática de senhas',
+            'hint' => 'A contagem será efetuada em dias corridos. Se o valor preenchido for zero (0) ou nenhum, não ocorrerá automatização',
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Setting::query()->updateOrInsert([
+            'key' => 'legacy.app.user_accounts.default_password_expiration_period',
+        ], [
+            'description' => 'Dias para expiração de senha',
+            'hint' => '',
+        ]);
+    }
+}
