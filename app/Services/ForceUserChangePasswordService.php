d27e29322b2f054bdc057badd821e7c8fede4a8a
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-07-01 14:53:01 -0300

    Deixa o código mais claro e aplica PSR-12

--- app/Services/ForceUserChangePasswordService.php
+++ app/Services/ForceUserChangePasswordService.php
@@ -7,23 +7,30 @@ use Illuminate\Contracts\Config\Repository;
 
 class ForceUserChangePasswordService
 {
-
     public $config;
 
     public function __construct(Repository $config)
     {
         $this->config = $config;
     }
+
     public function execute(Authenticatable $user)
     {
         $expirationPeriod = $this->config->get('legacy.app.user_accounts.default_password_expiration_period');
 
-        if (empty($expirationPeriod) === false && $user->isAdmin() === false) {
-            $daysGone = $user->getDaysSinceLastPasswordUpdated();
-            if ($daysGone >= $expirationPeriod) {
-                $user->employee->force_reset_password = true;
-                $user->employee->save();
-            }
+        if (empty($expirationPeriod)) {
+            return;
+        }
+
+        if ($user->isAdmin()) {
+            return;
+        }
+
+        $daysGone = $user->getDaysSinceLastPasswordUpdated();
+
+        if ($daysGone >= $expirationPeriod) {
+            $user->employee->force_reset_password = true;
+            $user->employee->save();
         }
     }
 }
