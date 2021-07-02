9f9a0240ba2f769e3d852c0384abea7d63ec207b
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-07-02 11:27:12 -0300

    Valida se a nova senha é igual à senha atual

--- app/Services/ValidateUserPasswordService.php
+++ app/Services/ValidateUserPasswordService.php
@@ -2,22 +2,53 @@
 
 namespace App\Services;
 
+use Illuminate\Contracts\Hashing\Hasher;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 
 class ValidateUserPasswordService
 {
-    public function execute(string $password)
+    const MIN_LENGTH_PASSWORD = 8;
+
+    private $hash;
+
+    public function __construct(Hasher $hash)
     {
-        $this->validate($password);
+        $this->hash = $hash;
     }
 
-    public function validate(string $password)
+    public function execute(string $newPassword, string $oldPassword)
     {
+        try {
+            $this->validate($newPassword, $oldPassword);
+        } catch (ValidationException $ex){
+
+            throw ValidationException::withMessages([
+                'password' => [
+                    'A senha deve conter pelo menos ' .
+                    self::MIN_LENGTH_PASSWORD .
+                    ' caracteres e uma combinação de letras maiúsculas e minúsculas, números e símbolos (!@#$%*).'
+                ],
+                'similar' => [$ex->validator->errors()->first()]
+            ]);
+        } catch (\Exception $ex){
+            throw ValidationException::withMessages([
+                'password' => $ex->getMessage()
+            ]);
+        }
+    }
+
+    public function validate(string $newPassword, string $oldPassword)
+    {
+        if ($this->hash->check($newPassword, $oldPassword)){
+            throw new \Exception('A senha informada foi usada recentemente. Por favor, escolha outra.');
+        }
+
         validator(
-            ['password' => $password],
+            ['password' => $newPassword],
             [
                 'password' => [
-                    Password::min(8)
+                    Password::min(self::MIN_LENGTH_PASSWORD)
                         ->mixedCase()
                         ->letters()
                         ->numbers()
