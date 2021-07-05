081eb6593b4781506bf0d9b17d114563e290a0a3
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-07-05 15:11:24 -0300

    Não considerar avaliações descritivas já que a mesma não possui uma data específica informada pelo cliente, por pertencer a etapa, portanto, seria inviável considerar a data de saída do aluno.

--- app/Rules/CanChangeExitDate.php
+++ app/Rules/CanChangeExitDate.php
@@ -11,32 +11,27 @@ class CanChangeExitDate implements Rule
     use iDiario;
 
     protected $msg;
-    public const ACTIVITY_TYPE_DESCRIPTION = [
+
+    private const ACTIVITY_TYPE_DESCRIPTION = [
         'daily_note' => 'Diário de avaliações',
         'conceptual_exam' => 'Diário de avaliações conceituais',
-        'descriptive_exam' => 'Avaliações descritivas',
         'avaliation_exemption' => 'Dispensa de avaliações',
         'transfer_note' => 'Notas de transferência',
         'complementary_exam' => 'Recuperação de avaliações',
-        'general_descriptive_exam' => 'Avaliação Descritiva Geral',
         'recovery_diary_record_student' => 'Recuperação de etapas',
         'school_term_recovery_diary_record' => 'Diário de avaliações complementares',
         'observation_diary_record' => 'Diário de observações',
     ];
-    /**
-     * Determine if the validation rule passes.
-     *
-     * @param  string  $attribute
-     * @param  mixed  $value
-     * @return bool
-     */
+
+    private const IGNORED_ACTIVITY_TYPES = ['descriptive_exam', 'general_descriptive_exam'];
+
     public function passes($attribute, $value)
     {
         $iDiarioService = $this->getIdiarioService();
         if ($iDiarioService) {
             $studentActivity = $iDiarioService->getStudentActivity($value['student_id'], $value['exit_date']);
+            $studentActivity = $this->removeIgnoredTypes($studentActivity);
             $hasActivity = count($studentActivity['student_activity']) > 0;
-
             if ($hasActivity) {
                 $this->msg = $this->buildHtmlMessage($studentActivity);
                 return false;
@@ -45,14 +40,28 @@ class CanChangeExitDate implements Rule
         return true;
     }
 
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
-    public function message()
+    private function buildHtmlMessage(array $studentActivity)
     {
-        return 'Existem lançamentos no i-Diário: <br><br>'.$this->msg;
+        $html = "";
+        foreach ($studentActivity['student_activity'] as $activityType) {
+            $html .= '<b>' . self::ACTIVITY_TYPE_DESCRIPTION[$activityType->type] . '</b>';
+            if (isset($activityType->disciplines)) {
+                $html .= ': (';
+                $disciplinesName = [];
+                $contDisciplines = count($activityType->disciplines);
+                for ($i = 0; $i < $contDisciplines; $i++) {
+                    $disciplinesName[] = $this->getDisciplineNameById($activityType->disciplines[$i]);
+                    if ($i === 4) {
+                        break;
+                    }
+                }
+                $html .= implode(', ', $disciplinesName);
+                $html .= $contDisciplines > 5 ? ' e outras) <br><br>' : ')<br><br>';
+            } else {
+                $html .= ': Sim <br><br>';
+            }
+        }
+        return $html;
     }
 
     private function getDisciplineNameById($disciplinaId)
@@ -60,33 +69,18 @@ class CanChangeExitDate implements Rule
         return LegacyDiscipline::find($disciplinaId)->nome;
     }
 
-    /**
-     * Exibe detalhes dos tipos de lançamentos que existem no i-Diário
-     * Quando há disciplinas, exibe apenas 5
-     * @param array $studentActivity
-     * @return string
-     */
-    private function buildHtmlMessage(array $studentActivity){
-        $html = "";
-        foreach ($studentActivity['student_activity'] as $activityType) {
-            $html .= '<b>'.self::ACTIVITY_TYPE_DESCRIPTION[$activityType->type].'</b>';
-            if(isset($activityType->disciplines)){
-                $html .= ': (';
-                $disciplinesName = [];
-                $contDisciplines = count($activityType->disciplines);
-                for ($i = 0; $i < $contDisciplines; $i++){
-                    $disciplinesName[] = $this->getDisciplineNameById($activityType->disciplines[$i]);
-                    if($i === 4) {
-                        break;
-                    }
-                }
-                $html .= implode(', ',$disciplinesName);
-                $html .= $contDisciplines > 5 ? ' e outras) <br><br>' : ')<br><br>';
-            } else {
-                $html .= ': Sim <br><br>';
+    private function removeIgnoredTypes(array $studentActivity)
+    {
+        foreach ($studentActivity['student_activity'] as $key => $item) {
+            if (in_array($item->type, ['descriptive_exam', 'general_descriptive_exam'])) {
+                unset($studentActivity['student_activity'][$key]);
             }
         }
+        return $studentActivity;
+    }
 
-        return $html;
+    public function message()
+    {
+        return 'Existem lançamentos no i-Diário: <br><br>'.$this->msg;
     }
 }
