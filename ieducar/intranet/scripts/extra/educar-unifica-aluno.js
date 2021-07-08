ab2eb5559b1aa1cca46742d88ce20aade7d6bdad
bonot <paula_bonot@hotmail.com>
2021-07-08 11:43:38 -0300

    Torna todos itens da lista como clicaveis

--- ieducar/intranet/scripts/extra/educar-unifica-aluno.js
+++ ieducar/intranet/scripts/extra/educar-unifica-aluno.js
@@ -97,13 +97,13 @@ function montaTabelaDadosAluno(response) {
   response.alunos.each(function(aluno, id) {
     html += '<tr id="' + aluno.codigo + '" class="linha_listagem">';
     html += '<td><input onclick="validaCheckAlunoPrincipal(this)" type="checkbox" class="check_principal" id="check_principal_' + aluno.codigo + '"</td>';
-    html += '<td>'+ aluno.codigo +'</td>';
-    html += '<td>'+ aluno.inep +'</td>';
-    html += '<td><a target="_new" href="/intranet/educar_aluno_det.php?cod_aluno=' + aluno.codigo + '">'+ aluno.nome +'</a></td>';
-    html += '<td>'+ aluno.data_nascimento +'</td>';
-    html += '<td>'+ aluno.cpf +'</td>';
-    html += '<td>'+ aluno.rg +'</td>';
-    html += '<td>'+ aluno.mae_aluno +'</td>';
+    html += '<td><a target="_new" href="/module/Cadastro/aluno?id=' + aluno.codigo + '">'+ aluno.codigo +'</a></td>';
+    html += '<td><a target="_new" href="/module/Cadastro/aluno?id=' + aluno.codigo + '">'+ aluno.inep +'</a></td>';
+    html += '<td><a target="_new" href="/module/Cadastro/aluno?id=' + aluno.codigo + '">'+ aluno.nome +'</a></td>';
+    html += '<td><a target="_new" href="/module/Cadastro/aluno?id=' + aluno.codigo + '">'+ aluno.data_nascimento +'</a></td>';
+    html += '<td><a target="_new" href="/module/Cadastro/aluno?id=' + aluno.codigo + '">'+ aluno.cpf +'</a></td>';
+    html += '<td><a target="_new" href="/module/Cadastro/aluno?id=' + aluno.codigo + '">'+ aluno.rg +'</a></td>';
+    html += '<td><a target="_new" href="/module/Cadastro/aluno?id=' + aluno.codigo + '">'+ aluno.mae_aluno +'</a></td>';
     html += '<td><a onclick="visualizarDadosAlunos(' + aluno.codigo + ', \'' + aluno.nome + '\')">Visualizar</a></td>';
     html += '<td><a class="link_remove" onclick="removeAluno(' + aluno.codigo + ')">EXCLUIR</a></td>';
     html += '</tr>';
