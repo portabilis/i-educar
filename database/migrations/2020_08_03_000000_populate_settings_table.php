08120a3ad09f49f953ed24f53e13b6239854793a
Marcos Oliveira Soares <marcosoliveirasoares94@gmail.com>
2021-07-09 17:38:55 -0300

    Removendo created_at e updated_at pois é adicionado pelo Model.

--- database/migrations/2020_08_03_000000_populate_settings_table.php
+++ database/migrations/2020_08_03_000000_populate_settings_table.php
@@ -118,10 +118,6 @@ class PopulateSettingsTable extends Migration
                 'key' => $key,
             ], [
                 'value' => $value,
-            ], [
-                'created_at' => date('Y-m-d H:i:s'),
-            ], [
-                'updated_at' => date('Y-m-d H:i:s'),
             ]);
         });
     }
