eb062dd6eb92dc95a53b24701838f43f17efbd79
Joalisson Barros <joalissonricardo@gmail.com>
2021-07-12 07:56:42 -0300

    Corrige label

--- ieducar/modules/Cadastro/Assets/Javascripts/Escola.js
+++ ieducar/modules/Cadastro/Assets/Javascripts/Escola.js
@@ -195,7 +195,7 @@ function changePossuiDependencias() {
     $j("#salas_gerais,#salas_funcionais,#banheiros,#laboratorios,#salas_atividades,#dormitorios,#areas_externas").trigger("chosen:updated");
 }
 
-const link = '<span> Caso não encontre a pessoa jurídica, cadastre em </span><a href="empresas_cad.php" target="_blank">Pessoas > Cadastros > Pessoa jurídica.</a>';
+const link = '<span> Caso não encontre a pessoa jurídica, cadastre em </span><a href="empresas_cad.php" target="_blank">Pessoas > Cadastros > Pessoas jurídicas.</a>';
 $j('#pessoaj_idpes').after(link);
 
 //abas
