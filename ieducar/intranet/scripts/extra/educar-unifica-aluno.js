ccf0faaa8dc431071d859562c6268ea3ea19f7ad
bonot <paula_bonot@hotmail.com>
2021-07-26 16:54:29 -0300

    Corrige nome do botao

--- ieducar/intranet/scripts/extra/educar-unifica-aluno.js
+++ ieducar/intranet/scripts/extra/educar-unifica-aluno.js
@@ -222,7 +222,7 @@ function removeTr(codAluno) {
 
 function adicionaBotoes() {
   let htmlBotao = '<input type="button" class="botaolistagem" onclick="voltar();" value="Voltar" autocomplete="off">';
-  htmlBotao += '<input id="unifica_pessoa" type="button" class="botaolistagem" onclick="showConfirmationMessage();" value="Unificar pessoas da lista" autocomplete="off">';
+  htmlBotao += '<input id="unifica_pessoa" type="button" class="botaolistagem" onclick="showConfirmationMessage();" value="Unificar alunos da lista" autocomplete="off">';
   $j('.linhaBotoes td').html(htmlBotao);
 }
 
