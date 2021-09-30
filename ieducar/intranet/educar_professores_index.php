7f66a875032fa8a8559d18673bd76a1d826f590c
Booul <lucas.leite@souunit.com.br>
2021-09-30 10:50:09 -0300

    Página "professores"

---
+++ ieducar/intranet/educar_professores_index.php
@@ -0,0 +1,14 @@
+<?php
+
+return new class {
+    public function RenderHTML()
+    {
+        return '';
+    }
+
+    public function Formular()
+    {
+        $this->title = 'Professores';
+        $this->processoAp = 58;
+    }
+};
