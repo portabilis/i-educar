ebda906fb50ee9e40f921689be619090cd8a7650
Lucas Estevam <lucasferreiraestevam@users.noreply.github.com>
2021-11-23 00:25:05 -0300

    Update app/Console/Commands/DisableEmployeeLogin.php
    Co-authored-by: Eder Soares <edersoares@me.com>

--- app/Console/Commands/DisableEmployeeLogin.php
+++ app/Console/Commands/DisableEmployeeLogin.php
@@ -23,16 +23,6 @@ class DisableEmployeeLogin extends Command
     protected $description = 'Inativa login de funcionário pela matricula';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return int
