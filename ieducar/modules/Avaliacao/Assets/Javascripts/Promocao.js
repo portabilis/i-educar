fa4f96e7ec3c6b1718c45b864699c82e5ff327f5
Joalisson Barros <joalissonricardo@gmail.com>
2021-12-06 17:23:46 -0300

    Aplica melhoria estéticas e corrige falha no parâmetro ano

--- ieducar/modules/Avaliacao/Assets/Javascripts/Promocao.js
+++ ieducar/modules/Avaliacao/Assets/Javascripts/Promocao.js
@@ -3,29 +3,29 @@
 processOptions.validatesResourcesAfterSearch = false;
 
 // #DEPRECADO, migrar para novo padrao processOptions
-var PAGE_URL_BASE      = 'promocao';
-var API_URL_BASE       = 'promocaoApi';
-var RESOURCE_NAME      = 'promocao';
-var RESOURCES_NAME     = 'quantidade_matriculas';
-var POST_LABEL         = '';
-var DELETE_LABEL       = '';
-var SEARCH_ORIENTATION = '';
+const PAGE_URL_BASE      = 'promocao';
+const API_URL_BASE       = 'promocaoApi';
+const RESOURCE_NAME      = 'promocao';
+const RESOURCES_NAME     = 'quantidade_matriculas';
+let POST_LABEL           = '';
+let DELETE_LABEL         = '';
+let SEARCH_ORIENTATION   = '';
 
 // funcoes usados pelo modulo Frontend/Process.js
-var onClickSelectAllEvent = false;
-var onClickActionEvent    = false;
-var onClickDeleteEvent    = false;
+let onClickSelectAllEvent = false;
+let onClickActionEvent    = false;
+let onClickDeleteEvent    = false;
 
 //url builders
 
-var setTableSearchDetails = function(){};
+let setTableSearchDetails = function(){};
 
-var postPromocaoMatricula = function(){
-  var $proximoMatriculaIdField = $j('#proximo-matricula-id');
+let postPromocaoMatricula = function(){
+  let $proximoMatriculaIdField = $j('#proximo-matricula-id');
   $proximoMatriculaIdField.data('initial_matricula_id', $proximoMatriculaIdField.val());
 
   if (validatesIfValueIsNumeric($proximoMatriculaIdField.val())) {
-    var options = {
+    let options = {
       url : postResourceUrlBuilder.buildUrl(API_URL_BASE, 'promocao', {matricula_id : $proximoMatriculaIdField.val()}),
       dataType : 'json',
       data : {
@@ -43,9 +43,9 @@ var postPromocaoMatricula = function(){
   }
 };
 
-var deleteOldComponentesCurriculares = function() {
-  var options = {
-    url : deleteResourceUrlBuilder.buildUrl(API_URL_BASE, 'old_componentes_curriculares', {ano_escolar : $j('#ano_escolar').val()}),
+let deleteOldComponentesCurriculares = function() {
+  let options = {
+    url : deleteResourceUrlBuilder.buildUrl(API_URL_BASE, 'old_componentes_curriculares', {ano : $j('#ano').val()}),
     dataType : 'json',
     data : {},
     success : handleDelete
@@ -59,9 +59,9 @@ var deleteOldComponentesCurriculares = function() {
 function handlePostPromocaoMatricula(dataResponse){
   handleMessages(dataResponse.msgs);
 
-  var $proximoMatriculaIdField = $j('#proximo-matricula-id');
+  let $proximoMatriculaIdField = $j('#proximo-matricula-id');
 
-  var $proximaMatricula = ((dataResponse.any_error_msg) ?  (parseInt($proximoMatriculaIdField.val()) + parseInt(1)) : dataResponse.result.proximo_matricula_id);
+  let $proximaMatricula = ((dataResponse.any_error_msg) ?  (parseInt($proximoMatriculaIdField.val()) + parseInt(1)) : dataResponse.result.proximo_matricula_id);
 
   $proximoMatriculaIdField.val($proximaMatricula);
 
@@ -83,30 +83,54 @@ function handleDelete(dataResponse){
 }
 
 function handleSearch($resultTable, dataResponse) {
-  var $text = $j('<p />').html('Quantidade de matrículas ativas na rede: ' +
-                              dataResponse.quantidade_matriculas + '<br />');
 
-  $j('<input />').attr('type', 'checkbox').attr('id', 'continuar-processo').attr('name', 'continuar-processo').appendTo($text);
-  $j('<span />').html('Continuar processo <br />').appendTo($text);
+  let $text = $j('<div />');
 
-  $j('<span />').html('proxima matricula:').appendTo($text);
-  $j('<input />').attr('type', 'text').attr('name', 'proximo-matricula-id').attr('id', 'proximo-matricula-id').val('0').appendTo($text);
+  $j('<span />')
+    .html('Quantidade de matrículas ativas na rede : ' + '<b>' + dataResponse.quantidade_matriculas + '<b><br />')
+    .attr('class','qnt-matriculas')
+    .appendTo($text);
+
+  $j('<span />')
+    .html('Próxima matrícula:')
+    .attr('class','qnt-matriculas')
+    .appendTo($text);
+
+  $j('<input />').attr('type', 'text')
+    .attr('name', 'proximo-matricula-id')
+    .attr('id', 'proximo-matricula-id')
+    .attr('class','proximo-matricula')
+    .val('0').appendTo($text);
 
   $j('<br />').appendTo($text);
 
-  $j('<a />').attr('id', 'promover-matricula')
+  $j('<input />')
+    .attr('type', 'checkbox')
+    .attr('id', 'continuar-processo')
+    .attr('name', 'continuar-processo')
+    .attr('class','continuar-processo')
+    .appendTo($text);
+
+  $j('<span />')
+    .html('Continuar processo <br />')
+    .attr('class','qnt-matriculas')
+    .appendTo($text);
+
+  $j('<input />').attr('id', 'promover-matricula')
             .attr('href', '#')
-            .html('Iniciar processo')
-            .attr('style', 'text-decoration:underline')
+            .attr('type','button')
+            .attr('class','btn-green')
+            .attr('value','Iniciar processo')
             .bind('click', postPromocaoMatricula)
             .appendTo($text);
 
   $j('<span />').html(' ').appendTo($text);
 
-  $j('<a />').attr('id', 'delete-old-componentes-curriculares')
+  $j('<input />').attr('id', 'delete-old-componentes-curriculares')
             .attr('href', '#')
-            .html('Limpar antigos componentes curriculares')
-            .attr('style', 'text-decoration:underline')
+            .attr('type','button')
+            .attr('class','btn-danger')
+            .attr('value','Limpar antigos componentes curriculares')
             .bind('click', deleteOldComponentesCurriculares)
             .appendTo($text);
 
