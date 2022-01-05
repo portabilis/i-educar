b51321b57c14bf5e878e9c5d35356a9edf37e1cf
Joalisson Barros <joalissonricardo@gmail.com>
2022-01-05 14:25:46 -0300

    Cria rule para validação de regra

--- app/Rules/IncompatibleChangeToMultiGrades.php
+++ app/Rules/IncompatibleChangeToMultiGrades.php
@@ -7,18 +7,19 @@ use Illuminate\Contracts\Validation\Rule;
 
 class IncompatibleChangeToMultiGrades implements Rule
 {
+
     public function passes($attribute, $value): bool
     {
-        $grades = [];
+        $series = [];
         foreach ($value[1] as $item) {
-            $grades[] = $item['serie_id'];
+            $series[] = $item['serie_id'];
         }
 
         /** @var LegacySchoolClass $legacySchoolClass */
         $legacySchoolClass = $value[0];
 
-        $changeToMulti = (int) $legacySchoolClass->multiseriada === 1 && $legacySchoolClass->originalMultiGradesInfo === 0;
-        $notFindOriginalGrade = ! in_array($legacySchoolClass->originalGrade, $grades);
+        $changeToMulti = $legacySchoolClass->multiseriada === true && $legacySchoolClass->originalMultiGradesInfo === 0;
+        $notFindOriginalGrade = ! in_array($legacySchoolClass->originalSerie, $series);
         $containsActiveEnrollments = $legacySchoolClass->getTotalEnrolled() > 0;
 
         if ($changeToMulti && $notFindOriginalGrade && $containsActiveEnrollments) {
@@ -30,6 +31,6 @@ class IncompatibleChangeToMultiGrades implements Rule
 
     public function message(): string
     {
-        return 'Não foi possível alterar a turma para ser multisseriada, pois a série original possui matrículas vinculadas.';
+       return 'Não foi possível alterar a turma para ser multisseriada, pois a série original possui matrículas vinculadas.';
     }
 }
