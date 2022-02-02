11cd4fd80a77bc9430d9d7ff4d3aaff760b86a3c
Booul <lucas.leite@souunit.com.br>
2022-02-02 08:35:01 -0300

    Muda campo para obrigatório
    Campo "Etapa de ensino" no cadastro/edição de turmas deve ser obrigatório

--- ieducar/intranet/educar_turma_cad.php
+++ ieducar/intranet/educar_turma_cad.php
@@ -612,7 +612,7 @@ return new class extends clsCadastro {
         $etapas_educacenso = loadJson('educacenso_json/etapas_ensino.json');
         $etapas_educacenso = array_replace([null => 'Selecione'], $etapas_educacenso);
 
-        $options = ['label' => 'Etapa de ensino', 'resources' => $etapas_educacenso, 'value' => $this->etapa_educacenso, 'required' => false, 'size' => 70,];
+        $options = ['label' => 'Etapa de ensino', 'resources' => $etapas_educacenso, 'value' => $this->etapa_educacenso, 'required' => true, 'size' => 70];
         $this->inputsHelper()->select('etapa_educacenso', $options);
 
         $cursos = loadJson('educacenso_json/cursos_da_educacao_profissional.json');
