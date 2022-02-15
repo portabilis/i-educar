44f6c2ce4db78460466a95ca91212c9099971c68
Lucas Leite <lucas.leite@souunit.com.br>
2022-02-15 20:30:49 -0300

    IED-34 Classe do planejamento pedagógico - Listar

--- ieducar/intranet/include/modules/clsModulesPlanejamentoPedagogico.inc.php
+++ ieducar/intranet/include/modules/clsModulesPlanejamentoPedagogico.inc.php
@@ -26,11 +26,37 @@ class clsModulesPlanejamentoPedagogico extends Model {
         $this->_tabela = "{$this->_schema}planejamento_pedagogico";
 
         $this->_from = "
-            modules.planejamento_pedagogico as pp
+                modules.planejamento_pedagogico as pp
+            JOIN pmieducar.turma t
+                ON (t.cod_turma = pp.ref_cod_turma)
+            JOIN pmieducar.instituicao i
+                ON (i.cod_instituicao = t.ref_cod_instituicao)
+            JOIN pmieducar.escola e
+                ON (e.cod_escola = t.ref_ref_cod_escola)
+            JOIN cadastro.juridica j
+                ON (j.idpes = e.ref_idpes)
+            JOIN pmieducar.curso c
+                ON (c.cod_curso = t.ref_cod_curso)
+            JOIN pmieducar.serie s
+                ON (s.cod_serie = t.ref_ref_cod_serie)
+            JOIN pmieducar.turma_turno u
+                ON (u.id = t.turma_turno_id)
+            LEFT JOIN pmieducar.turma_modulo q
+                ON (q.ref_cod_turma = t.cod_turma AND q.sequencial = 1)
+            LEFT JOIN pmieducar.modulo l
+                ON (l.cod_modulo = q.ref_cod_modulo)
         ";
 
         $this->_campos_lista = $this->_todos_campos = '
-            *
+            pp.id,
+            pp.data_inicial,
+            pp.data_final,
+            i.nm_instituicao AS instituicao,
+            j.fantasia AS escola,
+            c.nm_curso AS curso,
+            s.nm_serie AS serie,
+            t.nm_turma AS turma,
+            u.nome AS turno
         ';
 
         if (is_numeric($id)) {
@@ -74,7 +100,7 @@ class clsModulesPlanejamentoPedagogico extends Model {
         ) {
             $db = new clsBanco();
 
-            $campos = "data_inicial, data_final, ref_cod_turma, ddp, atividades";
+            $campos = "data_inicial, data_final, ref_cod_turma, ddp, atividades, data_cadastro";
             $valores = "'{$this->data_inicial}',
                         '{$this->data_final}',
                         '{$this->turma_id}',
@@ -116,8 +142,101 @@ class clsModulesPlanejamentoPedagogico extends Model {
      * @return array
      */
     public function lista (
-        
+        $int_ano = null,
+        $int_ref_cod_ins = null,
+        $int_ref_cod_escola = null,
+        $int_ref_cod_curso = null,
+        $int_ref_cod_serie = null,
+        $int_ref_cod_turma = null,
+        $int_ref_cod_componente_curricular = null,
+        $int_ref_cod_turno = null,
+        $time_data_inicial = null,
+        $time_data_final = null
     ) {
+        $sql = "
+                SELECT
+                    {$this->_campos_lista}
+                FROM
+                    {$this->_from}
+                ";
+
+        $whereAnd = ' AND ';
+        $filtros = " WHERE TRUE ";
+
+    
+        if (is_numeric($int_ref_cod_ins)) {
+            $filtros .= "{$whereAnd} i.cod_instituicao = '{$int_ref_cod_ins}'";
+            $whereAnd = ' AND ';
+        }
+
+        if (is_numeric($int_ref_cod_escola)) {
+            $filtros .= "{$whereAnd} e.cod_escola = '{$int_ref_cod_escola}'";
+            $whereAnd = ' AND ';
+        }
+
+        if (is_numeric($int_ref_cod_curso)) {
+            $filtros .= "{$whereAnd} c.cod_curso = '{$int_ref_cod_curso}'";
+            $whereAnd = ' AND ';
+        }
+
+        if (is_numeric($int_ref_cod_serie)) {
+            $filtros .= "{$whereAnd} s.cod_serie = '{$int_ref_cod_serie}'";
+            $whereAnd = ' AND ';
+        }
+
+        if (is_numeric($int_ref_cod_turma)) {
+            $filtros .= "{$whereAnd} t.cod_turma = '{$int_ref_cod_turma}'";
+            $whereAnd = ' AND ';
+        }
+        
+        if (is_numeric($int_ref_cod_turno)) {
+            $filtros .= "{$whereAnd} t.turma_turno_id = '{$int_ref_cod_turno}'";
+            $whereAnd = ' AND ';
+        }
+
+        if ($time_data_inicial) {
+            $filtros .= "{$whereAnd} pp.data_inicial >= '{$time_data_inicial}'";
+            $whereAnd = ' AND ';
+        }
+
+        if ($time_data_final) {
+            $filtros .= "{$whereAnd} pp.data_final <= '{$time_data_final}'";
+            $whereAnd = ' AND ';
+        }
+
+        $db = new clsBanco();
+        $countCampos = count(explode(',', $this->_campos_lista));
+        $resultado = [];
+
+        $sql .= $filtros . $this->getOrderby() . $this->getLimite();
+
+        $this->_total = $db->CampoUnico(
+            "SELECT
+                COUNT(0)
+            FROM
+                {$this->_from}
+            {$filtros}"
+        );
+        
+        $db->Consulta($sql);
+
+        if ($countCampos > 1) {
+            while ($db->ProximoRegistro()) {
+                $tupla = $db->Tupla();
+
+                $tupla['_total'] = $this->_total;
+                $resultado[] = $tupla;
+            }
+        } else {
+            while ($db->ProximoRegistro()) {
+                $tupla = $db->Tupla();
+                $resultado[] = $tupla[$this->_campos_lista];
+            }
+        }
+        if (count($resultado)) {
+            return $resultado;
+        }
+
         return false;
     }
 
