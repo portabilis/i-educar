fe971b700fb417bac38b96081d6bf8886f5e393c
Joalisson Barros <joalissonricardo@gmail.com>
2022-02-24 16:21:50 -0300

    Adiciona informações de delecão corretamente

--- app/Services/SchoolClass/SchoolClassService.php
+++ app/Services/SchoolClass/SchoolClassService.php
@@ -124,6 +124,8 @@ class SchoolClassService
         )->validate();
 
         $schoolClass->ativo = 0;
+        $schoolClass->visivel = 0;
+        $schoolClass->data_cadastro = 0;
         $schoolClass->save();
     }
 
