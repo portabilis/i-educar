83019846dfd43e70a42407c6eda861c2d7574faf
Booul <lucas.leite@souunit.com.br>
2022-02-24 16:26:36 -0300

    IED-64 Migração de áreas do conhecimento padrões

---
+++ database/seeders/DefaultModulesAreaConhecimentoTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DefaultModulesAreaConhecimentoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::unprepared(
+            file_get_contents(__DIR__ . '/../sqls/inserts/modules.area_conhecimento.sql')
+        );
+    }
+}
