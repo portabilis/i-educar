fbbdc7a3217237387f11f6d5a73b537d7a141dbc
LKisaki <95627229+LKisaki@users.noreply.github.com>
2022-03-10 10:48:35 -0300

    Cores para domínio tecsis
    Alteração das cores para domínio da Tecsis dinamicamente.

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -1,3 +1,8 @@
+<?php  
+    $baseTecsis = $_SERVER['SERVER_NAME'];
+    $corTecsis = "style='color:rgb(243, 135, 42)'";
+?>
+
 <div id="ieducar-quick-search" class="ieducar-quick-search">
     <h4 class="ieducar-quick-search-title">Busca rápida</h4>
     <quick-search></quick-search>
@@ -6,8 +11,12 @@
 @foreach($menu as $item)
     @if($item->hasLinkInSubmenu())
     <li>
-        <a href="{{ $item->link }}"><i class="fa {{$item->icon}}"></i> <span>{{$item->title}}</span></a>
+        <a href="{{ $item->link }}"><i class="fa {{$item->icon}}" id="iconID"
+            <?php // Icones se adaptam a cor da tecsis
+                if($baseTecsis == "tecsis.tec.br"){echo $corTecsis;}?> 
+                ></i> <span>{{$item->title}}</span></a>
     </li>
     @endif
 @endforeach
 </ul>
+
