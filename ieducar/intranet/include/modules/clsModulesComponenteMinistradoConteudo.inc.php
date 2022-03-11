6793e6df8d95efa642685dd9759af47c02dc9db3
Booul <lucas.leite@souunit.com.br>
2022-03-11 15:52:30 -0300

    Classe para controle do conteúdo do registro de aula

---
+++ ieducar/intranet/include/modules/clsModulesComponenteMinistradoConteudo.inc.php
@@ -0,0 +1,180 @@
+<?php
+
+use iEducar\Legacy\Model;
+
+class clsModulesComponenteMinistradoConteudo extends Model {
+    public $id;
+    public $conteudo_ministrado_id;
+    public $planejamento_aula_conteudo_id;
+
+    public function __construct(
+        $id = null,
+        $conteudo_ministrado_id = null,
+        $planejamento_aula_conteudo_id = null
+    ) {
+        $this->_schema = 'modules.';
+        $this->_tabela = "{$this->_schema}conteudo_ministrado_conteudo";
+
+        $this->_from = "
+            modules.conteudo_ministrado_conteudo as cmc
+        ";
+
+        $this->_campos_lista = $this->_todos_campos = '
+            *
+        ';
+
+        if (is_numeric($id)) {
+            $this->id = $id;
+        }
+
+        if (is_numeric($conteudo_ministrado_id)) {
+            $this->conteudo_ministrado_id = $conteudo_ministrado_id;
+        }
+
+        if (is_numeric($planejamento_aula_conteudo_id)) {
+            $this->planejamento_aula_conteudo_id = $planejamento_aula_conteudo_id;
+        }
+    }
+
+    /**
+     * Cria um novo registro
+     *
+     * @return bool
+     */
+    public function cadastra() {
+        if (is_numeric($this->conteudo_ministrado_id) && is_numeric($this->planejamento_aula_conteudo_id)) {
+            $db = new clsBanco();
+
+            $db->Consulta("
+                INSERT INTO {$this->_tabela}
+                    (conteudo_ministrado_id, planejamento_aula_conteudo_id)
+                VALUES ('{$this->conteudo_ministrado_id}', '{$this->planejamento_aula_conteudo_id}')
+            ");
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Lista relacionamentos entre os conteudos e o plano de aula
+     *
+     * @return array
+     */
+    public function lista($conteudo_ministrado_id) {
+        if (is_numeric($conteudo_ministrado_id)) {
+            $db = new clsBanco();
+            $db->Consulta("
+                SELECT
+                    *
+                FROM
+                    modules.conteudo_ministrado_conteudo as cmc
+                WHERE
+                    cmc.conteudo_ministrado_id = '{$conteudo_ministrado_id}'
+            ");
+
+            while($db->ProximoRegistro()) {
+                $cmc = $db->Tupla();
+
+                $obj = new clsModulesPlanejamentoAulaConteudo($cmc['planejamento_aula_conteudo_id']);
+                $cmc['planejamento_aula_conteudo'] = $obj->detalhe();
+
+                $conteudos[] = $cmc;
+            }
+
+            return $conteudos;
+        }
+
+        return false;
+    }
+
+    /**
+     * Retorna um array com os dados de um registro
+     *
+     * @return array
+     */
+    public function detalhe () {
+        $data = [];
+
+        if (is_numeric($this->id)) {
+            $db = new clsBanco();
+            $db->Consulta("
+                SELECT
+                    {$this->_todos_campos}
+                FROM
+                    {$this->_from}
+                WHERE
+                    cmc.id = {$this->id}
+            ");
+
+            $db->ProximoRegistro();
+            $data = $db->Tupla();
+
+            return $data;
+        }
+
+        return false;
+    }
+
+    /**
+     * Retorna um array com os dados de um registro
+     *
+     * @return array
+     */
+    public function existe () {
+        return false;
+    }
+
+    /**
+     * Exclui um registro
+     *
+     * @return bool
+     */
+    public function excluir () {
+        if (is_numeric($this->conteudo_ministrado_id) && is_numeric($this->planejamento_aula_conteudo_id)) {
+            $db = new clsBanco();
+
+            $db->Consulta("
+                DELETE FROM
+                    {$this->_tabela}
+                WHERE
+                    conteudo_ministrado_id = '{$this->conteudo_ministrado_id}' AND planejamento_aula_conteudo_id = '{$this->planejamento_aula_conteudo_id}'
+            ");
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Retorna array com duas arrays, uma com os conteúdos a serem cadastrados e a outra com os que devem ser removidos
+     *
+     * @return array
+     */
+    public function retornaDiferencaEntreConjuntosConteudos($atuaisConteudos, $novosConteudos) {
+        $resultado = [];
+        $resultado['adicionar'] = $novosConteudos;
+
+        for ($i=0; $i < count($atuaisConteudos); $i++) {
+            $resultado['remover'][] = $atuaisConteudos[$i]['planejamento_aula_conteudo_id']; 
+        }
+        $atuaisConteudos = $resultado['remover'];
+
+        for ($i=0; $i < count($novosConteudos); $i++) {
+            $novo = $novosConteudos[$i];
+
+            for ($j=0; $j < count($atuaisConteudos); $j++) {
+                $atual = $atuaisConteudos[$j];
+
+                if ($novo == $atual) {
+                    unset($resultado['adicionar'][$i]);
+                    unset($resultado['remover'][$j]);
+                }
+            }
+        }
+
+        return $resultado;
+    }
+}
