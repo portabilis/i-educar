1cccc288e790d37989a067ddb8e525f267271d39
Booul <lucas.leite@souunit.com.br>
2022-03-11 15:55:32 -0300

    Correções no input do BNCC

--- ieducar/modules/Cadastro/Assets/Javascripts/BNCC.js
+++ ieducar/modules/Cadastro/Assets/Javascripts/BNCC.js
@@ -4,14 +4,12 @@
             document.getElementById('frequencia').onchange = function () {
                 pegarBNCCFrequencia();
             };
-
-            document.getElementById('campoExperiencia').onchange = function () {
-                pegarBNCCFrequencia();
-            };
         }
 
         if (document.getElementById('ref_cod_turma') && document.getElementById('ref_cod_componente_curricular')) {
             document.getElementById('ref_cod_componente_curricular').onchange = function () {
+                document.getElementById('bncc_chosen').setAttribute('style', 'width: 100%');
+
                 const codTurma = document.getElementById('ref_cod_turma').value
                 const codComponenteCurricular = document.getElementById('ref_cod_componente_curricular').value
 
@@ -23,15 +21,16 @@
                 }
 
                 let url = getResourceUrlBuilder.buildUrl(
-                '/module/Api/BNCC',
-                'bncc_turma',
-                { turma : codTurma, componente_curricular: codComponenteCurricular}
+                    '/module/Api/BNCC',
+                    'bncc_turma',
+                    { turma : codTurma, componente_curricular: codComponenteCurricular }
                 );
 
                 var options = {
                     url      : url,
                     dataType : 'json',
                     success  : function (dataResponse) {
+                        console.log($('#bncc'));
                         $('#bncc').html(
                             (Object.keys(dataResponse.bncc || []).map(key => `<option value='${key}'>${dataResponse.bncc[key]}</option>`)).join()
                         ).trigger('chosen:updated');
@@ -70,7 +69,7 @@
                 }
             };
 
-            getResource(options);      
+            getResource(options);
         }
     });
 })(jQuery);
