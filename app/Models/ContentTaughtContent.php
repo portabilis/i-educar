51e11f5349b91789b5c51b6cb1ac96dc54f5904a
Booul <lucas.leite@souunit.com.br>
2022-03-11 15:42:33 -0300

    Reaproveitar model de registro de aula

--- app/Models/ContentTaughtContent.php
+++ app/Models/ContentTaughtContent.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ContentTaughtBNCC extends Model
+class ContentTaughtContent extends Model
 {
     /**
      * @var string
      */
-    protected $table = 'modules.conteudo_ministrado_bncc';
+    protected $table = 'modules.conteudo_ministrado_conteudo';
 
     /**
      * @var string
