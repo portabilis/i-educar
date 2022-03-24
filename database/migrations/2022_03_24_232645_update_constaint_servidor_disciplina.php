7af5d4460743109f5e1b0281ff7572a7562298e3
Joalisson Barros <joalissonricardo@gmail.com>
2022-03-24 10:19:06 -0300

    Altera constraint servidor_disciplina_pkey

---
+++ database/migrations/2022_03_24_232645_update_constaint_servidor_disciplina.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Support\Database\DropPrimaryKey;
+use App\Support\Database\PrimaryKey;
+use Illuminate\Database\Migrations\Migration;
+
+
+class UpdateConstaintServidorDisciplina extends Migration
+{
+     use DropPrimaryKey;
+     use PrimaryKey;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $colluns = [
+            'ref_cod_disciplina',
+            'ref_ref_cod_instituicao',
+            'ref_cod_servidor',
+            'ref_cod_curso',
+            'ref_cod_funcao'
+        ];
+
+        $this->dropPrimaryKeyIn('pmieducar', 'servidor_disciplina','servidor_disciplina_pkey');
+        $this->createConstraint('pmieducar','servidor_disciplina', $colluns,'servidor_disciplina_pkey');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {}
+}
