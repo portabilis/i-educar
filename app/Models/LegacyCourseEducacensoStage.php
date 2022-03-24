20dce65b1f4461ee6055aebf946b41fa934b4959
Eder Soares <edersoares@me.com>
2022-03-24 14:20:54 -0300

    Permite usar atribuição massiva

--- app/Models/LegacyCourseEducacensoStage.php
+++ app/Models/LegacyCourseEducacensoStage.php
@@ -11,6 +11,14 @@ class LegacyCourseEducacensoStage extends Model
 
     protected $table = 'modules.etapas_curso_educacenso';
 
+    protected $primaryKey = 'curso_id';
+
+    protected $fillable = [
+        'etapa_id', 'curso_id',
+    ];
+
+    public $timestamps = false;
+
     public static function getIdsByCourse(int $course): array
     {
         return static::query()->where('curso_id', $course)->pluck('etapa_id')?->toArray();
