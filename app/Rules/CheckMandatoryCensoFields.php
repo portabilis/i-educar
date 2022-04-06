15d8470b38ee0044c8e5ebfb6f199e3e813ebe2d
Joalisson Barros <joalissonricardo@gmail.com>
2022-04-06 11:19:14 -0300

    Adiciona novas validações

--- app/Rules/CheckMandatoryCensoFields.php
+++ app/Rules/CheckMandatoryCensoFields.php
@@ -315,7 +315,25 @@ class CheckMandatoryCensoFields implements Rule
         }
 
         if (count($estruturaCurricular) > 1 && in_array(3, $estruturaCurricular, true)) {
-            $this->message = "Não é possível informar mais de uma opção no campo: <b>Estrutura curricular</b>, quando a opção: <b>Não se aplica</b> estiver selecionada";
+            $this->message = 'Não é possível informar mais de uma opção no campo: <b>Estrutura curricular</b>, quando a opção: <b>Não se aplica</b> estiver selecionada';
+            return false;
+        }
+
+        $etapaEnsinoCanNotContainsWithFormacaoGeralBasica = [1, 2, 3, 39, 40, 64, 68];
+        if (in_array(1, $estruturaCurricular, true) && in_array((int) $params->tipo_atendimento, $etapaEnsinoCanNotContainsWithFormacaoGeralBasica)) {
+            $this->message = 'Quando o campo: <b>Estrutura curricular</b> for preenchido com: <b>Formação geral básica</b>, o campo: <b>Etapa de ensino</b> deve ser uma das seguintes opções: 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 41, 69, 70, 71, 72, 56, 73, 74 ou 67';
+            return false;
+        }
+
+        $etapaEnsinoCanNotContainsWithItinerarioFormativo = [1, 2, 3, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 39, 40, 41, 56, 64, 68, 69, 70, 72, 73];
+        if (in_array(2, $estruturaCurricular, true) && in_array((int) $params->tipo_atendimento, $etapaEnsinoCanNotContainsWithItinerarioFormativo)) {
+            $this->message = 'Quando o campo: <b>Estrutura curricular</b> for preenchido com: <b>Itinerário formativo</b>, o campo: <b>Etapa de ensino</b> deve ser uma das seguintes opções: 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 71, 74, 67';
+            return false;
+        }
+
+        $etapaEnsinoCanNotContainsWithNaoSeAplica = [1, 2, 3, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 39, 40, 41, 56, 64, 68, 69, 70, 72 , 73];
+        if (in_array(3, $estruturaCurricular, true) && in_array((int) $params->tipo_atendimento, $etapaEnsinoCanNotContainsWithNaoSeAplica)) {
+            $this->message = 'Quando o campo: <b>Estrutura curricular</b> for preenchido com: <b>Não se aplica</b>, o campo: <b>Etapa de ensino</b> deve ser uma das seguintes opções: 1, 2, 3, 24, 39, 40, 64, 68';
             return false;
         }
 
