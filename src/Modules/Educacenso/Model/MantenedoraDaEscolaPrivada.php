2446716dbcf01dc182f4319c80da8ed6c024dd8c
Robert Ferraz <robert@portabilis.com.br>
2022-04-07 08:44:52 -0300

    [Educacenso 2022] Altera labels

--- src/Modules/Educacenso/Model/MantenedoraDaEscolaPrivada.php
+++ src/Modules/Educacenso/Model/MantenedoraDaEscolaPrivada.php
@@ -19,7 +19,7 @@ class MantenedoraDaEscolaPrivada
         return [
             self::GRUPOS_EMPRESARIAIS => 'Empresa, grupos empresariais do setor privado ou pessoa física',
             self::SINDICATOS_TRABALHISTAS => 'Sindicatos de trabalhadores ou patronais, associações ou cooperativas',
-            self::ORGANIZACOES_NAO_GOVERNAMENTAIS => 'Organização não governamental - ONG internacional ou nacional',
+            self::ORGANIZACOES_NAO_GOVERNAMENTAIS => 'Organização não governamental (ONG) - nacional ou internacional',
             self::INSTITUICOES_SIM_FINS_LUCRATIVOS => 'Instituições sem fins lucrativos',
             self::SISTEMA_S => 'Sistema S (Sesi, Senai, Sesc, outros)',
             self::OSCIP => 'Organização da Sociedade Civil de Interesse Público (Oscip)',
