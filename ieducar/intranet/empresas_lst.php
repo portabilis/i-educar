9c8c273d9b21619fe9dc14f8cfcc1908da5691a8
Matheus Reis Teixeira <tteu18@gmail.com>
2022-04-08 11:38:21 -0300

    Correção de titulo na página Empresas

--- ieducar/intranet/empresas_lst.php
+++ ieducar/intranet/empresas_lst.php
@@ -73,7 +73,7 @@ return new class extends clsListagem {
 
     public function Formular()
     {
-        $this->title = 'Empresas!';
+        $this->title = 'Empresas';
         $this->processoAp = 41;
     }
 };
