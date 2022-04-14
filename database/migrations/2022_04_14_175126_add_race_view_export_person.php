f1c41c2eb3233b7bce8f5f382b4e0f58d494a919
Robert Ferraz <robert@portabilis.com.br>
2022-04-14 18:11:51 -0300

    Adiciona campo Raça na seção aluno no exportador de dados

---
+++ database/migrations/2022_04_14_175126_add_race_view_export_person.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Support\Database\AsView;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRaceViewExportPerson extends Migration
+{
+    use AsView;
+
+    public function up()
+    {
+        $this->dropViews();
+        $this->upViews();
+    }
+
+    public function down()
+    {
+        $this->dropViews();
+        $this->downViews();
+    }
+
+    private function dropViews()
+    {
+        $this->dropView('public.exporter_teacher');
+        $this->dropView('public.exporter_social_assistance');
+        $this->dropView('public.exporter_student');
+        $this->dropView('public.exporter_person');
+    }
+
+    private function upViews()
+    {
+        $this->createView('public.exporter_person', '2022-04-14');
+        $this->createView('public.exporter_student', '2021-07-19');
+        $this->createView('public.exporter_social_assistance', '2020-05-07');
+        $this->createView('public.exporter_teacher', '2021-10-13');
+    }
+
+    private function downViews()
+    {
+        $this->createView('public.exporter_person', '2021-04-13');
+        $this->createView('public.exporter_student', '2021-07-19');
+        $this->createView('public.exporter_social_assistance', '2020-05-07');
+        $this->createView('public.exporter_teacher', '2021-10-13');
+    }
+}
