8283163d207adf104ef92b08a4b7709fb4879ef1
Joalisson Barros <joalissonricardo@gmail.com>
2022-04-28 14:13:58 -0300

    Cria migration para atualizar campos de exportação do censo

---
+++ database/migrations/2022_04_28_140857_alter_table_settings_disable_educacenso_export.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Setting;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Setting::query()->updateOrCreate([
+            'key' => 'legacy.educacenso.enable_export',
+            'value' => 0,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Setting::query()->updateOrCreate([
+            'key' => 'legacy.educacenso.enable_export',
+            'value' => 1,
+        ]);
+    }
+};
