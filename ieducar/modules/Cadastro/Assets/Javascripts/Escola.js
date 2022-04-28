b58c4877b20fb4df577729ba0012f3105edcf83d
Joalisson Barros <joalissonricardo@gmail.com>
2022-04-28 10:15:46 -0300

    Adiciona validações

--- ieducar/modules/Cadastro/Assets/Javascripts/Escola.js
+++ ieducar/modules/Cadastro/Assets/Javascripts/Escola.js
@@ -295,6 +295,7 @@ $j(document).ready(function() {
           return false;
       });
       changeLocalFuncionamento();
+      changeNumeroDeSalas();
       changePredioCompartilhadoEscola();
     });
 
@@ -377,7 +378,16 @@ $j(document).ready(function() {
         habilitaCampoEducacaoIndigena();
         habilitaCampoLinguaMinistrada();
         habilitaReservaVagasCotas();
+        obrigraInstrumentosPedagogicos();
       });
+
+
+  function  obrigraInstrumentosPedagogicos() {
+    $j('#instrumentos_pedagogicos').makeUnrequired();
+    if (obrigarCamposCenso) {
+      $j('#instrumentos_pedagogicos').makeRequired();
+    }
+  }
 
   // fix checkboxs
   $j('input:checked').val('on');
