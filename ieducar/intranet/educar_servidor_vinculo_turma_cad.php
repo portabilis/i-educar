fe7dfe55c2b23e975e5660b3b0d2e95b6e6b60dd
Joalisson Barros <joalissonricardo@gmail.com>
2022-05-20 14:46:17 -0300

    Adiciona label hint

--- ieducar/intranet/educar_servidor_vinculo_turma_cad.php
+++ ieducar/intranet/educar_servidor_vinculo_turma_cad.php
@@ -127,6 +127,7 @@ return new class extends clsCadastro {
         $helperOptions = ['objectName' => 'unidades_curriculares'];
         $options = [
             'label' => 'Adicionar campo Unidade(s) curricular(es) que leciona',
+            'label_hint' => 'Campo referente a unidade(s) curricular(es) da turma',
             'size' => 50,
             'required' => false,
             'options' => [
