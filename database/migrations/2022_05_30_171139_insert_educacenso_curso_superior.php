d4814ed40e48035a0b1bd5f6b2e6cc52010368aa
jrbarros <jrbarros@users.noreply.github.com>
2022-06-10 18:36:38 +0000

    Style code

--- database/migrations/2022_05_30_171139_insert_educacenso_curso_superior.php
+++ database/migrations/2022_05_30_171139_insert_educacenso_curso_superior.php
@@ -2,11 +2,8 @@
 
 use App\Models\EducacensoDegree;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
@@ -29,7 +26,6 @@ return new class extends Migration
                 ]
             );
         }
-
     }
 
     /**
