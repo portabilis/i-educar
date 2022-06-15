c3e9720bf6064b73f6e04b61f34080bc3b9ec138
Joalisson Barros <joalissonricardo@gmail.com>
2022-06-15 18:48:20 -0300

    Remove dados disponíveis para exportação

--- app/Models/Exporter/Student.php
+++ app/Models/Exporter/Student.php
@@ -38,8 +38,6 @@ class Student extends Model
             'Códigos' => [
                 'id' => 'ID Pessoa',
                 'student_id' => 'ID Aluno',
-
-                'registration_id' => 'ID Matrícula',
                 'school_id' => 'ID Escola',
                 'school_class_id' => 'ID Turma',
                 'grade_id' => 'ID Série',
@@ -69,12 +67,8 @@ class Student extends Model
                 'school_class' => 'Turma',
                 'grade' => 'Série',
                 'course' => 'Curso',
-                'registration_date' => 'Data da Matrícula',
-                'registration_out' => 'Data de saída da matrícula',
                 'year' => 'Ano',
-                'status_text' => 'Situação da Matrícula',
                 'period' => 'Turno',
-                'school_class_stage' => 'Etapa Educacenso',
             ],
             'Informações' => [
                 'nationality' => 'Nacionalidade',
