f99c6d7348c59c5c6bb90098535e57c7cfa5cee2
Joalisson Barros <joalissonricardo@gmail.com>
2022-06-22 16:01:41 -0300

    Cria migration da nova view de alunos

---
+++ database/migrations/2022_06_22_120616_create_view_student_grouped_registration.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Support\Database\AsView;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    use AsView;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->createView('public.exporter_student_grouped_registration', '2022-06-22');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->dropView('public.exporter_student_grouped_registration');
+    }
+};
