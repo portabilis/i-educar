f71cb557c7c9d14bc7e896a791a9dc863341e38b
Joalisson Barros <joalissonricardo@gmail.com>
2022-06-29 10:56:03 -0300

    Adiciona novo campo ao exportador

--- app/Models/Exporter/Teacher.php
+++ app/Models/Exporter/Teacher.php
@@ -30,6 +30,30 @@ class Teacher extends Model
     }
 
     /**
+     * @return string
+     */
+    public function getLabel()
+    {
+        return 'Professores';
+    }
+
+    /**
+     * @param string $column
+     *
+     * @return string
+     */
+    public function alias($column)
+    {
+        if (empty($this->alias)) {
+            $this->alias = collect($this->getExportedColumnsByGroup())->flatMap(function ($item) {
+                return $item;
+            });
+        }
+
+        return $this->alias->get($column, $column);
+    }
+
+    /**
      * @return array
      */
     public function getExportedColumnsByGroup()
@@ -66,6 +90,7 @@ class Teacher extends Model
                 'course' => 'Curso',
                 'year' => 'Ano',
                 'disciplines.disciplines' => 'Disciplinas',
+                'enrollments' => 'Matrículas',
             ],
             'Informações' => [
                 'phones.phones' => 'Telefones',
@@ -90,29 +115,5 @@ class Teacher extends Model
                 'place.country' => 'País',
             ],
         ];
-    }
-
-    /**
-     * @return string
-     */
-    public function getLabel()
-    {
-        return 'Professores';
-    }
-
-    /**
-     * @param string $column
-     *
-     * @return string
-     */
-    public function alias($column)
-    {
-        if (empty($this->alias)) {
-            $this->alias = collect($this->getExportedColumnsByGroup())->flatMap(function ($item) {
-                return $item;
-            });
-        }
-
-        return $this->alias->get($column, $column);
     }
 }
