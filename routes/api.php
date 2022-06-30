c63e4612f529be73edd433ce894901627651ec22
Joalisson Barros <joalissonricardo@gmail.com>
2022-06-30 15:43:04 -0300

    Adiciona rota de country

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CountryController;
 use App\Http\Controllers\Api\EmployeeWithdrawalController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,7 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'middleware' => 'auth:sanctum',
 ], function () {
-    Route::apiResources([]);
+    Route::apiResources([
+        'country' => CountryController::class,
+    ]);
 });
 
 Route::get('version', 'Api\\VersionController@version');
