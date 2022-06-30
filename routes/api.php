35deaed30a822d2ea0d48fc7856dfb713b2ad545
Joalisson Barros <joalissonricardo@gmail.com>
2022-06-30 15:16:03 -0300

    Adiciona middleware de auth

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group([
+    'middleware' => 'auth:sanctum',
+], function () {
+    // api routes
+});
+
 Route::get('version', 'Api\\VersionController@version');
 
 Route::get('/postal-code/{postalCode}', 'Api\PostalCodeController@search');
