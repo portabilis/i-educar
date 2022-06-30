db6f8c8447776c853c9d0090c371c846cd62fd75
Joalisson Barros <joalissonricardo@gmail.com>
2022-06-30 10:57:08 -0300

    Ajusta campo da view para agrupar por situação

--- database/migrations/2022_06_30_120616_create_view_student_grouped_registration.php
+++ database/migrations/2022_06_30_120616_create_view_student_grouped_registration.php
@@ -15,7 +15,7 @@ return new class extends Migration
     public function up()
     {
         $this->down();
-        $this->createView('public.exporter_student_grouped_registration', '2022-06-22');
+        $this->createView('public.exporter_student_grouped_registration', '2022-06-30');
     }
 
     /**
