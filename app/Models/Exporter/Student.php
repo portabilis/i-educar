2877475c3d9c51c33786fafc10d44ca6fd2f9c27
Joalisson Barros <joalissonricardo@gmail.com>
2022-07-01 14:14:50 -0300

    Corrige mensagem do exportador de alunos

--- app/Models/Exporter/Student.php
+++ app/Models/Exporter/Student.php
@@ -157,7 +157,7 @@ class Student extends Model
 
     public function getDescription()
     {
-        return 'Os dados exportados serão contabilizados por quantidade de alunos(as), agrupando as informações de séries, cursos, turmas e turnos quando o(a) aluno(a) possuir mais de uma matrícula no ano filtrado.';
+        return 'Os dados exportados serão contabilizados por quantidade de alunos(as), agrupando as informações de séries, cursos, turmas quando o(a) aluno(a) possuir mais de uma matrícula para a situação e ano filtrados.';
     }
 
     /**
