c8a8fff71135c93c95410caba49d854050fb20d2
Joalisson Barros <joalissonricardo@gmail.com>
2022-07-05 11:27:44 -0300

    Adiciona middleware para aplicação de header padrão json

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\AcceptJson;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -41,6 +42,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            AcceptJson::class,
             'bindings',
             'throttle:60,1',
         ],
