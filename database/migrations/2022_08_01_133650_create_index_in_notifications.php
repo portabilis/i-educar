e508e81a98f9d75bbce8291a8b0f7097c788344c
Edinei Valdameri <edineivaldameri@gmail.com>
2022-08-01 13:50:51 -0300

    Cria os indices na tabela de notificações

---
+++ database/migrations/2022_08_01_133650_create_index_in_notifications.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $sm = Schema::getConnection()->getDoctrineSchemaManager();
+            $doctrineTable = $sm->listTableDetails('notifications');
+
+            if (! $doctrineTable->hasIndex('notifications_user_id')) {
+                $table->index('user_id', 'notifications_user_id');
+            }
+
+            if (! $doctrineTable->hasIndex('notifications_read_at')) {
+                $table->index('read_at', 'notifications_read_at');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $sm = Schema::getConnection()->getDoctrineSchemaManager();
+            $doctrineTable = $sm->listTableDetails('notifications');
+
+            if ($doctrineTable->hasIndex('notifications_user_id')) {
+                $table->dropIndex('notifications_user_id');
+            }
+            if ($doctrineTable->hasIndex('notifications_read_at')) {
+                $table->dropIndex('notifications_read_at');
+            }
+        });
+    }
+};
