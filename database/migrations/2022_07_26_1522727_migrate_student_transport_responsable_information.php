26496e6ac6796eb524977090372e60c36e3a36b7
Eder Soares <edersoares@me.com>
2022-08-05 11:42:04 -0300

    Não roda dentro de uma transação

--- database/migrations/2022_07_26_1522727_migrate_student_transport_responsable_information.php
+++ database/migrations/2022_07_26_1522727_migrate_student_transport_responsable_information.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
 return new class () extends Migration {
+    public $withinTransaction = false;
+
     public function up(): void
     {
         DB::statement(
