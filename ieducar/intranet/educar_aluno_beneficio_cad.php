6b0c119f27c2c7af32c5d2d549f301abd8cabed6
Edinei Valdameri <edineivaldameri@gmail.com>
2022-08-15 17:47:18 -0300

    Altera a classe clsPmieducarAlunoBeneficio para o model Laravel LegacyBenefit

--- ieducar/intranet/educar_aluno_beneficio_cad.php
+++ ieducar/intranet/educar_aluno_beneficio_cad.php
@@ -63,15 +63,12 @@ return new class extends clsCadastro {
         // text
         $this->campoTexto('nm_beneficio', 'Benefício', $this->nm_beneficio, 30, 255, true);
         $this->campoMemo('desc_beneficio', 'Descrição Benefício', $this->desc_beneficio, 60, 5, false);
-
-        // data
     }
 
     public function Novo()
     {
         $classType = new LegacyBenefit();
         $classType->ref_usuario_cad = $this->pessoa_logada;
-        $classType->ativo = 1;
         $classType->nm_beneficio = $this->nm_beneficio;
         $classType->desc_beneficio = $this->desc_beneficio;
 
