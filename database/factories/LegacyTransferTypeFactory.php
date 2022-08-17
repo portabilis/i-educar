eb3aa8ff6ccfdb9d1bdb1ad94cf400748da4a33f
Edinei Valdameri <edineivaldameri@gmail.com>
2022-08-17 14:20:25 -0300

    Cria a factory necessária

---
+++ database/factories/LegacyTransferTypeFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\LegacyDisciplinaryOccurrenceType;
+use App\Models\LegacyTransferType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\LegacyDisciplinaryOccurrenceType>
+ */
+class LegacyTransferTypeFactory extends Factory
+{
+    protected $model = LegacyTransferType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+
+        ];
+    }
+}
