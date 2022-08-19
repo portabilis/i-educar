c1cc118db917d44eb9ed3c961b624d10059b815d
Edinei Valdameri <edineivaldameri@gmail.com>
2022-08-19 10:19:12 -0300

    Criado o teste unitário para o model LegacyDisciplinaryOccurrenceType

---
+++ tests/Unit/Eloquent/LegacyDisciplinaryOccurrenceTypeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit\Eloquent;
+
+use App\Models\LegacyDisciplinaryOccurrenceType;
+use Tests\EloquentTestCase;
+
+class LegacyDisciplinaryOccurrenceTypeTest extends EloquentTestCase
+{
+    /**
+     * @return string
+     */
+    protected function getEloquentModelName()
+    {
+        return LegacyDisciplinaryOccurrenceType::class;
+    }
+}
+
+
+
