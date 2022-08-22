e0954ae154cfbe4e28317bb6ab92a6ed7b843e0c
Edinei Valdameri <edineivaldameri@gmail.com>
2022-08-22 10:15:39 -0300

    Filtra listagem de ocorrências disciplinares

--- ieducar/intranet/educar_matricula_ocorrencia_disciplinar_lst.php
+++ ieducar/intranet/educar_matricula_ocorrencia_disciplinar_lst.php
@@ -109,6 +109,10 @@ return new class extends clsListagem {
             ->where('ativo', 1)
             ->orderBy('cod_ocorrencia_disciplinar', 'DESC');
 
+        if (is_numeric($this->ref_cod_tipo_ocorrencia_disciplinar)) {
+            $query->where('ref_cod_tipo_ocorrencia_disciplinar', $this->ref_cod_tipo_ocorrencia_disciplinar);
+        }
+
         $result = $query->paginate($this->limite,'*', 'pagina_'.$this->nome);
 
         $lista = $result->items();
