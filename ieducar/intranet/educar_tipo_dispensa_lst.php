750e94d2bb19f7209a88c11f5387ce979670ce7f
Edinei Valdameri <edineivaldameri@gmail.com>
2022-08-22 10:36:13 -0300

    Corrige falha na paginação

--- ieducar/intranet/educar_tipo_dispensa_lst.php
+++ ieducar/intranet/educar_tipo_dispensa_lst.php
@@ -44,12 +44,9 @@ return new class extends clsListagem {
 
         // Paginador
         $this->limite = 20;
-        $this->offset = ($_GET["pagina_{$this->nome}"]) ? $_GET["pagina_{$this->nome}"]*$this->limite-$this->limite: 0;
 
         $query = LegacyExemptionType::query()
             ->where('ativo', 1)
-            ->limit($this->limite)
-            ->offset($this->offset)
             ->orderBy('nm_tipo', 'ASC');
 
         if (is_string($this->nm_tipo)) {
@@ -60,10 +57,10 @@ return new class extends clsListagem {
             $query->where('ref_cod_instituicao', $this->ref_cod_instituicao);
         }
 
-        $result = $query->get();
+        $result = $query->paginate($this->limite,'*', 'pagina_'.$this->nome);
 
-        $lista = $result->toArray();
-        $total = $result->count();
+        $lista = $result->items();
+        $total = $result->total();
 
         // monta a lista
         if (is_array($lista) && count($lista)) {
