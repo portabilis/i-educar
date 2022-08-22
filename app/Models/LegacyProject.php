3159c0e65ba7473b2f7e22e9cf6abdb98275a36e
Edinei Valdameri <edineivaldameri@gmail.com>
2022-08-22 17:04:33 -0300

    Remove o HasFactory do Model

--- app/Models/LegacyProject.php
+++ app/Models/LegacyProject.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class LegacyProject extends Model
 {
-    use HasFactory;
 
     /**
      * @var bool
