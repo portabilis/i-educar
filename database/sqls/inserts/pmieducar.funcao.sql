8d9fc7ea0df89b72eb9d99af767779e804ec1e88
Josenilson Silva <nilsonnegraodeveloper@gmail.com>
2022-08-23 17:24:48 -0300

    feat: scripts sql para inserts na base de implantação

---
+++ database/sqls/inserts/pmieducar.funcao.sql
@@ -0,0 +1,16 @@
+INSERT INTO pmieducar.funcao(cod_funcao, ref_usuario_exc, ref_usuario_cad, nm_funcao, abreviatura, professor, data_cadastro, data_exclusao, ativo, ref_cod_instituicao)
+	VALUES (1, NULL, 1, 'Coordenador', 'Coord', 0, 'now()', NULL, 1, 1);	
+INSERT INTO pmieducar.funcao(cod_funcao, ref_usuario_exc, ref_usuario_cad, nm_funcao, abreviatura, professor, data_cadastro, data_exclusao, ativo, ref_cod_instituicao)
+	VALUES (2, NULL, 1, 'Pedagogo', 'Pedag', 0, 'now()', NULL, 1, 1);
+INSERT INTO pmieducar.funcao(cod_funcao, ref_usuario_exc, ref_usuario_cad, nm_funcao, abreviatura, professor, data_cadastro, data_exclusao, ativo, ref_cod_instituicao)
+	VALUES (3, NULL, 1, 'Serviços Gerais', 'Serv', 0, 'now()', NULL, 1, 1);
+INSERT INTO pmieducar.funcao(cod_funcao, ref_usuario_exc, ref_usuario_cad, nm_funcao, abreviatura, professor, data_cadastro, data_exclusao, ativo, ref_cod_instituicao)
+	VALUES (4, NULL, 1, 'Merendeira', 'Merend', 0, 'now()', NULL, 1, 1);
+INSERT INTO pmieducar.funcao(cod_funcao, ref_usuario_exc, ref_usuario_cad, nm_funcao, abreviatura, professor, data_cadastro, data_exclusao, ativo, ref_cod_instituicao)
+	VALUES (5, NULL, 1, 'Vigilante', 'Vigi', 0, 'now()', NULL, 1, 1);
+INSERT INTO pmieducar.funcao(cod_funcao, ref_usuario_exc, ref_usuario_cad, nm_funcao, abreviatura, professor, data_cadastro, data_exclusao, ativo, ref_cod_instituicao)
+	VALUES (6, NULL, 1, 'Secretário Escolar', 'Secret', 0, 'now()', NULL, 1, 1);
+INSERT INTO pmieducar.funcao(cod_funcao, ref_usuario_exc, ref_usuario_cad, nm_funcao, abreviatura, professor, data_cadastro, data_exclusao, ativo, ref_cod_instituicao)
+	VALUES (7, NULL, 1, 'Administrativo', 'Adm', 0, 'now()', NULL, 1, 1);
+INSERT INTO pmieducar.funcao(cod_funcao, ref_usuario_exc, ref_usuario_cad, nm_funcao, abreviatura, professor, data_cadastro, data_exclusao, ativo, ref_cod_instituicao)
+	VALUES (8, NULL, 1, 'Cuidador', 'Cuid', 0, 'now()', NULL, 1, 1);
