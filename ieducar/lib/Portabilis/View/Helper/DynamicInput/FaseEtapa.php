fa0d0973dc21bb276485b4bb303c84ad3806e57e
Alexia <alexiiianunes@gmail.com>
2022-08-24 21:33:52 -0300

    feat: add período na listagem de etapas - planejamento de aula

--- ieducar/lib/Portabilis/View/Helper/DynamicInput/FaseEtapa.php
+++ ieducar/lib/Portabilis/View/Helper/DynamicInput/FaseEtapa.php
@@ -17,23 +17,37 @@ class Portabilis_View_Helper_DynamicInput_FaseEtapa extends Portabilis_View_Help
         if ($turmaId and empty($resources)) {
             $sql = "
                 SELECT
-                    m.nm_tipo, m.num_etapas
+                    m.nm_tipo, m.num_etapas,
+                    t.data_inicio, t.data_fim
                 FROM
-                    pmieducar.modulo m 
+                    pmieducar.modulo m
                 JOIN pmieducar.turma_modulo t
                     ON (t.ref_cod_modulo = m.cod_modulo)
                 WHERE t.ref_cod_turma = {$turmaId}
+                ORDER BY T.data_inicio ASC
             ";
 
             $db = new clsBanco();
             $db->Consulta($sql);
-            $db->ProximoRegistro();
 
-            $nm_tipo = $db->Campo('nm_tipo');
-            $num_etapas = $db->Campo('num_etapas');
-            
-            for ($i=1; $i < $num_etapas + 1; $i++) { 
-                $resources[$i] = $i . 'º ' . $nm_tipo;
+            $data = [];
+
+            while($db->ProximoRegistro()) {
+                $data[] = [
+                    'nm_tipo' => $db->Campo('nm_tipo'),
+                    'num_etapas' => $db->Campo('num_etapas'),
+                    'data_inicio' => $db->Campo('data_inicio'),
+                    'data_fim' => $db->Campo('data_fim'),
+                ];
+            }
+
+            $nm_tipo = $data[0]['nm_tipo'];
+            $num_etapas = $data[0]['num_etapas'];
+            $unidade = 1;
+
+            for ($i=0; $i < $num_etapas; $i++) {
+                $resources[$unidade] = $unidade . 'º ' . $nm_tipo. ' (' . date('d/m/Y', strtotime($data[$i]['data_inicio'])) . ' à ' . date('d/m/Y', strtotime($data[$i]['data_fim'])) . ')';
+                $unidade++;
             }
         }
 
