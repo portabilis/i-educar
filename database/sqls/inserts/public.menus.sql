2475f67a8e8812c4fb89017ef08e070d396ed368
xlcn2 <lucianopeas@gmail.com>
2022-08-31 20:43:05 -0300

    menu

--- database/sqls/inserts/public.menus.sql
+++ database/sqls/inserts/public.menus.sql
@@ -178,3 +178,4 @@ INSERT INTO public.menus VALUES (316, 305, 'AEE', NULL, NULL, NULL, 3, 1, 58, NU
 INSERT INTO public.menus VALUES (317, 316, 'Ficha - AEE', NULL, '/intranet/educar_professores_ficha_aee_lst.php', NULL, 1, 2, 58, NULL, NULL, true, NULL, NULL);
 INSERT INTO public.menus VALUES (318, 316, 'Plano de aula - AEE', NULL, '/intranet/educar_professores_planejamento_de_aula_aee_lst.php', NULL, 2, 2, 58, NULL, NULL, true, NULL, NULL);
 INSERT INTO public.menus VALUES (319, 316, 'Atendimento - AEE', NULL, '/intranet/educar_professores_conteudo_ministrado_aee_lst', NULL, 3, 3, 58, NULL, NULL, true, NULL, NULL);
+INSERT INTO public.menus VALUES (335, 115, 'BNCC', 'BNCC', '/intranet/educar_bncc_lst.php', NULL, 0, 5, 9206, NULL, NULL, true, NULL, NULL);
