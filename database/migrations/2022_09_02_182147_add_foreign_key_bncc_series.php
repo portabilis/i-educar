b51b0066e11687aa5ebb5d37007ccb68425e8066
xlcn2 <lucianopeas@gmail.com>
2022-09-02 18:23:19 -0300

    correção migration bncc series foreign

--- database/migrations/2022_09_02_182147_add_foreign_key_bncc_series.php
+++ database/migrations/2022_09_02_182147_add_foreign_key_bncc_series.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class SeriesBncc extends Migration
+class AddForeignKeyBnccSeries extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,12 @@ class SeriesBncc extends Migration
      */
     public function up()
     {
-        Schema::create('pmieducar.bncc_series', function (Blueprint $table) {
-            $table->id();
-            $table->integer('id_bncc');
-            $table->integer('id_serie');
-          
+        Schema::table('pmieducar.bncc_series', function (Blueprint $table) {
+            $table->foreign('id_bncc')
+                ->references('id')
+                ->on('modules.bncc')
+                ->onDelete('cascade');
         });
-
-     
-
     }
 
     /**
