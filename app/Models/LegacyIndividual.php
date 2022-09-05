61e6b26f0c248aa4edd4974688b29540fd5e8785
Joalisson Barros <joalissonricardo@gmail.com>
2022-09-05 09:51:37 -0300

    Refatora busca por cpf

--- app/Models/LegacyIndividual.php
+++ app/Models/LegacyIndividual.php
@@ -176,16 +176,19 @@ class LegacyIndividual extends Model
     }
 
     /**
-     * @param string $cpf
+     * @param string|int $cpf
      *
-     * @return $this
+     * @return Model|null
      */
-    public static function findByCpf($cpf)
+    public static function findByCpf(string|int $cpf) :? Model
     {
-        $cpf = preg_replace('/[^0-9]/', '', $cpf);
-        $cpf = intval($cpf);
+        $cpf = preg_replace('/\D/', '', $cpf);
 
-        return static::query()->where('cpf', $cpf)->first();
+        if ($cpf === null) {
+            return  null;
+        }
+
+        return static::query()->where('cpf', (int) $cpf)->first();
     }
 
     /**
