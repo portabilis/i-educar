c70860e3d17ef09bfe484bfbbb4d2866e58f78ce
Edinei Valdameri <edineivaldameri@gmail.com>
2022-09-08 13:23:46 -0300

    Cria o Teste do Controller

---
+++ tests/Feature/Http/Controllers/Api/ReligionControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use App\Models\Religion;
+use Database\Factories\ReligionFactory;
+use Tests\ResourceTestCase;
+
+class ReligionControllerTest extends ResourceTestCase
+{
+    protected string $uri = '/api/religion';
+    protected string $model = Religion::class;
+    protected string $factory = ReligionFactory::class;
+
+    public function testIndex(): void
+    {
+        $this->index();
+    }
+
+    public function testStore(): void
+    {
+        $this->store();
+    }
+
+    public function testShow(): void
+    {
+        $this->show();
+    }
+
+    public function testUpdate(): void
+    {
+        $this->update();
+    }
+
+    public function testDelete(): void
+    {
+        $this->destroy();
+    }
+}
