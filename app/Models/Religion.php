f7b0b1c4261c93a42afeade7252826c0abb593aa
Edinei Valdameri <edineivaldameri@gmail.com>
2022-09-12 10:56:01 -0300

    Organiza model

--- app/Models/Religion.php
+++ app/Models/Religion.php
@@ -9,22 +9,11 @@ class Religion extends Model
 {
     use SoftDeletes;
 
-    protected $table = "pmieducar.religiao";
+    protected $table = "pmieducar.religions";
 
     protected $dates = ['deleted_at'];
 
     protected $fillable = [
-        'created_by',
         'name'
     ];
-
-    public function createdBy()
-    {
-        return $this->belongsTo(Individual::class, 'created_by', 'id');
-    }
-
-    public function deletedBy()
-    {
-        return $this->belongsTo(Individual::class, 'deleted_by', 'id');
-    }
 }
