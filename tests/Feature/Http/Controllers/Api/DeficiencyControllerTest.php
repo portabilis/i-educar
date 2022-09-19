d1fa1debe068d2b1872a790a79d8323526e5ff9c
Robert Ferraz <robert@portabilis.com.br>
2022-09-19 09:08:21 -0300

    Adiciona os testes da api e eloquente

---
+++ tests/Feature/Http/Controllers/Api/DeficiencyControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use App\Models\LegacyDeficiency;
+use Database\Factories\LegacyDeficiencyFactory;
+use Tests\ResourceTestCase;
+
+class DeficiencyControllerTest extends ResourceTestCase
+{
+    protected string $uri = '/api/person/deficiency';
+    protected string $model = LegacyDeficiency::class;
+    protected string $factory = LegacyDeficiencyFactory::class;
+
+    public function testIndex(): void
+    {
+        $this->index();
+    }
+
+    public function testStore(): void
+    {
+        $this->store();
+    }
+
+    public function testShow(): void
+    {
+        $this->show();
+    }
+
+    public function testUpdate(): void
+    {
+        $this->update();
+    }
+
+    public function testDelete(): void
+    {
+        $this->destroy();
+    }
+}
