57bf3743048a9d2c30c79209790de51ae590c432
Edinei Valdameri <edineivaldameri@gmail.com>
2022-09-19 16:39:54 -0300

    Altera migration devido erro nos testes

--- database/migrations/2022_09_19_112155_move_fields_custom_labels_to_public_settings.php
+++ database/migrations/2022_09_19_112155_move_fields_custom_labels_to_public_settings.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\LegacyGeneralConfiguration;
 use App\Setting;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -9,28 +10,29 @@ return new class extends Migration
 {
     public function up()
     {
-        $configuracoes = new clsPmieducarConfiguracoesGerais(1);
-        $configuracoes = $configuracoes->detalhe();
+        $configutarion = LegacyGeneralConfiguration::query()->first();
 
-        $this->custom_labels = $configuracoes['custom_labels'];
+        if ($configutarion) {
+            $custom_labels = json_decode($configutarion->custom_labels, true);
 
-        $customLabel = new CustomLabel();
-        $defaults = $customLabel->getDefaults();
+            $customLabel = new CustomLabel();
+            $defaults = $customLabel->getDefaults();
 
-        ksort($defaults);
-        $rotulo = null;
+            ksort($defaults);
+            $rotulo = null;
 
-        foreach ($defaults as $k => $v) {
-            $rotulo2 = explode('.', $k)[0];
+            foreach ($defaults as $k => $v) {
+                $rotulo2 = explode('.', $k)[0];
 
-            $setting = new Setting();
-            $setting->key = $k;
-            $setting->value = $this->custom_labels[$k];
-            $setting->type = 'string';
-            $setting->description = $this->getLabel($v);
-            $setting->setting_category_id = $this->getCategoryId($rotulo2);
-            $setting->hint = $k;
-            $setting->save();
+                $setting = new Setting();
+                $setting->key = $k;
+                $setting->value = $custom_labels[$k];
+                $setting->type = 'string';
+                $setting->description = $this->getLabel($v);
+                $setting->setting_category_id = $this->getCategoryId($rotulo2);
+                $setting->hint = $k;
+                $setting->save();
+            }
         }
     }
 
