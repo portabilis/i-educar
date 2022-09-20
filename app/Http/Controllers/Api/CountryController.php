4500c9adec0a3387498f47f3cecd6a777422a661
Robert Ferraz <robert@portabilis.com.br>
2022-09-20 17:36:16 -0300

    Adiciona testes no país

--- app/Http/Controllers/Api/CountryController.php
+++ app/Http/Controllers/Api/CountryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ResourceController;
 use App\Http\Requests\Api\Addressing\AddressingCountryRequest;
 use App\Models\Country;
 use App\Rules\Addressing\AddressingCountryRule;
+use App\Rules\CountryRestrictOperationRule;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -39,9 +40,19 @@ class CountryController extends ResourceController
         return $this->delete($country, $request);
     }
 
+    public function rules($district, Request $request): array
+    {
+        $accessLevel = $request->user()->getLevel();
+        return [
+            new CountryRestrictOperationRule($accessLevel)
+        ];
+    }
+
     protected function deleteRules(Model $model, Request $request)
     {
+        $accessLevel = $request->user()->getLevel();
         return [
+            new CountryRestrictOperationRule($accessLevel),
             new AddressingCountryRule()
         ];
     }
