6b35f8eace739ce2c27342e63d02229de75bd91a
Joalisson Barros <joalissonricardo@gmail.com>
2022-09-26 18:43:59 -0300

    Adiciona testes na classe de NullOutput

--- tests/Unit/Support/Output/NullOutputTest.php
+++ tests/Unit/Support/Output/NullOutputTest.php
@@ -2,7 +2,34 @@
 
 namespace Tests\Unit\Support\Output;
 
-class NullOutputTest
-{
+use Tests\TestCase;
+use App\Contracts\Output;
+use iEducar\Support\Output\NullOutput;
 
+class NullOutputTest extends TestCase
+{
+    public function testNullOutputImplementInterfaceOutPut()
+    {
+        $this->assertInstanceOf(Output::class, new NullOutput());
+    }
+
+    public function testMethodProgressAdvanceWillReturnVoid()
+    {
+        $this->assertNull((new NullOutput())->progressAdvance());
+    }
+
+    public function testMethodInfoWillReturnVoid()
+    {
+        $this->assertNull((new NullOutput())->info('message'));
+    }
+
+    public function testMethodProgressStartWillReturnVoid()
+    {
+        $this->assertNull((new NullOutput())->progressStart('max'));
+    }
+
+    public function testMethodProgressFinishWillReturnVoid()
+    {
+        $this->assertNull((new NullOutput())->progressFinish());
+    }
 }
