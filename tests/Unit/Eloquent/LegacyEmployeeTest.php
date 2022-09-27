4ac60cd92d5de28ac4ab2ab0dc6748afa1a59eef
Joalisson Barros <joalissonricardo@gmail.com>
2022-09-27 10:11:10 -0300

    Adiciona teste para o model funcionario

---
+++ tests/Unit/Eloquent/LegacyEmployeeTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit\Eloquent;
+
+use App\Models\LegacyEmployee;
+use Tests\EloquentTestCase;
+
+class LegacyEmployeeTest  extends EloquentTestCase
+{
+    /**
+     * @return string
+     */
+    protected function getEloquentModelName()
+    {
+        return LegacyEmployee::class;
+    }
+}
