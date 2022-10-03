07a6e5b31d377badf2e66141d3c0dc1c3a12fcc4
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-03 16:56:09 -0300

    Adiciona testes ao Model State.

--- tests/Unit/Eloquent/StateTest.php
+++ tests/Unit/Eloquent/StateTest.php
@@ -2,16 +2,38 @@
 
 namespace Tests\Unit\Eloquent;
 
+use App\Models\Country;
 use App\Models\State;
+use Database\Factories\StateFactory;
 use Tests\EloquentTestCase;
 
 class StateTest extends EloquentTestCase
 {
+
     /**
      * @return string
      */
     protected function getEloquentModelName()
     {
         return State::class;
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->state = StateFactory::new()->create();
+    }
+
+    public function testRelationshipCountry()
+    {
+        $this->assertInstanceOf(Country::class, $this->state->country);
+    }
+
+    public function testFindByAbbreviation()
+    {
+        $state = State::findByAbbreviation($this->state->abbreviation);
+
+        $this->assertArrayHasKey('abbreviation', $state->toArray());
+        $this->assertEquals($state->abbreviation, $this->state->abbreviation);
     }
 }
