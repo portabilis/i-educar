e943371fbdac871339391c91e2f6702fb97311bd
Joalisson Barros <joalissonricardo@gmail.com>
2022-10-04 11:06:36 -0300

    Cria teste de retorno da estrutura da api

---
+++ tests/Feature/Http/Controllers/Api/VersionControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use Tests\LoginFirstUser;
+use Tests\TestCase;
+
+class VersionControllerTest extends TestCase
+{
+
+    public function testGetVersion()
+    {
+        $this->get('api/version')
+            ->assertSuccessful()
+            ->assertJsonStructure(
+                [
+                    'entity',
+                    'version',
+                    'build'
+                ]
+            );
+    }
+}
