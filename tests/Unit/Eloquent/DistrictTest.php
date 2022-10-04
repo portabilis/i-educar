e328aed4c20b060dedf2efa7def3ca90b769a3a1
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-04 10:08:34 -0300

    Adiciona Teste ao Model District

--- tests/Unit/Eloquent/DistrictTest.php
+++ tests/Unit/Eloquent/DistrictTest.php
@@ -2,7 +2,11 @@
 
 namespace Tests\Unit\Eloquent;
 
+use App\Models\City;
 use App\Models\District;
+use App\Models\State;
+use Database\Factories\CityFactory;
+use Database\Factories\DistrictFactory;
 use Tests\EloquentTestCase;
 
 class DistrictTest extends EloquentTestCase
@@ -13,5 +17,16 @@ class DistrictTest extends EloquentTestCase
     protected function getEloquentModelName()
     {
         return District::class;
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->district = DistrictFactory::new()->create();
+    }
+
+    public function testRelationshipCity()
+    {
+        $this->assertInstanceOf(City::class, $this->district->city);
     }
 }
