9737e95cd9359723082ea76ad1091c2012a14180
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-04 17:22:14 -0300

    Adiciona teste na City

--- tests/Unit/Eloquent/CityTest.php
+++ tests/Unit/Eloquent/CityTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Unit\Eloquent;
 
 use App\Models\City;
+use App\Models\District;
+use Database\Factories\CityFactory;
 use Tests\EloquentTestCase;
 
 class CityTest extends EloquentTestCase
@@ -13,5 +15,17 @@ class CityTest extends EloquentTestCase
     protected function getEloquentModelName()
     {
         return City::class;
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->city = CityFactory::new()->hasDistricts()->create();
+    }
+
+    public function testRelationshipDistricts()
+    {
+        $this->assertCount(1, $this->city->districts);
+        $this->assertInstanceOf(District::class, $this->city->districts->first());
     }
 }
