269bff5e0f89d7346ff07d11d027bd8cd222b939
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-04 14:04:06 -0300

    Organiza fillable

--- app/Models/EmployeeAllocation.php
+++ app/Models/EmployeeAllocation.php
@@ -10,10 +10,26 @@ class EmployeeAllocation extends Model
 
     protected $table = 'pmieducar.servidor_alocacao';
 
-    protected $fillable = ['ativo', 'carga_horaria', 'periodo', 'hora_final',
-        'hora_inicial', 'dia_seaman', 'ano', 'data_admissao', 'ref_ref_cod_instituicao', 'ref_usuario_exc',
-        'ref_usuario_cad', 'ref_cod_escola', 'ref_cod_servidor', 'ref_cod_servidor_funcao', 'ref_cod_funcionario_vinculo', 'hora_atividade',
-        'horas_excedentes', 'data_saida'];
+    protected $fillable = [
+        'ativo',
+        'carga_horaria',
+        'periodo',
+        'hora_final',
+        'hora_inicial',
+        'dia_seaman',
+        'ano',
+        'data_admissao',
+        'ref_ref_cod_instituicao',
+        'ref_usuario_exc',
+        'ref_usuario_cad',
+        'ref_cod_escola',
+        'ref_cod_servidor',
+        'ref_cod_servidor_funcao',
+        'ref_cod_funcionario_vinculo',
+        'hora_atividade',
+        'horas_excedentes',
+        'data_saida'
+    ];
 
     public const CREATED_AT = 'data_cadastro';
     public const UPDATED_AT = 'data_exclusao';
