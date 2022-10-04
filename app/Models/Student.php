f36981927a0f3e629e498d52927a127836ad6a43
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-04 10:26:09 -0300

    Remove model não utilizado

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -21,11 +21,6 @@ class Student extends Model
         return $this->belongsTo(Religion::class);
     }
 
-    public function census()
-    {
-        return $this->hasOne(CensusStudent::class);
-    }
-
     public function registrations()
     {
         return $this->hasMany(Registration::class);
