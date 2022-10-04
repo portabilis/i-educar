3bd02146c96b3afbd372004de2474f7ab928890c
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-04 10:03:58 -0300

    Adiciona Teste ao Model Country

--- tests/Unit/Eloquent/CountryTest.php
+++ tests/Unit/Eloquent/CountryTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Unit\Eloquent;
 
 use App\Models\Country;
+use App\Models\State;
+use Database\Factories\CountryFactory;
 use Tests\EloquentTestCase;
 
 class CountryTest extends EloquentTestCase
@@ -14,4 +16,17 @@ class CountryTest extends EloquentTestCase
     {
         return Country::class;
     }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->country = CountryFactory::new()->hasStates()->create();
+    }
+
+    public function testRelationshipStates()
+    {
+        $this->assertCount(1, $this->country->states);
+        $this->assertInstanceOf(State::class, $this->country->states->first());
+    }
+
 }
