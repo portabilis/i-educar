c7e88fe62c43295fc42c23dd9b46e0a18f620b5c
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-04 13:35:31 -0300

    Adiciona teste EmployeeTest

---
+++ tests/Unit/Eloquent/EmployeeTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\Eloquent;
+
+use App\Models\Employee;
+use App\Models\EmployeeInep;
+use Database\Factories\EmployeeFactory;
+use Database\Factories\EmployeeInepFactory;
+use Tests\EloquentTestCase;
+
+class EmployeeTest extends EloquentTestCase
+{
+    /**
+     * @return string
+     */
+    protected function getEloquentModelName()
+    {
+        return Employee::class;
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->employee = EmployeeFactory::new()->create();
+        $this->employee->inep = EmployeeInepFactory::new()->create([
+            'cod_servidor' => $this->employee->cod_servidor,
+        ]);
+    }
+
+    public function testRelationshipInep()
+    {
+        $this->assertInstanceOf(EmployeeInep::class, $this->employee->inep);
+    }
+}
