f42ed3b9366c96e8bef50fb940e99a190edb1c5f
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-05 09:49:49 -0300

    Adiciona Teste de relacionamento

--- tests/Unit/Eloquent/EmployeePosgraduateTest.php
+++ tests/Unit/Eloquent/EmployeePosgraduateTest.php
@@ -2,11 +2,19 @@
 
 namespace Tests\Unit\Eloquent;
 
+use App\Models\Employee;
 use App\Models\EmployeePosgraduate;
 use Tests\EloquentTestCase;
 
 class EmployeePosgraduateTest extends EloquentTestCase
 {
+    /**
+     * @var array
+     */
+    protected $relations = [
+        'employee' => Employee::class,
+    ];
+
     /**
      * @return string
      */
