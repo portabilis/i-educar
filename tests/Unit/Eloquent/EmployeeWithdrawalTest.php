f94a5496b3b3f9b6c5e6403f86e7dfa75532d472
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-05 12:40:02 -0300

    Adiciona teste de relacionamentos no model EmployeeWithdrawal

--- tests/Unit/Eloquent/EmployeeWithdrawalTest.php
+++ tests/Unit/Eloquent/EmployeeWithdrawalTest.php
@@ -6,12 +6,18 @@ use App\Models\Employee;
 use App\Models\EmployeeInep;
 use App\Models\EmployeeWithdrawal;
 use App\Models\LegacyDiscipline;
+use App\Models\WithdrawalReason;
 use Database\Factories\EmployeeFactory;
 use Database\Factories\EmployeeInepFactory;
 use Tests\EloquentTestCase;
 
 class EmployeeWithdrawalTest extends EloquentTestCase
 {
+    protected $relations = [
+        'employee' => Employee::class,
+        'reason' => WithdrawalReason::class
+    ];
+
     /**
      * @return string
      */
