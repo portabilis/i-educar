2048e22599f5c481bf7306011523ede31928a1f9
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-05 09:36:58 -0300

    Corrige Factory EmployeeInepFactory

--- database/factories/EmployeeInepFactory.php
+++ database/factories/EmployeeInepFactory.php
@@ -23,6 +23,7 @@ class EmployeeInepFactory extends Factory
     public function definition(): array
     {
         return [
+            'cod_servidor' => EmployeeFactory::new()->create(),
             'cod_docente_inep' => $this->faker->randomNumber(3),
             'nome_inep' => $this->faker->name,
             'fonte' => 'U',
