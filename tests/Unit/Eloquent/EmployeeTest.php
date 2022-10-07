050389a1bf4f418c38d46566688c4266ae846ea4
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-07 12:11:49 -0300

    Adiciona testes de relacionamento do model Employee

--- tests/Unit/Eloquent/EmployeeTest.php
+++ tests/Unit/Eloquent/EmployeeTest.php
@@ -3,8 +3,13 @@
 namespace Tests\Unit\Eloquent;
 
 use App\Models\Employee;
+use App\Models\EmployeeAllocation;
+use App\Models\EmployeeGraduation;
 use App\Models\EmployeeInep;
+use App\Models\LegacyEmployeeRole;
+use App\Models\LegacyIndividual;
 use App\Models\LegacyPerson;
+use App\Models\LegacySchoolingDegree;
 use Database\Factories\EmployeeFactory;
 use Database\Factories\EmployeeInepFactory;
 use Tests\EloquentTestCase;
@@ -13,6 +18,9 @@ class EmployeeTest extends EloquentTestCase
 {
     protected $relations = [
         'person' => LegacyPerson::class,
+        'employeeAllocations' => [EmployeeAllocation::class],
+        'employeeRoles' => [LegacyEmployeeRole::class],
+        'graduations' => [EmployeeGraduation::class],
     ];
 
     /**
