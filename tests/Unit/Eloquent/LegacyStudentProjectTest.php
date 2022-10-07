2596d482a96e0bd37566ee7f2c26ad2581f249ec
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-07 11:44:03 -0300

    Adiciona testes de relacionamento do model LegacyStudentProject

--- tests/Unit/Eloquent/LegacyStudentProjectTest.php
+++ tests/Unit/Eloquent/LegacyStudentProjectTest.php
@@ -2,11 +2,18 @@
 
 namespace Tests\Unit\Eloquent;
 
+use App\Models\LegacyProject;
+use App\Models\LegacyStudent;
 use App\Models\LegacyStudentProject;
 use Tests\EloquentTestCase;
 
 class LegacyStudentProjectTest extends EloquentTestCase
 {
+    protected $relations = [
+        'student' => LegacyStudent::class,
+        'project' => LegacyProject::class,
+    ];
+
     /**
      * @return string
      */
