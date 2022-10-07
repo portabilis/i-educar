be641df9768ad7f4f4055918e867ce50fc9e31e8
Robert Ferraz <robert@portabilis.com.br>
2022-10-07 13:45:44 -0300

    Cria novo método de relaciomento que aumenta o desempenho com relacionamento pessoa

--- app/Support/Database/JoinableBuilder.php
+++ app/Support/Database/JoinableBuilder.php
@@ -28,4 +28,40 @@ trait JoinableBuilder
 
         return parent::select($columns);
     }
+
+    /**
+     * Obtem dinamicamente filtrado a lista de colunas da relação
+     *
+     * @param string $relation
+     * @param array  $columns
+     *
+     * @return array|null
+     */
+    public function getLegacyExportedColumns(string $relation, array $columns): array|null
+    {
+        //obtem a lista de colunas e nome legados
+        $legacy = $this->getLegacyColumns();
+        //obtem somente as columas do relacionamento
+        $legacyColumns = $legacy[$relation];
+        //filtra somente as keys
+        $keys = array_keys($legacyColumns);
+        //usa as key filtradas para verificar se existem na seleção do exportador
+        $filterColumns = array_intersect($keys, $columns);
+        //gera novas colunas com os atributos legados e seus aliases visiveis no arquivo exportado
+        $newColumns = [];
+
+        foreach ($filterColumns as $column) {
+            if (array_key_exists($column, $legacyColumns)) {
+                $newColumns[$column] = $legacyColumns[$column];
+            }
+        }
+
+        //se não encontrar, deve-se retornar null, para a query não adicionar o join da relação
+        //carregando assim só o necessário
+        if (empty($newColumns)) {
+            return null;
+        }
+
+        return $newColumns;
+    }
 }
