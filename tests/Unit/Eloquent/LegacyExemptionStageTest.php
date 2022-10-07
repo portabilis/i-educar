bb5dbcbddf02b625bdc7f02e3a64884660e5404b
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-07 11:12:50 -0300

    Adiciona testes de relacionamento do model LegacyExemptionStage

--- tests/Unit/Eloquent/LegacyExemptionStageTest.php
+++ tests/Unit/Eloquent/LegacyExemptionStageTest.php
@@ -2,11 +2,16 @@
 
 namespace Tests\Unit\Eloquent;
 
+use App\Models\LegacyDisciplineExemption;
 use App\Models\LegacyExemptionStage;
 use Tests\EloquentTestCase;
 
 class LegacyExemptionStageTest extends EloquentTestCase
 {
+    protected $relations = [
+        'exemption' => LegacyDisciplineExemption::class,
+    ];
+
     /**
      * @return string
      */
