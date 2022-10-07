4382c9c63d2f518f4d3d1f3779a0c69122f066b0
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-07 11:13:00 -0300

    Adiciona testes de relacionamento do model LegacyGrade

--- tests/Unit/Eloquent/LegacyGradeTest.php
+++ tests/Unit/Eloquent/LegacyGradeTest.php
@@ -2,11 +2,16 @@
 
 namespace Tests\Unit\Eloquent;
 
+use App\Models\LegacyCourse;
 use App\Models\LegacyGrade;
 use Tests\EloquentTestCase;
 
 class LegacyGradeTest extends EloquentTestCase
 {
+    protected $relations = [
+        'course' => LegacyCourse::class,
+    ];
+
     /**
      * @return string
      */
