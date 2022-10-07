0eb7c7eed3482eb7c3d4ec934d3b889920082bb8
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-07 11:49:10 -0300

    Adiciona testes de relacionamento do model SchoolInep

--- tests/Unit/Eloquent/SchoolInepTest.php
+++ tests/Unit/Eloquent/SchoolInepTest.php
@@ -2,11 +2,16 @@
 
 namespace Tests\Unit\Eloquent;
 
+use App\Models\LegacySchool;
 use App\Models\SchoolInep;
 use Tests\EloquentTestCase;
 
 class SchoolInepTest extends EloquentTestCase
 {
+    protected $relations = [
+        'school' => LegacySchool::class,
+    ];
+
     /**
      * @return string
      */
