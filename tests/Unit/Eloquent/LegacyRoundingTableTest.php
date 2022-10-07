fdc206456430d94fe73bcc0c2f2b76a3dd042861
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-07 11:24:53 -0300

    Adiciona testes de relacionamento do model LegacyRoundingTable

--- tests/Unit/Eloquent/LegacyRoundingTableTest.php
+++ tests/Unit/Eloquent/LegacyRoundingTableTest.php
@@ -3,10 +3,15 @@
 namespace Tests\Unit\Eloquent;
 
 use App\Models\LegacyRoundingTable;
+use App\Models\LegacyValueRoundingTable;
 use Tests\EloquentTestCase;
 
 class LegacyRoundingTableTest extends EloquentTestCase
 {
+    protected $relations = [
+        'roundingValues' => [LegacyValueRoundingTable::class],
+    ];
+
     public function getEloquentModelName()
     {
         return LegacyRoundingTable::class;
