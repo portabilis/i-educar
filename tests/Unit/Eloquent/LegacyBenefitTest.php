f1bc104b7b740072aa7da85a05368df3b0975d7e
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-11 16:34:37 -0300

    Atualiza factoty e teste do model LegacyBenefit

--- tests/Unit/Eloquent/LegacyBenefitTest.php
+++ tests/Unit/Eloquent/LegacyBenefitTest.php
@@ -3,6 +3,10 @@
 namespace Tests\Unit\Eloquent;
 
 use App\Models\LegacyBenefit;
+use App\Models\LegacyStudent;
+use App\Models\LegacyStudentBenefit;
+use Database\Factories\LegacyBenefitFactory;
+use Database\Factories\LegacyStudentFactory;
 use Tests\EloquentTestCase;
 
 class LegacyBenefitTest extends EloquentTestCase
@@ -13,5 +17,19 @@ class LegacyBenefitTest extends EloquentTestCase
     protected function getEloquentModelName()
     {
         return LegacyBenefit::class;
+    }
+
+    public function testStudents()
+    {
+        $benefit = LegacyBenefitFactory::new()->create();
+        $student = LegacyStudentFactory::new()->create();
+
+        LegacyStudentBenefit::create([
+            'aluno_id' => $student->cod_aluno,
+            'aluno_beneficio_id' => $benefit->id,
+        ]);
+
+        $this->assertCount(1, $benefit->students);
+        $this->assertInstanceOf(LegacyStudent::class, $benefit->students->first());
     }
 }
