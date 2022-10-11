e30c422610d23c5dfcacd86be2200d3e8352c7db
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-11 15:58:34 -0300

    Organiza teste do model EmployeeInep

--- tests/Unit/Eloquent/EmployeeInepTest.php
+++ tests/Unit/Eloquent/EmployeeInepTest.php
@@ -2,16 +2,27 @@
 
 namespace Tests\Unit\Eloquent;
 
+use App\Models\Employee;
 use App\Models\EmployeeInep;
 use Tests\EloquentTestCase;
 
 class EmployeeInepTest extends EloquentTestCase
 {
+    public $relations = [
+        'employee' => Employee::class,
+    ];
+
     /**
      * @return string
      */
     protected function getEloquentModelName()
     {
         return EmployeeInep::class;
+    }
+
+    public function testGetNumberAttribute()
+    {
+        $employeeInep = $this->createNewModel();
+        $this->assertEquals($employeeInep->cod_docente_inep, $employeeInep->getNumberAttribute());
     }
 }
