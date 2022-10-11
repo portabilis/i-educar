50cece2d2f1bab6c013e65e9c25e87c287291736
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-11 15:34:49 -0300

    Organiza factory e teste do model EmployeeGraduation

--- tests/Unit/Eloquent/EmployeeGraduationTest.php
+++ tests/Unit/Eloquent/EmployeeGraduationTest.php
@@ -2,16 +2,34 @@
 
 namespace Tests\Unit\Eloquent;
 
+use App\Models\Employee;
 use App\Models\EmployeeGraduation;
 use Tests\EloquentTestCase;
 
 class EmployeeGraduationTest extends EloquentTestCase
 {
+    public $relations = [
+        'employee' => Employee::class,
+    ];
+
     /**
      * @return string
      */
     protected function getEloquentModelName()
     {
         return EmployeeGraduation::class;
+    }
+
+    public function testScopeProfessor()
+    {
+        $employeeGraduation = $this->createNewModel();
+
+        $employeeQuery = EmployeeGraduation::query()
+            ->ofEmployee($employeeGraduation->employee->id)
+            ->first();
+
+        $this->assertInstanceOf(EmployeeGraduation::class, $employeeQuery);
+        $this->assertInstanceOf(Employee::class, $employeeQuery->employee);
+        $this->assertEquals($employeeGraduation->id, $employeeQuery->id);
     }
 }
