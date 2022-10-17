479dbcf986a3821fd2e53584c0165a85d5c2b6f2
Edinei Valdameri <99054215+edineivaldameri@users.noreply.github.com>
2022-10-17 13:21:32 -0300

    Update database/migrations/2022_10_17_115025_create_table_failed_jobs.php
    Co-authored-by: Eder Soares <edersoares@me.com>

--- database/migrations/2022_10_17_115025_create_table_failed_jobs.php
+++ database/migrations/2022_10_17_115025_create_table_failed_jobs.php
@@ -24,13 +24,4 @@ return new class () extends Migration {
             });
         }
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-    }
 };
