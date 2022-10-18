8c9298cc3a3a0aac8e34f6cba9a2b6f332fd2815
Joalisson Barros <joalissonricardo@gmail.com>
2022-10-18 11:08:43 -0300

    Adiciona busca de atributos

--- ieducar/modules/Api/Views/MatriculaController.php
+++ ieducar/modules/Api/Views/MatriculaController.php
@@ -1018,7 +1018,7 @@ class MatriculaController extends ApiCoreController
             $legacyActiveLooking->whereIn('ref_ref_cod_escola', explode(',', $escola));
         }
 
-        $buscaAtiva = $legacyActiveLooking->get();
+        $buscaAtiva = $legacyActiveLooking->getAttributes();
 
         return ['busca_ativa' => $buscaAtiva];
     }
