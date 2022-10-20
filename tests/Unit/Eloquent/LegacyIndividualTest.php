9d508592826122b296fa8fac85f8bb13cbd7c4de
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-20 17:02:50 -0300

    Adiciona teste para o model  LegacyIndividual

--- tests/Unit/Eloquent/LegacyIndividualTest.php
+++ tests/Unit/Eloquent/LegacyIndividualTest.php
@@ -3,10 +3,17 @@
 namespace Tests\Unit\Eloquent;
 
 use App\Models\LegacyIndividual;
+use App\Models\LegacyPerson;
 use Tests\EloquentTestCase;
 
 class LegacyIndividualTest extends EloquentTestCase
 {
+    public $relations = [
+        'mother' => LegacyPerson::class,
+        'father' => LegacyPerson::class,
+        'responsible' => LegacyPerson::class,
+    ];
+
     /**
      * @return string
      */
