21e873823e0fa762ed2a0837dad892ac436ff01b
Robert Ferraz <robert@portabilis.com.br>
2022-10-21 17:05:51 -0300

    Organiza model MigratedDiscipline e cria factory e teste

---
+++ database/factories/MigratedDisciplineFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\MigratedDiscipline;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class MigratedDisciplineFactory extends Factory
+{
+    protected $model = MigratedDiscipline::class;
+
+    public function definition(): array
+    {
+        return [
+            'old_discipline_id' => static fn () => LegacyDisciplineFactory::new()->create(),
+            'new_discipline_id' => static fn () => LegacyDisciplineFactory::new()->create(),
+            'grade_id' => static fn () => LegacyGradeFactory::new()->create(),
+            'year' => Carbon::now()->year,
+            'created_by' => static fn () => LegacyUserFactory::new()->create(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
+    }
+}
