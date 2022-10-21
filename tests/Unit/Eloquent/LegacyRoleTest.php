336adec730cb7fe6e44151450ed49765caee5d58
Edinei Valdameri <edineivaldameri@gmail.com>
2022-10-21 11:42:51 -0300

    Organiza factory e testes do model LegacyRole

--- tests/Unit/Eloquent/LegacyRoleTest.php
+++ tests/Unit/Eloquent/LegacyRoleTest.php
@@ -7,11 +7,41 @@ use Tests\EloquentTestCase;
 
 class LegacyRoleTest extends EloquentTestCase
 {
+    private LegacyRole $role;
+
     /**
      * @return string
      */
     protected function getEloquentModelName()
     {
         return LegacyRole::class;
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->role = $this->createNewModel();
+    }
+
+    /** @test */
+    public function getIdAttribute()
+    {
+        $this->assertEquals($this->role->getIdAttribute(), $this->role->cod_funcao);
+    }
+
+    /** @test  */
+    public function scopeProfessor()
+    {
+        $this->createNewModel();
+
+        $roleQuery = LegacyRole::query()
+            ->professor()
+            ->ativo()
+            ->first();
+
+        $this->assertInstanceOf(LegacyRole::class, $roleQuery);
+        $this->assertEquals(1, $roleQuery->professor);
+        $this->assertEquals(1, $roleQuery->ativo);
     }
 }
