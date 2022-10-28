b91f4f91087551fc6a577dde30fd6947ec5c962a
Robert Ferraz <robert@portabilis.com.br>
2022-10-28 13:39:51 -0300

    Remove scope active e substituido com SoftDeletes

--- app/Models/Builders/LegacyTransferRequestBuilder.php
+++ app/Models/Builders/LegacyTransferRequestBuilder.php
@@ -4,11 +4,6 @@ namespace App\Models\Builders;
 
 class LegacyTransferRequestBuilder extends LegacyBuilder
 {
-    public function active(): LegacyBuilder
-    {
-        return $this->where('ativo', 1);
-    }
-
     public function unattended(): LegacyBuilder
     {
         return $this->whereNull('ref_cod_matricula_entrada');
