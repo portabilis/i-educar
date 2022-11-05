5652239198c7fc867c4f2f827bd109d00aa99036
xlcn2 <lucianopeas@gmail.com>
2022-11-05 17:28:02 -0300

    exportacao-responsaveis

--- app/Http/Requests/ResponsavelTurmaExport.php
+++ app/Http/Requests/ResponsavelTurmaExport.php
@@ -4,7 +4,7 @@
  
  use Illuminate\Foundation\Http\FormRequest;
  
- class ResponsavelExport extends FormRequest
+ class ResponsavelTurmaExport extends FormRequest
  {
      /**
       * Determine if the user is authorized to make this request.
