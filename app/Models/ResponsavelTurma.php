2cb81eff4bb70868076239da792e6990cca1ecf0
xlcn2 <lucianopeas@gmail.com>
2022-11-05 17:37:42 -0300

    exportacao-responsaveis

--- app/Models/ResponsavelTurma.php
+++ app/Models/ResponsavelTurma.php
@@ -9,43 +9,7 @@ class ResponsavelTurma extends Model
 {
     protected $table = 'exporter_responsaveis_turma';
 
-    public function individual()
-    {
-        return $this->hasOne(Individual::class);
-    }
+    
 
-    public function createdBy()
-    {
-        return $this->belongsTo(Individual::class, 'created_by', 'id');
-    }
-
-    public function updatedBy()
-    {
-        return $this->belongsTo(Individual::class, 'updated_by', 'id');
-    }
-
-    public function getTypeDescriptionAttribute()
-    {
-        return (new PersonType())->getDescriptiveValues()[(int) $this->type];
-    }
-
-    public function getRegistryOriginDescriptionAttribute()
-    {
-        return (new RegistryOrigin())->getDescriptiveValues()[(int) $this->registry_origin];
-    }
-
-    /**
-     * @return HasOneThrough
-     */
-    public function place()
-    {
-        return $this->hasOneThrough(
-            Place::class,
-            PersonHasPlace::class,
-            'person_id',
-            'id',
-            'id',
-            'place_id'
-        )->orderBy('type');
-    }
+    
 }
