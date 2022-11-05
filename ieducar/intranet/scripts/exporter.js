6dc11aa6424525a76b976eb0140244f470852b20
xlcn2 <lucianopeas@gmail.com>
2022-11-05 15:33:33 -0300

    exportacao-responsaveis

--- ieducar/intranet/scripts/exporter.js
+++ ieducar/intranet/scripts/exporter.js
@@ -12,4 +12,17 @@
 
   window.dataExport = dataExport
   document.getElementById('export-btn').style.marginTop = 0;
-})()
+
+  const dataExportResponsaveis = (formId1, resource1) => {
+    const form1 = document.getElementById(formId1)
+    const data1 = new FormData(form1)
+    const queryString1 = new URLSearchParams(data1).toString()
+    const url1 = `/exports/${resource1}?${queryString1}`
+
+    window.location = url1
+  }
+
+  window.dataExport = dataExportResponsaveis
+  document.getElementById('export-btn-responsaveis').style.marginTop = 0;
+}
+)()
