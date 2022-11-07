d59c7f8b396c0c5fed0593086f95d9fbef4d21f8
xlcn2 <lucianopeas@gmail.com>
2022-11-07 08:47:09 -0300

    exportacao-responsaveis

--- ieducar/intranet/educar_aluno_lst.php
+++ ieducar/intranet/educar_aluno_lst.php
@@ -215,16 +215,18 @@ return new class extends clsListagem {
             $this->acao = 'go("/module/Cadastro/aluno")';
             $this->nome_acao = 'Novo';
         }
- 
+       
         if ($_GET) {
-            $this->array_botao_script = ['dataExport("formcadastro", "students")'];
-            $this->array_botao = ['Exportar para planilha'];
-            $this->array_botao_id = ['export-btn'];
+            $this->array_botao_script = ['dataExport("formcadastro", "students")','dataExport("formcadastro", "responsaveis_turma")'];
+            $this->array_botao = ['Exportar para planilha','Exportar responsáveis para planilha'];
+            $this->array_botao_id = ['export-btn','export-btn-responsaveis'];
         }
+       
 
         $this->largura = '100%';
 
         Portabilis_View_Helper_Application::loadJavascript($this, ['/intranet/scripts/exporter.js']);
+        Portabilis_View_Helper_Application::loadJavascript($this, ['/intranet/scripts/exporter_responsaveis.js']);
 
         $this->breadcrumb('Alunos', ['/intranet/educar_index.php' => 'Escola']);
     }
