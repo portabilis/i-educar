863523036727c7bbac36125989c94631212e5390
Edinei Valdameri <edineivaldameri@gmail.com>
2022-11-07 17:48:31 -0300

    Cria variável da data de matrícula

--- tests/Unit/Educacenso/Import/Import2021Test.php
+++ tests/Unit/Educacenso/Import/Import2021Test.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Educacenso\Import;
 
 use App\Services\Educacenso\HandleFileService;
 use App\Services\Educacenso\ImportServiceFactory;
+use Carbon\Carbon;
 use Illuminate\Http\UploadedFile;
 use Tests\EducacensoTestCase;
 
@@ -14,10 +15,11 @@ class Import2021Test extends EducacensoTestCase
         parent::setUp();
 
         $this->year = 2021;
+        $this->dateEnrollment = new Carbon('2021-01-01');
 
         $yearImportService = ImportServiceFactory::createImportService(
             $this->year,
-            '01/01/2021'
+            $this->dateEnrollment->format('d/m/Y')
         );
 
         $importFileService = new HandleFileService($yearImportService, $this->user);
