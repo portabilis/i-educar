bce7651e599b99d7623382d0f6d868c26993dcf3
Edinei Valdameri <edineivaldameri@gmail.com>
2022-11-10 17:14:22 -0300

    Remove endpoint alunos-matriculados-turma da API do I-Diário

--- ieducar/modules/Api/Views/TurmaController.php
+++ ieducar/modules/Api/Views/TurmaController.php
@@ -32,14 +32,6 @@ class TurmaController extends ApiCoreController
         );
     }
 
-    protected function canGetAlunosMatriculadosTurma()
-    {
-        return (
-            $this->validatesPresenceOf('instituicao_id') &&
-            $this->validatesPresenceOf('turma_id')
-        );
-    }
-
     protected function canGetAlunosExameTurma()
     {
         return (
@@ -286,90 +278,6 @@ class TurmaController extends ApiCoreController
         }
     }
 
-    protected function getAlunosMatriculadosTurma()
-    {
-        if ($this->canGetAlunosMatriculadosTurma()) {
-            $instituicaoId = $this->getRequest()->instituicao_id;
-            $turmaId = $this->getRequest()->turma_id;
-            $disciplinaId = $this->getRequest()->disciplina_id;
-            $dataMatricula = $this->getRequest()->data_matricula;
-
-            $sql = 'SELECT a.cod_aluno as id,
-                     m.dependencia,
-                     mt.sequencial_fechamento as sequencia,
-                     mt.data_enturmacao
-              FROM pmieducar.aluno a
-              INNER JOIN pmieducar.matricula m ON m.ref_cod_aluno = a.cod_aluno
-              INNER JOIN pmieducar.matricula_turma mt ON m.cod_matricula = mt.ref_cod_matricula
-              INNER JOIN pmieducar.turma t ON mt.ref_cod_turma = t.cod_turma
-              INNER JOIN cadastro.pessoa p ON p.idpes = a.ref_idpes
-              WHERE m.ativo = 1
-                AND a.ativo = 1
-                AND t.ativo = 1
-                AND t.ref_cod_instituicao = $1
-                AND t.cod_turma  = $2
-                AND (CASE WHEN coalesce($3, current_date)::date = current_date
-                      THEN mt.ativo = 1
-                     ELSE
-                       (CASE WHEN mt.ativo = 0 THEN
-                          mt.sequencial = ( select max(matricula_turma.sequencial)
-                                              from pmieducar.matricula_turma
-                                             inner join pmieducar.matricula on(matricula_turma.ref_cod_matricula = matricula.cod_matricula)
-                                             where matricula_turma.ref_cod_matricula = mt.ref_cod_matricula
-                                               and matricula_turma.ref_cod_turma = mt.ref_cod_turma
-                                               and ($3::date >= matricula_turma.data_enturmacao::date
-                                                   and $3::date < coalesce(matricula_turma.data_exclusao::date, matricula.data_cancel::date, current_date))
-                                               and matricula_turma.ativo = 0
-                                               and not exists(select 1
-                                                                from pmieducar.matricula_turma mt_sub
-                                                               where mt_sub.ativo = 1
-                                                                 and mt_sub.ref_cod_matricula = mt.ref_cod_matricula
-                                                                 and mt_sub.ref_cod_turma = mt.ref_cod_turma
-                                                              )
-                                          )
-                       ELSE
-                          ($3::date >= mt.data_enturmacao::date
-                          and $3::date < coalesce(m.data_cancel::date, mt.data_exclusao::date, current_date))
-                       END)
-                      END)';
-
-            $params = [$instituicaoId, $turmaId, $dataMatricula];
-
-            if (is_numeric($disciplinaId)) {
-                $params[] = $disciplinaId;
-                $sql .= 'AND
-                  CASE WHEN m.dependencia THEN
-                    (
-                      SELECT 1 FROM pmieducar.disciplina_dependencia dd
-                      WHERE dd.ref_cod_matricula = m.cod_matricula
-                      AND dd.ref_cod_disciplina = $4
-                      LIMIT 1
-                    ) IS NOT NULL
-                  ELSE
-                   (
-                    SELECT 1 FROM pmieducar.dispensa_disciplina dd
-                    WHERE dd.ativo = 1
-                    AND dd.ref_cod_matricula = m.cod_matricula
-                    AND dd.ref_cod_disciplina = $4
-                    LIMIT 1
-                  ) IS NULL
-                END';
-            }
-
-            $sql .= ' ORDER BY m.dependencia, (upper(p.nome))';
-
-            $alunos = $this->fetchPreparedQuery($sql, $params);
-
-            $attrs = ['id','dependencia', 'sequencia', 'data_enturmacao'];
-            $alunos = Portabilis_Array_Utils::filterSet($alunos, $attrs);
-
-            foreach ($alunos as &$aluno) {
-                $aluno['dependencia'] = dbBool($aluno['dependencia']);
-            }
-
-            return ['alunos' => $alunos];
-        }
-    }
     protected function getAlunosExameTurma()
     {
         $instituicaoId = $this->getRequest()->instituicao_id;
@@ -432,8 +340,6 @@ class TurmaController extends ApiCoreController
             $this->appendResponse($this->ordenaSequencialAlunosTurma());
         } elseif ($this->isRequestFor('get', 'turmas-por-escola')) {
             $this->appendResponse($this->getTurmasPorEscola());
-        } elseif ($this->isRequestFor('get', 'alunos-matriculados-turma')) {
-            $this->appendResponse($this->getAlunosMatriculadosTurma());
         } elseif ($this->isRequestFor('get', 'alunos-exame-turma')) {
             $this->appendResponse($this->getAlunosExameTurma());
         } elseif ($this->isRequestFor('get', 'series-da-turma')) {
