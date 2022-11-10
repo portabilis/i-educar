efbd9345443b5653ef2a2330cda175d7360fdc8c
bonot <paulabonot@gmail.com>
2022-11-10 17:27:00 -0300

    Corrige importação de campos do registro 10

--- app/Services/Educacenso/Version2022/Models/Registro10Model.php
+++ app/Services/Educacenso/Version2022/Models/Registro10Model.php
@@ -20,7 +20,7 @@ class Registro10Model extends Registro10
         $this->localFuncionamentoUnidadePrisional = $arrayColumns[7];
         $this->localFuncionamentoOutros = $arrayColumns[8];
         $this->condicao = $arrayColumns[9];
-        $this->predioCompartilhadoOutraEscola = $arrayColumns[10];
+        $this->predioCompartilhadoOutraEscola = (int) $arrayColumns[10];
         $this->codigoInepEscolaCompartilhada = $arrayColumns[11];
         $this->codigoInepEscolaCompartilhada2 = $arrayColumns[12];
         $this->codigoInepEscolaCompartilhada3 = $arrayColumns[13];
@@ -156,7 +156,7 @@ class Registro10Model extends Registro10
         $this->instrumentosPedagogicosEducacaoEtnicoRacial = $arrayColumns[143];
         $this->instrumentosPedagogicosEducacaoCampo = $arrayColumns[144];
         $this->instrumentosPedagogicosNenhum = $arrayColumns[145];
-        $this->educacaoIndigena = $arrayColumns[146];
+        $this->educacaoIndigena = (int) $arrayColumns[146];
         $this->linguaIndigena = $arrayColumns[147];
         $this->linguaPortuguesa = $arrayColumns[148];
         $this->linguaIndigena1 = $arrayColumns[149];
