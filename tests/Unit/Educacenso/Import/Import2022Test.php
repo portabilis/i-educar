0492d16c66781fa40b89560222702a9465fc23ff
Edinei Valdameri <edineivaldameri@gmail.com>
2022-11-17 15:41:18 -0300

    Adiciona validação de campos específicos do Censo 2022

--- tests/Unit/Educacenso/Import/Import2022Test.php
+++ tests/Unit/Educacenso/Import/Import2022Test.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Unit\Educacenso\Import;
 
+use App\Models\LegacyEnrollment;
+use App\Models\LegacySchool;
+use App\Models\LegacySchoolClassTeacher;
 use App\Services\Educacenso\HandleFileService;
 use App\Services\Educacenso\ImportServiceFactory;
 use Carbon\Carbon;
@@ -30,5 +33,42 @@ class Import2022Test extends EducacensoTestCase
             resource_path('../tests/Unit/assets/importacao_educacenso_2022.txt'),
             'importacao_educacenso_2022.txt'
         ));
+    }
+
+    /** @test */
+    public function import2022Specific()
+    {
+        $legacySchool = LegacySchool::first();
+        $this->assertEquals('{1}', $legacySchool->poder_publico_parceria_convenio);
+        $this->assertEquals('{6}', $legacySchool->formas_contratacao_adm_publica_e_outras_instituicoes);
+
+        foreach ($legacySchool->schoolClasses as $schoolClass) {
+            $this->assertNotNull($schoolClass->estrutura_curricular);
+            $this->assertEquals(1, $schoolClass->formas_organizacao_turma);
+
+            if (str_contains('2', $schoolClass->estrutura_curricular)) {
+                $this->assertEquals('{8}', $legacySchool->estrutura_curricular);
+            }
+        }
+
+        $schoollClassTeachers = LegacySchoolClassTeacher::all();
+
+        foreach ($schoollClassTeachers as $schoollClassTeacher) {
+            if (str_contains('2', $schoollClassTeacher->schoolClass->estrutura_curricular)) {
+                $this->assertEquals('{8}', $schoollClassTeacher->estrutura_curricular);
+            }
+        }
+        $enrollments = LegacyEnrollment::all();
+
+        foreach ($enrollments as $enrollment) {
+            if (count($enrollment->tipo_itinerario) > 0) {
+                $this->assertCount(2, $enrollment->tipo_itinerario);
+                $this->assertNotNull($enrollment->composicao_itinerario);
+                $this->assertCount(1, $enrollment->composicao_itinerario);
+                $this->assertEquals(5, $enrollment->composicao_itinerario[0]);
+                $this->assertIsBool($enrollment->itinerario_concomitante);
+                $this->assertEquals(1, $enrollment->curso_itinerario);
+            }
+        }
     }
 }
