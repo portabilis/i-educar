fd358289ddb85957182a98e8ca0a799541cc983f
Edinei Valdameri <edineivaldameri@gmail.com>
2022-11-21 16:04:55 -0300

    Remove método não necessário

--- app/Events/ForgetCachedUserEvent.php
+++ app/Events/ForgetCachedUserEvent.php
@@ -3,7 +3,6 @@
 namespace App\Events;
 
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -20,15 +19,5 @@ class ForgetCachedUserEvent
      */
     public function __construct(public int $id)
     {
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
     }
 }
