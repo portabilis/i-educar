8c3156ef8f0da897752d37083b0ed5a8dc874ccc
xlcn2 <lucianopeas@gmail.com>
2022-11-24 11:53:18 -0300

    manutenção matrícula

--- ieducar/intranet/educar_manutencao_matricula.php
+++ ieducar/intranet/educar_manutencao_matricula.php
@@ -209,7 +209,6 @@ return new class extends clsListagem {
                  
                     $conteudo_acoes_matricula .= "<button style='margin:2px' class='btn btn-info'> Transferência</button><br>"; 
                 }
-                $conteudo_acoes_matricula .= "<button style='margin:2px' class='btn btn-info'> Transferência</button> <button  class='btn btn-danger' style='margin:2px'> Abandono</button><button style='margin:2px; color:white; background-color: grey' class='btn '> Falecido </button> <button  class='btn btn-success' style='margin:2px'> Enturmação</button><br>";
                 elseif($situacao=='Transferido'){
                  
                     $conteudo_acoes_matricula .= ""; 
