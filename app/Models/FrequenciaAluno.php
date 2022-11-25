ef8ab6405263e8673532edc8d6c0ea73c41be65e
xlcn2 <lucianopeas@gmail.com>
2022-11-25 03:37:59 -0300

    manutenção matrícula

---
+++ app/Models/FrequenciaAluno.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FrequenciaAluno extends Model
+{
+
+ /**
+     * @var string
+     */
+    protected $table = 'modules.frequencia_aluno';
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'id',
+        'ref_frequencia',
+        'ref_cod_matricula'
+    ];
+
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+
+    use HasFactory;
+}
