8e50fb3b818a68292c2b0ea8cc010a0a5733bef4
xlcn2 <lucianopeas@gmail.com>
2022-11-28 17:15:17 -0300

    manutenção matrícula

--- resources/views/enrollments/enrollmanutencao.blade.php
+++ resources/views/enrollments/enrollmanutencao.blade.php
@@ -104,20 +104,20 @@
 @endsection
 <script>
 
- $j('#btn_enviar').prop('disabled', true);
- $j('#btn_enviar').addClass('btn-disabled');
+ $('#btn_enviar').prop('disabled', true);
+ $('#btn_enviar').addClass('btn-disabled');
 
-  $j('<tr id="tr_confirma_dados_unificacao"></tr>').insertBefore($j('#btn_botoes'));
+  $('<tr id="tr_confirma_dados_unificacao"></tr>').insertBefore($('#btn_botoes'));
 
   let htmlCheckbox = '<td colspan="2">'
   htmlCheckbox += '<input onchange="confirmaAnalise()" id="check_confirma_exclusao" type="checkbox" />';
   htmlCheckbox += '<label for="check_confirma_exclusao" style="color:red !important;">Esta rotina excluirá todas as informações do diário do aluno posteriormente a data de movimentação, assinale se concorda.</label>';
   htmlCheckbox += '</td>';
 
-  $j('#tr_confirma_dados_unificacao').html(htmlCheckbox);
+  $('#tr_confirma_dados_unificacao').html(htmlCheckbox);
 
   function confirmaAnalise() {
-  let checked = $j('#check_confirma_exclusao').is(':checked');
+  let checked = $('#check_confirma_exclusao').is(':checked');
 
   if (checked) {
     habilitaBotaoUnificar();
@@ -132,15 +132,15 @@
 }
 
 function habilitaBotaoUnificar() {
-    $j('#btn_enviar').prop('disabled', false);
-    $j('#btn_enviar').addClass('btn-green');
-    $j('#btn_enviar').removeClass('btn-disabled');
+    $('#btn_enviar').prop('disabled', false);
+    $('#btn_enviar').addClass('btn-green');
+    $('#btn_enviar').removeClass('btn-disabled');
   }
 
   function desabilitaBotaoUnificar() {
-    $j('#btn_enviar').prop('disabled', true);
-    $j('#btn_enviar').removeClass('btn-green');
-    $j('#btn_enviar').addClass('btn-disabled');
+    $('#btn_enviar').prop('disabled', true);
+    $('#btn_enviar').removeClass('btn-green');
+    $('#btn_enviar').addClass('btn-disabled');
   }
 
 </script>
