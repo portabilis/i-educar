1974fd41fab6b87b54b9760e01e9b38848104cb3
Edinei Valdameri <edineivaldameri@gmail.com>
2022-11-29 13:21:53 -0300

    Adiciona os evento UserDeleted

--- app/Events/UserDeleted.php
+++ app/Events/UserDeleted.php
@@ -2,22 +2,18 @@
 
 namespace App\Events;
 
+use App\User;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class ForgetCachedUserEvent
+class UserDeleted
 {
     use Dispatchable;
     use InteractsWithSockets;
     use SerializesModels;
 
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
-    public function __construct(public int $id)
+    public function __construct(public User $user)
     {
     }
 }
