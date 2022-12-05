30e39cd307c7a62abf879e32fcbdec535ab70e77
xlcn2 <lucianopeas@gmail.com>
2022-12-05 12:40:57 -0300

    migration menu manutenção matrícula

---
+++ database/migrations/2022_12_05_122213_insert_menu_manutencao_matriculas.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class InsertMenuManutencaoMatriculas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('public.menus')->insert(
+            array(
+                'id' => 396,
+                'parent_id' => 13,
+                'title' => 'Manutenção de Matrículas',
+                'description' => 'Manutenção de Matrículas',
+                'link' => '/intranet/educar_manutencao_matricula.php',
+                'icon' => NULL,
+                'order' => 100,
+                'type' => 1,
+                'process' => 7797,
+                'old' => NULL,
+                'parent_old' => NULL,
+                'active' => true,
+                'created_at' => NULL,
+                'updated_at' => NULL
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
