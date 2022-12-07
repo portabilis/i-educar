5db4f50733d260fbf0f4d9699945ffb17c8906a2
Edinei Valdameri <edineivaldameri@gmail.com>
2022-12-07 10:17:43 -0300

    Adiciona parametros ao fillable

--- app/Models/LegacySchoolHistoryDiscipline.php
+++ app/Models/LegacySchoolHistoryDiscipline.php
@@ -10,6 +10,18 @@ class LegacySchoolHistoryDiscipline extends LegacyModel
 
     protected $table = 'pmieducar.historico_disciplinas';
 
+    public $fillable = [
+        'ref_ref_cod_aluno',
+        'sequencial',
+        'ref_sequencial',
+        'nm_disciplina',
+        'nota',
+        'faltas',
+        'carga_horaria_disciplina',
+        'dependencia',
+        'tipo_base',
+    ];
+
     public function student(): BelongsTo
     {
         return $this->belongsTo(LegacyStudent::class, 'ref_ref_cod_aluno');
