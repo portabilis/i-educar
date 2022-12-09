15b0c70f891c3dde50804cb9c4b6c0a53debc3cf
xlcn2 <lucianopeas@gmail.com>
2022-12-09 17:09:47 -0300

    correcao calculo nota

--- ieducar/modules/Avaliacao/Views/DiarioApiController.php
+++ ieducar/modules/Avaliacao/Views/DiarioApiController.php
@@ -620,7 +620,7 @@ class DiarioApiController extends ApiCoreController
             $serie_id = $id->ref_ref_cod_serie;
            
         }
-        $substitui_menor_nota = false;
+        $substitui_menor_nota = 0;
         $regra_avaliacao = RegraAvaliacaoSerieAno::where('serie_id', $serie_id)->where('ano_letivo', $this->getRequest()->ano_escolar)->get();
         foreach($regra_avaliacao as $regra) {
             
