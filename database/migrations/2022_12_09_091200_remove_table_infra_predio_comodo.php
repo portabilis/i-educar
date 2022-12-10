082cb046f97706e231cf73e199a6e54ad055a6f1
Edinei Valdameri <edineivaldameri@gmail.com>
2022-12-10 17:22:44 -0300

    Remove tabela pmieducar.infra_predio_comodo

---
+++ database/migrations/2022_12_09_091200_remove_table_infra_predio_comodo.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up()
+    {
+        Schema::dropIfExists('pmieducar.infra_predio_comodo');
+    }
+};
