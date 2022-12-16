43f961ed68c224d5611615b66ad49460c9809eeb
xlcn2 <lucianopeas@gmail.com>
2022-12-16 17:07:32 -0300

    exportacao responsaveis novos campos

--- app/Exports/ResponsavelTurmaExport.php
+++ app/Exports/ResponsavelTurmaExport.php
@@ -36,6 +36,9 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
             $row[] = $responsavel->gender;
             $row[] = $responsavel->estado_civil;
             $row[] = $responsavel->rg;
+            $row[] = $responsavel->rg_issue_date;
+            $row[] = $responsavel->orgao_exp_rg;
+            $row[] = $responsavel->rg_state_abbreviation;
             $row[] = $responsavel->nis;
             $row[] = $responsavel->sus_number;
             $row[] = $responsavel->certidao;
@@ -48,7 +51,9 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
             $row[] = $responsavel->nationality;
             $row[] = $responsavel->naturalidade;
             $row[] = $responsavel->profession;
-            $row[] = $responsavel->endereco.' '.$responsavel->numero_casa.' '.$responsavel->bairro.' '.$responsavel->cep.' '.$responsavel->complemento;
+            $row[] = $responsavel->complemento.", ".$responsavel->endereco.', '.$responsavel->numero_casa;
+            $row[] = $responsavel->bairro;
+            $row[] = $responsavel->cep;
             $row[] = $responsavel->ddd.' '.$responsavel->telefone;
             $row[] = $responsavel->email_address;
             $row[] = $responsavel->bank_branch;
@@ -65,6 +70,9 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
             $row[] = $responsavel->gender_mae;
             $row[] = $responsavel->estado_civil_mae;
             $row[] = $responsavel->rg_mae;
+            $row[] = $responsavel->rg_issue_date_mae;
+            $row[] = $responsavel->orgao_exp_rg_mae;
+            $row[] = $responsavel->rg_state_abbreviation_mae;
             $row[] = $responsavel->nis_mae;
             $row[] = $responsavel->sus_number_mae;
             $row[] = $responsavel->certidao_mae;
@@ -77,13 +85,15 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
             $row[] = $responsavel->nationality_mae;
             $row[] = $responsavel->naturalidade_mae;
             $row[] = $responsavel->profession_mae;
-            $row[] = $responsavel->endereco_mae.' '.$responsavel->numero_casa_mae.' '.$responsavel->bairro_mae.' '.$responsavel->cep_mae.' '.$responsavel->complemento_mae;
-            $row[] = $responsavel->ddd.' '.$responsavel->telefone_mae;
+            $row[] = $responsavel->complemento_mae.", ".$responsavel->endereco_mae.', '.$responsavel->numero_casa_mae;
+            $row[] = $responsavel->bairro_mae;
+            $row[] = $responsavel->cep_mae;
+            $row[] = $responsavel->ddd_mae.' '.$responsavel->telefone_mae;
             $row[] = $responsavel->email_address_mae;
             $row[] = $responsavel->bank_branch_mae;
             $row[] = $responsavel->bank_account_mae;
             $row[] = $responsavel->type_bank_account_mae;  
-
+  
         }elseif($responsavel->tipo_responsavel=='p'){
             
             $row[] = $responsavel->name_pai;
@@ -93,6 +103,9 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
             $row[] = $responsavel->gender_pai;
             $row[] = $responsavel->estado_civil_pai;
             $row[] = $responsavel->rg_pai;
+            $row[] = $responsavel->rg_issue_date_pai;
+            $row[] = $responsavel->orgao_exp_rg_pai;
+            $row[] = $responsavel->rg_state_abbreviation_pai;
             $row[] = $responsavel->nis_pai;
             $row[] = $responsavel->sus_number_pai;
             $row[] = $responsavel->certidao_pai;
@@ -105,8 +118,10 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
             $row[] = $responsavel->nationality_pai;
             $row[] = $responsavel->naturalidade_pai;
             $row[] = $responsavel->profession_pai;
-            $row[] = $responsavel->endereco_pai.' '.$responsavel->numero_casa_pai.' '.$responsavel->bairro_pai.' '.$responsavel->cep_pai.' '.$responsavel->complemento_pai;
-            $row[] = $responsavel->ddd.' '.$responsavel->telefone_pai;
+            $row[] = $responsavel->complemento_pai.", ".$responsavel->endereco_pai.' '.$responsavel->numero_casa_pai;
+            $row[] = $responsavel->bairro_pai;
+            $row[] = $responsavel->cep_pai;
+            $row[] = $responsavel->ddd_pai.' '.$responsavel->telefone_pai;
             $row[] = $responsavel->email_address_pai;
             $row[] = $responsavel->bank_branch_pai;
             $row[] = $responsavel->bank_account_pai;
@@ -121,6 +136,9 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
             $row[] = $responsavel->gender_pai.", ".$responsavel->gender_mae;
             $row[] = $responsavel->estado_civil_pai.", ".$responsavel->estado_civil_mae;
             $row[] = $responsavel->rg_pai.", ".$responsavel->rg_mae;
+            $row[] = $responsavel->rg_issue_date_pai.", ".$responsavel->rg_issue_date_mae;
+            $row[] = $responsavel->orgao_exp_rg_pai.", ".$responsavel->rg_issue_date_mae;
+            $row[] = $responsavel->rg_state_abbreviation_pai.", ".$responsavel->rg_issue_date_mae;
             $row[] = $responsavel->nis_pai.", ".$responsavel->nis_mae;
             $row[] = $responsavel->sus_number_pai.", ".$responsavel->sus_number_mae;
             $row[] = $responsavel->certidao_pai.", ".$responsavel->certidao_mae;
@@ -133,7 +151,9 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
             $row[] = $responsavel->nationality_pai.", ".$responsavel->nationality_mae;
             $row[] = $responsavel->naturalidade_pai.", ".$responsavel->naturalidade_mae;
             $row[] = $responsavel->profession_pai.", ".$responsavel->profession_mae;
-            $row[] = $responsavel->endereco_pai.' '.$responsavel->numero_casa_pai.' '.$responsavel->bairro_pai.' '.$responsavel->cep_pai.' '.$responsavel->complemento_pai;
+            $row[] = $responsavel->complemento_pai.", ".$responsavel->endereco_pai.' '.$responsavel->numero_casa_pai;
+            $row[] = $responsavel->bairro_pai;
+            $row[] = $responsavel->cep_pai;
             $row[] = $responsavel->ddd_pai.' '.$responsavel->telefone_pai.", ".$responsavel->ddd_mae.' '.$responsavel->telefone_mae;
             $row[] = $responsavel->email_address_pai.", ".$responsavel->email_address_mae;
             $row[] = $responsavel->bank_branch_pai.", ".$responsavel->bank_branch_mae;
@@ -150,6 +170,9 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
                 $row[] = $responsavel->gender_pai;
                 $row[] = $responsavel->estado_civil_pai;
                 $row[] = $responsavel->rg_pai;
+                $row[] = $responsavel->rg_issue_date_pai;
+                $row[] = $responsavel->orgao_exp_rg_pai;
+                $row[] = $responsavel->rg_state_abbreviation_pai;
                 $row[] = $responsavel->nis_pai;
                 $row[] = $responsavel->sus_number_pai;
                 $row[] = $responsavel->certidao_pai;
@@ -162,8 +185,10 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
                 $row[] = $responsavel->nationality_pai;
                 $row[] = $responsavel->naturalidade_pai;
                 $row[] = $responsavel->profession_pai;
-                $row[] = $responsavel->endereco_pai.' '.$responsavel->numero_casa_pai.' '.$responsavel->bairro_pai.' '.$responsavel->cep_pai.' '.$responsavel->complemento_pai;
-                $row[] = $responsavel->ddd.' '.$responsavel->telefone_pai;
+                $row[] = $responsavel->complemento_pai.", ".$responsavel->endereco_pai.' '.$responsavel->numero_casa_pai;
+                $row[] = $responsavel->bairro_pai;
+                $row[] = $responsavel->cep_pai;
+                $row[] = $responsavel->ddd_pai.' '.$responsavel->telefone_pai;
                 $row[] = $responsavel->email_address_pai;
                 $row[] = $responsavel->bank_branch_pai;
                 $row[] = $responsavel->bank_account_pai;
@@ -178,6 +203,9 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
             $row[] = $responsavel->gender_mae;
             $row[] = $responsavel->estado_civil_mae;
             $row[] = $responsavel->rg_mae;
+            $row[] = $responsavel->rg_issue_date_mae;
+            $row[] = $responsavel->orgao_exp_rg_mae;
+            $row[] = $responsavel->rg_state_abbreviation_mae;
             $row[] = $responsavel->nis_mae;
             $row[] = $responsavel->sus_number_mae;
             $row[] = $responsavel->certidao_mae;
@@ -190,8 +218,10 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
             $row[] = $responsavel->nationality_mae;
             $row[] = $responsavel->naturalidade_mae;
             $row[] = $responsavel->profession_mae;
-            $row[] = $responsavel->endereco_mae.' '.$responsavel->numero_casa_mae.' '.$responsavel->bairro_mae.' '.$responsavel->cep_mae.' '.$responsavel->complemento_mae;
-            $row[] = $responsavel->ddd.' '.$responsavel->telefone_mae;
+            $row[] = $responsavel->complemento_mae.", ".$responsavel->endereco_mae.', '.$responsavel->numero_casa_mae;
+            $row[] = $responsavel->bairro_mae;
+            $row[] = $responsavel->cep_mae;
+            $row[] = $responsavel->ddd_mae.' '.$responsavel->telefone_mae;
             $row[] = $responsavel->email_address_mae;
             $row[] = $responsavel->bank_branch_mae;
             $row[] = $responsavel->bank_account_mae;
@@ -215,6 +245,9 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
             'Sexo',
             'Estado Cívil',
             'RG',
+            'Data Exp. RG',
+            'Órg Em. RG',
+            'Estado Em. RG',
             'NIS',
             'Carteira do SUS',
             'Certidão de nascimento',
@@ -227,7 +260,9 @@ class ResponsavelTurmaExport implements FromCollection, ShouldAutoSize, WithColu
             'Nacionalidade',
             'Naturalidade',
             'Profissão',
-            'Endereço Completo',
+            'Endereço',
+            'Bairro',
+            'CEP',
             'Telefones',
             'Email',
             'Agência',
