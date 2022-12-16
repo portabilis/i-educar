e7ba09f824bd7a0adf14d92fd9295259bd8d6f09
xlcn2 <lucianopeas@gmail.com>
2022-12-16 18:00:33 -0300

    exportacao responsaveis novos campos

--- database/migrations/2022_12_16_173801_update_resp_campos_resp_resp.php
+++ database/migrations/2022_12_16_173801_update_resp_campos_resp_resp.php
@@ -1,11 +1,10 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use App\Support\Database\MigrationUtils;
 
 class UpdateRespCamposRespResp extends Migration
 {
+    use MigrationUtils;
     /**
      * Run the migrations.
      *
