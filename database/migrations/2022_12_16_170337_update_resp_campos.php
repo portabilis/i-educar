fb62d4f5fa65802ebfa74bd8a282c154eac44ca1
xlcn2 <lucianopeas@gmail.com>
2022-12-19 10:02:00 -0300

    colunas excel responsaveis

--- database/migrations/2022_12_16_170337_update_resp_campos.php
+++ database/migrations/2022_12_16_170337_update_resp_campos.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use App\Support\Database\MigrationUtils;
 
 class UpdateRespCampos extends Migration
 {
+    use MigrationUtils;
+
     /**
      * Run the migrations.
      *
