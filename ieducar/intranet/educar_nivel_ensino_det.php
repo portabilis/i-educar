1d4fe450f083fb1f17b51ceb80a222975b9f61d8
Edinei Valdameri <edineivaldameri@gmail.com>
2022-12-22 09:40:30 -0300

    Adiciona named parameters em Nivel de Ensino

--- ieducar/intranet/educar_nivel_ensino_det.php
+++ ieducar/intranet/educar_nivel_ensino_det.php
@@ -26,39 +26,39 @@ return new class extends clsDetalhe {
 
         $this->cod_nivel_ensino = $_GET['cod_nivel_ensino'];
 
-        $registro = LegacyEducationLevel::find($this->cod_nivel_ensino)?->getAttributes();
+        $registro = LegacyEducationLevel::find(id: $this->cod_nivel_ensino)?->getAttributes();
 
         if (!$registro) {
-            $this->simpleRedirect('educar_nivel_ensino_lst.php');
+            $this->simpleRedirect(url: 'educar_nivel_ensino_lst.php');
         }
 
-        $obj_instituicao = new clsPmieducarInstituicao($registro['ref_cod_instituicao']);
+        $obj_instituicao = new clsPmieducarInstituicao(cod_instituicao: $registro['ref_cod_instituicao']);
         $obj_instituicao_det = $obj_instituicao->detalhe();
         $registro['ref_cod_instituicao'] = $obj_instituicao_det['nm_instituicao'];
 
         $obj_permissoes = new clsPermissoes();
-        $nivel_usuario = $obj_permissoes->nivel_acesso($this->pessoa_logada);
+        $nivel_usuario = $obj_permissoes->nivel_acesso(int_idpes_usuario: $this->pessoa_logada);
         if ($nivel_usuario == 1) {
             if ($registro['ref_cod_instituicao']) {
-                $this->addDetalhe(['Instituição', "{$registro['ref_cod_instituicao']}"]);
+                $this->addDetalhe(detalhe: ['Instituição', "{$registro['ref_cod_instituicao']}"]);
             }
         }
         if ($registro['nm_nivel']) {
-            $this->addDetalhe(['Nível Ensino', "{$registro['nm_nivel']}"]);
+            $this->addDetalhe(detalhe: ['Nível Ensino', "{$registro['nm_nivel']}"]);
         }
         if ($registro['descricao']) {
-            $this->addDetalhe(['Descrição', "{$registro['descricao']}"]);
+            $this->addDetalhe(detalhe: ['Descrição', "{$registro['descricao']}"]);
         }
 
-        if ($obj_permissoes->permissao_cadastra(571, $this->pessoa_logada, 3)) {
+        if ($obj_permissoes->permissao_cadastra(int_processo_ap: 571, int_idpes_usuario: $this->pessoa_logada, int_soma_nivel_acesso: 3)) {
             $this->url_novo = 'educar_nivel_ensino_cad.php';
             $this->url_editar = "educar_nivel_ensino_cad.php?cod_nivel_ensino={$registro['cod_nivel_ensino']}";
         }
         $this->url_cancelar = 'educar_nivel_ensino_lst.php';
         $this->largura = '100%';
 
-        $this->breadcrumb('Detalhe do nível de ensino', [
-            url('intranet/educar_index.php') => 'Escola',
+        $this->breadcrumb(currentPage: 'Detalhe do nível de ensino', breadcrumbs: [
+            url(path: 'intranet/educar_index.php') => 'Escola',
         ]);
     }
 
--- ieducar/intranet/educar_nivel_ensino_lst.php
+++ ieducar/intranet/educar_nivel_ensino_lst.php
@@ -49,7 +49,7 @@ return new class extends clsListagem {
             $this->$var = ($val === '') ? null: $val;
         }
 
-        $this->addCabecalhos([
+        $this->addCabecalhos(coluna: [
             'Nivel Ensino'
         ]);
 
@@ -58,39 +58,39 @@ return new class extends clsListagem {
         ];
 
         $obj_permissoes = new clsPermissoes();
-        $nivel_usuario = $obj_permissoes->nivel_acesso($this->pessoa_logada);
+        $nivel_usuario = $obj_permissoes->nivel_acesso(int_idpes_usuario: $this->pessoa_logada);
 
         if ($nivel_usuario == 1) {
             $lista_busca[] = 'Instituição';
         }
-        $this->addCabecalhos($lista_busca);
+        $this->addCabecalhos(coluna: $lista_busca);
 
         // Filtros de Foreign Keys
         include('include/pmieducar/educar_campo_lista.php');
 
         // outros Filtros
-        $this->campoTexto('nm_nivel', 'Nível Ensino', $this->nm_nivel, 30, 255, false);
+        $this->campoTexto(nome: 'nm_nivel', campo: 'Nível Ensino', valor: $this->nm_nivel, tamanhovisivel: 30, tamanhomaximo: 255);
 
         // Paginador
         $this->limite = 20;
 
         $query = LegacyEducationLevel::query()
-            ->where('ativo', 1)
-            ->orderBy('nm_nivel', 'ASC');
+            ->where(column: 'ativo', operator: 1)
+            ->orderBy(column: 'nm_nivel', direction: 'ASC');
 
-        if (is_string($this->nm_nivel)) {
-            $query->where('nm_nivel', 'ilike', '%' . $this->nm_nivel . '%');
+        if (is_string(value: $this->nm_nivel)) {
+            $query->where(column: 'nm_nivel', operator: 'ilike', value: '%' . $this->nm_nivel . '%');
         }
 
-        $result = $query->paginate($this->limite, pageName: 'pagina_'.$this->nome);
+        $result = $query->paginate(perPage: $this->limite, pageName: 'pagina_'.$this->nome);
 
         $lista = $result->items();
         $total = $result->total();
 
         // monta a lista
-        if (is_array($lista) && count($lista)) {
+        if (is_array(value: $lista) && count(value: $lista)) {
             foreach ($lista as $registro) {
-                $obj_cod_instituicao = new clsPmieducarInstituicao($registro['ref_cod_instituicao']);
+                $obj_cod_instituicao = new clsPmieducarInstituicao(cod_instituicao: $registro['ref_cod_instituicao']);
                 $obj_cod_instituicao_det = $obj_cod_instituicao->detalhe();
                 $registro['ref_cod_instituicao'] = $obj_cod_instituicao_det['nm_instituicao'];
 
@@ -101,19 +101,19 @@ return new class extends clsListagem {
                 if ($nivel_usuario == 1) {
                     $lista_busca[] = "<a href=\"educar_nivel_ensino_det.php?cod_nivel_ensino={$registro['cod_nivel_ensino']}\">{$registro['ref_cod_instituicao']}</a>";
                 }
-                $this->addLinhas($lista_busca);
+                $this->addLinhas(linha: $lista_busca);
             }
         }
-        $this->addPaginador2('educar_nivel_ensino_lst.php', $total, $_GET, $this->nome, $this->limite);
+        $this->addPaginador2(strUrl: 'educar_nivel_ensino_lst.php', intTotalRegistros: $total, mixVariaveisMantidas: $_GET, nome: $this->nome, intResultadosPorPagina: $this->limite);
 
-        if ($obj_permissoes->permissao_cadastra(571, $this->pessoa_logada, 3)) {
+        if ($obj_permissoes->permissao_cadastra(int_processo_ap: 571, int_idpes_usuario: $this->pessoa_logada, int_soma_nivel_acesso: 3)) {
             $this->acao = 'go("educar_nivel_ensino_cad.php")';
             $this->nome_acao = 'Novo';
         }
         $this->largura = '100%';
 
-        $this->breadcrumb('Listagem de níveis de ensino', [
-            url('intranet/educar_index.php') => 'Escola',
+        $this->breadcrumb(currentPage: 'Listagem de níveis de ensino', breadcrumbs: [
+            url(path: 'intranet/educar_index.php') => 'Escola',
         ]);
     }
 
