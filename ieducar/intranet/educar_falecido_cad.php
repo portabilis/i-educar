85d3159946b18e6ef5c17c6e3615d06e6e282177
Joalisson Barros <joalissonricardo@gmail.com>
2022-12-22 12:05:53 -0300

    Adiciona named parameters em Falecido

--- ieducar/intranet/educar_falecido_cad.php
+++ ieducar/intranet/educar_falecido_cad.php
@@ -1,11 +1,6 @@
 <?php
 
 return new class extends clsCadastro {
-    /**
-     * Referencia pega da session para o idpes do usuario atual
-     *
-     * @var int
-     */
     public $pessoa_logada;
 
     public function Inicializar()
@@ -16,11 +11,11 @@ return new class extends clsCadastro {
         $this->ref_cod_aluno=$_GET['ref_cod_aluno'];
 
         $obj_permissoes = new clsPermissoes();
-        $obj_permissoes->permissao_cadastra(578, $this->pessoa_logada, 7, "educar_matricula_lst.php?ref_cod_aluno={$this->ref_cod_aluno}");
+        $obj_permissoes->permissao_cadastra(int_processo_ap: 578, int_idpes_usuario: $this->pessoa_logada, int_soma_nivel_acesso: 7, str_pagina_redirecionar: "educar_matricula_lst.php?ref_cod_aluno={$this->ref_cod_aluno}");
 
         $this->url_cancelar = "educar_matricula_det.php?cod_matricula={$this->ref_cod_matricula}";
 
-        $this->breadcrumb('Registro do falecimento do aluno', [
+        $this->breadcrumb(currentPage: 'Registro do falecimento do aluno', breadcrumbs: [
             url('intranet/educar_index.php') => 'Escola',
         ]);
 
@@ -32,26 +27,26 @@ return new class extends clsCadastro {
     public function Gerar()
     {
         // primary keys
-        $this->campoOculto('ref_cod_aluno', $this->ref_cod_aluno);
-        $this->campoOculto('ref_cod_matricula', $this->ref_cod_matricula);
+        $this->campoOculto(nome: 'ref_cod_aluno', valor: $this->ref_cod_aluno);
+        $this->campoOculto(nome: 'ref_cod_matricula', valor: $this->ref_cod_matricula);
 
         $obj_aluno = new clsPmieducarAluno();
-        $lst_aluno = $obj_aluno->lista($this->ref_cod_aluno, null, null, null, null, null, null, null, null, null, 1);
+        $lst_aluno = $obj_aluno->lista(int_cod_aluno: $this->ref_cod_aluno, int_ativo: 1);
         if (is_array($lst_aluno)) {
             $det_aluno = array_shift($lst_aluno);
             $this->nm_aluno = $det_aluno['nome_aluno'];
-            $this->campoTexto('nm_aluno', 'Aluno', $this->nm_aluno, 30, 255, false, false, false, '', '', '', '', true);
+            $this->campoTexto(nome: 'nm_aluno', campo: 'Aluno', valor: $this->nm_aluno, tamanhovisivel: 30, tamanhomaximo: 255, evento: '', disabled: true);
         }
 
-        $this->inputsHelper()->date('data_cancel', ['label' => 'Data do falecimento', 'placeholder' => 'dd/mm/yyyy', 'value' => date('d/m/Y')]);
+        $this->inputsHelper()->date(attrName: 'data_cancel', inputOptions: ['label' => 'Data do falecimento', 'placeholder' => 'dd/mm/yyyy', 'value' => date('d/m/Y')]);
 
-        $this->campoMemo('observacao', 'Observação', $this->observacao, 60, 5, false);
+        $this->campoMemo(nome: 'observacao', campo: 'Observação', valor: $this->observacao, colunas: 60, linhas: 5);
     }
 
     public function Novo()
     {
         $obj_permissoes = new clsPermissoes();
-        $obj_permissoes->permissao_cadastra(578, $this->pessoa_logada, 7, "educar_matricula_det.php?cod_matricula={$this->ref_cod_matricula}");
+        $obj_permissoes->permissao_cadastra(int_processo_ap: 578, int_idpes_usuario: $this->pessoa_logada, int_soma_nivel_acesso: 7, str_pagina_redirecionar: "educar_matricula_det.php?cod_matricula={$this->ref_cod_matricula}");
 
         $tamanhoObs = strlen($this->observacao);
         if ($tamanhoObs > 300) {
@@ -60,19 +55,19 @@ return new class extends clsCadastro {
             return false;
         }
 
-        $obj_matricula = new clsPmieducarMatricula($this->ref_cod_matricula, null, null, null, $this->pessoa_logada, null, null, 6);
+        $obj_matricula = new clsPmieducarMatricula(cod_matricula: $this->ref_cod_matricula, ref_cod_reserva_vaga: null, ref_ref_cod_escola: null, ref_ref_cod_serie: null, ref_usuario_exc: $this->pessoa_logada, ref_usuario_cad: null, ref_cod_aluno: null, aprovado: 6);
         $obj_matricula->data_cancel = Portabilis_Date_Utils::brToPgSQL($this->data_cancel);
 
         $det_matricula = $obj_matricula->detalhe();
 
         if (is_null($det_matricula['data_matricula'])) {
-            if (substr($det_matricula['data_cadastro'], 0, 10) > $obj_matricula->data_cancel) {
+            if (substr(string: $det_matricula['data_cadastro'], offset: 0, length: 10) > $obj_matricula->data_cancel) {
                 $this->mensagem = 'Data de falecimento não pode ser inferior a data da matrícula.<br>';
 
                 return false;
             }
         } else {
-            if (substr($det_matricula['data_matricula'], 0, 10) > $obj_matricula->data_cancel) {
+            if (substr(string: $det_matricula['data_matricula'], offset: 0, length: 10) > $obj_matricula->data_cancel) {
                 $this->mensagem = 'Data de falecimento não pode ser inferior a data da matrícula.<br>';
 
                 return false;
@@ -82,10 +77,10 @@ return new class extends clsCadastro {
         if ($obj_matricula->edita()) {
             if ($obj_matricula->cadastraObservacaoFalecido($this->observacao)) {
                 $enturmacoes = new clsPmieducarMatriculaTurma();
-                $enturmacoes = $enturmacoes->lista($this->ref_cod_matricula, null, null, null, null, null, null, null, 1);
+                $enturmacoes = $enturmacoes->lista(int_ref_cod_matricula: $this->ref_cod_matricula, int_ativo: 1);
 
                 foreach ($enturmacoes as $enturmacao) {
-                    $enturmacao = new clsPmieducarMatriculaTurma($this->ref_cod_matricula, $enturmacao['ref_cod_turma'], $this->pessoa_logada, null, null, null, 0, null, $enturmacao['sequencial']);
+                    $enturmacao = new clsPmieducarMatriculaTurma(ref_cod_matricula: $this->ref_cod_matricula, ref_cod_turma: $enturmacao['ref_cod_turma'], ref_usuario_exc: $this->pessoa_logada, ref_usuario_cad: null, data_cadastro: null, data_exclusao: null, ativo: 0, ref_cod_turma_transf: null, sequencial: $enturmacao['sequencial']);
 
                     if (! $enturmacao->edita()) {
                         $this->mensagem = 'Não foi possível desativar as enturmações da matrícula.';
@@ -97,11 +92,11 @@ return new class extends clsCadastro {
                 }
 
                 $notaAluno = (new Avaliacao_Model_NotaAlunoDataMapper())
-                                    ->findAll(['id'], ['matricula_id' => $obj_matricula->cod_matricula])[0] ?? null;
+                                    ->findAll(columns: ['id'], where: ['matricula_id' => $obj_matricula->cod_matricula])[0] ?? null;
 
                 if (!empty($notaAluno)) {
                     (new Avaliacao_Model_NotaComponenteMediaDataMapper())
-                        ->updateSituation($notaAluno->get('id'), App_Model_MatriculaSituacao::FALECIDO);
+                        ->updateSituation(notaAlunoId: $notaAluno->get('id'), situacao: App_Model_MatriculaSituacao::FALECIDO);
                 }
 
                 $this->mensagem .= 'Alteração realizado com sucesso.<br>';
