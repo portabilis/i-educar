0e02a55be1e52e4d570423e6893edb076a432e14
xlcn2 <lucianopeas@gmail.com>
2023-01-06 11:09:41 -0300

    dynamicInput informações de ferquencia

--- ieducar/modules/DynamicInput/Views/FrequenciaComponenteController.php
+++ ieducar/modules/DynamicInput/Views/FrequenciaComponenteController.php
@@ -18,9 +18,9 @@ class FrequenciaComponenteController extends ApiCoreController
         $ComponenteId = $this->getRequest()->componente_id;
         $ano = $this->getRequest()->ano;
         
-        $data_freq = ''.$this->getRequest()->data_frequencia;
+        $data_freq = $this->getRequest()->data_frequencia;
         $data_freq  = strtotime($input);
-        $data_freq = date('Y-M-d', $date);
+        $data_freq = date('Y-m-d',  $data_freq);
        
             $options = [];
           
