56075bfbfff34d258e22a23f755ef1c07adc72b4
xlcn2 <lucianopeas@gmail.com>
2023-01-17 01:12:24 -0300

    dynamicInput professor componente curricular

--- ieducar/lib/Portabilis/View/Helper/DynamicInput/ProfessorComponente.php
+++ ieducar/lib/Portabilis/View/Helper/DynamicInput/ProfessorComponente.php
@@ -14,97 +14,58 @@ class Portabilis_View_Helper_DynamicInput_ProfessorComponente extends Portabilis
         $escolaId = $this->getEscolaId($options['escolaId'] ?? null);
         $serieId = $this->getSerieId($options['serieId'] ?? null);
         $turmaId = $this->getTurmaId($options['turmaId'] ?? null);
+        $ComponenteId =
         $anoLetivo = $this->getAno($options['ano'] ?? null);
 
         $userId = $this->getCurrentUserId();
 
-        if ($turmaId and $anoLetivo and empty($resources)) {
-            $isOnlyProfessor = Portabilis_Business_Professor::isOnlyProfessor($instituicaoId, $userId);
+      
+        $pessoa_id = 0;
+        if(empty($ComponenteId) and !empty($turmaId)){
 
-            if ($isOnlyProfessor) {
-                $componentesCurriculares = Portabilis_Business_Professor::componentesCurricularesAlocado($instituicaoId, $turmaId, $ano, $userId);
-            } else {
-                $sql = "
-                        SELECT
-                            cc.id,
-                            cc.nome,
-                            ac.nome as area_conhecimento,
-                            ac.secao as area_conhecimento_secao,
-                            cc.ordenamento
-                        FROM
-                            pmieducar.turma,
-                            modules.componente_curricular_turma as cct,
-                            modules.componente_curricular as cc,
-                            modules.area_conhecimento as ac,
-                            pmieducar.escola_ano_letivo as al
-                        WHERE turma.cod_turma = {$turmaId}
-                            AND cct.turma_id = turma.cod_turma
-                            AND cct.escola_id = turma.ref_ref_cod_escola
-                            AND cct.componente_curricular_id = cc.id
-                            AND al.ano = {$anoLetivo}
-                            AND cct.escola_id = al.ref_cod_escola
-                            AND cc.area_conhecimento_id = ac.id
-                        ORDER BY
-                            ac.secao,
-                            ac.nome,
-                            cc.ordenamento,
-                        cc.nome
-                ";
+        $professor_turma = ProfessorTurma::where('turma_id', $turmaId)->get(); 
+        foreach($professor_turma as $professores_turma){
 
-                $db = new clsBanco();
-                $db->Consulta($sql);
+        
+        $pessoa = Pessoa::where('idpes', $professores_turma->servidor_id)->get(); 
+        foreach($pessoa as $pessoas){
 
-                while ($db->ProximoRegistro()) {
-                    $componentesCurriculares[] = $db->Tupla();
-                }
+            $options[
+                '__' . $pessoas->idpes
+            ] = [
+                'value' => mb_strtoupper($pessoas->idpes." - ".$pessoas->nome, 'UTF-8'),
+                'checked' => "checked",
+                'group' => ''
+            ];
 
-                if (!$componentesCurriculares) {
-                    $sql = "
-                                SELECT
-                                    cc.id,
-                                    cc.nome,
-                                    ac.nome as area_conhecimento,
-                                    ac.secao as secao_area_conhecimento,
-                                    cc.ordenamento
-                                FROM
-                                    pmieducar.turma as t,
-                                    pmieducar.escola_serie_disciplina as esd,
-                                    modules.componente_curricular as cc,
-                                    modules.area_conhecimento as ac,
-                                    pmieducar.escola_ano_letivo as al
-                                WHERE t.cod_turma = {$turmaId}
-                                    AND esd.ref_ref_cod_escola = t.ref_ref_cod_escola
-                                    AND esd.ref_ref_cod_serie = t.ref_ref_cod_serie
-                                    AND esd.ref_cod_disciplina = cc.id
-                                    AND al.ano = {$anoLetivo}
-                                    AND esd.ref_ref_cod_escola = al.ref_cod_escola
-                                    AND t.ativo = 1
-                                    AND esd.ativo = 1
-                                    AND al.ativo = 1
-                                    AND {$anoLetivo} = ANY(esd.anos_letivos)
-                                    AND cc.area_conhecimento_id = ac.id
-                                ORDER BY
-                                    ac.secao,
-                                    ac.nome,
-                                    cc.ordenamento,
-                                    cc.nome
-                            ";
-
-                    $db = new clsBanco();
-                    $db->Consulta($sql);
-
-                    while ($db->ProximoRegistro()) {
-                        $componentesCurriculares[] = $db->Tupla();
-                    }
-                }
-            }
-            $ultimo_nome ='';
-            foreach ($componentesCurriculares as $key => $componentesCurricular) {
-                $resources[$componentesCurricular['id']] = 'teste';
-                $ultimo_nome = $componentesCurricular['nome'];
-            }
+        }
             
-        } 
+        }
+
+    } elseif(!empty($ComponenteId) and !empty($turmaId)){
+
+        $professor_turma = ProfessorTurma::where('turma_id', $turmaId)->get(); 
+        foreach($professor_turma as $professores_turma){
+
+            $professor_disciplina = ProfessorDisciplina::where('ref_cod_disciplina',$ComponenteId)->where('ref_cod_servidor',$professores_turma->servidor_id)->get(); 
+            foreach($professor_disciplina as $professores_disciplina){
+
+            
+                $pessoa = Pessoa::where('idpes', $professores_disciplina->ref_cod_servidor)->get(); 
+                foreach($pessoa as $pessoas){
+                    
+                    $resources[$pessoas->idpes] = $pessoas->idpes.' - '.$pessoas->nome;
+                    $ultimo_nome = $pessoas->nome;
+
+                }
+                
+            }
+        }
+
+    }
+
+             
+         
 
         return $this->insertOption(null,  $ultimo_nome, $resources);
     }
