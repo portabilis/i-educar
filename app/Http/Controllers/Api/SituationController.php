3db0ae5d52a68d816f840b12d6a541d0ba788f76
Robert Ferraz <robert@portabilis.com.br>
2023-02-02 15:29:15 -0300

    Cria o controlador para o endpoint da situação

--- app/Http/Controllers/Api/SituationController.php
+++ app/Http/Controllers/Api/SituationController.php
@@ -3,27 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\ResourceController;
-use Illuminate\Http\Resources\Json\JsonResource;
 
 class SituationController extends ResourceController
 {
 
-    public function index(): JsonResource
+    public function index(): array
     {
-        return $this->newResource([
-            9 => 'Exceto Transferidos/Abandono',
-            0 => 'Todos',
-            1 => 'Aprovado',
-            2 => 'Reprovado',
-            3 => 'Cursando',
-            4 => 'Transferido',
-            5 => 'Reclassificado',
-            6 => 'Abandono',
-            8 => 'Aprovado sem exame',
-            10 => 'Aprovado após exame',
-            12 => 'Aprovado com dependência',
-            13 => 'Aprovado pelo conselho',
-            14 => 'Reprovado por falta'
-        ]);
+        return [
+            'data' => [
+                9 => 'Exceto Transferidos/Abandono',
+                0 => 'Todos',
+                1 => 'Aprovado',
+                2 => 'Reprovado',
+                3 => 'Cursando',
+                4 => 'Transferido',
+                5 => 'Reclassificado',
+                6 => 'Abandono',
+                8 => 'Aprovado sem exame',
+                10 => 'Aprovado após exame',
+                12 => 'Aprovado com dependência',
+                13 => 'Aprovado pelo conselho',
+                14 => 'Reprovado por falta'
+            ]
+        ];
     }
 }
