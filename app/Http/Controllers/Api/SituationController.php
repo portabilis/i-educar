13bdce6d421ab25e381459192cbf6fb9a1d23cf2
Robert Ferraz <robert@portabilis.com.br>
2023-02-06 11:37:01 -0300

    Substitui dados pela classe de filtros de situações

--- app/Http/Controllers/Api/SituationController.php
+++ app/Http/Controllers/Api/SituationController.php
@@ -3,27 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\ResourceController;
+use iEducar\Modules\Enrollments\Model\EnrollmentStatusFilter;
 
 class SituationController extends ResourceController
 {
     public function index(): array
     {
         return [
-            'data' => [
-                9 => 'Exceto Transferidos/Abandono',
-                0 => 'Todos',
-                1 => 'Aprovado',
-                2 => 'Reprovado',
-                3 => 'Cursando',
-                4 => 'Transferido',
-                5 => 'Reclassificado',
-                6 => 'Abandono',
-                8 => 'Aprovado sem exame',
-                10 => 'Aprovado após exame',
-                12 => 'Aprovado com dependência',
-                13 => 'Aprovado pelo conselho',
-                14 => 'Reprovado por falta'
-            ]
+          'data' => EnrollmentStatusFilter::getDescriptiveValues()
         ];
     }
 }
