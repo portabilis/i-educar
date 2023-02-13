bf295499d8928be2f859b4b5f7c25826294b6cfc
jrbarros <joalissonricardo@gmail.com>
2023-02-13 08:59:05 -0300

    Refatora busca de falta

--- ieducar/modules/Avaliacao/Views/PromocaoApiController.php
+++ ieducar/modules/Avaliacao/Views/PromocaoApiController.php
@@ -231,7 +231,7 @@ class PromocaoApiController extends ApiCoreController
 
                 if ($hasNotaOrParecerInEtapa) {
                     // FIXME #parameters
-                    $falta = $this->boletimService()->getFalta($etapa) ? $this->boletimService()->getFalta($etapa)->quantidade : null;
+                    $falta = $this->boletimService()->getFalta($etapa)?->quantidade;
 
                     if (is_null($falta)) {
                         $notaFalta = new Avaliacao_Model_FaltaGeral([
@@ -253,7 +253,7 @@ class PromocaoApiController extends ApiCoreController
 
                     if ($regraNaoUsaNota || trim($nota) != '' || trim($parecer) != '') {
                         // FIXME #parameters
-                        $falta = $this->boletimService()->getFalta($etapa, $key)->quantidade;
+                        $falta = $this->boletimService()->getFalta($etapa, $key)?->quantidade;
 
                         if (is_null($falta)) {
                             // FIXME #parameters
