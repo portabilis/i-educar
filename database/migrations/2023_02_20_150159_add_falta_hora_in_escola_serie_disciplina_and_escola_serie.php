f3a6862964d133256796499115a5e8bc2bcd9c33
jrbarros <joalissonricardo@gmail.com>
2023-02-23 15:12:16 -0300

    Adiciona migration

---
+++ database/migrations/2023_02_20_150159_add_falta_hora_in_escola_serie_disciplina_and_escola_serie.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('modules.componente_curricular_ano_escolar', function (Blueprint $table) {
+            $table->decimal('hora_falta', 7,3)
+                ->nullable()
+                ->default(null);
+        });
+
+        Schema::table('pmieducar.escola_serie_disciplina', function (Blueprint $table) {
+            $table->decimal('hora_falta',7,3)
+                ->nullable()
+                ->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('modules.componente_curricular_ano_escolar', function (Blueprint $table) {
+            $table->dropColumn([
+                'hora_falta'
+            ]);
+        });
+
+        Schema::table('pmieducar.escola_serie_disciplina', function (Blueprint $table) {
+            $table->dropColumn([
+                'hora_falta'
+            ]);
+        });;
+    }
+};
