4ca9bf20331374df1fcb4758fd51b039b0814b95
Edinei Valdameri <edineivaldameri@gmail.com>
2023-02-27 15:12:35 -0300

    Adiciona factory, teste e model LegacyCalendarDayReason

---
+++ database/factories/LegacyCalendarDayReasonFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\LegacyCalendarDayReason;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LegacyCalendarDayReasonFactory extends Factory
+{
+    public $model = LegacyCalendarDayReason::class;
+
+    public function definition()
+    {
+        return [
+            'ref_cod_escola' => LegacySchoolFactory::new()->create(),
+            'sigla' => $this->faker->word,
+            'descricao' => $this->faker->sentence,
+            'tipo' => $this->faker->randomElement(['e', 'n']),
+            'nm_motivo' => $this->faker->word,
+            'ref_usuario_exc' =>  fn () => LegacyUserFactory::new()->unique()->make(),
+            'ref_usuario_cad' => fn () => LegacyUserFactory::new()->unique()->make(),
+        ];
+    }
+}
