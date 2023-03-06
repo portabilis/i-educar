c7565d0cf0b5bbbb5694d45ff4d29bd8a0c8468e
Robert Ferraz <robert@portabilis.com.br>
2023-03-06 20:42:47 -0300

    Cria tipo de deficiência

---
+++ database/migrations/2023_03_06_100423_update_title_menu_deficiency.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Menu;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    public function up()
+    {
+        Menu::where('process', '631')->update([
+            'title' => 'Tipos de deficiência e transtornos',
+            'description' => 'Tipos de deficiência e transtornos da pessoa'
+        ]);
+    }
+
+    public function down()
+    {
+        Menu::where('process', '631')->update([
+            'title' => 'Tipos de deficiência',
+            'description' => 'Tipos de deficiência da pessoa'
+        ]);
+    }
+};
