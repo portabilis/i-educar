ac543f1186131232892df88a7426809789610fed
Edinei Valdameri <edineivaldameri@gmail.com>
2023-03-15 17:57:05 -0300

    Renomeia variável $routeMiddleware para $middlewareAliases no Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,7 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
-    protected $routeMiddleware = [
+    protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
