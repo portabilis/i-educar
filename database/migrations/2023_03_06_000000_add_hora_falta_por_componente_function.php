82e2337959311b260525d803b37f654fae63b4d9
Edinei Valdameri <edineivaldameri@gmail.com>
2023-03-16 16:53:40 -0300

    Style code

--- database/migrations/2023_03_06_000000_add_hora_falta_por_componente_function.php
+++ database/migrations/2023_03_06_000000_add_hora_falta_por_componente_function.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
 return new class () extends Migration {
-
     public function up()
     {
         DB::unprepared(
