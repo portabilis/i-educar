a4dcf282b0a675c088d66b8503270eb8351d576f
Edinei Valdameri <edineivaldameri@gmail.com>
2023-03-27 20:13:53 -0300

    Corrige nomenclatura da chave

--- database/migrations/2023_03_27_165100_organiza_primary_key_table_modules_educacenso_cod_aluno.php
+++ database/migrations/2023_03_27_165100_organiza_primary_key_table_modules_educacenso_cod_aluno.php
@@ -22,7 +22,7 @@ return new class () extends Migration {
     public function down()
     {
         DB::unprepared('ALTER TABLE IF EXISTS modules.educacenso_cod_aluno DROP CONSTRAINT IF EXISTS educacenso_cod_aluno_pk;');
-        DB::unprepared('ALTER TABLE IF EXISTS modules.educacenso_cod_aluno DROP CONSTRAINT IF EXISTS educacenso_cod_aluno_pk;');
+        DB::unprepared('ALTER TABLE IF EXISTS modules.educacenso_cod_aluno DROP CONSTRAINT IF EXISTS educacenso_cod_aluno_pkey;');
         DB::unprepared('ALTER TABLE IF EXISTS modules.educacenso_cod_aluno DROP CONSTRAINT IF EXISTS educacenso_cod_aluno_cod_aluno_cod_aluno_inep_unique;');
         Schema::table('modules.educacenso_cod_aluno', function (Blueprint $table) {
             $table->dropColumn('id');
