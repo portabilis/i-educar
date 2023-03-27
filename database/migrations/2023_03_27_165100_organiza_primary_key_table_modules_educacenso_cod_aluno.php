6f4801d2ba5a643ce950a4f0597a42bc3df2e77f
Edinei Valdameri <edineivaldameri@gmail.com>
2023-03-27 19:54:49 -0300

    Adiciona migration para organizar a tabela `modules.educacenso_cod_aluno`

---
+++ database/migrations/2023_03_27_165100_organiza_primary_key_table_modules_educacenso_cod_aluno.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up()
+    {
+        DB::unprepared('ALTER TABLE IF EXISTS modules.educacenso_cod_aluno DROP CONSTRAINT IF EXISTS educacenso_cod_aluno_pk;');
+
+        Schema::table('educacenso_cod_aluno', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unique([
+                'cod_aluno',
+                'cod_aluno_inep'
+            ]);
+        });
+    }
+
+    public function down()
+    {
+        DB::unprepared('ALTER TABLE IF EXISTS modules.educacenso_cod_aluno DROP CONSTRAINT IF EXISTS educacenso_cod_aluno_pk;');
+        DB::unprepared('ALTER TABLE IF EXISTS modules.educacenso_cod_aluno DROP CONSTRAINT IF EXISTS educacenso_cod_aluno_pk;');
+        DB::unprepared('ALTER TABLE IF EXISTS modules.educacenso_cod_aluno DROP CONSTRAINT IF EXISTS educacenso_cod_aluno_cod_aluno_cod_aluno_inep_unique;');
+        Schema::table('educacenso_cod_aluno', function (Blueprint $table) {
+            $table->dropColumn('id');
+        });
+        DB::unprepared('ALTER TABLE IF EXISTS modules.educacenso_cod_aluno ADD CONSTRAINT educacenso_cod_aluno_pk PRIMARY KEY (cod_aluno, cod_aluno_inep);');
+    }
+};
