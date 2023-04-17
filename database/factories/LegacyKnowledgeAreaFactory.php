87e24d5d44565c6095d95a8e6eb53e30049ccfe3
Eder Soares <edersoares@me.com>
2023-04-17 16:04:32 -0300

    Disciplinas

--- database/factories/LegacyKnowledgeAreaFactory.php
+++ database/factories/LegacyKnowledgeAreaFactory.php
@@ -7,24 +7,118 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class LegacyKnowledgeAreaFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = LegacyKnowledgeArea::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition(): array
     {
         return [
             'instituicao_id' => fn () => LegacyInstitutionFactory::new()->current(),
             'nome' => $this->faker->words(3, true),
         ];
+    }
+
+    public function earlyChildhoodEducation(): LegacyKnowledgeArea
+    {
+        $data = [
+            'nome' => 'Educação Infantil',
+        ];
+
+        return LegacyKnowledgeArea::query()->where($data)->first() ?? $this->withEarlyChildhoodEducationDisciplines()->create($data);
+    }
+
+    public function elementarySchool(): LegacyKnowledgeArea
+    {
+        $data = [
+            'nome' => 'Ensino Fundamental',
+        ];
+
+        return LegacyKnowledgeArea::query()->where($data)->first() ?? $this->withElementarySchoolDisciplines()->create($data);
+    }
+
+    public function withEarlyChildhoodEducationDisciplines(): static
+    {
+        return $this->afterCreating(function (LegacyKnowledgeArea $knowledgeArea) {
+            LegacyDisciplineFactory::new()->createMany([
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'O eu, o outro e o nós',
+                    'abbreviation' => 'EF',
+                ],
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'Corpo, gestos e movimento',
+                    'abbreviation' => 'EF',
+                ],
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'Traços, sons, cores e formas',
+                    'abbreviation' => 'EF',
+                ],
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'Escuta, fala, pensamento e imaginação',
+                    'abbreviation' => 'EF',
+                ],
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'Espaços, tempos, quantidades, relações e transformações',
+                    'abbreviation' => 'EF',
+                ],
+            ]);
+        });
+    }
+
+    public function withElementarySchoolDisciplines(): static
+    {
+        return $this->afterCreating(function (LegacyKnowledgeArea $knowledgeArea) {
+            LegacyDisciplineFactory::new()->createMany([
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'Ensino Religioso',
+                    'abbreviation' => 'ENR',
+                ],
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'História',
+                    'abbreviation' => 'HIS',
+                ],
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'Geografia',
+                    'abbreviation' => 'GEO',
+                ],
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'Matemática',
+                    'abbreviation' => 'MAT',
+                ],
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'Ciências',
+                    'abbreviation' => 'CIE',
+                ],
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'Língua Portuguesa',
+                    'abbreviation' => 'POR',
+                ],
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'Língua Inglesa',
+                    'abbreviation' => 'ING',
+                ],
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'Artes',
+                    'abbreviation' => 'ART',
+                ],
+                [
+                    'area_conhecimento_id' => $knowledgeArea,
+                    'name' => 'Educação Física',
+                    'abbreviation' => 'EDF',
+                ],
+            ]);
+        });
     }
 
     public function unique(): self
