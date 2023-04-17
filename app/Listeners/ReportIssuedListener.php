73e541b4b628efb12e950a119bccec870a32d0fa
Edinei Valdameri <edineivaldameri@gmail.com>
2023-04-17 18:40:23 -0300

    Adiciona event ReportIssued e seu Listener

---
+++ app/Listeners/ReportIssuedListener.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ReportIssued;
+use App\Models\ReportsCount;
+
+class ReportIssuedListener
+{
+    public function handle(ReportIssued $event): void
+    {
+        ReportsCount::updateOrCreate([
+            'render' => $event->render,
+            'template' => $event->template,
+            'success' => $event->success,
+            'date' => now()->format('Y-m-d'),
+        ]);
+    }
+}
