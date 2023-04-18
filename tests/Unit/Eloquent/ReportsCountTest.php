88813cabc517b4a8d34bf6ff7db8e7e5df492888
Edinei Valdameri <edineivaldameri@gmail.com>
2023-04-18 11:05:19 -0300

    Implementa teste para garantir o funcionamento da contagem

--- tests/Unit/Eloquent/ReportsCountTest.php
+++ tests/Unit/Eloquent/ReportsCountTest.php
@@ -14,4 +14,58 @@ class ReportsCountTest extends EloquentTestCase
     {
         return ReportsCount::class;
     }
+    /** @test  */
+    public function validIncrementCount()
+    {
+        ReportsCount::updateOrCreate([
+            'render' => 'html',
+            'template' => 'relatorio_html',
+            'success' => true,
+            'date' => now(),
+        ]);
+
+        ReportsCount::updateOrCreate([
+            'render' => 'html',
+            'template' => 'relatorio_html',
+            'success' => true,
+            'date' => now(),
+        ]);
+
+        ReportsCount::updateOrCreate([
+            'render' => 'html',
+            'template' => 'relatorio_html',
+            'success' => false,
+            'date' => now(),
+        ]);
+
+        ReportsCount::updateOrCreate([
+            'render' => 'html',
+            'template' => 'relatorio_html',
+            'success' => true,
+            'date' => now(),
+        ]);
+
+        ReportsCount::updateOrCreate([
+            'render' => 'html',
+            'template' => 'relatorio_html',
+            'success' => false,
+            'date' => now(),
+        ]);
+
+        $this->assertDatabaseHas(ReportsCount::class, [
+                'render' => 'html',
+                'template' => 'relatorio_html',
+                'success' => true,
+                'date' => now(),
+                'count' => 3
+            ]
+        )->assertDatabaseHas(ReportsCount::class, [
+            'render' => 'html',
+            'template' => 'relatorio_html',
+            'success' => false,
+            'date' => now(),
+            'count' => 2
+        ]
+    );
+    }
 }
