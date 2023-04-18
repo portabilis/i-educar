041e014fea950ae837118623f263a248e4e3b7dd
Eder Soares <edersoares@me.com>
2023-04-18 15:42:07 -0300

    Matrícula de uma escola

--- app/Models/LegacySchool.php
+++ app/Models/LegacySchool.php
@@ -232,4 +232,9 @@ class LegacySchool extends Model
     {
         return $this->belongsTo(LegacyPerson::class, 'ref_idpes_secretario_escolar');
     }
+
+    public function registrations(): HasMany
+    {
+        return $this->hasMany(LegacyRegistration::class, 'ref_ref_cod_escola');
+    }
 }
