52d0e654ce5bc0af866999e916714fe47e98c594
Eder Soares <edersoares@me.com>
2023-04-18 14:15:53 -0300

    Ajusta factories de notas

--- database/factories/LegacyDisciplineScoreFactory.php
+++ database/factories/LegacyDisciplineScoreFactory.php
@@ -25,6 +25,11 @@ class LegacyDisciplineScoreFactory extends Factory
             'nota_aluno_id' => fn () => LegacyRegistrationScoreFactory::new()->create(),
             'componente_curricular_id' => fn () => LegacyDisciplineFactory::new()->create(),
             'etapa' => $this->faker->randomElement([2, 3, 4]),
+            'nota' => 10,
+            'nota_arredondada' => 10,
+            'nota_recuperacao' => null,
+            'nota_original' => 10,
+            'nota_recuperacao_especifica' => null,
         ];
     }
 }
