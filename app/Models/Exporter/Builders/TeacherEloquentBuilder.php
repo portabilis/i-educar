dfe746533c537fee3f3daecb900ccdc486f013da
Edinei Valdameri <edineivaldameri@gmail.com>
2023-05-08 23:59:24 -0300

    Adiciona novos campos ao exportador de professores

--- app/Models/Exporter/Builders/TeacherEloquentBuilder.php
+++ app/Models/Exporter/Builders/TeacherEloquentBuilder.php
@@ -3,8 +3,11 @@
 namespace App\Models\Exporter\Builders;
 
 use App\Support\Database\JoinableBuilder;
+use iEducar\Modules\Servidores\Model\FuncaoExercida;
+use iEducar\Modules\Servidores\Model\TipoVinculo;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Query\JoinClause;
+use Illuminate\Support\Facades\DB;
 
 class TeacherEloquentBuilder extends Builder
 {
@@ -83,6 +86,53 @@ class TeacherEloquentBuilder extends Builder
 
         return $this->leftJoin('exporter_teacher_disciplines as disciplines', function (JoinClause $join) {
             $join->on('exporter_teacher.pivot_id', '=', 'disciplines.pivot_id');
+        });
+    }
+
+    public function allocations($columns)
+    {
+
+        if (in_array('funcao_exercida', $columns)) {
+            unset($columns[array_search('funcao_exercida', $columns)]);
+
+            $this->addSelect(DB::raw('
+                CASE pt.funcao_exercida
+                    WHEN ' . FuncaoExercida::DOCENTE . ' THEN \'Docente\'::varchar
+                    WHEN ' . FuncaoExercida::AUXILIAR_EDUCACIONAL . ' THEN \'Auxiliar/Assistente educacional\'::varchar
+                    WHEN ' . FuncaoExercida::MONITOR_ATIVIDADE_COMPLEMENTAR . ' THEN \'Profissional/Monitor de atividade complementar\'::varchar
+                    WHEN ' . FuncaoExercida::INTERPRETE_LIBRAS . ' THEN \'Tradutor Intérprete de LIBRAS\'::varchar
+                    WHEN ' . FuncaoExercida::DOCENTE_TITULAR_EAD . ' THEN \'Docente titular - Coordenador de tutoria (de módulo ou disciplina) - EAD\'::varchar
+                    WHEN ' . FuncaoExercida::DOCENTE_TUTOR_EAD . ' THEN \'Docente tutor - Auxiliar (de módulo ou disciplina) - EAD\'::varchar
+                    WHEN ' . FuncaoExercida::GUIA_INTERPRETE_LIBRAS . ' THEN \'Guia-Intérprete\'::varchar
+                    WHEN ' . FuncaoExercida::APOIO_ALUNOS_DEFICIENCIA . ' THEN \'Profissional de apoio escolar para aluno(a)s com deficiência (Lei 13.146/2015)\'::varchar
+                    WHEN ' . FuncaoExercida::INSTRUTOR_EDUCACAO_PROFISSIONAL . ' THEN \'Instrutor da Educação Profissional\'::varchar
+                    ELSE \'Não Informado\'::varchar
+                END AS funcao_exercida
+            '));
+        }
+
+        if (in_array('tipo_vinculo', $columns)) {
+            unset($columns[array_search('tipo_vinculo', $columns)]);
+
+            $this->addSelect(DB::raw('
+                CASE pt.tipo_vinculo
+                    WHEN ' . TipoVinculo::EFETIVO . ' THEN \'Concursado/efetivo/estável\'::varchar
+                    WHEN ' . TipoVinculo::TEMPORARIO . ' THEN \'Contrato temporário\'::varchar
+                    WHEN ' . TipoVinculo::TERCEIRIZADO . ' THEN \'Contrato terceirizado\'::varchar
+                    WHEN ' . TipoVinculo::CLT . ' THEN \'Contrato CLT\'::varchar
+                    ELSE \'Não Informado\'::varchar
+                END AS tipo_vinculo
+            '));
+        }
+
+        $this->addSelect(
+            $this->joinColumns('pt', $columns)
+        );
+
+        return $this->leftJoin('modules.professor_turma as pt', function (JoinClause $join) {
+            $join->on('pt.servidor_id', '=', 'exporter_teacher.cod_servidor')
+                ->whereRaw('pt.ano = exporter_teacher.year')
+                ->whereRaw('pt.turma_id = exporter_teacher.school_class_id');
         });
     }
 }
