06fed00505e9a753084f70b0131d91306b3ea814
Edinei Valdameri <edineivaldameri@gmail.com>
2023-05-22 11:42:19 -0300

    Altera a inclusão do código do usuário admin

--- database/migrations/2023_05_18_101047_update_ies_educacenso_2023.php
+++ database/migrations/2023_05_18_101047_update_ies_educacenso_2023.php
@@ -1,12 +1,19 @@
 <?php
 
 use App\Models\EducacensoInstitution;
+use App\Models\LegacyUser;
+use App\Models\LegacyUserType;
 use Illuminate\Database\Migrations\Migration;
 
 return new class () extends Migration {
     public function up(): void
     {
         $file = file(database_path('csv/censo/2023/update_or_create_ies_2023.csv'));
+
+        $admin = LegacyUser::query()
+            ->where('ativo', 1)
+            ->where('ref_cod_tipo_usuario', LegacyUserType::LEVEL_ADMIN)
+            ->first();
 
         foreach ($file as $line) {
             $data = str_getcsv(
@@ -20,7 +27,7 @@ return new class () extends Migration {
                 'nome' => $data[1],
                 'dependencia_administrativa_id' => $data[2],
                 'tipo_instituicao_id' => $data[3],
-                'user_id' => 1,
+                'user_id' => $admin->getKey(),
             ]);
         }
     }
