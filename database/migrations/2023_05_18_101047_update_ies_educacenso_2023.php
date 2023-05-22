a5d129f463b8d616b1df996e12bc57545d07d5e3
Edinei Valdameri <edineivaldameri@gmail.com>
2023-05-22 14:14:48 -0300

    Valida existência de usuário administrador

--- database/migrations/2023_05_18_101047_update_ies_educacenso_2023.php
+++ database/migrations/2023_05_18_101047_update_ies_educacenso_2023.php
@@ -15,20 +15,22 @@ return new class () extends Migration {
             ->where('ref_cod_tipo_usuario', LegacyUserType::LEVEL_ADMIN)
             ->first();
 
-        foreach ($file as $line) {
-            $data = str_getcsv(
-                string: $line,
-                separator: ';'
-            );
+        if ($admin) {
+            foreach ($file as $line) {
+                $data = str_getcsv(
+                    string: $line,
+                    separator: ';'
+                );
 
-            EducacensoInstitution::updateOrCreate([
-                'ies_id' => $data[0],
-            ], [
-                'nome' => $data[1],
-                'dependencia_administrativa_id' => $data[2],
-                'tipo_instituicao_id' => $data[3],
-                'user_id' => $admin->getKey(),
-            ]);
+                EducacensoInstitution::updateOrCreate([
+                    'ies_id' => $data[0],
+                ], [
+                    'nome' => $data[1],
+                    'dependencia_administrativa_id' => $data[2],
+                    'tipo_instituicao_id' => $data[3],
+                    'user_id' => $admin->getKey(),
+                ]);
+            }
         }
     }
 };
