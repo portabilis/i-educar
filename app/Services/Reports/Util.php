0b953b2b4a113992d0617956d5b05c8b79ea07a0
Robert Ferraz <robert@portabilis.com.br>
2023-07-04 15:39:21 -0300

    Adiciona função para formatar cep

--- app/Services/Reports/Util.php
+++ app/Services/Reports/Util.php
@@ -71,4 +71,32 @@ class Util
             default => [],
         })->toArray();
     }
+
+    public static function mask(string|int $val, string $mask): string
+    {
+        $maskared = '';
+        $k = 0;
+        for ($i = 0; $i <= strlen($mask) - 1; ++$i) {
+            if ($mask[$i] == '#') {
+                if (isset($val[$k])) {
+                    $maskared .= $val[$k++];
+                }
+            } else {
+                if (isset($mask[$i])) {
+                    $maskared .= $mask[$i];
+                }
+            }
+        }
+
+        return $maskared;
+    }
+
+    public static function formatPostcode(string $postcode): string
+    {
+        if (strlen($postcode) === 8) {
+            return self::mask('#####-###', $postcode);
+        }
+
+        return '';
+    }
 }
