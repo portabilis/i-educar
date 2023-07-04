eaf777bd0cb0c58fadc4460cf25fb9ac1bca089f
Robert Ferraz <robert@portabilis.com.br>
2023-07-04 11:55:28 -0300

    Adiciona builder e relacionamentos na tabela acessos

--- app/Models/LegacyAccess.php
+++ app/Models/LegacyAccess.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\Builders\LegacyAccessBuilder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class LegacyAccess extends LegacyModel
 {
     public const CREATED_AT = 'data_hora';
@@ -18,6 +21,8 @@ class LegacyAccess extends LegacyModel
      */
     protected $primaryKey = 'cod_acesso';
 
+    protected string $builder = LegacyAccessBuilder::class;
+
     public array $legacy = [
         'id' => 'cod_acesso',
         'access' => 'data_hora',
@@ -32,5 +37,20 @@ class LegacyAccess extends LegacyModel
         return $this->query()
             ->orderBy('data_hora', 'DESC')
             ->first();
+    }
+
+    public function person(): BelongsTo
+    {
+        return $this->belongsTo(LegacyPerson::class, 'cod_pessoa');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(LegacyUser::class, 'cod_pessoa');
+    }
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(LegacyEmployee::class, 'cod_pessoa');
     }
 }
