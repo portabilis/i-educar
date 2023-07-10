b8471ef52f534de19265c3b84ee2e5a8277cd7ad
Robert Ferraz <robert@portabilis.com.br>
2023-07-10 14:55:17 -0300

    Adiciona novos atributos e filtros

--- app/Models/LegacyCity.php
+++ app/Models/LegacyCity.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -68,5 +69,12 @@ class LegacyCity extends Model
     public function districts()
     {
         return $this->hasMany(LegacyDistrict::class, 'idmun', 'idmun');
+    }
+
+    protected function nameWithState(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->nome . '-' . $this->sigla_uf
+        );
     }
 }
