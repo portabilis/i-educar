08eadbe0fdcb630e05b4f2b5e55a5d9189b49388
Edinei Valdameri <edineivaldameri@gmail.com>
2023-08-29 21:41:02 -0300

    Adiciona novo campo para divisão de autenticados

--- app/Listeners/ReportIssuedListener.php
+++ app/Listeners/ReportIssuedListener.php
@@ -14,6 +14,7 @@ class ReportIssuedListener
             'template' => $event->template,
             'success' => $event->success,
             'date' => now(),
+            'authenticated' => $event->authenticate,
         ]);
     }
 }
