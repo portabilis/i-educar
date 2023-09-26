0a423c37fc3e19dc225e770bce15d01de7f8d8cb
Edinei Valdameri <99054215+edineivaldameri@users.noreply.github.com>
2023-09-26 10:52:05 -0300

    Update database/migrations/2023_08_29_213138_add_field_in_table_public_reports_counts.php
    Co-authored-by: Eder Soares <edersoares@me.com>

--- database/migrations/2023_08_29_213138_add_field_in_table_public_reports_counts.php
+++ database/migrations/2023_08_29_213138_add_field_in_table_public_reports_counts.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('public.reports_counts', function (Blueprint $table) {
-            $table->boolean('authenticated')->default(false);
+            $table->boolean('authenticated')->default(false)->index();
         });
     }
 
