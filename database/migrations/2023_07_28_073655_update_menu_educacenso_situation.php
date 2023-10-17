67030b526344436693b81a54e3afa0d7e7c17a95
Edinei Valdameri <edineivaldameri@gmail.com>
2023-10-17 14:05:58 -0300

    Altera migration para remover menu da Exportação da 2ª Fase

--- database/migrations/2023_07_28_073655_update_menu_educacenso_situation.php
+++ database/migrations/2023_07_28_073655_update_menu_educacenso_situation.php
@@ -7,11 +7,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Menu::where('process', '9998845')->update(['link' => '/educacenso/export-situation']);
-    }
-
-    public function down(): void
-    {
-        Menu::where('process', '9998845')->update(['link' => '/intranet/educar_exportacao_educacenso.php?fase2=1']);
+        Menu::where('process', '9998845')
+            ->delete();
     }
 };
