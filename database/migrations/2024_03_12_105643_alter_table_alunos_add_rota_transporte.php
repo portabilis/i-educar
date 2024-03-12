1d79a31c16cd70c204a9895eb1c9e8eaa6e74e06
Robert Ferraz <robert@portabilis.com.br>
2024-03-12 17:09:37 -0300

    Adiciona rota de transporte na tabela aluno

---
+++ database/migrations/2024_03_12_105643_alter_table_alunos_add_rota_transporte.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('pmieducar.aluno', function (Blueprint $table) {
+            $table->text('rota_transporte')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('pmieducar.aluno', function (Blueprint $table) {
+            $table->dropColumn('rota_transporte');
+        });
+    }
+};
