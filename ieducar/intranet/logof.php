b1c784e14966c83a32e51c41f35fabf1b4d70e36
Edinei Valdameri <edineivaldameri@gmail.com>
2024-06-12 01:42:35 -0300

    Dispara evento de logoff no arquivo legado

--- ieducar/intranet/logof.php
+++ ieducar/intranet/logof.php
@@ -1,9 +1,11 @@
 <?php
 
+use Illuminate\Auth\Events\Logout;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Session;
 
+event(new Logout('web', auth()->user()));
 Session::flush();
 
 throw new HttpResponseException(
