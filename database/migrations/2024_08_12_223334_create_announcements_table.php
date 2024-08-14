bee0cc242d716768968752ab8c6342c06b0e8d08
Robert Ferraz <robert@portabilis.com.br>
2024-08-14 14:38:59 -0300

    Cria migration do menu de publicação de avisos

--- database/migrations/2024_08_12_223334_create_announcements_table.php
+++ database/migrations/2024_08_12_223334_create_announcements_table.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Menu;
-use App\Process;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -48,20 +46,10 @@ return new class extends Migration
                 'announcement_id',
             ]);
         });
-
-        Menu::query()->updateOrCreate(['old' => Process::ANNOUNCEMENT], [
-            'parent_id' => Menu::query()->where('old', Process::CONFIGURATIONS_TOOLS)->firstOrFail()->getKey(),
-            'process' => Process::ANNOUNCEMENT,
-            'title' => 'Publicação de avisos',
-            'order' => 0,
-            'parent_old' => Process::CONFIGURATIONS_TOOLS,
-            'link' => '/avisos/publicacao',
-        ]);
     }
 
     public function down(): void
     {
-        Menu::query()->where('old', Process::ANNOUNCEMENT)->delete();
         Schema::dropIfExists('announcement_users');
         Schema::dropIfExists('announcement_user_types');
         Schema::dropIfExists('announcements');
