d18976bfffbb67579e07b838de201878118f3c40
Robert Ferraz <robert@portabilis.com.br>
2024-08-28 17:43:30 -0300

    Altera forma de salvamento

--- app/Http/Controllers/AnnouncementPublishController.php
+++ app/Http/Controllers/AnnouncementPublishController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\AnnouncementRequest;
 use App\Models\Announcement;
 use App\Process;
+use iEducar\Support\Exceptions\Exception;
 use Illuminate\Support\Facades\DB;
 
 class AnnouncementPublishController extends Controller
@@ -30,38 +31,34 @@ class AnnouncementPublishController extends Controller
 
     public function update(AnnouncementRequest $request, $announcementId)
     {
-        $announcement = DB::transaction(function () use ($announcementId, $request) {
+        try {
+            DB::beginTransaction();
             $announcement = Announcement::query()->withTrashed()->findOrFail($announcementId);
             $announcement->fill($request->all());
             $announcement->save();
             $announcement->userTypes()->sync($request->get('tipo_usuario'));
             $request->get('active') ? $announcement->restore() : $announcement->delete();
-
-            return $announcement;
-        });
-
-        if ($announcement) {
+            DB::commit();
             session()->flash('success', 'Edição efetuada com sucesso.');
-        } else {
+        } catch (Exception) {
+            DB::rollBack();
             session()->flash('error', 'Edição não realizada.');
         }
 
-        return redirect()->route('announcement.publish.edit', $announcement);
+        return redirect()->route('announcement.publish.edit', $announcementId);
     }
 
     public function store(AnnouncementRequest $request)
     {
-        $announcement = DB::transaction(function () use ($request) {
+        try {
+            DB::beginTransaction();
             $announcement = Announcement::create($request->all());
             $announcement->userTypes()->sync($request->get('tipo_usuario'));
             $request->get('active') ? $announcement->restore() : $announcement->delete();
-
-            return $announcement;
-        });
-
-        if ($announcement) {
+            DB::commit();
             session()->flash('success', 'Cadastro efetuado com sucesso.');
-        } else {
+        } catch (Exception) {
+            DB::rollBack();
             session()->flash('error', 'Cadastro não realizado.');
         }
 
