5e042ac3cdb3044830977d6ff8b61c1fd6042904
Murilo Martins <murilom676@gmail.com>
2025-11-17 18:04:11 -0300

    refactor: melhorar codigo com arrow functions e reduzir IFs

--- public/vendor/legacy/Cadastro/Assets/Javascripts/Addresses.js
+++ public/vendor/legacy/Cadastro/Assets/Javascripts/Addresses.js
@@ -1,86 +1,32 @@
 $j(document).ready(function () {
   const optional = $j('#search-postal-code').data('optional') === 1;
+  const CEP_FORMAT_REGEX = /[0-9]{5}-[0-9]{3}/;
+  const REPEATED_DIGITS_REGEX = /^(.)\1+$/;
+  const INVALID_CEPS = ['00000000', '99999999'];
 
-  var disableAddressing = function (flag) {
-    $j('#search-postal-code').css('opacity', flag ? 0.5 : 1);
-    $j('#address').attr('disabled', flag);
-    $j('#number').attr('disabled', flag);
-    $j('#complement').attr('disabled', flag);
-    $j('#neighborhood').attr('disabled', flag);
-    $j('#city_city').attr('disabled', flag);
+  const ERROR_MESSAGES = {
+    INVALID_FORMAT: 'Formato de CEP inválido. Use o formato: 00000-000',
+    INVALID_CEP: 'CEP inválido. Por favor, verifique o número digitado.',
+    NOT_FOUND: 'CEP não encontrado. Verifique se o número está correto ou preencha o endereço manualmente.',
+    SERVER_ERROR: 'Erro ao buscar CEP. Tente novamente ou preencha o endereço manualmente.'
   };
 
-  var searchCep = function () {
-    var postalCode = $j('#postal_code').val();
-    var regexp = /[0-9]{5}\-[0-9]{3}/;
-    var valid = regexp.test(postalCode);
+  const disableAddressing = (flag) => {
+    const disabled = !optional && flag;
+    $j('#search-postal-code').css('opacity', disabled ? 0.5 : 1);
+    $j('#address, #number, #complement, #neighborhood, #city_city').attr('disabled', disabled);
+  };
 
-    // Remove mensagem de erro anterior, se existir
+  const clearPostalCodeError = () => {
     $j('#postal_code').removeClass('error');
     $j('.postal-code-error-message').remove();
-
-    if (valid) {
-      var cleanPostalCode = postalCode.replace(/\D/g, '');
-
-      // Validar se não é um CEP obviamente inválido
-      if (cleanPostalCode === '00000000' || cleanPostalCode === '99999999' || /^(.)\1+$/.test(cleanPostalCode)) {
-        showPostalCodeError('CEP inválido. Por favor, verifique o número digitado.');
-        if (!optional) {
-          disableAddressing(true);
-        }
-        return;
-      }
-
-      $j('#postal_code_search_loading').css('visibility', 'visible');
-
-      $j.get('/api/postal-code/' + cleanPostalCode)
-        .done(function (res) {
-          $j('#address').val(res.address);
-          $j('#complement').val(res.complement);
-          $j('#neighborhood').val(res.neighborhood);
-          $j('#city_id').val(res.city.id);
-          $j('#city_city').val(res.city.id + ' - ' + res.city.name + ' (' + res.state_abbreviation + ')');
-
-          // Remove classe de erro em caso de sucesso
-          $j('#postal_code').removeClass('error');
-        })
-        .fail(function (xhr) {
-          // Exibir mensagem de erro quando CEP não é encontrado
-          if (xhr.status === 404) {
-            showPostalCodeError('CEP não encontrado. Verifique se o número está correto ou preencha o endereço manualmente.');
-          } else {
-            showPostalCodeError('Erro ao buscar CEP. Tente novamente ou preencha o endereço manualmente.');
-          }
-
-          if (!optional) {
-            disableAddressing(true);
-          }
-        })
-        .always(function() {
-          $j('#postal_code_search_loading').css('visibility', 'hidden');
-          if (!optional) {
-            disableAddressing(false);
-          }
-        });
-    } else {
-      // Formato de CEP inválido
-      if (postalCode && postalCode.length > 0) {
-        showPostalCodeError('Formato de CEP inválido. Use o formato: 00000-000');
-      }
-      if (!optional) {
-        disableAddressing(true);
-      }
-    }
   };
 
-  var showPostalCodeError = function(message) {
+  const showPostalCodeError = (message) => {
     $j('#postal_code').addClass('error');
-
-    // Remove mensagem anterior se existir
     $j('.postal-code-error-message').remove();
 
-    // Adiciona nova mensagem de erro
-    var errorMessage = $j('<span>')
+    $j('<span>')
       .addClass('postal-code-error-message error')
       .css({
         'color': '#b94a48',
@@ -88,25 +34,67 @@ $j(document).ready(function () {
         'margin-top': '5px',
         'font-size': '12px'
       })
-      .text(message);
+      .text(message)
+      .appendTo($j('#postal_code').parent());
 
-    $j('#postal_code').parent().append(errorMessage);
+    !optional && disableAddressing(true);
   };
 
-  var changePostalCode = function () {
-    var postalCode = $j('#postal_code').val();
-    var regexp = /[0-9]{5}\-[0-9]{3}/;
-    var valid = regexp.test(postalCode);
+  const isInvalidCep = (cleanCep) => {
+    return INVALID_CEPS.includes(cleanCep) || REPEATED_DIGITS_REGEX.test(cleanCep);
+  };
 
-    // Limpar mensagem de erro ao digitar
-    if (postalCode.length === 0) {
-      $j('#postal_code').removeClass('error');
-      $j('.postal-code-error-message').remove();
+  const fillAddressFields = (data) => {
+    $j('#address').val(data.address);
+    $j('#complement').val(data.complement);
+    $j('#neighborhood').val(data.neighborhood);
+    $j('#city_id').val(data.city.id);
+    $j('#city_city').val(`${data.city.id} - ${data.city.name} (${data.state_abbreviation})`);
+    clearPostalCodeError();
+  };
+
+  const handleApiError = (xhr) => {
+    const message = xhr.status === 404 ? ERROR_MESSAGES.NOT_FOUND : ERROR_MESSAGES.SERVER_ERROR;
+    showPostalCodeError(message);
+  };
+
+  const fetchPostalCode = (cleanCep) => {
+    $j('#postal_code_search_loading').css('visibility', 'visible');
+
+    $j.get(`/api/postal-code/${cleanCep}`)
+      .done(fillAddressFields)
+      .fail(handleApiError)
+      .always(() => {
+        $j('#postal_code_search_loading').css('visibility', 'hidden');
+        !optional && disableAddressing(false);
+      });
+  };
+
+  const searchCep = () => {
+    const postalCode = $j('#postal_code').val();
+    clearPostalCodeError();
+
+    if (!CEP_FORMAT_REGEX.test(postalCode)) {
+      postalCode && showPostalCodeError(ERROR_MESSAGES.INVALID_FORMAT);
+      return;
     }
 
-    if (!optional) {
-      disableAddressing(!valid);
+    const cleanCep = postalCode.replace(/\D/g, '');
+
+    if (isInvalidCep(cleanCep)) {
+      showPostalCodeError(ERROR_MESSAGES.INVALID_CEP);
+      return;
     }
+
+    fetchPostalCode(cleanCep);
+  };
+
+  const changePostalCode = () => {
+    const postalCode = $j('#postal_code').val();
+    const isValid = CEP_FORMAT_REGEX.test(postalCode);
+
+    !postalCode && clearPostalCodeError();
+    !optional && disableAddressing(!isValid);
   };
 
   $j('#search-postal-code').click(searchCep);
