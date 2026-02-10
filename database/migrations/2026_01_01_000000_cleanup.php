f08ddea4bf19c15dd54b3b228b8fc8076f8defed
Eder Soares <edersoares@me.com>
2026-02-10 19:08:06 -0300

    Remove `cadastro.endereco_externo`

---
+++ database/migrations/2026_01_01_000000_cleanup.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Support\Database\AsView;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    use AsView;
+
+    public function up(): void
+    {
+        $this->dropView('cadastro.endereco_externo');
+    }
+};
