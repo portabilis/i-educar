3f8836823a4986a54a74b6b551b157dcfcb2c031
Robert Ferraz <robert@portabilis.com.br>
2026-05-14 12:48:04 -0300

    Corrigida descrição das opções do campo Equipamentos que os alunos usam para acessar a internet

--- src/Modules/Educacenso/Model/EquipamentosAcessoInternet.php
+++ src/Modules/Educacenso/Model/EquipamentosAcessoInternet.php
@@ -13,9 +13,9 @@ class EquipamentosAcessoInternet
     public static function getDescriptiveValues()
     {
         return [
-            self::COMPUTADOR_MESA => 'Computadores de mesa, portáteis e tablets da escola (no laboratório de informática, biblioteca, sala de aula, etc.)',
+            self::COMPUTADOR_MESA => 'Computadores de mesa, portáteis e tablets da escola(laboratório de informática, biblioteca, salas de aula, etc)',
             self::DISPOSITIVOS_PESSOAIS => 'Dispositivos pessoais (computadores portáteis, celulares, tablets, etc.)',
-            self::AMBOS => 'Computadores de mesa, portáteis e tablets da escola e Dispositivos pessoais',
+            self::AMBOS => 'Computadores de mesa, portáteis e tablets da escola (no laboratório de informática, biblioteca, sala de aula, etc.) e Dispositivos pessoais (computadores portáteis, celulares, tablets, etc.)',
         ];
     }
 }
