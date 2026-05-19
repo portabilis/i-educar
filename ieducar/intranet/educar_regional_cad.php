6d1b38787d8bce4ef56ed47a5ee4e17db8c004d5
Edinei Valdameri <edineivaldameri@gmail.com>
2026-05-19 20:59:30 -0300

    Adiciona nova área de regionais

---
+++ ieducar/intranet/educar_regional_cad.php
@@ -0,0 +1,117 @@
+<?php
+
+use App\Models\RegionalType;
+
+return new class extends clsCadastro
+{
+    /**
+     * Referencia pega da session para o idpes do usuario atual
+     *
+     * @var int
+     */
+    public $pessoa_logada;
+
+    public $id;
+
+    public $name;
+
+    public $description;
+
+    public function Inicializar()
+    {
+        $retorno = 'Novo';
+
+        $this->id = request('id');
+
+        $obj_permissoes = new clsPermissoes;
+        $obj_permissoes->permissao_cadastra(int_processo_ap: 581, int_idpes_usuario: $this->pessoa_logada, int_soma_nivel_acesso: 3, str_pagina_redirecionar: 'educar_regional_lst.php');
+
+        if (is_numeric($this->id)) {
+            $registro = RegionalType::find($this->id)?->getAttributes();
+            if (!empty($registro)) {
+                foreach ($registro as $campo => $val) {  // passa todos os valores obtidos no registro para atributos do objeto
+                    $this->$campo = $val;
+                }
+
+                // ** verificao de permissao para exclusao
+                $this->fexcluir = $obj_permissoes->permissao_excluir(int_processo_ap: 581, int_idpes_usuario: $this->pessoa_logada, int_soma_nivel_acesso: 3);
+                // **
+
+                $retorno = 'Editar';
+            }
+        }
+        $this->url_cancelar = ($retorno == 'Editar') ? "educar_regional_det.php?cod_aluno_beneficio={$registro['id']}" : 'educar_regional_lst.php';
+
+        $nomeMenu = $retorno == 'Editar' ? $retorno : 'Cadastrar';
+
+        $this->breadcrumb(currentPage: $nomeMenu . ' regionais', breadcrumbs: [
+            url('intranet/educar_index.php') => 'Escola',
+        ]);
+
+        $this->nome_url_cancelar = 'Cancelar';
+
+        return $retorno;
+    }
+
+    public function Gerar()
+    {
+        // primary keys
+        $this->campoOculto(nome: 'id', valor: $this->id);
+
+        // text
+        $this->campoTexto(nome: 'name', campo: 'Regional', valor: $this->name, tamanhovisivel: 30, tamanhomaximo: 255, obrigatorio: true);
+        $this->campoMemo(nome: 'description', campo: 'Descrição', valor: $this->description, colunas: 60, linhas: 5);
+    }
+
+    public function Novo()
+    {
+        $classType = new RegionalType;
+        $classType->name = $this->name;
+        $classType->description = $this->description;
+
+        if ($classType->save()) {
+            $this->mensagem .= 'Cadastro efetuado com sucesso.<br>';
+            $this->simpleRedirect('educar_regional_lst.php');
+        }
+
+        $this->mensagem = 'Cadastro não realizado.<br>';
+
+        return false;
+    }
+
+    public function Editar()
+    {
+        $classType = RegionalType::findOrFail($this->id);
+        $classType->name = $this->name;
+        $classType->description = $this->description;
+
+        if ($classType->save()) {
+            $this->mensagem .= 'Edição efetuada com sucesso.<br>';
+            $this->simpleRedirect('educar_regional_lst.php');
+        }
+
+        $this->mensagem = 'Edição não realizada.<br>';
+
+        return false;
+    }
+
+    public function Excluir()
+    {
+        $classType = RegionalType::findOrFail($this->id);
+
+        if ($classType->delete()) {
+            $this->mensagem .= 'Exclusão efetuada com sucesso.<br>';
+            $this->simpleRedirect('educar_aluno_beneficio_lst.php');
+        }
+
+        $this->mensagem = 'Exclusão não realizada.<br>';
+
+        return false;
+    }
+
+    public function Formular()
+    {
+        $this->title = 'Regionais';
+        $this->processoAp = '581';
+    }
+};
---
+++ ieducar/intranet/educar_regional_lst.php
@@ -0,0 +1,99 @@
+<?php
+
+use App\Models\RegionalType;
+
+return new class extends clsListagem
+{
+    /**
+     * Referencia pega da session para o idpes do usuario atual
+     *
+     * @var int
+     */
+    public $pessoa_logada;
+
+    /**
+     * Titulo no topo da pagina
+     *
+     * @var int
+     */
+    public $titulo;
+
+    /**
+     * Quantidade de registros a ser apresentada em cada pagina
+     *
+     * @var int
+     */
+    public $limite;
+
+    /**
+     * Inicio dos registros a serem exibidos (limit)
+     *
+     * @var int
+     */
+    public $offset;
+
+    public $name;
+
+    public $description;
+
+    public function Gerar()
+    {
+        $this->titulo = 'Regionais - Listagem';
+
+        $this->name = request('name');
+
+        $this->addCabecalhos(coluna: [
+            'Regional',
+            'Descrição',
+        ]);
+
+        // outros Filtros
+        $this->campoTexto(nome: 'name', campo: 'Regional', valor: $this->name, tamanhovisivel: 30, tamanhomaximo: 255);
+
+        // Paginador
+        $this->limite = 20;
+        $this->offset = ($_GET["pagina_{$this->nome}"]) ? $_GET["pagina_{$this->nome}"] * $this->limite - $this->limite : 0;
+
+        $query = RegionalType::query()
+            ->orderBy('name');
+
+        if (is_string(value: $this->name)) {
+            $query->where(column: 'name', operator: 'ilike', value: '%' . $this->name . '%');
+        }
+
+        $result = $query->paginate(perPage: $this->limite, pageName: 'pagina_'.$this->nome);
+
+        $lista = $result->items();
+        $total = $result->total();
+
+        // monta a lista
+        if (is_array(value: $lista) && count(value: $lista)) {
+            foreach ($lista as $registro) {
+                $this->addLinhas(linha: [
+                    "<a href=\"educar_regional_det.php?id={$registro['id']}\">{$registro['name']}</a>",
+                    "<a href=\"educar_regional_det.php?id={$registro['id']}\">{$registro['description']}</a>",
+                ]);
+            }
+        }
+        $this->addPaginador2(strUrl: 'educar_regional_lst.php', intTotalRegistros: $total, mixVariaveisMantidas: $_GET, nome: $this->nome, intResultadosPorPagina: $this->limite);
+
+        $obj_permissao = new clsPermissoes;
+
+        if ($obj_permissao->permissao_cadastra(int_processo_ap: 581, int_idpes_usuario: $this->pessoa_logada, int_soma_nivel_acesso: 3)) {
+            $this->acao = 'go("educar_regional_cad.php")';
+            $this->nome_acao = 'Novo';
+        }
+
+        $this->largura = '100%';
+
+        $this->breadcrumb(currentPage: 'Regionais', breadcrumbs: [
+            url(path: 'intranet/educar_index.php') => 'Escola',
+        ]);
+    }
+
+    public function Formular()
+    {
+        $this->title = 'Regionais';
+        $this->processoAp = '950';
+    }
+};
