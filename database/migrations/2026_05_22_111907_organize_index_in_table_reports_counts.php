f478524ec8b33129d366057397e2a53a77a5011f
Edinei Valdameri <edineivaldameri@gmail.com>
2026-05-22 09:23:53 -0300

    Adiciona index na table reports_counts

---
+++ database/migrations/2026_05_22_111907_organize_index_in_table_reports_counts.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('public.reports_counts', function (Blueprint $table) {
+            $table->index(['render', 'template', 'success', 'date', 'authenticated']);
+            $table->index(['id']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('public.reports_counts', function (Blueprint $table) {
+            $table->index(['render', 'template', 'success', 'date', 'authenticated']);
+            $table->dropIndex(['id']);
+        });
+    }
+};
