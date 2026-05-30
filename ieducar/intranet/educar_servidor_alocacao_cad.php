9aee305c6e54eb194702c041f6a370e641566afe
Robert Ferraz <robert@portabilis.com.br>
2026-05-30 12:46:07 -0300

    Removida classe legada clsPmieducarFuncionarioVinculo

--- ieducar/intranet/educar_servidor_alocacao_cad.php
+++ ieducar/intranet/educar_servidor_alocacao_cad.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\LegacyBondType;
+
 return new class extends clsCadastro
 {
     public $pessoa_logada;
@@ -231,8 +233,7 @@ return new class extends clsCadastro
         $this->campoLista('cod_servidor_funcao', 'Função', $opcoes, $this->cod_servidor_funcao, '', false, '', '', false, false);
 
         // Vínculos
-        $objFuncionarioVinculo = new clsPmieducarFuncionarioVinculo;
-        $opcoes = ['' => 'Selecione'] + $objFuncionarioVinculo->lista();
+        $opcoes = ['' => 'Selecione'] + LegacyBondType::orderBy('cod_funcionario_vinculo')->pluck('nm_vinculo', 'cod_funcionario_vinculo')->all();
 
         $this->campoLista('ref_cod_funcionario_vinculo', 'Vínculo', $opcoes, $this->ref_cod_funcionario_vinculo, null, false, '', '', false, false);
 
