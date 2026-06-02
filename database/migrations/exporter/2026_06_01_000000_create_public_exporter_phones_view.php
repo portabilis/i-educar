69f8cfbcaad5e6fb69b7b86e0a870e21b79187be
Eder Soares <edersoares@me.com>
2026-06-02 16:45:27 -0300

    refactor: `exporter_phones` database view

--- database/migrations/exporter/2026_06_01_000000_create_public_exporter_phones_view.php
+++ database/migrations/exporter/2026_06_01_000000_create_public_exporter_phones_view.php
@@ -3,18 +3,18 @@
 use App\Support\Database\AsView;
 use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
+class CreatePublicExporterPhonesView extends Migration
 {
     use AsView;
 
     public function up(): void
     {
-        $this->createView('public.exporter_phones', '2024-01-11');
+        $this->dropView('public.exporter_phones');
+        $this->createView('public.exporter_phones');
     }
 
     public function down(): void
     {
         $this->dropView('public.exporter_phones');
-        $this->createView('public.exporter_phones', '2020-04-01');
     }
-};
+}
