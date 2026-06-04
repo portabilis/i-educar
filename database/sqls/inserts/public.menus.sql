19be8439923857784b0f5c091da73e037c825cbd
Robert Ferraz <robert@portabilis.com.br>
2026-06-04 05:20:18 -0300

    Removidas tela e tabela legadas de bloqueio de lançamento de notas e faltas

--- database/sqls/inserts/public.menus.sql
+++ database/sqls/inserts/public.menus.sql
@@ -73,7 +73,7 @@ INSERT INTO public.menus VALUES (115, 51, 'Séries', NULL, NULL, NULL, 6, 4, NUL
 INSERT INTO public.menus VALUES (116, 51, 'Turmas', NULL, NULL, NULL, 7, 4, NULL, 21165, 999917, true, NULL, NULL);
 INSERT INTO public.menus VALUES (118, 51, 'Calendários', NULL, NULL, NULL, 9, 4, NULL, 21169, 999917, true, NULL, NULL);
 INSERT INTO public.menus VALUES (119, 82, 'Unificação de alunos', 'Unificação de alunos', '/unificacao-aluno', NULL, 0, 4, 999847, 999847, 999927, true, NULL, NULL);
-INSERT INTO public.menus VALUES (120, 83, 'Bloqueio de lançamento de notas e faltas', 'Bloqueio de lançamento de notas e faltas', '/intranet/educar_bloqueio_lancamento_faltas_notas_lst.php', NULL, 0, 4, 999848, 999848, 999928, true, NULL, NULL);
+INSERT INTO public.menus VALUES (120, 83, 'Período de lançamento de notas e faltas por etapa', 'Período de lançamento de notas e faltas por etapa', '/periodo-lancamento', NULL, 0, 4, 999848, 999848, 999928, true, NULL, NULL);
 INSERT INTO public.menus VALUES (121, 83, 'Bloqueio do ano letivo', 'Bloqueio do ano letivo', '/intranet/educar_bloqueio_ano_letivo_lst.php', NULL, 0, 4, 21251, 21251, 999928, true, NULL, NULL);
 INSERT INTO public.menus VALUES (123, 84, 'Tipos de cor ou raça', 'Tipos de cor ou raça da pessoa', '/intranet/educar_raca_lst.php', NULL, 0, 4, 678, 21223, 999934, true, NULL, NULL);
 INSERT INTO public.menus VALUES (124, 84, 'Tipos de deficiência', 'Tipos de deficiência da pessoa', '/intranet/educar_deficiencia_lst.php', NULL, 0, 4, 631, 21170, 999934, true, NULL, NULL);
