7559f012dad92a31a389390f675cf3a824890bec
Eder Soares <edersoares@me.com>
2026-06-09 11:34:15 -0300

    refactor: reordena drops

--- database/migrations/view/2026_06_01_000000_create_exporter_database_view.php
+++ database/migrations/view/2026_06_01_000000_create_exporter_database_view.php
@@ -15,6 +15,7 @@ class CreateExporterDatabaseView extends Migration
         $this->createView('public.exporter_student');
         $this->createView('public.exporter_student_grouped_registration');
         $this->createView('public.exporter_teacher');
+        $this->createView('public.exporter_teacher_disciplines');
         $this->createView('public.exporter_employee');
         $this->createView('public.exporter_social_assistance');
         $this->createView('public.exporter_benefits');
@@ -24,14 +25,15 @@ class CreateExporterDatabaseView extends Migration
 
     public function down(): void
     {
+        $this->dropView('public.exporter_projects');
+        $this->dropView('public.exporter_disabilities');
+        $this->dropView('public.exporter_benefits');
         $this->dropView('public.exporter_social_assistance');
-        $this->dropView('public.exporter_student_grouped_registration');
         $this->dropView('public.exporter_employee');
+        $this->dropView('public.exporter_teacher_disciplines');
         $this->dropView('public.exporter_teacher');
+        $this->dropView('public.exporter_student_grouped_registration');
         $this->dropView('public.exporter_student');
         $this->dropView('public.exporter_person');
-        $this->dropView('public.exporter_benefits');
-        $this->dropView('public.exporter_disabilities');
-        $this->dropView('public.exporter_projects');
     }
 }
