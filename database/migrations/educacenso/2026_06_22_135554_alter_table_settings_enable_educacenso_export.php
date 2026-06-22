c2895899b7c8000657af9a9e9232e6536b8ba55a
Edinei Valdameri <edineivaldameri@gmail.com>
2026-06-22 14:48:41 -0300

    Adiciona migration para ativar exportação do educacenso

---
+++ database/migrations/educacenso/2026_06_22_135554_alter_table_settings_enable_educacenso_export.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Setting;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Setting::query()
+            ->where('key', 'legacy.educacenso.enable_export')
+            ->update([
+                'value' => true,
+            ]);
+    }
+};
