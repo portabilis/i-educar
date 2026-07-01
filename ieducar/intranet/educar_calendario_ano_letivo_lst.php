da695a76a7d6ee9d772d7cc63877b8e6ba7ddf60
Robert Ferraz <robert@portabilis.com.br>
2026-07-01 16:19:46 -0300

    Corrigida listagem de calendários que não exibia sem escola selecionada

--- ieducar/intranet/educar_calendario_ano_letivo_lst.php
+++ ieducar/intranet/educar_calendario_ano_letivo_lst.php
@@ -95,6 +95,10 @@ return new class extends clsListagem
             $this->ano = date(format: 'Y');
         }
 
+        if (!$this->ref_cod_escola) {
+            $this->ref_cod_escola = false;
+        }
+
         if (!$this->ref_cod_instituicao) {
             $this->ref_cod_instituicao = $obj_permissoes->getInstituicao(int_idpes_usuario: $this->pessoa_logada);
         }
