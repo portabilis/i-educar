2f052b92d8391dbbc19038601736f71c15f427d3
Edinei Valdameri <edineivaldameri@gmail.com>
2026-07-21 09:07:02 -0300

    Atualiza migrations para view's já removidas

--- database/migrations/2026_01_01_000000_cleanup.php
+++ database/migrations/2026_01_01_000000_cleanup.php
@@ -1,22 +1,11 @@
 <?php
 
-use App\Support\Database\AsView;
 use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
-    use AsView;
-
     public function up(): void
     {
-        $this->dropView('cadastro.endereco_externo');
-        $this->dropView('cadastro.endereco_pessoa');
-        $this->dropView('cadastro.v_fone_pessoa');
-        $this->dropView('cadastro.v_pessoa_fj');
-        $this->dropView('cadastro.v_pessoafj_count');
-        $this->dropView('relatorio.view_dados_modulo');
-        $this->dropView('relatorio.view_modulo');
-
         Schema::dropIfExists('pmieducar.escola_complemento');
     }
 };
