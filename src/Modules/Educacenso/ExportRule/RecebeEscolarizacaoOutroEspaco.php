72adc2c84800e64d08f98a29139632e8e185f5a4
Robert Ferraz <robert@portabilis.com.br>
2026-07-21 17:52:12 -0300

    Ajustada exportação de componentes e campos do aluno para turmas curriculares com atividade complementar

--- src/Modules/Educacenso/ExportRule/RecebeEscolarizacaoOutroEspaco.php
+++ src/Modules/Educacenso/ExportRule/RecebeEscolarizacaoOutroEspaco.php
@@ -14,7 +14,7 @@ class RecebeEscolarizacaoOutroEspaco implements EducacensoExportRule
      */
     public static function handle(RegistroEducacenso $registro60): RegistroEducacenso
     {
-        if (!in_array(TipoAtendimentoTurma::CURRICULAR_ETAPA_ENSINO, $registro60->tipoAtendimentoTurma) ||
+        if (!TipoAtendimentoTurma::possuiCurricular($registro60->tipoAtendimentoTurma) ||
             $registro60->tipoMediacaoTurma != TipoMediacaoDidaticoPedagogico::PRESENCIAL ||
             (
                 $registro60->localFuncionamentoDiferenciadoTurma != \App_Model_LocalFuncionamentoDiferenciado::NAO_ESTA &&
