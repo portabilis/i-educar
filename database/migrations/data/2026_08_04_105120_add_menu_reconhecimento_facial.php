f189a19acc59255c92e31b3081d758835793e3a1
Edinei Valdameri <edineivaldameri@gmail.com>
2026-08-04 16:47:17 -0300

    Adiciona área pra upload de fotos do reconhecimento facial

---
+++ database/migrations/data/2026_08_04_105120_add_menu_reconhecimento_facial.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Menu;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Menu::query()->updateOrCreate(['old' => 5781], [
+            'parent_id' => Menu::query()->where('old', 60)->firstOrFail()->getKey(),
+            'process' => 5781,
+            'title' => 'Reconhecimento facial',
+            'description' => 'Menu para acessar a tela de reconhecimento facial',
+            'order' => 0,
+            'parent_old' => 21205,
+            'link' => '/intranet/educar_reconhecimento_facial_det.php',
+        ]);
+    }
+
+    public function down(): void
+    {
+        Menu::query()->where('process', 5781)->delete();
+    }
+};
---
+++ ieducar/intranet/educar_reconhecimento_facial_det.php
@@ -0,0 +1,66 @@
+<?php
+
+use App\Models\Enums\FacialRecognitionPosition;
+use App\Models\LegacyStudent;
+use App\Services\FileService;
+use App\Services\UrlPresigner;
+
+return new class extends clsDetalhe
+{
+    public $titulo;
+
+    public $cod_aluno;
+
+    public $service;
+
+    public function Gerar()
+    {
+        $this->titulo = 'Reconhecimento Facial - Detalhe';
+        $this->cod_aluno = request('ref_cod_aluno');
+
+        $student = LegacyStudent::find($this->cod_aluno);
+
+        $this->service = new FileService(new UrlPresigner);
+
+        foreach (FacialRecognitionPosition::cases() as $position) {
+            $this->getDetalhe(
+                relation: $student,
+                type: $position,
+                name: $position->name()
+            );
+        }
+
+        $this->url_cancelar = "educar_aluno_det.php?cod_aluno={$this->cod_aluno}";
+        $this->url_editar = 'educar_reconhecimento_facial_cad.php?ref_cod_aluno=' . $this->cod_aluno;
+        $this->largura = '100%';
+
+        $this->breadcrumb(currentPage: 'Reconhecimento Facial - Detalhes', breadcrumbs: [
+            'educar_index.php' => 'Escola',
+        ]);
+    }
+
+    public function Formular()
+    {
+        $this->title = 'Reconhecimento Facial - Detalhes';
+        $this->processoAp = '5781';
+    }
+
+    private function getDetalhe($relation, $type, $name)
+    {
+        $files = $this->service->getFiles(
+            relation: $relation,
+            type: $type
+        );
+
+        if ($files->isNotEmpty()) {
+            $title = 'Reconhecimento Facial - ' . $name;
+            $file = $files->first();
+            $this->addDetalhe(
+                [
+                    $title,
+                    "<img src='{$file->url}' alt='{$title}' style='max-width: 200px; max-height: 200px;' />",
+                ]
+            );
+        }
+    }
+};
