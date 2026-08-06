74acfe92ab3ba788ee807fbf248f98fd57dfe952
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-08-06 14:25:59 -0300

    Adiciona fake do UpdateDisciplinesCommand
    Isola o copier usado apenas pelo teste do comando.
    
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ tests/Feature/Commands/UpdateDisciplinesCommand/RecordingMoveDisciplineDataFake.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Commands\UpdateDisciplinesCommand;
+
+use App\Services\Discipline\MoveDisciplineDataInterface;
+
+class RecordingMoveDisciplineDataFake implements MoveDisciplineDataInterface
+{
+    public static array $calls = [];
+
+    public static int $updatedResources = 7;
+
+    public function moveData($disciplineFrom, $disciplineTo, $year, $gradeId)
+    {
+        self::$calls[] = [
+            'disciplineFrom' => $disciplineFrom,
+            'disciplineTo' => $disciplineTo,
+            'year' => $year,
+            'gradeId' => $gradeId,
+        ];
+
+        return self::$updatedResources;
+    }
+
+    public static function reset(): void
+    {
+        self::$calls = [];
+        self::$updatedResources = 7;
+    }
+}
