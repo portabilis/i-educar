bb9cf7f250794d6dfd4e2fccea446da0e2ac2a19
Edinei Valdameri <edineivaldameri@gmail.com>
2026-08-18 17:39:20 -0300

    Configura limite de taxa para a API e adiciona arquivo de configuração

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,7 +35,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $this->configureRateLimiting();
+        $this->configureRateLimiting();
 
         $this->routes(function () {
             Route::prefix('api')
@@ -62,7 +62,9 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(
+                config('rate_limit.api.max_attempts')
+            )->by($request->user()?->id ?: $request->ip());
         });
     }
 }
