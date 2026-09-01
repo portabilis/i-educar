d3132747f5b511a86c38c78c5b837da97161c696
Robert Ferraz <robert@portabilis.com.br>
2026-09-01 18:21:03 -0300

    Corrigida paginação da listagem de áreas de conhecimento

--- ieducar/modules/AreaConhecimento/Views/IndexController.php
+++ ieducar/modules/AreaConhecimento/Views/IndexController.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\LegacyKnowledgeArea;
+
 class IndexController extends Core_Controller_Page_ListController
 {
-    protected $_dataMapper = 'AreaConhecimento_Model_AreaDataMapper';
+    public $limite;
 
     protected $_titulo = 'Listagem de áreas de conhecimento';
 
@@ -14,6 +16,44 @@ class IndexController extends Core_Controller_Page_ListController
         'Agrupa descritores' => 'agrupar_descritores',
     ];
 
+    public function Gerar()
+    {
+        $this->addCabecalhos(array_keys($this->getTableMap()));
+
+        $this->limite = 20;
+
+        $paginador = LegacyKnowledgeArea::query()
+            ->orderBy('nome')
+            ->paginate(perPage: $this->limite, pageName: 'pagina_' . $this->nome);
+
+        $lista = $paginador->getCollection();
+        $total = $paginador->total();
+
+        if ($lista->isNotEmpty()) {
+            foreach ($lista as $registro) {
+                $options = ['query' => ['id' => $registro->id]];
+
+                $this->addLinhas([
+                    CoreExt_View_Helper_UrlHelper::l($registro->nome, 'view', $options),
+                    CoreExt_View_Helper_UrlHelper::l($registro->secao, 'view', $options),
+                    CoreExt_View_Helper_UrlHelper::l($registro->agrupar_descritores ? 'Sim' : 'Não', 'view', $options),
+                ]);
+            }
+        }
+
+        $this->addPaginador2(
+            strUrl: '',
+            intTotalRegistros: $total,
+            mixVariaveisMantidas: $_GET,
+            nome: $this->nome,
+            intResultadosPorPagina: $this->limite
+        );
+
+        $this->setAcao();
+
+        $this->largura = '100%';
+    }
+
     protected function _preRender()
     {
         parent::_preRender();
@@ -21,17 +61,5 @@ class IndexController extends Core_Controller_Page_ListController
         $this->breadcrumb('Listagem de áreas de conhecimento', [
             url('intranet/educar_index.php') => 'Escola',
         ]);
-    }
-
-    public function getEntries()
-    {
-        $areas = $this->getDataMapper()->findAll();
-
-        foreach ($areas as $key => $area) {
-            $descriptorsGroup = $area->agrupar_descritores ? 'Sim' : 'Não';
-            $areas[$key]->agrupar_descritores = $descriptorsGroup;
-        }
-
-        return $areas;
     }
 }
